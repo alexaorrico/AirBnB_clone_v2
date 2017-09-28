6abd53a749cbf9ff563bae0e3d2b4a764ea59fa1
alexaorrico <alexaorrico@gmail.com>
2017-09-28 20:56:56 +0000

    create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,14 +6,24 @@ Contains class BaseModel
 from datetime import datetime
 import uuid
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            self.__dict__ = kwargs
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """String representation of the BaseModel class"""
@@ -26,7 +36,6 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
-        time = "%Y-%m-%dT%H:%M:%S.%f"
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = new_dict["created_at"].strftime(time)
         new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
