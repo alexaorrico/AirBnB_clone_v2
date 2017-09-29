0270366b55b53db218fa679f6cd8312c0a3495e2
alexaorrico <alexaorrico@gmail.com>
2017-09-29 22:48:43 +0000

    add create command

--- console.py
+++ console.py
@@ -3,10 +3,12 @@
 
 import cmd
 import models
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     """ HBNH console """
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     def do_EOF(self, arg):
         """Exits console"""
@@ -14,14 +16,25 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """ overwriting the emptyline method """
-        pass
+        return False
 
-    def do_quit( self, arg):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-#    def do_create(self, arg):
-
+    def do_create(self, arg):
+        """Creates a new instance of a class"""
+        classes = ["BaseModel"]
+        args = arg.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] in classes:
+            instance = eval(args[0])()
+        else:
+            print("** class doesn't exist **")
+            return False
+        print(instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
