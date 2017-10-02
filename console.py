73f025071fa249ed5be5320c9b43e04fe5504667
alexaorrico <alexaorrico@gmail.com>
2017-10-02 19:58:27 +0000

    fix do_all, add do_update, add more classes

--- console.py
+++ console.py
@@ -2,10 +2,19 @@
 """ console """
 
 import cmd
+from datetime import datetime
 import models
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.user import User
-classes = ["BaseModel", "User"]
+import shlex  # for splitting the line along spaces except in double quotes
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -26,12 +35,12 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Creates a new instance of a class"""
-        args = arg.split()
+        args = shlex.split(arg)
         if len(args) == 0:
             print("** class name missing **")
             return False
         if args[0] in classes:
-            instance = eval(args[0])()
+            instance = classes[args[0]]()
         else:
             print("** class doesn't exist **")
             return False
@@ -40,7 +49,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, arg):
         """Prints an instance as a string based on the class and id"""
-        args = arg.split()
+        args = shlex.split(arg)
         if len(args) == 0:
             print("** class name missing **")
             return False
@@ -58,11 +67,10 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, arg):
         """Deletes an instance based on the class and id"""
-        args = arg.split()
+        args = shlex.split(arg)
         if len(args) == 0:
             print("** class name missing **")
-            return False
-        if args[0] in classes:
+        elif args[0] in classes:
             if len(args) > 1:
                 key = args[0] + "." + args[1]
                 if key in models.storage.all():
@@ -76,19 +84,64 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
     def do_all(self, arg):
-        """ """
-        args = arg.split()
+        """Prints string representations of instances"""
+        args = shlex.split(arg)
+        obj_list = []
         if len(args) == 0:
-            for item in models.storage.all():
-                print(item)
-
+            for value in models.storage.all().values():
+                obj_list.append(str(value))
+            print("[", end="")
+            print(", ".join(obj_list), end="")
+            print("]")
         elif args[0] in classes:
             for key in models.storage.all():
                 if args[0] in key:
-                    print(models.storage.all()[key])
+                    obj_list.append(str(models.storage.all()[key]))
+            print("[", end="")
+            print(", ".join(obj_list), end="")
+            print("]")
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """Update an instance based on the class name, id, attribute & value"""
+        args = shlex.split(arg)
+        integers = ["number_rooms", "number_bathrooms", "max_guest",
+                    "price_by_night"]
+        floats = ["latitude", "longitude"]
+        datetimes = ["created_at", "updated_at"]
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] in classes:
+            if len(args) > 1:
+                k = args[0] + "." + args[1]
+                if k in models.storage.all():
+                    if len(args) > 2:
+                        if len(args) > 3:
+                            try:
+                                if args[2] in datetimes:
+                                    pass  # need to see how value is passed
+                                if args[0] == "Place":
+                                    if args[2] in integers:
+                                        args[3] = int(args[3])
+                                    elif args[2] in floats:
+                                        args[3] = float(args[3])
+                                    elif args[2] == "amenity_ids":
+                                        pass  # need to see how value is passed
+                            except:
+                                print("Invalid value")
+                            setattr(models.storage.all()[k], args[2], args[3])
+                            models.storage.all()[k].save()
+                        else:
+                            print("** value missing **")
+                    else:
+                        print("** attribute name missing **")
                 else:
-                    print("** class doesn't exist **")
-                    return False
+                    print("** no instance found **")
+            else:
+                print("** instance id missing **")
+        else:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
