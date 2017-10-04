5c285965011176f31a611ce4d6b1d069b92e17cf
alexaorrico <alexaorrico@gmail.com>
2017-10-04 03:33:44 +0000

    save new object in initialization

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,7 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = self.created_at
             models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """String representation of the BaseModel class"""
