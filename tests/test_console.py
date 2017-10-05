e812f985a3e15b8a29a124727033a7dd7fce70ea
alexaorrico <alexaorrico@gmail.com>
2017-10-05 00:09:09 +0000

    empty test class

---
+++ tests/test_console.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains the class TestConsole
+"""
+
+import unittest
+
+class TestConsole(unittest.TestUnit):
+    """Class for future testing of the console"""
+    pass
