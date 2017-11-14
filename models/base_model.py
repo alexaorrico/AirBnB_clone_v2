61aa9c58b195accd496b87dd02e426677c3a0497
alexaorrico <alexaorrico@gmail.com>
2017-11-14 00:44:07 +0000

    remove test print

--- models/base_model.py
+++ models/base_model.py
@@ -42,8 +42,6 @@ class BaseModel:
                 self.updated_at = datetime.utcnow()
             if kwargs.get("id", None) is None:
                 self.id = str(uuid.uuid4())
-            else:
-                print("Not supposed to be here")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
