e45ef45dd5a82610ef0c800988df6f9396f761f0
alexaorrico <alexaorrico@gmail.com>
2017-11-14 02:15:32 +0000

    add if statement for sqlalchemy table

--- models/place.py
+++ models/place.py
@@ -7,11 +7,12 @@ import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'),
-                             primary_key=True),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'), primary_key=True))
+if models.storage_t == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'), primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'), primary_key=True))
 
 
 class Place(BaseModel, Base):
