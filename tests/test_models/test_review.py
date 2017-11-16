95281ef64d10c8339b38568c800dfbb3c8546a2c
alexaorrico <alexaorrico@gmail.com>
2017-11-16 03:00:41 +0000

    fix unittests to accomodate DBStorage

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -5,6 +5,7 @@ Contains the TestReviewDocs classes
 
 from datetime import datetime
 import inspect
+import models
 from models import review
 from models.base_model import BaseModel
 import pep8
@@ -70,28 +71,39 @@ class TestReview(unittest.TestCase):
         """Test Review has attr place_id, and it's an empty string"""
         review = Review()
         self.assertTrue(hasattr(review, "place_id"))
-        self.assertEqual(review.place_id, "")
+        if models.storage_t == 'db':
+            self.assertEqual(review.place_id, None)
+        else:
+            self.assertEqual(review.place_id, "")
 
     def test_user_id_attr(self):
         """Test Review has attr user_id, and it's an empty string"""
         review = Review()
         self.assertTrue(hasattr(review, "user_id"))
-        self.assertEqual(review.user_id, "")
+        if models.storage_t == 'db':
+            self.assertEqual(review.user_id, None)
+        else:
+            self.assertEqual(review.user_id, "")
 
     def test_text_attr(self):
         """Test Review has attr text, and it's an empty string"""
         review = Review()
         self.assertTrue(hasattr(review, "text"))
-        self.assertEqual(review.text, "")
+        if models.storage_t == 'db':
+            self.assertEqual(review.text, None)
+        else:
+            self.assertEqual(review.text, "")
 
     def test_to_dict_creates_dict(self):
         """test to_dict method creates a dictionary with proper attrs"""
         r = Review()
         new_d = r.to_dict()
         self.assertEqual(type(new_d), dict)
+        self.assertFalse("_sa_instance_state" in new_d)
         for attr in r.__dict__:
-            self.assertTrue(attr in new_d)
-            self.assertTrue("__class__" in new_d)
+            if attr is not "_sa_instance_state":
+                self.assertTrue(attr in new_d)
+        self.assertTrue("__class__" in new_d)
 
     def test_to_dict_values(self):
         """test that values in dict returned from to_dict are correct"""
