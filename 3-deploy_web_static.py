4e6c02ad8a7c6a0cc425d699fd80d468cbd375c3
alexaorrico <alexaorrico@gmail.com>
2017-11-17 15:37:57 +0000

    add server info

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,8 +4,10 @@ Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
 """
 
+from fabric.api import env
 file1 = __import__('1-pack_web_static')
 file2 = __import__('2-do_deploy_web_static')
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def deploy():
