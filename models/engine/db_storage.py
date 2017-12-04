26b7d184cbde4d00e495c8719d02c414e5628606
alexaorrico <alexaorrico@gmail.com>
2017-12-04 06:29:02 +0000

    add ability to specify class using a string

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -44,7 +44,7 @@ class DBStorage:
         """query on the current database session"""
         new_dict = {}
         for clss in classes:
-            if cls is None or cls is classes[clss]:
+            if cls is None or cls is classes[clss] or cls is clss:
                 objs = self.__session.query(classes[clss]).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
@@ -69,7 +69,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
-        self.__session = Session()
+        self.__session = Session
 
     def close(self):
         """call remove() method on the private session attribute"""
