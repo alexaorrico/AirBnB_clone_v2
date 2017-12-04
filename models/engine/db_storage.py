a17b75fab865ac486bb957db5eb529e62edce7c5
alexaorrico <alexaorrico@gmail.com>
2017-12-04 05:46:32 +0000

    add close method to storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,3 +70,7 @@ class DBStorage:
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session()
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
