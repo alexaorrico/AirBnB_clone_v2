b9e7dac4126da4a7fe36733ea40de1da6a668f6d
alexaorrico <alexaorrico@gmail.com>
2017-12-04 06:31:20 +0000

    add ability to specify class using a string

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,7 +29,7 @@ class FileStorage:
         if cls is not None:
             new_dict = {}
             for key, value in self.__objects.items():
-                if cls == value.__class__:
+                if cls == value.__class__ or cls == value.__class__.__name__:
                     new_dict[key] = value
             return new_dict
         return self.__objects
