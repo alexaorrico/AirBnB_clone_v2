a0c37219bee8f87fee934e89760cdef10b377b58
anovacap <anovacap@yahoo.com>
2019-02-04 23:56:44 +0000

    stats route added for object counts

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    return ("{{\n  {}\n}}\n".format(jsonify({"status": "OK"})))
+    return (jsonify({"status": "OK"}))
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    i = 0
+    ob_d = {"amenities": "Amenity", "cities": "City", "places": "Place",
+            "reviews": "Review", "states": "State", "users": "User"}
+    count_d = {}
+    for k, v in ob_d.items():
+        count_d[k] = storage.count(v)
+    return(jsonify(count_d))
