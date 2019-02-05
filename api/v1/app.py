c15e873938954f3ee9ca381575ab410f2ac1785f
Francesca <fpcantor@gmail.com>
2019-02-05 22:35:24 +0000

    Imported getenv from os and used environment variables for running app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,4 +15,10 @@ def teardown(close):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', threaded=True)
+    host = '0.0.0.0'
+    port = 5000
+    if (getenv('HBNB_API_HOST')):
+        host = getenv('HBNB_API_HOST')
+    if (getenv('HBNB_API_PORT')):
+        port = int(getenv('HBNB_API_PORT'))
+    app.run(host=host, port=port, threaded=True)
