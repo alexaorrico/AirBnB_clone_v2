fc8c43e59375dbc27eea4d5096eb577084f273cd
anovacap <anovacap@yahoo.com>
2019-02-05 18:48:09 +0000

    delete + get_id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import jsonify
+from flask import jsonify, abort
 from api.v1.views import app_views
 from models import storage
 # from models import State
@@ -15,37 +15,19 @@ def showStates():
     return(jsonify(count_l))
 
 
-@app_views.route('/states', strict_slashes=False, methods=['GET'])
-def showStates():
-    """ show the state of the ID or 404 if not found """
-    count_l = []
-    for value in storage.all("State").values():
-        count_l.append(value.to_dict())
-    return(jsonify(count_l))
+@app_views.route('/states/<id>', strict_slashes=False, methods=['GET'])
+def a_states_id(id):
+    for s_id in storage.all('State').values():
+        if s_id.id == id:
+            return jsonify(s_id.to_dict())
+        else:
+            return (jsonify({"error": "Not found"}), 404)
 
 
-@app_views.route('/states', strict_slashes=False, methods=['DELETE'])
-def showStates():
-    """ deletes the state with the id and """
-    count_l = []
-    for value in storage.all("State").values():
-        count_l.append(value.to_dict())
-    return(jsonify(count_l))
-
-
-@app_views.route('/states', strict_slashes=False, methods=['POST'])
-def showStates():
-    """ Posts an updated state to the database """
-    count_l = []
-    for value in storage.all("State").values():
-        count_l.append(value.to_dict())
-    return(jsonify(count_l))
-
-
-@app_views.route('/states', strict_slashes=False, methods=['PUT'])
-def showStates():
-    """ updates a new state in the data base """
-    count_l = []
-    for value in storage.all("State").values():
-        count_l.append(value.to_dict())
-    return(jsonify(count_l))
+@app_views.route('/states/<id>', strict_slashes=False, methods=["DELETE"])
+def del_states_id(id):
+    for s_id in storage.all('State').values():
+        if s_id.id == id:
+            return jsonify(s_id.delete())
+        else:
+            return (jsonify({"error": "Not found"}), 404)
