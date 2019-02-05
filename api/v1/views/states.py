b9d02c209ca7b2bc6d6b7e6a0607aceef3563e18
leobyeon <386@holbertonschool.com>
2019-02-05 23:39:05 +0000

    Created states.py for api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """ States view """
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
@@ -13,10 +13,52 @@ def all_states():
     return (jsonify([state.to_dict() for state in storage.all("State").values()]))
 
 
-@app_views.route('/states/<state_id>', strict_slashes=False)
-def a_state(state_id):
+@app_views.route('/states', strict_slashes=False, methods=['POST'])
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET', 'DELETE', 'POST', 'PUT'])
+def a_state(state_id=None):
     """ retrieves a State object by id """
-    try:
-        return (jsonify(storage.all("State").pop("State." + state_id).to_dict()))
-    except KeyError:
-        abort(404)
+
+    if state_id:
+        try:
+            obj = storage.all("State").pop("State." + state_id)
+        except KeyError:
+            abort(404)
+
+        if request.method == 'GET':
+            return (jsonify(obj.to_dict()))
+
+        if request.method == 'DELETE':
+            storage.delete(obj)
+            storage.save()
+            return (jsonify({}), 200)
+
+        try:
+            data = request.get_json()
+        except:
+            return (jsonify('Not a JSON'), 400)
+
+        if request.method == 'PUT':
+            for k,v in data.items():
+                if k not in ['id', 'created_at', 'updated_at']:
+                    setattr(obj, k, v)
+            storage.new(obj)
+            storage.save()
+            return (jsonify(obj.to_dict()), 200)
+
+    else:
+
+
+        if request.method == 'POST':
+            try:
+                data = request.get_json()
+            except:
+                return (jsonify('Not a JSON'), 400)
+            if 'name' not in data.keys():
+                abort(400, 'Missing name')
+            obj = State(**data)
+            storage.new(obj)
+            storage.save()
+            return (jsonify(obj.to_dict()), 201)
+
+        else:
+            return (jsonify([state.to_dict() for state in storage.all("State").values()]))
