953c05254508d1ac897d4938fb1aa668bb59779b
sumin3 <suminyu@yahoo.com>
2019-02-05 23:52:45 +0000

    call save() method after delete a object

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -63,6 +63,7 @@ class DBStorage:
         """delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
+            self.save()
 
     def reload(self):
         """reloads data from the database"""
