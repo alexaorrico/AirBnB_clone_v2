f0a521f4a3b5e83c2b6f9f08dc3d234335412a96
Jenn <jennogden95@gmail.com>
2019-02-05 22:26:59 +0000

    ADD: cascade relationship so that if parent is deleted, children are deleted as well

--- models/place.py
+++ models/place.py
@@ -33,9 +33,13 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
+        reviews = relationship("Review", 
+                               backref="place",
+                               cascade="all, delete-orphan")
+        amenities = relationship("Amenity", 
+                                 secondary="place_amenity",
                                  backref="place_amenities",
+                                 cascade="all, delete-orphan",
                                  viewonly=False)
     else:
         city_id = ""
