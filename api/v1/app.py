41c48fcd7da5dc1c7f0e34c0e1f58d8483a40638
Jenn <jennogden95@gmail.com>
2019-02-05 00:23:33 +0000

    ADD: Handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,17 @@
 """
 Starts up a flask web app
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.teardown_appcontext
