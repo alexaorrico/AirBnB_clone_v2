98f1e04c4cd715158a49793c23db58c94712d17f
anovacap <anovacap@yahoo.com>
2019-02-05 00:27:20 +0000

    JSONIF_PP_REG added

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from os import environ
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
 
 
 @app.teardown_appcontext
