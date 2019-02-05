de11b71b0b14cbd865d0f1b66cb5e2078c26f033
byn3 <bryanleung92@gmail.com>
2019-02-05 18:36:55 +0000

    Add: creates function for passing in a disct in URL

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from flask import jsonify
+from flask import jsonify, request
 from api.v1.views import app_views
 from models import storage
-# from models import State
-# from models.base_model import BaseModel
+from models.base_model import BaseModel
+from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
@@ -15,37 +15,28 @@ def showStates():
     return(jsonify(count_l))
 
 
-@app_views.route('/states', strict_slashes=False, methods=['GET'])
-def showStates():
-    """ show the state of the ID or 404 if not found """
-    count_l = []
-    for value in storage.all("State").values():
-        count_l.append(value.to_dict())
-    return(jsonify(count_l))
-
-
-@app_views.route('/states', strict_slashes=False, methods=['DELETE'])
-def showStates():
-    """ deletes the state with the id and """
-    count_l = []
-    for value in storage.all("State").values():
-        count_l.append(value.to_dict())
-    return(jsonify(count_l))
-
-
 @app_views.route('/states', strict_slashes=False, methods=['POST'])
-def showStates():
-    """ Posts an updated state to the database """
-    count_l = []
-    for value in storage.all("State").values():
-        count_l.append(value.to_dict())
-    return(jsonify(count_l))
+def postStates():
+    """ creates a new state """
+    thing = request.get_json()
+    if thing is None or len(thing) == 0:
+        return (jsonify({"error": "Not a JSON"}), 400)
+    state = thing.get("name")
+    if state is None or len(thing) == 0:
+        return (jsonify({"error": "Missing name"}), 400)
+    s = State()
+    s.name = state
+    s.save()
+    return (jsonify(s.to_dict()), 201)
 
 
-@app_views.route('/states', strict_slashes=False, methods=['PUT'])
-def showStates():
-    """ updates a new state in the data base """
-    count_l = []
-    for value in storage.all("State").values():
-        count_l.append(value.to_dict())
-    return(jsonify(count_l))
+
+
+if __name__ == '__main__':
+    if not environ.get('HBNB_API_HOST'):
+        environ['HBNB_API_HOST'] = '0.0.0.0'
+    if not environ.get('HBNB_API_PORT'):
+        environ['HBNB_API_HOST'] = '5000'
+    app.run(host=environ['HBNB_API_HOST'],
+            port=environ['HBNB_API_PORT'],
+            threaded=True)
