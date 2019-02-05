747626925383addbc36dc16a001e81a9fcd4867f
leinefran <penny.leine@gmail.com>
2019-02-05 00:13:25 +0000

    a handler for 404 errors that returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,16 @@
 """Module"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """A function to handle page not found"""
+    return jsonify({"error": "Not found"}), 404
 
 @app.teardown_appcontext
 def teardown_db(self):
