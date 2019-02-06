5504647ac3e5d8a3b14989331d2c41506ec00679
eboayue <keboayue@gmail.com>
2019-02-06 15:02:14 -0800

    Change import from states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *  # noqa
-from api.v1.views import states  # noqa
+from api.v1.views.states import *  # noqa
