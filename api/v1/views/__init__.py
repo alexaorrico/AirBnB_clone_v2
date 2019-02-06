3fa788bb01dc076006b15e393e3cdb230e91ca89
Rd-Feng <394@holbertonschool.com>
2019-02-06 13:30:52 -0800

    space before *

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,5 +13,5 @@ if app_views:
     from api.v1.views.cities import *
     from api.v1.views.users import *
     from api.v1.views.amenities import *
-    from api.v1.views.places import*
+    from api.v1.views.places import *
     from api.v1.views.places_reviews import *
