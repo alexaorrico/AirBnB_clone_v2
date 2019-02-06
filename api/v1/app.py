175066ff4953d67d8fd30d2b32579ee48bd2ad93
Paul Lave <pmlave@gmail.com>
2019-02-06 00:31:29 +0000

    Solution to #6, still needing formatting fix for output

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ flask app web server listening on 0.0.0.0 on port 5000 """
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
+app.JSONIFY_PRETTYPRINT_REGULAR = True
 app.register_blueprint(app_views)
 
 
@@ -16,6 +16,9 @@ app.register_blueprint(app_views)
 def teardown_app(resp):
     storage.close()
 
+@app.errorhandler(404)
+def _handle_api_error(ex):
+    return (jsonify({'error': 'Not found'}), 404)
 
 HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
 PORT = os.getenv('HBNB_API_PORT', '5000')
