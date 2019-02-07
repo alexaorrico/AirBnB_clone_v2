434dd2178fe9432191a406e063a89b3730f03326
JackWanaCode <kazucuong@yahoo.com>
2019-02-07 19:45:01 +0000

    update with place_amenity

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,4 +12,4 @@ if app_views:
     from api.v1.views.amenities import *
     from api.v1.views.users import *
     from api.v1.views.places import *
-    
+    from api.v1.views.places_amenities import *
