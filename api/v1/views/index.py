1f4d9fce0c35e4b0d8ad73a304bffabede1bc978
leobyeon <386@holbertonschool.com>
2019-02-07 00:21:55 +0000

    Fixed pep8 errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,13 +16,16 @@ from models.user import User
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ returns JSON string """
     return (jsonify({"status": "OK"}))
 
+
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ retrieves the number of each objects by type """
-    stat_objs = { v.__tablename__: storage.count(k) for k,v in classes.items() }
+    stat_objs = {
+            v.__tablename__: storage.count(k) for k, v in classes.items()}
     return (jsonify(stat_objs))
