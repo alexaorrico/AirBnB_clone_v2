072ac6bd40aa63ce8c69612d4d4eef3c2d8ce18b
aslebloas <aslebloas@gmail.com>
2019-02-08 01:12:51 +0000

    Fixed reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -13,17 +13,20 @@ from models.review import Review
                  strict_slashes=False)
 def get_review(place_id):
     """Retrieves the list of all Review objs of a Place by place_id"""
-    review = storage.get('Review', str(place_id))
+    place = storage.get('Place', place_id)
+    list_of_reviews = []
     if place is None:
         abort(404)
-    return jsonify(review.to_dict())
+    for review in place.reviews:
+        list_of_reviews.append(element.to_dict())
+    return jsonify(list_of_reviews)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'],
                  strict_slashes=False)
 def get_review_by_review_id(review_id):
     """Retrieves a Review object by review_id"""
-    review = storage.get('Review', str(review_id))
+    review = storage.get('Review', review_id)
     if review is None:
         abort(404)
     return jsonify(review.to_dict())
@@ -33,7 +36,7 @@ def get_review_by_review_id(review_id):
                  strict_slashes=False)
 def delete_review_by_id(review_id):
     """Deletes a review by ID"""
-    review = storage.get('Review', str(review_id))
+    review = storage.get('Review', review_id)
     if review is None:
         abort(404)
     storage.delete(review)
@@ -45,6 +48,9 @@ def delete_review_by_id(review_id):
                  strict_slashes=False)
 def create_review(place_id):
     """Post a Review object"""
+    place = storage.get('Place', place_id)
+    if place is None:
+        abort(404)
     data = request.get_json(silent=True)
     if not data:
         abort(400)
@@ -52,7 +58,10 @@ def create_review(place_id):
     if 'name' not in data:
         abort(400)
         abort(Response("Missing name"))
+    data['place_id'] = place_id
     new_review = Review(**data)
+    storage.new(new_review)
+    storage.save()
     return jsonify(new_review.to_dict()), 201
 
 
@@ -60,10 +69,15 @@ def create_review(place_id):
                  strict_slashes=False)
 def put_review(review_id):
     """Put a Review object"""
+    review = storage.get('Review', review_id)
+    if review is None:
+        abort(404)
+
     data = request.get_json(silent=True)
     if not data:
         abort(400)
         abort(Response("Not a JSON"))
+
     new_review = Review(**data)
     storage.new(new_review)
     storage.save()
