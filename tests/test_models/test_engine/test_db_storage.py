89f9ba86ac46130c64dcbdf25a9ac08b7b6f6c71
BennettDixon <bennettdixon16@gmail.com>
2019-04-29 21:08:06 +0000

    basic tests for count and get of db storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,24 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage"
+    def test_get(self):
+        """Test that the get properly gets objects"""
+        obj = State()
+        got_obj = models.storage.get('State', obj.id)
+        self.assertIs(obj, got_obj)
+
+        got_obj = models.storage.get('ls', obj.id)
+        self.assertIsNot(obj, got_obj)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage"
+    def test_count(self):
+        """Test that the count properly counts objects"""
+        got_obj_count = models.storage.count()
+        obj_count = len(models.storage.all())
+        self.assertEqual(got_obj_count, obj_count)
+
+        got_obj_count = models.storage.count('State')
+        obj_count = len(models.storage.all('State'))
+        self.assertEqual(got_obj_count, obj_count)
