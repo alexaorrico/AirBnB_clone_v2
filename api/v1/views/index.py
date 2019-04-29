0c44bd3c55c770e74e7a91bd4aed57b9c04ee3b9
christopherchoe <chchchoe@gmail.com>
2019-04-29 22:35:23 +0000

    added stats method, returns count of different classe

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -11,3 +12,19 @@ def service_status():
     """ TODO check if this formatting is okay for json response with
         holberton checker """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', strict_slashes=False)
+def object_stats():
+    """returns the count of object types"""
+
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+        }
+    for k, v in classes.items():
+        classes[k] = storage.count(v)
+    return jsonify(classes)
