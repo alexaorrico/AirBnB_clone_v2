9aadccc0484e4ea6dc357c3eb4fb4ef1b6725682
BennettDixon <bennettdixon16@gmail.com>
2019-04-29 21:41:46 +0000

    added app startup for flask and registered blue print

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import jsonify
 from models.status import Status
 
+
 @app_views.route('/status', strict_slashes=False)
 def service_status():
     """returns the status of the RESTful service"""
