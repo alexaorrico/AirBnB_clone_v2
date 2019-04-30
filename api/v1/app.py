9cc0b277f6a0fcdfaa71a58b9b57504a41dc9c44
sazad44 <530@holbertonschool.com>
2019-04-30 04:56:08 +0000

    Uploading api directory and tests

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Py module utilizes flask to run app"""
+from flask import Flask, Blueprint
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+
+
+app_views = Blueprint("app_views", __name__)
+
+
+@app.teardown_appcontext
+def tearDown():
+    """tearDown method"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=getenv('HBNB_API_HOST'), port=int(getenv('HBNB_API_PORT')),
+            threaded=True)
---
+++ tests/test_api/test_v1/test_app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Test file for app.py"""
+import os
+import flaskr
+import unittest
+import tempfile
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """setUp method for Flask test class"""
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        flaskr.app.testing = True
+        self.app = flaskr.app.test_client()
+        with flaskr.app.app_context():
+            flaskr.init_db()
+
+    def tearDown(self):
+        """tearDown method for flask unittest"""
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config["DATABASE"])
+
+    def test_empty_db(self):
+        """Testing for no entries in empty databases"""
+        rv = self.app.get('/')
+        self.assertEqual("No entries here so far", rv.data)
+
+if __name__ == "__main__":
+    print("RAN TEST)
+    unittest.main()
