6609a474df8c84cac2350a211e86801eac50bca2
Andrew Lindburg <atlindburg@gmail.com>
2019-04-30 20:31:57 +0000

    6 is basically done. Connection is closed when the second local test is run. I am not sure if that's normal behavior. Json output still not formatted correctly

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """Starts flask"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
-
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
+
 
 @app.teardown_appcontext
 def tear_down(n):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(host=host, port=port, threaded=True)
