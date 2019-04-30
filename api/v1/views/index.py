7e2966f2300ff32ed9799795e978a51cfdc8880b
bdbaraban <375@holbertonschool.com>
2019-04-30 21:59:57 +0000

    Define route /stats returning a JSON object with counts of all storage objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,13 +2,30 @@
 """Defines a status route for the HolbertonBnB API."""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
-def server_status():
+def status():
     """Returns the server status.
 
     Returns:
         JSON object with the current server status.
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """Retrives the count of each object type.
+
+    Returns:
+        JSON object with the number of objects by type."""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
