dddbe01194241091b223f8b1ca382a9a7611c295
Andrew Lindburg <atlindburg@gmail.com>
2019-04-30 20:20:32 +0000

    Outputting incorrectly formatted json of class counts

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
+"""Routing for AirBnB"""
 
 
 @app_views.route('/status', methods=['GET'])
@@ -8,6 +9,30 @@ def status():
     if request.method == 'GET':
         return jsonify({'status': 'OK'})
 
-@app_views.route('/api/v1/stats', methods=['GET'])
+
+@app_views.route('/stats', methods=['GET'])
 def stats():
-    return jsonify(storage.count())
+    dic = {}
+    classes = [
+        "Amenity",
+        "City",
+        "Place",
+        "Review",
+        "State",
+        "User"
+    ]
+    class_plural = [
+        "amenities",
+        "cities",
+        "places",
+        "reviews",
+        "states",
+        "users"
+    ]
+    j = 0
+    for i in classes:
+        count = storage.count(i)
+        dic[class_plural[j]] = count
+        j += 1
+    if request.method == 'GET':
+        return jsonify(dic)
