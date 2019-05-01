68d7897677dd30c331c523d2dc293f565cb0c146
Martin Smith <msmith3dart@icloud.com>
2019-05-01 00:15:46 +0000

    updated api test
    opted to use subprocess to load a dump file rather than SQLAlchemy

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,7 +8,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 from sqlalchemy.orm import sessionmaker
-
+import subprocess
 
 class AirbnbTestCase(unittest.TestCase):
 
@@ -17,24 +17,14 @@ class AirbnbTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """setup the class for testing"""
-        print("flask test")
-        cls.__engine = create_engine(
-            "mysql+mysqldb://{}:{}@{}/{}".format(
-                os.getenv("HBNB_MYSQL_USER"),
-                os.getenv("HBNB_MYSQL_PWD"),
-                os.getenv("HBNB_MYSQL_HOST"),
-                os.getenv("HBNB_MYSQL_DB")
-            ),
-            pool_pre_ping=True
-        )
-        Base.metadata.create_all(cls.__engine)
-        # with cls.__engine.connect() as con:
-        # with open("100-dump.sql", encoding='utf-8') as f:
-        #   dump = text(f.read())
-        # con.execute()
+        """setup the class for testing with DBSTORAGE"""
+        if os.getenv("HBNB_TYPE_STORAGE") == "db":
+            with open("tests/100-dump.sql", "r") as f:
+                cmd = ["mysql", "-uroot", "-pdocker"]
+                proc = subprocess.Popen(cmd, stdin=f)
+                err, out = proc.communicate()
         cls.__app = app.test_client()
-
+    
     def test_server_status(self):
         """Test if Status of Api returns a json"""
         with self.__app as a:
@@ -50,17 +40,13 @@ class AirbnbTestCase(unittest.TestCase):
             msg = resp.get_json()
             self.assertEqual(msg["status"], "OK")
 
-    @unittest.skip("Route unimplemented")
     def test_some_stats(self):
         """Test if the responce data is json"""
         with self.__app as a:
             resp = a.get("/api/v1/stats")
             self.assertTrue(resp.get_json(silent=True))
 
-    @unittest.skipIf(
-        os.getenv("HBNB_MYSQL_DB") == "db",
-        "Not using File storage"
-    )
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == "db","Not using File storage")
     def test_some_stats_count(self):
         """Test the count method for FileStorage and the API call to stats
         The responce message is also tested that the format matches 
