8f07b64563b85db8b55d55d5e74598d59b6b6c6f
bdbaraban <375@holbertonschool.com>
2019-05-01 20:29:50 +0000

    Clean up typos in docstring documentation

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -8,15 +8,14 @@ from models.amenity import Amenity
 
 @app_views.route("/amenities", methods=["GET", "POST"])
 def amenities():
-    """Defines the GET and POST method for amenities route.
+    """Defines GET and POST methods for the /amenities route.
 
-    GET - Retries a list of all the Amenity objects.
-    POST - Create a Amenity.
+    GET - Retrieves a list of all Amenity objects.
+    POST - Creates a Amenity.
     """
-
     # GET method
     if request.method == "GET":
-        return jsonify([s.to_dict() for s in storage.all("Amenity").values()])
+        return jsonify([a.to_dict() for a in storage.all("Amenity").values()])
 
     # POST method
     data = request.get_json(silent=True)
@@ -31,11 +30,11 @@ def amenities():
 
 @app_views.route("/amenities/<amenity_id>", methods=["GET", "PUT", "DELETE"])
 def amenity_id(amenity_id):
-    """Defines the GET, PUT and DELETE methods for a spacific ID on amenities.
+    """Defines GET, PUT and DELETE methods for a specific ID on /amenities.
 
-    GET - Retrieves a Amenity object with the given id.
-    PUT - Updates a Amneity object with the given id using a json key/value
-    DELETE = Deletes a Amenity object with the given id.
+    GET - Retrieves an Amenity object with the given id.
+    PUT - Updates an Amenity object with the given id using JSON key/values.
+    DELETE - Deletes an Amenity object with the given id.
     """
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None:
