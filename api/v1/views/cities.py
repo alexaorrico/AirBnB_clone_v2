f3bb1a5a3fcc8f5a619a11f1ffb01447251835b7
stefansilverio <494@holbertonschool.com>
2019-05-01 17:35:25 +0000

    working on POST route

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+ #!/usr/bin/python
 """holds class City"""
 from models.state import State
 from models.city import City
@@ -14,16 +14,18 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 cities = storage.all(City).values()
 
 
-@app_views.route("states/<state_id>/cities", strict_slashes=False)
+@app_views.route("states/<state_id>/cities", strict_slashes=False, methods=['GET'])
 def all_cities(state_id):
     """grab all cities in a state"""
+    print(state_id)
     obj = [city.to_dict() for city in cities if city.state_id == state_id]
+    print("here she is {}".format(obj))
     if len(obj) == 0:
         abort(404)
     return jsonify(obj)
 
 
-@app_views.route("cities/<city_id>")
+@app_views.route("cities/<city_id>", strict_slashes=False, methods=['GET'])
 def get_city_obj(city_id):
     """retrieve city obj"""
     obj = [city.to_dict() for city in cities if city.id == city_id]
@@ -36,7 +38,6 @@ def get_city_obj(city_id):
 def delete_city(city_id):
     """delete a city"""
     obj = storage.get("City", city_id)
-    print(obj)
     if obj is not None:
         storage.delete(obj)
         storage.save()
@@ -47,3 +48,18 @@ def delete_city(city_id):
 @app_views.route("states/<state_id>/cities",
                  strict_slashes=False,
                  methods=['POST'])
+def create_city(state_id):
+    """create new city obj"""
+    data = request.get_json()
+    if 'name' not in data.keys():
+        abort 404
+    try:
+        json_obj = json.loads(data)
+    except ValueError as e:
+        abort 400
+    new_city = City(state_id=state_id)
+    cities = storage.all(City)
+    for city in cities.values():
+        if city.state_id == state_id:
+            return new_city, 201
+    abort 404
