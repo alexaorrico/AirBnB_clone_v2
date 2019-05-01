214aefe8b678fd0ea4b6c3ccd82b42ca051a5861
bdbaraban <375@holbertonschool.com>
2019-05-01 01:23:21 +0000

    Condense multi-line flask imports into one comma-separated line

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """HolbertonBnB State view."""
 from api.v1.views import app_views
-from flask import abort
-from flask import jsonify
-from flask import request
+from flask import abort, jsonify, request
 from models import storage
 from models.state import State
 
