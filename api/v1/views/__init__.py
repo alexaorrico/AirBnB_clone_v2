e7d8a98dabcd9a4b83085cabba41acf79187d3a9
wescottsharples <wesc@ttsharpl.es>
2019-05-02 19:34:48 +0000

    Add cities import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,4 +4,5 @@ from flask import Blueprint
 app_views = Blueprint('app_voews', __name__, url_prefix="/api/v1")
 
 from api.v1.views import *
+from api.v1.views.cities import *
 from api.v1.views.index import *
