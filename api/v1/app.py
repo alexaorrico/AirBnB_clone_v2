b40f95de4334debdeac5d0dfc6bae9e2229f7597
stak21 <stakashima21@gmail.com>
2019-05-02 01:14:23 +0000

    Added json pretify config && added a parameter exception to teardown

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,11 @@ from os import environ
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url="/app_views")
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exception):
     """calls storage.close"""
     storage.close()
 
