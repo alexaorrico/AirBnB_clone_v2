1afe322f2b79b07db9ebd4385a877a91103d2b25
stefansilverio <494@holbertonschool.com>
2019-05-02 04:43:34 +0000

    fixed error codes & integrated get method

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -48,20 +48,19 @@ def delete_city(city_id):
                  methods=['POST'])
 def create_city(state_id):
     """create new city obj"""
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
     data = request.get_json()
-    if 'name' not in data.keys():
-        abort(404, "Missing name")
     if data is None:
         abort(400, "Not a JSON")
+    if 'name' not in data.keys():
+        abort(400, "Missing name")
     new_city = City(**data)
     setattr(new_city, 'state_id', state_id)
     storage.new(new_city)
     storage.save()
-    cities = storage.all(City)
-    for city in cities.values():
-        if city.state_id == state_id:
-            return jsonify(new_city.to_dict()), 201
-    abort(404)
+    return jsonify(new_city.to_dict()), 201
 
 
 @app_views.route("/cities/<city_id>", strict_slashes=False,
@@ -69,10 +68,11 @@ def create_city(state_id):
 def update_city(city_id):
     """create or update: idempotent"""
     data = request.get_json()
+    city = storage.get("City", city_id)
+    if city is None:
+        abort(404)
     if data is None:
         abort(400, "Not a JSON")
-    if 'name' not in data.keys():
-        abort(404, "Missing name")
     city = storage.get(City, city_id)
     for k, v in data.items():
         if k != "id" and k != "created_at" and k != "updated_at":
