4848af385f03b2dc07f4db5866a01681dfbe4941
Wescott Sharples <wesc@ttsharpl.es>
2019-05-03 00:57:33 +0000

    Add post method

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -44,6 +44,25 @@ def delete_city(city_id):
 
 
 @app_views.route(
+    '/states/<state_id>/cities',
+    methods=['POST'],
+    strict_slashes=False)
+def post_city(state_id):
+    state = storage.get('State', state_id)
+    city_dict = request.get_json()
+    if not city_dict:
+        return (jsonify({'error': 'Not a JSON'}), 400)
+    elif 'name' not in city_dict:
+        return (jsonify({'error': 'Missing name'}), 400)
+    if state:
+        city_dict['state_id'] = state.id
+        city = City(**city_dict)
+        city.save()
+        return (jsonify(city.to_dict()), 201)
+    abort(404)
+
+
+@app_views.route(
     '/cities/<city_id>',
     methods=['PUT'],
     strict_slashes=False)
