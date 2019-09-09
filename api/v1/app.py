293a03395b83b598870a756aab2a261fc3edfd96
vdphan <duyphan0@gmail.com>
2019-09-09 23:44:10 +0000

    add task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,14 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
+from flask import make_response
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
