2ed1bc76f61e08c047fdb0c12c36a7a63ae01a9d
andres-martin <andresmartin266@gmail.com>
2019-09-09 20:56:00 +0000

    [feature] get and count methods implemented in db and file storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,22 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        ''' Returns the object based on the class name and its ID
+            , or None if not found
+        '''
+        if cls is not None:
+            objects = self.all(cls)
+        key = cls + '.' + id
+        if key in objects:
+            return objects[key]
+        return None
+
+    def count(self, cls=None):
+        ''' Returns the number of objects in storage
+            matching the given class name. If no name is passed,
+            returns the count of all objects in storage.
+        '''
+        objects = self.all(cls)
+        return len(objects)
