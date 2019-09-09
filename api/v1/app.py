fa1b24ddc16a106089758169825ebe36ebfb4bff
Ferney Medina <fermed28@gmail.com>
2019-09-09 23:57:01 +0000

    [add] status end point

---
+++ api/v1/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+""" API class """
+from models import storage
+from api.v1.views import app_views
+from flask import Flask
+from os import getenv
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=getenv('HBNB_API_HOST') or '0.0.0.0',
+            port=getenv('HBNB_API_PORT') or 5000, threaded=True)
