8f644608173e9baf4d40b0ced451952091c466a4
yawzyag <yesid.yag@gmail.com>
2019-09-09 22:20:07 +0000

    [Fix] changin the order of import and adding comments, pep8 etc

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,17 +2,21 @@
 """
 starts a Flask web application
 """
-from api.v1.views import app_views
+
 from models import storage
 from flask import Flask
+from api.v1.views import app_views
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
+
 if __name__ == '__main__':
+    """init of the flask app"""
     app.run(host='0.0.0.0', port='5000', threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""init for creating the blueprint"""
 from flask import Blueprint
-import api.v1.views.index
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
