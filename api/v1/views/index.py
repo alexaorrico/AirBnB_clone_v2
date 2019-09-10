c1626663ee8561435f5ffd0c1e4317b6d649317a
OrangeB0lt <667@holbertonschool.com>
2019-09-10 16:56:02 -0400

    [AirBnB v3][Update][index] Update index for use on count method for the named class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,27 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
 
+FileToText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place"
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 @app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
     ''' hbnb Status '''
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    ''' hbnb Stats '''
+    return_dict = {}
+    for key, value in FileToText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
+if __name__ == "__main__":
+    pass
