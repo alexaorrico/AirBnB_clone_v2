6ff91ca321e8a0ef5023a52174ae26711df23350
OrangeB0lt <667@holbertonschool.com>
2019-09-10 17:00:09 -0400

    [AirBnB v3][Update][app] Update app for use on 404 pages

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,10 @@ def teardown_appcontext(code):
     'closes storage method'
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == "__main__":
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(getenv('HBNB_API_PORT', '5000')))
