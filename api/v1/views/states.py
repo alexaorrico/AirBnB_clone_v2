bf0d7d2f552392e25478a8405a77169eb3989583
Rmolimock <495@holbertonschool.com>
2019-09-10 03:42:32 +0000

    handle delete instance of a state requests, also deletes associated cities

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+from flask import jsonify, request, abort
 from models.state import State
 
 
-@app_views.route('/states/', methods=['GET'])
-def list_all_states():
+@app_views.route('/states/', methods=['GET'], strict_slashes=False)
+def get_all_states():
     data = storage.all(State)
     new = [val.to_dict() for key, val in data.items()]
     return jsonify(new)
 
-@app_views.route('/states/<state_id>', methods=['GET'])
-def specific_state(state_id=None):
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id=None):
     ''' returns an individual state object '''
     obj = storage.get(State, state_id)
+    if obj is None:
+        ''' if no state obj with that id '''
+        abort(404, 'Not found')
     obj = obj.to_dict()
     return jsonify(obj)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id=None):
+    obj = storage.get(State, state_id)
+    if obj is None:
+        ''' if no state obj with that id '''
+        abort(404, 'Not found')
+
+    obj.delete()
+    storage.save()
+    storage.close()
+    return jsonify({})
