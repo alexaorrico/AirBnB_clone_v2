0e93ab8f7edbb579e7cc21a425253fa9103ab749
Diegokernel <777@holbertonschool.com>
2019-09-10 17:06:05 -0500

    add file storage features

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        '''Returns the object based on the class name and its ID, or None if not found'''
+        for key, values in self._objects.items():
+            if cls in key and str(id) in key:
+                return values
+            else:
+                return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class name"""
+        return len(self.all(cls))
