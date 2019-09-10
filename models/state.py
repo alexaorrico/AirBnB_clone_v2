859fe67b67f92595b594dbd833029e06c5657c5a
Arthur Damm <indigoarthur@gmail.com>
2019-09-10 05:15:10 +0000

    Fix relationships to delete-orphan on cascade

--- models/state.py
+++ models/state.py
@@ -14,7 +14,9 @@ class State(BaseModel, Base):
     if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all,delete-orphan")
     else:
         name = ""
 
