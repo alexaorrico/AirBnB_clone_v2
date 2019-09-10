7bdca2729bc86358d358c1d5c94fe5eb1b325b28
Arley Brinez <arleybri18@gmail.com>
2019-09-10 21:25:30 +0000

    [Fix] fix case when not exist objects

--- test_get_count.py
+++ test_get_count.py
@@ -6,5 +6,6 @@ from models import storage
 print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count("State")))
 
-first_state_id = list(storage.all("State").values())[0].id
+first_state_id = '1s'
+#first_state_id = list(storage.all("State").values())[0].id
 print("First state: {}".format(storage.get("State", first_state_id)))
