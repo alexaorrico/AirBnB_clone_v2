7c0039514593d6885d7f028e31d44310168f8abf
Rmolimock <495@holbertonschool.com>
2019-09-10 01:45:36 +0000

    handle get request for specific State obj

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,8 +7,13 @@ from models.state import State
 
 @app_views.route('/states/', methods=['GET'])
 def list_all_states():
-    data = storage.all('State')
+    data = storage.all(State)
     new = [val.to_dict() for key, val in data.items()]
     return jsonify(new)
 
-
+@app_views.route('/states/<state_id>', methods=['GET'])
+def specific_state(state_id=None):
+    ''' returns an individual state object '''
+    obj = storage.get(State, state_id)
+    obj = obj.to_dict()
+    return jsonify(obj)
