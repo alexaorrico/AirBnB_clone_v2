acd5fd6853c2e43e6596b37b70fb68dd2db90198
Arthur Damm <indigoarthur@gmail.com>
2019-09-11 21:54:45 +0000

    Add CORS attempt

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,11 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask import make_response
+from flask_cors import CORS
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app)
 
 
 @app.teardown_appcontext
