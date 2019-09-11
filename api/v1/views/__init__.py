4bdb17df806f217943afe726c695935972144e14
HausCloud <hauclouds@gmail.com>
2019-09-11 18:43:13 +0000

    Move import back

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 ''' Initializes Blueprint views for API '''
 from flask import Blueprint
-from api.v1.views.index import *
+
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+from api.v1.views.index import *
 # from api.v1.views.amenities import *
 # from api.v1.views.cities import *
 # from api.v1.views.places import *
