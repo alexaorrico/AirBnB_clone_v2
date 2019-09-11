9a64c4e85cf088450a64d10bc0f18fa166ea2cee
andres-martin <andresmartin266@gmail.com>
2019-09-11 22:08:11 +0000

    [feature] CORS enable

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,12 @@ import os
 from models import storage
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask_cors import CORS
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
