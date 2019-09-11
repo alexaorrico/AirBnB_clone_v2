020b2f73dd6e10aece4b24c8dec66373a63b81b4
jembantonio <jembantonio@gmail.com>
2019-09-11 22:58:39 +0000

    syntax error fixes and error fix for same function name

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,10 +6,10 @@ from flask import jsonify, request, abort
 from models.amenity import Amenity
 
 
-@app_views.route('/amenities/', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_all_amenities():
     ''' get all amenity objs '''
-    data = storage.all(Amenity)
+    data = storage.all('Amenity')
     new = [val.to_dict() for key, val in data.items()]
     return jsonify(new)
 
@@ -19,7 +19,7 @@ def get_all_amenities():
                  strict_slashes=False)
 def get_amenity(amenity_id=None):
     ''' returns an individual amenity object '''
-    obj = storage.get(Amenity, amenity_id)
+    obj = storage.get('Amenity', amenity_id)
     if obj is None:
         ''' if no state obj with that id '''
         abort(404, 'Not found')
@@ -32,17 +32,17 @@ def get_amenity(amenity_id=None):
                  strict_slashes=False)
 def delete_amenity(amenity_id=None):
     ''' deletes an individual amenity '''
-    obj = storage.get(Amenity, amenity_id)
+    obj = storage.get('Amenity', amenity_id)
     if obj is None:
         ''' if no state obj with that id '''
         abort(404, 'Not found')
 
-    obj.delete()
+    storage.delete(obj)
     storage.save()
     return jsonify({}), 200
 
 
-@app_views.route("/amenities/", methods=["POST"], strict_slashes=False)
+@app_views.route("/amenities", methods=["POST"], strict_slashes=False)
 def create_amenity():
     ''' create an amenity if doesn't already exist '''
     args = request.get_json()
@@ -59,9 +59,9 @@ def create_amenity():
 @app_views.route('/amenities/<amenity_id>',
                  methods=['PUT'],
                  strict_slashes=False)
-def update_state(state_id=None):
+def update_amenity(amenity_id=None):
     ''' updates an individual amenity '''
-    obj = storage.get(Amenity, amenity_id)
+    obj = storage.get('Amenity', amenity_id)
     if obj is None:
         ''' if no state obj with that id '''
         abort(404, 'Not found')
