51d46a610c03602b23a8ca2db86bf9176cef5a8e
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-09-10 21:16:25 -0500

    [Feature] Added 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import jsonify
 import os
 
 
@@ -19,6 +20,12 @@ p = os.environ.get("HBNB_API_PORT", "5000")
 @app.teardown_appcontext
 def close(exception=None):
     storage.close()
+
+
+@app.errorhandler(404)
+def _handle_api_error(ex):
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(host=str(h), port=int(p), threaded=True, debug=True)
