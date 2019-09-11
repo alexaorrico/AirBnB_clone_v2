665a53178fe11bbd48e7c7cb42e81eb4c4941a70
naxus1 <naxus1@gmail.com>
2019-09-11 18:09:23 +0000

    [Add]: funtion page_not_found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Create conection to server"""
 
 from models import storage
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 import os
 
@@ -17,6 +17,13 @@ port = int(os.environ.get("HBNB_API_PORT", 5000))
 def teardown(exception=None):
     """declare a method to handle @app.teardown_appcontext """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Handler 404"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(threaded=True, debug=True, host=host, port=port)
