71aadf2188b69781763a64f500e9a160cbf6a547
Kelly Hodis <668@holbertonschool.com>
2019-09-11 21:27:58 +0000

    Create dictionary

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,4 +7,4 @@ from flask import jsonify
 @app_views.route('/status', strict_slashes=False)
 def status():
     '''Returns a JSON with status: OK'''
-    return jsonify({"status": "OK"})
+    return jsonify(dict(status="OK"))
