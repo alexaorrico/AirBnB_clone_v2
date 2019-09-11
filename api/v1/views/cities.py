5a214faf15b20c867002cfa54aea5d0cbe5cb1fa
jembantonio <jembantonio@gmail.com>
2019-09-11 20:53:37 +0000

    retrieves a city object given a city id

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,7 +3,7 @@ from api.v1.views import app_views
 from models import storage
 from flask import jsonify, request, abort
 from models.state import State
-
+from models.city import City
 
 @app_views.route('/states/<state_id>/cities',
                  methods=['GET'],
@@ -25,6 +25,19 @@ def get_all_cities(state_id=None):
     return jsonify(city_list)
 
 
+@app_views.route('/cities/<city_id>',
+                 methods=['GET'],
+                 strict_slashes=False)
+def get_city(city_id=None):
+    ''' returns an individual state object '''
+    obj = storage.get('City', city_id)
+    if obj is None:
+        ''' if no state obj with that id '''
+        abort(404, 'Not found')
+    obj = obj.to_dict()
+    return jsonify(obj)
+
+
 
 
 
@@ -34,16 +47,6 @@ def get_all_states():
     data = storage.all(State)
     new = [val.to_dict() for key, val in data.items()]
     return jsonify(new)
-
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id=None):
-    ''' returns an individual state object '''
-    obj = storage.get(State, state_id)
-    if obj is None:
-        ''' if no state obj with that id '''
-        abort(404, 'Not found')
-    obj = obj.to_dict()
-    return jsonify(obj)
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
