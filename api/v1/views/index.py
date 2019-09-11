5bfba26b706957e4559e34817750f02d77530063
ethanpasta <ethan.mayer9@gmail.com>
2019-09-11 18:00:53 +0000

    Pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,15 +3,17 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-from  models import storage
+from models import storage
 
 classes = {"amenities": "Amenity", "cities": "City", "places": "Place",
            "reviews": "Review", "states": "State", "users": "User"}
+
 
 @app_views.route('/status', strict_slashes=False)
 def get_status_route():
     """ Returns first json object """
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', strict_slashes=False)
 def get_obj_count_route():
