441ad8e73ed21278616a93bf5c7a1d4cfa00d416
Ferney Medina <fermed28@gmail.com>
2019-09-11 19:38:56 +0000

    [fixes] delete pycahe and add stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,26 @@
 """ Index of several Class for JSON API """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
 def status():
     """ Return status ok """
     return jsonify(status='ok')
+
+
+@app_views.route('/stats')
+def stats():
+    """ Return status ok """
+    return jsonify(amenities=storage.count(Amenity),
+                   cities=storage.count(City),
+                   places=storage.count(Place), reviews=storage.count(Review),
+                   states=storage.count(State), users=storage.count(User))
