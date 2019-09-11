90f4031bf85c7ad836f6f8e74b3739cd97f3ee70
Cyn <cmdominguez@gmail.com>
2019-09-11 20:53:50 +0000

    remove index.py from v1

--- api/v1/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''Creates various routes and returns respective JSONs'''
-from api.v1.views import app_views
-
-
-@app_views.route('/status', strict_slashes=False)
-def status():
-    '''Returns a JSON with status: OK'''
-    return {"status": "OK"}
