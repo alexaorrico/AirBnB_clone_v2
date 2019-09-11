db1b3505450480eb08a1712dfa17f2d96b8ff0f9
ethanpasta <ethan.mayer9@gmail.com>
2019-09-11 07:11:23 +0000

    Task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """ Module for first API """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -9,6 +10,7 @@ from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
