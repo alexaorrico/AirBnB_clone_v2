1a2839e323e6275022c1f7ad03c236e50a72540d
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-09-10 20:22:51 -0500

    [Feature] Stats added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -11,3 +12,15 @@ def view_json():
     """
     return jsonify({"status": "OK"})
 
+
+@app_views.route('/stats')
+def stats():
+    """
+    Return the current status in a JSON file form
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
