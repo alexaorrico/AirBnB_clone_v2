a63c9946c927f4cf644b286c0381c5ecf87a970d
Arthur Damm <indigoarthur@gmail.com>
2019-09-11 01:11:44 +0000

    Fix comment

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -63,7 +63,7 @@ class DBStorage:
         """delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
-            self.save() # TODO: keep this?
+            self.save()  # TODO: keep this?
 
     def reload(self):
         """reloads data from the database"""
