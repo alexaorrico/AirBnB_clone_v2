9051371b0d9a8fe8a134ff9b5e4668b183893864
Ferney Medina <fermed28@gmail.com>
2019-09-11 23:51:31 +0000

    [fixes] delete try except

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -43,17 +43,14 @@ def delete_city_id(city_id):
 def create_cities(state_id):
     """ Route create cities with POST"""
     if request.is_json:
-        try:
-            data = request.get_json()
-            if 'name' in data:
-                data["state_id"] = state_id
-                new_city = City(**data)
-                new_city.save()
-                return jsonify(new_city.to_dict()), 201
-            else:
-                return jsonify(error="Missing name"), 400
-        except:
-            return jsonify(error="Not a JSON"), 400
+        data = request.get_json()
+        if 'name' in data:
+            data["state_id"] = state_id
+            new_city = City(**data)
+            new_city.save()
+            return jsonify(new_city.to_dict()), 201
+        else:
+            return jsonify(error="Missing name"), 400
     else:
         return jsonify(error="Not a JSON"), 400
 
@@ -63,18 +60,15 @@ def update_cities(city_id):
     """ Route update cities with PUT """
 
     if request.is_json:
-        try:
-            data = request.get_json()
-            my_object = storage.get('City', city_id)
-            if my_object is not None:
-                for keys, values in data.items():
-                    if keys not in ["created_at", "updated_at", "id"]:
-                        setattr(my_object, keys, values)
-                my_object.save()
-                return jsonify(my_object.to_dict()), 200
-            else:
-                abort(404)
-        except:
-            return jsonify(error="Not a JSON"), 400
+        data = request.get_json()
+        my_object = storage.get('City', city_id)
+        if my_object is not None:
+            for keys, values in data.items():
+                if keys not in ["created_at", "updated_at", "id"]:
+                    setattr(my_object, keys, values)
+            my_object.save()
+            return jsonify(my_object.to_dict()), 200
+        else:
+            abort(404)
     else:
         return jsonify(error="Not a JSON"), 400
