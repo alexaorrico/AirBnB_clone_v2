2e4487d1522e9ffdd4c46a94010f1f61c3e79038
xica369 <761@holbertonschool.com>
2019-09-12 21:34:42 +0000

    T5: pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,10 +6,12 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status_route():
     """returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def gstats():
