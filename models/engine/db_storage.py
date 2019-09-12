82c1c6c4a9b20a05d8c1e6710c91344dfe1023dc
jembantonio <jembantonio@gmail.com>
2019-09-12 19:08:37 +0000

    modified db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,14 +75,6 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-    def get(self, cls, id):
-        ''' get obj by cls and id '''
-        obj = self.all(cls)
-        for key, val in obj.items():
-            if id == val.id:
-                return val
-        return None
-
     def count(self, cls=None):
         ''' count number of objs by class '''
         if cls is None:
