6c23f1463f69ac8864f78cfd594ba0c26c07a2d2
xica369 <761@holbertonschool.com>
2019-09-12 20:11:18 +0000

    T4: [fixed] change the values to port and host to env variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ web application must be listening on 0.0.0.0, port 5000
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,4 +20,5 @@ def teardown_db(exception=None):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0', threaded=True)
+    app.run(port=getenv("HBNB_API_PORT"),
+            host=("HBNB_API_HOST"), threaded=True)
