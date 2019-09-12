70f7f6e1c4ebd8e2f022ac278edf171bb7cef4a4
jembantonio <jembantonio@gmail.com>
2019-09-12 00:30:16 +0000

    added route if city id is notp rovided

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -28,11 +28,18 @@ def get_all_cities(state_id=None):
     return jsonify(city_list)
 
 
+@app_views.route('/cities', methods=['GET'], strict_slashes=False)
 @app_views.route('/cities/<city_id>',
                  methods=['GET'],
                  strict_slashes=False)
 def get_city(city_id=None):
     ''' returns an individual city object '''
+    if city_id is None:
+        city_list = []
+        cities = storage.all('City')
+        for obj in cities.values():
+            city_list.append(obj.to_dict())
+
     obj = storage.get('City', city_id)
     if obj is None:
         ''' if no state obj with that id '''
