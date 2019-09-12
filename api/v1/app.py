8066cc8fb7959d2e00a5b9d422cf8eaa60439f7b
xica369 <761@holbertonschool.com>
2019-09-12 06:38:47 +0000

    T4: add exception=None

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ web application must be listening on 0.0.0.0, port 5000
 
 from flask import Flask
 from models import storage
-from api.v1.views import app_view
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -14,7 +14,7 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_db():
+def teardown_db(exception=None):
     """closes or otherwise deallocates the resource"""
     storage.close()
 
