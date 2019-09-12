aeced000fefebe6fe7604721ff67c133e89eea72
Alexa <alexafdeveloper@gmail.com>
2019-09-12 21:22:54 +0000

    define storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ import app_views and create a route /status
 """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status_route():
