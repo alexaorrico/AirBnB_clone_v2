422c757eee2ac5a94e65c76e836ddb83d786f23a
xica369 <761@holbertonschool.com>
2019-09-12 01:49:43 +0000

    T4: [update]

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,9 @@ from flask import Blueprint
 
 
 app_views = Blueprint("mold", __name__, url_prefix='/api/v1')
+
+
+@app_views.route('/api/v1')
+def api_vi_route():
+    """ print returns a JSON: "status": "OK" """
+    
