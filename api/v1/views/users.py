a763117745b943548984cbfaacc039e66c66a61c
rodrigocruz13 <rodrigocruzayala@gmail.com>
2019-09-12 17:42:10 -0500

    [Fix]. Fixed jsonify output

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -51,11 +51,11 @@ def create_user():
     Create a new object user
     """
     if not request.is_json():
-        return make_response(jsonify(message='Not a JSON'), 400)
+        return make_response(jsonify({"error": 'Not a JSON'}), 400)
     if 'email' not in request.get_json():
-        return make_response(jsonify(message='Missing email'), 400)
+        return make_response(jsonify({"error": 'Missing email'}), 400)
     if 'password' not in request.get_json():
-        return make_response(jsonify(message='Missing password'), 400)
+        return make_response(jsonify({"error":'Missing password'}), 400)
     user = request.get_json()
     new_user = User(**user)
     storage.new(new_user)
@@ -72,7 +72,7 @@ def update_user(user_id):
     if user is None:
         abort(404)
     if not request.is_json():
-        return make_response(jsonify(message='Not a JSON'), 400)
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
     params = request.get_json()
     skip = ['id', 'email', 'created_at', 'updated_at']
     for key, value in params.items():
