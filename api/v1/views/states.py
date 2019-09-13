cc80d80cbaf001fa3b91d23524a7378245165f0d
xica369 <761@holbertonschool.com>
2019-09-13 01:37:39 +0000

    T7: [fixed] change idstate to state_id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,19 +9,18 @@ from models.state import State
 
 
 @app_views.route("/states/", methods=['GET'], strict_slashes=False)
-def get_state_route():
+def get_states():
     """ Retrieves the list of all State objects """
     states_list = []
-    for key, value in BaseModel.to_dict().items():
-        if "State" in key:
-            states_list.append(value)
+    for key, value in storage.all("State").items():
+        states_list.append(value.to_dict())
     return jsonify(states_list)
 
 
 @app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
-def get_state(idstate):
+def get_state(state_id):
     """ Retrieves a State object """
-    estado = storage.get('State', idstate)
+    estado = storage.get('State', state_id)
     if estado:
         return jsonify(estado.to_dict())
     else:
@@ -30,9 +29,9 @@ def get_state(idstate):
 
 @app_views.route("/states/<state_id>", methods=['DELETE'],
                  strict_slashes=False)
-def delete_state(idstate):
+def delete_state(state_id):
     """ Delete a State object """
-    estado = storage.get('State', idstate)
+    estado = storage.get('State', state_id)
     if estado:
         storage.delete(estado)
         storage.save()
@@ -58,17 +57,17 @@ def create_state():
 
 
 @app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
-def update_state(idstate):
+def update_state(state_id):
     """ Delete a State object """
     if not request.is_json:
         abort(400, "Not a JSON")
-    estado = storage.get('State', idstate)
+    estado = storage.get('State', state_id)
     if estado:
         datos = request.get_json()
         if type(datos) is dict:
             omitir = ['id', 'created_at', 'updated_at']
             for name, value in datos.items():
-                if name not in ignore:
+                if name not in omitir:
                     setattr(estado, name, value)
             storage.save()
             return jsonify(estado.to_dict())
