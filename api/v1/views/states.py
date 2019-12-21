021573d8e8df9b8f51f809add1765a860cb9cf07
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-12-21 22:59:20 +0000

    Fixed list_state GET method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,15 +4,15 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/states', methods=['GET', 'POST'])
-def states():
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
+def list_states():
     """
     Retrieves the list of all State objects: GET /api/v1/states
     Creates a State: POST /api/v1/states
     """
     if request.method == 'GET':
         list_states = []
-        states = storage.all('States')
+        states = storage.all('State').values()
         for state in states:
             list_states.append(state.to_dict())
         return jsonify(list_states)
