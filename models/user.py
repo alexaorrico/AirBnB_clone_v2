a667030beda705313b743924b4aaf8ce95ecce29
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-12-22 10:01:13 +0000

    Added security to user password

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,12 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def set_password(self, password):
+        hashed = hashlib.md5(password.encode())
+        self.password = hashed.hexdigest()
