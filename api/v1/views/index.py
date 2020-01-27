1a40be70aa9dbfa57a81818f3544ad0824b7c4cd
mrdoomus <mrdoom.official@gmail.com>
2020-01-27 18:16:18 -0500

    storage_get_count: [Upload] app.py creates a Blueprint later used by index.py to print the status in a json

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  """
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def get_status():
+    return jsonify({'status': 'OK'})
