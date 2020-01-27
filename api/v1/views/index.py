39da78f4be5b765458de35dc55e0d7c8083689e1
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-27 16:45:40 -0500

    [Feature] Implemented count method from storage to route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
 """
-file
+File that configures the routes of the app
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status():
+    """
+    Return the state of status
+    """
     return jsonify(status="OK")
+
+
+@app_views.route("/stats")
+def stats():
+    """
+    Return the number of instances for each class
+    """
+    objects = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+        }
+
+    obj_count = {}
+
+    for key, val in objects.items():
+        print(key + " " + val + " " + str(storage.count(key)))
+        obj_count[val] = storage.count(key)
+
+    return jsonify(obj_count)
