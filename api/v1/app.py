da3d1fc72f654174d5927b5a2ab3ed4718745dec
diegolopezq95 <927@holbertonschool.com>
2020-01-27 22:10:38 +0000

    [Update]: adding error 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,16 +6,21 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import jsonify
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.errorhandler(404)
+def error_404(error):
+    """error 404 Not found"""
+    return (jsonify(error="Not found"), 404)
+
 @app.teardown_appcontext
 def close_sesssion(_):
     storage.close()
-
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
