6e5d7bfaed8dbe4eb5b5de45271a9334b14c476c
Santiago <sagudecod97@gmail.com>
2020-01-27 22:45:32 +0000

    Added route to retrieve the count of all the objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,23 @@
 #!usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    objects = {
+        "amenities": 0,
+        "cities": 0,
+        "places": 0,
+        "reviews": 0,
+        "states": 0,
+        "users": 0
+    }
+    for key in objects.keys():
+        objects[key] = storage.count(key[0].upper+key[1:])
+    return jsonify(objects)
+
