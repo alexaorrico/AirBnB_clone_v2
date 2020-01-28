c7d2c7299d9b32b623453bf2b11cb5ae883fcf1b
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-28 15:31:07 -0500

    [Feature] taskn 6 error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import jsonify
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -11,6 +13,12 @@ app.register_blueprint(app_views)
 @app.teardown_request
 def teardown_storage(self):
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'error': "Not found"}), 404
+
 
 if __name__ == '__main__':
     hb_host = getenv("HBNB_API_HOST")
