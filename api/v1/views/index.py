d8c3558d2288544ee78d8562d919e02625ba6ef0
Dioni1195 <dionisio.arango95@gmail.com>
2020-01-28 16:24:33 +0000

    (storage_get_count): [Add] Add route to the stats of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,24 @@
 """  """
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def get_status():
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def get_stats():
+    amenity = storage.count('Amenity')
+    cities=storage.count('City')
+    places=storage.count('Place')
+    reviews=storage.count('Review')
+    states=storage.count('State')
+    users=storage.count('User')
+    return jsonify({'amenities': amenity,
+                    'cities': cities,
+                    'places': places,
+                    'reviews': reviews,
+                    'states': states,
+                    'users': users})
