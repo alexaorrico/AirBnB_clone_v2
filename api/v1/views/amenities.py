d6478a84c99fe11b954b63aab4f4a770002475e9
Hugo Vargas <vargas88hugo@gmail.com>
2020-01-28 18:10:09 -0500

    [Feature] Created amenities and updated init and states with pep8

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,51 +1,51 @@
 #!/usr/bin/python3
 """
-File that configures the routes of state
+File that configures the routes of amenity
 """
 from api.v1.views import app_views
 from flask import jsonify, abort, request
-from models.state import State
+from models.amenity import Amenity
 from models import storage
 
 
-@app_views.route("/states", strict_slashes=False)
-@app_views.route("/states/<state_id>", strict_slashes=False)
-def get_state(state_id=None):
+@app_views.route("/amenities", strict_slashes=False)
+@app_views.route("/amenities/<amenity_id>", strict_slashes=False)
+def get_amenity(amenity_id=None):
     """
-    Route to get states
+    Route to get amenities
     """
     list_obj = []
-    if not state_id:
-        for val in storage.all("State").values():
+    if not amenity_id:
+        for val in storage.all("Amenity").values():
             list_obj.append(val.to_dict())
         return jsonify(list_obj)
-    state_obj = storage.get("State", state_id)
-    if state_obj:
-        return jsonify(state_obj.to_dict())
+    amenity_obj = storage.get("Amenity", amenity_id)
+    if amenity_obj:
+        return jsonify(amenity_obj.to_dict())
     else:
         abort(404)
 
 
-@app_views.route("/states/<state_id>", methods=['DELETE'],
+@app_views.route("/amenities/<amenity_id>", methods=['DELETE'],
                  strict_slashes=False)
-def delete_state(state_id=None):
+def delete_amenity(amenity_id=None):
     """
-    deletes a State object
+    deletes a Amenity object
     """
-    state_obj = storage.get("State", state_id)
-    if state_obj:
-        storage.delete(state_obj)
+    amenity_obj = storage.get("Amenity", amenity_id)
+    if amenity_obj:
+        storage.delete(amenity_obj)
         storage.save()
         return jsonify({})
     else:
         abort(404)
 
 
-@app_views.route("/states", methods=["POST"],
+@app_views.route("/amenities", methods=["POST"],
                  strict_slashes=False)
-def post_state():
+def post_amenity():
     """
-    Route that create a new State
+    Route that create a new Amenity
     """
     try:
         data = request.get_json()
@@ -53,7 +53,7 @@ def post_state():
         abort(400, "Not a JSON")
     if data:
         if "name" in data:
-            obj = State(**data)
+            obj = Amenity(**data)
             obj.save()
             return (jsonify(obj.to_dict()), 201)
         else:
@@ -62,22 +62,22 @@ def post_state():
         abort(400, "Not a JSON")
 
 
-@app_views.route("/states", methods=["PUT"], strict_slashes=False)
-@app_views.route("/states/<state_id>", methods=["PUT"],
+@app_views.route("/amenities", methods=["PUT"], strict_slashes=False)
+@app_views.route("/amenities/<amenity_id>", methods=["PUT"],
                  strict_slashes=False)
-def put_state(state_id=None):
+def put_amenity(amenity_id=None):
     """
-    Route that update a State
+    Route that update an Amenity
     """
-    obj = storage.get("State", state_id)
-    if state_id and obj:
+    obj = storage.get("Amenity", amenity_id)
+    if amenity_id and obj:
         try:
             data = request.get_json()
         except Exception:
             abort(400, "Not a JSON")
         if data:
             for key, value in data.items():
-                if (key != "id" and key != "state_id" and key != "created_at"
+                if (key != "id" and key != "amenity_id" and key != "created_at"
                    and key != "updated_at"):
                     setattr(obj, key, value)
 
