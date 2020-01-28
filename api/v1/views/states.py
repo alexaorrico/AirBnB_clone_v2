157694caa3dedd12f2e58762e13b3361859b273e
Jessica Bathel <811@holbertonschool.com>
2020-01-28 20:59:19 +0000

    Uploading in progress files for debugging

---
+++ api/v1/views/states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from flask import request, jsonify
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/states', methods=['GET', 'POST'])
+def states():
+    """
+    Retrieves the list of all State objects: GET /api/v1/states
+    Creates a State: POST /api/v1/states
+    """
+    if request.method == 'GET':
+        list_states = []
+        states = storage.all('States')
+        for state in states:
+            list_states.append(state.to_dict())
+        return jsonify(list_states)
