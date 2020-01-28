6247351b117f9158bcaa36e7eccc3178b61c81cd
mrdoomus <mrdoom.official@gmail.com>
2020-01-28 17:05:32 -0500

    storage_get_count: [Fix] Adding cascade to model/states [Add] Routes for POST PUT and DELETE methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 from models import storage
+from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False)
@@ -13,6 +14,19 @@ def get_states():
     return jsonify(states_list)
 
 
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_states():
+    """ Return status of the APP as OK """
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict())
+
+
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_states_id(state_id):
     """ Return status of the APP as OK """
@@ -20,6 +34,7 @@ def get_states_id(state_id):
         if state.id == state_id:
             return jsonify(state.to_dict())
     abort(404)
+
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def del_states_id(state_id):
@@ -31,3 +46,18 @@ def del_states_id(state_id):
         storage.delete(catch_state)
         storage.save()
         return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_states_id(state_id):
+    """ Return status of the APP as OK """
+    catch_state = storage.get('State', state_id)
+    if catch_state is None:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    for key, value in data.items():
+        setattr(catch_state, key, value)
+    storage.save()
+    return jsonify(catch_state.to_dict()), 200
