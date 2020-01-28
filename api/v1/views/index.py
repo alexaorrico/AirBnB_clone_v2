4164689b4abb619732f8486a89bccea5e588cd0e
Jessica Bathel <811@holbertonschool.com>
2020-01-28 19:49:27 +0000

    Added endpoint route that retrieves count of each object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Module for status and stats endpoint
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
@@ -12,4 +13,18 @@ def status():
     create a route /status on the object app_views
     that returns a JSON: "status": "OK"
     """
-    return jsonify({'status': 'OK'})
+    return jsonify(status='OK')
+
+
+@app_views.route("/stats")
+def stats():
+    """
+    Create an endpoint that retrieves the number of each objects by type.
+    """
+    return jsonify(amenities=storage.count('Amenity'),
+                   cities=storage.count('City'),
+                   places=storage.count('Place'),
+                   reviews=storage.count('Review'),
+                   states=storage.count('State'),
+                   users=storage.count('User')
+                   )
