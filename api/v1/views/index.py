705087f3ded83403f183a1c58a17fd5806db49ed
cherjiosclear <slopez0286@gmail.com>
2020-01-28 23:36:05 +0000

    Adding some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,13 +3,30 @@
 
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ return a JSON """
     return jsonify({"status": "OK"}), 200
+
+
+@app_view.route('/stats', methods=['GET'])
+def stats():
+    """ Retrives the number of each objects by type:"""
+    o = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+        }
+    o = {k: storage.count(v) for k, v in o.items()}
+    return jsonify(o)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
