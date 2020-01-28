e0e4695d4ae5d531019ec6be5e4f9e1c325a1416
Karen Herrera <837@holbertonschool.com>
2020-01-28 16:54:45 +0000

    error 404-not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,8 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import make_response
+from flask import jsonify
 import os
 
 
@@ -17,6 +19,10 @@ try:
         storage.close()
 except:
     print("error")
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("HBNB_API_HOST", "0.0.0.0"),
