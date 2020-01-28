10527be2e71d1d7c38e2702f28fe1712372c602c
Fidel Caicedo <869@holbertonschool.com>
2020-01-28 14:04:00 -0500

    Uploading error Handler to the flask application

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
  Start Flask Application
 """
 from models import storage
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, make_response, jsonify
 from api.v1.views import app_views
 from os import getenv
 
@@ -16,8 +16,12 @@ def finish(NaN):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", '0.0.0.0')
     port = getenv("BNB_API_PORT", '5000')
     app.run(host=host, port=port, threaded=True)
-
