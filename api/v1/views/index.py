cd51c9fc717730310ad6689558df12e4756d24d9
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-28 13:55:33 -0500

    Add stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,24 @@
 """
     Module of blueprints of flask
 """
+from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route("/status")
 def status_ok():
+    """Send status of the application"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route("/stats")
+def fetch_all_stats():
+    """Fetch of counts for all classes"""
+    classes = {"amenities": "Amenity", "cities": "City", "places": "Place",
+               "reviews": "Review", "states": "State", "users": "User"}
+    count_dict = {}
+    for key, value in classes.items():
+        count = storage.count(value)
+        count_dict[key] = count
+    return jsonify(count_dict)
