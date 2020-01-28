86e903d54f5086f7af5e00ee143033278e1996fc
Jessica Bathel <811@holbertonschool.com>
2020-01-28 20:07:39 +0000

    Added errorhandler(404) method

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,18 @@
 Build Flask Application
 """
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """errors that returns a JSON-formatted 404 status code response"""
+    return jsonify(error='Not found'), 404
 
 
 @app.teardown_appcontext
