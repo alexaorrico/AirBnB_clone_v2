afa111d3846bad43ae44c3592bd64c38155c9433
chriswill88 <williechri79@gmail.com>
2020-01-28 08:51:06 -0500

    [ADD] task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python3
-#import api.v1.views
+# import api.v1.views
 from flask import Flask
 from api.v1.views import app_views
 from json import dumps, loads
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """returns status ok"""
     return (loads('{"status": "OK"}'))
+
+
+@app_views.route('/stats')
+def numtypes():
+    dicty = {}
+    amenC = storage.count("Amenities")
+    dicty["amenities"] = amenC
+    cityC = storage.count("City")
+    dicty["cities"] = cityC
+    placeC = storage.count("Places")
+    dicty["places"] = placeC
+    reviewC = storage.count("Review")
+    dicty["reviews"] = reviewC
+    stateC = storage.count("State")
+    dicty["states"] = stateC
+    userC = storage.count("User")
+    dicty["users"] = userC
+
+    return loads(dumps(dicty))
