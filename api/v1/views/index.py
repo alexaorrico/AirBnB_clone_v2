dfcb24071f48af1ff3a20a1229a69fa802b2df08
aliavang <714@holbertonschool.com>
2020-01-28 00:28:57 +0000

    Added count retrieval to index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,21 @@
 Index module
 """
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """return dictionary with status"""
     return {"status": "OK"}
+
+@app_views.route('/stats')
+def stats():
+    """Retrieve number of each objects by type"""
+    stat_dict = {}
+    stat_dict["amenities"] = storage.count("Amenity")
+    stat_dict["cities"] = storage.count("City")
+    stat_dict["places"] = storage.count("Place")
+    stat_dict["states"] = storage.count("State")
+    stat_dict["users"] = storage.count("User")
+    return stat_dict
