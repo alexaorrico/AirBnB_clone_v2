58190e40bd60d4dee89f7710d51ea2d423577d73
Max Stuart <maxastuart@gmail.com>
2020-01-29 03:02:37 +0000

    fixed /status

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,6 @@ initializes the api views from a flask blueprint set up in api/v1/app.py
 
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,21 +3,21 @@
 returns the status of the app_views object at route /status
 """
 
-from . import app_views
+from api.v1 import views
 from models import storage
 from json import dumps
 from collections import defaultdict
 
 
-@app_views.route("/status")
+@views.app_views.route("/status")
 def status():
     return dumps({"status": "OK"})
 
 
-@app_views.route("/stats")
+@views.app_views.route("/stats")
 def stats():
     """Get number of objects indexed by type"""
-    numbers = defaultdict(0)
+    numbers = defaultdict(lambda: 0)
     for value in storage.all().values():
         numbers[str(type(value))] += 1
     return dumps(numbers)
