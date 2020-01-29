dd55b76879eeaf0e1a28253133723d4c498e7280
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-29 10:25:55 -0500

    Fix index, app and cities

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,13 +7,13 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def status_ok():
     """Send status of the application"""
-    return jsonify({'status': 'OK'})
+    return jsonify({'status': 'OK'}), 200
 
 
-@app_views.route("/stats")
+@app_views.route("/stats", strict_slashes=False)
 def fetch_all_stats():
     """Fetch of counts for all classes"""
     classes = {"amenities": "Amenity", "cities": "City", "places": "Place",
@@ -22,4 +22,4 @@ def fetch_all_stats():
     for key, value in classes.items():
         count = storage.count(value)
         count_dict[key] = count
-    return jsonify(count_dict)
+    return jsonify(count_dict), 200
