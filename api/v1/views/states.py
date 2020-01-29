bf8b2319a9a42a16e1a6e09ef5ff0cda7ffb8fcc
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-01-29 11:56:23 -0500

    adding file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,13 +2,16 @@
 """
 New view for State objects that handles taht handles all default ResFul API.
 """
- 
+
+
 from flask import abort
 from flask import jsonify
 from models.state import State
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
+from flask import request
+
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_task():
@@ -20,7 +23,8 @@ def get_task():
     return jsonify(state_list)
 
 
-@app_views.route("/states/<string:state_id>",methods=['GET'], strict_slashes=False)
+@app_views.route("/states/<string:state_id>", methods=['GET'],
+                 strict_slashes=False)
 def get_task_id(state_id):
     """
     Return id of the function
@@ -31,7 +35,8 @@ def get_task_id(state_id):
     return jsonify(stateArr.to_dict())
 
 
-@app_views.route("states/<string:state_id>", methods=['DELETE'], strict_slashes=False)
+@app_views.route("states/<string:state_id>", methods=['DELETE'],
+                 strict_slashes=False)
 def get_task_delete(state_id):
     """
     method Delete of the function
@@ -42,6 +47,23 @@ def get_task_delete(state_id):
     else:
         storage.delete(stateArr)
     return jsonify({}), 200
+
+
+@app_views.route("/states", methods=['POST'], strict_slashes=False)
+def set_task_POST():
+    """
+    State object
+    """
+    if not request.json:
+        abort(400)
+        return jsonify({"error": "Not a JSON"})
+    if 'name' not in request.json:
+        abort(400)
+        return jsonify({"error": "Missing name"})
+
+    state_post = State(**request.get_json())
+    state_post.save()
+    return jsonify(state_post.to_dict()), 201
 
 
 if __name__ == "__main__":
