cdd8cb546565a1729f121b95184816d2d939dc8a
gefranco <933@holbertonschool.com>
2020-01-29 03:51:24 +0000

    [add] 7. view for State objects that handles all default RestFul API actions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,9 @@
 
 from flask import Blueprint, jsonify
 from api.v1.views.index import *
+from api.v1.views.states import *
 
 app_views = Blueprint('/api/v1', __name__)
 
 index.init()
+states.init_states()
