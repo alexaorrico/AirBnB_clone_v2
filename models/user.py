ec35eb655b625789de5678503a0f2d7667487dfe
Karen Herrera <837@holbertonschool.com>
2020-01-29 21:22:39 +0000

    import backref

--- models/user.py
+++ models/user.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
