c39b6833781483115184f8f02e2be413f42535c8
untaljacko <untaljacko@gmail.com>
2020-01-29 16:22:55 +0000

    [new] create the app.py file indide the v1 folder and inits

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" first part of the restful api """
+from models import storage
+from api.v1.views import app_views
+from flask import Flask
+from os import environ
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_app(exception=None):
+    """ a method to call close() """
+    storage.close()
+
+if __name__ == "__main__":
+    host1 = environ.get("HBNB_API_HOST")
+    port1 = environ.get("HBNB_API_PORT")
+    app.run(host=host1, port=port1, threaded=True)
