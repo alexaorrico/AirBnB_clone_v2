fd215fe18b6df39e279b092ea3d5482cbcc10bda
821-N <nathaniel.marofsky@gmail.com>
2020-01-29 20:27:03 +0000

    fixk

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,9 @@ set up blueprint
 """
 
 from flask import Blueprint
-from api.v1.views.index import *
+
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+
+from api.v1.views.index import *
