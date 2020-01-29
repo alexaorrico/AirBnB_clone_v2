7b5afacb7868c39d3744642061d8bd5277ba9dd6
jaime <jandres.vr@hotmail.com>
2020-01-29 17:41:59 -0500

    fix: changes in the repo

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,10 @@
 starts a Flask web application
 """
 from models import storage
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from os import getenv
 
-host = getenv("HBNB_API_HOST")
-port = getenv("HBNB_API_PORT")
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -23,12 +21,10 @@ def teardown_db(self):
 @app.errorhandler(404)
 def page_not_found(error):
     """ 404 handler """
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
-    if host is None:
-        host = '0.0.0.0'
-    if port is None:
-        port = 5000
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = getenv("HBNB_API_HOST") or '0.0.0.0'
+    port = getenv("HBNB_API_PORT") or 5000
+    app.run(host=host, port=port, threaded=True)
