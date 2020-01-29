eaf54b60da3cede3566f0160385c7602bb499561
joshua <joshuaciencia2016@gmail.com>
2020-01-29 00:20:51 +0000

    Added error handler for page not founf - 404 && added code style to .py files

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,24 @@
-from flask import Flask
-from flask import Blueprint
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+
+"""
+This module runs the flask api on
+"""
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def shut(exec):
+    """ remove storage session after request """
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """ handle page not found error """
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     app.run(getenv('HBNB_API_HOST'), getenv('HBNB_API_PORT'), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,11 @@
 from api.v1.views import app_views
 from json import dumps
 from flask import jsonify
+"""
+index route of the api
+"""
 
 @app_views.route('/status')
 def status():
-
+    """ returns status for the api """
     return jsonify({"status": "OK"})
