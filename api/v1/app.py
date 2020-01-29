b4ef5f22270858cd826524bd3029199b40712a7e
jaime <jandres.vr@hotmail.com>
2020-01-29 19:41:34 +0000

    update: changes in the blueprints and 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,17 +5,17 @@ starts a Flask web application
 from models import storage
 from flask import Flask, jsonify
 from api.v1.views import app_views
-import sys
+from os import getenv
 
-# host = sys.getenv("HBNB_API_HOST")
-# port = sys.getenv("HBNB_API_PORT")
+host = getenv("HBNB_API_HOST")
+port = getenv("HBNB_API_PORT")
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown_db(self):
     """closes the storage on teardown"""
     storage.close()
 
@@ -23,8 +23,12 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def page_not_found(error):
     """ 404 handler """
-    return {"error": "Not found"}
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
+    if host is None:
+        host = '0.0.0.0'
+    if port is None:
+        port = 5000
     app.run(host='0.0.0.0', port=5000, threaded=True)
