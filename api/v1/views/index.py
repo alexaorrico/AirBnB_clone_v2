6eceb988562127a39c503baad2737a96eab6ac88
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-01-29 02:06:00 +0000

    Blueprint renamed app_views; status() returns JSON

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,9 @@ returns the status of the app_views object at route /status
 """
 
 from . import app_views
+from json import dumps
 
 
 @app_views.route("/status")
 def status():
-    return {
-        "status": "OK"
-    }
+    return dumps({"status": "OK"})
