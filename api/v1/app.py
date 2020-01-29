72d8c16f2c42dac8f2442949da3f427fb021883e
david-develop <924@holbertonschool.com>
2020-01-29 10:24:51 -0500

    [Fix] api/v1/views/index.py return make_response method

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,9 +21,9 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    hb_host = getenv("HBNB_API_HOST")
-    hb_port = getenv("HBNB_API_PORT")
-    if hb_port and hb_host:
+    app.run(host=getenv("HBNB_API_HOST", default="0.0.0.0"),
+            port=getenv("HBNB_API_PORT", default="5000"))
+    """if hb_port and hb_host:
         app.run(
             host=getenv(hb_host),
             port=getenv(hb_port),
@@ -33,3 +33,4 @@ if __name__ == '__main__':
             host="0.0.0.0",
             port="5000",
             threaded=True)
+    """
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response
 from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status_api():
-    status_api = 'OK'
-    return jsonify({'status': status_api})
+    return make_response(jsonify({'status': 'Ok'}), 200)
 
 
 @app_views.route('/stats', strict_slashes=False)
