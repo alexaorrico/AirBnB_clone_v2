567958d6522cfa100b170f885e57aed677008d81
david-develop <924@holbertonschool.com>
2020-01-29 18:37:08 -0500

    [Add] cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,18 +6,18 @@ from models.city import City
 from api.v1.views import app_views
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'],
+"""@app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def get_cities(state_id):
-    """Return list of cities in a state"""
-    unique_state = storage.get("State", state_id)
+   """ """Return list of cities in a state"""
+    """unique_state = storage.get("State", state_id)
     if not unique_state:
         abort(404)
     city_list = []
     for city in unique_state.cities:
         city_list.append(city.to_dict())
     return jsonify(city_list)
-
+"""
 
 #@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE'],
 #                 strict_slashes=False)
