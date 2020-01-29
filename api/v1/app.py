edbadd0322dbe4aaa0e130518e4dafed4c27c956
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-29 22:46:43 +0000

    added environmental variable

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ Status of your API
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -24,4 +25,6 @@ def not_found(error):
     return jsonify({"error": "Not found"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
