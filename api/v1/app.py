18f77a446604d816d0bbef39f0f032e889197210
david-develop <924@holbertonschool.com>
2020-01-29 10:30:07 -0500

    [Update] files permissions

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,14 +23,4 @@ def page_not_found(e):
 if __name__ == '__main__':
     app.run(host=getenv("HBNB_API_HOST", default="0.0.0.0"),
             port=getenv("HBNB_API_PORT", default="5000"))
-    """if hb_port and hb_host:
-        app.run(
-            host=getenv(hb_host),
-            port=getenv(hb_port),
-            threaded=True)
-    else:
-        app.run(
-            host="0.0.0.0",
-            port="5000",
-            threaded=True)
-    """
+
