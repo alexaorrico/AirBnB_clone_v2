0fe85743a34e3b6a0d63bfb27abf7fae295a2ce5
Teslothorcha <juandavidmarinbernal@gmail.com>
2020-01-29 16:55:47 -0500

    [Feature] cities methods api, task (unsued import removed)

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Flask application that handle views"""
 from api.v1.views import app_views
-from flask import jsonify, make_response
+from flask import jsonify
 from models import storage
 
 
