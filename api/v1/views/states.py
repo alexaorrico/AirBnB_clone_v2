4887c507b8542c0c9ed41b45b0391909b2eb3fff
Max Stuart <maxastuart@gmail.com>
2020-01-29 04:50:24 +0000

    Functioning GET by state_id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,6 +6,7 @@ Handles all default RESTful API actions for State objects
 from . import app_views
 from models.state import State
 from flask.json import jsonify
+from flask import abort
 from models import storage
 
 
@@ -14,4 +15,11 @@ def states():
     """Retrieves the list of all State objects"""
     return jsonify([state.to_dict() for state in storage.all('State').values()])
 
-# @app_views.route("/states/<state_id>", methods=('GET', 'POST'))
+@app_views.route("/states/<state_id>", methods=['GET'])
+def state(state_id):
+    """Retrieves a state given its ID"""
+    try:
+        s = storage.all('State')
+        return jsonify(s['State.' + state_id].to_dict())
+    except KeyError:
+        abort(404)
