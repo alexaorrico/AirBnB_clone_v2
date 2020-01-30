d8af88dcdcb3fa30758789294cb1a9907ea0797e
vivianlorenaortiz <vivianlorena0414@gmail.com>
2020-01-30 15:55:33 -0500

    exercise 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,14 @@ from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 from models import storage
 import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix="/api/v1")
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_app(code):
