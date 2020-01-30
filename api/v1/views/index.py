ea54a97511244e4584891153dc555007373fa800
821-N <nathaniel.marofsky@gmail.com>
2020-01-30 16:20:57 -0500

    add stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,26 @@
 """
 from flask import Flask
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status")
 def status():
     """ status """
     return {"status": "OK"}
+
+
+@app_views.route("/stats")
+def stats():
+    """ stats """
+    ret = {}
+
+    def form(name):
+        """ simple format (lowercase + plural) """
+        name = name.lower()
+        if name[-1] == 'y':
+            name = name[:-1] + 'ie'
+        return name + 's'
+    for name in ["Amenity", "City", "Place", "Review", "State", "User"]:
+        ret[form(name)] = storage.count(name)
+    return ret
