930fbff4da18fd63ab004118eaae6145e0428f2d
arq-gabo <jgabrielguerral@gmail.com>
2020-01-30 04:54:09 +0000

    [FIX]Add stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,20 @@ Module for create route in the object and return a JSON status Ok
 
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route('/status')
 def jsonReturn():
+    """Return JSON"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def statReturn():
+    """Return Stats"""
+    return jsonify({ "amenities": storage.count("Amenity"), 
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
