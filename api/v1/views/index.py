dfaf35e6525ec9ca287dfa3c70ea2c6f1fb8d93f
Diego Monroy <difemo91@gmail.com>
2020-01-30 03:29:48 +0000

    [FEAT] add endpoint that returns the number of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def status():
     """ status function """
-    return jsonify({"status": "OK"})
+    return jsonify({"status":"OK"})
+
+@app_views.route('/stats')
+def stats():
+    """number of of objects by type"""
+    dic = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(dic)
