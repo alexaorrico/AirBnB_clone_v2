0e7c33271a7bae664e50218899f8f3ccadeb769e
david-develop <924@holbertonschool.com>
2020-01-29 19:56:40 -0500

    [Fix] trial

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -34,7 +34,7 @@ def get_states_id(state_id):
             return jsonify({}), 200
 
 
-@app_views.route('/states/', methods=['POST'])
+@app_views.route('/states', methods=['POST'])
 def post_states():
     """Post states"""
     json_tmp = request.get_json()
