4b1eebc8a3e326ea56c0ed98c3036a516919f3e3
guilmeister <ebg.edwardbguillermo@yahoo.com>
2020-01-30 05:20:36 +0000

    fixed import errors

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,7 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
-from models.users import User
+from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
