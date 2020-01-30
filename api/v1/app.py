20e78211f0be8ff3dd1a916b3822edd1fd527823
mrdoomus <mrdoom.official@gmail.com>
2020-01-30 14:02:19 -0500

    master: [Fix] getenv for HBNB_PORT and HOST

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
@@ -22,5 +23,8 @@ def error_handler(err):
     return jsonify({"error": "Not found"}), 404
 
 
+hosts = getenv('HBNB_API_HOST', '0.0.0.0')
+ports = getenv('HBNB_API_PORT', '5000')
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000', threaded='True', debug=True)
+    app.run(host=hosts, port=ports, threaded='True', debug=True)
