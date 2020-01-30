cfd08aa52ed48d21976273b528b4f66f690a367d
arq-gabo <jgabrielguerral@gmail.com>
2020-01-30 04:15:24 +0000

    [FIX]Correction Pep8 file index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Module for create route in the object and return a JSON status Ok
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status')
 def jsonReturn():
     return jsonify({"status": "OK"})
