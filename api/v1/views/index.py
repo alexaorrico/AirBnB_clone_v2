573aa143f0b8395403c57394bc6208613117bed7
Doniben <doniben@esperanto.co>
2020-01-30 00:44:25 +0000

    [FEATURE] index file

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" index file """
+
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    """ status function """
+    return jsonify({"status": "OK"})
