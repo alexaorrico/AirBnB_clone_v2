47045798a87009c0964241d4014e8afd8ebc0a4b
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-30 17:05:16 +0000

    FIxed PUT

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,7 +6,7 @@ New view for State objects that handles taht handles all default ResFul API.
 
 from flask import abort
 from flask import jsonify
-from models.state import User
+from models.user import User
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
@@ -19,7 +19,7 @@ def get_user():
     """
     user_list = []
     for user in storage.all('User').values():
-        user_list.append(state.to_dict())
+        user_list.append(user.to_dict())
     return jsonify(user_list)
 
 
@@ -35,7 +35,7 @@ def get_user_id(user_id):
     return jsonify(userArr.to_dict())
 
 
-@app_views.route("users/<user_id>", methods=['DELETE'],
+@app_views.route("/users/<user_id>", methods=['DELETE'],
                  strict_slashes=False)
 def get_user_delete(user_id):
     """
@@ -43,17 +43,20 @@ def get_user_delete(user_id):
     """
     userArr = storage.get('User', user_id)
     if userArr is None:
+        print("entre al ifffffffffffffffffffffffff")
         abort(404)
     else:
+        print("entre al elseeeeeeeeeeeeeeeeeeeeeee")
         storage.delete(userArr)
         storage.save()
     return jsonify({}), 200
 
 
-@app_views.route("/states", methods=['POST'], strict_slashes=False)
+
+@app_views.route("/users", methods=['POST'], strict_slashes=False)
 def set_user_POST():
     """
-    State object
+    User object
     """
     if not request.json:
         return jsonify({"error": "Not a JSON"}), 400
@@ -61,12 +64,14 @@ def set_user_POST():
         return jsonify({"error": "Missing email"}), 400
     if 'password' not in request.json:
         return jsonify({"error": "Missing password"}), 400
-    user_post = User(**request.get_json())
+    user_post = User(email=request.json["email"],
+                     password=request.json["password"])
+    storage.new(user_post)
     user_post.save()
     return jsonify(user_post.to_dict()), 201
 
 
-# @app_views.route("/users/<user_id>", methods=['PUT'],
+@app_views.route("/users/<user_id>", methods=['PUT'],
                  strict_slashes=False)
 def set_user_PUT(user_id):
     """
@@ -78,7 +83,9 @@ def set_user_PUT(user_id):
     if user is None:
         abort(404)
     for atriv, val in request.get_json().items():
-        setattr(user, atriv, val)
+        if ((atriv != "id" and atriv != "state_id" and
+             atriv != "created_at" and atriv != "updated_at")):
+            setattr(user, atriv, val)
     storage.save()
     return jsonify(user.to_dict()), 200
 
