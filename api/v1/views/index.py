b7ef30dc042fbd33e0fe45700298d852a03b3153
joshua <joshuaciencia2016@gmail.com>
2020-01-30 02:16:33 +0000

    Fixed docstring + pep8 + made files executable

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
-from json import dumps
-from flask import jsonify
 """
 index route of the api
 """
+from api.v1.views import app_views
+from flask import jsonify
+
 
 @app_views.route('/status')
 def status():
