6051a8ef8348b75989f290e5d497f9df00856beb
arq-gabo <jgabrielguerral@gmail.com>
2020-01-30 04:10:59 +0000

    [FIX]Correction Host and Port

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ Module Aplication
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,6 +21,6 @@ def closeMethod(exception=None):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host=os.eviron['0.0.0.0'],
-            port=os.eviron['5000'],
+    app.run(host=os.environ['HBNB_API_HOST'],
+            port=os.environ['HBNB_API_PORT'],
             threaded=True)
