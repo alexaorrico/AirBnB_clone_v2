5a388176b5667c441fdc37918b60cfc9a0c866a7
Dany <danyramirezg@gmail.com>
2020-01-30 22:00:15 -0500

    Modify test_db_storage.
    Add skipTest

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -59,13 +59,14 @@ test_db_storage.py'])
         self.assertTrue(len(DBStorage.__doc__) >= 1,
                         "DBStorage class needs a docstring")
 
-    # def test_dbs_func_docstrings(self):
-    #     """Test for the presence of docstrings in DBStorage methods"""
-    #     for func in self.dbs_f:
-    #         self.assertIsNot(func[1].__doc__, None,
-    #                          "{:s} method needs a docstring".format(func[0]))
-    #         self.assertTrue(len(func[1].__doc__) >= 1,
-    #                         "{:s} method needs a docstring".format(func[0]))
+    def test_dbs_func_docstrings(self):
+        """Test for the presence of docstrings in DBStorage methods"""
+        self.skipTest("Testing")
+        for func in self.dbs_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
 
 
 class TestFileStorage(unittest.TestCase):
