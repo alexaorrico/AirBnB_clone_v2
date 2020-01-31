87dda8509a92a556a6c899e2f3813543d57a2dd8
Max Stuart <maxastuart@gmail.com>
2020-01-31 04:26:19 +0000

    renamed file for checker

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,4 +13,4 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.reviews import *
+from api.v1.views.places_reviews import *
