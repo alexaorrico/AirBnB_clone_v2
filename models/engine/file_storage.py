d2bdada166bb46d10d023bb815f9505cdd82e5a7
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-01-30 19:49:39 -0800

    Removed extra lines and commit() from DB delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -75,7 +75,6 @@ class FileStorage:
                 del self.__objects[key]
                 self.save()
 
-
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
