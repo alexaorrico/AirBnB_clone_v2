c764989a21374ab867f897584fa4d8f75bb456c5
apatinoc4 <apatinoc4@gmail.com>
2020-01-30 19:13:13 -0500

    test: updating methods and init

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,8 +11,8 @@ from models.city import City
 @app_views.route('/states/<state_id>/cities',
                  methods=['GET'], strict_slashes=False)
 def show_cities(state_id):
-    """lists all states"""
-    s_list = []
+    """This functions lists all the cities"""
+    list_t = []
     states = storage.all("State")
     s_id = "State." + state_id
     if states.get(s_id) is None:
@@ -21,13 +21,13 @@ def show_cities(state_id):
         cities = storage.all("City")
         for city in cities.values():
             if city.state_id == state_id:
-                s_list.append(city.to_dict())
-    return jsonify(s_list)
+                list_t.append(city.to_dict())
+    return jsonify(list_t)
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
-def GetCityById(city_id):
-    """Retrieves city based on its id for GET HTTP method"""
+def get_city(city_id):
+    """This functions get a specific state by id"""
     cities = storage.all("City")
     c_id = "City." + city_id
     if cities.get(c_id) is None:
@@ -38,8 +38,8 @@ def GetCityById(city_id):
 
 @app_views.route('/cities/<city_id>',
                  methods=['DELETE'], strict_slashes=False)
-def DeleteCityById(city_id):
-    """Deletes a city based on its id for DELETE HTTP method"""
+def delete_city(city_id):
+    """This function delete a state by id"""
     cities = storage.all('City')
     c_id = "City." + city_id
     to_del = cities.get(c_id)
@@ -52,19 +52,19 @@ def DeleteCityById(city_id):
 
 @app_views.route('/states/<state_id>/cities',
                  methods=['POST'], strict_slashes=False)
-def PostCity(state_id):
-    """Posts a city"""
-    info = request.get_json()
+def create_city(state_id):
+    """This function create a new city"""
+    data = request.get_json()
     states = storage.all("State")
-    pair = "State." + state_id
-    if states.get(pair) is None:
+    match = "State." + state_id
+    if states.get(match) is None:
         abort(404)
-    if not info:
+    if not data:
         abort(400, 'Not a JSON')
-    elif "name" not in info:
+    elif 'name' not in data:
         abort(400, 'Missing name')
     city = City()
-    city.name = info['name']
+    city.name = data['name']
     city.state_id = state_id
     city.save()
     city = city.to_dict()
@@ -72,22 +72,18 @@ def PostCity(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-def PutState(city_id):
-    """ Updates a City uses PUT HTTP method"""
-    info = request.get_json()
-    cities = storage.all("City")
-    pair = "City." + city_id
-    if cities.get(pair) is None:
+def update_city(city_id):
+    """This function update a state by id"""
+    data = request.get_json()
+    cities = storage.all('City')
+    match = 'City.' + city_id
+    if cities.get(match) is None:
         abort(404)
-    if not info:
+    if not data:
         abort(400, 'Not a JSON')
     else:
-        city = cities.get(info)
-        city.name = info['name']
+        city = cities.get(match)
+        city.name = data['name']
         city.save()
         city = city.to_dict()
     return jsonify(city), 200
-
-
-if __name__ == '__main__':
-    pass
