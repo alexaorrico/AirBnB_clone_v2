340043bdd7679d6fe5fd76fb2ddd7b1c73eb135c
JulianDavidG07 <julygsaenz1995@hotmail.com>
2020-01-30 20:55:42 -0500

    [FIX] CORS Configuration

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,12 @@ from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
 port = os.environ.get('HBNB_API_PORT', '5000')
