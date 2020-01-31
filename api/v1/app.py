1864f7ce1a2b5c3aa3143c59e34b110035013421
joshua <joshuaciencia2016@gmail.com>
2020-01-31 02:12:48 +0000

    added cross-origin HTTP request

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,14 @@
 This module runs the flask api on
 """
 from flask import Flask, Blueprint, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -21,6 +23,7 @@ def shut(exec):
 def page_not_found(e):
     """ handle page not found error """
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(getenv('HBNB_API_HOST'), getenv('HBNB_API_PORT'), threaded=True)
