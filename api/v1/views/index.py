7441ab88030710ed6682f9ab46eb9d2bf5c7b95c
ecaoili24 <erika.caoili@gmail.com>
2020-04-27 20:26:23 +0000

    Added the import of jsonify

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
