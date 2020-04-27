c768d72130cb513e333f164162bb52a510f93cf2
jzamora5 <jzamora_5@yahoo.com>
2020-04-27 21:32:18 +0000

    Fix delete typo

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -28,7 +28,8 @@ def get_states(state_id=None):
         return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """
     Deletes a State Object
