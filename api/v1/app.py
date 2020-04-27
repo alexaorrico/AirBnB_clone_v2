095683823bbb848e8d9fb3ff3875f022bbeff5d7
JDorangetree <julian.naranjo2014@gmail.com>
2020-04-27 17:52:13 -0500

    [Fiz] pep8 style

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,4 +15,5 @@ def teardown_db(exception):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(getenv('HBNB_API_PORT', '5000')), threaded=True)
