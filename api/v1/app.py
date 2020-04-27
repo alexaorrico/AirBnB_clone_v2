d93884fbf36609f6b583f2999c1eb42bb7b09384
felipesv <1165@holbertonschool.com>
2020-04-27 17:55:43 -0500

    fix pep8, fix some import and finish the task #5 'Some stats?'

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,8 @@ Flask App that integrates with AirBnB static HTML Template
 
 from api.v1.views import app_views
 from models import storage
+from flask import Flask
+import os
 
 # Variable instance of flask named app
 app = Flask(__name__)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,9 @@
 Flask index file that returns the json status response
 """
 from api.v1.views import app_views
+from models import storage
+from flask import jsonify
+from flask import request
 
 
 @app_views.route('/status', methods=['GET'])
@@ -13,3 +16,15 @@ def status():
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
+
+
+@app_views.route("/stats", methods=['GET'])
+def count_objects():
+    """
+    function for count each obj in each class
+    """
+    list_class = ["Amenity", "City", "Place", "Review", "State", "User"]
+    dict_class = {}
+    for item in list_class:
+        dict_class[item] = storage.count(item)
+    return jsonify(dict_class)
