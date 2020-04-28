b753cb3b4f26b02405c1db823f57a8008aea95b7
jzamora5 <jzamora_5@yahoo.com>
2020-04-28 22:29:11 +0000

    Add conditions with length

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -123,12 +123,15 @@ def places_search():
 
     data = request.get_json()
 
-    if data:
+    if data and len(data):
         states = data.get('states', None)
         cities = data.get('cities', None)
         amenities = data.get('amenities', None)
 
-    if not data or (not states and not cities and not amenities):
+    if not data or not len(data) or (
+            not states and
+            not cities and
+            not amenities):
         places = storage.all(Place).values()
         list_places = []
         for place in places:
