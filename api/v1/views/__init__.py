4efb9ee7f02b94c6a48bd5e2b7e010e806ace014
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-28 00:14:16 +0000

    [feature]: API functions for users, cities and amenities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,3 +8,6 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
