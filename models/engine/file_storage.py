737213a5bf0af07a2f7ccbf28e131542e7706ef1
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-04-28 16:46:32 +0000

    [fix]: added default parameter for filestorage hash saving

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,7 +41,7 @@ class FileStorage:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
-    def save(self):
+    def save(self, save_fs=1):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
