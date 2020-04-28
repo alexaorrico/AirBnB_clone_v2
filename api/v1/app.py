8302355bd967ad21a7ef462be0f79f6bd33f26a0
cristian0497 <cristian04.97@hotmail.com>
2020-04-28 01:30:57 +0000

    [ADD] Add task 6, Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ API v1 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -12,6 +12,11 @@ app.register_blueprint(app_views)
 def close(close):
     """ Close the session """
     storage.close()
+
+@app.errorhandler(404)
+def error_handler(error):
+    """ Error Handler """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     h = os.getenv('HBNB_API_HOST') if os.getenv('HBNB_API_HOST') else '0.0.0.0'
