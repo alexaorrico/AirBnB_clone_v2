bc834cc6cc4557fa65dc5bf1a41b956ffed1d8cd
jzamora5 <jzamora_5@yahoo.com>
2020-04-28 00:25:48 +0000

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,8 +4,10 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
