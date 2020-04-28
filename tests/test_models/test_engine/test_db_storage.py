fad09a38690fdf945df5ac2201ce5e043380fd85
0x00developer <info@tecnoayuda.co>
2020-04-28 11:31:56 -0500

    [Feature] Task 3. Improve storage 	modified:   models/engine/db_storage.py 	modified:   models/engine/file_storage.py 	new file:   test_get_count.py 	modified:   tests/test_models/test_engine/test_db_storage.py 	modified:   tests/test_models/test_engine/test_file_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,15 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """Test method to retrieve one object"""
+        user = User(name="User1")
+        user.save()
+        self.assertEqual(models.storage.get("User", user.id), user)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """Test method to count the number of objects in storage"""
+        self.assertEqual(len(models.storage.all()), models.storage.count())
