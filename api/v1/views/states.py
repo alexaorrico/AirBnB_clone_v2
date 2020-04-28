21ede91aad160347b6aeb79019e81bf75333dab1
Doouh <ake.giraldo@hotmail.com>
2020-04-27 20:14:35 -0500

    Add method put for states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,6 +3,7 @@
 from models.base_model import *
 from api.v1.views import app_views
 from models import storage
+from models.state import *
 from flask import jsonify, abort, request, make_response
     
 
@@ -20,8 +21,9 @@ def states():
         elif not 'name' in request.json:
             return make_response(jsonify({'error': "Missing name"}), 400)
         else:
-           new = State(request.json)
-           new.save()
+            new = State(**request.json)
+            new.save()
+            return make_response(new.to_dict(), 201)
            
 
 
@@ -36,4 +38,15 @@ def state(state_id):
                 storage.delete(value)
                 storage.save()
                 return {}
+            elif request.method == "PUT":
+                if not request.json:
+                    return make_response(jsonify({'error': "Not a JSON"}), 400)
+                else:
+                    json = request.json
+                    for key2, value2 in json.items():
+                        if key2 != 'id' and key2 != 'created_at' and key2 != "updated_at":
+                            setattr(value, key2, value2)
+                    value.updated_at = datetime.utcnow() 
+                    storage.save()
+                    return make_response(value.to_dict(), 200)
     abort(404)
