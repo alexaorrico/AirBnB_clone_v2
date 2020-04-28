101a054146da5bb72f5edf19a5799e774971a8e1
evertcolombia <evertescalante@gmail.com>
2020-04-28 16:10:27 +0000

    adding get function on filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Returns object of specific class based on the id
+        args:
+            @cls: class name
+            @id: id instance to match
+        """
+        if cls is not None:
+            cls_instances = self.all(cls)
+            match = {}
+            for key, value in cls_instances.items():
+                if value.id == id:
+                    match[key] = value
+                    return match
+            return None
