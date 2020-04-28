1b6d7eaa370d73b90b113ef6f576d3f5a941c52c
cristian0497 <cristian04.97@hotmail.com>
2020-04-28 01:44:00 +0000

    [FIX] wrong merge

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,18 +13,11 @@ def close(close):
     """ Close the session """
     storage.close()
 
-<<<<<<< HEAD
+
 @app.errorhandler(404)
 def error_handler(error):
     """ Error Handler """
     return make_response(jsonify({'error': 'Not found'}), 404)
-=======
-    
-@app.errorhandler(404)
-def error_handler(error):
-        """ Error Handler """
-            return make_response(jsonify({'error': 'Not found'}), 404) 
->>>>>>> 13c7948dc3e2bf921761351403137d58163d365c
 
 if __name__ == "__main__":
     h = os.getenv('HBNB_API_HOST') if os.getenv('HBNB_API_HOST') else '0.0.0.0'
