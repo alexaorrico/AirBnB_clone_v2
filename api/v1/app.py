9654bdcc99c442207545f28095b2a5189f55676f
JDorangetree <julian.naranjo2014@gmail.com>
2020-04-27 19:07:35 -0500

    [Feature] handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,16 @@ from models import storage
 from flask import Flask
 from api.v1.views import app_views
 from os import getenv
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """handler for 404 errors that returns a JSON-formatted"""
+    return jsonify(error="Not found"), 404
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
@@ -22,7 +21,7 @@ def status():
 @app_views.route('/stats')
 def stats():
     """returns a JSON: "status": "OK"""
-    classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+    classes = {"Amenity": Amenity, "City": City,
                "Place": Place, "Review": Review, "State": State, "User": User}
     dict_count = {}
     count = 0
