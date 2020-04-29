e72c7a9d681f9560e0f3a1343a60ef998f09c951
francisco0522 <pacho0522@gmail.com>
2020-04-28 23:33:52 -0500

    Adding task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,17 @@ def status():
     """ object app_views that returns a JSON """
     app_views = {'status': 'OK'}
     return jsonify(app_views)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ endpoint that retrieves the number of each objects """
+    class_stat = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(class_stat)
