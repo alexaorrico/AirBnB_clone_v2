2e7db6e6f66381944cdd992d0534d57773ce4fbd
Thorlak2202 <darkdrakkar@gmail.com>
2020-04-29 09:55:03 -0500

    starting the api

---
+++ api/v1/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+"""
+
+from flask import Flask, render_template
+from api.v1.views import app_views
+from models import *
+from models import storage
+from flask import jsonify
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """handler for 404 errors that returns a JSON-formatted
+    404 status code response.
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', 5000), threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """returns the status of the api"""
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def object_by_type():
+    """retrieves the number of each objects by type
+    """
+    objects = jsonify({
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+        })
+    return objects
