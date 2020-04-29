dddbc56e480a4d781871d7b1c41ae553e406b039
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-29 01:43:38 -0500

    Added handler for 404 errors that returns a  JSON-formatted 404 status code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """App module
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -17,6 +17,15 @@ def teardown_db(error):
     """Closes the database again at the end of the request.
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """ handler for 404 errors that returns a
+        JSON-formatted 404 status code
+    """
+    err_stat = {"error": "Not found"}
+    return jsonify(err_stat), 404
 
 
 if __name__ == "__main__":
