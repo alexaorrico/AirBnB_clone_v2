f6cfad0c64586858fc782fe0d2e9a7249d807ec8
marcewp15 <970@holbertonschool.com>
2020-04-29 01:26:28 +0000

    [Fix] Documentation, pep8 and do executable all files.
    Files:
    api/__init__.py
    api/v1/__init__.py
    api/v1/app.py
    api/v1/views/__init__.py
    api/v1/views/index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
+"""
+Index file
+"""
 from flask import jsonify
+from api.v1.views import app_views
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def jsonify_app():
-    return jsonify({"status":"OK"})
+    """ Function that returns a JSON """
+    return jsonify({"status": "OK"})
