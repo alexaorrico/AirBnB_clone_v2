708476054dd1693f938c617cae909fe4acfdde41
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-29 00:53:59 +0000

    removed users

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -2,7 +2,6 @@
 """Place module"""
 from api.v1.views import app_views
 from models import storage
-from models.user import User
 from models.city import City
 from models.place import Place
 from flask import jsonify, request, abort
