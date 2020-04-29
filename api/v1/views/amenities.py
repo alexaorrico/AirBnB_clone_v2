d7fe042f00dc7cf60762eb5c9df4f7e618393089
darkares23 <desarrollojuseca@gmail.com>
2020-04-29 18:19:51 -0500

    new routes for amenities get method

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,47 +1,47 @@
 #!/usr/bin/python3
-"""Create a new view for Amenities"""
+"""Create a new view for amenities"""
+
+
 from flask import jsonify, abort, request
 from models import storage
 from models.amenity import Amenity
 from api.v1.views import app_views
 
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False,
-                 defaults={'amenity_id': 0})
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 @app_views.route('/amenities/<amenity_id>',
-                 methods=['GET'],
-                 strict_slashes=False)
-def get_amenities(amenity_id):
-    dic = storage.all(Amenity)
-    ids = "Amenity." + str(amenity_id)
-    if (amenity_id == 0):
-        amenities_list = []
-        for k, v in dic.items():
-            amenities_list.append(v.to_dict())
-        return jsonify(amenities_list)
-    elif (ids not in dic.keys()):
-        abort(404)
+                 methods=['GET'], strict_slashes=False)
+def amenities(amenity_id=None):
+    """view for State objects that handles all default RestFul API actions"""
+    if amenity_id:
+        amenity_id = "Amenity.{}".format(amenity_id)
+        all_Amenity = storage.all('Amenity')
+        if amenity_id in all_Amenity:
+            return jsonify(all_Amenity[amenity_id].to_dict())
+        else:
+            abort(404)
     else:
-        return jsonify(dic[ids].to_dict())
+        amenityList = []
+        all_amenities = storage.all('Amenity')
+        for amenity in all_amenities.values():
+            amenityList.append(amenity.to_dict())
+        return jsonify(amenityList)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False, methods=['DELETE'])
 def amenities_delete(amenity_id=None):
-    """Deletes a State object:: DELETE /api/v1/states/state_id"""
     id_amenity = storage.get('Amenity', amenity_id)
     if amenity_id is None:
         abort(404)
-    else:
-        storage.delete(id_amenity)
-        storage.save()
-        return jsonify({}), 200
+    storage.delete(id_amenity)
+    storage.save()
+    return jsonify({}), 200
 
 
 @app_views.route('/amenities', methods=['POST'],
                  strict_slashes=False)
 def amenities_create():
-    """Creates new states"""
     res = request.get_json()
     if res is None:
         abort(400, "Not a JSON")
@@ -53,17 +53,16 @@ def amenities_create():
     return jsonify(newAmenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
-                 strict_slashes=False)
-def amenities_put(amenity_id=None):
-    """updates a value from an instance"""
-    amenity = storage.get('Amenity', amenity_id)
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False,
+                 methods=['PUT'])
+def modify_amenity(amenity_id):
+    """ An endpoint that modifies an existing amenity """
     res = request.get_json()
-
+    if res is None:
+        abort(400, 'Not a JSON')
+    amenity = storage.get('Amenity', amenity_id)
     if amenity is None:
         abort(404)
-    if res is None:
-        abort(400, "Not a JSON")
     for k, v in res.items():
         if k != 'id' and k != 'created_at' and k != 'updated_at':
             setattr(amenity, k, v)
