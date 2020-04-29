dc747e0f1b9413f73df882df7df95c2844d4026c
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-04-29 22:37:43 +0000

    add file api 4. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+"""
+
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from os import getenv
+#from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+#cors = CORS(app, resources={r'/*': {'origins': "*"}})
+
+
+@app.teardown_appcontext
+def teardown_app(self):
+    """
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
