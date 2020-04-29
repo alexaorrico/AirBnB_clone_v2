09a0b1d907f73159005706c0e13d30da028744dc
ArthurAbeshouse <artieabe99@gmail.com>
2020-04-29 15:28:13 +0000

    changes

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -45,11 +45,13 @@ def post_review(place_id):
     """Creates a new review object"""
     if storage.get(Place, place_id) is None:
         abort(404)
+    if storage.get(User, user_id) is None:
+        abort(404)
     if not request.get_json():
         return (jsonify({"error": "Not a JSON"})), 400
-    if "user_id" not in request.get_json():
-        return (jsonify({"error": "Missing user_id"})), 400
     data = request.get_json()
+    if "user_id" not in data:
+        return (jsonify({"error": "Missing user_id"})), 400
     user_id = data["user_id"]
     if "text" not in data:
         return (jsonify({"error": "Missing text"})), 400
