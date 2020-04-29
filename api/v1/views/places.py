ae723eb9d930366b9e889b88ff5be221a39f910e
cristian0497 <cristian04.97@hotmail.com>
2020-04-29 05:49:48 +0000

    [ADD] New version, pep8 OK

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,9 +6,10 @@ from flask import jsonify, abort, request
 from models.place import Place
 
 
-@app_views.route("/cities/<city_id>/places", methods=['GET'], strict_slashes=False)
+@app_views.route("/cities/<city_id>/places", methods=['GET'],
+                 strict_slashes=False)
 def all_Places(city_id):
-    """ return all Place on a City ID 
+    """ return all Place on a City ID
     if not found, error 404 """
     dic = []
     if item_locator(city_id, 'City') is False:
@@ -20,9 +21,10 @@ def all_Places(city_id):
     return jsonify(dic)
 
 
-@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['GET'],
+                 strict_slashes=False)
 def place_id(place_id):
-    """ Return a Place id 
+    """ Return a Place id
     or Return 404 if it not found """
     place = storage.get('Place', place_id)
     if place:
@@ -31,7 +33,8 @@ def place_id(place_id):
         abort(404)
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_place(place_id):
     """ Delete a Place with their ID, return a void dictionary,
     if place not found, return 404 status"""
@@ -44,48 +47,53 @@ def delete_place(place_id):
         abort(404)
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
 def create_place(city_id):
-    """ Create a Place """
+    """ Create a Place, and return their dictionary representation
+    if city_id its not found, 404 error return """
     if not (request.is_json):
         abort(400, "Not a JSON")
     city = storage.get('City', city_id)
     if city is None:
         abort(404)
-    if 'user_id' not in request.json:
-        abort(400, "Missing user_id")
-    if 'name' not in request.json:
-        abort(400, "Missing name")
     kwargs = request.get_json()
-    if not item_locator(kwargs['user_id'], 'User'):
+    if 'user_id' not in kwargs:
+        abort(400, "Missing user_id")
+    elif item_locator(kwargs['user_id'], 'User') is False:
         abort(404)
+    if 'name' not in kwargs:
+        abort(400, "Missing name")
+    if 'city_id' not in kwargs:
+        kwargs['city_id'] = city_id
     place = Place(**kwargs)
     storage.new(place)
     storage.save()
     return jsonify(place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-def upd_place(place_id):
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_place(place_id):
     """ Update Place ID, method PUT"""
     place = storage.get('Place', place_id)
-    print("--> {}".format(place))
+    ignore_list = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
     if not place:
         abort(404)
     json_content = request.get_json()
     if json_content is None:
         abort(400, 'Not a JSON')
     for key, value in json_content.items():
-        if key != 'id' and key != 'created_ad' and key != 'updated_at':
+        if key not in ignore_list:
             setattr(place, key, value)
     storage.save()
     return jsonify(place.to_dict()), 200
 
 
 def item_locator(id, item):
-    """ fin into items list """
+    """ find into items list """
     list_items = storage.all(str(item)).items()
     for key, value in list_items:
-        if value.to_dict()['id']== id:
+        if value.to_dict()['id'] == id:
             return True
     return False
