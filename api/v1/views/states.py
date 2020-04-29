771cf51044f6d9b7befa75ac709fb0852a5b7b42
0x00developer <info@tecnoayuda.co>
2020-04-29 10:54:19 -0500

    [Feature] 7. State 	modified:   api/v1/views/__init__.py 	new file:   api/v1/views/states.py

---
+++ api/v1/views/states.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" view for State objects that handles all default RestFul API actions """
+from flask import jsonify, abort, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """ Retrieves the list of all State objects """
+    states = storage.all(State)
+    return jsonify([obj.to_dict() for obj in states.values()])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state_id(state_id):
+    """ Retrieves a State object """
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def del_state_id(state_id):
+    """ Deletes a State object """
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    state.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_states():
+    """ Creates a State object """
+    new_state = request.get_json()
+    if not new_state:
+        abort(400, "Not a JSON")
+    if "name" not in new_state:
+        abort(400, "Missing name")
+    state = State(**new_state)
+    storage.new(state)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state_id(state_id):
+    """ Updates a State object """
+    state = storage.get("State", state_id)
+    if not state:
+        abort(404)
+    my_dict = request.get_json()
+    if not my_dict:
+        abort(400, "Not a JSON")
+    for key, value in my_dict.items():
+        if key != 'id' and key != 'created_at' and key != 'updated_at':
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
