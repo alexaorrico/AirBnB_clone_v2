f79c01830e440c7fe0f186e0a6704bc2f8413889
ACholberton <1103@holbertonschool.com>
2020-04-29 05:02:27 +0000

    created several files for the api initialization

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,3 +2,11 @@
 """ Api """
 
 from api.v1.views import app_views
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+@app_views.route('api/v1/status')
+
+def return_jason():
+    """ return json status"""
+    return jsonify(status = 'OK')
