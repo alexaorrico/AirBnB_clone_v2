8368d3974a9e23a9aadcff48f017138b230539a0
Juan-Bogota <1213@holbertonschool.com>
2020-04-28 22:20:44 -0500

    [feature] Design a “404 page”, a “Not found”

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 Flask web application api
 """
 from flask import Blueprint
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -17,6 +17,13 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """set the 404 status explicitly"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     my_host = os.getenv('HBNB_API_HOST')
