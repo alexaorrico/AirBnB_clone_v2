a3a801195e09267bdee542fe4e0a54e32ab43e29
cbayonao <drcamilob@live.com>
2020-04-29 20:01:44 +0000

    [Documentation]

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
-
+"""
+Status of your API
+"""
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
