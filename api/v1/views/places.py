2a2bf81202cfdfe6b09b80206f6feadb582f33d1
Juan-Bogota <1213@holbertonschool.com>
2020-04-29 22:37:06 -0500

    [Feature] Place objects that handles all default RestFul API actions

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,6 +9,7 @@ from models.state import State
 from models.city import City
 from models.place import Place
 
+
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
 def jsonify_places_1(city_id):
@@ -44,18 +45,21 @@ def jsonify_places_3(place_id):
 @app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
 def jsonify_places_4(city_id):
-    json_post = request.get_json()
-    the_obj = storage.get(City, city_id)
-    if the_obj is None:
+    try:
+        json_post = request.get_json()
+        the_obj = storage.get(City, city_id)
+        if the_obj is None:
+            abort(404)
+        if not json_post:
+            return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        if 'user_id' not in json_post:
+            return make_response(jsonify({'error': 'Missing name'}), 400)
+        json_post['city_id'] = city_id
+        new = Place(**json_post)
+        new.save()
+        return make_response(jsonify(new.to_dict()), 201)
+    except:
         abort(404)
-    if not json_post:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'user_id' not in json_post:
-        return make_response(jsonify({'error': 'Missing name'}), 400)
-    json_post['city_id'] = city_id
-    new = Place(**json_post)
-    new.save()
-    return make_response(jsonify(new.to_dict()), 201)
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
@@ -67,7 +71,7 @@ def jsonify_places_5(place_id):
     if not json_put:
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     for key, value in json_put.items():
-        if key not in ['id', 'state_id', 'created_at', 'update_at']:
+        if key not in ['id', 'created_at', 'city_id', 'update_at', 'user_id']:
             setattr(the_obj, key, value)
     storage.save()
     return make_response(jsonify(the_obj.to_dict()), 200)
