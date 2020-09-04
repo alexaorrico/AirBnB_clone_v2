04c57c75964d131e039f6bb5bbefba7c4bf9b889
GunterPearson <1715@holbertonschool.com>
2020-09-04 12:17:46 -0500

    pushing new methods for storage get to retrieve an object from class and count to count either specific class or all

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ method to get based on class[id]"""
+        if cls and id:
+            obj = "{}.{}".format(cls.__name__, id)
+            all_obj = self.all(cls)
+            return all_obj.get(obj)
+        return None
+
+    def count(self, cls=None):
+        """ method to count obj in class"""
+        return len(self.all(cls))
