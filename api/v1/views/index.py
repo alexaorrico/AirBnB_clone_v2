98e7ed09225c201b8fddb6f90a40e6b6d6d95105
Jilroge7 <1672@holbertonschool.com>
2020-09-06 19:48:27 +0000

    bug fix to api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify, Blueprint, render_template, abort
 
 
-@app_views.route('/status', method['GET'])
+@app_views.route('/status', methods=['GET'])
 def status():
     """status"""
-    return (jsonify({'status':'OK'}))
+    return (jsonify({"status": "OK"}))
