42726af83ba55c3939fd24cd935f4f2204dc2a94
Diego <dagomez2530@gmail.com>
2020-09-07 17:30:00 -0500

    Adding count endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Task 0"""
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 
@@ -8,3 +9,8 @@ from flask import jsonify
 def Index():
     """Function index"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def number_objects():
+    numb = storage.count()
+    return jsonify({"objects": numb})
