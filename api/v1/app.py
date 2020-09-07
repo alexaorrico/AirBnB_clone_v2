4838fe8a313a8340780802dd7c2ea483aedf0a9a
derric-d <derric.donehoo@gmail.com>
2020-09-07 18:16:40 +0000

    init apiV1 with app and views

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""make api"""
+from models import storage
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_appcontext():
+    """teardown method"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host = getenv('HBNB_API_HOST', '0.0.0.0'),
+            port = int(getenv('HBNB_API_PORT', '5000')),
+            threaded=True)
