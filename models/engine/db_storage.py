2b951cd6912d77cf5eb6da47194100f418baeed0
Sammy <1449@holbertonschool.com>
2020-09-07 14:27:28 -0500

    Fix index db storage and create first api

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,7 +75,7 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-        def get(self, cls, id):
+    def get(self, cls, id):
         """ retrieves one object """
         try:
             obj_dict = {}
