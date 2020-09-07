538b0d3e7d691d25f145e9afea0762067bb38b76
Sammy <1449@holbertonschool.com>
2020-09-07 15:50:18 -0500

    create first API and count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,13 +3,33 @@
 App views for AirBnB_clone_v3
 """
 
-from flask import jsonify
-from models import storage
 from api.v1.views import app_views
+from flask import jsonify, request
+from models import storage
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def status():
     """ returns status """
     status = {"status": "OK"}
     return jsonify(status)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        CLASSES = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in CLASSES.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
