318aa73fb04213f230415b0cfe60490b12ea76e8
Diego Gómez <dagomez2530@gmail.com>
2020-09-07 22:58:40 +0000

    Adding file for task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """Task 0"""
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
 from flask import jsonify
 
@@ -10,7 +17,14 @@ def Index():
     """Function index"""
     return jsonify({"status": "OK"})
 
-@app_views.route('/api/v1/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'])
 def number_objects():
-    numb = storage.count()
-    return jsonify({"objects": numb})
+    """counts the number of objects for each model"""
+
+    classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
+    dictionary = {}
+    for key, cl in classes.items():
+        numb = storage.count(cl)
+        dictionary[key] = numb
+    return jsonify(dictionary)
