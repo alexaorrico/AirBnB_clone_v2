eec0f3bccfcbf0e1fece6ebef8350bdf3a35f432
kimha1030 <1478@holbertonschool.com>
2020-09-07 23:14:51 +0000

    Add error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,18 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask import make_response
+from flask import jsonify
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
