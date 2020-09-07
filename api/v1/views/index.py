e0155b2e018b5bc26b58fd10d098eae76a55ae82
kimha1030 <1478@holbertonschool.com>
2020-09-07 21:55:18 +0000

    Fix route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('api/v1/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def Index():
     """Function index"""
     return jsonify({"status": "OK"})
