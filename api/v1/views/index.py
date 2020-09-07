abeeae00ba56cbd1ab16aef0c958de3c6bb9c414
tabbykatz <tomelay@gmail.com>
2020-09-07 17:11:11 +0000

    add work on index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" View index:
-    endpoint that retrieves
-    number of objects by type"""
+"""import app_views from api.v1.views
+create a route /status on the object app_views that returns a JSON: status:
+    OK (see example)"""
 from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 from models import storage
@@ -15,13 +15,13 @@ from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def _status():
-    """returns a JSON file with Status: OK"""
+    """ return a JSON file with Status: OK """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    """retrieves the number of each objects by type"""
+    """Create an endpoint that retrieves the number of each objects by type"""
     my_amenity = storage.count("Amenity")
     my_cities = storage.count("City")
     my_places = storage.count("Place")
