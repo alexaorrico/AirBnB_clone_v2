f0047d5048606f19fd2287d441a4d80557f40722
duvanjm <duvanjarin@gmail.com>
2020-09-08 18:19:12 +0000

    update file_storage, db_storage, add api folder

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Status of your API"""
+
+from flask import Flask, make_response, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """calls storage.close()"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def error_handler(not_found):
+    """“404 page”, a “Not found”"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host, port, threaded=True)
---
+++ api/v1/views/states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Create a new view for State objects"""
+
+from flask import jsonify, request
+from models.state import State
+from models import storage
+
+
+@app_views.route('/states', methods=['GET', 'POST'])
+def states_list():
+    """Retrieves the list of all State objects"""
+    if request.method == 'GET':
+        all_states = []
+        for key in storage.all("State").values():
+            all_states.append(ket.to_dict())
+        return jsonify(all_states)
+    if request.method == 'POST':
+        response = request.get_json()
+        if not response:
+            abort(400, "Not a JSON")
+        if "name" not in response:
+            abort(400, "Missing name")
+        new_state = State(**response)
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route("/states/<state_id>", methods=['GET', 'DELETE', 'PUT'])
+def states_id(state_id):
+    """Deletes a State object:"""
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    if request.method == 'GET':
+        return jsonify(state.to_dict())
+    if request.method == 'DELETE':
+        storage.detete(state)
+        storage.save()
+        return jsonify({}), 200
+    if request.method == 'PUT':
+        response = request.get_json()
+        if not response:
+            abort(400, "Not a JSON")
+        for key, val in response.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(state, key, val)
+        storage.save()
+        return jsonify(state.to_dict()), 200
