c4d6c23474fe3bcaaccb142b7cfbf52a2830a141
GunterPearson <1715@holbertonschool.com>
2020-09-08 14:33:30 -0500

    pushing updated security for password

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,13 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """ gets password """
+        return self.password
+
+    @password.setter
+    def password(self, password):
+        """ encrypt password"""
+        self.password = md5(password.encode()).hexdigest()
