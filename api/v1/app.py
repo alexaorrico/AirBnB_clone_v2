e18d290def308393b41d5628405e397d069c7410
Ian Culp <icculp@gmail.com>
2020-09-08 15:59:42 +0000

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,4 +25,5 @@ def dudewermypge(e):
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv("HBNB_API_HOST"), port=os.getenv("HBNB_API_PORT"), threaded=True)
+    app.run(host=os.getenv("HBNB_API_HOST"),
+            port=os.getenv("HBNB_API_PORT"), threaded=True)
