672158ecf088b9abb7dbe704e0dc209e3a31de61
FouedDadi <foued.dadii@gmail.com>
2020-09-08 17:13:53 +0100

    fix repo

--- api/v1/views/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-    """[Blueprint and wildcard import of everything]
-    """
-
-from flask doc import Blueprint
-app_views = Blueprint('app', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
