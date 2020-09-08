c6c7175e335fcc13f0628730310f704b8b98b3b9
Viet Tran <vietnamtran@gmail.com>
2020-09-08 15:11:05 +0000

    pep8 compliance

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,6 +9,7 @@ def get_status():
     """returns a JSON: 'status': 'OK'"""
     return jsonify({'status': 'OK'})
 
+
 @app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def get_stats():
     """returns a JSON: 'class': count"""
