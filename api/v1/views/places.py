75b5684f313d350c76d5da87f1429684e380e06f
Corbin Enterline <corbinenterline1@gmail.com>
2020-09-08 23:50:32 +0000

    places fix

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -21,22 +21,22 @@ def viewalltheplacethings(city_id):
     if request.method == 'POST':
         try:
             body = request.get_json()
+            if "user_id" not in body.keys():
+                return "Missing user_id", 400
+            elif "name" not in body.keys():
+                return "Missing name", 400
+            else:
+                if storage.get(User, body.get('user_id')) is None:
+                    abort(404)
+                body.update({"city_id": city_id})
+                newplace = Place(**body)
+                """for k in body.keys():
+                    setattr(newstate, k, body.get(k))"""
+                """newstate.__dict__.update(body)"""
+                newplace.save()
+                return jsonify(newplace.to_dict()), 201
         except:
             return "Not a JSON", 400
-        if "user_id" not in body.keys():
-            return "Missing user_id", 400
-        elif "name" not in body.keys():
-            return "Missing name", 400
-        else:
-            if storage.get(User, body.get('user_id')) is None:
-                abort(404)
-            body.update({"city_id": city_id})
-            newplace = Place(**body)
-            """for k in body.keys():
-                setattr(newstate, k, body.get(k))"""
-            """newstate.__dict__.update(body)"""
-            newplace.save()
-            return jsonify(newplace.to_dict()), 201
 
 
 @app_views.route('/places/<place_id>', strict_slashes=False,
@@ -55,20 +55,20 @@ def placeidtime(place_id):
         if request.method == 'PUT':
             try:
                 body = request.get_json()
+                body.pop("id", "")
+                body.pop("created_at", "")
+                body.pop("updated_at", "")
+                body.pop("user_id", "")
+                body.pop("city_id", "")
+                """s.__dict__.update(body)"""
+                for k in body.keys():
+                    setattr(ptl, k, body.get(k))
+                """s.save()"""
+                ptl.save()
+                sd = ptl.to_dict()
+                return jsonify(sd)
             except:
                 return "Not a JSON", 400
-            body.pop("id", "")
-            body.pop("created_at", "")
-            body.pop("updated_at", "")
-            body.pop("user_id", "")
-            body.pop("city_id", "")
-            """s.__dict__.update(body)"""
-            for k in body.keys():
-                setattr(ptl, k, body.get(k))
-            """s.save()"""
-            ptl.save()
-            sd = ptl.to_dict()
-            return jsonify(sd)
 
     else:
         abort(404)
