887e5dc82dfcf370a7c1146ef437f4a16138a464
R6Doc <fiendgear@gmail.com>
2020-09-08 10:52:01 -0500

    added stats retrieving method for index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python3
 """!"""
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Blueprint
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+abnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """Retrieve the stats"""
+    new_dict = {}
+    for key, value in abnbText.items():
+        new_dict[key] = storage.count(value)
+    return jsonify(new_dict)
 
 
 if __name__ == "__main__":
