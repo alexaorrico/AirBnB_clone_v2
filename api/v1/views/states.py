0b69e76562b7f679cb096a2dc3dd030f9cc15438
bryanbuiles <1482@holbertonschool.com>
2020-09-08 12:01:53 -0500

    task states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,11 +5,28 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/states', methods=['GET'])
-def allstates():
+@app_views.route('states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def allstates(state_id=None):
     """ jsonify """
-    lista = []
-    for i in storage.all(State):
-        j = i.BaseModel.to_dict()
-        lista.append(j)
-    return (lista)
+    if state_id is None:
+        lista = []
+        for v in storage.all(State).values():
+            lista.append(v.to_dict())
+        return (jsonify(lista))
+    else:
+        for v in storage.all(State).values():
+            if v.id == state_id:
+                attr = (v.to_dict())
+        return (jsonify(attr))
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete(state_id=None):
+    if state_id is None:
+        return
+    for v in storage.all(State).values():
+        if v.id == state_id:
+            storage.delete(v.id)
+            attr = (v.to_dict())
+    return (jsonify(attr))
