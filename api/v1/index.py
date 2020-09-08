ebb7af0bc6980c0f4535d3e4b4ed596372f66546
JuanJoseGomezR <juangomez0149@gmail.com>
2020-09-08 22:32:56 +0000

    [Fixing] File Locations (From wrong Locations)

--- api/v1/index.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Index for the API"""
-from api.v1.views import app_views
-
-
-@app_views.route('/status', strict_slashes=False)
-def ret_status():
-    """funct that returns status ok"""
-    return jsonify({"status": "OK"})
