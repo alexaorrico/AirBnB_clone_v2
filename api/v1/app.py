1521582820814f1c3e89d13516fd8a060534a023
derric-d <derric.donehoo@gmail.com>
2020-09-08 20:51:25 +0000

    add cors instance to app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,13 @@ from models import storage
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={"/*":{"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(self):
