ca1b056334cf2bf8afb74a68cae044b170fed109
FatChicken277 <1637@holbertonschool.com>
2020-09-09 15:48:47 -0500

    added app.py file

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module is responsible for the execution and management of the app."""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_storage(self):
+    """Close session."""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(self):
+    """Handle 404 errors.
+
+    Returns:
+        dict: Returns a JSON-formatted 404 status code response.
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    h = getenv("HBNB_API_HOST", "0.0.0.0")
+    p = getenv("HBNB_API_PORT", 5000)
+    app.run(debug=True, host=h, port=p, threaded=True)
