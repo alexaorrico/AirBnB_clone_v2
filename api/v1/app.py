624b6c1c54685f594b10ed82474285768956b1c7
davehh1211 <davehh1211@gmail.com>
2020-09-09 18:57:42 +0000

    correct pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,10 +15,12 @@ def closeteardown(exception):
     """close storage"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     """handles the error page"""
-    return jsonify({ "error": "Not found" }), 404
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'), port=int(os.getenv('HBNB_API_PORT','5000')))
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
