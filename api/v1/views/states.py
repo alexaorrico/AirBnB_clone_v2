a08366578cbed2ce08872daee42f7158fcc430dc
bryanbuiles <1482@holbertonschool.com>
2020-09-08 19:55:06 -0500

    task state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,5 @@
 from models.base_model import BaseModel, Base
-from flask import jsonify
+from flask import jsonify, abort, request
 from models.state import State
 from models import storage
 from api.v1.views import app_views
@@ -15,18 +15,44 @@ def allstates(state_id=None):
             lista.append(v.to_dict())
         return (jsonify(lista))
     else:
+        flag = 0
         for v in storage.all(State).values():
             if v.id == state_id:
                 attr = (v.to_dict())
-        return (jsonify(attr))
+                flag = 1
+        if flag == 0:
+            abort(404)
+        else:
+            return (jsonify(attr))
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete(state_id=None):
     if state_id is None:
-        return
+        abort(404)
+    dicti = {}
+    flag = 0
     for v in storage.all(State).values():
         if v.id == state_id:
-            storage.delete(v.id)
-            attr = (v.to_dict())
-    return (jsonify(attr))
+            storage.delete(v)
+            storage.save()
+            flag = 1
+    if flag == 0:
+        abort(404)
+    else:
+        return (jsonify(dicti), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_task():
+    if not request.json or not 'name' in request.json:
+        abort(400)
+
+    result = request.get_json()
+    obj = State()
+    for k, v in result.items():
+        setattr(obj, k, v)
+    storage.new(obj)
+    storage.save()
+    var = obj.to_dict()
+    return (jsonify(var), 201)
