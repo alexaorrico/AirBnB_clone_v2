8eee129ba86cdc9483e39155e87bca5728ba11c2
bryanbuiles <1482@holbertonschool.com>
2020-09-09 12:13:13 -0500

    init changes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,5 +4,6 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
+import api.v1.views.cities
 import api.v1.views.states
+from api.v1.views.index import *
