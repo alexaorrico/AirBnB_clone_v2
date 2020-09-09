124ab7748523e8550f6d1be7fb8a8fab6e04ba20
linkjavier <linkjavier@hotmail.com>
2020-09-09 13:07:47 -0500

    First commit to test new branch branch

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,8 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ """
+
+    def count(self, cls=None):
