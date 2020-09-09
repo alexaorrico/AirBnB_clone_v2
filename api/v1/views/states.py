fea5b60bbc332f03347d40e3ded10946090c1d7b
AndrewB4y <andresbaymon@gmail.com>
2020-09-09 13:28:23 +0000

    Fixing Restful API responses

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,10 +7,10 @@
 from models.state import State
 from models import storage
 from api.v1.views import app_views
-from flask import jsonify, request, make_response
+from flask import jsonify, request, make_response, abort
 
 
-@app_views.route('/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     """ Retrieves all the states stored """
     states = storage.all(State)
@@ -18,7 +18,7 @@ def get_all_states():
     return jsonify(out)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_a_state(state_id=None):
     """ Retrieves a state object according to its id """
 
@@ -40,14 +40,14 @@ def delete_a_state(state_id=None):
         return abort(404)
     my_state = storage.get(State, state_id)
     if my_state is not None:
-        storage.delete(my_state.id)
+        storage.delete(my_state)
         storage.save()
         return jsonify({})
 
     return abort(404)
 
 
-@app_views.route('/states', methods=['POST'])
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_a_state():
     """
         Creates a new State object according to
@@ -65,13 +65,15 @@ def create_a_state():
     return make_response(jsonify(new.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'])
-def get_a_state(state_id=None):
-    """ Retrieves a state object according to its id """
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_a_state(state_id=None):
+    """ Updates a state object according to its id """
 
     if state_id is None:
         return abort(404)
+
     my_state = storage.get(State, state_id)
+
     if my_state is not None:
         body = request.get_json(silent=True)
         if body is None:
@@ -83,5 +85,7 @@ def get_a_state(state_id=None):
         if 'updated_at' in body:
             del body['updated_at']
         my_state.__dict__.update(**body)
+        my_state.save()
+        return jsonify(my_state.to_dict())
 
     return abort(404)
