cb23b6166e56a1753078f7c5d9b4ffdbcd8acd7d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-09-09 11:43:04 -0500

    fix return status task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 """New Funtion index"""
 from api.v1.views import app_views
-from models import storage
 from flask import jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def reoute_status():
@@ -18,10 +10,5 @@ def reoute_status():
         json: json count number of instances
     """
     return jsonify({
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
+        "status": "OK"
     }), 200
