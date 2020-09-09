698b556cb6a8eba9a6192eaa5faaec2fd6097f03
Felipe Layos <jfelipelayos@gmail.com>
2020-09-09 20:27:07 +0000

    PEP8 fix

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,6 +12,7 @@ HBNBCommand = console.HBNBCommand
 
 class TestConsoleDocs(unittest.TestCase):
     """Class for testing documentation of the console"""
+
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
