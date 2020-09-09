0aee20ccfb4423cbc2bfd6c7f2e05835021be962
Marisol2201 <1529@holbertonschool.com>
2020-09-09 13:33:10 +0000

    task 4. mysql was failing

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Index module"""
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"})
