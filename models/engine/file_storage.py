ccbcf1bf0365e73e1e9e0ea45fa683aa38d4a44b
linkjavier <linkjavier@hotmail.com>
2020-09-09 13:57:12 -0500

    Added get and count function to file_storage and db_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -68,3 +69,30 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+            Returns the object based on the class name and its ID, or None if not found
+        """
+        all_obj = self.all(cls)
+        for id_obj in all_obj.values():
+            if id_obj.id == id:
+                return id_obj
+        return None
+
+
+    def count(self, cls=None):
+        """
+            Returns the number of objects in storage matching the given class name.
+            If no name is passed, returns the count of all objects in storage.
+        """
+        all_obj = classes.values()
+        
+        if not cls:
+            count = 0
+            for item in all_obj:
+                count += len(models.storage.all(item).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
