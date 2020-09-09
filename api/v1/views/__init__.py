0ec42621e7e70ba37ceba3305e85c28d1db38d01
FatChicken277 <1637@holbertonschool.com>
2020-09-09 12:21:53 -0500

    remove url_prefix from app.py and added to theinit file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """This module is responsible for creating the necessary blueprint."""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__)
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
 if app_views:
     from api.v1.views.index import *
