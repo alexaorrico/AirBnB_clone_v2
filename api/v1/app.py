153536da84d09ba59c0edce9bc17c8edd3af645f
sarias12 <1638@holbertonschool.com>
2020-09-09 18:50:49 +0000

    [Feature] - Task 6 Handler for 404 errors with response 'Not found'

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from flask import Flask
 from models import storage
 from os import getenv
 from api.v1.views import app_views
+from flask import make_response, jsonify
 
 app = Flask(__name__)
 
@@ -16,6 +17,15 @@ app.register_blueprint(app_views)
 def close_db(error):
     """Closes the database again at the end of the request."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handler for 404 errors that returns a JSON-formatted
+    404 status code response.
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
