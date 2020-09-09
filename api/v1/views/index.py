aae797dd478f29c0938f606bde5665492930e657
R6Doc <fiendgear@gmail.com>
2020-09-09 14:05:18 -0500

    changing dict place

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""!"""
+"""!!!!"""
 from api.v1.views import app_views
 from flask import Flask, jsonify, Blueprint
 from models import storage
-
-
-@app_views.route('/status', strict_slashes=False)
-def status():
-    return jsonify({"status": "OK"})
 
 abnbText = {
     "amenities": "Amenity",
@@ -17,6 +12,11 @@ abnbText = {
     "states": "State",
     "users": "User"
 }
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
