3245aea630ed85731af5f1310db5dda91cffdf4c
julianfrancor <julianfrancor@gmail.com>
2020-09-09 16:04:00 -0500

    adding tests for db_storage and for file_storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,37 +87,45 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_create(self):
-            """ Test create a class """
-            storage = DBStorage()
-            storage.reload()
-            initial_count = len(storage.all(State))
-            initial_db = cursor_objects.execute(
-                """SELECT COUNT(id) FROM states;""")
 
-            self.assertTrue(initial_count == 0)
+class TestDBStorageMethod(unittest.TestCase):
+    """Test the DBStorage class methods"""
+    @classmethod
+    def setUpClass(cls):
+        """Creating the objects to use"""
+        cls.object_state = State(name="California")
+        cls.object_city = City(state_id=cls.object_state.id,
+                               name="Los Angeles")
+        cls.object_state.save()
+        cls.object_city.save()
 
-            new_state = State(name="Antioquia")
-            storage.reload()
-            new_state.save()
-            storage.save()
-
-            final_count = len(storage.all(State))
-            final_db = cursor_objects.execute(
-                """SELECT COUNT(id) FROM states;""")
-
-            self.assertEqual(final_count, final_db)
-            self.assertEqual(initial_count + 1, final_db)
+    @classmethod
+    def tearDownClass(cls):
+        """ Remove objects in storage at end of each tests """
+        models.storage.delete(cls.object_state)
+        models.storage.delete(cls.object_city)
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
-            """ Confirm __objects is a dict """
-            first_place_id = list(storage.all(Place).values())[0].id
-            expected_answer = models.storage.get(State, first_state_id)
-            self.assertEqual(expected_answer.__class__.__name__, "State")
+        """ test the get method for an existing object of class cls  """
+        first_state_id = list(models.storage.all(State).values())[0].id
+        expected_answer = models.storage.get(State, first_state_id)
+        self.assertEqual(expected_answer.__class__.__name__, "State")
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_count(self):
-            """ Confirm __objects is a dict """
-            self.assertEqual(type(storage.count()), int)
+    def test_count_all(self):
+        """ test the count method for all existing objects in the DB """
+        expected_answer = models.storage.count()
+        self.assertEqual(expected_answer, 2)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_cls(self):
+        """ test the count method for an existing object of class cls  """
+        expected_answer = models.storage.count(State)
+        self.assertEqual(expected_answer, 1)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_None_cls(self):
+        """ test the count method for a none existing object of class cls """
+        expected_answer = models.storage.count(Review)
+        self.assertEqual(expected_answer, 0)
