41230f9028ff8efc9f153ea2936bc83059511522
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-09 14:50:35 -0500

    fix tasks in the project

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-views
-"""
+"""This module is responsible for creating the necessary blueprint."""
 from flask import Blueprint
 
+app_views = Blueprint("app_views", __name__)
 
-app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
