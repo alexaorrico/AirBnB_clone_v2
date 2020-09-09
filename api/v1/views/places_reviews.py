f246dba504fe2d8c4a187c1d3a5e82195778a8c7
bryanbuiles <1482@holbertonschool.com>
2020-09-09 18:45:44 -0500

    task review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,7 +1,7 @@
 
 #!/usr/bin/python3
 """
-places
+Rest api for Reviews
 """
 from models.base_model import BaseModel, Base
 from flask import jsonify, abort, request
@@ -10,17 +10,18 @@ from models.city import City
 from models.place import Place
 from models.user import User
 from models import storage
+from models.review import Review
 from api.v1.views import app_views
 
 
-@app_views.route('cities/<city_id>/places', methods=['GET'], strict_slashes=False)
-def all_places_get(city_id=None):
-    """ Show all places objects """
+@app_views.route('/places/<place_id>/reviews', methods=['GET'], strict_slashes=False)
+def all_review(place_id=None):
+    """ Show all review objects """
     lista = []
     flag = 0
-    for v in storage.all(City).values():
-        if v.id == city_id:
-            for place in v.places:
+    for v in storage.all(Place).values():
+        if v.id == place_id:
+            for place in v.reviews:
                 lista.append(place.to_dict())
             flag = 1
     if flag == 0:
@@ -29,12 +30,12 @@ def all_places_get(city_id=None):
         return (jsonify(lista))
 
 
-@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
-def all_places(place_id=None):
-    """ Show one place object """
+@app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
+def onereview(review_id=None):
+    """ Show the review object """
     flag = 0
-    for v in storage.all(Place).values():
-        if v.id == place_id:
+    for v in storage.all(Review).values():
+        if v.id == review_id:
             attr = (v.to_dict())
             flag = 1
     if flag == 0:
@@ -43,15 +44,13 @@ def all_places(place_id=None):
         return (jsonify(attr))
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
-def delete_places(place_id=None):
-    """ delete a place """
-    if place_id is None:
-        abort(404)
+@app_views.route('/reviews/<review_id>', methods=['DELETE'], strict_slashes=False)
+def delete_reviews(review_id=None):
+    """ delete """
     dicti = {}
     flag = 0
-    for v in storage.all(Place).values():
-        if v.id == place_id:
+    for v in storage.all(Review).values():
+        if v.id == review_id:
             storage.delete(v)
             storage.save()
             flag = 1
@@ -61,27 +60,27 @@ def delete_places(place_id=None):
         return (jsonify(dicti), 200)
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
-def create_places(city_id=None):
-    """ Create a object place """
+@app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
+def create_reviews(place_id=None):
+    """ Post and create object """
     if not request.json:
         abort(400, "Not a JSON")
-    if not 'name' in request.json:
-        abort(400, "Missing name")
+    if not 'text' in request.json:
+        abort(400, "Missing text")
     if not 'user_id' in request.json:
         abort(400, "Missing user_id")
     result = request.get_json()
-    obj = Place()
+    obj = Review()
     flag = 0
-    for v in storage.all(City).values():
-        if v.id == city_id:
+    for v in storage.all(Place).values():
+        if v.id == place_id:
             for user in storage.all(User).values():
                 if result["user_id"] == user.id:
                     for k, values in result.items():
                         flag += 1
                         setattr(obj, k, values)
                         if flag == 1:
-                            setattr(obj, "city_id", city_id)
+                            setattr(obj, "place_id", place_id)
                             flag += 1
                     storage.new(obj)
                     storage.save()
@@ -92,16 +91,20 @@ def create_places(city_id=None):
         return (jsonify(var), 201)
 
 
-@app_views.route('places/<place_id>', methods=['PUT'], strict_slashes=False)
-def change_places(place_id=None):
-    """ change a atribute of place """
+@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
+def change_Reviews(review_id=None):
+    """ change an class atribute """
+    lista = ["user_id", "id", "place_id", "created_at", "updated_at"]
     if not request.json:
         abort(400, "Not a JSON")
+    for security in lista:
+        if security in request.json:
+            abort(400)
 
     result = request.get_json()
     flag = 0
-    for values in storage.all(Place).values():
-        if values.id == place_id:
+    for values in storage.all(Review).values():
+        if values.id == review_id:
             for k, v in result.items():
                 setattr(values, k, v)
                 storage.save()
