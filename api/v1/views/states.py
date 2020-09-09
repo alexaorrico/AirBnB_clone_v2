860a5703b69d5b5b1f5dc1a21deacd3fb2a459db
Fabian Andres <f4bian.andres@gmail.com>
2020-09-09 22:35:59 +0000

    [feature] update error mesage for restful API for State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -29,10 +29,10 @@ def get_states(state_id):
 def create_state():
     """ Create a new State object """
     if not request.json:
-        return make_response(jsonify({"message": "Not a JSON"}), 400)
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
 
     if not 'name' in request.json:
-        return make_response(jsonify({"message": "Missing name"}), 400)
+        return make_response(jsonify({"error": "Missing name"}), 400)
 
     data = request.get_json()
     new_state = State(**data)
@@ -45,7 +45,7 @@ def create_state():
 def update_state(state_id):
     """ Update a State object """
     if not request.json:
-        return make_response(jsonify({"message": "Not a JSON"}), 400)
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
     try:
         state_obj = storage.get(State, state_id)
         data = request.get_json()
