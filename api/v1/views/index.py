73a95c54f5a56c266fe9e34c3c3f2ef31b59b15c
llanojs <juanllano93@gmail.com>
2020-09-09 10:29:31 -0500

    update index.py added the endpoint to get the number of each objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,33 @@
 """  views index module """
 
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenity": Amenity,
+           "city": City,
+           "place": Place,
+           "review": Review,
+           "state": State,
+           "user": User}
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def api_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def api_stats():
+    dict_stats = {}
+    for key, value in classes.items():
+        count = storage.count(value)
+        dict_stats.update({key: count})
+    return jsonify(dict_stats)
