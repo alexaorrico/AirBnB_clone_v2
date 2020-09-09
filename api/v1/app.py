132d387f5e4309b836b5c6e5d1f9b517d97bc174
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-09 13:44:47 -0500

    fix functions

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-"""This module is responsible for the execution and management of the app."""
-from flask import Flask, jsonify
+'''Basic Flask Application'''
+from flask import Flask, Blueprint, make_response, jsonify
+from os import getenv
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
-def teardown_storage(self):
-    """Close session."""
+def teardown(self):
+    """
+    Closes session
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(self):
-    """Handle 404 errors.
-
-    Returns:
-        dict: Returns a JSON-formatted 404 status code response.
-
+def errorhandler(error):
     """
-    return jsonify({"error": "Not found"}), 404
+    Error handling for 404
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    h = getenv("HBNB_API_HOST", "0.0.0.0")
-    p = getenv("HBNB_API_PORT", 5000)
-    app.run(debug=True, host=h, port=p, threaded=True)
+    env_var = {'host': '0.0.0.0', 'port': 5000}
+    if getenv('HBNB_API_HOST'):
+        env_var['host'] = getenv('HBNB_API_HOST')
+    if getenv('HBNB_API_PORT'):
+        env_var['port'] = int(getenv('HBNB_API_PORT'))
+    app.run(host=env_var.get('host'), port=env_var.get('port'), threaded=True)
