e816514cd735d6f00594b1ead1a5230f445d921c
cbarros7 <cj.barros07@gmail.com>
2020-09-09 01:38:24 +0000

    Rest API task 3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,25 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class name and its ID, or None if not found"""
+        if cls in classes.values():
+            obj = self.all(cls)
+            for value in obj.values():
+                if value.id == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects
+        in storage matching the given class name.
+        If no name is passed, returns the count of all objects in storage.
+        """
+        count = 0
+        if cls in classes.values():
+            count = len(self.all(cls))
+        elif cls is None:
+            count = len(self.__objects)
+        return count
