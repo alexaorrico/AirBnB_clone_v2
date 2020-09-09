ebfbc543047d863334a25aef473bce0b9b8d44a4
FatChicken277 <1637@holbertonschool.com>
2020-09-09 15:10:16 -0500

    Added new two methods to the storage files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,13 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Retrieve one objet."""
+        return self.__objects.get("{}.{}".format(cls.__name__, id))
+
+    def count(self, cls=None):
+        """Count the number of objects in storage."""
+        if cls is not None:
+            return len(self.all(cls))
+        return len(self.__objects)
