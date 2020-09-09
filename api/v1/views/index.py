240860340161215a3f7d6b620fe0ee6d1ba608a8
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-09 12:12:52 -0500

    add task5 and __init__.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """New Funtion index"""
 from api.v1.views import app_views
 from flask import jsonify
+from models.states import States
+from models.city import City
+from models.amenities import Amenity
+from models.places import Places
+from modes.review import Review
+from models.users import Users
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def reoute_status():
@@ -12,3 +19,21 @@ def reoute_status():
     return jsonify({
         "status": "OK"
     }), 200
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def reoute_count():
+    """objects by type """
+    reoute_count = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Places'),
+        'review': storage.count('Review'),
+        'states': storage.count('States'),
+        'users': storage.count('Users')
+    }
+    return jsonify(reoute_count)
+
+
+if __name__ == '__main__':
+    pass
