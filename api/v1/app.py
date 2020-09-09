f21d7f3578e1fd21af9f0f0a80d0a323b61654ea
angellovc <vc.angel@outlook.com>
2020-09-09 23:00:21 +0000

    New file:   api/v1/app.py
    flask app was added to create an api

---
+++ api/v1/app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def error_404(e):
+    """ handler for 404 error
+    return a machine friendly response
+    """
+    return {"error": "Not found"}, 404
+
+
+@app.errorhandler(400)
+def error_400(e):
+    """ handler for 400 error
+    return a machine friendly response
+    """
+    return {"error": "{}".format(e.description)}, 400
+
+
+@app.teardown_appcontext
+def storage_close(error):
+    """ Close the database when app fails
+    avoiding to save any change potencially
+    harmful
+    """
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
