631588096ca6763b025ad1c3836226b39e626019
Ang3l1t0 <1546@holbertonschool.com>
2020-09-10 13:09:42 -0500

    new view for State objects

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Registering Blueprints"""
+from api.v1.views.states import *
+from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
