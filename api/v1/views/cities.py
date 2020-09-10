f3ee33feaa63f86e2f9dda9eff6dd301262c1d24
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-10 13:30:11 +0000

    mthod updated_city added

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -22,7 +22,7 @@ def all_cities(state_id):
 
 @app_views.route('/cities/city_id>',
                  methods=['GETT'], strict_slashes=False)
-def cities(city_id):
+def get_city(city_id):
     """ Retrieves a city linked with city_id. """
     city = storage.get(City, city_id)
     if city:
@@ -30,20 +30,21 @@ def cities(city_id):
     abort(404)
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
-    """Update a current state"""
-    state_obj = storage.get(State, state_id)
-    if state_obj:
+@app_views.route('/cities/<city_id>',
+                 methods=['PUT'], strict_slashes=False)
+def update_state(city_id):
+    """Update a current City"""
+    city_obj = storage.get(City, city_id)
+    if city_obj:
         body_dic = request.get_json()
         if not body_dic:
             return jsonify({'error': 'Not a JSON'}), 400
         for key, value in body_dic.items():
-            ignore_keys = ['id', 'created_at']
+            ignore_keys = ['id', 'state_id', 'created_at']
             if key not in ignore_keys:
-                setattr(state_obj, key, value)
-        state_obj.save()
-        return jsonify(state_obj.to_dict()), 200
+                setattr(city_obj, key, value)
+        city_obj.save()
+        return jsonify(city_obj.to_dict()), 200
     else:
         abort(404)
 
