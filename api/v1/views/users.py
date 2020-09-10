ac67f4d8a46b82aac1f32f3a8ebc2d693768cd07
R6Doc <fiendgear@gmail.com>
2020-09-10 16:27:42 -0500

    added users fix on import

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Users"""
+"""Users !!"""
 
 from flask import jsonify, request, abort, make_response
 from models import storage
-from models.user import User
 from api.v1.views import app_views
+from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
