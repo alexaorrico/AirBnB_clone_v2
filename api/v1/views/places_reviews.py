80aef3ba2918ff71e6842d71d8636909d84ddea0
duvanjm <duvanjarin@gmail.com>
2020-09-10 20:32:57 +0000

    fix place function

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,6 +4,7 @@
 from flask import jsonify, request, abort
 from models.place import Place
 from models.review import Review
+from models.user import User
 from models import storage
 from api.v1.views import app_views
 
@@ -28,20 +29,19 @@ def reviews_place(place_id):
             abort(400, "Not a JSON")
         if "user_id" not in response:
             abort(400, "Missing user_id")
-        user = storage.get("User", response["user_id"])
         if user is None:
             abort(404)
         if "text" not in response:
             abort(400, "Missing text")
-        response["city_id"] = city_id
         new_review = Review(**response)
+        new_review.place_id = place_id
         new_review.save()
         return jsonify(new_review.to_dict()), 201
 
 
 @app_views.route('/reviews/<review_id>', methods=["GET", "DELETE", "PUT"],
                  strict_slashes=False)
-def place(place_id):
+def place_review(place_id):
     """ Manipulate an specific Place """
     review = storage.get(Review, review_id)
     if review is None:
