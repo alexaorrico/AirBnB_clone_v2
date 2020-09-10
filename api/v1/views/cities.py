661716e5fcb7d32d3b6fc9a6f7791fea3c341319
jchois <choisjuliana@gmail.com>
2020-09-10 15:30:30 -0500

    Adding methods for return Cities by state_id/id

---
+++ api/v1/views/cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""View for City objects that handles all default RestFul API"""
+from api.v1.views import app_views
+from models.state import State
+from models.city import City
+from models import storage
+from flask import jsonify, abort, request, make_response
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_cities(state_id):
+    """List cities per state"""
+    state_obj = storage.get('State', state_id)
+    if state_obj is None:
+        abort(404)
+    all_cities = storage.all("City").values()
+    st_cities = [c.to_dict() for c in all_cities if c.state_id == state_id]
+    return jsonify(st_cities)
+
+@app_views.route('/cities/<city_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_city(city_id):
+    """Retrieves a City object"""
+    city = storage.get('City', city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
