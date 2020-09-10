23f05804f359ac939a1784737762e2def9736c85
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-09 21:53:47 -0500

    new import city

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,10 @@
 """This module is responsible for creating the necessary blueprint."""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-if app_views:
+if app_views is not None:
     from api.v1.views.index import *
     from api.v1.views.states import *
-
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
