f01fb7eeac8aa2f595ce09c213bf242d876b17fa
jchois <choisjuliana@gmail.com>
2020-09-10 16:42:39 -0500

    Adding method for delete a user obj by id

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -23,3 +23,16 @@ def get_user_id(user_id):
     if not user_obj:
         abort(404)
     return (jsonify(user_obj.to_dict()))
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_user(user_id):
+    """Delete users by id"""
+    user_obj = storage.get(User, user_id)
+    if not user_obj:
+        abort(404)
+    else:
+        user_obj.delete()
+        storage.save()
+        return make_response(jsonify({}), 200)
