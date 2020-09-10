08971a54a5e00a414af347fdf29d246894a9054e
sarias12 <1638@holbertonschool.com>
2020-09-10 17:47:18 +0000

    [Feature] Task 13 - Adding HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
