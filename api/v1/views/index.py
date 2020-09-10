4af9bb3f5c1c22595422f24068ab272751cd1063
R6Doc <fiendgear@gmail.com>
2020-09-10 17:31:40 -0500

    added imports

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify, Blueprint
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 abnbText = {
     "amenities": "Amenity",
