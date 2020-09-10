776fe330f2d8748fa85d873f221aa7da52c54641
julianfrancor <julianfrancor@gmail.com>
2020-09-09 20:52:46 -0500

    fixing task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -32,5 +32,5 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     HBNB_API_HOST = getenv("HBNB_API_HOST", '0.0.0.0')
-    HBNB_API_PORT = getenv("HBNB_API_PORT ", '5000')
+    HBNB_API_PORT = getenv("HBNB_API_PORT", 5000)
     app.run(debug=True, host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
