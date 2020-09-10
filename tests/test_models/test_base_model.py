f34286b299b25de14c9b6b6c2dd1a8e785308402
julianfrancor <julianfrancor@gmail.com>
2020-09-09 20:08:30 -0500

    fixing test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,6 +13,7 @@ module_doc = models.base_model.__doc__
 
 class TestBaseModelDocs(unittest.TestCase):
     """Tests to check the documentation and style of BaseModel class"""
+
     @classmethod
     def setUpClass(self):
         """Set up for docstring tests"""
@@ -57,7 +58,6 @@ class TestBaseModelDocs(unittest.TestCase):
 
 class TestBaseModel(unittest.TestCase):
     """Test the BaseModel class"""
-
     def test_instantiation(self):
         """Test that object is correctly created"""
         inst = BaseModel()
@@ -78,23 +78,23 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(inst.name, "Holberton")
         self.assertEqual(inst.number, 89)
 
-    # def test_datetime_attributes(self):
-    #     """Test that two BaseModel instances have different datetime objects
-    #     and that upon creation have identical updated_at and created_at
-    #     value."""
-    #     tic = datetime.now()
-    #     inst1 = BaseModel()
-    #     toc = datetime.now()
-    #     # self.assertTrue(tic <= inst1.created_at <= toc)
-    #     time.sleep(1e-4)
-    #     tic = datetime.now()
-    #     inst2 = BaseModel()
-    #     toc = datetime.now()
-    #     # self.assertTrue(tic <= inst2.created_at <= toc)
-    #     self.assertEqual(inst1.created_at, inst1.updated_at)
-    #     self.assertEqual(inst2.created_at, inst2.updated_at)
-    #     self.assertNotEqual(inst1.created_at, inst2.created_at)
-    #     self.assertNotEqual(inst1.updated_at, inst2.updated_at)
+    def test_datetime_attributes(self):
+        """Test that two BaseModel instances have different datetime objects
+        and that upon creation have identical updated_at and created_at
+        value."""
+        tic = datetime.now()
+        inst1 = BaseModel()
+        toc = datetime.now()
+        self.assertTrue(tic <= inst1.created_at <= toc)
+        time.sleep(1e-4)
+        tic = datetime.now()
+        inst2 = BaseModel()
+        toc = datetime.now()
+        self.assertTrue(tic <= inst2.created_at <= toc)
+        self.assertEqual(inst1.created_at, inst1.updated_at)
+        self.assertEqual(inst2.created_at, inst2.updated_at)
+        self.assertNotEqual(inst1.created_at, inst2.created_at)
+        self.assertNotEqual(inst1.updated_at, inst2.updated_at)
 
     def test_uuid(self):
         """Test that id is a valid uuid"""
