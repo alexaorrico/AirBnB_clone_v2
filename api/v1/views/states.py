beed7782678fa17cb856ac6c14986e6aa6be8ad5
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-09 21:39:28 -0500

    Add make_reponse method to 200 and 201 status codes.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -19,13 +19,13 @@ def states_list():
     # method is POST
     body_dic = request.get_json()
     if "name" not in body_dic:
-        return make_response(jsonify({"error": "Missing name"}), 400)
+        return jsonify({"error": "Missing name"}), 400
     if body_dic is None:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
+        return jsonify({"error": "Not a JSON"}), 400
     new_state = State(**body_dic)
     storage.new(new_state)
     storage.save()
-    return jsonify(new_state.to_dict()), 201
+    return make_response(jsonify(new_state.to_dict()), 201)
 
 
 @app_views.route("/states/<state_id>",
@@ -40,15 +40,15 @@ def states_id(state_id):
         if request.method == "DELETE":
             storage.delete(state_obj)
             storage.save()
-            return jsonify({}), 200
+            return make_response(jsonify({}), 200)
         else:  # method = PUT
             body_dic = request.get_json()
             if body_dic is None:
-                return make_response(jsonify({"error": "Not a JSON"}), 400)
+                return jsonify({"error": "Not a JSON"}), 400
             for key, value in body_dic.items():
                 ignore_keys = ["id", "created_at", "updated_at"]
                 if key not in ignore_keys:
                     setattr(state_obj, key, value)
             storage.save()
-            return jsonify(state_obj.to_dict()), 200
+            return make_response(jsonify(state_obj.to_dict()), 200)
     abort(404)  # when the id is not linked with any state
