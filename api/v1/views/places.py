38627de94848e551eef249adeb267c15a0c4e935
jchois <choisjuliana@gmail.com>
2020-09-10 17:42:27 -0500

    Import User in places view file

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from models.place import Place
 from models.city import City
+from models.user import User
 from models import storage
 from flask import jsonify, abort, request, make_response
 
