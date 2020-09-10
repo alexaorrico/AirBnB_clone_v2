6d0d11cb617ebeaa73364497017fed96eefc6982
s-macias <1552@holbertonschool.com>
2020-09-10 22:15:08 +0000

    fixed typo in __init__py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,8 +13,8 @@ from api.v1.views.cities import *
 
 from api.v1.views.amenities import *
 
-from api.vi.views.users import *
+from api.v1.views.users import *
 
-from api.vi.views.places import *
+from api.v1.views.places import *
 
-from api.vi.views.reviews import *
+from api.v1.views.reviews import *
