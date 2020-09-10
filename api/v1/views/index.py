8cef4cd30ab05e08075e673a6719771d98618f14
dondropo <alejandroruscamoreno@gmail.com>
2020-09-10 16:41:15 +0000

    [Add files]: Task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,17 @@ from api.v1.views import app_views
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"}), 200
+
+
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
+def stats():
+    """send json with the count of objects"""
+    counts = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(counts), 200
