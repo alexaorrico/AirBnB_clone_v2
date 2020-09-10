ca2c2780d32210338f633cf0e1a88eac592457f1
jchois <choisjuliana@gmail.com>
2020-09-10 12:58:20 -0500

    Endpoint that retrieves the number of each objects by type added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,28 @@
 """Index file"""
 
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Return status"""
     return ({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Function that return stats"""
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    new_json = {}
+    for key, value in classes.items():
+        new_json[key] = storage.count(value)
+    return jsonify(new_json)
