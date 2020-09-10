65c71b8e783028c4f2a04f80a9b9ee68dfa14f08
Felipe Layos <jfelipelayos@gmail.com>
2020-09-10 17:12:31 +0000

    Fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+
 @app.teardown_appcontext
 def teardown(self):
     """calls storage.close()"""
