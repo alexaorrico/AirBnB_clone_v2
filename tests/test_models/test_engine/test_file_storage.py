c02430ee7a3dd92d24e6c4a2b056e3e188826964
mahdibz97 <ben.zouitina.mahdi97@gmail.com>
2020-09-10 19:32:28 +0100

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,11 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test count"""
+        a = State()
+        models.storage.new(a)
+        models.storage.save()
+        self.assertEqual(models.storage.get(State, a.id), a)
