26300c631a4c8a0dd43a4d87b64151f59745c3f1
Ang3l1t0 <1546@holbertonschool.com>
2020-09-10 13:18:00 -0500

    IMPORT ORDER

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Registering Blueprints"""
-from api.v1.views.states import *
-from api.v1.views.index import *
+"""Initializate views and create blueprint"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
