7f5f3e9a332c305e7c2e532d18379170feecb942
Tony Sims <zocle@zocle.net>
2020-09-10 17:11:02 -0500

    added CORS per requirements in task

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,18 +3,21 @@
 Flask api app file
 """
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown(self):
     """ close session """
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True)
