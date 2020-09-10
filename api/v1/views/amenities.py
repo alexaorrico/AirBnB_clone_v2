bcd9980862753316c65ff09961fa3e092d9aa1c7
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-10 14:57:28 +0000

    all methods updated to use with Amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -2,7 +2,7 @@
 """ Restful API for State objects. """
 from flask import jsonify, request, abort
 from api.v1.views import app_views
-from models.state import State
+from models.state import Amenities
 from models.city import City
 from models import storage
 
@@ -41,40 +41,36 @@ def delete_city(amenity_id):
         abort(404)
 
 
-@app_views.route('/cities/<city_id>',
-                 methods=['PUT'], strict_slashes=False)
-def update_city(city_id):
-    """Update a current City"""
-    city_obj = storage.get(City, city_id)
-    if city_obj:
-        body_dic = request.get_json()
-        if not body_dic:
-            return jsonify({'error': 'Not a JSON'}), 400
-        for key, value in body_dic.items():
-            ignore_keys = ['id', 'state_id', 'created_at']
-            if key not in ignore_keys:
-                setattr(city_obj, key, value)
-        city_obj.save()
-        return jsonify(city_obj.to_dict()), 200
-    else:
-        abort(404)
-
-
-
-
-@app_views.route('/states/<state_id>/cities',
+@app_views.route('/amenities/<amenity_id>',
                  methods=['POST'], strict_slashes=False)
-def new_city(state_id):
-    """Create a new City object. """
+def new_city(amenity_id):
+    """Create a new Amenity object. """
     body_dic = request.get_json()
-    state_obj = storage.get(State, state_id)
-    if not state_obj:
-        abort(404)
     if not body_dic:
         return jsonify({'error': 'Not a JSON'}), 400
     if "name" not in body_dic:
         return jsonify({'error': 'Missing name'}), 400
 
-    new_city = City(**body_dic)
+    new_amenity = Amenity(**body_dic)
     storage.save()
-    return jsonify(new_city.to_dict()), 201
+    return jsonify(new_amenity.to_dict()), 201
+
+
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
+def update_city(amenity_id):
+    """Update a current Amenity"""
+    amenity_obj = storage.get(Amenity, city_id)
+    if amenity_obj:
+        body_dic = request.get_json()
+        if not body_dic:
+            return jsonify({'error': 'Not a JSON'}), 400
+        for key, value in body_dic.items():
+            ignore_keys = ['id', 'created_at']
+            if key not in ignore_keys:
+                setattr(amenity_obj, key, value)
+        amenity_obj.save()
+        return jsonify(amenity_obj.to_dict()), 200
+    else:
+        abort(404)
