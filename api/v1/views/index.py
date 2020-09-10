24381c15059ae20e0c2bd20c901fc84a852d5603
michedomingo <michedomingo@gmail.com>
2020-09-10 01:07:10 +0000

    Task5: Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,5 +8,15 @@ from api.v1.views import app_views
 
 @app_views.route('/api/v1/status', strict_slashes=False)
 def return_status():
-    """ returns a JSON """
+    """returns a JSON string"""
     return {"status": "OK"}
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def count_objects():
+    """retrieves the number of each object by type"""
+    return {"amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')}
