e75424e33a34080728a1f72654c31553eba2b187
jlassi1 <khawlajlassi1990@gmail.com>
2020-09-10 19:59:26 +0100

    task3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -73,11 +73,11 @@ class FileStorage:
         """A method to retrieve one object"""
         from models import storage
 
+        if cls is None or id is None:
+            return None
         for obj in storage.all(cls).values():
             if obj.id == id:
                 return obj
-        if cls is None or id is None:
-            return None
         return None
 
     def count(self, cls=None):
