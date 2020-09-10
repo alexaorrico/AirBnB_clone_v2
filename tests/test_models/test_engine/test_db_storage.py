6bbe1f193bee399d4eb130fb50885608df88aa67
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-10 22:09:23 +0000

    Documentation message changed

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -97,7 +97,7 @@ class TestFileStorage(unittest.TestCase):
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
-        """Test for count method"""
+        """ Test for count method """
         count_1 = models.storage.count()
         self.obj = State(name="California")
         self.obj.save()
