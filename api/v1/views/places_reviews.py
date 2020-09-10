aff91c6ec1936ce5a726d2b4755c94dbd5594775
Nic Basilio <bnbasilio@gmail.com>
2020-09-10 07:58:31 +0000

    fixed pep8 errors

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -83,8 +83,9 @@ def update_review(review_id):
     if not upd_review:
         abort(400, {'Not a JSON'})
     this_review = storage.get(review, review_id)
+    ignore = ['id', 'user_id', 'place_id', 'created_at', 'updated_at']
     for key, value in upd_place.items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at', 'updated_at']:
+        if key not in ignore:
             setattr(this_review, key, value)
     storage.save()
     return jsonify(this_review.to_dict()), 200
