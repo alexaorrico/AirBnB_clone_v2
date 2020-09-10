91749d560edb4c7861c703f2e51eec90f7b5d997
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-09-09 21:55:47 -0500

    Add Task 6 Not Found--- RESTfull API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """API Module"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ def teardown(self):
     """Method Handle"""
     storage.close()
 
+@app.errorhandler(404)
+def error_404(error):
+    """Error 404"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
-    app.run(getenv('HBNB_API_HOST'), getenv('HBNB_API_PORT'), threaded=True)
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True, debug=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,33 @@
 #!/usr/bin/python3
 """Index Module"""
 
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from api.v1.views import app_views
+from flask import Flask, jsonify
 
+from models import storage
 
 @app_views.route('/status')
 def status():
     """Return status OK"""
     return jsonify({"status": "OK"}), 200
+
+
+@app_views.route('/stats')
+def endpoint():
+    """Endpoint that retrieves the number of each object"""
+    count = {
+        "amenities": storage.count(State),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(count), 200
