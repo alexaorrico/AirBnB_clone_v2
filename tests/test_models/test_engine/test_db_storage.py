36c649e830b0cf9803972d3d7444bc82d774cec9
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-09-10 21:03:34 -0500

    Creating and updating new tests for the created file

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -68,7 +68,7 @@ test_db_storage.py'])
                             "{:s} method needs a docstring".format(func[0]))
 
 
-class TestDBStorage(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_returns_dict(self):
@@ -78,33 +78,58 @@ class TestDBStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_no_class(self):
         """Test that all returns all rows when no class is passed"""
+        pass
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_new(self):
         """test that new adds an object to the database"""
+        pass
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+        pass
+
+
+class TestDBStorageMethod(unittest.TestCase):
+    """Test the DBStorage class methods"""
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    @classmethod
+    def setUpClass(cls):
+        """Creating the objects to use"""
+        cls.object_state = State(name="California")
+        cls.object_city = City(state_id=cls.object_state.id,
+                               name="Los Angeles")
+        cls.object_state.save()
+        cls.object_city.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """ Remove objects in storage at end of each tests """
+        models.storage.delete(cls.object_state)
+        models.storage.delete(cls.object_city)
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
-        """test get method"""
-        obj = list(models.storage.all().values())[0]
-        self.assertEqual(obj, models.storage.get(obj.__class__, obj.id))
-
-        state = State(name="Bogota on fire more than medellin lol")
-        state.save()
-        self.assertEqual(state, models.storage.get(State, state.id))
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_count(self):
-        """test count method"""
-        len_all_states = len(models.storage.all(State))
-        self.assertEqual(len_all_states, models.storage.count(State))
+        """ test the get method for an existing object of class cls  """
+        first_state_id = list(models.storage.all(State).values())[0].id
+        expected_answer = models.storage.get(State, first_state_id)
+        self.assertEqual(expected_answer.__class__.__name__, "State")
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count_all(self):
         """ test the count method for all existing objects in the DB """
-        len_all_states = len(models.storage.all())
-        self.assertEqual(len_all_states, models.storage.count())
+        expected_answer = models.storage.count()
+        self.assertEqual(expected_answer, 2)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_cls(self):
+        """ test the count method for an existing object of class cls  """
+        expected_answer = models.storage.count(State)
+        self.assertEqual(expected_answer, 1)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_None_cls(self):
+        """ test the count method for a none existing object of class cls """
+        expected_answer = models.storage.count(Review)
+        self.assertEqual(expected_answer, 0)
