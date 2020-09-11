d8e3956858296491a08422227f08309b5e9eecb4
Sammy <1449@holbertonschool.com>
2020-09-10 21:22:36 -0500

    change comment

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+start a Flask web application
 """
 
 from flask import Flask
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
