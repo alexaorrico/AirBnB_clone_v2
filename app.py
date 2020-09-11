36f675d1a6d865d0da7788a8bf0470eb97048701
s-macias <1552@holbertonschool.com>
2020-09-11 00:35:46 +0000

    task 13 adding CORS to app.py

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ from models import storage
 from flask import Blueprint
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask_cors import CORS
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
 @app.teardown_appcontext
 def teardown_appcontext(self):
     """ method to handle @app.teardown_appcontext """
     storage.close()
+
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
