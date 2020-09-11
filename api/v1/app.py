6ebe7790bdefb8f5c672587af09c432ff97be264
Valeria Alvarez G <vale.agiraldo@gmail.com>
2020-09-10 22:18:20 -0500

    [Feature] Task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """API Status"""
-from flask import Flask, jsonify
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def CloseSession(self):
+def CloseSession(x=None):
     """Close session"""
     storage.close()
 
@@ -18,8 +20,8 @@ def CloseSession(self):
 @app.errorhandler(404)
 def error_404(error):
     """Error 404 handler"""
-    return jsonify({"error": "Not found"}), 404
+    return jsonify(error="Not found"), 404
 
 if __name__ == "__main__":
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', 5000)), threaded=True)
+            port=getenv('HBNB_API_PORT', 5000), threaded=True)
