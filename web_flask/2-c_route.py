ee47e567dd26795acfc8e8ef7fc990a1be06c0af
Sammy <1449@holbertonschool.com>
2020-09-10 21:19:41 -0500

    change comment

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+start a Flask web application
 """
 
 from flask import Flask
@@ -23,6 +23,7 @@ def hbnb():
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
