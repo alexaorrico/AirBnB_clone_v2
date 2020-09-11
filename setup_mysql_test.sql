638c775325f5e27086ed3c8e1d866c3e6d8583c8
Sammy <1449@holbertonschool.com>
2020-09-10 21:34:19 -0500

    change comment

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
--- prepares a MySQL server for the project
+-- prepares a MySQL server for the projects
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
