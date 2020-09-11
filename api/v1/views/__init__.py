8feeba85bcfcb2bfdadc0cb7d6fa8c234b71bb89
peluza <edisonisaza@gmail.com>
2020-09-11 01:55:05 +0000

    final test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,6 +12,6 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.states import *
 from api.v1.views.index import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+# from api.v1.views.places_reviews import *
+# from api.v1.views.places_amenities import *
 
