e078a202cc48c83a731aded507bf615cf2219d32
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-09-11 00:12:18 +0000

    fixing identation of file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,23 +4,24 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 from models.amenity import Amenity
-from models.city import City 
+from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
 
 classes = {'amenities': Amenity,
-    'cities': City,
-    'places': Place,
-    'reviews': Review,
-    'states': State,
-    'users': User
-}
+           'cities': City,
+           'places': Place,
+           'reviews': Review,
+           'states': State,
+           'users': User}
+
 
 @app_views.route('/status')
 def status():
     return jsonify({'status': 'OK'})
+
 
 @app_views.route('/stats')
 def stats():
