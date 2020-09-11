b31c6bbe557d7b2a3bd550f61008e0f6f97269b0
Immaannn2222 <imennaayari@gmail.com>
2020-09-11 11:51:05 +0100

    clean branch

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,3 +75,15 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        if id is not None or cls is not None:
+            for obj in self.all(cls).values():
+                if obj.id == id:
+                    return obj
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        obj = self.all(cls)
+        return len(obj)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,3 +69,15 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        if id is not None or cls is not None:
+            for obj in self.all(cls).values():
+                if obj.id == id:
+                    return obj
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        obj = self.all(cls)
+        return len(obj)
