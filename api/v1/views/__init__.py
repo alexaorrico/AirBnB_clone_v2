b6fa94d85d4939dd51775360a69fa9bb58baa944
chmlsud <1188@holbertonschool.com>
2020-09-16 20:58:59 +0000

    add views

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" init file of module views """
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__)
+
+if app_views:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.places_reviews import *
