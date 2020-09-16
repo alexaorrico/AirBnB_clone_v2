f20e61d9c20d4721f97c4ac57206ed0a2fe521d3
chmlsud <1188@holbertonschool.com>
2020-09-16 21:09:13 +0000

    Fix code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,17 +9,3 @@ from models import storage
 def status():
     """ Return http status response """
     return jsonify({'status': 'OK'})
-
-
-@app_views.route('/stats')
-def get_stats():
-    """ retive the number of each obj by type """
-    new_dict = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states":  storage.count("State"),
-        "users":  storage.count("User")
-    }
-    return jsonify(new_dict)
