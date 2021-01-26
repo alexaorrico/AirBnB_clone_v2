aab9a477f6781f68a46d4736d1d3588abd3d729b
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-01-26 17:32:35 -0500

    More stats.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-
+Status of our Api and some stats.
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
@@ -12,3 +13,13 @@ def app_status():
     Simply returns the state of the api.
     """
     return(jsonify(status="OK"))
+
+@app_views.route("/api/v1/stats")
+def stats():
+    total = {"amenities": storage.count("Amenity"),
+             "cities": storage.count("City"),
+             "places": storage.count("Place"),
+             "reviews": storage.count("Review"),
+             "states": storage.count("State"),
+             "users": storage.count("User")}
+    return jsonify(total)
