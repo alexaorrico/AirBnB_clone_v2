5d83550103e602ff18ce29b79aebfae05fcd7312
JwowSquared <Jdwagner1@hotmail.com>
2021-01-26 18:34:02 +0000

    cleaned up code, and fixed 1 issue in places

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -13,27 +13,29 @@ from models.city import City
                  methods=["GET", "POST"])
 def cities_base(s_id):
     """this is a test string"""
+    # GET method
     if request.method == "GET":
         out = []
         state = storage.get(State, s_id)
-        if state:
-            for city in state.cities:
-                out.append(city.to_dict())
-            return jsonify(out)
-        abort(404)
+        if not state:
+            abort(404)
+        for city in state.cities:
+            out.append(city.to_dict())
+        return jsonify(out)
+    # POST method
     if request.method == "POST":
         if not request.is_json:
             return "Not a JSON", 400
         state = storage.get(State, s_id)
-        if state:
-            kwargs = {"state_id": s_id}
-            kwargs.update(request.get_json())
-            out = City(**kwargs)
-            if "name" not in out.to_dict().keys():
-                return "Missing name", 400
-            out.save()
-            return out.to_dict(), 201
-        abort(404)
+        if not state:
+            abort(404)
+        kwargs = {"state_id": s_id}
+        kwargs.update(request.get_json())
+        out = City(**kwargs)
+        if "name" not in out.to_dict().keys():
+            return "Missing name", 400
+        out.save()
+        return out.to_dict(), 201
 
 
 @app_views.route("/cities/<c_id>",
@@ -41,26 +43,29 @@ def cities_base(s_id):
                  methods=["GET", "DELETE", "PUT"])
 def cities_id(c_id):
     """this is a test string"""
+    # GET method
     if request.method == "GET":
         city = storage.get(City, c_id)
-        if city:
-            return city.to_dict()
-        abort(404)
+        if not city:
+            abort(404)
+        return city.to_dict()
+    # DELETE method
     if request.method == "DELETE":
         city = storage.get(City, c_id)
-        if city:
-            city.delete()
-            storage.save()
-            return {}, 200
-        abort(404)
+        if not city:
+            abort(404)
+        city.delete()
+        storage.save()
+        return {}, 200
+    # PUT method
     if request.method == "PUT":
         city = storage.get(City, c_id)
-        if city:
-            if not request.is_json:
-                return "Not a JSON", 400
-            for k, v in request.get_json().items():
-                if k not in ["id", "state_id", "created_at", "updated_at"]:
-                    setattr(city, k, v)
-            storage.save()
-            return city.to_dict(), 200
-        abort(404)
+        if not city:
+            abort(404)
+        if not request.is_json:
+            return "Not a JSON", 400
+        for k, v in request.get_json().items():
+            if k not in ["id", "state_id", "created_at", "updated_at"]:
+                setattr(city, k, v)
+        storage.save()
+        return city.to_dict(), 200
--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -13,29 +13,33 @@ from models.place import Place
                  methods=["GET", "POST"])
 def places_base(c_id):
     """this is a test string"""
+    # GET method
     if request.method == "GET":
         out = []
         city = storage.get(City, c_id)
-        if city:
-            for place in city.places:
-                out.append(place.to_dict())
-            return jsonify(out)
-        abort(404)
+        if not city:
+            abort(404)
+        for place in city.places:
+            out.append(place.to_dict())
+        return jsonify(out)
+    # POST method
     if request.method == "POST":
         if not request.is_json:
             return "Not a JSON", 400
         city = storage.get(City, c_id)
-        if city:
-            kwargs = {"city_id": c_id}
-            kwargs.update(request.get_json())
-            out = Place(**kwargs)
-            if "user_id" not in out.to_dict().keys():
-                return "Missing user_id", 400
-            if "name" not in out.to_dict().keys():
-                return "Missing name", 400
-            out.save()
-            return out.to_dict(), 201
-        abort(404)
+        if not city:
+            abort(404)
+        kwargs = {"city_id": c_id}
+        kwargs.update(request.get_json())
+        out = Place(**kwargs)
+        if "user_id" not in out.to_dict().keys():
+            return "Missing user_id", 400
+        if not storage.get(User, out.to_dict()["user_id"]):
+            abort(404)
+        if "name" not in out.to_dict().keys():
+            return "Missing name", 400
+        out.save()
+        return out.to_dict(), 201
 
 
 @app_views.route("/places/<p_id>",
@@ -43,27 +47,30 @@ def places_base(c_id):
                  methods=["GET", "DELETE", "PUT"])
 def places_id(p_id):
     """this is a test string"""
+    # GET method
     if request.method == "GET":
         place = storage.get(Place, p_id)
-        if place:
-            return place.to_dict()
-        abort(404)
+        if not place:
+            abort(404)
+        return place.to_dict()
+    # DELETE method
     if request.method == "DELETE":
         place = storage.get(Place, p_id)
-        if place:
-            place.delete()
-            storage.save()
-            return {}, 200
-        abort(404)
+        if not place:
+            abort(404)
+        place.delete()
+        storage.save()
+        return {}, 200
+    # PUT method
     if request.method == "PUT":
         place = storage.get(Place, p_id)
-        if place:
-            if not request.is_json:
-                return "Not a JSON", 400
-            for k, v in request.get_json().items():
-                if k not in ["id", "user_id", "city_id",
-                             "created_at", "updated_at"]:
-                    setattr(place, k, v)
-            storage.save()
-            return place.to_dict(), 200
-        abort(404)
+        if not place:
+            abort(404)
+        if not request.is_json:
+            return "Not a JSON", 400
+        for k, v in request.get_json().items():
+            if k not in ["id", "user_id", "city_id",
+                         "created_at", "updated_at"]:
+                setattr(place, k, v)
+        storage.save()
+        return place.to_dict(), 200
