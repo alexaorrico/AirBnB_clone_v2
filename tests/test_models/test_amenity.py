ab8a56b687fd9e15ae4110ae5dd5bb6bc28b9766
Deyber2000 <garzondeiber6@gmail.com>
2021-01-26 18:54:00 -0500

    fix amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the TestAmenityDocs classes
+Contains the TestAmenityDocs Classes
 """
 
 from datetime import datetime
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Test BaseModel for expected behavior and documentation"""
+"""Test BaseModel for expected behavior and Documentation"""
 from datetime import datetime
 import inspect
 import models
