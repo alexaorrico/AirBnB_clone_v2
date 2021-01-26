8aca4922d561f3d43b77d42632542889d52b1f47
thiago1623 <thiago.96t16@gmail.com>
2021-01-25 22:21:12 -0500

    [UPDATE]--FILES--(add new changes)

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""modulo to create API  with Flask"""
+from flask import Flask
+from models import storage
+from models.engine import *
+from api.v1.views import app_views
+from os import getenv
+
+
+"""instance of Flask"""
+app = Flask(__name__)
+
+"""register the blueprint"""
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_storage(error):
+    """
+    Remove the database, exit and save file
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True, debug=True)
