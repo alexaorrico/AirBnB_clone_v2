26247a2cc6e8638b019ac7262ad8032393f5ece1
thiago1623 <thiago.96t16@gmail.com>
2021-01-25 23:54:52 -0500

    [UPDATE]--FILES--(add new changes)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """modulo to create API  with Flask"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from models.engine import *
 from api.v1.views import app_views
@@ -21,6 +21,12 @@ def teardown_storage(error):
     """
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    handler error 404
+    """
+    return (jsonify(error="Not found"), 404)
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", "0.0.0.0")
