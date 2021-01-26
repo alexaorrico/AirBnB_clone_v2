1ca8a5cbd4213f25c886a414c872775d5c1fbc18
JwowSquared <Jdwagner1@hotmail.com>
2021-01-26 03:56:43 +0000

    added technically functioning template to use for tasks 7-12

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,12 @@
 
 from flask import Blueprint
 
-# no idea if this is actually right
-app_views = Blueprint("app_views", __name__, url_prefix="/ap1/v1")
+app_views = Blueprint("app_views", __name__)
 
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.reviews import *
---
+++ api/v1/views/users.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from flask import request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+@app_views.route("/users", strict_slashes=False, methods=["GET", "POST"])
+def users_base():
+    """x"""
+    if request.method == "GET":
+        return {"GET": "Not implemented"}
+    if request.method == "POST":
+        return {"POST": "Not implemented"}
+
+
+@app_views.route("/users/<u_id>", strict_slashes=False, methods=["GET", "DELETE", "PUT"])
+def users_id(u_id):
+    """x"""
+    if request.method == "GET":
+        return {"GET": "Not implemented"}
+    if request.method == "DELETE":
+        return {"DELETE": "Not implemented"}
+    if request.method == "PUT":
+        return {"PUT": "Not implemented"}
