84a03378ee6da70be3fdacd13d0d0e8a3fdbfb6a
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-27 14:20:23 -0600

    pep8 on views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,17 +7,19 @@ from api.v1.views import app_views
 from models import storage
 app = Flask(__name__)
 
+
 @app_views.route('/status/', strict_slashes=False)
 def status():
     nominal = {"Status": "OK"}
     return jsonify(nominal)
 
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats():
-    stats = {"amenities": storage.count('Amenity'), 
-             "cities": storage.count('City'), 
-             "places": storage.count('Place'), 
-             "reviews": storage.count('Review'), 
-             "states": storage.count('State'), 
+    stats = {"amenities": storage.count('Amenity'),
+             "cities": storage.count('City'),
+             "places": storage.count('Place'),
+             "reviews": storage.count('Review'),
+             "states": storage.count('State'),
              "users": storage.count('User')}
     return jsonify(stats)
