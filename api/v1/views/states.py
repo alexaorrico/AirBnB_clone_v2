6b981dc58a588c80a363ff2f4e2220355a7113ba
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-01-27 00:43:57 -0500

    States API

---
+++ api/v1/views/states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+ New view for State objects that handles all default RestFul API actions.
+"""
+from models import BaseModel, storage
+from flask import Flask, request, jasonify
+from api.v1.views import app_views
+
+
+@app_views.route('/api/v1/states', methods=['GET'])
+def all_states():
+    """ Retrieves the list of all State objects. """
+
+
+@app_views.route('/api/v1/states/<state_id>', methods=['GET'])
+def specific_state(state_id):
+    """ Retrieves a State object. """
+
+
+@app_views.route('/api/v1/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """ Deletes a State object. """
+
+
+@app_views.route('/api/v1/states', methods=['POST'])
+def create_state():
+    """ Creates a State. """
+
+
+@app_views.route('/api/v1/states/<state_id>', methods=['PUT'])
+def updates_state(state_id):
+    """ Updates a State object. """
