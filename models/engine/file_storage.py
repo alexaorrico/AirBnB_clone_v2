cca0f2bbebefc36172da77cf08a59eed9b55999e
onsjannet <onsjannet@gmail.com>
2021-01-27 15:37:20 +0100

    added engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieves one object"""
+        if id and cls:
+            return self.all(cls)[str(cls.__name__)+"."+id]
+        else:
+            return None
+
+    def count(self, cls=None):
+        """
+            counts object
+        """
+        return (len(self.all(cls)))
