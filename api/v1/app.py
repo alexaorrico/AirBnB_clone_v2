40ebe846f6f2bfbff95a10f88a95a86ba7b30ca6
hfog <hfog2000@yahoo.com>
2021-01-27 16:03:37 -0500

    modified app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-"""
-Sets up Flask application
-
-"""
-
-
-from os import getenv
-from flask import Flask, make_response, jsonify
+"""app.py to connect to API"""
+import os
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
-from api.v1.views import app_views
-from models import storage
 
 app = Flask(__name__)
-CORS(app, orgins='0.0.0.0')
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_appcontext(code):
+    """teardown_appcontext"""
+    storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """Returns JSON error repsponse"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-
-@app.teardown_appcontext
-def teardown(self):
-    """Closes storage session"""
-    storage.close()
-
-
-if __name__ == '__main__':
-    api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    api_port = getenv('HBNB_API_PORT', default=5000)
-    app.run(host=api_host, port=int(api_port), threaded=True)
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
