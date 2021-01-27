15efad457e38691fd5cfcd11879b96145a1ec270
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-01-27 23:16:56 +0000

    Adding branch

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,8 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+    
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not found
+        """
+        self.all(cls)
