7820471b31d0b8ebd15715af9fc7eae0cfd6c782
dreeseh <1859@holbertonschool.com>
2021-01-27 20:34:52 +0000

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,13 @@ def end_session(exception):
     return storage.close()
 
 
+@app.errorhandler(404)
+def not_found(self):
+    """handle a 404 error"""
+    json_not_found = (jsonify({'error': 'Not found'}), 404)
+    return json_not_found
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', '5000'))
