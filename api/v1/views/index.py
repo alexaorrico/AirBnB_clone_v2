cd3820cf0b2a58a2951ff71d17734f26faf01076
ElianaGomez2020 <elianagomez.s@outlook.com>
2021-01-26 19:05:06 -0500

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,25 @@
 """Create a route that returns a JSON"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 @app_views.route('/status')
 def status():
     """returns json file"""
     return jsonify ({"status": "OK"})
+
+@app_views.route('/stats')
+def endpoint():
+    """retrieves the number of each objects by type"""
+    stats = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+        }
+    stats_dic = {}
+    for key, value in stats.items():
+        stats_dic[value] = storage.count(key)
+    return jsonify(stats_dic)
