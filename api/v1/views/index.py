8268c3348117f01cc78f1a9af42477755419d579
Fabkar <fab_862@hotmail.com>
2021-01-27 10:38:14 -0500

    update files with documentation and changes permission

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+"""Status APi """"
 from flask import Flask, jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status')
 def status():
+    """Status API by json file"""
     return jsonify({"status": "OK"})
