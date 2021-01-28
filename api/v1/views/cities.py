e9d0ccea062f1c5fefcd26484a98018e433eae04
xd <xd@xds-MacBook-Pro.local>
2021-01-27 23:38:42 -0500

    update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,14 +7,17 @@ from models.city import City
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
-def states(state_id=None):
-    """ xd """
+def cities(state_id=None):
+    """ falta arreglar xd """
 
     data = storage.get(State, state_id)
     if data is None:
         abort(404)
 
     cities = []
-    for city in data.cities:
-        cities.append(city.dict())
-    return jsonify(cities)
+    if data:
+
+        for city in data.cities:
+            cities = city.to_dict()
+        return (jsonify(cities), 200)
+    abort(404)
