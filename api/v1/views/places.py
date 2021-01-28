a2ebdc9dba3fc5ea191b2a1cf7ccc752500d7b4b
jfbm74 <jfbm74@gmail.com>
2021-01-28 18:12:34 -0500

    Modules that handles all Restful API actions for Places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,7 @@ from flask import jsonify, abort, make_response, request
 from models import storage
 from models.place import Place
 from models.city import City
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places',
@@ -23,4 +24,62 @@ def places_by_city(city_id):
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
-    pass
+    """Returns a given  object place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    return jsonify(place.to_dict())
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_place(place_id):
+    """Deletes a given object place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    storage.delete(place)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route("/cities/<city_id>/places", methods=["POST"],
+                 strict_slashes=False)
+def post_place(city_id):
+    """Creates a place resource"""
+    new_city = storage.get(City, city_id)
+    if new_city is None:
+        abort(404)
+    query = request.get_json()
+    if not query:
+        return jsonify({'error': 'Not a JSON'}), 400
+    if 'name' not in query:
+        return jsonify({'error': 'Missing name'}), 400
+    if 'user_id' not in query:
+        return jsonify({'error': 'Missing user_id'}), 400
+    else:
+        user_id = query['user_id']
+        user = storage.get(User, user_id)
+        if user is None:
+            abort(404)
+
+    obj = Place(**query)
+    setattr(obj, "city_id", city_id)
+    storage.new(obj)
+    storage.save()
+    return jsonify(obj.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_place(place_id):
+    """update a place with PUT method"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    if not request.get_json():
+        return jsonify({'error': 'Not a JSON'}), 400
+    for attr, val in request.get_json().items():
+        setattr(place, attr, val)
+    place.save()
+    return jsonify(place.to_dict()), 200
