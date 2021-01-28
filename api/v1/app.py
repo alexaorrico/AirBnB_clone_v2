84244d0200e1f44f81275d2dd37760aff149ee76
xd <xd@xds-MacBook-Pro.local>
2021-01-28 16:36:14 -0500

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,12 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint
 from os import getenv
 from flask import make_response
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
