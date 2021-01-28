0a665ddd03c0989f6a408eca9d33019a52c2fee8
Tim <timmcmackenjr@gmail.com>
2021-01-28 16:57:12 -0600

    Moved app_views back to it's proper place and accepted pep8 error alert

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,7 @@
 Module for hbnb API api
 """
 from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
@@ -10,5 +11,3 @@ from api.v1.views.cities import *
 from api.v1.views.places import *
 from api.v1.views.users import *
 from api.v1.views.places_reviews import *
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
