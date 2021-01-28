eedc894e3ce584b54d807e8d537ce0dcd5ff72df
cort-robinson <cortrobinson99@gmail.com>
2021-01-28 15:16:57 +0000

    add error handling for error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,12 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(e):
+    """handle 404 error"""
+    return jsonify(error='Not found'), 404
+
+
 if __name__ == '__main__':
     app.run(
         host=getenv('HBNB_API_HOST', '0.0.0.0'),
