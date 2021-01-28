bd8983b2765095e5a2e771cdf910503b1b598663
onsjannet <onsjannet@gmail.com>
2021-01-28 19:52:55 +0100

    changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,8 +6,8 @@ Contains the FileStorage class
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
-from models.city import City
 from models.place import Place
+from models.city import City
 from models.review import Review
 from models.state import State
 from models.user import User
