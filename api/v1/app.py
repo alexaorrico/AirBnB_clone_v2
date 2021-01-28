2fdddd030d66aa520f1da46d9cd0c2dafc0f9328
valen2510 <valenjaramillo1025@hotmail.com>
2021-01-28 14:17:15 -0500

    Determine HTTP access control CORS.

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,14 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 host = getenv('HBNB_API_HOST')
 port = getenv('HBNB_API_PORT')
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
