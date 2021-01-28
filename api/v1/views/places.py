1ec760773e43d54fd258ad4eb7c14d82900d2e5a
rogerrendons <1584@holbertonschool.com>
2021-01-28 16:17:37 -0500

    Task 11 v3

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,7 +6,6 @@ from flask import jsonify, abort, request
 from models.state import State
 from models.city import City
 from models.place import Place
-from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
@@ -53,7 +52,7 @@ def post_place(city_id):
     data = request.get_json()
     if data is None:
         abort(400, "Not a JSON")
-    elif storage.get("User", data["user_id"]) is None:
+    elif not storage.get("User", data["user_id"]):
         abort(404)
     elif "user_id" not in data.keys():
         abort(400, "Missing user_id")
