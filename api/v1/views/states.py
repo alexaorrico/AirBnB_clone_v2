764f91ba63ec813349b66a46f7489738a8ce6813
julianalvarezcaro <julian.alvarez.caro@hotmail.com>
2021-01-28 17:17:33 +0000

    Forgot to import make_response

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,7 +5,7 @@
 # from models.state import State
 from models import storage
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from models.state import State
 
 
