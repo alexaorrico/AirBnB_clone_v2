5ee8cad31414fdf4d8418a73cac2f92247b1ccbe
RicardoArquimedes <richardcqt28@gmail.com>
2021-01-28 21:10:02 -0500

    fixxing output 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,25 +4,27 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 from os import getenv
 
-
 app = Flask(__name__)
+
+
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
-def teardown_app(self):
-    ''' remove current storage sessions '''
+def teardown(self):
+    """ handles teardown """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
+def page_not_found(error):
     """handler for 404 errors that returns a
     JSON-formatted 404 status code response"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return (jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-''' Creates blueprint for flask app which will
-    be imported into app.py
-'''
+"""Creates blueprint for flask app"""
+
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
