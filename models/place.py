ea146dc188d799d6ddd09c97fb57ab87a5840e97
dreeseh <1859@holbertonschool.com>
2021-01-29 19:38:24 +0000

    added cascade

--- models/place.py
+++ models/place.py
@@ -9,12 +9,16 @@ from sqlalchemy.orm import relationship
 
 if models.storage_t == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id', onupdate='CASCADE',
+                          Column('place_id',
+                                 String(60),
+                                 ForeignKey('places.id',
+                                            onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id', onupdate='CASCADE',
+                          Column('amenity_id',
+                                 String(60),
+                                 ForeignKey('amenities.id',
+                                            onupdate='CASCADE',
                                             ondelete='CASCADE'),
                                  primary_key=True))
 
@@ -33,8 +37,11 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship("Review", backref="place")
-        amenities = relationship("Amenity", secondary="place_amenity",
+        reviews = relationship("Review",
+                               backref="place",
+                               cascade="all, delete")
+        amenities = relationship("Amenity",
+                                 secondary="place_amenity",
                                  backref="place_amenities",
                                  viewonly=False)
     else:
