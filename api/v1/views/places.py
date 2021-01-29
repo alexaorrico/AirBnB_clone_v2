153391508ba50b6f21cba53bc6c3abeb8cf9d5b9
dreeseh <1859@holbertonschool.com>
2021-01-29 14:49:46 +0000

    added places.py

---
+++ api/v1/views/places.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+""" a new view for State objects that handles all default RestFul API actions
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models import storage
+from models.city import City
+from models.user import User
+from models.place import Place
+
+
+@app_views.route('/cities/<city_id>/places',
+                 methods=['GET', 'POST'], strict_slashes=False)
+def _places(city_id):
+    """retrieves the list of all place objects
+    """
+    if request.method == "GET":
+        all_places = []
+        city_info = storage.get('City', city_id)
+
+        if city_info is not None:
+            for key in city_info.places:
+                all_places.append(key.to_dict())
+            return jasonify(city_info)
+        abort(404)
+
+    if request.method == 'POST':
+        if not request.is_json:
+            return "Not a JSON", 400
+
+        city_info = storage.get('City', city_id)
+        if city_info is not None:
+            dict = {'city_id':, city_id}
+            dict.update(request.get_json())
+            all_places = Place(**dict)
+            dict_info = all_places.to_dict()
+
+        if "user_id" not in dict_info.keys():
+            return "Missing user_id", 400
+
+        if not storage.get(User, dict_info.get("user_id")):
+            abort(404)
+
+        if "name" not in dict_info.keys():
+            return "Missing name", 400
+
+        all_places.save()
+        return all_amenities.to_dict(), 201
+    abort(404)
+
+
+@app_views.route('/places/<places_id>', methods=['GET', 'PUT', 'DELETE'],
+                 strict_slashes=False)
+def places_id(place_id):
+    """updates a place object
+    """
+    if request.method == "GET":
+        place_info = storage.get(Place, place_id)
+        if place_info is not None:
+            return amenity_info.to_dict()
+        abort(404)
+
+    if request.method == "PUT":
+        amenity_info = storage.get(Amenity, amenity_id)
+        if amenity_info is not None:
+            if not request.is_json:
+                return "Not a JSON", 400
+
+            for k, v in request.get_json().items():
+                setattr(amenity_info, k, v)
+            storage.save()
+            return amenity_info.to_dict()
+        abort(404)
+
+    if request.method == "DELETE":
+        amenity_info = storage.get(Amenity, amenity_id)
+        if amenity_info:
+            amenity_info.delete()
+            storage.save()
+            return {}, 200
+        abort(404)
