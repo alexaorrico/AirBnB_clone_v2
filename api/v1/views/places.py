250b29ba5d78e2260ac273ee390c17754141d287
cristian-bedoya <crisbed321@gmail.com>
2021-01-29 00:51:57 +0000

    add views states & places

---
+++ api/v1/views/places.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""Handles all default RestFul API actions for State objects"""
+
+from api.v1.views import app_views
+from flask import jsonify, make_response, abort, request
+from models import storage
+from models.amenity import Amenity
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.review import Review
+
+
+@app_views.route("/cities/<city_id>/places",
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
+@app_views.route("/places/<place_id>",
+                 methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
+def cities_places_list(city_id=None, place_id=None):
+    """Retrieves a list of all Place objects derived from City IDs"""
+    if request.method == 'GET':
+        l_1 = []
+        if city_id and not place_id:
+            city = storage.get("City", city_id)
+            if not city:
+                abort(404)
+            for value in city.places:
+                d = value.to_dict()
+                l_1.append(d)
+            return jsonify(l_1)
+        elif place_id and not city_id:
+            place = storage.get("Place", place_id)
+            if not place:
+                abort(404)
+            return jsonify(place.to_dict())
+    elif request.method == 'DELETE':
+        place = storage.get("Place", place_id)
+        if not place:
+            abort(404)
+        place.delete()
+        storage.save()
+        return jsonify({}), 200
+    elif request.method == 'POST':
+        city = storage.get("City", city_id)
+        if not city:
+            abort(404)
+        dic = request.get_json()
+        if not dic:
+            return 'Not a JSON', 400
+        if "user_id" not in dic.keys():
+            return 'Missing user_id', 400
+        user = storage.get("User", dic["user_id"])
+        if not user:
+            abort(404)
+        if "name" not in dic.keys():
+            return 'Missing name', 400
+        dic.update(city_id=city_id)
+        p = Place(**dic)
+        p.save()
+        return jsonify(p.to_dict()), 201
+    elif request.method == 'PUT':
+        place = storage.get("Place", place_id)
+        if place is None:
+            abort(404)
+        dic = request.get_json()
+        if not dic:
+            return 'Not a JSON', 400
+        for k, v in dic.items():
+            if (k != "id" or k != "created_at" or k != "updated_at" or
+                    k != "city_id" or k != "user_id"):
+                setattr(place, k, v)
+        place.save()
+        return jsonify(place.to_dict()), 200
---
+++ api/v1/views/states.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Handles all default RestFul API actions for State objects"""
+
+from api.v1.views import app_views
+from flask import jsonify, make_response, abort, request
+from models import storage
+from models.amenity import Amenity
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.review import Review
+
+
+@app_views.route("/states",
+                 methods=['GET', 'DELETE', 'POST', 'PUT'],
+                 strict_slashes=False)
+@app_views.route("/states/<state_id>",
+                 methods=['GET', 'DELETE', 'POST', 'PUT'],
+                 strict_slashes=False)
+def states_list(state_id=None):
+    """Retrieves a list of all State objects"""
+    if request.method == 'GET':
+        if not state_id:
+            states = storage.all("State").values()
+            l_1 = []
+            for value in states:
+                d = value.to_dict()
+                l_1.append(d)
+            return jsonify(l_1)
+        else:
+            state = storage.get("State", state_id)
+            if not state:
+                abort(404)
+            return jsonify(state.to_dict())
+    elif request.method == 'DELETE':
+        state = storage.get("State", state_id)
+        if not state:
+            abort(404)
+        state.delete()
+        storage.save()
+        return jsonify({}), 200
+    elif request.method == 'POST':
+        dic = request.get_json()
+        if not dic:
+            return 'Not a JSON', 400
+        if "name" not in dic.keys():
+            return 'Missing name', 400
+        s = State(**dic)
+        s.save()
+        return jsonify(s.to_dict()), 201
+    elif request.method == 'PUT':
+        state = storage.get("State", state_id)
+        if state is None:
+            abort(404)
+        dic = request.get_json()
+        if not dic:
+            return 'Not a JSON', 400
+        for k, v in dic.items():
+            if k != "id" or k != "created_at" or k != "updated_at":
+                setattr(state, k, v)
+        state.save()
+        return jsonify(state.to_dict())
