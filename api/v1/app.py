2634a2ca40edd2748eae3c4759faa012271505ab
hurtadojara <andreshurtadojaramillo@gmail.com>
2021-01-28 19:54:07 -0500

    ready point 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,25 +2,25 @@
 ''' our first API '''
 
 from flask import Flask, jsonify, make_response
-from api.v1.views import app_views
 from os import getenv
 from models import storage
+from api.v1.views import app_views
 
 host = getenv('HBNB_API_HOST')
 port = getenv('HBNB_API_PORT')
 
 app = Flask(__name__)
-app.register_blueprints(app_views)
+app.register_blueprint(app_views)
 
 @app.errorhandler(404)
 def error_404(error):
-    """Handling 404 error with JSON output"""
+    """Handling 404 error with JSON"""
     return make_response(jsonify({"error": "Not found"})), 404
 
 
 @app.teardown_appcontext
 def storage_close(exception):
-    """ Closes Storage with exception"""
+    """ Close Storage with exception"""
     storage.close()
 
 
@@ -29,4 +29,4 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if not port:
         port = 5000
-    app.run(host=host, debug=True, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """ Initialize the register of Blueprint"""
-from flask import Blueprint
+from flask import Flask, Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
