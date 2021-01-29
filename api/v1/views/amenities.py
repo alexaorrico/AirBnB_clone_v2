e62928fabf5cf0ded37ed3926df4f599cb17af20
dreeseh <1859@holbertonschool.com>
2021-01-29 02:42:18 +0000

    reworked delete into second function

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -8,10 +8,8 @@ from models.amenity import Amenity
 
 
 @app_views.route('/amenities/', methods=['GET', 'POST'], strict_slashes=False)
-@app_views.route('/amenities/<amenity_id>/', methods=['GET', 'POST'],
-                 strict_slashes=False)
-def amenities_get():
-    """retrieves the list of all State objects
+def amenities():
+    """retrieves the list of all Amenity objects
     """
     if request.method == "GET":
         all_amenities = []
@@ -29,23 +27,9 @@ def amenities_get():
         return all_amenities.to_dict(), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+@app_views.route('/amenity/<amenity_id>', methods=['GET', 'PUT', 'DELETE'],
                  strict_slashes=False)
-def delete_amenities(amenity_id):
-    """deletes a State object
-    """
-    amenity = storage.get('Amenity', amenity_id)
-    if amenity is None:
-        return jsonify(abort(404))
-    amenity.delete()
-    storage.save()
-    dict = {}
-    return (jsonify(dict), 200)
-
-
-@app_views.route('/amenity/<amenity_id>', methods=['PUT', 'GET'],
-                 strict_slashes=False)
-def put_amenity(amenity_id):
+def amenities_id(amenity_id):
     """updates a state object
     """
     if request.method == "GET":
@@ -63,4 +47,12 @@ def put_amenity(amenity_id):
                 setattr(amenity_info, k, v)
             storage.save()
             return amenity_info.to_dict()
+        abort(404)
+
+    if request.method == "DELETE":
+        amenity_info = storage.get(Amenity, amenity_id)
+        if amenity_info:
+            amenity_info.delete()
+            storage.save()
+            return {}, 200
         abort(404)
