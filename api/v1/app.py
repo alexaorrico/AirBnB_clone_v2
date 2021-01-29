a83a3e25bff1567bd87b994ac222e045fa4542ff
TMcMac <timmcmackenjr@gmail.com>
2021-01-29 03:57:36 +0000

    goes back to using getenv()

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,13 +27,13 @@ def not_found(self):
 
 
 if __name__ == "__main__":
-    """host = os.getenv('HBNB_API_HOST')
+    host = os.getenv('HBNB_API_HOST')
     port = int(os.getenv('HBNB_API_PORT'))
-    ###THIS IS SETTING IT, NOT CHECKING """
-
-    """ os.environ['HBNB_API_HOST'] = '0.0.0.0' """
+    
+    #THIS IS SETTING IT, NOT CHECKING
+    """ os.environ['HBNB_API_HOST'] = '0.0.0.0'
     host = '0.0.0.0'
-    """ os.environ['HBNB_API_PORT'] = '5000'    """
-    port = '5000'
+    os.environ['HBNB_API_PORT'] = '5000'
+    port = '5000'"""
 
     app.run(host=host, port=port, threaded=True)
