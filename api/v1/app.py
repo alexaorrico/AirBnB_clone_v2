6e1f32ad1a8e7eef976cc3b10253136c5f20eb66
cristian-bedoya <crisbed321@gmail.com>
2021-01-29 00:08:08 +0000

    add module Os for handle enviroment

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,7 +17,7 @@ def teardown_app(self):
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
