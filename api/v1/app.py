34584ad7c0166b626025b0b1e27dcc901a029ca4
1819@holbertonschool.com <1819@holbertonschool.com>
2021-01-28 19:15:05 -0600

    Raw set host and port for now

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,15 +27,13 @@ def not_found(self):
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST')
+    """host = os.getenv('HBNB_API_HOST')
     port = int(os.getenv('HBNB_API_PORT'))
-    """
-    if !host:
-        os.environ['HBNB_API_HOST'] = '0.0.0.0'
-        host = os.getenv('HBNB_API_HOST')
-    port = int(os.getenv('HBNB_API_PORT'))
-    if !port:
-        os.environ['HBNB_API_PORT'] = int(5000)
-        port = int(os.getenv('HBNB_API_PORT'))
-    """
+    ###THIS IS SETTING IT, NOT CHECKING """
+
+    """ os.environ['HBNB_API_HOST'] = '0.0.0.0' """
+    host = '0.0.0.0'
+    """ os.environ['HBNB_API_PORT'] = '5000'    """
+    port = '5000'
+
     app.run(host=host, port=port, threaded=True)
