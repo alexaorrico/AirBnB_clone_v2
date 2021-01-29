9880278f852c225f03257d24cc0c77968814fe8c
RicardoArquimedes <richardcqt28@gmail.com>
2021-01-28 20:16:30 -0500

    fixxing docstring and outputs

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-''' Starts a flask session and imports blueprint '''
+"""Starts a flask session and imports blueprint"""
+
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
@@ -17,9 +20,12 @@ def teardown_app(self):
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Not found"}), 404
+    """handler for 404 errors that returns a
+    JSON-formatted 404 status code response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__ == "__main__":
-    app.run(threaded=True, host='0.0.0.0' or HBNB_API_HOST,
-            port='5000' or HBNB_API_PORT)
+if __name__ == '__main__':
+    HBNB_API_HOST = getenv("HBNB_API_HOST", '0.0.0.0')
+    HBNB_API_PORT = getenv("HBNB_API_PORT", 5000)
+    app.run(debug=True, host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
