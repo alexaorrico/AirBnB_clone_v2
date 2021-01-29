dd0d33442cca836c1cb629322cfbd36bb00cee90
dreeseh <1859@holbertonschool.com>
2021-01-29 20:13:26 +0000

    added kwargs

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -30,8 +30,8 @@ def _places(city_id):
 
         city_info = storage.get(City, city_id)
         if city_info is not None:
-            dict = {"city_id": city_id}
-            dict.update(request.get_json())
+            kwargs = {"city_id": city_id}
+            kwargs.update(request.get_json())
             all_places = Place(**dict)
             dict_info = all_places.to_dict()
 
