90852e0fbc9551443c8625d4413d92d8e08aaa9a
dreeseh <1859@holbertonschool.com>
2021-01-29 21:29:51 +0000

    did not change models

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,7 +4,7 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models import storage
-from models.city import Review
+from models.review import Review
 from models.user import User
 from models.place import Place
 
