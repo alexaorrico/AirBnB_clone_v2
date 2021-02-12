ae5e2c82a6fe0fa2bdd8d74cece76da1058ab6ef
Deyber2000 <garzondeiber6@gmail.com>
2021-02-12 14:28:52 -0500

    change cors scope

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
