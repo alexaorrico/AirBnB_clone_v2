79fff46fddc09bfd92c2f474b37fc90cd1bff8aa
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-05-06 09:20:01 +0000

    Add route /status on the Flask app

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    This is the Blueprint handler for Flask.
+"""
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    This is the index page handler for Flask.
+"""
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    """
+        Flask route at /status.
+        Displays the status of the API.
+    """
+    return {"status": "OK"}
