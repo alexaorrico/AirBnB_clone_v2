86a4b31cea71ec23f2252fd7438744d8f7cfb85e
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-05-06 21:52:59 +0000

    Some modifications on stats, status and handler error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,14 +4,16 @@ from api.v1.views import app_views
 import flask
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """ return json """
-    return flask.jsonify({'status':'OK'})
+    return flask.jsonify({'status': 'OK'})
 
-@app_views.route('/stats', methods=['GE'])
+
+@app_views.route('/stats', methods=['GET'])
 def stats():
-    """ 
+    """
     Endpoint that retrieves the number of each objects by type
     """
     stats = {
@@ -22,9 +24,8 @@ def stats():
             "states": "State",
             "users": "User"
             }
-    for key,value in stats.items():
-        key = storage.count(value)
+    new = {}
+    for key, value in stats.items():
+        new[key] = storage.count(value)
 
-    return flask.jsonify(stats)
-
-
+    return flask.jsonify(new)
