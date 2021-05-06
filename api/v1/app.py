0be905ee8b296ffdb3f3d4c705936a09836c59e3
7honarias <7honarias@gmail.com>
2021-05-06 20:31:29 +0000

    [UPDATE] - task 5 - 6 of flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Module app flask"""
 from models import storage
-from flask import Blueprint, Flask
+from flask import Blueprint, Flask, jsonify, make_response
 from os import environ
 from api.v1.views import app_views
 
@@ -10,11 +10,15 @@ env = environ.copy()
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
 def teardown(exception):
     """close storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     if "HBNB_API_HOST" in env:
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+from models.amenity import Amenity
 from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
 from api.v1.views import app_views
 from flask import Flask, jsonify
+
 
 @app_views.route('/status', strict_slashes=False)
 def route_status():
@@ -9,8 +16,18 @@ def route_status():
 
 @app_views.route('/stats', strict_slashes=False)
 def statsRoute():
-    count = storage.count()
-    return jsonify({})
+    dircount = {}
+    dir_clases = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    for key, value in dir_clases.items():
+        dircount[key] = storage.count(value)
+    return jsonify(dircount)
 
 if __name__ == "__main__":
     pass
