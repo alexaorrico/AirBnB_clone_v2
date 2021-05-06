4c2fecab0fb9a221eab7f972b4f27393ca1f40ae
Wiston Venera Macías <wisvem@hotmail.com>
2021-05-06 16:33:27 +0000

    Added function to retreive one object Added function to count objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ A method to retrieve one object """
+        storage_all = self.all(cls)
+        if cls.__name__+'.'+id in storage_all:
+            return storage_all[cls.__name__+'.'+id]
+        return None
+
+    def count(self, cls=None):
+        """ A method to count the number of objects in storage """
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
