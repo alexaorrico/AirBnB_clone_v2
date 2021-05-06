cf76ef802541280484a137746de71d4989a1fe78
yagomfh <yagomfh@gmail.com>
2021-05-06 07:51:09 +0000

    feat: methods count + get added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Method to retrieve a specific object"""
+        for obj in self.all(cls).values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """Method that counts the number of object of a class"""
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
