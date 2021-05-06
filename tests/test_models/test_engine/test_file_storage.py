ae793fd7c9f9133a6d7d6f4f919ffa9c0da0baec
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-05-06 20:20:29 +0000

    Adding test of get and count

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,27 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+    
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Testing get method, that recieve a class name and id and return object"""
+        state_test = State("Alabama")
+        dict_state = storage.all()
+        id_object_state = list(storage.all().values())[0].id
+        object_state = list(storage.all().values())[0]
+        test_get = storage.get(obj.__class__, id_object_state)
+        # testing object and get
+        self.assertEqual(object_state, test_get)
+
+        State2_test = State("Ale")
+        Satet2_test.save()
+        test = storage.get(State, State2_test.id)
+        self.assertEqual(type(test), State)
+    
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Testing count method, that receive a class name and return number of objects"""
+        State2_test = State("Ale")
+        count = storage.count(State)
+        self.assertEqual(count, storage.count(State))
+
