40a9506ec7b0d112c3b88e6699f9ada562694007
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-05-06 09:21:01 +0000

    Fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,9 @@ from flask import Flask
 from models import storage
 from os import getenv
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 """
 from api.v1.views import app_views
 
+
 @app_views.route('/status')
 def status():
     """
