bd3af49fb4a2b63bdf55364b903993beaf0cc99a
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-05-06 09:48:11 +0000

    Add stats and 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,11 @@ def teardown_db(exception):
     """Closes the storage on teardown"""
     storage.close()
 
+@app.errorhandler(404)
+def error_404(exception):
+    """Returns the 404 error custom messsage"""
+    return {"error": "Not found"}, 404
+
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
