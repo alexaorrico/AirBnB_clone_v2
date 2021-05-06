03b28b4023be09caf3c93d4fcf1eb5bac73b2301
Brandixitor <med.salah.bourouba@gmail.com>
2021-05-06 14:51:48 +0100

    Update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -46,7 +46,7 @@ class FileStorage:
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w') as f:
-            json.dump(json_objects, f)
+            json.dump(json_objects, f, indent=4)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
@@ -68,3 +68,12 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieves an object in the storage"""
+        obj = self.all(cls).get("{}.{}".format(cls.__name__, id), None)
+        return obj
+
+    def count(self, cls=None):
+        """count the number of objects in the storage"""
+        return len(self.all(cls))
