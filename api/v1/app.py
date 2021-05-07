44d0920d72db99e176cadcd964d7f82928255415
Aleia DeVore <2496@holbertonschool.com>
2021-05-07 21:28:54 +0000

    Why does it claim there's no documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,10 +22,5 @@ def teardown(self):
     """Calls storage.close()"""
     storage.close()
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Handles 404 error"""
-    return jsonify("error": "Not found"), 404
-
 if __name__ == "__main__":
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
