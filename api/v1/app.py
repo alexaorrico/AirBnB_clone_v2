5963c0f353cd033d1e48e798903b1be35ff7ff0c
root <maxwellloganlovell@gmail.com>
2021-05-07 01:35:49 +0000

    code_add for task 5 & 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-'''  flask app  '''
+'''flask app'''
 
 from os import environ
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def remove_session(exception):
-    ''' removes current Session '''
+    '''removes current Session'''
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    '''handles 404'''
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
