9545efb0469c2436506de66b79928cff17953950
mauriciosierrac <2376@holbertonschool.com>
2021-05-07 15:18:08 -0500

    fixed app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
-@app.teardown_appcontext()
-def close():
+@app.teardown_appcontext
+def close(exception):
     storage.close()
 
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 from flask import Flask
+from flask.globals import request
 from flask.json import jsonify
 
 from api.v1.views import app_views
 
-app = Flask(__name__)
 
-@app.route('/status')
-def app_views():
+@app_views.route('/status', methods=['GET'])
+def status():
     return jsonify({"status": "OK"})
