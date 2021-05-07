c803a36fe1323f703ce59d80040f317416e60a05
jfangwang <qbs1864@gmail.com>
2021-05-07 18:17:51 +0000

    added jsonify

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""index file"""
+
+from api.v1.views import app_views
+from Flask import jsonify
+
+@app_views.route("/status")
+def status():
+    """status"""
+    return jsonify({"status": "OK"})
