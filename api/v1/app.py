d004b276eefdcd9ebcd38c9782f95845c1b036b8
laurendobratz00 <laurendobratz@icloud.com>
2021-05-07 21:16:41 +0000

    pep8 and documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+""" starts api """
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" create a variable app_views which is an instance of Blueprint """
 from api.v1.views import app_views
 
 
