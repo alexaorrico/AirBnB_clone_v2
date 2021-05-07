31a72e2bf8a4e65e59e523769ff4cd328d9c6037
ranicholson <ranicho2@gmail.com>
2021-05-07 17:26:23 +0000

    Updates

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Index file that returns a JSON status"""
+
+from api.v1.views import app_views
+from flask import Flask
+from flask import jsonify
+
+
+@app_views.route('/status')
+def status():
+    """Method that returns a JSON status"""
+    return jsonify(status=OK)
