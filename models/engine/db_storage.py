ea5bc499556423141da98c4ecb4a46bbd35005b7
jashjchoi <jashjchoi@gmail.com>
2021-05-07 02:06:21 +0000

    Add task 3 files but mysql is not running.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,25 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ Method to retrieve one object. Returns the object
+            based on the class and its ID, or None if not found
+        """
+        if cls:
+            key = cls.__name__ + '.' + id
+            objs = self.all(cls)
+            obj = objs.get(key)
+            return obj
+        else:
+            return None
+
+    def count(self, cls=None):
+        """ Method to count the number of objects in storage
+            Returns the number of objects in storage matching the given class.
+            If no class is passed, returns the count of all objects in storage.
+        """
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
