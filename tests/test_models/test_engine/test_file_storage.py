31f174b451a87513832ad3a49908b4ebf337c3ff
huy75 <huy.nguyen66@gmail.com>
2021-05-07 14:47:10 +0000

    fix add from models import storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -14,6 +14,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models import storage
 import json
 import os
 import pep8
