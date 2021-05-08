3d7b954c0bdcb33ff4c69ce8a5cadf6b61b12878
AmilcarArmmand <armmand@gmail.com>
2021-05-08 19:55:12 +0000

    added error response

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,10 +2,10 @@
 """
 Flask web application
 """
-from flask import Flask, jsonify
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 from models import storage
-from api.v1.views import app_views
 from os import getenv
 
 
@@ -23,6 +23,10 @@ app.register_blueprint(app_views)
 def teardown_down(Exception):
     """ """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
