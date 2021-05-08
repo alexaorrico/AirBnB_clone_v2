be3c1235604bc7aa614d8a828de545dc1d79f83d
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-05-07 21:21:45 -0500

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-""" Module for storing indeces for the route Blueprints. """
+""" Index to api to handle status and stats route"""
 from api.v1.views import app_views
-from flask import jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.review import Review
-from models.place import Place
-from models.state import State
-from models.user import User
+import flask
 from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
-def return_status():
-    """ Returns the status of the api. """
-    status = {"status": "OK"}
-    return(jsonify(status))
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ return json with status OK"""
+    return flask.jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
-def return_stats():
-    """ Returns the stats in numbers of the objects available. """
-    classes = {"amenities": Amenity, "cities": City, "places": Place,
-               "reviews": Review, "states": State, "users": User}
-    stats = {}
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """
+    Endpoint that retrieves the number of each objects by type
+    """
+    statsc = {
+            "amenities": "Amenity",
+            "cities": "City",
+            "places": "Place",
+            "reviews": "Review",
+            "states": "State",
+            "users": "User"
+            }
+    for key, value in statsc.items():
+        statsc[key] = storage.count(value)
 
-    for key, obj in classes.items():
-        stats[key] = storage.count(obj)
-
-    return(jsonify(stats))
+    return flask.jsonify(statsc)
