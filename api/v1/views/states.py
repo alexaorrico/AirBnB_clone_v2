2399545bc58da7bc5578b31cf0d8f55f2aae8da1
Aleia DeVore <2496@holbertonschool.com>
2021-05-08 16:58:52 +0000

    Works with no id passed

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,20 +2,23 @@
 """Default restful api actions for state"""
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from models.state import State
 
-@app_views.route('/states')
-def GET(state_id=None):
+
+@app_views.route('/states', methods=['GET'])
+def get_state(state_id=None):
     """Method to retrieve list of all state objects or state object by id"""
+    #return "In the right place"
     if state_id is None:
         state_list = []
         for obj in storage.all(State).values():
             state_list.append(obj.to_dict())
-        return state_list
+        return jsonify(state_list)
+        return "State id is None"
     for obj in storage.all(State).values():
-        if obj.id = state_id:
+        if obj.id == state_id:
             state = obj.to_dict()
             return state
     abort(404)
