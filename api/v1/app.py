cf0f31bc7d669450b266d65ee6def24558c88e36
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-05-08 01:36:26 +0000

    Fixing

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """
 flask application api
 """
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models import storage
