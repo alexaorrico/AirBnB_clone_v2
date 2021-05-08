db23a4828796386cc7bc4fa44e419ecdc0ec1cc0
Sergio Urrego <2016@holbertonschool.com>
2021-05-07 19:32:32 -0500

    update Sergio

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -84,7 +84,7 @@ class DBStorage:
         return None
 
     def count(self, cls=None):
-        """i amss count"""
+        """i amsss count"""
         if cls is not None:
             carlstive = self.all(cls.__name__)
             return (len(carlstive))
