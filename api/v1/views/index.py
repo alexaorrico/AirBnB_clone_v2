f88a65821ff0e7885873294fa57eccd0b5161617
blanketmanatee <ashleyl.welch@gmail.com>
2021-05-08 05:01:15 +0000

    added stat

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ create a variable app_views which is an instance of Blueprint """
+from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route("/status")
 def status():
-    """ this might be broken """
+    """ returns status: OK """
     return ({'"status": "OK"'})
+
+@app_views.route("api/v1/stats")
+def stats():
+    """ create endpoint that retrieves the number of eachobjects by type """
+    return jsonify({storage.count()})
