4ba8e7f8b6df3a6de5f05336d1b6b78ca58ebe88
huy75 <huy.nguyen66@gmail.com>
2021-05-08 15:11:27 +0000

    feat delete, put, post methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,22 +6,64 @@ all default RestFul API actions.
 """
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get():
-    """Retrieve all state objects"""
+def getAll():
+    """Retrieve all objects"""
     l = [obj.to_dict() for obj in storage.all(State).values()]
     return jsonify(l)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'])
-def get_by_id(state_id):
-    """Retrieve a state object by id"""
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get(state_id):
+    """Retrieve an object by id"""
     obj = storage.get(State, state_id)
     if obj:
         return jsonify(obj.to_dict())
     abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete(state_id):
+    """Delete a state object by id"""
+    obj = storage.get("State", state_id)
+    if obj:
+        obj.delete()
+        storage.save()
+        return jsonify({}), 200
+    abort(404)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create():
+    """Create an object"""
+    body = request.get_json()
+    if not body:
+        abort(400, "Not a JSON")
+    if body.get('name') is None:
+        abort(400, "Missing name")
+    d = request.get_json()
+    obj = State(**d)
+    obj.save()
+    return jsonify(obj.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update(state_id):
+    """Update an object"""
+    body = request.get_json()
+    if not body:
+        abort(400, "Not a JSON")
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    for k, v in body.items():
+        if k not in ['id', 'created_at', 'updated_at']:
+            setattr(obj, k, v)
+    storage.save()
+    return jsonify(obj.to_dict())
