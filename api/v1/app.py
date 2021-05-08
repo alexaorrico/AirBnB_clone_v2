1dc54650cbd23addabaa0cf6b93ec72f1a40b6c4
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-05-07 21:25:17 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-""" Flask module, returs status of the api. """
+""" Index to api to handle status and stats route"""
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
+import flask
 from models import storage
-from os import getenv as env
-
-app = Flask(__name__)
-app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
-def close_connection(exception):
-    """ Closes the session's connection. """
-    storage.close()
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ return json with status OK"""
+    return flask.jsonify({"status": "OK"})
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """ Error handler 404"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """
+    Endpoint that retrieves the number of each objects by type
+    """
+    statsc = {
+            "amenities": "Amenity",
+            "cities": "City",
+            "places": "Place",
+            "reviews": "Review",
+            "states": "State",
+            "users": "User"
+            }
+    for key, value in statsc.items():
+        statsc[key] = storage.count(value)
 
-if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = "5000"
-    if env("HBNB_API_HOST"):
-        host = env("HBNB_API_HOST")
-    if env("HBNB_API_PORT"):
-        port = env("HBNB_API_PORT")
-    app.run(host=host, port=port, threaded=True)
+    return flask.jsonify(statsc)
