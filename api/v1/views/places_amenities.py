debb526df9d43d4fa130a306d38cf935aa9b50d7
ChechG <2100@holbertonschool.com>
2021-05-08 18:12:43 +0000

    places amenities

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Your first endpoint (route) will be to return the status of your API """
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.user import User
+from models.amenity import Amenity
+import json
+import models
+
+@app_views.route("/places/<place_id>/amenities/", methods=['GET'])
+@app_views.route("/places/<place_id>/amenities", methods=['GET'])
+def show_amenities(place_id):
+    """ returns list of amenities """
+    if models.storage_t == "db"
+        if request.method == 'GET':
+            places = storage.get(Place, place_id)
+            if places:
+                lista = []
+                for amenity in places.amenities:
+                    lista.append(amenity.to_dict())
+                return jsonify(lista)
+            abort(404)
+    else:
+        if request.method == 'GET':
+            places = storage.get(Place, place_id)
+            if place:
+                lista = []
+                for amenity in places.amenity_ids:
+                    lista.append(amenity.to_dict())
+                return jsonify(lista)
+            abort(404)
+
+@app_views.route("places/<place_id>/amenities/<amenities_id>", methods=['DELETE', 'POST'])
+@app_views.route("places/<place_id>/amenities/<amenities_id>/", methods=['DELETE', 'POST'])
+def show_amenity(place_id, amenities_id):
+    """ returns amenity data from place """
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if place and amenity:
+        if request.method == 'DELETE':
+            for amenit in place.amenities:
+                if amenit.id == amenity_id:
+                    new_dict = {}
+                    amenit.delete()
+                    storage.save()
+                    return jsonify(review.to_dict()), 200
+        elif request.method == 'POST':
+            for amenit in place.amenities:
+                if amenit.id == amenity_id:
+                    return jsonify(amenit.to_dict()), 200
+            setattr(place, 'amenity_id', amenity_id)
+            return jsonify(amenit.to_dict()), 201
+    abort(404)
