2f9c996a4b36c54caa59b6e4d56285b5b416512c
4ions <leovalenciasantana@gmail.com>
2021-05-07 23:13:49 -0500

    Added places

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+""" Initializes module """
+
+
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
@@ -8,3 +12,4 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+from api.v1.views.places import *
