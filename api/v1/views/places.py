28250519ab11a20334a3a0f3748c76ca7b4dc771
Mfeliciano778 <1652@holbertonschool.com>
2021-05-08 01:56:28 +0000

    Task 11 complete??

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -2,7 +2,8 @@
 '''creates a new view for place objects'''
 from models import storage
 from api.v1.views import app_views
-from models.places import Place
+from models.place import Place
+from models.city import City
 from flask import jsonify, request, abort
 
 
@@ -17,13 +18,18 @@ def getter_places():
     return jsonify(new_list)
 
 
-@app_views.route('/places/<place_id>',
+@app_views.route('/cities/<city_id>/places',
                  methods=['GET'], strict_slashes=False)
-def getter_places_id(place_id):
+def getter_places_id(city_id):
     '''getter_id - gets all Place objects by id'''
     try:
-        place = storage.get(Place, place_id).to_dict()
-        return jsonify(place)
+        new_list = []
+        city = storage.get(City, city_id).to_dict()
+        allplaces = storage.all("Place").values()
+        for object in allplaces:
+            if object.city_id == city.id:
+                new_list.append(object.to_dict())
+        return jsonify(city)
     except Exception:
         abort(404)
 
@@ -42,16 +48,20 @@ def deleter_places(place_id):
         abort(404)
 
 
-@app_views.route('/places/', methods=['POST'], strict_slashes=False)
-def post_place():
+@app_views.route('/cities/city_id/places',
+                 methods=['POST'], strict_slashes=False)
+def post_place(city_id):
     '''post_place - create an place object with post'''
     try:
         if not request.get_json():
             return jsonify({"error": "Not a JSON"}), 400
-        body_dict = request.get_json()
-        if "name" not in body_dict:
-            return jsonify({"error": "Missing name"}), 400
-        place = Place(name=body_dict["name"])
+        if storage.get(City, city_id) is None:
+            abort(404)
+        if "user_id" not in request.get_json():
+            return jsonify({"Missing Name"}), 400
+        request = request.get_json()
+        request["city_id"] = city_id
+        place = Place(**request)
         place.save()
         return jsonify(place.to_dict()), 201
     except Exception:
@@ -72,6 +82,8 @@ def put_place(place_id):
     body_dict.pop("id", None)
     body_dict.pop("created_at", None)
     body_dict.pop("updated_at", None)
+    body_dict.pop("user_id", None)
+    body_dict.pop("city_id", None)
     for key, value in body_dict.items():
         setattr(placeId, key, value)
     placeId.save()
