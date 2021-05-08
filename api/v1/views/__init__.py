a71ecf20acd54b5c381c3ead97d6bdba5d149f62
jfangwang <qbs1864@gmail.com>
2021-05-08 20:38:23 +0000

    task 7

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,4 @@ from flask import Blueprint
 
 app_views = Blueprint(__name__, 'app_views', url_prefix='/api/v1')
 from api.v1.views.index import *
+from api.v1.views.states import *
---
+++ api/v1/views/states.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""index file"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+from flask import request
+
+
+@app_views.route("/states", methods=['GET'], strict_slashes=False)
+def get_states():
+    """states"""
+    states_dict = []
+    for item in storage.all('State').values():
+        states_dict.append(item.to_dict())
+    return jsonify(states_dict)
+
+
+@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
+def get_state(state_id=None):
+    """state"""
+    if storage.get('State', state_id) is None:
+        abort(404)
+    else:
+        return jsonify(storage.get('State', state_id).to_dict())
+
+
+@app_views.route("/states/<state_id>",
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def del_state(state_id=None):
+    """state"""
+    willy = storage.get('State', state_id)
+    if willy is None:
+        abort(404)
+    else:
+        storage.delete(willy)
+        storage.save()
+        return jsonify({}), 200
+
+
+@app_views.route("/states", methods=['POST'], strict_slashes=False)
+def post_state(state_id=None):
+    """state"""
+    willy = request.get_json()
+    if willy is None:
+        abort(404)
+    elif "name" not in willy.keys():
+        abort(400, 'Missing name')
+    else:
+        new_state = State(name=willy['name'])
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
+def put_state(state_id=None):
+    """put/update state"""
+    """ Request dict """
+    state_store = storage.get(State, state_id)
+    willy = request.get_json()
+    if state_id is None:
+        abort(404)
+    elif willy is None:
+        abort(404)
+    for key, val in willy.items():
+        print(key, val)
+        # if key == 'id' or key == 'created_at' or key == 'updated_at':
+        #     pass
+        # else:
+        #     setattr(willy, key, val)
+    state_store.save()
+    return jsonify(state_store.to_dict()), 200
