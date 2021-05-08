dadc1c5604d78ce4d5b64c475f5e9b0845b5253e
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-05-07 21:24:20 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""
-flask application api
-"""
+""" Flask module, returs status of the api. """
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
-from os import getenv
+from os import getenv as env
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def toclose(exception):
-    """
-    Handle app.teardown and calls close function
-    """
+def close_connection(exception):
+    """ Closes the session's connection. """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """handler for 404 errors that returns a JSON-formatted
-    404 status code response"""
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(e):
+    """ Error handler 404"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
+    host = "0.0.0.0"
+    port = "5000"
+    if env("HBNB_API_HOST"):
+        host = env("HBNB_API_HOST")
+    if env("HBNB_API_PORT"):
+        port = env("HBNB_API_PORT")
     app.run(host=host, port=port, threaded=True)
