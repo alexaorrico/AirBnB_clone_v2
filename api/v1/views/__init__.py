b961cf0aaf702fb506942631ae76905f615c0222
blanketmanatee <ashleyl.welch@gmail.com>
2021-05-08 04:38:56 +0000

    changed import order

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask
+#!/usr/bin/python3
+""" views """
+
+
 from flask import Blueprint
-from api.v1.views.index import *
-app = Flask(__name__)
 app_views = Blueprint('api/v1', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
