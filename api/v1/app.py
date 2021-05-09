c25991cce477c51824a06ff0d77bca43beb0f4ce
laurendobratz00 <laurendobratz@icloud.com>
2021-05-09 03:03:24 +0000

    add task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,8 +4,11 @@ from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
