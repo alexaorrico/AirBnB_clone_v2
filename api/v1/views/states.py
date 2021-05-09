7db21b8e1e6adae283f904382f1363c45a2797ff
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-05-09 00:47:19 +0000

    Modifications in pep8 style

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -54,7 +54,7 @@ def post_obj():
         new_o = State(**object_state)
         storage.new(new_o)
         new_o.save()
-        return jsonify(new_o.to_dict()), 201       
+        return jsonify(new_o.to_dict()), 201  
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
