6ef9c15c351b3dd775f22bfb4f50fa03051c57f4
laurendobratz00 <laurendobratz@icloud.com>
2021-05-09 02:55:34 +0000

    imported all classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
