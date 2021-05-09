26329202df58d042f3c50dfc1965ce0f0d3fffd0
OyugoObonyo <nyikal1999@gmail.com>
2021-05-09 19:26:18 +0300

    Fix: Handle circular import error

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,9 @@
 """initialize app_views"""
 
 from flask import Blueprint
-from api.v1.views.index import *
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
