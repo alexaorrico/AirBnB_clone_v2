469a5cedf8efaf6990f4cdbdd7a294ead181e81b
Themis <midinfotn401@gmail.com>
2021-05-09 02:16:22 +0100

    fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,13 +79,10 @@ class DBStorage:
         """
         Returns the object
         """
-        if cls not in classes.values():
+        obj = self.__session.query(cls).get(id)
+        if obj is None:
             return None
-
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
+        return obj
 
         return None
 
@@ -93,13 +90,7 @@ class DBStorage:
         """
         count the number of objects in storage
         """
-        all_class = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
+        objs = self.all(cls)
+        return (len(objs))
 
         return count
