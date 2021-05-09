49caed3f6ba4d54c99973bc091246bb3abe88dfb
laurendobratz00 <laurendobratz@icloud.com>
2021-05-09 01:59:25 +0000

    changed route to /stats/ and imported storage from models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 """ create a variable app_views which is an instance of Blueprint """
 from flask import Flask, jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route("/status")
 def status():
@@ -10,7 +10,7 @@ def status():
     return jsonify({'status': 'OK'})
 
 
-@app_views.route("api/v1/stats")
+@app_views.route("/stats")
 def stats():
     """ create endpoint that retrieves the number of eachobjects by type """
     return jsonify({"amenities": storage.count("Amenity"),
