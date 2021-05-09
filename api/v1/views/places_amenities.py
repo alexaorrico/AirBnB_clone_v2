04e41b9bd7b94e72d91857be55f7e21e9b5597f1
TS-N <2501@holbertonschool.com>
2021-05-09 17:12:45 +0000

    feat: task 14 first commit, not tested

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+"""
+Create a new view for the link between Place objects and
+Amenity objects that handles
+all default RestFul API actions.
+"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage, storage_t
+from models.place import Place
+from models.amenity import Amenity
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
+def get_amen(place_id):
+    """ Return all amenities """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404, "Not found")
+    if storage_t == "db":
+        """ Not sure about place.amenities type, i think it's a list as well"""
+        return jsonify(place.amenities)
+    else:
+        return jsonify(place.amenities_ids)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amen(place_id, amenity_id):
+    """ Delete an amenity """
+    pl = storage.get(Place, place_id)
+    amn = storage.get(Amenity, amenity_id)
+    if not pl or not amn:
+        abort(404)
+
+    amn.delete()
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
+                 strict_slashes=False)
+def link_amen(place_id, amenity_id):
+    """ Link an amenity to a place """
+    place = storage.get(Place, place_id)
+    amn = storage.get(Amenity, amenity_id)
+    if not place or not amn:
+        abort(404, "Not found")
+    if storage_t == "db":
+        """ Not sure about place.amenities type, i think it's a list as well"""
+        if amn in place.amenities:
+            return amn, 200
+        place.amenities.append(amn)
+    else:
+        if amn in place.amenities:
+            return amn, 200
+        place.amenities_ids.append(amn)
+    return amn, 201
