a86ff7b9ea12b4870fc764e2d6416f99f6298aee
laurendobratz00 <laurendobratz@icloud.com>
2021-05-09 01:52:12 +0000

    changed quotes in status function in views and changed __init__ file from api/v1 to app_views aaaaaand defined variables at top of app.py file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 """ starts api """
-
-
-from flask import Flask
-
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from models import storage
+from os import getenv
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+HBNB_API_HOST = getenv("HBNB_API_HOST")
+HBNB_API_PORT = getenv("HBNB_API_PORT")
+
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """ not sure if it should be exception or self """
+def teardown(exception):
+    """ i think it's supposed to be exception """
     """ a method that calls storage.close """
     storage.close()
 
-@app.pagenotfound(404)
+
+@app.errorhandler(404)
 def custom_404(error):
     """ Returns JSON 404 """
-    return jsonify({"error": "Not found"}), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ create a variable app_views which is an instance of Blueprint """
-from flask import jsonify
+from flask import Flask, jsonify
 from api.v1.views import app_views
 
 
 @app_views.route("/status")
 def status():
     """ returns status: OK """
-    return ({'"status": "OK"'})
+    return jsonify({'status': 'OK'})
+
 
 @app_views.route("api/v1/stats")
 def stats():
