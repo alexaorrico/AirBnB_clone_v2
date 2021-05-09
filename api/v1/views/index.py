78b933bf4ba1e3d7a4203bc57fe8dd0aef049e15
kiba0510 <kiba0510@gmail.com>
2021-05-09 15:22:13 -0500

    changed to executable the files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,17 @@ def app_status():
     Returns the Status of the API
     '''
     return(jsonify(status="OK"))
+
+
+@app_views.route("/stats")
+def stats():
+    '''
+    Returns stats
+    '''
+    total = {"amenities": storage.count("Amenity"),
+             "cities": storage.count("City"),
+             "places": storage.count("Place"),
+             "reviews": storage.count("Review"),
+             "states": storage.count("State"),
+             "users": storage.count("User")}
+    return jsonify(total)
