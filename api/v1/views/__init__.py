df137f48add46a590c194b58db5ee0c3715a02fe
Alex Smith <1763@holbertonschool.com>
2021-05-09 00:27:57 +0000

    Created view for State objects that handles GET, DELETE, POST, and PUT

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,3 +12,11 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
