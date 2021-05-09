0c5e4fc42c38702daa3df4ed099e5e09e05fc4f8
James <jmartinez21834@stu.nuc.edu>
2021-05-09 00:47:07 +0000

    fixing pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,4 +25,4 @@ def page_not_found(error):
 if __name__ == '__main__':
     HBNB_API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     HBNB_API_PORT = getenv('HBNB_API_PORT', '5000')
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT,threaded=True)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
