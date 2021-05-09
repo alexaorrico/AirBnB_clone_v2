15f656e551c3e2e4284cc4eb2619f59d9df84063
OyugoObonyo <nyikal1999@gmail.com>
2021-05-09 19:43:26 +0300

    Update: Return error response as json

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,9 +2,9 @@
 """
 Airbnb clone application implementation using flask
 """
+from flask import Flask, jsonify, make_response, Blueprint
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response, Blueprint
 from os import getenv
 
 
@@ -21,10 +21,11 @@ def tear_down(code):
 @app.errorhandler(404)
 def not_found(error):
     """Displays json string in the evnt of a 404 error"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(getenv('HBNB_API_PORT', '5000')),
-            threaded=True)
+            threaded=True,
+            debug=True)
