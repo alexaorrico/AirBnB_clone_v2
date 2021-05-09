6253a2c0cdb6ba387b5ac23cd1548c416b5ed6db
TS-N <2501@holbertonschool.com>
2021-05-09 14:02:01 +0000

    add: CORS specifications task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 from os import getenv
 
 app = Flask(__name__)
+cors = CORS(app, origins='0.0.0.0')
 app.register_blueprint(app_views)
 
 
