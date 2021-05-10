03a87b7dc810dc070552f1ec527eed7df262d9af
Brandixitor <med.salah.bourouba@gmail.com>
2021-05-10 15:17:05 +0100

    Update

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -13,17 +14,30 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column("password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if kwargs.get("password", None):
+                pwd = hashlib.md5(kwargs["password"].encode('utf-8'))
+                kwargs["password"] = pwd.hexdigest()
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """getter for hashed password"""
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """setter for hashed password"""
+        self._password = hashlib.md5(pwd.encode('utf-8')).hexdigest()
