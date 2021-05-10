45f1baf9c11253d375472e8ac239545c46f31d57
Jenni-Foued <medfouedjenni@gmail.com>
2021-05-10 08:45:20 +0100

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,4 +29,4 @@ def page_404(error):
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default='5000')
-    app.run(host=host, port=port, threaded=True,debug=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
