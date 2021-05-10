d63acef622c36d12020b8714d8d02d4a33812c71
Alex Smith <1763@holbertonschool.com>
2021-05-10 00:38:24 +0000

    CORS added for task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
