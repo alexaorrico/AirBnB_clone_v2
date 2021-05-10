fec52c6ac1a9a6410686fad36641a19042a30d12
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-05-10 01:27:09 +0000

    Updating stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 import flask
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -16,13 +22,12 @@ def stats():
     """
     Endpoint that retrieves the number of each objects by type
     """
-    stats = {"amenities": "Amenity",
-             "cities": "City",
-             "places": "Place",
-             "reviews": "Review",
-             "states": "State",
-             "users": "User"}
+    stats = {"amenities": Amenity,
+             "cities": City,
+             "places": Place,
+             "reviews": Review,
+             "states": State,
+             "users": User}
     for key, value in stats.items():
         stats[key] = storage.count(value)
-
     return flask.jsonify(stats)
