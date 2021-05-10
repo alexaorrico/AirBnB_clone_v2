7cf06a3f348d402fbe33da7ca6218de8b778c6f6
JulianArbini97 <1890@holbertonschool.com>
2021-05-10 15:11:54 +0000

    task 15

--- models/base_model.py
+++ models/base_model.py
@@ -68,7 +68,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if getenv("HBNB_TYPE_STORAGE") == "db":
+        if models.storage_t == "db":
             if 'password' in new_dict:
                 del new_dict['password']
         return new_dict
