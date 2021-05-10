8672c918699e94dc990131261a899737d7ab7cc4
Mfeliciano778 <1652@holbertonschool.com>
2021-05-10 23:57:20 +0000

    13 task??

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 '''Returning the status of API'''
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import environ
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
