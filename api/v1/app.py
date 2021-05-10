69d089380cc5d15871192e59d8c0ce7c8fee31eb
root <maxwellloganlovell@gmail.com>
2021-05-10 16:40:10 +0000

    pep8 cities and app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+
 @app.teardown_appcontext
 def remove_session(exception):
     '''removes current Session'''
