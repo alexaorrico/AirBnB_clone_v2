d6e7a92d55178b79693e8c37f4c8e49be1623141
kimengu-david <davidmutune200@gmail.com>
2021-05-10 08:01:06 +0000

    Retrieve No of ojbects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,11 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status', strict_slashes=False)
 def get_status():
     return jsonify({"status": "OK"})
+
 
 @app_views.route("/stats", strict_slashes=False)
 def stats():
