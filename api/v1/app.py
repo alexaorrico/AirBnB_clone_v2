6446ce1a1e168f58aec8da02161db69fc4ab285c
Ruben <ruben.oliveros@gmail.com>
2021-05-10 00:35:37 -0500

    Creating files

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
     port = os.getenv('HBNB_API_PORT', default='5000')
 
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
