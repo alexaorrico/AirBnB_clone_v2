adfe7bde4daec980a9e20da25965ba9da61b31e8
kimengu-david <davidmutune200@gmail.com>
2021-05-10 07:59:18 +0000

    Retrieve No of ojbects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 """Retrieve status code"""
 from flask import Flask, jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def get_status():
