9c76d98767f15afed5a15c43bae75c8777384bee
samuelsrmv <samuel.srmv@gmail.com>
2021-05-10 13:10:42 -0500

    update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
