bf1a12215dc29b124e61c57c4dbb5f238d338d71
kimengu-david <davidmutune200@gmail.com>
2021-05-10 10:28:17 +0000

    Remove typo error

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,4 +9,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.amenities import *
-from api.v1.veews.users import *
+from api.v1.views.users import *
