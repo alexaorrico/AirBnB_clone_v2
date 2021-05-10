f2a5a6026eb638e704d8e6acaebe185209496e1e
JennyHadir <hadirjenny@hotmail.com>
2021-05-10 03:13:17 +0100

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -18,12 +18,12 @@ def states():
         try:
             kwargs = request.get_json()
         except:
-            return {"error": "Not a JSON"}, 400
+            return jsonify({"error": "Not a JSON"}, 400)
         if "name" not in kwargs:
-            return {"error": "Missing name"}, 400
+            return jsonify({"error": "Missing name"}, 400)
         new_state = State(**kwargs)
         new_state.save()
-        return new_state.to_dict(), 201
+        return jsonify(new_state.to_dict(), 201)
 
     elif request.method == 'GET':
         return jsonify([o.to_dict() for o in storage.all("State").values()])
@@ -51,5 +51,5 @@ def states_id(id):
                 if k not in ["id", "created_at", "updated_at"]:
                     setattr(state, k, v)
             state.save()
-            return state.to_dict(), 200
+            return jsonify(state.to_dict(), 200)
     abort(404)
