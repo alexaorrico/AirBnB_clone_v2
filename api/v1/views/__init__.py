56c41ccefa4e57a5c1faf1db27cf349ef1391344
Ethan-23 <emanroberts@gmail.com>
2021-05-10 05:32:09 +0000

    added ammenities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,4 @@ app_views = Blueprint(__name__, 'app_views', url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
