e76e6e5bf8ab4ecc43685b0fac56944679e3523a
Sergio Urrego <2016@holbertonschool.com>
2021-05-10 23:02:50 -0500

    Fix documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,7 @@ from models.user import User
 
 @app_views.route("/status")
 def function_hola():
-    """Function status"""
+    """Function status return OK"""
     return jsonify({"status": "OK"})
 
 
