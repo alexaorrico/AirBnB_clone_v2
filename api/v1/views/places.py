e95e46e1112894edfffc4a1cd18ff4a6072d11a8
Sergio Urrego <2016@holbertonschool.com>
2021-05-10 23:23:30 -0500

    Fix blank spaces for pep8

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """New view for Place objects that handles all"""
+
 from api.v1.views import app_views
 from models import storage
 from flask import Flask, abort, jsonify, make_response
