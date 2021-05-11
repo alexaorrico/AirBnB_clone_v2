42b639317c04ef581ea9b82f6b33d1e8b778ae17
Sergio Urrego <2016@holbertonschool.com>
2021-05-10 22:45:41 -0500

    Fix documentation

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""new view for Amenity objects that handles all"""
+"""New view for Amenity objects that handles all"""
+
 from models.amenity import Amenity
 from flask import Flask, abort, jsonify, make_response
 from flask import request
