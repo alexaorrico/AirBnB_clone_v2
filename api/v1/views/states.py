c9bee6c963c848f76eb9a344b5bd2bd06fe5773c
Sergio Urrego <2016@holbertonschool.com>
2021-05-10 23:54:29 -0500

    Fix documentation

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """new view for State objects that handles all """
+
 from flask import Flask, abort, jsonify, make_response
 from flask import request
 from api.v1.views import app_views
