ffd45d73348426957b26351c312652a8c6155f79
Aleia DeVore <2496@holbertonschool.com>
2021-05-14 16:01:27 +0000

    Amenities popover doesn't work

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" starts Flask app using db """
+
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
-from os import environ
-from flask import Flask, render_template
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
-    storage.close()
 
 
 @app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filter():
-    """ HBNB filters """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
+def hbnbfilters():
+        """ Displays dynamic filters """
+        all_states = storage.all(State).values()
+        all_amenities = storage.all(Amenity).values()
+        return render_template(
+                '10-hbnb_filters.html', states=all_states, amenities=all_amenities)
 
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    return render_template('10-hbnb_filters.html',
-                           states=st_ct,
-                           amenities=amenities)
-
+@app.teardown_appcontext
+def teardown(self):
+        """ Removes current session """
+        storage.close()
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port='5000')
