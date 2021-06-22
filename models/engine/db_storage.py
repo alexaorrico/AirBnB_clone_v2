923f59fa0df575d1ba52d2facb9fae27f1726b36
breezybrow <browder.bree@gmail.com>
2021-06-22 19:31:35 +0000

    Adding new method outlines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,10 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """method to retrieve one object"""
+
+
+    def count(self, cls=None):
+        """method to count the number of objects in storage"""
