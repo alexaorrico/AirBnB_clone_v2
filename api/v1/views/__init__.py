b3f2ccacfacb885828f64eae52e1b44a6ed621ec
Kyle <kyg555@outlook.com>
2021-06-22 20:08:04 +0000

    Create api dir, and necessary files for task 4.

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module instantiates app_views as a Blueprint()"""
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__)
+
+from api.v1.views.index import *
---
+++ api/v1/views/index.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains routes for app_views"""
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    return {'status': 'OK'}
