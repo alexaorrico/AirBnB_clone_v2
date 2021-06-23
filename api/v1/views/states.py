d2eb177073ff3b4a6aa8b7c1f6387fe7c1bec31f
kyle-gross <kyg555@outlook.com>
2021-06-23 21:46:01 +0000

    Update files for task 7.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,28 +5,47 @@ from flask import abort, jsonify, request
 from models.state import State
 from models import storage
 
-states = storage.all(State)
+
+@app_views.route('/states/', methods=['GET', 'POST'])
+def all_states():
+    states = storage.all(State).values()
+    if request.method == 'GET':
+        new_list = []
+        state_list = [state for state in states]
+        for state in state_list:
+            new_list.append(state.to_dict())
+        return jsonify(new_list)
+
+    if request.method == 'POST':
+        if not request.json:
+            abort(400, 'Not a JSON')
+        if not 'name' in request.json:
+            abort(400, 'Missing name')
+        obj = State(**request.get_json())
+        storage.new(obj)
+        storage.save()
+        return jsonify(obj.to_dict()), 201
 
 
-@app_views.route('/states/', methods=['GET'])
-@app_views.route('/states/<string:state_id>', methods=['GET'])
-def get_state(state_id=None):
-    if state_id:
-        state = [state for state in states.values() if state.id == state_id]
-        if len(state) == 0:
-            abort(404)
-        return state[0].to_dict()
-    new_list = []
-    state_list = [state for state in states.values()]
-    for state in state_list:
-        new_list.append(state.to_dict())
-    return jsonify(new_list)
-
-@app_views.route('/states/<string:state_id>', methods=['DELETE'])
-def delete_state(state_id):
-    state = [state for state in states.values() if state.id == state_id]
+@app_views.route('/states/<string:state_id>', methods=['DELETE', 'GET', 'PUT'])
+def one_state(state_id):
+    states = storage.all(State).values()
+    state = [state for state in states if state.id == state_id]
     if len(state) == 0:
         abort(404)
-    state[0].delete()
-#    storage.save()
-    return {}, 200
+    
+    if request.method == 'GET':
+        return state[0].to_dict()
+
+    if request.method == 'PUT':
+        if not request.json:
+            abort(400, 'Not a JSON')
+        for k, v in request.get_json().items():
+            if k != 'id' and k != 'created_at' and k != 'updated_at':
+                setattr(state[0], k, v)
+        return jsonify(state[0].to_dict()), 200
+
+    if request.method == 'DELETE':
+        storage.delete(state[0])
+        storage.save()
+        return {}, 200
