f779854708817d2fcf513ee5d82cfd4a645ee11b
breezybrow <browder.bree@gmail.com>
2021-06-23 15:11:14 +0000

    Creating an endpoint in index.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" An endpoint that retrieves the number of objects by each type """
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
+
+@app_views_route('/status')
+def status():
+    """ Return OK """
+    return jsonify({'status': 'OK'})
+
+
+@app_views_route('/stats')
+def stats():
+    """ Using count() method from storage """
+    obj_dict = {}
+    obj_dict['amenities'] = storage.count(Amenity)
+    obj_dict['states'] = storage.count(State)
+    obj_dict['cities'] = storage.count(City)
+    obj_dict['places'] = storage.count(Place)
+    obj_dict['reviews'] = storage.count(Review)
+    obj_dict['users'] = storage.count(User)
+    return jsonify(obj_dict)
