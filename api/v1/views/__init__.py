2dd9d2adc82bdd1ad0973aa6816853f1487e91ab
Rachel Lewis <RLewis11769@gmail.com>
2021-06-24 18:21:27 +0000

    Add data to amenities.py module

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,5 +6,6 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__)
 
 from api.v1.views.index import *
-from api.v1.views.states import *
+from api.v1.views.amenities import *
 from api.v1.views.cities import *
+from api.v1.views.states import *
---
+++ api/v1/views/amenities.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+""" Module to handle amenities RESTful API """
+
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models import storage
+from models.amenity import Amenity
+
+
+@app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
+def all_amenities():
+    amenities = storage.all(Amenity).values()
+
+    if request.method == 'GET':
+        return jsonify(list(map(lambda x: x.to_dict(), amenities)))
+
+    if request.method == 'POST':
+        if not request.json:
+            abort(400, 'Not a JSON')
+        if 'name' not in request.json:
+            abort(400, 'Missing name')
+        obj = Amenity(**request.get_json())
+        storage.new(obj)
+        storage.save()
+        return jsonify(obj.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE', 'GET', 'PUT'],
+                 strict_slashes=False)
+def one_amenity(amenity_id):
+    amenities = storage.all(Amenity).values()
+    amenity = [amenity for amenity in amenities if amenity.id == amenity_id]
+    if len(amenity) == 0:
+        abort(404)
+
+    if request.method == 'GET':
+        return amenity[0].to_dict()
+
+    if request.method == 'PUT':
+        if not request.json:
+            abort(400, 'Not a JSON')
+        for k, v in request.get_json().items():
+            if k not in ('id', 'created_at', 'updated_at'):
+                setattr(amenity[0], k, v)
+        storage.save()
+        return jsonify(amenity[0].to_dict()), 200
+
+    if request.method == 'DELETE':
+        storage.delete(amenity[0])
+        storage.save()
+        return {}, 200
