d919958dd816e06035b0697eee92b4f46992759b
Christopher <christopher.caswell@rocketmail.com>
2021-06-24 21:36:31 +0000

    doc edits

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,8 @@ from api.v1.views import app_views
 from models import storage
 from flask import Flask
 
-host = "HBNB_API_HOST" or 0.0.0.0 if not in defined
-port = "HBNB_API_PORT" or 5000 if not in defined
+host = 'HBNB_API_HOST' or 0.0.0.0 if not in defined
+port = 'HBNB_API_PORT' or 5000 if not in defined
 
 app = Flask(__name__, instance_relative_config=True)
 app.register_blueprint(app_views)
