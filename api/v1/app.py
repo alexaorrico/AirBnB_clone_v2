b338f15e3bb5820c72e75f49650198f8f3bb5fcc
Christopher <christopher.caswell@rocketmail.com>
2021-06-24 21:02:16 +0000

    init for task 4, added Blueprint, Flask sturcture

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+ #!/usr/bin/python3
+ # unknown comments so far
+
+from api.v1.views import app_views
+from models import storage
+from flask import Flask
+
+host = "HBNB_API_HOST" or 0.0.0.0 if not in defined
+port = "HBNB_API_PORT" or 5000 if not in defined
+
+app = Flask(__name__, instance_relative_config=True)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext 
+def teardown(self):
+    return storage.close()
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True, host=host, port=port)
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# insert vague gesture of commentary here
+
+from flask import Blueprint
+from api.v1.views.index import *
+
+app_views = Blueprint('simple_page', /api/v1/views/Blueprint)
