62f1dd486705545291d660f67db15e22a574f7dc
Miguel22247 <minipachru@gmail.com>
2021-09-16 13:51:54 -0300

    fix imports and pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """hacer comentario"""
+
 from flask import Blueprint
-import api.v1.views.index
+from api.v1.views.index import *
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """returns the status of the API"""
+from flask import Flask, jsonify
 from api.v1.views import app_views
-from flask import jsonify
 
 @app_views.route('/status')
 def api_status():
-	"""returns the status of the API"""
-	return jsonify({'status':'OK'})
+    """returns the status of the API"""
+    return jsonify({'status':'OK'})
