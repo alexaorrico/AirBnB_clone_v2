6bfd7ed30c7d510436a1d2ddd7c841fda886362b
Miguel22247 <minipachru@gmail.com>
2021-09-16 14:22:04 -0300

    remove Flask import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """returns the status of the API"""
-from flask import Flask, jsonify
+from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status')
