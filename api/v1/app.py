4c74bb7a7ebd55354f4167eafd1a16db8c67d720
derpmagician <renzin@gmail.com>
2021-09-16 09:39:52 -0500

    [UPDATE] instructions

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,22 @@
 """ Flask app with cors module   """
 
 from models import storage
+from api.v1.views import app_views
 from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_request(exception=None):
+    """Closes the connection when the execution finish"""
+    storage.close()
+
 
 if __name__ == "__main__":
     """ Flask main function """
+    my_host = environ.get("HBNB_API_HOST")
+    my_port = environ.get("HBNB_API_PORT")
     app.run(debug=True)
