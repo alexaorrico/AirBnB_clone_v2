726b7a48a75c9226f927f319f5a13333c25d73c5
Miguel22247 <minipachru@gmail.com>
2021-09-16 13:58:46 -0300

    change things

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,4 +6,4 @@ from api.v1.views import app_views
 @app_views.route('/status')
 def api_status():
     """returns the status of the API"""
-    return jsonify({'status':'OK'})
+    return jsonify(api_status='OK')
