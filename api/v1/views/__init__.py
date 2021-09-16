58436aeba6f0d1c267241a95d535b261a9c88b3d
Miguel22247 <minipachru@gmail.com>
2021-09-16 13:47:04 -0300

    fix import on __init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """hacer comentario"""
 from flask import Blueprint
-from api.v1.views.index import *
+import api.v1.views.index
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
