b8bec1b69b1d4611ea8a95f5d022be45fc64da7b
Sarolus <cactussius.primus@gmail.com>
2021-09-16 21:52:01 +0200

    Add stats entrypoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,15 @@ from models.state import State
 @app_views.route('/status')
 def index():
     return {'status': 'OK'}
+
+
+@app_views.route('/stats')
+def stats():
+    return {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User),
+    }
