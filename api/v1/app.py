6d10ab3f49194a4c19ae83398402bc05e892579b
Miguel22247 <minipachru@gmail.com>
2021-09-16 15:03:23 -0300

    add 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Contains the FileStorage class
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,16 +11,21 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-	"""Remove the current SQLAlchemy session"""
-	storage.close()
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a error 404"""
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == "__main__":
-	if getenv('HBNB_API_HOST'):
-		host = getenv('HBNB_API_HOST')
-	else:
-		host = '0.0.0.0'
-	if getenv('HBNB_API_PORT'):
-		port = getenv('HBNB_API_PORT')
-	else:
-		port = 5000
-	app.run(host=host, port=port, threaded=True)
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    else:
+        host = '0.0.0.0'
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
