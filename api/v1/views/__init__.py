23494111bbfe13474ef4acdbf23a61d9332cc10c
SimonBr017 <simonbrard017@gmail.com>
2021-09-17 01:29:02 +0200

    add views_states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Blueprint package """
 from flask import Blueprint, render_template
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+from api.v1.views.states import *
 
