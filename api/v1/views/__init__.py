57f81134905a3a1d07bf85f68d44608ae65aed08
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-16 22:14:41 +0300

    Server working

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Blueprint package """
 from flask import Blueprint, render_template
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
