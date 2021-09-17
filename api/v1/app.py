79cca3807d2c4ebf024ca6e148a5d27875493592
tieje <toj320@gmail.com>
2021-09-17 12:16:08 -0400

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,16 +3,24 @@
 starts a Flask web application
 """
 
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
-
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-storage.close()
+def tearthisdown():
+    storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    thisHost = os.getenv('HBNB_API_HOST')
+    thisPort = os.getenv('HBNB_API_PORT')
+    if thisHost is None:
+        thisHost = '0.0.0.0'
+    if thisPort is None:
+        thisPort = '5000'
+    app.run(host=thisHost, port=thisPort, threaded=True)
