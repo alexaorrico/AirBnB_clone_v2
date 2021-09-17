8c0bcdb24a49d14075219ce666d95d9797a38335
Alex Rivera <alexriveracruz4@gmail.com>
2021-09-17 16:19:04 +0000

    [ADD] error handler method

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask application"""
-from flask import Flask
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
