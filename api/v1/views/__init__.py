aa8f1d4e5a04af939d7fed29c199fa6fcd62e87a
daniapm <daniajibellis@hotmail.com>
2021-09-17 17:45:56 -0500

    fix import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,5 @@ script that starts a Flask web application:
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__,
-                      url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *  # noqa
