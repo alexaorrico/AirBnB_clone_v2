483443649ff5794c1072df25c5fc60fe9c9de9e4
Sarolus <cactussius.primus@gmail.com>
2021-09-17 17:33:35 +0200

    Add API 404 error handler
    Co-authored-by: Adrien MILLOT <adrien.millot@gmail.com>

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-from flask import Flask
+import os
+from werkzeug.exceptions import NotFound
+from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(NotFound)
+def handle_not_found(e):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
