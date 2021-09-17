d091c3a03e53228c51ad921c94cca28346a32b29
Orcha02 <orcha2002@gmail.com>
2021-09-17 18:28:16 +0000

    Add jsonify import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''index route for flask app'''
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
