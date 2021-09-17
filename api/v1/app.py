aaa8f893b9eda05c377527640c1a364637f4bac2
Orcha02 <orcha2002@gmail.com>
2021-09-17 18:46:17 +0000

    Fix environment variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from models import storage
 from api.v1.views import app_views
 import flask
+from os import getenv
 
 
 app = flask.Flask(__name__)
@@ -21,6 +22,10 @@ def teardown(self):
     storage.close()
 
 if __name__ == "__main__":
-    HBNB_API_HOST = '0.0.0.0'
-    HBNB_API_PORT = '5000'
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
+    if HBNB_API_HOST is None:
+        HBNB_API_HOST = '0.0.0.0.'
+    if HBNB_API_PORT is None:
+        HBNB_API_PORT = '5000'
     app.run(debug=True, threaded=True, host=HBNB_API_HOST, port=HBNB_API_PORT)
