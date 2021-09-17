f11de57e9e091f1d8600896b80a5e8ff41504563
Vanessa <vagava.123@gmail.com>
2021-09-17 16:12:23 -0500

    update the file storage with methods .get and .conut

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not found"""
+        search_key = cls.__name__ + '.' + str(id)
+        if search_key in self.__objects.keys():
+            return self.__objects[search_key]
+        else:
+            return None
+
+    def count(self, cls=None):
+        """ Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage."""
+        list_to_count = self.all(cls).keys()
+        return len(list_to_count)
