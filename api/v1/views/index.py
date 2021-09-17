5be4d48be7eed8a590b5bf2de4d75d7aee557ac4
DiegoSusviela <dieguitosus@homail.com>
2021-09-17 17:33:18 +0000

    updated index master

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """returns the status of the API"""
+
 import models
 from models import storage
 from models.amenity import Amenity
@@ -18,12 +19,12 @@ def api_status():
     """returns the status of the API"""
     return jsonify(status='OK')
 
-
 @app_views.route('/stats')
 def api_stats():
     """checks the API stats of all classes"""
     classes = {"amenities": "Amenity", "cities": "City",
-               "places": "Place", "reviews": "Review", "states": "State", "users": "User"}
+               "places": "Place", "reviews": "Review",
+               "states": "State", "users": "User"}
     for key in classes:
         classes[key] = storage.count(classes[key])
     return jsonify(classes)
