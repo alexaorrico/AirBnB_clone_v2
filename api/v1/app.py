27eedc1751806d9db0f04f964a6ab592c8107181
EtienneBrJ <etiennebrxv@gmail.com>
2021-09-17 13:55:03 +0200

    Mandats done

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,10 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask
 from flask import Blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
