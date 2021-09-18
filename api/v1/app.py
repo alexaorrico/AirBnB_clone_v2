088ea2a65ba5c699963c417dee0fe7edef6b81b1
philimon-reset <philimonderib2019@gmail.com>
2021-09-19 02:22:49 +0300

    error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """return status of route"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.teardown_appcontext
 def teardown_db(exception):
