715eb09da11aaf6ccaa71e955159b479ec1f91ee
philimon-reset <philimonderib2019@gmail.com>
2021-09-19 02:01:48 +0300

    storage branch adding and task #2 and 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ status of web server """
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 @app_views.route("/status")
 def status():
 	return jsonify({ 'status': 'OK' })
 
+@app_views.route("/api/v1/stats")
+def stats():
+	for clss in storage.classes:
+		task = jsonify({clss : storage.count(storage.classes[clss])})
+	return task
