529cfe020199e582d9e72d9e03d1462a1eb30684
yoyogold-a11 <tmrfack@gmail.com>
2021-09-17 21:17:08 -0500

    fixing basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,12 @@ Contains class BaseModel
 
 from datetime import datetime
 import models
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+from os import getenv
 
-time = "%Y-%m-%dT%H:%M:%S.%f"
+time_fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
 if models.storage_t == "db":
     Base = declarative_base()
