f5d4b4a4aedb7050286bec318296fed49aff85f6
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-09-18 12:29:53 -0500

    changes for change the root

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def close_storage(self):
+def close_storage(error):
   """[close de db call close]
   """
   storage.close()
