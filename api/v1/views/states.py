cdb8dcf4aa09bfcac998e5e2bd6cb52602d46400
daniapm <daniajibellis@hotmail.com>
2021-09-18 13:33:05 -0500

    fix state request

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,13 +6,16 @@ from os import error, getenv
 from models import storage
 from api.v1.views import app_views
 from flask import Blueprint, render_template, abort
-from flask import Flask, jsonify, Response, make_response
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route('/api/v1/states', methods=['GET'])
 def state():
+    """
+    Retrieves the list of all State objects
+    """
     list = []
     for state in storage.all["State"].values():
         list.append(state.to_dict)
@@ -21,26 +24,35 @@ def state():
 
 @app.route('/api/v1/states/<state_id>', methods=['GET'])
 def state_get(state_id):
-    list = []
+    """
+    Retrieves a State object:
+    """
     state = storage.get('State', state_id)
     if state is None:
         abort(404)
-    return state.to_dict()
+    else:
+        return state.to_dict()
 
 
 @app.route('/api/v1/states/<state_id>', methods=['DELETE'])
 def state_delete(state_id):
-    empy = {}
+    """
+    Deletes a State object
+    """
     state = storage.get('State', state_id)
     if state is None:
         abort(404)
-    state.delete()
-    state.save()
-    return jsonify({}), 202
+    else:
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 202
 
 
 @app.route('/api/v1/states', methods=['POST'])
 def state_post():
+    """
+    Creates a State
+    """
     state = request.get_json()
     if state is None:
         abort(404, "Not a JSON")
@@ -48,13 +60,17 @@ def state_post():
     if 'name' not in state:
         abort(404, "Missing name")
     else:
-        state.save()
-        state.new()
-        return (jsonify(state), 201)
+        new = state.State(**state)
+        storage.new(new)
+        storage.save()
+        return jsonify(new.to_dict()), 201
 
 
 @app.route('/api/v1/states/<state_id>', methods=['PUT'])
 def state_put(state_id):
+    """
+    Updates a State object
+    """
     state = request.get_json()
     if state is None:
         abort(404, "Not a JSON")
