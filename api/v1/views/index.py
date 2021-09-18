7f5de458d4e364becb3101e5a83a062e3efcd8ef
SrDri <juanjcarabali@gmail.com>
2021-09-19 02:36:18 +0700

    Fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-<<<<<<< HEAD
 @app_views.route("/status")
-=======
-@app_views.route("/status", strict_slashes=False)
->>>>>>> 09c2ab1f0098ca4fbea9572f4570ebd72ef72184
 def status():
     """Returns status in jason format"""
     return jsonify({'status': 'OK'})
