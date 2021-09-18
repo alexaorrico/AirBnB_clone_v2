c670405a07dd2ee32fc531472037552d339b061d
Louvani <paula.louvani@gmail.com>
2021-09-18 14:59:12 -0500

    Create folder API 4. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''first endpoint (route) will be to return the status of your API'''
+
+from os import getenv
+
+from api.v1.views import app_views
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+if __name__ == '__main__':
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    if HBNB_API_HOST is None:
+        HBNB_API_HOST = '0.0.0.0'
+
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
+    if HBNB_API_PORT is None:
+        HBNB_API_PORT = 5000
+
+    app.run(debug=True, port=HBNB_API_PORT, host=HBNB_API_HOST, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""index"""
+from api.v1.views import app_views
+from flask import Flask, jsonify
+
+@app_views.route('/status')
+def status():
+	return jsonify({"status": "OK"})
