12fcfd82778d21433107275bdd971d9898046e1e
Orcha02 <orcha2002@gmail.com>
2021-09-18 05:22:49 +0000

    pep8 fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,23 +10,23 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
     """return the status"""
     return jsonify({"status": "OK"}), 200
 
+
 @app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def count_stats():
-    """return the stats""" 
-    
-
+    """return the stats"""
     counts = {
-	"amenities": storage.count(Amenity), 
- 	"cities": storage.count(City), 
-  	"places": storage.count(Place), 
-  	"reviews": storage.count(Review), 
-  	"states": storage.count(State), 
-  	"users": storage.count(User)
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
 
     return jsonify(counts), 200
