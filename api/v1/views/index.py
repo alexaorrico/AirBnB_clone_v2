8a8852aad7effe90712831bda02936aaded2f770
AlexisCORAN <alexis_ca_@outlook.com>
2021-09-18 21:16:50 +0000

    Added docstring and code optimization

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+This module contains endpoint(route) status
+"""
 from flask import Flask
 from api.v1.views import app_views
 from flask import jsonify
 app = Flask(__name__)
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
+    """
+    Returns a JSON status
+    """
     return jsonify({"status": "OK"})
