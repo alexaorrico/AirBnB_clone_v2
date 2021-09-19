810db44c6525b20c15fa6745a170dcf651f5dc71
B1ueberry121 <christianlaboy839@gmail.com>
2021-09-19 17:49:35 +0000

    updating tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,15 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """ Test's that get properly gets objects"""
+        first_state_id = list(self.all(State).values())[0].id
+        print("First state: {}".format(self.get(State, first_state_id)))
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """ Test that count returns the correct result"""
+        print("All objects: {}".format(self.count()))
+        print("State objects: {}".format(self.count(State)))
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,17 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """ Test that get properly gets the object """
+        storage = FileStorage()
+        first_state_id = list(storage.all(State).values())[0].id
+        print("First state: {}".format(storage.get(State, first_state_id)))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """ Test's that count returns the correct result """
+        storage = FileStorage()
+        print("All objects: {}".format(storage.count()))
+        print("State objects: {}".format(storage.count(State)))
