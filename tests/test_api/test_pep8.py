e92f4ffcc5d5342f499d562058b22a70709010de
yoyogold-a11 <tmrfack@gmail.com>
2021-09-18 19:20:20 -0500

    adding tests and task5-6

---
+++ tests/test_api/test_pep8.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Contains the TestStateDocs classes
+"""
+
+from datetime import datetime
+import inspect
+import models
+import os
+from models import state
+from models.base_model import BaseModel
+import pep8
+import unittest
+from api.v1 import app
+State = state.State
+
+
+class TestStateDocs(unittest.TestCase):
+    """Tests to check the documentation for all api files"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.state_f = inspect.getmembers(State, inspect.isfunction)
+
+    def test_pep8_conformance_state(self):
+        """Test that models/state.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['api/v1/app.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_state(self):
+        """Test that tests/test_models/test_state.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_api/test_pep8.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_state_module_docstring(self):
+        """Test for the state.py module docstring"""
+        self.assertIsNot(app.__doc__, None,
+                         "state.py needs a docstring")
+        self.assertTrue(len(app.__doc__) >= 1,
+                        "state.py needs a docstring")
+
+    def test_state_class_docstring(self):
+        """Test for the State class docstring"""
+        self.assertIsNot(State.__doc__, None,
+                         "State class needs a docstring")
+        self.assertTrue(len(State.__doc__) >= 1,
+                        "State class needs a docstring")
+
+    def test_state_func_docstrings(self):
+        """Test for the presence of docstrings in State methods"""
+        for func in self.state_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
