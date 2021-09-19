f7305df4e5cc55d017b121bfb0d6d34aa2a486bf
csoriano2832 <2832@holbertonschool.com>
2021-09-19 18:59:23 +0000

    work in progress

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from flask import jsonify, abort
+""" This module handles the HTTP methods of a state object"""
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 from api.v1.views import app_views
@@ -10,14 +11,22 @@ states = []
 for state in all_states.values():
     states.append(state.to_dict())
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
+    """ Handles HTTP request of all the state object """
     return jsonify(states)
+
 
 @app_views.route('/states/<state_id>', methods=['GET', 'DELETE'],
                  strict_slashes=False)
 def get_state(state_id):
+    """ Handles HTTP requests of a single state object """
     state = [state for state in states if state['id'] == state_id]
     if len(state) == 0:
         abort(404)
-    return jsonify(state[0])
+    if request.method == 'DELETE':
+        storage.delete(state)
+        storage.save()
+        return jsonify({}), 200
+    return jsonify(state)
