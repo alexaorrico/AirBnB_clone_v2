1a558d0221309dd281023172918b0233792eada2
kiminzajnr <kiminzaeric@gmail.com>
2021-09-19 11:35:48 +0000

    return number of object by each type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python3
 """this module has route /status
 """
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
 from flask import Flask, jsonify
 from api.v1.views import app_views
+
+
+amenities_ = storage.count(Amenity)
+cities_ = storage.count(City)
+places_ = storage.count(Place)
+reviews_ = storage.count(Review)
+states_ = storage.count(State)
+users_ = storage.count(User)
 
 
 @app_views.route('/status', strict_slashes=False)
 def return_json():
     """return a JSON: 'status': 'OK'"""
     data = {"status": "OK"}
+    return jsonify(data)
+
+
+@app_views.route('/stats', strict_slashes=False)
+def obj_no():
+    """retrieves the number of each objects by type"""
+    data = {"amenities": amenities_,
+            "cities": cities_,
+            "places": places_,
+            "reviews": reviews_,
+            "states": states_,
+            "users": users_
+            }
     return jsonify(data)
