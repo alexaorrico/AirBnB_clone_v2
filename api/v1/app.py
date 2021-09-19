4a2c269cba8a7d25a96124d96910d22c2cc78d83
gavazcal <2392@holbertonschool.com>
2021-09-19 20:25:11 +0000

    debugging CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
