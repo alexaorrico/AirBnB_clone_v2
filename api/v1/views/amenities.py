b25e4356ff27cac58232560641fa9bda76a5cd9b
FRANK-GRIJALBA <fjgrijalba@gmail.com>
2021-09-19 15:06:34 -0500

    Fix the codes in return from user.py and amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -30,7 +30,7 @@ def delete_amenity(amenity_id):
         abort(404)
     storage.delete(amenity)
     storage.save()
-    return (jsonify({}))
+    return (jsonify({})), 200
 
 
 @app_views.route('/amenities', methods=["POST"], strict_slashes=False)
--- api/v1/views/user.py
+++ api/v1/views/user.py
@@ -30,7 +30,7 @@ def delete_user(user_id):
         abort(404)
     storage.delete(user)
     storage.save()
-    return (jsonify({}))
+    return (jsonify({})), 200
 
 
 @app_views.route('/users', methods=["POST"], strict_slashes=False)
