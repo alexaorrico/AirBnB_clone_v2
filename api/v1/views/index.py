0953a574e6618b31b31fae28e623a4ccbab684dc
Vanessa <vagava.123@gmail.com>
2021-09-19 12:52:18 -0500

    modify the format of response of json

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,9 +18,9 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
 	return jsonify(
-		{"amenities": "{}".format(storage.count(Amenity)),
-		"cities": "{}".format(storage.count(City)),
-		"places": "{}".format(storage.count(Place)),
-		"reviews": "{}".format(storage.count(Review)),
-		"states": "{}".format(storage.count(State)),
-		"users": "{}".format(storage.count(User()))})
+		{"amenities": storage.count(Amenity),
+		"cities": storage.count(City),
+		"places": storage.count(Place),
+		"reviews": storage.count(Review),
+		"states": storage.count(State),
+		"users": storage.count(User)})
