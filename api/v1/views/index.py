c00a7f88170ced5a31867f1c4eb298f1887acd2c
Thegermanblob <thegermanblob@gmail.com>
2021-09-18 20:10:08 -0400

    Add stats to api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """Index for our web flask"""
+from re import S, U
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -12,3 +19,11 @@ def status():
     """
     bob = {'status': 'OK'}
     return jsonify(bob)
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ Returns json of stats of number of objs """
+    result = {"ammenities": storage.count(Amenity), "cities": storage.count(City),
+              "places": storage.count(Place), "reviews": storage.count(Review),
+              "states": storage.count(State), "users": storage.count(User)}
+    return jsonify(result)
