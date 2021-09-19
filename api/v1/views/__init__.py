9acb8238ffd07eed70ad10ebe699787698f6d006
kiminzajnr <kiminzaeric@gmail.com>
2021-09-19 09:36:42 +0000

    fix import errror

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """this module creates app_views which is an instance of Blueprint
 """
-from api.v1.views.index import *
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__)
+
+from api.v1.views.index import *
