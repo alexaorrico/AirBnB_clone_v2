136316b8f87c7cb614c2ed48bf97fe236989a10e
Jose Vazquez <vazqjose@gmail.com>
2021-09-19 04:15:17 +0000

    Fix file structure

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_close(msg=None):
     '''
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,9 +7,10 @@ Index file
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 
+
 @app_views.route('/status', strict_slashes=False)
 def appStatus():
     ''' Verifies status '''
 
-    status = { "status": "OK" }
+    status = {"status": "OK"}
     return jsonify(status)
