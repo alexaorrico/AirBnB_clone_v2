df22fc0e56926da76aca364df925605716fd3e04
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-09-19 18:46:18 +0000

    creating main flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
-"""return status of route"""
-from flask import Flask, make_response, jsonify
-from models import storage
+"""
+return the status of your API for AirBnB_clone
+"""
+
+import os
+from flask_cors import CORS
+from flask import Flask, jsonify, Response
 from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(self):
+    ''' to teardown the app '''
     storage.close()
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, threaded=True)
+@app.errorhandler(404)
+def page_not_found(e):
+    ''' 404 errors definition '''
+    return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
+        host = '0.0.0.0'
+
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
+        host = '5000'
+
+    app.run(host=host, port=port)
