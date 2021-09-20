5099209cc6a5667a51be9e46fec6dc575dca7bf1
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-20 16:11:48 +0300

    fix code error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Index"""
-from models import storage
+""" View index:
+    endpoint that retrieves
+    number of objects by type"""
+from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
-from flask import jsonify
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -12,21 +14,26 @@ from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
-def status():
-    """ Status of API """
-    ok_status = {"status": "OK"}
-    return jsonify(ok_status)
+def _status():
+    """returns a JSON file with Status: OK"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def stats():
-    """
-        return dict count of data
-    """
-    my_dict = {"amenities": storage.count(Amenity),
-               "cities": storage.count(City),
-               "places": storage.count(Place),
-               "reviews": storage.count(Review),
-               "states": storage.count(State),
-               "users": storage.count(User)}
-    return jsonify(my_dict)
+    """retrieves the number of each objects by type"""
+    my_amenity = storage.count("Amenity")
+    my_cities = storage.count("City")
+    my_places = storage.count("Place")
+    my_reviews = storage.count("Review")
+    my_states = storage.count("State")
+    my_users = storage.count("User")
+    return jsonify(amenities=my_amenity,
+                   cities=my_cities,
+                   places=my_places,
+                   reviews=my_reviews,
+                   states=my_states,
+                   users=my_users)
+
+if __name__ == "__main__":
+    pass
