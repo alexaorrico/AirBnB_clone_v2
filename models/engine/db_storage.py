1c065b68b964e1835b458177483982eef413a4f9
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-20 14:22:41 -0300

    udTED DBSORAGE AND PLACES, ETC.PY

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,15 +76,22 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """gets the object on id"""
-        if cls and id:
-            inst = cls.__name__ + "." + id
-            obj = self.all(cls)
-            for key in obj:
-                if key == inst:
-                    return obk[key]
-        return None
+        """obj"""
+        if cls is not None and type(cls) is str and id is not None and\
+           type(id) is str and cls in name2class:
+            cls = name2class[cls]
+            result = self.__session.query(cls).filter(cls.id == id).first()
+            return result
+        else:
+            return None
 
     def count(self, cls=None):
-        """count of objects"""
-        return len(self.all(cls))
+        """obj in stor"""
+        total = 0
+        if type(cls) == str and cls in name2class:
+            cls = name2class[cls]
+            total = self.__session.query(cls).count()
+        elif cls is None:
+            for cls in name2class.values():
+                total += self.__session.query(cls).count()
+        return total
