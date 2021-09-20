d0fbf2343ee76d4bbab1b527b03ac3b6eaa46375
SimonBr017 <simonbrard017@gmail.com>
2021-09-20 13:55:42 +0200

    pep8 app

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,16 +11,19 @@ from flask import Blueprint
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
-    """handler for 404 errors that returns a 
+    """handler for 404 errors that returns a
     JSON-formatted 404 status code response"""
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
