58b08e0fe9f6393c048f1e8336af67745fad43f2
yoyogold-a11 <tmrfack@gmail.com>
2021-09-19 20:25:59 -0500

    testing

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -46,36 +46,37 @@ def del_review(review_id):
                  strict_slashes=False)
 def create_obj_review(place_id):
     """ create new instance """
-    Place = storage.get(Place, place_id)
-    if Place is None:
+    place = storage.get(Place, place_id)
+    if place is None:
         abort(404)
     if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    if 'user_id' not in request.get_json():
-        return make_response(jsonify({"error": "Missing user_id"}), 400)
-    if 'text' not in request.get_json():
-        return make_response(jsonify({"error": "Missing text"}), 400)
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'user_id' not in kwargs:
+        return make_response(jsonify({'error': 'Missing user_id'}), 400)
     kwargs = request.get_json()
-    kwargs['place_id'] = place_id
     user = storage.get(User, kwargs['user_id'])
     if user is None:
         abort(404)
-    obj = Review(**kwargs)
-    obj.save()
-    return (jsonify(obj.to_dict()), 201)
+    if 'text' not in kwargs:
+        return make_response(jsonify({'error': 'Missing text'}), 400)
+    kwargs['place_id'] = place_id
+    review = Review(**kwargs)
+    review.save()
+    return (jsonify(review.to_dict()), 201)
 
 
 @app_views.route('/reviews/<string:review_id>', methods=['PUT'],
                  strict_slashes=False)
 def post_review(review_id):
-    """  """
-    if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    obj = storage.get(Review, review_id)
-    if obj is None:
+    """ update review """
+    review = storage.get("Review", review_id)
+    if review is None:
         abort(404)
-    for key, value in request.get_json().items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at', 'updated']:
-            setattr(obj, key, value)
-    storage.save()
-    return jsonify(obj.to_dict())
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    for attr, val in request.get_json().items():
+        if attr not in ['id', 'user_id', 'place_id',
+                        'created_at', 'updated_at']:
+            setattr(review, attr, val)
+    review.save()
+    return jsonify(review.to_dict())
