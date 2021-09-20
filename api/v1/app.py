007a9318791df4f23a71d591fdc329b2260bccf3
Miguel22247 <minipachru@gmail.com>
2021-09-20 11:21:22 -0300

    fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Remove the current SQLAlchemy session"""
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
