a850687474a07f25ebfe29ac674a10b3843ad4c3
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-09-20 18:17:45 -0500

    review

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,3 +11,4 @@ if app_views is not None:
     from api.v1.views.states import *
     from api.v1.views.users import *
     from api.v1.views.places import *
+    from api.v1.views.places_reviews import * 
--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-User for API.
+Review for API.
 """
 from flask import abort, request, jsonify, make_response
 from api.v1.views import app_views
