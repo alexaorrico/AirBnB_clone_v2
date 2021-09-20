dd954b0940c7ca4b6c176748f1eec1396e30e681
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-09-20 18:30:44 -0500

    init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,4 +11,3 @@ if app_views is not None:
     from api.v1.views.states import *
     from api.v1.views.users import *
     from api.v1.views.places import *
-    from api.v1.views.places_reviews import * 
