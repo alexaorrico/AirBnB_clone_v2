2eb78d69449bbb342f35af345921ed45523a04d9
derpmagician <renzin@gmail.com>
2021-09-20 10:19:02 -0500

    [UPDATE] defs

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,7 +3,7 @@
 
 import models
 from flask import jsonify, abort
-from flask import request as req
+from flask import request
 from models.amenity import Amenity
 from api.v1.views import app_views
 
@@ -11,13 +11,13 @@ from api.v1.views import app_views
 @app_views.route('/amenities', methods=['GET', 'POST'])
 def amenity_objects():
     """Returns amenity objects as JSON response"""
-    if req.method == 'GET':
+    if method == 'GET':
         amenities = models.storage.all('Amenity')
         amenities = [am.to_dict() for am in amenities.values()]
         return jsonify(amenities)
 
-    if req.method == 'POST':
-        body = req.get_json()
+    if method == 'POST':
+        body = get_json()
         if body is None:
             abort(400, 'Not a JSON')
         if body.get('name', None) is None:
@@ -34,11 +34,11 @@ def amenity_res(amenity_id):
     if amenity is None:
         abort(404)
 
-    if req.method == 'GET':
+    if method == 'GET':
         return jsonify(amenity.to_dict())
 
-    if req.method == 'PUT':
-        amenity_json = req.get_json()
+    if method == 'PUT':
+        amenity_json = get_json()
         if amenity_json is None:
             abort(400, 'Not a JSON')
         ignore = ['id', 'created_at', 'updated_at']
@@ -48,7 +48,7 @@ def amenity_res(amenity_id):
         models.storage.save()
         return jsonify(amenity.to_dict())
 
-    if req.method == 'DELETE':
+    if method == 'DELETE':
         amenity.delete()
         models.storage.save()
         return jsonify({})
