ac40d084f67253d4ea104bbc3c89dc7e51072cf0
DiegoSusviela <dieguitosus@hotmail.com>
2021-09-20 17:53:27 -0300

    added place ameniies

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""creates a new view for State Objects"""
+
+from flask import Flask, jsonify, abort, request
+from models import storage
+from api.v1.views import app_views
+from models.amenity import Amenity
+from models.place import Place
+import os
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
+def get_amenity_for_place(place_id=None):
+    """get place information for all places in a specified city"""
+    obj = storage.get(Place, place_id)
+    if obj is None:
+        return jsonify({}), 404
+    lis = []
+    for pos in obj.amenities:
+        lis.append(pos.to_dict())
+    return jsonify(lis), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_amenity_to_place(place_id=None, amenity_id=None):
+    """deletes"""
+    obj = storage.get(Place, place_id)
+    if obj is None:
+        abort(404)
+    lis = storage.get(Amenity, amenity_id)
+    if lis is None:
+        abort(404)
+    if lis not in obj.amenities:
+        abort(404)
+    else:
+        storage.delete(lis)
+        storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
+                 strict_slashes=False)
+def post_amenity_to_place(place_id=None, amenity_id=None):
+    """linker"""
+    obj = storage.get(Place, place_id)
+    if obj is None:
+        abort(404)
+    lis = storage.get(Amenity, amenity_id)
+    if lis is None:
+        abort(404)
+    amenity_ids = []
+    for amenity in obj.amenities:
+        if amenity.id == lis.id:
+            return jsonify(amenity_object.to_dict()), 200
+    obj.amenities.append(lis)
+    storage.save()
+    return jsonify(amenity_object.to_dict()), 201
