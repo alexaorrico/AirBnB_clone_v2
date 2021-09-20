d28b51aba3c7727be22be6b3cfbe939d5e26ee6d
derpmagician <renzin@gmail.com>
2021-09-20 10:46:02 -0500

    [UPDATE] defs

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -12,11 +12,9 @@ from api.v1.views import app_views
 def amenityAll():
     """Retrieves all amenities with a list of objects"""
     if req.method == 'GET':
-        ll = []
-        s = storage.all('Amenity').values()
-        for v in s:
-            ll.append(v.to_dict())
-        return jsonify(ll)
+        amenities = models.storage.all('Amenity')
+        amenities = [am.to_dict() for am in amenities.values()]
+        return jsonify(amenities)
 
     if req.method == 'POST':
         body = req.get_json()
