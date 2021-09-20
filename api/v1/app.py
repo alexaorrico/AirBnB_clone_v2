f42d816b6aad2f6a9fc32648eef2bb391ebefd42
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-20 14:11:18 +0100

    Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from flask import make_response
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+@app.teardown_appcontext
+def close_storage(self):
+    """"""
+    self.storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', threaded=True)
