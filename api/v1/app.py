e6351a1ed89428c9259b61bcea95ff8d3a8c694b
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-09-20 12:36:18 +0300

    Implement CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """module containing main app"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
+CORS(app, resources = r"/*", origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
