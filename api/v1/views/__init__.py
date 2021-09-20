e4d780e41b627d1a420d85b36deef33ab038fa4d
SrDri <juanjcarabali@gmail.com>
2021-09-21 05:46:21 +0700

    Update project

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,4 @@ if app_views is not None:
     from api.v1.views.amenities import *
     from api.v1.views.states import *
     from api.v1.views.users import *
+    from api.v1.views.places import *
