94939250a1cd931beb5fdf4bc121e633d01978da
livejo <yoseph.kefale@gmail.com>
2021-09-20 13:37:45 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""Index file"""
-from models import storage
+'''
+index page for flask
+displays status and stats
+'''
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
-from flask import jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """ Status of API """
-    ok_status = {"status": "OK"}
-    return jsonify(ok_status)
+@app_views.route('/status')
+def getStatus():
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats')
-def stats():
-    """
-        return dict count of data
-    """
-    my_dict = {"amenities": storage.count(Amenity),
-               "cities": storage.count(City),
-               "places": storage.count(Place),
-               "reviews": storage.count(Review),
-               "states": storage.count(State),
-               "users": storage.count(User)}
-    return jsonify(my_dict)
+def getCount():
+    count_dict = {"amenities": 'Amenity',
+                  "cities": 'City',
+                  "places": 'Place',
+                  "reviews": 'Review',
+                  "states": 'State',
+                  "users": 'User'}
+
+    for k in count_dict.keys():
+        count_dict[k] = storage.count(count_dict.get(k))
+    return jsonify(count_dict)
