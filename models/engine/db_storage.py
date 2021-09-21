726b863da5e6181b465b8cf89b6159cd3b754322
luismelendez94 <2709@holbertonschool.com>
2021-09-21 21:10:27 +0000

    Fixes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,19 +77,19 @@ class DBStorage:
 
     def get(self, cls, id):
         """retrieve one object"""
-        allObjs = self.__session.query(classes[cls]).all()
-        for obj in allObjs:
-            if obj.__class__.__name__ == cls and obj.id == id:
-                return obj
+        obj = self.__session.query(cls).get(id)
+
+        if obj:
+            return obj
         return None
 
     def count(self, cls=None):
         """count the number of objects in storage"""
         countObj = 0
         if cls is None:
-            for value in classes.values():
-                countObj += self.__session.query(value).count()
+            for value in classes.keys():
+                countObj += self.__session.query(classes[value]).count()
         else:
             if cls in classes:
-                countObj += self.__session.query(classes[cls]).count()
+                countObj += self.__session.query(cls).count()
         return countObj
