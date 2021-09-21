d2c2f7d95efbec74ac84f658c727a3f5cba6dc65
Edheramirez <edher.ramirez@outlook.es>
2021-09-21 18:31:27 -0500

    fix create flask server

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,10 +25,7 @@ def close_teardown(error):
 
 
 if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = 5000
-    if env("HBNB_API_HOST"):
-        host = env("HBNB_API_HOST")
-    if env("HBNB_API_PORT"):
-        port = int(env("HBNB_API_PORT"))
+
+    host = env("HBNB_API_HOST", default="0.0.0.0")
+    port = env("HBNB_API_PORT", default=5000)
     app.run(host=host, port=port, threaded=True)
