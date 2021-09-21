c835b460018f11f5b72a61eb416c0292caa9c66c
csoriano2832 <2832@holbertonschool.com>
2021-09-21 00:18:36 +0000

    add state view with http methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -12,21 +12,44 @@ for state in all_states.values():
     states.append(state.to_dict())
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
 def get_states():
     """ Handles HTTP request of all the state object """
+
+    if request.method == 'POST':
+        data = request.get_json()
+        if not data:
+            abort(400, 'Not a JSON')
+        if not 'name' in data:
+            abort(400, 'Missing name')
+        state = State(name=data.get('name'))
+        state.save()
+        return jsonify(state.to_dict()), 201
+
     return jsonify(states)
 
 
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE'],
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def get_state(state_id):
+def get_state(state_id=None):
     """ Handles HTTP requests of a single state object """
-    state = [state for state in states if state['id'] == state_id]
-    if len(state) == 0:
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
+
     if request.method == 'DELETE':
         storage.delete(state)
         storage.save()
         return jsonify({}), 200
-    return jsonify(state)
+
+    if request.method == 'PUT':
+        data = request.get_json()
+        if not data:
+            abort(400, 'Not a JSON')
+        data['id'] = state.id
+        data['created_at'] = state.created_at
+        state.__init__(**data)
+        state.save()
+        return jsonify(state.to_dict()), 200
+
+    return jsonify(state.to_dict())
