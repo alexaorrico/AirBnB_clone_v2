c958f17c206c20b894b330b0a5c1dabd2a5ec905
B1ueberry121 <christianlaboy839@gmail.com>
2021-09-21 20:04:54 +0000

    change

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-""" Index for our web flask """
-from api.v1.views import app_views
+"""Index for our web flask"""
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """ Return json status of web flask """
-    return jsonify({'status': 'OK'})
+    """
+    Return json status of web flask
+    """
+    bob = {'status': 'OK'}
+    return jsonify(bob)
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ Returns json of stats of number of objs """
-    return jsonify({'amenities': storage.vount(Amenity)})
+    result = {"ammenities": storage.count(Amenity),
+              "cities": storage.count(City), "places": storage.count(Place),
+              "reviews": storage.count(Review), "states": storage.count(State),
+              "users": storage.count(User)}
+    return jsonify(result)
