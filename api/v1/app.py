a6487e6dc669bdd1d742ff9a039b8ed56eefc139
B1ueberry121 <christianlaboy839@gmail.com>
2021-09-21 19:44:06 +0000

    upd

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Api for AirBnB_clone
-"""
+""" Api for AirBnB_clone """
 from api.v1.views import app_views
 from flask import Blueprint, Flask, jsonify
 from models import storage
@@ -13,29 +11,11 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception=None):
-    """
-    Application teardown
-    """
+    """ Application teardown """
     storage.close()
 
-
-@app.errorhandler(404)
-def page_not_found(exception=None):
-    """
-    Display an jsonify error
-    """
-    return jsonify({"error": "Not found"}), 404
-
 if __name__ == '__main__':
-    """
-    Execute api
-    """
-    host = getenv('HBNB_API_HOST')
-    if (host is None):
-        host = '0.0.0.0'
-
-    port = getenv('HBNB_API_PORT')
-    if (port is None):
-        port = '5000'
-
+    """ Execute api """
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host, port, threaded=True)
