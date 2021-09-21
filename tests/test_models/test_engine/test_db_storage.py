b552eaba70000f0ee2f3c1d7eb84795826948588
Miguel22247 <minipachru@gmail.com>
2021-09-21 08:46:37 -0300

    add tests for DB storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,25 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """Test method get that retrives an object"""
+        storage = DBStorage()
+        new_obj = State()
+        new_obj.save()
+        new_obj_get = storage.get(State, new_obj.id)
+        self.assertEqual(new_obj_get, new_obj)
+        self.assertEqual(storage.get(State, None), None)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """Test method count that counts number of objects"""
+        storage = DBStorage()
+        obj_count = models.storage.count()
+        state_count = models.storage.count(State)
+        new_obj = State()
+        new_obj.save()
+        self.assertEqual(obj_count + 1, storage.count())
+        self.assertEqual(storage.count(State), state_count + 1)
+        self.assertEqual(storage.count("Not_an_object"), None)
