65e87bb976514676382700e5102ea7ecb5529ddf
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-25 10:56:23 -0700

    fix code error

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -39,7 +39,7 @@ def hbnb():
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=str(uuid.uuid4()))
+                           cache_id=str(uuid4()))
 
 
 if __name__ == "__main__":
