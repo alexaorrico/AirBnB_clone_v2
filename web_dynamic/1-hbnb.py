19ba2163a960ab96571bbe4236cbb5eaf5ab98bf
Kalkidan Demes <kalkidandemes99@gmail.com>
2021-09-25 12:05:21 -0700

    change html page

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -35,7 +35,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
