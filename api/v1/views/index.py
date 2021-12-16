deea828f70506f8e1e7953876980fee6d65c1d3c
lyndonpett <lyndonpett98@gmail.com>
2021-12-16 10:48:46 -0600

    Adding task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,12 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
@@ -12,3 +18,14 @@ def app_viewsRoute():
     '''doing json things for status'''
     jStatus = {'status': 'OK'}
     return jsonify(jStatus)
+
+
+@app_views.route('/stats')
+def stats():
+    ''' returns json of object counts'''
+    return jsonify(amenities=storage.count(Amenity),
+                   cities=storage.count(City),
+                   places=storage.count(Place),
+                   reviews=storage.count(Review),
+                   states=storage.count(State),
+                   users=storage.count(User))
