d0aaa332d55ee7856322a9af914411868b2a1e75
ttkeele <3264@holbertonschool.com>
2021-12-16 20:26:35 +0000

    more imports and starting flask

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,3 +4,10 @@ the app module
 """
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
