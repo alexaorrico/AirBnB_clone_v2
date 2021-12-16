f67d2fb86c22ee38f75180c9bf58ffdda9b598c6
Ronroeandassociates <3321@holbertonschool.com>
2021-12-16 11:08:28 -0600

    Update DBStorage and FileStorage, adding two new methods. All changes should be done in the branch storage_get_count:
    A method to retrieve one object:
    
    Prototype: def get(self, cls, id):
    cls: class
    id: string representing the object ID
    Returns the object based on the class and its ID, or None if not found
    A method to count the number of objects in storage:
    
    Prototype: def count(self, cls=None):
    cls: class (optional)
    Returns the number of objects in storage matching the given class. If no class is passed, returns the count of all objects in storage.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,21 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Returns the object
+        """
+        obj = self.__session.query(cls).get(id)
+        if obj is None:
+            return None
+        return obj
+
+    def count(self, cls=None):
+        """
+        count the number of objects in storage
+        """
+        objs = self.all(cls)
+        return (len(objs))
+
+        return count
