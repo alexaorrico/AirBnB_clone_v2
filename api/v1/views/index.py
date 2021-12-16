d4515bf6cfe62e26617d0307a63aceaab03b1e20
Scatmandu <mcclur05@nsuok.edu>
2021-12-16 14:27:00 -0600

    populated index.py with content to satisfy the requirements for task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""index"""
+
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def status_ok():
+    """ returns JSON string "status": "OK" """
+    return jsonify(status="OK")
