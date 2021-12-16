4c4c4fe6b2064c68d31897967b1ddbcf215360ba
lyndonpett <lyndonpett98@gmail.com>
2021-12-16 10:57:04 -0600

    Adding task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 ''' Starting flask! '''
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,12 @@ app.register_blueprint(app_views)
 def teardown(context):
     ''' Calling close for app '''
     storage.close()
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    ''' Handles 404 error page'''
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
