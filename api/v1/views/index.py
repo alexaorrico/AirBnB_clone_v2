9a39ec575a07b0a23da7102c9622546ecaf9d515
ttkeele <3264@holbertonschool.com>
2021-12-17 23:11:35 +0000

    added count(), made executable

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,16 @@ def status():
     returns json string
     """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", methods=["GET"])
+def stats_class_count():
+    """Retrieves the number of each object by type"""
+    dic = {
+            'amenities': storage.count('Amenity'),
+            'cities': storage.count('City'),
+            'places': storage.count('Place'),
+            'reviews': storage.count('Review'),
+            'states': storage.count('State'),
+            'users': storage.count('User')
+          }
+    return jsonify(dic)
