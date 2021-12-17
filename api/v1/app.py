ca05cb3a47b40d1c14882c5461f0312d6bc4ab94
mutantEris <mmires@live.com>
2021-12-17 12:35:19 -0800

    Pepper fixes all

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,11 +9,11 @@ from flask_cors import CORS
 from os import getenv
 
 
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def close_storage(exception):
@@ -21,12 +21,13 @@ def close_storage(exception):
     calls storage.close() method
     """
     storage.close()
-    
+
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST"), port=getenv("HBNB_API_PORT"),
             threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,6 @@ initializes views package
 """
 
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -12,3 +11,6 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
