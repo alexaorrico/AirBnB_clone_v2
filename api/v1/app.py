a820d0e43fc518b3f6cc053c023cb15ad1573abe
ttkeele <3264@holbertonschool.com>
2021-12-17 23:19:19 +0000

    fixed for task 4 made executable

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,14 +5,11 @@ the app module
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from os import getenv
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
