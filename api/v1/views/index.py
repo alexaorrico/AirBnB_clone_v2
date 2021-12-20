e0616f6ef6048f9ac0549f769de9e1fc2d39450e
ttkeele <3264@holbertonschool.com>
2021-12-20 17:13:27 +0000

    changed return list

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,12 +6,6 @@ index module
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route("/status", methods=["GET"])
@@ -25,12 +19,9 @@ def status():
 @app_views.route("/stats", methods=["GET"])
 def stats():
     """Retrieves the number of each object by type"""
-    dic = {
-            'amenities': storage.count('Amenity'),
-            'cities': storage.count('City'),
-            'places': storage.count('Place'),
-            'reviews': storage.count('Review'),
-            'states': storage.count('State'),
-            'users': storage.count('User')
-          }
-    return jsonify(dic)
+      return jsonify(amenities=storage.count("Amenity"),
+                     cities=storage.count("City"),
+                     places=storage.count("Place"),
+                     reviews=storage.count("Review"),
+                     states=storage.count("State"),
+                     users=storage.count("User"))
