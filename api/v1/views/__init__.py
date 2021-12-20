e4924a536a952cb9278daa87dfe663131b70244a
Ronroeandassociates <3321@holbertonschool.com>
2021-12-20 12:07:48 -0600

    fixed a couple of things

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,31 @@
-#!/usr/bin/pytho
-"""Blueprint for API"""
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+#!/usr/bin/python3
+''' Creating Flask App route'''
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.users import *
-from api.v1.views.amenities import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
+
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status')
+def app_viewsRoute():
+    '''doing json things for status'''
+    jStatus = {'status': 'OK'}
+    return jsonify(jStatus)
+
+
+@app_views.route('/stats')
+def stats():
+    ''' returns json of object counts'''
+    return jsonify(amenities=storage.count(Amenity),
+                   cities=storage.count(City),
+                   places=storage.count(Place),
+                   reviews=storage.count(Review),
+                   states=storage.count(State),
+                   users=storage.count(User))
