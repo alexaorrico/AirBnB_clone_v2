84ac7862d53718057f99ee014b3b97013e2719f5
Ronroeandassociates <3321@holbertonschool.com>
2021-12-20 12:07:18 -0600

    fixed a couple of items

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
-"""connecting to flask"""
+''' Starting flask! '''
 
 
-from flask import Flask, Blueprint
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,22 +12,17 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def ap_te_do(self):
-    """storage close"""
+def teardown(context):
+    ''' Calling close for app '''
     storage.close()
 
 
 @app.errorhandler(404)
-def 404(e):
-    """json 404"""
-    return jsonify(error="Not found"), 404
+def pageNotFound(error):
+    ''' Handles 404 error page'''
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST")
-    port = getenv("HBNB_API_PORT")
-    if host is None:
-        host = "0.0.0.0"
-    if port is None:
-        port = "5000"
-    app.run(host, port, threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'), port=int(
+        getenv('HBNB_API_PORT', '5000')), threaded=True)
