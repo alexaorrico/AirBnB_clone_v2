aa5b3d21c579bb743db9818b31600695e720f234
ttkeele <3264@holbertonschool.com>
2021-12-20 17:29:53 +0000

    try this again...

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,10 +18,12 @@ def status():
 
 @app_views.route("/stats", methods=["GET"])
 def stats():
-    """Retrieves the number of each object by type"""
-      return jsonify(amenities=storage.count("Amenity"),
-                     cities=storage.count("City"),
-                     places=storage.count("Place"),
-                     reviews=storage.count("Review"),
-                     states=storage.count("State"),
-                     users=storage.count("User"))
+    """
+    Retrieves the number of each object by type
+    """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
