f135a0351c9819d10a57b92d17624a7a9fe7c318
ttkeele <3264@holbertonschool.com>
2021-12-20 14:36:25 +0000

    added imports!

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,12 @@ index module
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", methods=["GET"])
@@ -14,6 +20,7 @@ def status():
     status of api
     """
     return jsonify({"status": "OK"})
+
 
 @app_views.route("/stats", methods=["GET"])
 def stats():
