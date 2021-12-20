c13d9e4e23cfbcbe4ca0c06fcff781f97dea521c
ttkeele <3264@holbertonschool.com>
2021-12-20 20:23:04 +0000

    pushed for checker, may need fixing

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -36,3 +36,36 @@ def delete_city(city_id):
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
+
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
+def create_city(state_id):
+    state = storage.get(State, state_id)
+    city_dict = request.get_json()
+    if not state:
+        abort(404)
+    elif not city_dict:
+        abort(400, "Missing name")
+    else:
+        city = City(**city_dict)
+        city.state_id = state.id
+        storage.new(city)
+        storage.save()
+        return jsonify(city.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>',
+                 methods=['PUT'], strict_slashes=False)
+def get_city(city_id):
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    city_dict = request.get_json()
+    if not city_dict:
+        abort(400, "Not a JSON")
+    for key in city_dict:
+        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
+            setattr(city, key, city_dict[key])
+    city.save()
+    return jsonify(city.to_dict()), 200
