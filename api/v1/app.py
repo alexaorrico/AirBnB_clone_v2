a5ce278ec4a6f398b6ab45cf19b5672ffe7b0c5d
1107tomkim <cryptotaekim@gmail.com>
2021-12-21 19:10:39 +0000

    empty init file and app.py file added

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Script checking status of API"""
+
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(self):
+    """Call on storage.close that handles teardowns"""
+    storage.close()
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST")
+    """if host == None:
+        host = 0.0.0.0"""
+    port = getenv("HBNB_API_PORT")
+    """if port == None:
+        port = 5000"""
+    app.run(host=host, port=port, threaded=True)
