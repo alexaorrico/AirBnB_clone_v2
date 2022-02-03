9a490d4a395b6d71c425f7acd349da23f2e54f5d
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-03 08:41:54 +0100

    Add handlers for the states endpoint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,8 @@ from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
 
 
 from api.v1.views.index import *
+from api.v1.views.states import *
