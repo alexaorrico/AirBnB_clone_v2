08a30e02d2894f7ba75cff8184bafe7879d3e3eb
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-03 11:59:36 +0100

    Enable CORS for the API

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 '''
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from models import storage
 from api.v1.views import app_views
@@ -10,8 +11,6 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 '''The Flask web application instance.'''
-app.url_map.strict_slashes = False
-app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -30,6 +29,9 @@ def error_404(error):
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST')
     app_port = os.getenv('HBNB_API_PORT')
+    app.url_map.strict_slashes = False
+    app.register_blueprint(app_views)
+    CORS(app, resources={"/*": {"origins": app_host}})
     app.run(
         host=app_host if app_host else '0.0.0.0',
         port=app_port if app_port else '5000',
