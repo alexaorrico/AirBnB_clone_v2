173723f873d87d4979633600437d1afc0cf6b8ca
wuhibe <wuhibetamire710@gmail.com>
2022-02-04 20:11:19 +0300

    storage_get_count

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,20 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """ test that validates number of objs in file """
+        storage = FileStorage()
+        count = len(storage.all())
+        self.assertEqual(count, storage.count())
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing db storage")
+    def test_get(self):
+        """ test whether get method returns valid data """
+        storage = FileStorage()
+        obj = State(name='Gondar')
+        obj.save()
+        storage.save()
+        new = storage.get(State, obj.id)
+        self.assertEqual(new, obj)
