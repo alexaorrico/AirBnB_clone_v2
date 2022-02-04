f3e419ae1f7aad08d1b8e14e64ce9f633b06609e
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-04 05:03:56 +0100

    Fix PEP8 styling issues

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,10 +2,12 @@
 """
 starts a Flask web application
 """
-
 from flask import Flask, render_template
+
 from models import *
 from models import storage
+
+
 app = Flask(__name__)
 
 
@@ -22,6 +24,7 @@ def filters():
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
