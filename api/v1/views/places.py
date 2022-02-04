fb83cbb31b721e022375a2a9b7107f8eb76841b2
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-04 23:14:28 +0100

    Refactor script and fix some bugs

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,7 +4,7 @@ from flask import jsonify, request
 from werkzeug.exceptions import NotFound, MethodNotAllowed, BadRequest
 
 from api.v1.views import app_views
-from models import storage
+from models import storage, storage_t
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -20,7 +20,7 @@ def handle_places(city_id=None, place_id=None):
         'GET': get_places,
         'DELETE': remove_place,
         'POST': add_place,
-        'PUT': update_place,
+        'PUT': update_place
     }
     if request.method in handlers:
         return handlers[request.method](city_id, place_id)
@@ -35,9 +35,15 @@ def get_places(city_id=None, place_id=None):
     if city_id:
         city = storage.get(City, city_id)
         if city:
-            all_places = city.places
-            all_places = list(map(lambda x: x.to_dict(), all_places))
-            return jsonify(all_places)
+            places = []
+            if storage_t == 'db':
+                places.extend(list(map(lambda x: x.to_dict(), city.places)))
+            else:
+                places.extend(list(filter(
+                    lambda x: x.city_id == city_id,
+                    storage.all(Place).values()
+                )))
+            return jsonify(places)
     elif place_id:
         place = storage.get(Place, place_id)
         if place:
@@ -83,17 +89,16 @@ def update_place(city_id=None, place_id=None):
     '''Updates the place with the given id.
     '''
     xkeys = ('id', 'user_id', 'city_id', 'created_at', 'updated_at')
-    if place_id:
-        old_place = storage.get(Place, place_id)
-        if old_place:
-            data = request.get_json()
-            if data is None or type(data) is not dict:
-                raise BadRequest(description='Not a JSON')
-            for key, value in data.items():
-                if key not in xkeys:
-                    setattr(old_place, key, value)
-            old_place.save()
-            return jsonify(old_place.to_dict()), 200
+    place = storage.get(Place, place_id)
+    if place:
+        data = request.get_json()
+        if data is None or type(data) is not dict:
+            raise BadRequest(description='Not a JSON')
+        for key, value in data.items():
+            if key not in xkeys:
+                setattr(place, key, value)
+        place.save()
+        return jsonify(place.to_dict()), 200
     raise NotFound()
 
 
