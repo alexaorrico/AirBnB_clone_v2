175cdc81fca223685379ae628a9cc1373827400b
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-04 12:02:19 -0500

    import set below app_views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application
+Script that starts a Flask web application and blueprint implementation.
 """
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
