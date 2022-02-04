e8e768eadc5ca0f0a07f5c49e06842b0fbf5a64b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-02-04 10:11:49 +0100

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,14 +2,15 @@
 '''Flask application module'''
 
 
-from flask import Flask
-from models.engine import storage
+from flask import Flask, make_response, jsonify
+from models import storage
 from api.v1.views import app_views
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
@@ -18,6 +19,12 @@ def teardown(exception):
     if storage is not None:
         storage.close()
 
+
+@app.errorhandler(404)
+def errorhandler(error):
+    '''404 error handler'''
+    return make_response(jsonify({'error': 'Not found'}), 400)
+    
 
 if __name__ == '__main__':
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
