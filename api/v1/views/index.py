0bfc19c158a27c66fcba0a6e91bd23ac6d3583ae
jcgonzalezb <jcgonzalez@gmail.com>
2022-02-04 15:59:40 -0500

    check

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,7 @@ classes = ["Amenity", "City", "Place", "Review", "State", "User"]
 @app_views.route('/status', methods=["GET"], strict_slashes=False)
 def status():
     """ Returns a JSON: "status": "OK"""
-    return jsonify({"status": "OK"})
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats', methods=["GET"], strict_slashes=False)
