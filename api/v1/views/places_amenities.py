fd12ba0b49914d0c2e8be5371040ae1190a0ce95
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-02-04 15:57:55 +0100

    remove unwated import

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 '''places amenities relationship blueprint'''
 
-import re
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage, storage_t
