a2302dd4b582b6beaaf912d18d578cb0150b2f12
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-02-04 13:07:15 +0100

    task 9

---
+++ api/v1/views/users.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+'''users blueprint'''
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def getusers():
+    '''get all users available'''
+    users = storage.all(User)
+    return jsonify([user.to_dict() for user in users.values()])
+
+
+@app_views.route('/users/<user_id>',
+                 methods=['GET'],
+                 strict_slashes=False)
+def getUserById(user_id=None):
+    '''gets user by id'''
+    if user_id is None:
+        abort(404)
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def deleteUser(user_id=None):
+    '''deletes an user'''
+    if user_id is not None:
+        res = storage.get(User, user_id)
+        if res is not None:
+            storage.delete(res)
+            storage.save()
+            return make_response(jsonify({}), 200)
+    abort(404)
+
+
+@app_views.route('/users',
+                 methods=['POST'],
+                 strict_slashes=False)
+def postUser():
+    '''posts a new user'''
+    body = request.get_json()
+    if body is None:
+        abort(400, 'Not a JSON')
+    if 'email' not in body.keys():
+        abort(400, 'Missing email')
+    if 'password' not in body.keys():
+        abort(400, 'Missing password')
+
+    user = User(**body)
+    user.save()
+    return make_response(jsonify(user.to_dict()), 201)
+
+
+@app_views.route('/users/<user_id>',
+                 methods=['PUT'],
+                 strict_slashes=False)
+def updateUser(user_id=None):
+    '''updates a user'''
+    if user_id is None:
+        abort(404)
+    obj = storage.get(User, user_id)
+    if obj is None:
+        abort(404)
+
+    body = request.get_json()
+    if body is None:
+        abort(400, 'Not a JSON')
+    for key in body.keys():
+        if key not in ['id', 'created_at', 'updated_at', 'email']:
+            setattr(obj, key, body[key])
+    obj.save()
+    return make_response(jsonify(obj.to_dict()), 200)
