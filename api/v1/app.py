c3ffe304c2f11d31dba69ef6bf420701f936f6cf
laurajcb <callejaslaura96@gmail.com>
2022-02-04 16:31:46 +0000

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def tear(self):
     """Method to handle tearmod"""
@@ -20,7 +21,7 @@ if __name__ == "__main__":
 
     HBNB_API_HOST = getenv("HBNB_API_HOST") or "0.0.0.0"
 
-    HBNB_API_PORT = getenv("HBNB_API_PORT") or 5000 
+    HBNB_API_PORT = getenv("HBNB_API_PORT") or 5000
 
     app.run(host=HBNB_API_HOST, port=int(
         HBNB_API_PORT), debug=True, threaded=True)
