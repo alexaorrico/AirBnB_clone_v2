45329e9aea56d129c0be248adf2384b44552af6f
JuanSebastianGB <3393@holbertonschool.com>
2022-02-05 13:42:50 +0000

    FIX: Accomplish env catching requiremente

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,4 +19,5 @@ def teardown(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'), threaded=True)
