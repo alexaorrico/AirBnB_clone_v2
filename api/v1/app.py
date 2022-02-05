679c7e863efb74f134e25468b1203e8404810764
JuanSebastianGB <3393@holbertonschool.com>
2022-02-05 13:21:03 +0000

    UPD: Adding docstrings, Removing unused imports and using name condition into index routes file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 """
-Create a api
+Create an api
 """
-
 
 from os import getenv
 from api.v1.views import app_views
 from models import storage
-from flask import Flask, Blueprint
+from flask import Flask
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown(exception):
+    """ Close session """
     storage.close()
 
 
 if __name__ == '__main__':
-    """if getenv(HBNB_API_HOST) or getenv(HBNB_API_PORT):
-        host = getenv(HBNB_API_HOST)
-        port = getenv(HBNB_API_PORT)
-    else:
-        host = '0.0.0.0'
-        port = 5000"""
-
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host='0.0.0.0', port='5000', threaded=True)
