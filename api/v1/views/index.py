7ac12fbe211ae2ea0b656128cae9fe1f0317f4d5
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-02-05 12:15:12 +0000

    retrieve api stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,32 @@ Index for hbnb REST api
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', strict_slashes=False)
 def api_status():
     """returns status of the api"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def api_stats():
+    """
+    Returns statistics for the api
+    The count of every entity
+    """
+    stat_dict = {}
+    for key, value in stat_dict.items():
+        stat_dict[key] = storage.count(value)
+    return jsonify(stat_dict)
