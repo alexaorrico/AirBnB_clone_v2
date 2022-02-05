db1e8e10d229634017cdcb04e741b9504af2fb6c
JuanSebastianGB <3393@holbertonschool.com>
2022-02-05 14:34:34 +0000

    UPD: Adding stats index endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,29 @@
 """ index routes """
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ Status ok """
     return jsonify({"status": "OK"}), 200
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ hbnb stats """
+    objects = {
+        'amenities': 'Amenity',
+        'cities': 'City',
+        'places': 'Place',
+        'reviews': 'Review',
+        'states': 'State',
+        'users': 'User'
+    }
+    processed_stats = {key: storage.count(value)
+                       for key, value in objects.items()}
+    return jsonify(processed_stats), 200
 
 
 if __name__ == '__main__':
