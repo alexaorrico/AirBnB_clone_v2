66930fb1285d518a4ff8b2632261382a4f8f1247
partychu <ytorres667@gmail.com>
2022-02-05 11:58:33 +0000

    Adding API folder with task 4 running

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+API module
+"""
+
+from flask import Flask, Blueprint
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Call storage.close() """
+    storage.close()
+
+if __name__ == '__main__':
+
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+
+    if not host:
+        host = "0.0.0.0"  #default host
+    if not port:
+        port = "5000"  #default port
+
+    app.run(host, port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+View routes
+"""
+
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def status():
+    d = {"status": "OK"}
+    return d
