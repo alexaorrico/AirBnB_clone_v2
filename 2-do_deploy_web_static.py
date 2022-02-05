1c6c76038b60277199af0a6a0e87bc7a74bcdefb
Nicky-muindi <nicholasmuindi25@gmail.com>
2022-02-04 23:02:45 -0800

    updated-env-and-mode

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['66.70.184.249', '54.210.138.75']
 
 
 def do_deploy(archive_path):
