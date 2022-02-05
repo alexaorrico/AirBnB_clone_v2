407b6858d4403f49f01492af96eacae4a1c98be8
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-02-05 17:49:02 +0000

    404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Hbnb module app.y
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -17,6 +17,15 @@ def teardown_appcontex(ctx):
     calls storage.close
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found_error_handler(error):
+    """
+    Handle 404 case when a page is not found
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
