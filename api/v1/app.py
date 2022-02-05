1b03c67c97ed1079dcfdb2c01d8c674dd8c93c50
oangel26 <oa.angel26@gmail.com>
2022-02-05 16:12:07 -0500

    [FIX]

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,9 +11,9 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown(self):
+def close(error):
     """Method wich tears down storage"""
-    self.storage.close()
+    storage.close()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,4 +7,4 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    return jsonify({'status': status})
+    return jsonify({'status': 'OK'})
