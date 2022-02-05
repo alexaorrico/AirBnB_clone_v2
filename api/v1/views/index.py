5da2ea3c2692f8ef0a9244c06bfb66282845dff7
oangel26 <oa.angel26@gmail.com>
2022-02-05 17:05:18 -0500

    First commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ Module that represents index """
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -9,3 +15,15 @@ from api.v1.views import app_views
 def status():
     """ Status code """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """ Endpoint that retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    variables = ["amenities", "cities", "places", "reviews", "states", "users"]
+    stats_dict = {}
+    for i in range(len(classes)):
+        stats_dict[variables[i]] = storage.count(classes[i])
+
+    return jsonify(stats_dict)
