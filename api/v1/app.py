74c98bc8686c55c8d604110af4a5a8949e47fd0d
JuanSebastianGB <3393@holbertonschool.com>
2022-02-05 15:08:06 +0000

    UPD: Implementing add response function for additional information

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ Create an api
 from os import getenv
 from api.v1.views import app_views
 from models import storage
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -21,7 +21,7 @@ def teardown(exception):
 @app.errorhandler(404)
 def not_found(error):
     """ Handle not found page """
-    return jsonify({'error': 'Not found'}), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
