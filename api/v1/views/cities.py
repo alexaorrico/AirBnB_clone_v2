3ed6d30ba90a700fa00dbd12273cba993a0bf93e
JuanSebastianGB <3393@holbertonschool.com>
2022-02-06 02:21:58 +0000

    FIX: Checkin more missing conditionals

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """cities routes"""
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+from flask import Flask, jsonify, abort, make_response, request
 from models import storage
 from models.state import State
 from models.city import City
 
 
 @app_views.route('/states/<string:state_id>/cities', strict_slashes=False)
-def all_city(state_id):
+def cities(state_id):
     """list all cities by state"""
     state = storage.get('State', state_id)
     if state is None:
@@ -28,20 +28,21 @@ def city_id(city_id):
     abort(404)
 
 
-@app_views.route('/cities/<string:city_id>', methods=['DELETE'], strict_slashes=False)
-def del_city(city_id):
+@app_views.route('/cities/<string:city_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_city(city_id):
     """delete json data of a single city"""
-    city = (storage.get('City', city_id))
+    city = storage.get('City', city_id)
     if city:
         city.delete()
         storage.save()
-        return jsonify({}), 200
-    abort(404)
+        return jsonify({})
+    abort(404, 'Not found')
 
 
 @app_views.route('/states/<string:state_id>/cities', methods=['POST'],
                  strict_slashes=False)
-def creat_city(state_id):
+def create_city(state_id):
     """create a new city"""
     dictionary = request.get_json()
     if dictionary is None:
@@ -54,18 +55,19 @@ def creat_city(state_id):
     dictionary['state_id'] = state_id
     city = City(**dictionary)
     city.save()
-    return jsonify(city.to_dict()), 201
+    return make_response(jsonify(city.to_dict()), 201)
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-def update(city_id):
+@app_views.route('/cities/<string:city_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_city(city_id):
     """Update data of city"""
     dictionary = request.get_json()
-    city = (storage.get('City', city_id))
-    if city is None:
-        abort(404)
     if dictionary is None:
         abort(400, 'Not a JSON')
+    city = storage.get('City', city_id)
+    if city is None:
+        abort(404)
     [setattr(city, key, value) for key, value in dictionary.items()
      if key not in ['id', 'state_id', 'created_at', 'updated_at']]
     city.save()
