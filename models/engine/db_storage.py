2ab486d4f0f945022e4d7d08aa68726d0a75078f
Sergio Vera <sergio.vera@icloud.com>
2022-02-06 17:38:51 +0000

    changes to dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,7 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-    
+
     def get(self, cls, id):
         """
         cls: __class__
