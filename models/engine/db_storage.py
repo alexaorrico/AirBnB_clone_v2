699d5c96aee36a2ab3b089517b186a409ca6cef3
Firdaus Salim <firdaushassan033@gmail.com>
2022-02-06 19:53:35 +0300

    created get() and count() in DBStorage class

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,23 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls=None, id=None):
+        """Returns obj based on class name and its ID"""
+        if cls is not None and id is not None:
+            try:
+                return self.__session.query(classes[cls]).get(id)
+            except:
+                return None
+
+        return None
+
+    def count(self, cls=None):
+        """Returns the amount of objects"""
+        if cls is not None:
+            try:
+                return len(self.all(classes[cls]))
+            except:
+                return None
+        else:
+            return len(self.all())
