f68a0d19aff68cf39422e14cf138b7b9dcbadf73
rmatovu987 <rmatovu987@gmail.com>
2022-02-06 12:48:03 +0300

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Flask application """
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
