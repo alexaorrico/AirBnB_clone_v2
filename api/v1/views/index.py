405ee1f1b5863a44f826032fdd4904d9950b8f1d
kenialo19 <3396@holbertonschool.com>
2022-02-05 22:52:02 -0500

    delete whitespace

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,10 +15,10 @@ def status():
 @app_views.route('/stats')
 def stats():
     """stats"""
-    counters = {"amenities": storage.count("Amenity"), 
-                "cities": storage.count("City"), 
-                "places": storage.count("Place"), 
-                "reviews": storage.count("Review"), 
-                "states": storage.count("State"), 
+    counters = {"amenities": storage.count("Amenity"),
+                "cities": storage.count("City"),
+                "places": storage.count("Place"),
+                "reviews": storage.count("Review"),
+                "states": storage.count("State"),
                 "users": storage.count("User")}
     return jsonify(counters)
