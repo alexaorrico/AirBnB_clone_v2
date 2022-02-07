867eb63bfd8463ed370fb8f4f4b8b8a65b01f9eb
GuzhiRegem <sebastian.moreiradl@gmail.com>
2022-02-07 18:29:30 -0300

    commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,12 +2,7 @@
 """
 smth blueprint smth
 """
-
-
 from flask import Blueprint
-
-app_views = Blueprint('__init__', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -15,3 +10,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+app_views = Blueprint('__init__', __name__, url_prefix='/api/v1')
