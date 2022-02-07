ee74ab99d87a64df4d71857c8c16a7143e32c134
Mahalaksmy <3354@holbertonschool.com>
2022-02-07 13:10:58 +0100

    Update api file and index

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """ This is the app folder"""
 
+from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def storage_close():
-    """ method to close """
+def teardown_close(self):
+    """ Method to close """
     storage.close()
+
+
+@app.errorhandler(404)
+def error_not_found(error):
+    """Method no found page"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
     """ Run Flask Server """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', "0.0.0.0"), port=os.getenv(
+        'HBNB_API_PORT', 5000), threaded=True,  debug=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ This a index file"""
+from flask import Flask, jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ Status of API """
-    return jsonify({"status": "OK"})
+    """ Valid method of objects """
+    num_obj = {}
+    for key, value in classes.items():
+        num_obj[value] = storage.count(key)
+    return jsonify(num_obj)
