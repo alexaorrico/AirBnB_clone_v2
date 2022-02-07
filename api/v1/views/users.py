3641409ed62c215af6d44e1f7ca37eea09a19c65
kate <kateincoding@gmail.com>
2022-02-07 14:20:07 -0500

    [fix sintax error in get a user with an id]

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -22,7 +22,7 @@ def get_user(user_id):
     user = storage.get(User, user_id)
     if not user:
         abort(404)
-    return jsonify(user.to_dict)
+    return jsonify(user.to_dict())
 
 
 @app_views.route('users/<user_id>', methods=['DELETE'], strict_slashes=False)
