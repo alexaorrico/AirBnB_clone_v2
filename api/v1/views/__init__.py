e40c5ed02ce8b8d65b5dee41665505a574992923
juandraper <e146613@gmail.com>
2022-02-07 11:28:34 -0800

    should check fine, udpated vars names for better understanding

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,11 @@
 from flask import Blueprint
-"""Blueprints module"""
+"""init"""
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
-import api.vi.views.states
+import api.v1.views.states
+import api.v1.views.cities
+import api.v1.views.amenities
+import api.v1.views.users
+import api.v1.views.places
+import api.v1.views.places_reviews
