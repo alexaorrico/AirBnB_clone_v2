43296bddd875f8e785e980d3c8ac4c14ee96d909
kate <kateincoding@gmail.com>
2022-02-06 23:45:09 -0500

    [cities]

---
+++ api/v1/views/cities.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Import City module"""
+from api.v1.views import app_views
+from Flask import jsonify, make_response, request
+from models import storage
+from models.city import City
+from models.state import State
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_all_cities(state_id):
+    """print all cities from state_id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    citiesList = []
+    for city in state.cities:
+        citiesList.append(city.to_dict())
+    return jsonfy(list_cities)
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+def get_city(city_id):
+    """Retrieves a City object"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+def delete_city(city_id):
+    """Deletes a City object:"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    city.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
+def create_city(state_id):
+    """create a city with POST"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    citydata = request.get_json()
+    newcity = City(**citydata)
+    newcity.state_id = state_id
+    newcity.save()
+    return make_response(jsonify(newcity.to_dict()), 201)
+
+
+@app_views.route('cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def update_city(city_id):
+    """updates a city with put"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
+    citydata = request.get_json()
+    for key, value in citydata.items():
+        if key not in ignore_keys:
+            setattr(city, key, value)
+    storage.save()
+    return make_response(jsonify(city.to_dict()), 200)
