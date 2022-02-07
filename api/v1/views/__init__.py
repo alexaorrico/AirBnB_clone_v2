0828a2c51b769cacb2674f637d8acc54d5538d87
juandraper <e146613@gmail.com>
2022-02-07 12:00:42 -0800

    __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,15 @@
-from flask import Blueprint
-"""
-Blueprints module to register operaions
-on an application
-"""
+#!/usr/bin/python3
+"""smth"""
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from flask import Blueprint
+
+app_views = Blueprint('__init__', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
-import api.v1.views.states
-import api.v1.views.cities
-import api.v1.views.amenities
-import api.v1.views.users
-import api.v1.views.places
-import api.v1.views.places_reviews
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
