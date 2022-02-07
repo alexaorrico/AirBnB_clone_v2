ab128aea11e01966fce4d649adf358d58ebc8bf7
Sergio Vera <sergio.vera@icloud.com>
2022-02-07 02:46:11 +0000

    test for count on file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,12 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """ Test that count works properly"""
+    storage = FileStorage()
+    test = storage.count()
+    test_1 = storage.count(self)
+    self.assertEqual(len(test, storage))
+    self.assertEqual(len(test_1, storage))
