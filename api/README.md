4d45bf9033ce299c41f69c0540ec6edec100f73b
enzonjagi <amosndungo@gmail.com>
2022-02-07 09:21:04 +0300

    Adding the stats of each class i.e how many instances it has in storage && a README file I forgot

---
+++ api/README.md
@@ -0,0 +1,7 @@
+# 0x05. AirBnB clone - RESTful API
+
+## Objectives
+
+- Creating API endpoints
+- Creating Flask blueprints to easily navigate the above
+- Write Quality Tests for the Flask Application and API endpoints
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,13 +3,21 @@
 from flask import Flask, jsonify, Response
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status', methods=['GET'])
 def return_status():
     '''returns the status fo the method'''
-    stat = Response(status=200)
-    return jsonify({"status": f"{stat}"})
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'])
@@ -18,3 +26,7 @@ def return_stats():
     new_dict = {}
     # query all classes and
     # return a count of the instances of the class in storage
+    for c in classes:
+        count = storage.count(classes[c])
+        new_dict[c] = count
+    return jsonify(new_dict)
