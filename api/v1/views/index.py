5ddf9d9f193f661a0a7e839c51bfad8604cbca09
Mateo <mateog91@gmail.com>
2022-02-06 20:58:16 -0500

    UPD: added code_stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,22 @@
 """ Index.py """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.engine.file_storage import FileStorage
+
+classes = {"amenities": "Amenity", "cities": "City", "places": "Place",
+           "reviews": "Review", "states": "State", "users": "User"}
 
 
 @app_views.route('/status', methods=['GET'])
 def code_status():
     """ Show status of the code"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def code_stats():
+    """ Show statas about the classes
+    counts the objects per class"""
+    d = {p_cls: storage.count(r_cls) for p_cls, r_cls in classes.items()}
+    return jsonify(d)
