287bb8b8e839efffc675b9494b571e84bcbfd0fc
Mahalaksmy <3354@holbertonschool.com>
2022-02-07 13:31:08 +0100

    Update index new method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,20 @@
 """ This a index file"""
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
+
+classes = {"Amenity": "amenities", "City": "cities", "Place": "places",
+           "Review": "reviews", "State": "states", "User": "users"}
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ Method that returns the status """
+    return ({"status": "OK"})
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+def stats():
     """ Valid method of objects """
     num_obj = {}
     for key, value in classes.items():
