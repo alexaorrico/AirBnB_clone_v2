510f3d23da631d5358ccd03ca29d8b747f117c25
enzonjagi <amosndungo@gmail.com>
2022-02-07 07:19:13 +0300

    API configurations

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 '''Flask App Blueprint Setup'''
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_storage():
+def teardown(exception):
     '''Closes connection to storage'''
     if storage is not None:
         storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 '''creating an index route'''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def return_status():
-    return jsonify({"status": "200"})
+    '''returns the status fo the method'''
+    stat = Response(status=200)
+    return jsonify({"status": f"{stat}"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def return_stats():
+    '''returns a count of the instances of a class'''
+    new_dict = {}
+    # query all classes and
+    # return a count of the instances of the class in storage
