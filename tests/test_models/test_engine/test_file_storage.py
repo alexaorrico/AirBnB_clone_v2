55a17730dd9e12aaee0763892b6fb225f588fd1f
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-02-07 04:38:02 +0300

    reverted back

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,7 +18,6 @@ import json
 import os
 import pep8
 import unittest
-import sys
 FileStorage = file_storage.FileStorage
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
