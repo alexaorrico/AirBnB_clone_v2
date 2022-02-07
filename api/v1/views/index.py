a78500b23934ced225b0c1b2215a158407e8af16
Mateo <mateog91@gmail.com>
2022-02-06 20:07:03 -0500

    FIX: pycode styel

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def code_status():
-  """ Show status of the code"""
-  return jsonify({"status": "OK"})
+    """ Show status of the code"""
+    return jsonify({"status": "OK"})
