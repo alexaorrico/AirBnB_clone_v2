34f5fd8fab0b69f0a0cfba0d28f98a1d0cad287a
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-02-07 05:42:56 +0300

    added tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,19 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get_file(self):
+        """Test file get"""
+        state = State(name="Kenya")
+        state.save()
+        self.assertEqual(models.storage.get("State", state.id), state)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count_file(self):
+        """Test db count"""
+        current_count = models.storage.count("State")
+        # add an item then check if count++
+        state = State(name="Kenya")
+        state.save()
+        self.assertEqual(models.storage.count("State"), current_count + 1)
