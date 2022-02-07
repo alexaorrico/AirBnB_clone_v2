ab3d35f7ee4973be072d954afa54ca586a0f5c8e
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-02-07 04:36:06 +0300

    fixed failing tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -18,6 +18,7 @@ import json
 import os
 import pep8
 import unittest
+import sys
 FileStorage = file_storage.FileStorage
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
