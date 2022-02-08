a1062602c8561e549c84849a2770634192c6500c
JessicaJulieth <3497@holbertonschool.com>
2022-02-07 23:41:11 -0500

    Up places method

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,6 +5,9 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 
 
