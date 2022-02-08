bf59a7ea775a39b5f27c2be38c3e418c4814d72a
JessicaJulieth <3497@holbertonschool.com>
2022-02-08 00:39:18 -0500

    Up

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """ This is the app folder"""
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -19,12 +17,12 @@ def teardown_close(self):
 
 
 @app.errorhandler(404)
-def error_not_found(self):
+def error_not_found(error):
     """Method no found page"""
-    return make_response(jsonify({"error": "Not found"})), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=getenv("HBNB_API_PORT", 5000),
-            threaded=True, debug=True)
+    """ Run Flask Server """
+    app.run(host=os.getenv('HBNB_API_HOST', "0.0.0.0"), port=os.getenv(
+        'HBNB_API_PORT', 5000), threaded=True,  debug=True)
