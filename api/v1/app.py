5b83b05c15be931506f34ffeec7e481860566fea
JessicaJulieth <3497@holbertonschool.com>
2022-02-07 21:45:57 -0500

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,5 +26,6 @@ def error_not_found(error):
 
 if __name__ == "__main__":
     """ Run Flask Server """
-    app.run(host=os.getenv('HBNB_API_HOST', "0.0.0.0"), port=os.getenv(
-        'HBNB_API_PORT', 5000), threaded=True,  debug=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host, port, threaded=True)
