f2634c3258cdc10fa4de4c1c2e93fda13fd31fdd
rayd1893 <rayd1893@gmail.com>
2022-02-07 20:10:15 -0500

    Add swagger

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,13 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flasgger import Swagger
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+swagger = Swagger(app)
 
 
 @app.teardown_appcontext
