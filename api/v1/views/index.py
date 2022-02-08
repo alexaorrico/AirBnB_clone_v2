9f885e64a86db61fbdc796c38a9e023803faf0b1
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-02-07 19:43:23 -0500

    Again task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,16 @@ from models import storage
 def status():
     """return  state in json"""
     return jsonify(status='OK')
+
+
+@app_views.route("/stats")
+def stats():
+    """ Return the number of objects by type """
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        })
