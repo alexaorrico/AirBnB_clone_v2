897406f87c12375f8f1b6ac9ee3cee9eb82564d7
JessicaJulieth <3497@holbertonschool.com>
2022-02-07 21:56:30 -0500

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,7 +21,7 @@ def teardown_close(self):
 @app.errorhandler(404)
 def error_not_found(error):
     """Method no found page"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': 'Not found'})), 404
 
 
 if __name__ == "__main__":
