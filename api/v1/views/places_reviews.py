12b85263e32d7e5b940c0d9b7adea035e46c42b7
kenialo19 <3396@holbertonschool.com>
2022-02-07 21:11:47 -0500

    modified file

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -10,26 +10,25 @@ from models import storage
 from sqlalchemy.exc import IntegrityError
 
 
-@app_views.route("/places/<place_id>/reviews", methods=["GET"],
-                 strict_slashes=False)
-def get_places(place_id=None):
-    """retrieves the list of all City objects"""
-    all_places = []
-    ob_place = storage.get("place", place_id)
-    if ob_place:
-        for place_obj in ob_place.places:
-            all_places.append(place_obj.to_dict())
-        return jsonify(all_places)
+@app_views.route("/places/<place_id>/reviews", strict_slashes=False)
+def get_reviews(place_id=None):
+    """retrieves the list of all Review objects"""
+    all_reviews = []
+    place_obj = storage.get("Place", place_id)
+    if place_obj:
+        for review_obj in place_obj.reviews:
+            all_reviews.append(review_obj.to_dict())
+        return jsonify(all_reviews)
     else:
         abort(404)
 
 
-@app_views.route("/reviews/<review_id>", methods=["GET"], strict_slashes=False)
-def get_reviw(review_id=None):
-    """retrieves a Place object"""
-    review_id = storage.get("Review", review_id)
-    if review_id:
-        return jsonify(review_id.to_dict())
+@app_views.route("/reviews/<review_id>", strict_slashes=False)
+def get_review(review_id=None):
+    """retrieves a Review object"""
+    review_obj = storage.get("Review", review_id)
+    if review_obj:
+        return jsonify(review_obj.to_dict())
     else:
         abort(404)
 
@@ -37,7 +36,7 @@ def get_reviw(review_id=None):
 @app_views.route("/reviews/<review_id>", methods=['DELETE'],
                  strict_slashes=False)
 def delete_review(review_id=None):
-    """deletes a Place object"""
+    """deletes a Review object"""
     review_obj = storage.get("Review", review_id)
     if review_obj:
         storage.delete(review_obj)
@@ -49,21 +48,21 @@ def delete_review(review_id=None):
 
 @app_views.route("/places/<place_id>/reviews", methods=['POST'],
                  strict_slashes=False)
-def create_place(place_id):
-    """creates a city object"""
+def create_review(place_id):
+    """creates a Review object"""
     place_obj = storage.get("Place", place_id)
     try:
         obj_request = request.get_json()
-    except exception:
+    except Exception:
         abort(400, "Not a JSON")
     try:
         if place_obj:
             if obj_request:
-                if 'text' in obj_request and 'user_id' in obj_request:
-                    new_review = Review(**obj_request)
-                    setattr(new_review, "place_id", place_id)
-                    new_review.save()
-                    return (jsonify(new_review.to_dict()), 201)
+                if 'user_id' in obj_request and 'text' in obj_request:
+                    new_review_obj = Review(**obj_request)
+                    setattr(new_review_obj, "place_id", place_id)
+                    new_review_obj.save()
+                    return (jsonify(new_review_obj.to_dict()), 201)
                 else:
                     if 'user_id' not in obj_request:
                         abort(400, "Missing user_id")
@@ -77,21 +76,20 @@ def create_place(place_id):
         abort(404)
 
 
-@app_views.route("/reviews/<review_id>", methods=["PUT"], strict_slashes=False)
-def update_review(review_id):
-    """updates a city object"""
-    place_onj = storage.get("Review", review_id)
+@app_views.route("/reviews/<review_id>", methods=['PUT'], strict_slashes=False)
+def updates_review(review_id):
+    """updates a Review object"""
+    review_obj = storage.get("Review", review_id)
     obj_request = request.get_json()
-    if place_onj:
+    if review_obj:
         if obj_request:
             for key, value in obj_request.items():
-                ignore = ["id", "user_id", "place_id",
-                          "created_at", "updated_at"]
-                if key != ignore:
-                    setattr(place_onj, key, value)
-            place_onj.save()
-            return jsonify(place_onj.to_dict()), 200
+                if (key != "id" and key != "user_id" and key != "place_id" and
+                        key != "created_at" and key != "updated_at"):
+                    setattr(review_obj, key, value)
+            review_obj.save()
+            return jsonify(review_obj.to_dict())
         else:
-            return "Not a JSON", 400
+            abort(400, "Not a JSON")
     else:
         abort(404)
