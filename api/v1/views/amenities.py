698b179d9831b5a340e635a741e62eadf0422e5b
JessicaJulieth <3497@holbertonschool.com>
2022-02-07 19:51:27 -0500

    Up date amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,11 +11,11 @@ from models.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET'],
                  strict_slashes=False)
-def allAmenity(state_id):
+def allAmenity():
     '''Retrieves the list of all Amenity objects of a State:
     GET /api/v1/states/<state_id>/amenities'''
 
-    allAmenity = storage.all('Amenity', amenity_id)
+    allAmenity = storage.all(Amenity)
     listAmenity = []
     for amenity in allAmenity.values():
         listAmenity.append(amenity.to_dict())
