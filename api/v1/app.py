ce3cf1c9cb898509cbb96646c00f99b80af12e40
mbukeRepo <mbuke404@gmail.com>
2022-02-12 10:27:33 +0000

    first request handled and project setup

---
+++ api/v1/app.py
@@ -0,0 +1,13 @@
+""" flask app main file """
+from flask import Flask
+from os import environ
+from api.v1.views import app_views
+
+HBNB_API_PORT = environ.get("HBNB_API_PORT")
+HBNB_API_HOST = environ.get("HBNB_API_HOST")
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+if __name__ == "__main__":
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+""" contain routes on app_views blueprint """
+
+from api.v1.views import app_views
+from flask import jsonify, request
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    function for status route
+    """
+    if request.method == 'GET':
+        return jsonify({"status": "OK"})
