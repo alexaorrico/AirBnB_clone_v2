f8415eb40bd57f9de817f375b4cba7775cf17f17
krummitz <amidbidee@gmail.com>
2022-02-21 03:30:34 +0100

    added make_response to 404 it now works

--- api/v1/app.py
+++ api/v1/app.py
@@ -37,7 +37,6 @@ def not_found(error):
     return make_response(jsonify({'error': "Not found"}), 404)
 
 
-
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
