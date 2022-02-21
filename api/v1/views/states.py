c2ca2ff3e03448d98db9d293647dc0c0af05ff7f
krummitz <amidbidee@gmail.com>
2022-02-21 06:57:28 +0100

    pep validation

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -95,7 +95,9 @@ def get_state(state_id=None):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', 
+methods=['DELETE'],
+strict_slashes=False)
 def delete_state(state_id=None):
     """
     Retrieves a state by a given id and deletes it
