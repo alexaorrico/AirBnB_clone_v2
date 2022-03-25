446f4d14ec63330f750fba806495a9934cfa9e66
Code Slayer <3613@holbertonschool.com>
2022-03-25 13:57:30 -0500

    task 5 waiting on task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """index file for api"""
-import json
-
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status')
 def status(text="is_cool"):
     """returns JSON status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats')
+def count_objects(text="is_cool"):
+    """returns count of objects by type"""
+    count = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+        }
+    return jsonify(count)
