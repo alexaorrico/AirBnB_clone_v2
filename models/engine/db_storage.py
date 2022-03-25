544d2f096baad2f28fdbe1a086aab311de4752de
ZugustheDugus <83614659+ZugustheDugus@users.noreply.github.com>
2022-03-25 15:10:05 -0500

    task 3, different branch

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,24 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID, or None if not found
+        """
+        if cls not in classes.values():
+            return None
+        
+        if cls and id:
+            all_objs = models.storage.all(cls)
+            for v in all_objs.values():
+                if (v.id == id):
+                    return v
+        return None
+
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the 
+        given class. If no class is passed, returns the count of
+        all objects in storage"""
+        return (len(models.storage.all(cls)))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 Contains the FileStorage class
 """
 
+import models
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -68,3 +69,24 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID, or None if not found
+        """
+        if cls not in classes.values():
+            return None
+        
+        if cls and id:
+            all_objs = models.storage.all(cls)
+            for v in all_objs.values():
+                if (v.id == id):
+                    return v
+        return None
+
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the 
+        given class. If no class is passed, returns the count of
+        all objects in storage"""
+        return (len(models.storage.all(cls)))
