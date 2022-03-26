8f3eb018df3ca2725071124088a68da2163f6ac2
calvert1212 <calvert1212@gmail.com>
2022-03-26 20:47:47 +0000

    Fixing app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,12 +7,14 @@ Module to check the status of the API
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
-app = flask(__name__)
+app = Flask(__name__)
 
-app.register=blueprint(app_views)
+app.register_blueprint(app_views)
+
+
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
 
 if __name__ == __main__:
-    app.run(host = '0.0.0.0', port = '5000')
+    app.run(host='0.0.0.0', port='5000', threaded=True)
