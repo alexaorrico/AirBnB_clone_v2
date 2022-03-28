bf4e8dd0f98021b7ac7622c45d32ced337d418f5
jswini <jswinikoff@gmail.com>
2022-03-28 10:07:21 -0500

    first commit with stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,15 @@
 """
 This module shows roots for our site
 """
-from Flask import Flask, jsonify
+from flask import Flask, jsonify
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 app = Flask(__name__)
 
 
@@ -12,3 +19,14 @@ def status():
     """Returns status of the website if running"""
     status = {'status': 'OK'}
     return jsonify(status)
+
+
+@app_views.route('/stats')
+def stats():
+    stats = {'amenities': storage.count(Amenity),
+             'cities': storage.count(City),
+             'places': storage.count(Place),
+             'reviews': storage.count(Review),
+             'states': storage.count(State),
+             'users': storage.count(User)}
+    return jsonify(stats)
