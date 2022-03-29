c30ca5f7c73daa7482402e9c8819ba03945344fc
jstevensonl337 <jeremy.m.stevesnon@gmail.com>
2022-03-29 06:25:16 -0500

    add: attempted to add CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,21 @@
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
+from flask_cors import CORS
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.errorshandler(404)
+def errorshandler(error):
+    """error handler"""
+    return (
+        "Not found",
+        404,
+    )
 
 
 @app.teardown_appcontext
@@ -16,10 +27,7 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = "0.0.0.0"
-    if not port:
-        port = "5000"
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    
     app.run(host=host, port=port, threaded=True)
