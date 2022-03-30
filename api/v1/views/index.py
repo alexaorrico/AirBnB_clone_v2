88a0df199c7a9fc6d20ae9c288af1f318aae237b
Cameron.Crupper <guitardude20795@yahoo.com>
2022-03-30 08:32:29 -0700

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 """
 import app_views from api.v1.views
-create a route/status on the object app_views that returns a JSON: "status": "OK" (see example)
+create a route/status on the object app_views that
+returns a JSON: "status": "OK" (see example)
 """
 from api.v1.views import app_views
 from models import storage
-from models.state import State
-from models.city import City
-from models.user import User
-from models.amenity import Amenity
-from models.review import Review
-from models.place import Place
-from flask import jsonify
-from flask import Flask
+from flask import jsonify, Flask
 
 
 @app_views.route('/status', strict_slashes=False, methods=['GET'])
@@ -20,7 +14,7 @@ def return_status():
     """
     returns status
     """
-    return jsonify({'status': 'OK'})
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False, methods=['GET'])
