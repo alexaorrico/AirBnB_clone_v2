c917f4ab738d569222f703eb5995c86548489369
IyasuH <eyasuhailegbr@gmail.com>
2022-04-14 20:32:03 -0700

    FileStorage updated by adding two methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,28 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
+    def get(self, cls, id):
+        """Method to retrive one object return object
+        based on the class and its ID"""
+        if cls not in classes.values():
+            return None
+        for key, value in models.storage.all(cls).items():
+            if (key == cls + '.' + id):
+                return value
+
+        return None
+
+    def count(self, cls=None):
+        """Returns number of objects in storage matching the given class
+        if no class passed returns the count of all objects in storage"""
+        if cls is None:
+            n = 0
+            for obj in classes.values():
+                n = n + len(models.storage.all(obj).values())
+        else:
+            n = len(models.storage.all(cls).values())
+    return n
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
