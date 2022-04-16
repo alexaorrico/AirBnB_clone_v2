f0b4ea9dd8d6fa111961d3477ababd4d5017edb8
avocadoclouds <rehana447@gmail.com>
2022-04-17 01:38:31 +0300

    task 2: new methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,26 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """A method to retrieve one object:
+            - cls: class
+            - id: string representing the object ID
+            - Returns the object based on the class and its ID
+            or None if not found
+        """
+        if cls and id:
+            fetch_obj = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+            return all_obj.get(fetch_obj)
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage:
+            - cls: class
+            - id: string representing the object ID
+            - Returns the object based on the class and its ID
+            or None if not found
+        """
+        count = len(self.all(cls))
+        return count
