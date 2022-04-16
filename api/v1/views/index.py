1c67e86b2e00fca062fe4a0d49f58cc2b4759093
olart <akanbi.ges201@gmail.com>
2022-04-16 11:26:50 +0100

    status

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""create a response form JSON file with the necessary modules"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def return_response():
+    """return the response status in form of json structure"""
+    return jsonify(status="OK")
