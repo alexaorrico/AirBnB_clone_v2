0cfdab1bb89009f53c616a85da679ba20994dd56
olart <akanbi.ges201@gmail.com>
2022-04-16 11:49:26 +0100

    status

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """import blueprint and creates varaible"""
-# init file
 
 from flask import Blueprint
-from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
