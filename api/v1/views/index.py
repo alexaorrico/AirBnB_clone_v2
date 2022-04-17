0587c31c00e599f4fd3d181998840e7af9ecd274
akakiplimo <adriankiplimo@gmail.com>
2022-04-17 08:42:13 +0000

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Flask route that returns json status response
 """
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -13,4 +14,23 @@ def status():
     """
     if request.method == 'GET':
         response = {"status": "OK"}
+        return jsonify(response)
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Endpoint that retrieves the number of each objects by type
+    """
+    if request.method == 'GET':
+        response = {}
+        Entities = {
+            "Amenity":"amenities",
+            "City":"cities",
+            "Place":"places",
+            "Review":"reviews",
+            "State":"states",
+            "User":"users"
+        }
+        for key, value in Entities.items():
+            response[value] = storage.count(key)
         return jsonify(response)
