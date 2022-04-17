4b6abac0706cea722c502b553381f07554b55792
iChigozirim <iamokwukwe@gmail.com>
2022-04-17 19:41:18 +0100

    Add task 6

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """view init file"""
 from flask import Blueprint
+from api.v1.views import states
 
 app_views = Blueprint('index', __name__, url_prefix='/api/v1')
 
---
+++ api/v1/views/states.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""State view model"""
+from flask import abort
+from flask import jsonify
+from flask import request
+from flask import make_response
+from models.state import State
+from api.v1.views import app_views
+
+
+states = State.to_dict()
+
+
+@app_views.errorhandler(404)
+def not_found(error):
+    """ 404 - Not found function """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    """Returns all state objects."""
+    return jsonify(states,)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    """Returns a specified state object."""
+    state_id = "State." + state_id
+
+    if state_id not in states.keys():
+        abort(404)
+    
+    return jsonify(states[state_id],)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """Deletes a specified state object."""
+    state_id = "State." + state_id
+
+    if state_id not in states.keys():
+        abort(404)
+
+    del states[state_id]
+    return jsonify({}), 200, {'ContentType':'application/json'}
+
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """Creates a new state object."""
+    if not request.json:
+        abort(400, "Not a JSON")
+    if "name" not in request.get_json().keys():
+        abort(400, 'Missing name')
+
+    return State(**request.get_json()), 201, {'ContentType':'application/json'}
+
+
+@app_views.route('/states/<state_id>', method=["PUT"])
+def update_state(state_id):
+    """Modifies a state object."""
+    state_id = "State." + state_id
+
+    if not request.json:
+        abort(400, "Not a JSON")
+    if state_id not in states.keys():
+        abort(404)
+
+    states[state_id]['name'] = request.get_json.get('name', states[state_id]['name'])
+    states[state_id]['__class__'] = request.get_json.get(
+        'name', states[state_id]['__class__'])
+
+    return states[state_id], 200, {'ContentType':'application/json'}
