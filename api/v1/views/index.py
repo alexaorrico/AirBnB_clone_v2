2ac9d91ee2a17b86d3fc855a84791e52bdc09723
nandweza <allannandweza@gmail.com>
2022-04-17 01:30:17 -0700

    added task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -11,3 +12,21 @@ def status():
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """an endpoint that retrieves the number of each objects by type"""
+    if request.method == 'GET':
+        response = {}
+        ITEMS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in ITEMS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
