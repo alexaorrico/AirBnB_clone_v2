f04917386aeac52bec2fece15a1b8a3cac414aa4
MahiET <meazalemma2020@gmail.com>
2022-04-18 10:18:48 +0000

    Modification

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,8 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+   def test_get(self):
+        """ Test that gets an obj """
+       
+    def test_count(self):
+        """ Test that count the object created """
