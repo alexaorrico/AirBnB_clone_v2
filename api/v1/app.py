cd20a7af0eddf4ea84b05cf944cc50eac34e8291
akakiplimo <adriankiplimo@gmail.com>
2022-04-18 20:34:53 +0000

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,20 +2,25 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, Blueprint
 from models import storage
 from api.v1.views import app_views
 from werkzeug.exceptions import HTTPException
-import os
+from os import getenv
+from flask_cors import CORS
 
 
+# Global Flask Application Variable: app
 app = Flask(__name__)
-
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5000)
-
+app.url_map.strict_slashes = False
 # app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
+""" Cors access to selected resources from a different origin."""
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+host = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', 5000)
+
 
 @app.teardown_appcontext
 def teardown_db(Exception):
@@ -24,6 +29,7 @@ def teardown_db(Exception):
     the current SQLAlchemy Session
     """
     storage.close()
+
 
 @app.errorhandler(Exception)
 def global_error_handler(err):
@@ -41,9 +47,19 @@ def global_error_handler(err):
     return make_response(jsonify(message), code)
 
 
+def setup_global_errors():
+    """
+    Updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
+
+
 if __name__ == "__main__":
     """
     Main Flask app
     """
+    # initializes global error handling
+    setup_global_errors()
     # start Flask app
     app.run(host=host, port=port, threaded=True)
