9d195efe32faff46d48205a74bceee56af5989d3
MahiET <meazalemma2020@gmail.com>
2022-04-18 13:45:44 +0000

    first line

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,3 +13,4 @@ if (__name__ == 'api.v1.views'):
     from api.v1.views.users import *
     from api.v1.views.places import *
     from api.v1.views.places_reviews import *
+    from api.v1.views.places_amenities import *
