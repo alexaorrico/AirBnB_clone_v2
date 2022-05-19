50e490268b90e62f91cd897ac4c9a11be8c029f6
Mxn-ptr <maxence0@hotmail.fr>
2022-05-19 16:37:35 +0200

    Add task 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" State """
+""" City """
 import json
 from models import storage
 from flask import jsonify, abort, request, make_response
+from models.city import City
 from models.state import State
 from api.v1.views import app_views
 
 
-@app_views.route("/states", methods=['GET', 'POST'], strict_slashes=False)
-def states():
-    """ Retrieves the list of all State objects """
+@app_views.route("/states/<state_id>/cities", methods=['GET', 'POST'],
+                 strict_slashes=False)
+def cities(state_id):
+    """ DO some method on City with a state_id """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
     if request.method == 'GET':
-        list_states = []
-        states = storage.all(State).values()
-        for state in states:
-            list_states.append(state.to_dict())
-        return jsonify(list_states)
+        list_cities = []
+        for city in state.cities:
+            list_cities.append(city.to_dict())
+        return jsonify(list_cities)
 
     if request.method == 'POST':
         response = request.get_json()
@@ -24,24 +29,25 @@ def states():
         if response.get("name") is None:
             abort(400, "Missing name")
 
-        new = State(**response)
+        new = City(**response)
+        new.state_id = state.id
         new.save()
         return make_response(jsonify(new.to_dict()), 201)
 
 
-@app_views.route("/states/<state_id>", methods=['GET', 'DELETE', 'PUT'],
+@app_views.route("/cities/<city_id>", methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def states_id(state_id):
-    """ Retrieves a State object """
-    state = storage.get(State, state_id)
-    if not state:
+def city_id(city_id):
+    """ Do different methods on a City object"""
+    city = storage.get(State, city_id)
+    if not city:
         abort(404)
 
     if request.method == 'GET':
-        return jsonify(state.to_dict())
+        return jsonify(city.to_dict())
 
     elif request.method == 'DELETE':
-        storage.delete(state)
+        storage.delete(city)
         storage.save()
         return jsonify({})
 
@@ -52,6 +58,6 @@ def states_id(state_id):
             abort(400, "Not a JSON")
         for key, value in response.items():
             if key not in ignore:
-                setattr(state, key, value)
+                setattr(city, key, value)
         storage.save()
-        return make_response(jsonify(state.to_dict()), 200)
+        return make_response(jsonify(city.to_dict()), 200)
