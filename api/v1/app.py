7348ef4d30663a9a6eaf7fdae9b7999c76ce82c4
Juan Gimenez <juancitogimenez0@gmail.com>
2022-05-20 23:19:33 +0000

    file storage updated and created api folder

---
+++ api/v1/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python flask
+"""
+
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from flask_cors import CORS
+
+app = Flask(__name__)
