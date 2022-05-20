626152700210e40e116f69e0f8298aa98c7e1287
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-05-20 09:08:56 +0000

    add task 6

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,3 +13,22 @@ def status():
         return satus of the api
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """
+        return number of objects
+    """
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    
+    response = {}
+    for key, value in classes.items():
+        response[value] = storage.count(key)
+    return jsonify(response)
