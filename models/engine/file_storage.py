545f2e06d93f9737f908f9709cab02d90ba5de45
brunonra-dev <40237383+brunonra-dev@users.noreply.github.com>
2022-05-21 16:25:32 -0300

    Task 3
    Co-authored-by: AngelPiva <AngelPiva@users.noreply.github.com>

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID
+        Otherwise returns None"""
+        objs = self.all(cls)
+        if objs:
+            return objs['{}.{}'.format(cls.__name__, id)]
+        else:
+            return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in
+        storage matching the given class
+        If no class is passed, returns the
+        count of all objects in storage."""
+        return len(self.all(cls))
