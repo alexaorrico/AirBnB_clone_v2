4813d597fba052e3be3b70ef20a617b6bd50e012
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-21 13:01:32 +0100

    task 4-

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,7 +74,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def get(self, cls, id):
-        id = ""
-        cls = 5
