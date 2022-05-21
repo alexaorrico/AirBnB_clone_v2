04713216514f87b0e9d58a3bf08863c3b93eb0b2
Philippe Willot <willot.ph@gmail.com>
2022-05-21 12:09:09 +0200

    bug fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,8 +3,8 @@
 Router for handling API calls on State objects
 """
 
+from flask import jsonify, request, abort
 from api.v1.views import app_views
-from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
