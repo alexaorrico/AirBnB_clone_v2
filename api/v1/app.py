80478d587d0eb83321d88f62e01e5fecc4b19e92
jodt <joel.dumortier@gmail.com>
2022-05-21 09:04:53 +0000

    add tasks 6,8 and 10

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 This is the app module
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from os import getenv
 from models import storage
 from api.v1.views import app_views
@@ -11,6 +11,8 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 
 @app.teardown_appcontext
 def session_close(exception):
@@ -19,9 +21,18 @@ def session_close(exception):
     """
     storage.close()
 
+
+@app.errorhandler(404)
+def wrong_route(e):
+    """
+    handles error 404
+    """
+    return jsonify(error="Not found"), 404
+
+
 if __name__ == "__main__":
     app.run(
-        host = getenv('HBNB_API_HOST') or '0.0.0.0',
-        port = getenv('HBNB_API_PORT') or '5000',
-        threaded = True
+        host=getenv('HBNB_API_HOST') or '0.0.0.0',
+        port=getenv('HBNB_API_PORT') or '5000',
+        threaded=True
     )
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ This module defines a status route
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', strict_slashes=False)
 def index():
     """
