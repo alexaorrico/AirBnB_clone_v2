05637050649a08678672d69a17596f38c1fc2dea
MarionBaussart <marion.baussart@gmail.com>
2022-05-23 15:04:16 +0000

    add place_reviews

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" view for City objects that handles all default RESTFul API actions """
+""" view for Place objects that handles all default RESTFul API actions """
 
 from api.v1.views import app_views
 from flask import jsonify, request, abort
