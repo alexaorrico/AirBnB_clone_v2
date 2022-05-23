41a6fd8f5a69e0fdca61c105b549b3d1589a9480
Axel Valentin <axel.valentinbvs@yahoo.com>
2022-05-23 08:37:48 +0000

    test

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,12 +8,3 @@ from models import storage
 from api.v1.views import app_views
 import requests
 from os import *
-
-
-@app.views.route("/states", methods=['GET'])
-def get_states():
-    """
-        comment
-    """
-    states = storage.all("State").values()
-    return jsonify([state.to_json() for state in states])
