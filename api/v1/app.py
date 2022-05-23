20d25e7bf83bad6843e4d854d5316d4043c83096
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-23 17:51:27 -0500

    Feat (Add) Api -> Method(error_not_found)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Init Flask service (my_api) """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,6 +16,12 @@ def close_session(exception):
     Typically also called when the request context is popped.
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Returns JSON response with 404 status """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
