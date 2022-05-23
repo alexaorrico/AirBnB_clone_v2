063c17874e4c7abf3bbd7d91c02b366050499387
RayBar72 <3963@holbertonschool.com>
2022-05-23 14:27:46 -0300

    Task 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for City related endpoints"""
+"""Module for City api"""
 from api.v1.views import app_views
 from flask import jsonify, make_response, abort, request
 from models import storage
@@ -12,7 +12,6 @@ def get_cities(state_id):
     state = storage.get("State", state_id)
     if not state:
         return make_response(jsonify({"error": "Not found"}), 404)
-
     return jsonify([c.to_dict() for c in state.cities])
 
 
@@ -22,7 +21,6 @@ def get_city(city_id):
     city = storage.get("City", city_id)
     if not city:
         return make_response(jsonify({"error": "Not found"}), 404)
-
     return jsonify(city.to_dict())
 
 
@@ -32,7 +30,6 @@ def delete_city(city_id):
     city = storage.get("City", city_id)
     if not city:
         return make_response(jsonify({"error": "Not found"}), 404)
-
     storage.delete(city)
     storage.save()
     return make_response(jsonify({}), 200)
@@ -44,14 +41,11 @@ def post_city(state_id):
     state = storage.get("State", state_id)
     if not state:
         return make_response(jsonify({"error": "Not found"}), 404)
-
     data = request.get_json(force=True, silent=True)
     if not data:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-
     if "name" not in data:
         return make_response(jsonify({"error": "Missing name"}), 400)
-
     data['state_id'] = state_id
     c = City(**data)
     c.save()
@@ -64,11 +58,9 @@ def put_city(city_id):
     city = storage.get("City", city_id)
     if not city:
         return make_response(jsonify({"error": "Not found"}), 404)
-
     data = request.get_json(force=True, silent=True)
     if not data:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-
     for key, value in data.items():
         if key not in ["id", "created_at", "updated_at"]:
             setattr(city, key, value)
