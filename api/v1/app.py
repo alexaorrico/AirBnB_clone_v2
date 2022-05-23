c59fa955319f8dda73ca4a72c67139d4f3986b7a
RayBar72 <3963@holbertonschool.com>
2022-05-23 10:58:41 -0300

    Up to task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ Python flask
 """
 
 
+from os import getenv
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -11,4 +12,18 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 
+
+@app.teardown_appcontext
+def close(bruh):
+    """Method that calls close storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(
+            host=getenv('HBNB_API_HOST') or '0.0.0.0',
+            port=int(getenv('HBNB_API_PORT') or 5000),
+            threaded=True
+        )
