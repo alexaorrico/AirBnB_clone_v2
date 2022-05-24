294950fd94bcd60838ecfb931f41396b46db8c91
mrgiulls <giuliano.mesias@hotmail.com>
2022-05-24 00:41:30 -0500

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""
-    This script starts a Flask web application
-"""
-from os import getenv
-from flask import Flask, jsonify
-from flask_cors import CORS
+"""app.py to connect to API"""
+import os
 from models import storage
-from api.v1.views import app_views
+from api.v1.views.index import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
-    return storage.close()
+def teardown_appcontext(code):
+    """teardown_appcontext"""
+    storage.close()
 
-
-@app.errorhandler(404)
-def error(e):
-    """Handler for 404 errors"""
-    return jsonify({"error": "Not found"}), 404
-
-
-if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
-    app.run(host=host, port=port, threaded=True)
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
