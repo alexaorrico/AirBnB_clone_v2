0c1e8e17a14cf62703f38b973a0ebcbf37db5e2d
mrgiulls <giuliano.mesias@hotmail.com>
2022-05-24 00:43:53 -0500

    update index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This module implement a rule that returns the status of the application"""
-from flask import jsonify
-import models
+"""index.py to connect to API"""
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
@@ -10,21 +10,29 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+hbnbText = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User
+}
 
-@app_views.route("/status", strict_slashes=False)
-def view_status():
-    """View function that return a json message"""
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
-def view_stats():
-    """Veiw function that retrieves the number of each object by type"""
-    return jsonify({
-        "amenities": models.storage.count(Amenity),
-        "cities": models.storage.count(City),
-        "places": models.storage.count(Place),
-        "reviews": models.storage.count(Review),
-        "states": models.storage.count(State),
-        "users": models.storage.count(User)
-    })
+@app_views.route('/stats', strict_slashes=False)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
+if __name__ == "__main__":
+    pass
