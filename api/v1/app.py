e57d6c76e208510fe26ce0405e1ff4e221e46278
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-23 21:04:17 -0500

    Feat (Add) Api -> CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ Init Flask service (my_api) """
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
