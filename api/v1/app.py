b78c689f522da16f14d769f28cda21e81d78eb8e
root <guzchelsea24@gmail.com>
2022-05-24 19:52:14 -0700

    upd repo

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """status of an api"""
+
+from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
+from api.v1.views.index.py import app_views
 from os import getenv
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def storage_close(exception):
-	"""manages storafe"""
-	storage.close()
+def teardown_db(exception):
+    """method to close a session"""
+    storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-	"""handles error 404"""
-	return make_response(jsonify({"error": "Not found"}), 404)
+def errorhandler(error):
+    """method to handle 404 error"""
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
-	host = getenv("HBNB_API_HOST", "0.0.0.0")
-	port = getenv("HBNB_API_PORT", "5000")
-	app.run(host=host, port=port, threaded=True, debug=True)
+    if getenv('HBNB_API_HOST'):
+        jost = getenv('HBNB_API_HOST')
+    else:
+        jost = '0.0.0.0'
+    if getenv('HBNB_API_PORT'):
+        puerto = getenv('HBNB_API_PORT')
+    else:
+        puerto = 5000
+    app.run(host=jost, port=puerto, threaded=True, debug=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""index conf"""
+"""mod for index.py"""
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status')
+@app_views.route("/status")
 def status():
-    """returns the status of the API: """
-    return jsonify({"status": "OK"})
+    """reroute status page"""
+    return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route("/stats")
 def stats():
-    """counts items of each class for all classes"""
-    if request.method == 'GET':
-        total_for_class = {}
-        classes = {
-                   "Amenity": "amenities",
-                   "City": "cities",
-                   "Place": "places",
-                   "Review": "reviews",
-                   "State": "states",
-                   "User": "users"}
-        for key, value in classes.items():
-            total_for_class[value] = storage.count(key)
-        return jsonify(total_for_class)
+    """ret dict w every class count"""
+    return jsonify(amenities=storage.count(Amenity),
+                   cities=storage.count(City),
+                   places=storage.count(Place),
+                   reviews=storage.count(Review),
+                   states=storage.count(State),
+                   users=storage.count(User))
