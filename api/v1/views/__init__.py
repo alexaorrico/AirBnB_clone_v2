4051b510fda55a95d4d6a5fdb80883a8e3c09690
DaboRestrepo <daborestrepo@gmail.com>
2022-05-28 13:29:59 -0500

    Add the Place_reviews new route: - GET: ✅ - DELETE: ✅ Return a vacum dict. - POST: ✅ Create a new obj with data, it has to have email and password. - PUT: ✅ Update an obj.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Views init file."""
-from api.v1.views import index, states, cities, amenities
 from flask import Blueprint
 
 
@@ -8,3 +7,4 @@ app_views = Blueprint('/api/v1', __name__)
 # La intranet dice que PEP 8 se va a quejar por el orden de la importación
 # pero que este módulo no se va a checkear
 # from api.v1.views import *
+from api.v1.views import index, states, cities, amenities, users, places_reviews
