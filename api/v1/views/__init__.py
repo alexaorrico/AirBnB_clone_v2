afd52f43291e28e515268e36574c4417022f737c
Daniel1375 <danydu1299@gmail.com>
2022-05-27 19:32:10 -0500

    Working in task4 with app flask

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,14 @@
 """"""
 from flask import Blueprint
 
-
-app_views = Blueprint('/api/v1')
+print("heeerrre", __name__)
+app_views = Blueprint('/api/v1', __name__)
 # La intranet dice que PEP 8 se va a quejar por el orden de la importación 
 # pero que este módulo no se va a checkear
+
+"""@app_views.route('/status', strict_slashes=False)
+def status():
+    return "jsonify({'status': 'OK'})"""
+
 from api.v1.views import *
 from api.v1.views import index
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,13 +2,11 @@
 """"""
 from flask import jsonify
 from api.v1.views import app_views
-app_views.url_map.strict_slashes = False
+# app_views.url_map.strict_slashes = False
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({'status': 'OK'})
 
 
-if __name__ == "__main__":
-    app_views.run(host='0.0.0.0', port=5000, threaded=True)
