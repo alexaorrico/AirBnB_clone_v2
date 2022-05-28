630ad2080e51f86460d55255e7dff9b2b662cd3d
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-28 17:03:29 -0500

    Fix appy.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext()
+@app.teardown_appcontext
 def close(self):
     """ Close storage """
     storage.close()
@@ -24,6 +24,4 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    HBNB_API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    HBNB_API_PORT = getenv('HBNB_API_PORT', 5000)
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True, debug=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'), port=getenv('HBNB_API_PORT', 5000), threaded=True, debug=True)
