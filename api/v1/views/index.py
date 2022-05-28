71f46ac5a5d07b03ca1ae7d30a5dee6332b506c0
Daniel1375 <danydu1299@gmail.com>
2022-05-27 20:26:56 -0500

    Add route stats
    Count the size of the objects for each class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,5 +7,36 @@ from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """Return the status"""
+    """
+    -----------------
+    Return the status
+    -----------------
+    """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """
+    -----------------
+    Call count method
+    -----------------
+    """
+    from models import storage
+    from models.amenity import Amenity
+    from models.base_model import BaseModel, Base
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    classes = {"Amenity": Amenity, "City": City,
+               "Place": Place, "Review": Review, "State": State, "User": User}
+
+    result = {}
+    for name, cls in classes.items():
+        size = storage.count(cls)
+        result.update({name: size})
+
+    return jsonify(result)
