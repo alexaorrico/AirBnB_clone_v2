a8a710792d2979ebdf39a180af3a1fbfec7d4632
DaboRestrepo <daborestrepo@gmail.com>
2022-05-28 12:24:13 -0500

    Add the Users new route: - GET: ✅ - DELETE: ✅ Return a vacum dict. - POST: ✅ Create a new obj with data, it has to have email and password. - PUT: ✅ Update an obj, cannot be updated: email. ⚠ views init was updated.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,4 +7,4 @@ app_views = Blueprint('/api/v1', __name__)
 # La intranet dice que PEP 8 se va a quejar por el orden de la importación
 # pero que este módulo no se va a checkear
 # from api.v1.views import *
-from api.v1.views import index, states, cities
+from api.v1.views import index, states, cities, users
---
+++ api/v1/views/users.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+-------------------
+New view for States
+-------------------
+"""
+from models.user import User
+from models import storage
+from flask import jsonify, request, abort
+from api.v1.views import app_views
+from api.v1.views.aux_func import aux_func
+
+
+methods = ["GET", "DELETE", "POST", "PUT"]
+@app_views.route("/users", methods=methods)
+@app_views.route("/users/<user_id>", methods=methods)
+def users(user_id=None):
+    """
+    ---------------
+    Route for Users
+    ---------------
+    """
+    users = storage.all(User)
+    met = request.method
+    if met in ["GET", "DELETE"]:
+        res = aux_func(User, met, user_id)
+        return res
+    if met == 'POST':
+        try:
+            data_user = request.get_json()
+            if "email" not in data_user.keys():
+                return jsonify("Missing email"), 400, {'ContentType':
+                                                       'application/json'}
+            elif "password" not in data_user.keys():
+                return jsonify("Missing password"), 400, {'ContentType':
+                                                          'application/json'}
+            else:
+                new_user = User(**data_user)
+                # No sabemos si hay que guardar
+                new_user.save()
+                return jsonify(new_user.to_dict()), 201, {'ContentType':
+                                                          'application/json'}
+        except Exception as err:
+            return jsonify("Not a JSON"), 400, {'ContentType':
+                                                'application/json'}
+    elif request.method == "PUT":
+        if user_id:
+            key = "User.{}".format(user_id)
+            if key not in users.keys():
+                abort(404)
+            else:
+                try:
+                    data_user = request.get_json()
+                    user = users[key]
+                    for attr, value in data_user.items():
+                        if attr not in ["id", "created_at", "updated_at",
+                                        "email"]:
+                            setattr(user, attr, value)
+                    # No sabemos si hay que guardar
+                    storage.save()
+                    return jsonify(user.to_dict()), 201, {'ContentType':
+                                                          'application/json'}
+                except Exception as err:
+                    return jsonify("Not a JSON"), 400, {'ContentType':
+                                                        'application/json'}
