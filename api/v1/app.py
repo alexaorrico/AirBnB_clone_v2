e298ef32132f0a35e1e521a95762becf81d04e1e
imanol <jjusturi@gmail.com>
2022-05-29 15:03:07 -0500

    api.py :wrench:

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,6 +22,7 @@ def teardown_appcontext(code):
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', '5000')))
