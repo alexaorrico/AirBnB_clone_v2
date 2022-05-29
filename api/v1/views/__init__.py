be10cff56b89a26f9768c535ad7b7d9e182ad2f8
diegojojoayandun <3922@holbertonschool.com>
2022-05-29 10:23:33 -0500

    fix Task3 404 return

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,8 @@
 Module which initializes BluePrint
 """
 
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint("route", __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
+@app_views.route("/status", strict_slashes=False)
 def status_route():
     """
     returns a JSON: "status": "OK"
