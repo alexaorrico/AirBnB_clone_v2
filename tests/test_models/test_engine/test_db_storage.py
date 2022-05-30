d32d570090c4388b2442dfbdef650322a8a9e234
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-30 14:12:53 -0500

    🛠️  pycode

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -88,14 +88,14 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
-    # @unittest.skipIf(models.storage_t != 'db', "not db testing storage")
-    # def test_get(self):
-    #     """Test that return a request method GET"""
-    #     user = User(name="User1")
-    #     user.save()
-    #     self.assertEqual(len(models.storage.all()), models.storage.count())
+    @unittest.skipIf(models.storage_t != 'db', "not db testing storage")
+    def test_get(self):
+        """Test that return a request method GET"""
+        # user = User(name="User1")
+        # user.save()
+        # self.assertEqual(len(models.storage.all()), models.storage.count())
 
-    # @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    # def test_count(self):
-    #     """Test that count properly counts all objects"""
-    #     self.assertEqual(len(models.storage.all()), models.storage.count())
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """Test that count properly counts all objects"""
+        # self.assertEqual(len(models.storage.all()), models.storage.count())
