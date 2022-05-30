94bf2f8a6c2d7d44b7f52fcc807bda1a35bfcbe4
DaboRestrepo <daborestrepo@gmail.com>
2022-05-30 10:23:06 -0500

    Changes is BaseModel an States BaseModel: I took off this: .%f of the time variable. States: I use the auxiliar function

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,6 +9,7 @@ from models.state import State
 from models import storage
 from flask import jsonify, request, abort
 from api.v1.views import app_views
+from api.v1.views.aux_func import aux_func
 
 methods = ["GET", "DELETE", "POST", "PUT"]
 
@@ -22,28 +23,10 @@ def states(id=None):
     ----------------
     """
     states = storage.all(State)
-    if request.method == "GET":
-        if id:
-            key = "State.{}".format(id)
-            if key in states.keys():
-                return jsonify(states[key].to_dict())
-            else:
-                abort(404)
-        else:
-            result = []
-            for state in states.values():
-                result.append(state.to_dict())
-            return jsonify(result)
-    elif request.method == "DELETE":
-        if id:
-            key = "State.{}".format(id)
-            if key in states.keys():
-                states[key].delete()
-                # No sabemos si hay que guardar
-                storage.save()
-                return jsonify({}), 200, {'ContentType': 'application/json'}
-        abort(404)
-    elif request.method == "POST":
+    met = request.method
+    if met in ["GET", "DELETE"]:
+        res = aux_func(State, met, id)
+    elif met == "POST":
         try:
             data = request.get_json()
             if "name" not in data.keys():
@@ -77,3 +60,4 @@ def states(id=None):
                 except Exception as err:
                     return jsonify("Not a JSON"), 400, {'ContentType':
                                                         'application/json'}
+    return res
