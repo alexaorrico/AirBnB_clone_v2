fb89ced607d3df02b9fb88cfaddaa3d803d194b9
jhonpulido1990 <3825@holbertonschool.com>
2022-05-30 15:33:37 -0500

    cahnge name variable

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -126,7 +126,7 @@ class TestFileStorage(unittest.TestCase):
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
-        storage = DBStorage()
+        storage = FileStorage()
         cant = len(storage.all())
         result = storage.count()
         new_obj = State()
