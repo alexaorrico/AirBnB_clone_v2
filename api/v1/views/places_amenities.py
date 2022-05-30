3f1bdd7285f305f68b8945ec083887bf35422c1e
imanol <jjusturi@gmail.com>
2022-05-30 11:17:20 -0500

    changes:wrench:

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""places_amenities.py"""
+"""places_amenities.py."""
 import os
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for Reviews """
+""" objects that handle all default RestFul API actions for Reviews. """
 from models.review import Review
 from models.place import Place
 from models.user import User
