93a061f22e01d50faf2adff264f6ad701f522eec
jhonpulido1990 <3825@holbertonschool.com>
2022-05-30 15:55:55 -0500

    cahnge is not to !=

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -101,7 +101,7 @@ class TestReview(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in r.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
