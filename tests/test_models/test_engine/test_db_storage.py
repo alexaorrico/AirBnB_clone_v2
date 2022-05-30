0fc301827f65eb51abfdd0132b5944c2e1fd76cb
cristaker <cristaker1927@hotmail.com>
2022-05-30 20:49:10 +0000

    test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -93,3 +93,8 @@ class TestFileStorage(unittest.TestCase):
         user = User(name="User1")
         user.save()
         self.assertEqual(models.storage.get("User", user.id), user)
+
+@unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """Test that count properly counts all objects"""
+        self.assertEqual(len(models.storage.all()), models.storage.count())
