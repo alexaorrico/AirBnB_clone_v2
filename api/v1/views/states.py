26e642149aa70231b4ef83f59e9246fac3c41d6f
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-29 21:06:24 -0500

    Fix method PUT

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,17 +9,15 @@ from models import storage
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
     """ Retrieves the list of all State objects """
-    states = []
-    for state in storage.all(State).values():
-        states.append(state.to_dict())
-    return jsonify(states)
+    states = storage.all(State)
+    return jsonify([obj.to_dict() for obj in states.values()])
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def state_id(state_id):
     """ Retrieves a State object """
     state = storage.get('State', state_id)
-    if not state:
+    if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
@@ -29,9 +27,9 @@ def state_id(state_id):
 def state_delete(state_id):
     """ Deletes a State object """
     state = storage.get('State', state_id)
-    if not state:
+    if state is None:
         abort(404)
-    states.delete()
+    state.delete()
     storage.save()
     return make_response(jsonify({}), 200)
 
@@ -40,7 +38,7 @@ def state_delete(state_id):
 def state_post():
     """ Create a new state """
     new_state = request.get_json()
-    if not new_state:
+    if new_state is None:
         abort(400, 'Not a JSON')
     if 'name' not in new_state:
         abort(400, 'Missing name')
@@ -56,11 +54,11 @@ def state_id_put(state_id):
     state = storage.get('State', state_id)
     if not state:
         abort(404)
-    request = request.get_json()
-    if not request:
-        abort(400, 'Not a JSON')
-    for key, value in request.items():
-        if key != 'id' and key != 'created_at' and key != 'updated_at':
-            setattr(state, key, value)
+    request_json = request.get_json()
+    if not request_json:
+        abort(400, "Not a JSON")
+    for k, v in request_json.items():
+        if k != 'id' and k != 'created_at' and k != 'updated_at':
+            setattr(state, k, v)
     storage.save()
     return make_response(jsonify(state.to_dict()), 200)
