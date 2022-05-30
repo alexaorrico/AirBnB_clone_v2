043c10e614aa53c99f33bc64db53c4ac45e31f9b
DaboRestrepo <daborestrepo@gmail.com>
2022-05-30 15:02:37 -0500

    Let reviews be exactly like Places

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -4,49 +4,11 @@
 New view for Cities
 -------------------
 """
-from models.place import Place
-from models.review import Review
-from models.user import User
-from models import storage
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from api.v1.views.aux_func import aux_func
 
 methods = ["GET", "DELETE", "POST", "PUT"]
-@app_views.route("/reviews/<review_id>", methods=methods)
-def review_id(review_id=None):
-    """
-    -----------------
-    Route for reviews
-    -----------------
-    """
-    reviews = storage.all(Review)
-    met = request.method
-    if met in ["GET", "DELETE"]:
-        res = aux_func(Review, met, review_id)
-        return res
-    elif met == "PUT":
-        if review_id:
-            key = "Review.{}".format(review_id)
-            if key not in reviews.keys():
-                abort(404)
-            else:
-                try:
-                    data_review = request.get_json()
-                    review = reviews[key]
-                    for attr, value in data_review.items():
-                        if attr not in ["id", "user_id", "place_id",
-                                        "created_at", "updated_at"]:
-                            setattr(review, attr, value)
-                    # No sabemos si hay que guardar
-                    storage.save()
-                    return jsonify(review.to_dict()), 200, {'Content-Type':
-                                                            'application/json'}
-                except Exception as err:
-                    return jsonify("Not a JSON"), 400, {'Content-Type':
-                                                        'application/json'}
-    else:
-        abort(404)
 
 
 @app_views.route("/places/<place_id>/reviews", methods=methods)
@@ -56,6 +18,11 @@ def place_reviews(place_id=None):
     Retrieve a list of cities by state
     ----------------------------------
     """
+    from models.place import Place
+    from models.review import Review
+    from models.user import User
+    from models import storage
+
     place_obj = storage.get(Place, place_id)
     if request.method == 'GET':
         if place_obj:
@@ -86,3 +53,44 @@ def place_reviews(place_id=None):
             new_review.save()
             return jsonify(new_review.to_dict()), 201, {'Content-Type':
                                                         'application/json'}
+
+
+@app_views.route("/reviews/<review_id>", methods=methods)
+def review_id(review_id=None):
+    """
+    -----------------
+    Route for reviews
+    -----------------
+    """
+    from models.review import Review
+    from models.place import Place
+    from models.user import User
+    from models import storage
+
+    reviews = storage.all(Review)
+    met = request.method
+    if met in ["GET", "DELETE"]:
+        res = aux_func(Review, met, review_id)
+        return res
+    elif met == "PUT":
+        if review_id:
+            key = "Review.{}".format(review_id)
+            if key not in reviews.keys():
+                abort(404)
+            else:
+                try:
+                    data_review = request.get_json()
+                    review = reviews[key]
+                    for attr, value in data_review.items():
+                        if attr not in ["id", "user_id", "place_id",
+                                        "created_at", "updated_at"]:
+                            setattr(review, attr, value)
+                    # No sabemos si hay que guardar
+                    storage.save()
+                    return jsonify(review.to_dict()), 200, {'Content-Type':
+                                                            'application/json'}
+                except Exception as err:
+                    return jsonify("Not a JSON"), 400, {'Content-Type':
+                                                        'application/json'}
+    else:
+        abort(404)
