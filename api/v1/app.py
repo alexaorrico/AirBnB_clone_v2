51ba40935fa2a478d36fd11ab02b2c1572d33d55
DaboRestrepo <daborestrepo@gmail.com>
2022-05-29 20:21:32 -0500

    Add the enviroment variables: -- host: can be HBNB_API_HOST or 0.0.0.0 -- port: can be HBNB_API_PORT or 5000

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -16,4 +17,6 @@ def close(current):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True, debug=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True, debug=True)
