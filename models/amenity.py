2730ad643db2c63986d69278adade1ef97628445
anfepema700 <anfepema700@github.com>
2022-05-30 19:46:18 -0500

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Amenity Class from Models Module
+"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import backref
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+
+
+class Amenity(BaseModel, Base):
+    """Amenity class handles all application amenities"""
+    if storage_type == "db":
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('PlaceAmenity', backref='amenities',
+                                       cascade='delete')
+    else:
+        name = ''
