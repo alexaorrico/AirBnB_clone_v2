59b9c9d094dec28ca5f20cea86a9c4a25c1c6fee
anfepema700 <anfepema700@github.com>
2022-05-30 19:47:48 -0500

    city.py

---
+++ models/city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+City Class from Models Module
+"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+
+
+class City(BaseModel, Base):
+    """City class handles all application cities"""
+    if storage_type == "db":
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities', cascade='delete')
+    else:
+        state_id = ''
+        name = ''
