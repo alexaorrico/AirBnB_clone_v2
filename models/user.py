e37fe90ebf4cbc991fa044299bb34d3fdf1219ec
imanol <jjusturi@gmail.com>
2022-05-31 10:15:35 -0500

    user.py:wrench:

--- models/user.py
+++ models/user.py
@@ -9,6 +9,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from hashlib import md5
 
+
 class User(BaseModel, Base):
     """Representation of a user """
     if models.storage_t == 'db':
