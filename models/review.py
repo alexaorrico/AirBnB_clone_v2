972d4997fecabc16a30ce46d56c497b5ff77df0e
anfepema700 <anfepema700@github.com>
2022-06-01 11:29:16 -0500

    review.py

---
+++ models/review.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Review Class from Models Module
+"""
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+
+
+class Review(BaseModel, Base):
+    """Review class handles all application reviews"""
+    if storage_type == "db":
+        __tablename__ = 'reviews'
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        place_id = ''
+        user_id = ''
+        text = ''
