fd0e3cab8b2633d333b24b93d6803adece757b39
Cecales <cesar.caleroes@gmail.com>
2022-06-01 23:23:26 -0500

    Web flask directory update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
-"""
-
+    Sript that starts a Flask web application
+ """
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello_hbn():
+    """
+        function to return Hello HBNB!
+    """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+    """
+        function to return HBNB
+    """
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
-
+def text_var(text):
+    """
+        function to display text variable passed in
+    """
+    return "C {}".format(text.replace("_", " "))
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+        app.run(host='0.0.0.0', port=5000)
