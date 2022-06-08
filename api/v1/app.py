ff1e5a08f283f354cc1dc32e473a9689d42dcd2c
“olaholunuga” <olaholunuga@gmail.com>
2022-06-08 14:59:57 +0100

    CORS setup

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,10 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources=r"/*")
 app.register_blueprint(app_views)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
