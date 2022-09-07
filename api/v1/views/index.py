4b9f5ab39a4e0b7416bf54279a75faaec9ed3b1f
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-07 19:13:35 +0000

    add endpoint stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
 # Index
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 @app_views.route('/status')
 def status():
     dictionary = {}
     dictionary['status'] = "Ok"
     return jsonify(dictionary)
+
+@app_views.route('/stats')
+def countype():
+    dic = {}
+    for key, value in classes.items():
+        count = storage.count(value)
+        dic[key] = count
+    return jsonify(dic)
