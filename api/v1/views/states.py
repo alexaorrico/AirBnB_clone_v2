478789a4971ab761c496ccfc91ef81cf754261b8
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-07 22:27:55 +0000

    test create state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -37,9 +37,9 @@ def states(state_id=None):
     elif request.method == 'POST':
         body = request.get_json()
         try:
-            
             if 'name' in body:
-                new_state = State({"name": "valentin"})
+                value['name'] = body['name']
+                new_state = State(value)
                 new_state.save()
                 return jsonify(new_state.to_dict()), 201
             else:
