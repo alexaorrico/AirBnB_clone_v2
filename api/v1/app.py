dbd5a90ef398f6169b74002249ed145870e7438a
MatiasMtz <4442@holbertonschool.com>
2022-09-08 16:46:55 +0000

    error 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,11 @@ def teardown(self):
     storage.close()
 
 
+@app.errorhandler(404)
+def pageNotFound(error):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
