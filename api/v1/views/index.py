6783e4ae6830910b6a81e237f29e58d365845226
mglssr <iglesiasmaia6@gmail.com>
2022-09-08 19:45:19 +0000

    pep8 style corrections

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-Index instance 
+Index instance
 """
 
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+
 
 @app_views.route("/status")
 def status():
@@ -14,18 +15,19 @@ def status():
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route("/stats")
 def stats():
     """
     Retrieves the number of each objects by type.
     """
     json_obj = {
-        "amenities": storage.count("Amenity"), 
-        "cities": storage.count("City"), 
-        "places": storage.count("Place"), 
-        "reviews": storage.count("Review"), 
-        "states": storage.count("State"), 
-        "users": storage.count("User") 
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
     }
-    
+
     return jsonify(json_obj)
