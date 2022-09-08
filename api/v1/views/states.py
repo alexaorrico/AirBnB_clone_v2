5b9780481fd8a89d59219081c0d512804c8f253b
mglssr <iglesiasmaia6@gmail.com>
2022-09-08 19:48:35 +0000

    pep8 style corrections

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """
-State instance 
+State instance
 """
+
 
 from flask import Flask, jsonify, request, abort, make_response
 from api.v1.views import app_views
 from models import storage
 from models.state import State
 
+
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
 def state():
     """
     Retrieves the list of all State objects
     """
-    
+
     states = []
-    
+
     for state in storage.all("State").values():
         states.append(state.to_dict())
-        
+
     return jsonify(states)
-        
+
+
 @app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def state_by_id(state_id):
     """
@@ -28,11 +31,12 @@ def state_by_id(state_id):
     """
 
     state = storage.get("State", state_id)
-    
+
     if state is None:
         abort(404)
     else:
         return jsonify(state.to_dict())
+
 
 @app_views.route("/states/<state_id>", methods=["DELETE"],
                  strict_slashes=False)
@@ -41,7 +45,7 @@ def del_state(state_id):
     Deletes a State object
     """
     state = storage.get("State", state_id)
-    
+
     if state is None:
         abort(404)
     else:
@@ -49,14 +53,15 @@ def del_state(state_id):
         # storage.save()
         return make_response(jsonify({}), 200)
 
+
 @app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
     """
     Creates a State
     """
-    
+
     body = request.get_json()
-    
+
     if body is None:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
     elif "name" not in body.keys():
@@ -65,21 +70,22 @@ def create_state():
         state = State(**body)
         # state.save()
         return make_response(jsonify(state.to_dict()), 201)
-    
+
+
 @app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
 def update_state(state_id):
     """
     Updates a State object
     """
-    
+
     body = request.get_json()
     no_update = ["id", "created_at", "updated_at"]
-    
+
     if body is None:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-    
+
     state = storage.get("State", state_id)
-    
+
     if state is None:
         abort(404)
     else:
@@ -88,10 +94,6 @@ def update_state(state_id):
                 setattr(state, key, value)
             else:
                 pass
-        
+
         # storage.save()
         return make_response(jsonify(state.to_dict()), 200)
-                
-            
-            
-
