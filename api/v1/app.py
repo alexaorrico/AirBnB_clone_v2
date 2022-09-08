f1ef6e2b552584ea13d0b5247387f7e509555016
Dak4rPrO <98028958+Dak4rPrO@users.noreply.github.com>
2022-09-08 18:48:03 -0300

    task 4, terminar

---
+++ api/v1/app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" first API """
+
+from models import storage 
+
+app 
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" returns a JSON: "status": "OK" """
+
+from api.v1.views import app_views
+import json
+import requests
+
+
+return 'status: "OK"'
