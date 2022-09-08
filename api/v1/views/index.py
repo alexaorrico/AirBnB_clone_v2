f30fc9020670656382765df6e62815596666a711
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-08 19:10:01 +0000

    fix errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """file index"""
 from models.amenity import Amenity
+from api.v1.views import app_views
 from models.city import City
+from flask import jsonify
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.user import User
 from models import storage
-from api.v1.views import app_views
-from flask import jsonify
+from models.user import User
+
+
+
 
 classes = {"amenities": Amenity, "cities": City,
            "places": Place, "reviews": Review, "states": State, "users": User}
