fefa3d1f66bf8a47973ae6d8730985ae2d4d3a4e
MatiasMtz <4442@holbertonschool.com>
2022-09-08 19:02:17 +0000

    another try states

--- aaMAT/api/v1/views/states.py
+++ aaMAT/api/v1/views/states.py
@@ -23,3 +23,12 @@ def getStateById(state_id):
 		abort(404)
 	state = state.to_dict()
 	return jsonify(state)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def deleteState(state_id):
+	"""asdasdasda"""
+	state = storage.get(State, state_id)
+	if state is None:
+		abort(404)
+	storage.delete(state)
+	return jsonify({}), 200
