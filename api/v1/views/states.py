c62ec901bdfe7be26033639b86d5e2ff1162e6d9
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-08 21:17:41 +0000

    fix 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,9 +16,9 @@ classes = {"amenities": Amenity, "cities": City,
            "places": Place, "reviews": Review, "states": State, "users": User}
 
 
-@app_views.route('/states/<state_id>', methods=['GET'],
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
 def states(state_id=None):
     """def function que devuelve una lista de todos los State"""
     lista_states = []
@@ -35,60 +35,46 @@ def states(state_id=None):
             for key, value in states.items():
                 if states[key].id == state_id:
                     return jsonify(value.to_dict())
-            return jsonify({'error': 'Not found'}), 404
-
-
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def states_post():
-    """def states post"""
-    try:
-        body = request.get_json()
-        if 'name' in body:
-            value = {}
-            value['name'] = body['name']
-            new_state = State(**value)
-            new_state.save()
-            return jsonify(new_state.to_dict()), 201
-        else:
+            abort(404)
+    elif request.method == 'POST':
+        try:
+            body = request.get_json()
+            if 'name' in body:
+                value = {}
+                value['name'] = body['name']
+                new_state = State(**value)
+                new_state.save()
+                return jsonify(new_state.to_dict()), 201
+            else:
+                return jsonify({
+                    "error": "Missing name"
+                }), 400
+        except Exception as err:
             return jsonify({
-                "error": "Missing name"
-            }), 400
-    except Exception as err:
-        return jsonify({
-                "error": "Not a JSON"
-            }), 400
-
-
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def states_delete(state_id=None):
-    """def states delete"""
-    states = storage.all()
-    for key, value in states.items():
-        if states[key].id == state_id:
-            storage.delete(states[key])
-            storage.save()
-            return jsonify({}), 200
-    return jsonify({'error': 'Not found'}), 404
-
-
-@app_views.route('/states/<state_id>', methods=['PUT'],
-                 strict_slashes=False)
-def state_put(state_id=None):
-    """def"""
-    try:
-        body = request.get_json()
-        notAttr = ['id', 'created_at', 'updated_at']
-        states = storage.all(State)
+                    "error": "Not a JSON"
+                }), 400
+    elif request.method == 'PUT':
+        try:
+            notAttr = ['id', 'created_at', 'updated_at']
+            body = request.get_json()
+            states = storage.all(State)
+            for key, value in states.items():
+                if states[key].id == state_id:
+                    for k, v in body.items():
+                        if k not in notAttr:
+                            setattr(value, k, v)
+                    value.save()
+                    return jsonify(value.to_dict()), 200
+            return jsonify({'error': 'Not found'})
+        except Exception as err:
+            return jsonify({
+                    "error": "Not a JSON"
+                }), 400
+    else:
+        states = storage.all()
         for key, value in states.items():
             if states[key].id == state_id:
-                for k, v in body.items():
-                    if k not in notAttr:
-                        setattr(value, k, v)
-                value.save()
-                return jsonify(value.to_dict()), 200
-        return jsonify({'error': 'Not found'}), 404
-    except Exception as error:
-        return jsonify({
-                "error": "Not a JSON"
-            }), 400
+                storage.delete(states[key])
+                storage.save()
+                return jsonify({}), 200
+        abort(404)
