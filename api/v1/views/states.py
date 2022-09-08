5bd283412220229a399656fbab5ba4e791049c79
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-08 20:57:46 +0000

    debbug

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -36,36 +36,45 @@ def states(state_id=None):
                 if states[key].id == state_id:
                     return jsonify(value.to_dict())
             abort(404)
-    elif request.method == 'POST':
-        try:
-            body = request.get_json()
-            if 'name' in body:
-                value = {}
-                value['name'] = body['name']
-                new_state = State(**value)
-                new_state.save()
-                return jsonify(new_state.to_dict()), 201
-            else:
-                return jsonify({
-                    "error": "Missing name"
-                }), 400
-        except Exception as err:
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def states_post():
+    """def states post"""
+    try:
+        body = request.get_json()
+        if 'name' in body:
+            value = {}
+            value['name'] = body['name']
+            new_state = State(**value)
+            new_state.save()
+            return jsonify(new_state.to_dict()), 201
+        else:
             return jsonify({
-                    "error": "Not a JSON"
-                }), 400
-    else:
-        states = storage.all()
-        for key, value in states.items():
-            if states[key].id == state_id:
-                storage.delete(states[key])
-                storage.save()
-                return jsonify({}), 200
-        abort(404)
+                "error": "Missing name"
+            }), 400
+    except Exception as err:
+        return jsonify({
+                "error": "Not a JSON"
+            }), 400
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def states_delete(state_id=None):
+    """def states delete"""
+    states = storage.all()
+    for key, value in states.items():
+        if states[key].id == state_id:
+            storage.delete(states[key])
+            storage.save()
+            return jsonify({}), 200
+    abort(404)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'],
                  strict_slashes=False)
-def state(state_id=None):
+def state_put(state_id=None):
     """def"""
     try:
         body = request.get_json()
