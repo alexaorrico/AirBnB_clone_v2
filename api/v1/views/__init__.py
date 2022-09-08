d6d92a65071218ca4f0071d984c182dbd9559b09
mglssr <iglesiasmaia6@gmail.com>
2022-09-08 19:43:30 +0000

    pep style corrections

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,4 @@
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -9,3 +6,5 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
