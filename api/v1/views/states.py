13c0ed2c494c219e268282eedff4ff80918fe26a
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-08 20:59:05 +0000

    debbug

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,9 +16,9 @@ classes = {"amenities": Amenity, "cities": City,
            "places": Place, "reviews": Review, "states": State, "users": User}
 
 
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
+@app_views.route('/states/<state_id>', methods=['GET'],
                  strict_slashes=False)
-@app_views.route('/states', methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states(state_id=None):
     """def function que devuelve una lista de todos los State"""
     lista_states = []
