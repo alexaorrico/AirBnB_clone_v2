60799f533333d658cb4a160977419d96f4cde96e
MatiasMtz <4442@holbertonschool.com>
2022-09-08 17:49:51 +0000

    added states by id

--- aaMAT/api/v1/views/states.py
+++ aaMAT/api/v1/views/states.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 """adasda"""
+from os import abort
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
 
 
-@app_views.route('/api/v1/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'])
 def getStates():
+	"""aaasdasdasd"""
 	states = []
 	for state in storage.all("State").values():
 		states.append(state.to_dict())
+	return jsonify(state)
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def getStateById(state_id):
+	"""asdasdasda"""
+	state = storage.get("State", state_id)
+	if state is None:
+		abort(404)
+	state = state.to_dict()
 	return jsonify(state)
