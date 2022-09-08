e1f61a621f642fee6bb9977a2e9dbb72ab845816
mglssr <iglesiasmaia6@gmail.com>
2022-09-08 21:34:38 +0000

    draft task 8

---
+++ api/v1/views/cities.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+City instance
+"""
+
+
+from crypt import methods
+from flask import Flask, jsonify, request, abort, make_response
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+from models.state import City
+
+
+@app_views.route("/api/v1/states/<state_id>/cities", methods=["GET"],
+                 strict_slashes=False)
+def cities_state(state_id):
+    """Retrieves the list of all City objects of a State"""
+
+    cities = []
+
+    if state_id not in storage.all().values().id:
+        abort(404)
+
+    for city in storage.all("City").values():
+        if city.state_id == state_id:
+            cities.append(city.to_dict)
+    
+    return jsonify(cities)
+
+
+@app_views.route("/api/v1/cities/<city_id>", methods=["GET"],
+                 strict_slashes=False)
+def cities(city_id):
+    """Retrieves a City object"""
+    city = storage.get("City", city_id)
+
+    if city is None:
+        abort(404)
+    else:
+        return jsonify(city.to_dict())
+
+
+@app_views.route("/api/v1/cities/<city_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def delete_city(city_id):
+    """Deletes a city instance"""
+    city = storage.get("City", city_id)
+
+    if city is None:
+        abort(404)
+    else:
+        storage.delete(city)
+        # storage.save()
+        return make_response(jsonify({}), 200)
+
+
+@app_views.route("/api/v1/states/<state_id>/cities", methods["POST"],
+                 strict_slashes=False)
+def post_city(state_id):
+    """Creates a City instance"""
+    body = request.get_json()
+
+    if state_id not in storage.all().values().id:
+        abort(404)
+
+    if body is None:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    elif "name" not in body.keys():
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    else:
+        body['state_id'] = state_id
+        city = City(**body)
+        # city.save()
+        return make_response(jsonify(city.to_dict()), 201)
+
+@app_views.route("")
