bb827ca5e91730616434ae97f004ec177dcb2ccd
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-08 20:34:23 +0000

    debbug

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,7 +23,7 @@ def teardown_appcontext(self):
 @app.errorhandler(404)
 def handle_exception(e):
     """method that handled error"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
