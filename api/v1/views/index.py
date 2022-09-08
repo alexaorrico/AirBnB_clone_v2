ec8abd5490cf378c32ef2d9085db1359fdd8d693
AxelBouvierM <axelbouvierm@gmail.com>
2022-09-08 17:07:01 +0000

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,20 @@
 """index"""
 from api.v1.views import app_views
 from flask import jsonify, request
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    dic = {}
+    classes = {"amenity": "Amenity", "city": "City",
+               "place": "Place", "review": "Review",
+               "state": "State", "user": "User"}
+    for key, value in classes.items():
+        dic[key] = storage.count(value)
+    return dic
