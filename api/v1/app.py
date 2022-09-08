e9c444f3d8287caaa9f668bbe5b18cb460dfc628
MaxiHBTN <4387@holbertonschool.com>
2022-09-08 17:49:49 +0000

    Task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,9 +11,6 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-HBNB_API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-HBNB_API_PORT = getenv('HBNB_API_PORT' , '5000')
-
 @app.teardown_appcontext
 def storage_close(self):
     """
@@ -24,5 +21,7 @@ def storage_close(self):
 
 
 if __name__ == "__main__":
+    HBNB_API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    HBNB_API_PORT = getenv('HBNB_API_PORT' , '5000')
     app.run(host=HBNB_API_HOST, port=
             HBNB_API_PORT, threaded=True)
