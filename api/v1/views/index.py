cc0ff9f9e00ba58225d51be389e2a5af8234faa2
ezesilva95 <4381@holberton.students.com>
2022-09-08 19:16:31 +0000

    Task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,22 @@ Status of your API
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route('/status')
 def status():
     message = {"status": "OK"}
     return jsonify(message)
+
+
+@app_views.route('/stats')
+def stats():
+    cls = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        }
+    return jsonify(cls)
