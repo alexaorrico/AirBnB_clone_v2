20b1aa499c5070eda0d566729ae8a7f5173676f0
MatiasMtz <4442@holbertonschool.com>
2022-09-08 18:40:30 +0000

    new try

--- aaMAT/api/v1/views/states.py
+++ aaMAT/api/v1/views/states.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify, abort
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -17,11 +18,8 @@ def getStates():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def getStateById(state_id):
 	"""asdasdasda"""
-	key = "State" + "." + "{}".format(state_id)
-	state = storage.get(key)
-	print(state)
+	state = storage.get(State, state_id)
 	if state is None:
 		abort(404)
 	state = state.to_dict()
-	print(state)
 	return jsonify(state)
