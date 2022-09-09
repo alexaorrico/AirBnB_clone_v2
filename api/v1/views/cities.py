a684e391680aaed513cd4a78e8807ef2388902d8
ezesilva95 <4381@holberton.students.com>
2022-09-09 16:08:50 +0000

    Task 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -50,7 +50,8 @@ def delete_city(city_id):
     return (jsonify({}), 200)
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """
     Creates a City
