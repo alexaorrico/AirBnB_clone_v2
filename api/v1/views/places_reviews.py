ee128a5baef83739c2cc008bcec6648b819d881e
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-09 17:48:09 -0300

    Task 12 Fix

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -102,5 +102,5 @@ def update_review(review_id):
         else:
             pass
 
-    body.save()
-    return make_response(jsonify(review.to_dict()), 201)
+    storage.save()
+    return make_response(jsonify(review.to_dict()), 200)
