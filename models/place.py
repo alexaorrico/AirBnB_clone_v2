1e2b78ae58f78d5993235520817ae304763f439f
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-08 23:07:40 -0300

    Task 9Fix

--- models/place.py
+++ models/place.py
@@ -36,7 +36,7 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
         amenities = relationship("Amenity", secondary="place_amenity",
                                  backref="place_amenities",
-                                 viewonly=False, cascade="all, delete-orphan")
+                                 viewonly=False)
     else:
         city_id = ""
         user_id = ""
