f179d25efea4faa9823c18a9d48dd5666a41abb3
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-09 20:18:54 +0000

    fix 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -14,26 +14,19 @@ from models.state import City
                  strict_slashes=False)
 def cities_state(state_id):
     """Retrieves the list of all City objects of a State"""
-
-    cities = []
-    state = storage.get("State", state_id)
-
-    if state is None:
+    states = storage.get(State, state_id)
+    if states is None:
         abort(404)
-
-    for city in state.cities:
-        cities.append(city.to_dict())
-
-    return jsonify(cities)
+    list_cities = []
+    for i in states.cities:
+        list_cities.append(i.to_dict)
 
 
 @app_views.route("/cities/<city_id>", methods=["GET"],
                  strict_slashes=False)
 def city(city_id):
     """Retrieves a City object"""
-
     city = storage.get("City", city_id)
-
     if city is None:
         abort(404)
     else:
@@ -45,34 +38,33 @@ def city(city_id):
 def delete_city(city_id):
     """Deletes a city instance"""
     city = storage.get("City", city_id)
-
-    if city is None:
-        abort(404)
-    else:
+    if city is not None:
         storage.delete(city)
         storage.save()
         return make_response(jsonify({}), 200)
+    else:
+        abort(404)
+       
 
 
 @app_views.route("/states/<state_id>/cities", methods=["POST"],
                  strict_slashes=False)
 def create_city(state_id):
     """Creates a City instance"""
-    body = request.get_json()
-
-    if body is None:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    elif "name" not in body.keys():
-        return make_response(jsonify({"error": "Missing name"}), 400)
-
-    state = storage.get("State", state_id)
-    if state is None:
-        abort(404)
-    else:
-        city = City(**body)
-        city.state_id = state_id
-        city.save()
-        return make_response(jsonify(city.to_dict()), 201)
+    try:
+        body = request.get_json()
+        if "name" not in body.keys():
+            return make_response(jsonify({"error": "Missing name"}), 400)
+        state = storage.get("State", state_id)
+        if state is None:
+            abort(404)
+        else:
+            city = City(**body)
+            city.state_id = state_id
+            city.save()
+            return make_response(jsonify(city.to_dict()), 201)}
+    except Exception as e:
+         return make_response(jsonify({"error": "Not a JSON"}), 400)
 
 
 @app_views.route("/cities/<city_id>", methods=["PUT"],
