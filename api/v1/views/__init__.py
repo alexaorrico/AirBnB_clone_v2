d9caae9d6b43010a84d1d1fbf14801e1f6f063b4
MaRS-208 <manuela.riossosa@gmail.com>
2022-09-09 18:38:29 -0300

    4. Status of your API III

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,4 +6,4 @@
 from api.v1.views.index import *
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url prefix=/api/v1)
+app_views = Blueprint("app_views", __name__, urlprefix=/api/v1)
