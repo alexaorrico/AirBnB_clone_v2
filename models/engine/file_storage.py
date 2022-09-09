afb1592e3cf2e6d369fe0f5ab227ff7eec2498d9
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-09 13:41:59 -0300

    Get and Count in filestorage and dbstorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,21 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID,
+        or None if not found"""
+        try:
+            key = cls.__name__ + '.' + id
+            if key in self.__objects:
+                return self.__objects[key]
+        except Exception:
+            return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage."""
+        if (cls is not None):
+            dict_len = len(self.all(cls).values())
+            return dict_len
+        return len(self.all().values())
