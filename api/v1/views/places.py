c5abf24d99bd86c1ee8100e8bc3b5687a26b9714
MateoArbini <mateoarbini1998@gmail.com>
2022-09-10 21:15:21 +0000

    11

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -76,6 +76,7 @@ def postplaces(city_id=None):
     if user is None:
         abort(404)
     new_place = Place(**body)
+    new_place.city_id = city_id
     new_place.save()
     return jsonify(new_place.to_dict()), 201
 
