41d0412fc50e31a8a1bfc0fde62a18ba29c1a8dc
MateoArbini <mateoarbini1998@gmail.com>
2022-09-10 20:22:12 +0000

    8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -78,18 +78,18 @@ def createcity(state_id=None):
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def updatecity(city_id=None):
     """Function to update a city obj"""
-    notAttr = ['id', 'state_id', 'created_at', 'updated_at']
-    body = request.get_json()
-    if body is None:
+    try:
+        notAttr = ['id', 'state_id', 'created_at', 'updated_at']
+        body = request.get_json()
+        city = storage.get("City", city_id)
+        if city is None:
+            abort(404)
+        for key, value in body.items():
+            if key not in notAttr:
+                setattr(city, key, value)
+        storage.save()
+        return jsonify(city.to_dict()), 200
+    except Exception as error:
         return jsonify({
             "error": "Not a JSON"
         }), 400
-
-    city = storage.get("City", city_id)
-    if city is None:
-        abort(404)
-    for key, value in body.items():
-        if key not in notAttr:
-            setattr(city, key, value)
-    storage.save()
-    return jsonify(city.to_dict()), 200
