5faccc5b6a145351824d6866dbb2167b522cc2be
MateoArbini <mateoarbini1998@gmail.com>
2022-09-10 21:19:40 +0000

    added file12

---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""file places_reviews"""
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+import json
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
