5ba6e4eb92b7681e3637533a8be3fd2e90884cc4
MaxiHBTN <4387@holbertonschool.com>
2022-09-10 14:21:30 +0000

    Task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,12 @@ Status of your API
 from os import getenv
 from models import storage
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
