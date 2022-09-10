afbeace49fb5d703870603dab0c4d2aa6656476b
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-10 02:54:46 +0000

    task 4 API running

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""It's time to start your API. Your first endpoint\
-(route) will be to return the status of your API"""
+""" Init API Blueprint with flask """
 
 
-from api.v1.views.index import *
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, urlprefix=/api/v1)
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
