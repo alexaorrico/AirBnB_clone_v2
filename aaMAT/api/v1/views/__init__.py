0264833df129bdd8d55c2b666bdd46fcad1593e2
AxelBouvierM <axelbouvierm@gmail.com>
2022-09-11 21:23:50 +0000

    delete file

--- aaMAT/api/v1/views/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""init"""
-from flask import Blueprint
-app_views = Blueprint('api', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.amenities import *
