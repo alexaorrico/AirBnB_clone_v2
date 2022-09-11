db88e76722aea154a086df165ae77b6b7b97550f
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-11 19:41:25 -0300

    cords add

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ from flask import Flask
 from api.v1.views import app_views
 from os import getenv
 from flask import jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
