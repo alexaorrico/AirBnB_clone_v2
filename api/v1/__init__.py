3818ca9cdb9430963fe28d874f39770ddc34425f
maurolabella <maurolabella@yahoo.com>
2022-09-10 23:30:53 -0300

    task4:Status_of_your_API

---
+++ api/v1/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""create a variable app_views which is an instance of Blueprint"""
+
+from api.v1.views.index import *
+from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1/")
--- api/v1/app.py
+++ api/v1/app.py
@@ -4,20 +4,28 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
-
-
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
-@app.route('/', methods = ['GET'])
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """function teardown_appcontext"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def errorhandler(error):
+    """Function that handles error 404"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+@app.route('/', methods=['GET'])
 def get_post():
-  return jsonify
+    return jsonify
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
-  
-  
-  
+    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'), threaded=True)
