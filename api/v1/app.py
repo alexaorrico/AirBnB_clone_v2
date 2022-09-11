42b4e4d91ce937858075a50f27c1292b56aa0be5
MateoArbini <mateoarbini1998@gmail.com>
2022-09-11 03:36:59 +0000

    13

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,9 +9,11 @@ from flask import Flask
 from flask import Blueprint, Response
 from os import getenv
 from flask import jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
