2c026be9ceb6aad83b8e8fd8acc33352ee9d72d9
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-12 00:36:12 +0000

    debbug 14

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -44,19 +44,18 @@ def del_place_amenity(place_id, amenity_id):
     storage.save()
     return jsonify({}), 200
 
-# @app_views.route('places/<place_id>/amenities/<amenity_id>',
-#                  methods=['DELETE'],
-#                  strict_slashes=False)
-# def amenis_delete(place_id, amenity_id):
-#     places = storage.get("Place", place_id)
-#     if place is None:
-#         abort(404)
-#     amenits = storage.get("Amenity", amenity_id)
-#     if amenits is None:
-#         abort(404)
-#     for key, value in places.amenties.items():
-#         if value.id == amenity_id:
-#             storage.delete(amenits)
-#             storage.save()
-#             return jsonify({}), 200
-#     abort(404)
+
+@app_views.route("/places/<p_id>/amenities/<a_id>", strict_slashes=False,
+                 methods=['POST'])
+def post_place_amenity(p_id, a_id):
+        """POST a new amenity on a place"""
+        the_place = storage.get(Place, p_id)
+        if the_place is not None:
+            the_amenity = storage.get(Amenity, a_id)
+            if the_amenity is not None:
+                place_amenity = the_place.amenities
+                if the_amenity in place_amenity:
+                    return jsonify(the_amenity.to_dict(), 200)
+                place_amenity.append(the_amenity)
+            abort(404)
+        abort(404)
