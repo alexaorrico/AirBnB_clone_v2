8d3c242227fedc711aed78ef6284414504382d73
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-12 00:37:46 +0000

    debbug 14

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -18,16 +18,14 @@ classes = {"amenities": Amenity, "cities": City,
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 
-@app_views.route('/places/<place_id>/amenities', methods=['GET'],
-                 strict_slashes=False)
-def amenis(place_id):
-    place = storage.get("Place", place_id)
-    if place is None:
-        abort(404)
-    list_ameniti = []
-    for i in place.amenities:
-        list_ameniti.append(i.to_dict())
-    return jsonify(list_ameniti)
+@app_views.route('/places/<p_id>/amenities', strict_slashes=False,
+                 methods=['GET'])
+def place_amenities(p_id):
+    """Get the amenities by place"""
+    the_amenity = storage.get(Place, p_id)
+    if the_amenity is not None:
+        return jsonify([a.to_dict() for a in the_amenity.amenities])
+    abort(404)
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
@@ -48,14 +46,14 @@ def del_place_amenity(place_id, amenity_id):
 @app_views.route("/places/<p_id>/amenities/<a_id>", strict_slashes=False,
                  methods=['POST'])
 def post_place_amenity(p_id, a_id):
-        """POST a new amenity on a place"""
-        the_place = storage.get(Place, p_id)
-        if the_place is not None:
-            the_amenity = storage.get(Amenity, a_id)
-            if the_amenity is not None:
-                place_amenity = the_place.amenities
-                if the_amenity in place_amenity:
-                    return jsonify(the_amenity.to_dict(), 200)
-                place_amenity.append(the_amenity)
-            abort(404)
+    """POST a new amenity on a place"""
+    the_place = storage.get(Place, p_id)
+    if the_place is not None:
+        the_amenity = storage.get(Amenity, a_id)
+        if the_amenity is not None:
+            place_amenity = the_place.amenities
+            if the_amenity in place_amenity:
+                return jsonify(the_amenity.to_dict(), 200)
+            place_amenity.append(the_amenity)
         abort(404)
+    abort(404)
