2594e643d819d51302ff902bb53d605eae602639
louay66 <louaylouay20@gmail.com>
2022-09-15 23:41:04 +0100

    simple fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,19 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def 
+def teardown():
+    """ calls storege.close to close
+    sqlalqemy """
+    storage.close()
+if __name__ == "__main__":
+    """ run fask app"""
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True )
