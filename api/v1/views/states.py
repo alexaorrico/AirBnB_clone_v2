80fb65c3231ce33500cb9d8d4467b05c470d8eb7
Jesus <jesus.junc10@gmail.com>
2022-09-15 15:55:15 -0500

    adding state file, and import inside inti

---
+++ api/v1/views/states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""view states object"""
+
+from api.v1.views import app_views
+from models import storage
+from flask import Flask, jsonify
+
+
+@app_views.route('/states')
+def states():
+    """return list of all objects State"""
+    new_list = list()
+    for value in storage.all('State').values():
+        new_list.append(value)
+    return jsonify(new_list)
+
+
