a9cbaf802f8990f06972d363155e987e469f29c2
J3rCast <jerson.castro.r@gmail.com>
2022-09-15 18:37:36 -0500

    view states/state_id added

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from os import getenv
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """view states object"""
 
-
 from api.v1.views import app_views
 from models import storage
 from flask import Flask, jsonify
 
 
-@app_views.route('/states')
+@app_views.route('/states', strict_slashes=False)
 def states():
     """return list of all objects State"""
     new_list = list()
     for value in storage.all('State').values():
         new_list.append(value.to_dict())
-    return new_list
+    return jsonify(new_list)
 
-
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def states_id(state_id=None):
+    """Return dictionary of specific state"""
+    return storage.get("State", state_id)
