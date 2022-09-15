ffa16f953bd50cc4f8def2a8657df8669c845bb9
louay66 <louaylouay20@gmail.com>
2022-09-15 23:54:07 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown():
+def teardown(error):
     """ calls storege.close to close
     sqlalqemy """
     storage.close()
