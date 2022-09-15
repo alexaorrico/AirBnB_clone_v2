1f01d0a4b6b57ab95141e70d62c8b9ee892fe5ec
Dontcode44 <kabutto378@gmail.com>
2022-09-15 15:42:38 -0500

    Updated - Index method 🍳

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,15 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status_():
     """Initial status
     """
     stats = {"status": "OK"}
     return jsonify(stats)
+
+@app_views.route('/api/v1/stats',)
+def count_():
+    """Count all
+    """
+    return jsonify
