2c42be4ae8eff9e6c8118246bafbd2bb23f59f2d
Jesus <jesus.junc10@gmail.com>
2022-09-15 09:57:40 -0500

    adding space

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,3 +10,4 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
+
