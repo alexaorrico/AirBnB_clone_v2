04873dd41b3d905dbd6605be41dd15e6177b6320
Jesus <jesus.junc10@gmail.com>
2022-09-15 12:58:42 -0500

    at index adding stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
@@ -11,3 +12,16 @@ def status():
     """Return json with status"""
     ret = {"status": "OK"}
     return jsonify(ret)
+
+@app_views.route('/stats')
+def stats():
+    """retrieves the number of eack objects by type"""
+    new_dict = dict()
+    new_dict['amenities'] = storage.count('Amenity')
+    new_dict['cities'] = storage.count('City')
+    new_dict['places'] = storage.count('Place')
+    new_dict['review'] = storage.count('Review')
+    new_dict['states'] = storage.count('State')
+    new_dict['users'] = storage.count('User')
+    return jsonify(new_dict)
+
