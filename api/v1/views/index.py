5274297ca50643f31cbdc00c89c9b94c3b4ee766
louay66 <louaylouay20@gmail.com>
2022-09-16 00:14:26 +0100

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python
 """ """
 from api.v1.views import app_views
-from json import dumps
-
+from flask import jsonify
+from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 @app_views.route('/status')
 def status():
     """return succsess code 200"""
-    json = {"status": "OK"}
-    return dumps(json)
+    json = {
+        "status": "OK"
+        }
+    return jsonify(json)
+@app_views.route('/stats')
+def stats():
+    """return each class how much object instance"""
+    obj_instance = {
+    "amenities": storage.count(Amenity), 
+    "cities": storage.count(City), 
+    "places":  storage.count(Place), 
+    "reviews": storage.count(Review), 
+    "states": storage.count(State), 
+    "users": storage.count(User)
+    }
+    return jsonify(obj_instance)
