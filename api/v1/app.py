2d1c910e366871fff8e4aa5bf5bf1cbe462ee17c
Jesus <jesus.junc10@gmail.com>
2022-09-15 12:47:05 -0500

    teardown self

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_appcontext():
+def teardown_appcontext(self):
     """call method to close"""
     storage.close()
 
