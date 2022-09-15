fd34c8e8a4e53217bc77f0dbc5abc6fe37ec0daa
Jesus <jesus.junc10@gmail.com>
2022-09-15 18:23:34 -0500

    [OK] state states route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,12 +7,11 @@ from models import storage
 from flask import Flask, jsonify
 
 
-@app_views.route('/states')
+@app_views.route('/states', strict_slashes=False)
 def states():
     """return list of all objects State"""
     new_list = list()
-    for value in storage.all('State').values():
+    lst_states = storage.all('State')
+    for value in lst_states.values():
         new_list.append(value.to_dict())
-    return new_list
-
-
+    return jsonify(new_list)
