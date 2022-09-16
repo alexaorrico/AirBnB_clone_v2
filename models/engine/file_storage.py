99878d2948d3c82edc235d10cc8240f779ced558
Aleph235 <akram239911@gmail.com>
2022-09-16 12:48:29 +0200

    get and count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,6 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        try:
+            obj = cls.get(str(id))
+        except cls.DoesNotExist:
+            obj = None
+        return obj
+
     def count(self, cls=None):
         """Returns the number of objects in storage matching the given class"""
         return (len(self.all(cls)))
