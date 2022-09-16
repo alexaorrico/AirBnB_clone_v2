ed48cbac4c6ee1fbdbeba6989170ae37a59de781
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-15 22:09:59 -0500

    method get() & count() added to file_storage and db_storage and the corresponding tests

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,16 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+    
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        key_name = cls + "." + id
+        return self.all().get(key_name)
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        if not cls:
+            return len(self.all())
+        else:
+            return len([v for v in self.all().values()
+                        if type(v).__name__ == cls])
