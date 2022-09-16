fc302c5bf319112a96d2b1cc1f3b3ea8d1de5067
Dontcode44 <kabutto378@gmail.com>
2022-09-16 11:06:57 -0500

    Fixed - Pycodestyle spaces 🧶

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,7 +12,8 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities(state_id):
     """ Retrieves the list of all State objects """
     state = storage.get(State, state_id)
@@ -45,7 +46,8 @@ def delete_city(city_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def post_city(state_id):
     """ Creates a City """
     state = storage.get(State, state_id)
