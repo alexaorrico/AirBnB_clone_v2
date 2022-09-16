c2aa1cc1ad3178c2d7d357d78f512d7e53d588ed
Aleph235 <akram239911@gmail.com>
2022-09-16 15:53:30 +0200

    done task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 """ flask framwork"""
-from flask import Flask
+from flask import Flask, abort
+
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,8 +15,16 @@ def teardown(error):
     """ calls storege.close to close
     sqlalqemy """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(e):
+    """ handle errors"""
+    error_404 = {"error": "Not found"}
+    return error_404, 404
+
 if __name__ == "__main__":
     """ run fask app"""
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True )
+    
