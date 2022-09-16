0e62b59c1654cf37d4d71b71f737d49a79afd5e4
J3rCast <jerson.castro.r@gmail.com>
2022-09-16 17:44:40 -0500

    CORS feature added

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """ Module for start the API """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -10,6 +11,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
