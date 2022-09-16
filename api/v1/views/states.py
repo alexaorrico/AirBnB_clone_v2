d077be4942d09a00cbfe5cc3987337fb2fae8a3b
rominamarmontel <romi.varnier@gmail.com>
2022-09-16 06:43:41 -0700

    update 2nd

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,72 +1,69 @@
 #!/usr/bin/python3
 """State objects that handles all default RESTFul API actions"""
 
-from models import storage
-from flask import jsonify, make_response, abort
-from models.state import State
 from api.v1.views import app_views
+from models import storage
+from flask import jsonify, abort, request
+from models.state import State
 
 
-@app.route('/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states_get():
     """Retrieves the list of all State"""
-    if request.methods == 'GET':
-        states_list = []
-        all_states = storage.all(State)
-        for key, value in all_states.items():
-            states_list.append(value.to_dict())
-        return jsonify(states_list)
+    states_list = []
+    all_states = storage.all(State)
+    for key, value in all_states.items():
+        states_list.append(value.to_dict())
+    return jsonify(states_list)
 
-@app.route('/states', methods=['POST'])
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def states_post():
     """Creates a State"""
-    if request.methods == 'POST':
-        transform_dict = request.get_json()
-        if transform_dict is None:
-            abort(400, "Not a JSON")
-        if 'name' not in transform_dict.keys():
-            abort(400, "Missing name")
-        else:
-            new_state = State(**transform_dict)
-            new_state.save()
-            return make_response(jsonify(new_state.to_dict()), 201)
+    transform_dict = request.get_json()
+    if transform_dict is None:
+        abort(400, "Not a JSON")
+    if 'name' not in transform_dict.keys():
+        abort(400, "Missing name")
+    else:
+        new_state = State(**transform_dict)
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
 
-@app.route('/states/<state_id>', methods=['GET'])
-def state_id_get():
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def state_id_get(state_id):
     """Retrieves a State object and 404 if it's an error"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if request.method == 'GET':
-        return (jsonify(obj.to_dict()))
+    return jsonify(state.to_dict())
 
-@app.route('/states/<state_id>', methods=['DELETE'])
-def state_id_delete():
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def state_id_delete(state_id):
     """Deletes a State object and 404 if it's an error"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if request.method == 'DELETE':
-        storage.delete(state)
-        storage.save()
-        return make_response(jsonify({}), 200)
+    state.delete()
+    storage.save()
+    return jsonify({}), 200
 
 
-@app.route('/states/<state_id>', methods=['PUT'])
-def state_id_put():
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def state_id_put(state_id):
     """Updates a State object"""
     ignore_list = ['id', 'created_at', 'updated_at']
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
-    if request.method == 'PUT':
-        transform_dict = request.get_json()
-        if transform_dict is None:
-            abort(400, "Not a JSON")
-        for key, value in transform_dict.items():
-            if key not in ignore_list:
-                setattr(state, key, value)
-            else:
-                storage.save()
-                return make_response(jsonify(state.to_dict()), 200)
-
+    transform_dict = request.get_json()
+    if transform_dict is None:
+        abort(400, "Not a JSON")
+    for key, value in transform_dict.items():
+        if key not in ignore_list:
+            setattr(state, key, value)
+        state.save()
+        return jsonify(state.to_dict()), 200
