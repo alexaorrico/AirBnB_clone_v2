89c284aa661af11105e19a97003e6977bd1ef945
Mr-emilio <pablopeag@hotmail.com>
2022-09-16 14:43:16 -0500

    problem fixed in database

--- models/city.py
+++ models/city.py
@@ -12,8 +12,8 @@ class City(BaseModel, Base):
     """Representation of city """
     if models.storage_t == "db":
         __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship("Place", backref="cities")
     else:
         state_id = ""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class name and its ID,
+        or None if not found"""
+        if cls is not None and id is not None:
+            key = cls + '.' + id
+            return self.__objects.get(key)
+        return None
+    
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class
+        name. If no name is passed, returns the count of all objects in
+        storage."""
+        if cls is not None:
+            return len(self.all(cls))
+        return len(self.all())
