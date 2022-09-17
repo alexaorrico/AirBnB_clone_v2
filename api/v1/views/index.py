4e8cd6d934da59fb657d6a81d91a5d59c21a2373
felipeholbi <4758@holbertonstudents.com>
2022-09-17 11:36:21 -0500

    point 5
    pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,6 +10,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns a JSON"""
