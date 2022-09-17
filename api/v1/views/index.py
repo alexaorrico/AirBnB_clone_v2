980abaa723e8302b6a41220e2f9d8d28a83008ac
Yasgc24 <4537@holbertonschool.com>
2022-09-17 10:55:24 -0500

    Modified task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """"""
-import json
+from flask import jsonify
 from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns a JSON"""
-    return json({"status": "OK"})
+    return jsonify({"status": "OK"})
