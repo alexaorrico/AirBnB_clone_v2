e8cf673dae50678bbe41ad8b9136799216509507
Yasgc24 <4537@holbertonschool.com>
2022-09-17 11:54:32 -0500

    Add task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """app file for app.teardown_appcontext"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown(exepcition):
     """Removes the current SQLAlchemy Session"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(exception):
+    """Handler for 404 errors that returns
+    a JSON-formatted 404 status code response"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
