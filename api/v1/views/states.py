5229ea90c33f4a692c56ebe878237306d666de01
David <4292@holbertonschool.com>
2022-09-17 17:29:15 -0400

    adding GET to state by id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 
-from flask import Flask, request, jsonify
+from crypt import methods
+from flask import Flask, request, jsonify, abort
 from models import storage
 from models.state import State
 from api.v1.views import app_views
 
 
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
-def states_list():
+def all_states():
     state_objects = storage.all(State)
-    state_list = []
+    states_list = []
     for key, val in state_objects.items():
-        state_list.append(val.to_dict())
-    return jsonify(state_list)
+        states_list.append(val.to_dict())
+    return jsonify(states_list)
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
+def state_by_id(state_id):
+    state_objects = storage.all(State)
+    for key, val in state_objects.items():
+        if val.id == state_id:
+            return val.to_dict()
+    abort(404)
