7a78f9c58b303fb5d0c7ae3a1463beab2646e7ff
Jebeniani <aymenjebeniani05@gmail.com>
2022-09-18 14:15:38 +0100

    objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-from crypt import methods
 from os import getenv
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from flask import Flask, jsonify
 from api.v1.views import app_views
 
@@ -14,6 +21,23 @@ def status():
     return jsonify({
                     "status": "OK"
                     })
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """stats view"""
+    return jsonify({
+        "amenities": storage.count(Amenity),
+
+        "cities": storage.count(City),
+
+        "places": storage.count(Place),
+
+        "reviews": storage.count(Review),
+
+        "states": storage.count(State),
+
+        "users": storage.count(User)
+    })
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST") or '0.0.0.0',
