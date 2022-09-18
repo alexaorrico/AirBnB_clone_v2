11a7df1a05efff856044d5975bbc317f02ca51dc
Mr-emilio <pablopeag@hotmail.com>
2022-09-18 16:42:35 -0500

    add class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,11 +20,11 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """retrieves the number of each objects by type"""
-    classes = {"User": "users", "Amenity": "amenities",
-               "City": "cities", "Place": "places",
-               "Review": "reviews", "State": "states"}
+    classes = [Amenity, City, Place, Review, State, User]
+    equal = ['amenities', 'cities', 'places', 'reviews', 'states', 'users']
     new_dict = {}
 
-    for i in classes.keys():
-        stats[classes[cls]] = storage.count(cls)
-    return jsonify(stats)
+    for i in range(len(classes)):
+        new_dict[equal[i]] = storage.count(classes[i])
+
+    return jsonify(new_dict)
