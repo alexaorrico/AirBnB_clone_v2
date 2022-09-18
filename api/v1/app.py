74ce169b4a373948b6dab0f2a010f8f322193764
khawladaouay <daouaykhawla782@gmail.com>
2022-09-18 12:58:06 +0100

    storage close call

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,10 @@
 from os import getenv
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint
-app.register_blueprint(app_views)
+from flask import Flask
 
 app = Flask(__name__)
-
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def app_teardown(self):
