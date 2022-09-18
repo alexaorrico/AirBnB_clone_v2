b3c99f76a61d268e398dc8319d004ffed98aabef
Aleph235 <akram239911@gmail.com>
2022-09-18 20:46:23 +0200

    no debug

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,4 +26,4 @@ if __name__ == "__main__":
     """ run fask app"""
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
-    app.run(debug=True, host=host, port=port, threaded=True )
+    app.run(host=host, port=port, threaded=True )
