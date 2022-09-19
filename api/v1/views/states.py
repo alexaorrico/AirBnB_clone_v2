df1afa3ddfaf31dd1e4186f1f753445426814c36
Yasgc24 <4537@holbertonschool.com>
2022-09-19 11:26:44 -0500

    Modified task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-from os import stat
 from models import state
-from models.base_model import *
 from flask import *
 from api.v1.views import app_views
 from models.state import State
