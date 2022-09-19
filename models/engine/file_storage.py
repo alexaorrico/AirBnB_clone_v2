eeb3caed76c17b1bca84907f2bfe5bb331446ec1
Chiara Caprasi <caprasichiara@gmail.com>
2022-09-19 15:02:11 +1000

    add two methods as per task 3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -68,3 +69,23 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object based on class and ID,
+        or None if not found"""
+
+        my_cls = classes.get(cls)
+        if my_cls is None:
+            return None
+
+        all_classes = models.storage.all(cls)
+        for item in all_classes:
+            if (item.id == id):
+                return item
+        return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
