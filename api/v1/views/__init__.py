bd770514e24812b428566dff309bd75e228705dd
Aleph235 <akram239911@gmail.com>
2022-09-19 20:21:38 +0200

    pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""  configuration of blueprinte"""
 from flask import Blueprint
 from api.v1.views.cities import *
 from api.v1.views.states import *
 from api.v1.views.index import *
 from api.v1.views.amenities import *
-
+"""  configuration of blueprinte"""
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
