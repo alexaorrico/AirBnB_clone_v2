fa178be55efcf9137f1b82aec5be5744242c884e
borrell-44 <alexanderborrell44@hotmail.com>
2022-09-20 08:03:44 -0400

    Adding eviromental variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 
 """Comments"""
 
-import json
+import os
 from werkzeug import exceptions
 from flask import Flask
 from models import storage
@@ -23,4 +23,10 @@ def handle_bad_request(exception):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+    host = os.getenv('HBNB_API_HOST')
+    if host is None:
+        host = "0.0.0.0"
+    port = os.getenv('HBNB_API_PORT')
+    if port is None:
+        port = 5000
+    app.run(host=host, port=port, debug=False, threaded=True)
