3049b954f2a9616c2fd9c839828eb47ee2067dbf
Farah <farah.mccurdy@gmail.com>
2022-09-20 10:14:39 +0000

    add: imported classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 @app_views.route('/status')
 def status():
@@ -13,13 +18,14 @@ def status():
 
 @app_views.route('/stats')
 def stats():
-    """Create an endpoint that retrieves
-    the number of each objects by type"""
-    count_dict = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")}
-    return jsonify(count_dict)
+    """return number of each objects by type"""
+    classes = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+        }
+    return jsonify(classes)
+    
