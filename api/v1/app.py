d04496962773287b8c0195c1878d8a6c97cfa325
David <4292@holbertonschool.com>
2022-09-20 09:26:27 -0400

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,13 @@
 import os
 from werkzeug import exceptions
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
