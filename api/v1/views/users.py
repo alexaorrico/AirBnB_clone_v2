7ccd070ba360bf1345274786d709f6c64f589ccf
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-09-20 13:44:12 +1000

    fix: typo

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -14,7 +14,7 @@ def get_all_users():
     return (jsonify(
             [obj.to_dict() for obj in storage.all("User").values()]))
 
-@app_views.route('/users/<string:<user_id>', methods=['GET'],
+@app_views.route('/users/<string:user_id>', methods=['GET'],
                  strict_slashes=False)
 def get_user_by_id(user_id):
     """Retrieves a User object: <user_id>"""
