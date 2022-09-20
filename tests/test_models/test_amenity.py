7bad0953c4a37cd9ded92443f97c17097c22d2bc
21Kgold <claudiarpa@gmail.com>
2022-09-19 20:10:17 -0700

    fix test space

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -21,7 +21,7 @@ class TestAmenityDocs(unittest.TestCase):
         cls.amenity_f = inspect.getmembers(Amenity, inspect.isfunction)
 
     def test_pep8_conformance_amenity(self):
-        """Test that models/amenity.py conforms to PEP8."""
+        """Test that models/amenity.py conforms to PEP8"""
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['models/amenity.py'])
         self.assertEqual(result.total_errors, 0,
