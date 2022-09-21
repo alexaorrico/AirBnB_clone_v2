e346b4c5f7c3da40c4667becbfe422ae25b7a2e8
declannoble <declanjnoble@hotmail.com>
2022-09-22 09:16:12 +1000

    build: add CORS instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,12 @@ import os
 from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+corsInstance = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
