652e6e4749748bfd454d8e8147f6f1d69c817f82
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-09-21 13:32:33 +1000

    change code to suit new coding style

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,62 +8,40 @@ from flask import abort, jsonify, request
 from models import storage
 from models.user import User
 
-@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@app_views.route('/users',
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
 def get_all_users():
     """Retrieves the list of all User objects"""
-    return (jsonify(
-            [obj.to_dict() for obj in storage.all("User").values()]))
+    print("in correct route")
+    if request.method == 'GET':
+        returnedValue, code = User.api_get_all(
+                    storage.all("User").values()
+        )
+    if request.method == 'POST':
+        returnedValue, code = User.api_post(
+                    ["email", "password"],
+                    request.get_json(silent=True))
+    return (jsonify(returnedValue), code)
 
-@app_views.route('/users/<string:user_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_user_by_id(user_id):
-    """Retrieves a User object: <user_id>"""
-    userToReturn = storage.get("User", user_id)
-    if userToReturn is None:
-        abort(404)
-    return (jsonify(userToReturn.to_dict()))
 
-@app_views.route('/users/<string:user_id>', methods=['DELETE'],
+@app_views.route('/users/<string:User_id>',
+                 methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def delete_user_by_id(user_id):
-    """Deletes a User object: user_id"""
-    userToDelete = storage.get("User", user_id)
-    if userToDelete is None:
+def User_by_id(User_id):
+    """handles User object: User_id"""
+    if request.method == 'GET':
+        returnedValue, code = User.api_get_single(
+                        storage.get("User", User_id))
+    if request.method == 'DELETE':
+        returnedValue, code = User.api_delete(
+                    storage.get("User", User_id))
+    if request.method == 'PUT':
+        returnedValue, code = User.api_put(
+                    ['id', 'email', 'created_at', 'updated_at'],
+                    request.get_json(silent=True),
+                    storage.get("User", User_id))
+    if code == 404:
         abort(404)
-    userToDelete.delete()
     storage.save()
-    return (jsonify({}))
-
-@app_views.route('/users/', methods=['POST'], strict_slashes=False)
-def post_user():
-    """create a new User object in DB"""
-    newUserData = request.get_json()
-    if newUserData is None or type(newUserData) != dict:
-        return (jsonify({'error': 'Not a JSON'}), 400)
-    newEmail = newUserData.get('email')
-    if newEmail is None:
-        return (jsonify({'error': 'Missing email'}), 400)
-    newPassword = newUserData.get('password')
-    if newPassword is None:
-        return (jsonify({'error': 'Missing password'}), 400)
-    newUser = User(**newUserData)
-    newUser.save()
-    return (jsonify(newUser.to_dict()), 201)
-
-@app_views.route('/users/<string:user_id>', methods=['PUT'],
-                 strict_slashes=False)
-def put_user_by_id(user_id):
-    """Updatea a User object: user_id"""
-    putUserData = request.get_json()
-    if putUserData is None or type(putUserData) != dict:
-        return (jsonify({'error': 'Not a JSON'}), 400)
-    UserToUpdate = storage.get("User", user_id)
-    if UserToUpdate is None:
-        abort(404)
-    keysToIgnore = ['id', 'email', 'created_at', 'updated_at']
-    for key, value in putUserData.items():
-            if key in keysToIgnore:
-                continue
-            setattr(UserToUpdate, key, value)
-    UserToUpdate.save()
-    return (jsonify(UserToUpdate.to_dict()), 200)
+    return (jsonify(returnedValue), code)
