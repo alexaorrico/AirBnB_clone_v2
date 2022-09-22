c11e0d2dee84b252beb3a2c286f65c595a121b5e
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-09-22 16:14:41 +1000

    build: add users CRUD api functionality

--- models/user.py
+++ models/user.py
@@ -24,6 +24,58 @@ class User(BaseModel, Base):
         first_name = ""
         last_name = ""
 
+    REQUIRED_ATTRS = ["email", "password"]
+    SKIP_UPDATE_ATTRS = ["id",
+                         "email",
+                         "created_at",
+                         "updated_at"]
+
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @classmethod
+    def api_put(cls, putDataAsDict, idOfObject):
+        """handles the API put command for all types
+        Return Values: item dictionary or
+        raise exception"""
+        cls.ensure_objectId_is_valid(idOfObject)
+        return (super(User, cls).
+                storage_update_item(putDataAsDict,
+                                    idOfObject))
+
+    @classmethod
+    def api_post(cls, postDataAsDict):
+        """handles the API post command for all types
+        Return Values: newObject dictionary
+        or Riase exception"""
+        return (super(User, cls).
+                storage_create_item(postDataAsDict))
+
+    @classmethod
+    def api_delete(cls, idOfObject):
+        """handles the API delete command for all types
+        return Values: empyt dictionary on success or 
+        raise exception
+        """
+        cls.ensure_objectId_is_valid(idOfObject)
+        return (super(User, cls).
+                storage_delete_single(idOfObject))
+
+    @classmethod
+    def api_get_single(cls, idOfObject):
+        """handles the API get command for specific object
+        return Values: 200: success
+        404: invalid object.
+        """
+        cls.ensure_objectId_is_valid(idOfObject)
+        return (super(User, cls).
+                storage_retrieve_single(idOfObject))
+
+    @classmethod
+    def api_get_all(cls):
+        """handles the API get command for all objects
+        return a list Always, sometime empty.
+        """
+        return (super(User, cls).
+                storage_retrieve_all_type(cls))
