51099661aa5033d2153f0b635098cdac05c633b9
Farah <farah.mccurdy@gmail.com>
2022-09-22 11:11:21 +0000

    add: post and put

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,11 +10,9 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-import json
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def all_cities(state_id):
     """ 
     Retrieves the list of all City objects of a State
@@ -50,3 +48,43 @@ def city_delete(city_id):
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
+
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+def city_post(state_id):
+    """ Adds a City to given state id """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        content = request.json
+        if 'name' not in content:
+            abort(400, "Missing name")
+        new_city = City(name=content['name'], state_id=state_id)
+        new_city.save()
+        return jsonify(new_city.to_dict()), 201
+    else:
+        abort(400, "Not a JSON")
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def city_put(city_id):
+    """ Update a city object based on given city id"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+        
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        content = request.json
+        new_dict = {}
+        for k, v in content.items():
+            if k not in ["id", "created_at", "updated_at"]:
+                new_dict[k] = v
+        city.update(**new_dict)
+        city.save()
+        return jsonify(city.to_dict()), 200
+    else:
+        abort(400, "Not a JSON")
