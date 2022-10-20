37f074065cfe25771f25caef76e7f3d98c29db3b
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-10-20 20:49:35 +0100

    Updated ip adsresses of host servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['3.238.176.253', '44.197.205.40']
 
 
 def do_deploy(archive_path):
