6fc2ee2da72c68ef98cd8678a7d292ec0e38a5af
Shamso Osman <94788755+S-Osman4@users.noreply.github.com>
2022-10-21 18:30:14 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,15 +15,3 @@ def app_status():
     return(jsonify(status="OK"))
 
 
-@app_views.route("/stats")
-def stats():
-    """
-    Returns statistics about the number of objects available.
-    """
-    total = {"amenities": storage.count("Amenity"),
-             "cities": storage.count("City"),
-             "places": storage.count("Place"),
-             "reviews": storage.count("Review"),
-             "states": storage.count("State"),
-             "users": storage.count("User")}
-    return jsonify(total)
