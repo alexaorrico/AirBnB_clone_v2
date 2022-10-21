973d06932a6954a87fb6acfe4a7c12b38d4e991d
YUSUF <96367786+Iyusuf40@users.noreply.github.com>
2022-10-21 21:54:48 +0000

    add stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,32 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+class_dict = {
+        "Amenity": Amenity,
+        "City": City,
+        "Place": Place,
+        "Review": Review,
+        "State": State,
+        "User": User
+        }
+
+names_dict = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+        }
 
 
 @app_views.route('/status')
@@ -12,3 +38,15 @@ def status():
     return jsonify({
             "status": "Ok"
         })
+
+
+@app_views.route('stats')
+def count():
+    """ returns count of all objects """
+    dct = {}
+
+    for item in class_dict:
+        count = storage.count(class_dict[item])
+        dct[names_dict[item]] = count
+
+    return jsonify(dct)
