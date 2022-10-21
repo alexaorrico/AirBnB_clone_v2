d96df80e526540ff1eb3999125de4c9597deeeaa
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2022-10-21 17:49:44 +0100

    Define api app folder structure

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""v1 api application"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """App Teardown functionality"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST") or "0.0.0.0",
+            port=getenv("HBNB_API_PORT") or 5000, threaded=True)
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""views root file for describing blueprints"""
+
+from flask import Blueprint
+
+
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+
+import api.v1.views.index
