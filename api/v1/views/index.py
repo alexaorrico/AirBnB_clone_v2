10788be31f21d08c2393351b2f09b369037dcfd3
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-22 12:41:18 +0100

    wrote the close mrthod

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,27 @@
-#!/usr/bin/python3
-"""
+!/usr/bin/python3
+'''
 index page for flask
-displays status
-"""
-from flask import Flask, jsonify
+displays status and stats
+'''
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def get_status():
-    return jsonify({"status": "OK"})
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def get_count():
+    count_dict = {"amenities": 'Amenity',
+                  "cities": 'City',
+                  "places": 'Place',
+                  "reviews": 'Review',
+                  "states": 'State',
+                  "users": 'User'}
+
+    for key in count_dict.keys():
+        count_dict[key] = storage.count(count_dict.get(key))
+    return jsonify(count_dict)
