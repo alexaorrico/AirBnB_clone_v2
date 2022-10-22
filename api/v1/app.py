509e28f73328708f6422f2e86c69529c8324d217
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-22 10:59:20 +0100

    registered app blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,8 +7,9 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.register_blueprint(appview)
 app.url_map.strict.strict_slashes = False
 
 @app.errorhandler
