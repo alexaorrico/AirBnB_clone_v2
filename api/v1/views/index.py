a0433d75b96f98772b2f5f2adab232d336ca0e09
kevohm <kevinkbet@gmail.com>
2022-10-22 19:00:33 +0000

    finished_task-4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,28 @@
 """ index views """
 from api.v1.views import app_views
 from flask import jsonify
+from models import State
+from models import Place
+from models import storage
+from models import User
+from models import Review
+from models import City
+from models import Amenity
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    data = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return jsonify(data)
