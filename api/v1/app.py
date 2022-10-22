ad7709ec26d7b73d3b0dc1db4df26734a5a0932b
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-22 13:00:11 +0100

    made init file executable

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,19 +12,15 @@ import os
 # Global Flask Application Variable: app
 app = Flask(__name__)
 
-# global strict slashes
+
 app.url_map.strict_slashes = False
 
-# flask server environmental setup
+
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
-
-
-# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
 
-# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
     """
@@ -38,5 +34,4 @@ if __name__ == "__main__":
     """
     MAIN Flask App
     """
-    # start Flask app
     app.run(host=host, port=port)
