2c66743a4eca32e24593ac3f198c19730b5d8a6a
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-22 10:32:09 +0100

    routed the /status with the app_view blueprint

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""index page for flask
+displays status and stats
+"""
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def get_status():
+    return jsonify({"status": "OK"})
