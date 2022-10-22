d7f7d3790b34435d3d218aac100527a763369f20
kevohm <kevinkbet@gmail.com>
2022-10-22 18:28:18 +0000

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', strict_slashes=False)
 def dashboard():
-    return jsonify({'status' : 'OK'})
+    return jsonify({'status': 'OK'})
