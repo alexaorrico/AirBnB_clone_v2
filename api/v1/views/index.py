8c416e8bb81d55b2ce0364b9ec54deaad0292443
felixokolo <pheelix.info@gmail.com>
2022-10-22 17:47:07 +0100

    corrections

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
