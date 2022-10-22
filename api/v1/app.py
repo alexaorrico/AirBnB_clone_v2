f8ed275285f2eef3de170d237249dee5fbe4b539
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-22 12:53:05 +0100

    rewriting the app.py again

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,42 @@
 #!/usr/bin/python3
-'''
-app setup for Airbnb_Clone_v3
-'''
-from flask import Flask, render_template, make_response, jsonify
-from models import storage
-from os import getenv
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
 from api.v1.views import app_views
+from flask import Flask, jsonify, make_response, url_for
+from flasgger import Swagger
+from models import storage
+import os
 
+
+# Global Flask Application Variable: app
 app = Flask(__name__)
+
+# global strict slashes
 app.url_map.strict_slashes = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+# flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
 
+# begin flask page rendering
 @app.teardown_appcontext
-def close(exception):
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
     storage.close()
 
 
 if __name__ == "__main__":
-    apiHost = getenv("HBNB_API_HOST", default="0.0.0.0")
-    apiPort = getenv("HBNB_API_PORT", default=5000)
-    app.run(host=apiHost, port=int(apiPort), threaded=True)
+    """
+    MAIN Flask App
+    """
+    # start Flask app
+    app.run(host=host, port=port)
