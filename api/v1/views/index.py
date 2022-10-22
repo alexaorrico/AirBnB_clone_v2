2da7e44913d0a9cb38fbe42ee41d316d475e15ad
Abuka-Victor <abukavictoro@gmail.com>
2022-10-22 23:55:57 +0100

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """ index module showing the status endpoint """
 import json
+from models import storage
 
 from api.v1.views import app_views
 
 
 @app_views.route('/status')
-def show_stats():
+def show_status():
     return json.dumps({"status": "OK"})
+
+@app_views.route('/api/v1/stats')
+def show_count():
+    new_dict = {
+        "amenities": storage.count("Amenities"),
+        "cities": storage.count("Cities"),
+        "places": storage.count("Places"),
+        "reviews": storage.count("Reviews"),
+        "states": storage.count("States"),
+        "users": storage.count("Users")
+    }
+    return json.dumps(new_dict)
