f9f95dc3de02de6a9dff954cbc76235b84bfbedf
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-22 12:40:54 +0100

    woret the get_count method

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,25 @@
-#!/usr/binpython3
-"""
+#!/usr/bin/python3
+'''
 app setup for Airbnb_Clone_v3
-"""
-
-from flask import Flask, jsonify
+'''
+from flask import Flask, render_template, make_response, jsonify
 from models import storage
-from api.v1.views import app_views
 from os import getenv
+from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(appview)
-app.url_map.strict.strict_slashes = False
+app.url_map.strict_slashes = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views)
 
-@app.errorhandler
-def page_not_found(e):
-    return jsonify({"error": "Not found"}), 404
+
+
 
 @app.teardown_appcontext
 def close(exception):
-    storage.close
+    storage.close()
+
+
 if __name__ == "__main__":
     apiHost = getenv("HBNB_API_HOST", default="0.0.0.0")
     apiPort = getenv("HBNB_API_PORT", default=5000)
