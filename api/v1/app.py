6faa94a8d3460a300c7fdf34cf2ca99176c8729b
tommydebisi <tommydebisi@gmail.com>
2022-10-22 15:06:53 +0100

    :construction: creation of the api module

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+    App module
+"""
+from flask import Flask
+from api import storage
+from api.v1.views import app_views
+from os import getenv
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    """
+        closes the storage
+    """
+    storage.close()
+
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST') if (getenv('HBNB_API_HOST')) else '0.0.0.0'
+    port = getenv('HBNB_API_PORT') if (getenv('HBNB_API_PORT')) else 5000
+
+    app.run(host, port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def view_app():
+    """
+        use of flask blueprint to return 'ok' meesage
+    """
+    return {'status': 'ok'}
+
+@app_views.route('/stats')
+def obj_counts():
+    """
+        count of all the objects
+    """
+    from api import Amenity, City, Place, Review, storage, State, User
+
+    count_dic = dict()
+    count_dic['amenities'] = storage.count(Amenity)
+    count_dic['cities'] = storage.count(City)
+    count_dic['places'] = storage.count(Place)
+    count_dic['reviews'] = storage.count(Review)
+    count_dic['states'] = storage.count(State)
+    count_dic['users'] = storage.count(User)
+
+    return count_dic
