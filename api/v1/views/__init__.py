18bab75967e124f4436c083587e7ca2ba0572e28
kevohm <kevinkbet@gmail.com>
2022-10-22 18:19:43 +0000

    finished task 3

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""views root file for describing blueprints"""
+from flask import Blueprint
+
+app_views = Blueprint("index", __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
