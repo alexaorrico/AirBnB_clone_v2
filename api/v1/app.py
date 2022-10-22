01191e36ddd2e004820fa5b5b263ac06128099dc
tommydebisi <tommydebisi@gmail.com>
2022-10-22 21:07:21 +0100

    :bug: import error fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,8 @@
 from api import storage
 from api.v1.views import app_views
 from flask import Flask
-from os import getenv
 import json
-from api import storage
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
