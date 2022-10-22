8ffc1bed92668cea30479302598ca8dc596b97f3
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-22 12:43:31 +0000

    error page designed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """Starts a Flask Application"""
 from flask import Flask
+from flask import jsonify
+from flask import make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """response to 404 errors"""
+    return jsonify({
+                    'error': 'Not found'
+                   }), 404
 
 
 if __name__ == "__main__":
