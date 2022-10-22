f45eb3a8994d23d4a29bf1fa1fba847abb3a7a53
felixokolo <pheelix.info@gmail.com>
2022-10-22 21:05:40 +0100

    additions

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask import make_response
+from flask import make_response, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
