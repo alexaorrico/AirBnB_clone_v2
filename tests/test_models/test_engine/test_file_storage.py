2857db02d653e73902ea585408340eeeeb59c478
root <adeboyeakingbelure18@yahoo.com>
2022-10-21 21:57:24 -0700

    task 2

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,28 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    def test_get(self):
+        """test method to retrieve one object"""
+        storage = FileStorage()
+        dic = {"name": "Cucurella"}
+        instance = State(**dic)
+        storage.new(instance)
+        storage.save()
+        storage = FileStorage()
+        get_instance = storage.get(State, instance.id)
+        self.assertEqual(get_instance, instance)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """ test method to count the number of objects in storage"""
+        storage = FileStorage()
+        dic = {"name": "Durella"}
+        state = State(**dic)
+        storage.new(state)
+        dic = {"name": "Ohio"}
+        city = City(**dic)
+        storage.new(city)
+        storage.save()
+        c = storage.count()
+        self.assertEqual(len(storage.all()), c)
