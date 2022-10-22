cbcdd94128b68860fe1e71005ad35520be57516f
Abuka-Victor <abukavictoro@gmail.com>
2022-10-23 00:00:49 +0100

    Task 4 fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,12 @@
 """ index module showing the status endpoint """
 import json
 from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.user import User
+from models.city import City
+from models.review import Review
+from models.place import Place
 
 from api.v1.views import app_views
 
@@ -14,11 +20,11 @@ def show_status():
 @app_views.route('/api/v1/stats')
 def show_count():
     new_dict = {
-        "amenities": storage.count("Amenities"),
-        "cities": storage.count("Cities"),
-        "places": storage.count("Places"),
-        "reviews": storage.count("Reviews"),
-        "states": storage.count("States"),
-        "users": storage.count("Users")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return json.dumps(new_dict)
