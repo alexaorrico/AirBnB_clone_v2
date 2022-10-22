db70b113292f20bd25aa497039fd730ce725aa9b
Jesica-akeyo <jessica.akeyo@gmail.com>
2022-10-22 09:08:19 +0000

    API

---
+++ api/v1/views/index.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Index py"""
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type"""
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
