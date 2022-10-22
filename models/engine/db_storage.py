c6e769f4b15d7f4738a5cdd614b33357df8092b9
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-22 17:14:17 +0000

    api for state

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,7 +68,7 @@ class DBStorage:
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
-    
+
     def get(self, cls, obj_id):
         """retreive an an object from storage"""
         try:
