820bea61317f9fe9c82ea85fb0a8a87d04e733a9
SNderi <sharonscarlet8@gmail.com>
2022-10-22 12:24:56 -0700

    Update amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,7 +5,7 @@ default RESTFul API actions. """
 from api.v1.views import app_views
 from flask import jsonify, request, abort, make_response
 from models import storage
-from models.state import Amenity
+from models.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
 def fetch_amenities():
