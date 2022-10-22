183fcb2f7a8646d9e92c9605d491d23de209eb2a
EbenGitHub <abenezergoo@gmail.com>
2022-10-22 13:15:20 +0000

    sts ind

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-Flask routes and returns json status reponse
+Flask route that returns json status response
 """
-from flask import Flask, jsonify, request
 from api.v1.views import app_views
+from flask import jsonify, request
 from models import storage
 
-@app_views.route("/status", methods=["GET"])
-def status():
-    """Function for the status of the route"""
-    if request.method == "GET":
-        return jsonify({"status": "OK"})
 
-@app_views.route("/stats", methods=["GET"])
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    function for status route that returns the status
+    """
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
 def stats():
-    """Function to count all objects of classes"""
+    """
+    function to return the count of all class objects
+    """
     if request.method == 'GET':
         response = {}
         PLURALS = {
