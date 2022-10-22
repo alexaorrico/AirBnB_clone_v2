cfdd57abcd14adbb46c9efe80e3d4ccafdb3b02e
felixokolo <pheelix.info@gmail.com>
2022-10-22 17:41:07 +0100

    corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def tear_down(exception):
     """Release Resources"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, threaded=True)
