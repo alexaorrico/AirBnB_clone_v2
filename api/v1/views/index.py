11f7ae7b3a92f1b03f4b21abe5f92984e6c253a5
abrhamadddis <abrhamaddis32@gmail.com>
2022-10-23 12:51:02 +0300

    some states are done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,12 +4,22 @@
 
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
+from models.engine import count, db_storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     '''return status'''
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats')
+def stats():
+    my_list = {}
+    for stats in db_storage.classes:
+        num = db_storage.count(stats)
+        my_list.update({'stats': num})
+    return jsonify({my_list})
 
 
 if __name__ == "__main__":
