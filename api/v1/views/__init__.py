08a509a39b22e457c8a790bcb491e29a599a44c9
SNderi <sharonscarlet8@gmail.com>
2022-10-23 23:10:37 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,11 +2,11 @@
 """
 Initializes views module
 """
-
-
 from flask import Blueprint
+
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views import *
+
 from api.v1.views.cities import *
 from api.v1.views.index import *
 from api.v1.views.states import *
