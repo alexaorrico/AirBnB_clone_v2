779d34c302641a47094f9f1abb2b422dd2e629c7
abrhamgg <abrhamgg@gmail.com>
2022-10-23 20:29:35 +0300

    update states.pu

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,13 +16,3 @@ def get_states():
     for state in all_states:
         list_states.append(state.to_dict())
     return jsonify(list_states)
-
-
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id):
-    """ Retrieves a specific State """
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-
-    return jsonify(state.to_dict())
