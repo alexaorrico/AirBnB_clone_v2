48d283bb47ee551892a840426245ff8fd6038266
Abuka-Victor <abukavictoro@gmail.com>
2022-10-23 23:07:09 +0100

    fix mysql charset for sqlalchemy

--- models/place.py
+++ models/place.py
@@ -16,13 +16,15 @@ if models.storage_t == 'db':
                           Column('amenity_id', String(60),
                                  ForeignKey('amenities.id', onupdate='CASCADE',
                                             ondelete='CASCADE'),
-                                 primary_key=True))
+                                 primary_key=True), mysql_charset="latin1")
 
 
 class Place(BaseModel, Base):
     """Representation of Place """
     if models.storage_t == 'db':
         __tablename__ = 'places'
+        __table_args__ = (
+            {'mysql_default_charset': 'latin1'})
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
