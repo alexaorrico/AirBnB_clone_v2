dceb6d3d23d0aae92c8997f2350a0a83e16f1f62
SNderi <sharonscarlet8@gmail.com>
2022-10-23 23:53:38 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,10 +23,5 @@ def not_found(e):
 
 
 if __name__ == '__main__':
-    api_host = getenv('HBNB_API_HOST')
-    api_port = getenv('HBNB_API_PORT')
-    if not api_host:
-        api_host = '0.0.0.0'
-    if not api_port:
-        api_port = '5000'
-    app.run(host=api_host, port=api_port, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST") or '0.0.0.0',
+            port=getenv("HBNB_API_PORT") or '5000', threaded=True)
