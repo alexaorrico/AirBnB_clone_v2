1c0077c4c1f99f4c3fa9d706dddf70b3340927ba
abrhamadddis <abrhamaddis32@gmail.com>
2022-10-23 11:49:49 +0300

    pycode style error fixed

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''a blueprint for our application'''
 from flask import Blueprint
-from api.v1.views.index import *
-
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
