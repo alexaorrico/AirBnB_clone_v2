fa02133ebd6f3844ff2c488c8f67329bed67999a
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-10-24 00:18:09 +0300

    Update cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,84 +1,119 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@kevohm 
+Innocent9712
+/
+AirBnB_clone_v3
+Public
+forked from alexaorrico/AirBnB_clone_v2
+Code
+Pull requests
+Actions
+Projects
+Security
+Insights
+AirBnB_clone_v3/api/v1/views/cities.py /
+@Innocent9712
+Innocent9712 Fix bug
+Latest commit 98decf8 8 hours ago
+ History
+ 1 contributor
+Executable File  77 lines (66 sloc)  2.33 KB
+
 #!/usr/bin/python3
-""" City view """
+"""cities.py"""
+
 from api.v1.views import app_views
-from flask import jsonify, make_response, request, abort
+from flask import abort, jsonify, make_response, request
 from models import storage
-from models.state import State
 from models.city import City
+from models.state import State
 
 
-@app_views.route("/states/<string:id>/cities", methods=['GET'],
+@app_views.route('/states/<string:state_id>/cities', methods=['GET'],
                  strict_slashes=False)
-def get_city_by_state(id):
-    """ get all instances of city linkedto a state"""
-    state = storage.get(State, id)
-    cities = storage.all(City)
+def get_cities(state_id):
+    """get city information for all cities in a specified state"""
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
-    else:
-        values = []
-        for value in cities.values():
-            if value.state_id == state.id:
-                values.append(value.to_dict())
-        return jsonify(values)
+    cities = []
+    for city in state.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
 
 
-@app_views.route('cities/<string:id>', methods=['GET'],
+@app_views.route('/cities/<string:city_id>', methods=['GET'],
                  strict_slashes=False)
-def get_city(id):
-    """ get an instatnce of city """
-    city = storage.get(City, id)
+def get_city(city_id):
+    """get city information for specified city"""
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
-    else:
-        return jsonify(city.to_dict())
+    return jsonify(city.to_dict())
 
 
-@app_views.route('cities/<string:id>', methods=['DELETE'],
+@app_views.route('/cities/<string:city_id>', methods=['DELETE'],
                  strict_slashes=False)
-def del_city(id):
-    """delete an instance of city class"""
-    city = storage.get(City, id)
+def delete_city(city_id):
+    """deletes a city based on its city_id"""
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
-    else:
-        city.delete()
-        storage.save()
-        return make_response(jsonify({}), 200)
+    city.delete()
+    storage.save()
+    return (jsonify({}))
 
 
-@app_views.route('states/<string:id>/cities', methods=['POST'],
+@app_views.route('/states/<string:state_id>/cities/', methods=['POST'],
                  strict_slashes=False)
-def add_city_linked_to_state(id):
-    """add an instance of city linked to a state_id"""
-    state = storage.get(State, id)
+def post_city(state_id):
+    """create a new city"""
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
-    if request.get_json():
-        body = request.get_json()
-    else:
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    if "name" not in body:
-        return make_response(jsonify({"error": "Missing name"}), 400)
-    body.update({'state_id': id})
-    new_city = City(**body)
-    new_city.save()
-    return make_response(jsonify(new_city.to_dict()), 201)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'name' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    kwargs = request.get_json()
+    kwargs['state_id'] = state_id
+    city = City(**kwargs)
+    city.save()
+    return make_response(jsonify(city.to_dict()), 201)
 
 
-@app_views.route('cities/<string:id>', methods=['PUT'],
+@app_views.route('/cities/<string:city_id>', methods=['PUT'],
                  strict_slashes=False)
-def update_city(id):
-    """ update an instatnce of city """
-    city = storage.get(City, id)
+def put_city(city_id):
+    """update a city"""
+    city = storage.get("City", city_id)
     if city is None:
         abort(404)
-    if request.get_json():
-        body = request.get_json()
-    else:
-        return make_respone(jsonify({"error": "Not a JSON"}), 400)
-    for k, v in body:
-        if k not in ['id', 'state_id', 'created_at', 'updated_at']:
-            setattr(city, k, v)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    for attr, val in request.get_json().items():
+        if attr not in ['id', 'state_id', 'created_at', 'updated_at']:
+            setattr(city, attr, val)
     city.save()
     return jsonify(city.to_dict())
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+AirBnB_clone_v3/cities.py at master · Innocent9712/AirBnB_clone_v3
