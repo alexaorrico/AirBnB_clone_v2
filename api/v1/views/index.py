76c56eaf12b4ca9235e46cc5cff4df6b7ca68500
samueltezera28 <samueltezera28@gmail.com>
2022-10-23 13:10:09 +0300

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,3 +15,23 @@ def status():
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
