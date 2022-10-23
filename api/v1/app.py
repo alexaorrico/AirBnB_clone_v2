1822d7665b1c68511cb56c58b7e4e67bb0d3d2f8
Abuka-Victor <abukavictoro@gmail.com>
2022-10-23 23:47:34 +0100

    Task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,20 @@
 """ The main flask app for the endpoint """
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def _handle_api_error(ex):
+    if request.path.startswith('/api/v1/'):
+        return jsonify({"error": "Not found"})
+    else:
+        return ex
 
 
 @app.teardown_appcontext
