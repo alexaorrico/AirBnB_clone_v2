80af8c01bfd260e1fcc7d650fca5fe33d420b827
abrhamgg <abrhamgg@gmail.com>
2022-10-23 09:05:45 +0300

    cities

---
+++ api/v1/views/cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""cities route"""
+from models import storage
+from models.state import State
+from models.city import City
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+
+
+@app_views.route('/states/<string:state_id>/cities',
+                 strict_slashes=False, methods=['GET'])
+def get_cities(state_id):
+    """Endpoint to retreive cities"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    all_city = []
+    cities = storage.all(City)
+    for city in state.cities:
+        all_city.append(city.to_dict())
+    return jsonify(all_city)
