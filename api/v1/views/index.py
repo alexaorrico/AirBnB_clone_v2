604d1829d170cdc01dda9547edcfaf53d00b5a2f
melakualehegn <melakualehegn34@gmail.com>
2022-10-23 12:00:37 +0300

    strict slashes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     '''return status'''
     return jsonify({"status": "OK"})
