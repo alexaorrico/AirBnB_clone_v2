399551370cad1572ba6e0105d99f4318ee79f3b9
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-24 07:56:32 +0000

    added state_id get

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,3 +15,13 @@ def get_states():
     for state in states.values():
         all_states.append(state.to_dict())
     return jsonify(all_states)
+
+
+@app_views.route("/states/<state_id>" strict_slashes=False,
+                 methods=["GET"])
+def get_a_state(state_id):
+    """Retrieve a state"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
