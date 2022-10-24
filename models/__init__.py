e16208f0ad99cf5fc74ca045aa588342fab589ca
Ayomide <adefeyisayo998@gmail.com>
2022-10-24 08:20:29 +0000

    rearrabged the clss names

--- models/__init__.py
+++ models/__init__.py
@@ -7,15 +7,15 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-"""CNC - dictionary = { Class Name (string) : Class Type }"""
+"""Classes - dictionary = { Class Name (string) : Class Type }"""
 
 if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
     from models.engine import db_storage
-    CNC = db_storage.DBStorage.CNC
+    Classes = db_storage.DBStorage.Classes
     storage = db_storage.DBStorage()
 else:
     from models.engine import file_storage
-    CNC = file_storage.FileStorage.CNC
+    Classes = file_storage.FileStorage.Classes
     storage = file_storage.FileStorage()
 
 storage.reload()
