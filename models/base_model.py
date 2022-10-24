634dbdcbed6502efda2c1ab7d296973c42b4205a
abrhamgg <abrhamgg@gmail.com>
2022-10-24 10:02:35 +0300

    md5 hash

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+from models import storage_t
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -68,6 +69,9 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if storage_t != 'db':
+            if 'password' in new_dict:
+                del new_dict['password']
         return new_dict
 
     def delete(self):
