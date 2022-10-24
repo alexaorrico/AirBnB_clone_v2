98798257f79d3eddd224a3f4311279641d64c743
surahj <odebodeopeyemi@gmail.com>
2022-10-24 19:43:33 +0100

    Create User object that handles all default RESTFul API actions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,4 +13,5 @@ if (__name__ == 'api.v1.views'):
     from api.v1.views.states import *
     from api.v1.views.cities import *
     from api.v1.views.amenities import *
+    from api.v1.views.users import *
 
