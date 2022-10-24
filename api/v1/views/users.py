e5a0b6018992ce21b38ee3845c71c9a25e048e54
kevohm <kevinkbet@gmail.com>
2022-10-24 15:32:46 +0000

    finished_task_9

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,41 +2,73 @@
 """Routings for amenity-related API requests"""
 
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from models import storage
-from models.amenity import Amenity
+from models.user import User
 
 
-@app_views.route('/amenities', methods=['GET'],
+@app_views.route('/users', methods=['GET'],
                  strict_slashes=False)
-def amenity_methods():
-    """ get all instance of amenity """
-    return jsonify({"GET_ALL": "OK"})
+def get_all_users():
+    """ get all instance of User """
+    if request.method == 'GET':
+        return jsonify([v.to_dict() for v in storage.all(User).values()])
 
 
-@app_views.route('/amenities/<string:id>', methods=['GET'],
+@app_views.route('/users/<string:id>', methods=['GET'],
                  strict_slashes=False)
-def get_single_amenity(id):
-    """"get an instance of amenity """
-    return jsonify({"GET_ONE": "OK"})
+def get_single_user(id):
+    """"get an instance of User """
+    user = storage.get(User, id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
 
 
-@app_views.route('amenities/<string:id>', methods=['DELETE'],
+@app_views.route('users/<string:id>', methods=['DELETE'],
                  strict_slashes=False)
-def del_amenity(id):
-    """ delete an instance of amenity """
-    return jsonify({"DELETE": "OK"})
+def del_user(id):
+    """ delete an instance of User """
+    if request.method == 'DELETE':
+        user = storage.get(User, id)
+        if user is None:
+            abort(404)
+        storage.delete(user)
+        storage.save()
+        return make_response(jsonify({}), 200)
 
 
-@app_views.route('amenities/<string:id>', methods=['POST'],
+@app_views.route('users', methods=['POST'],
                  strict_slashes=False)
-def add_amenity(id):
-    """ create an instance of amenity """
-    return jsonify({"POST": "OK"})
+def add_user():
+    """ create an instance of User """
+    if request.method == 'POST':
+        if not request.get_json():
+            return make_response(jsonify({"error": "Not a JSON"}), 400)
+        body = request.get_json()
+        if "email" not in body:
+            return make_response(jsonify({"error": "Missing email"}), 400)
+        if "password" not in body:
+            return make_response(jsonify({"error": "Missing password"}), 400)
+        new_user = User(**body)
+        new_user.save()
+        return make_response(jsonify(new_user.to_dict()), 201)
 
 
-@app_views.route('/amenities', methods=['PUT'],
+
+@app_views.route('/users/<string:id>', methods=['PUT'],
                  strict_slashes=False)
-def update_amenity():
-    """ update an instance of amenity """
-    return jsonify({"PUT": "OK"})
+def update_user(id):
+    """ update an instance of User """
+    if request.method == 'PUT':
+        user = storage.get(User, id)
+        if user is None:
+            abort(404)
+        if not request.get_json():
+            return make_response(jsonify({"error": "Not a JSON"}), 400)
+        body = request.get_json()
+        for key, value in body.items():
+            if key not in ['id', 'email', 'created_at', 'updated_at']:
+                setattr(user, key, value)
+        user.save()
+        return make_response(jsonify(user.to_dict()), 200)
