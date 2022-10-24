3e1a3a20da723162249e220220361e5214914972
abrhamgg <abrhamgg@gmail.com>
2022-10-24 08:11:01 +0300

    trying users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,7 +3,7 @@
 from models import storage
 from models.user import User
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
+from flask import jsonify
 
 
 @app_views.route('/users', strict_slashes=False, methods=['GET'])
@@ -14,24 +14,3 @@ def get_users():
     for v in users.values():
         all_users.append(v.to_dict())
     return jsonify(all_users)
-
-
-@app_views.route('/users/<string:user_id>', strict_slashes=False,)
-def get_user(user_id):
-    """Endpoint to retreive a user"""
-    user = storage.get(User, user_id)
-    if not user:
-        abort(404)
-    return jsonify(user.to_dict())
-
-
-@app_views.route('/users/<string:user_id>', strict_slashes=False,
-                 methods=['DELETE'])
-def delete_user(user_id):
-    """Endpoint to delete a user"""
-    user = storage.get(User, user_id)
-    if not user:
-        abort(404)
-    storage.delete(user)
-    storage.save()
-    return jsonify({}), 200
