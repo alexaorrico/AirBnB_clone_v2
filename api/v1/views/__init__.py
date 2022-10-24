ac1b6737ab43892cd65975bab45e9e1e5327d2ed
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-10-25 00:35:42 +0300

    all view objects

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,12 @@
 """Initialize Blueprint views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.users import *
 from flask import Blueprint
 
 
