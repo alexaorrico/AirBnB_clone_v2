f63518738a1abafa470df1ccde78516220ce63a7
SNderi <sharonscarlet8@gmail.com>
2022-10-24 20:18:11 +0300

    Modify cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""
-Flask app
-"""
-
-
+""" Flask app """
 from os import getenv
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
-CORS(app, orgins='0.0.0.0')
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
