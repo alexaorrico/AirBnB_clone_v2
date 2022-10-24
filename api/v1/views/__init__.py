e4c7e98225edc9b050af257475ab1ea70c459c15
surahj <odebodeopeyemi@gmail.com>
2022-10-24 20:24:12 +0100

    Create Place objects that handles all default RESTFul API actionsz

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -14,4 +14,5 @@ if (__name__ == 'api.v1.views'):
     from api.v1.views.cities import *
     from api.v1.views.amenities import *
     from api.v1.views.users import *
+    from api.v1.views.places import *
 
