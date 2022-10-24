78d70ecf756379882ba06e89384eeda9e8fc47ec
abrhamgg <abrhamgg@gmail.com>
2022-10-24 10:09:40 +0300

    default parameter

--- models/base_model.py
+++ models/base_model.py
@@ -59,7 +59,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, save_fs=None):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -69,7 +69,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if storage_t != 'db':
+        if save_fs is None:
             if 'password' in new_dict:
                 del new_dict['password']
         return new_dict
