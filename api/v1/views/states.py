778b77f89e3f6239e14f3929b64ff52a2df4c91d
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-24 08:29:31 +0000

    one function

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,10 +8,18 @@ from models import storage
 
 @app_views.route("/states", strict_slashes=False,
                  methods=["GET"])
-def get_states():
+@app_views.route("/states/<state_id>", strict_slashes=False,
+                 methods=["GET"])
+def get_states(state_id=None):
     """Retrieves all states"""
-    states = storage.all(States)
-    all_states = []
-    for state in states.values():
-        all_states.append(state.to_dict())
-    return jsonify(all_states)
+    if not state_id:
+        states = storage.all(States)
+        all_states = []
+        for state in states.values():
+            all_states.append(state.to_dict())
+        return jsonify(all_states)
+    else:
+        state = storage.get(state_id)
+        if not state:
+           abort(404)
+        return jsonify(state.to_dict())
