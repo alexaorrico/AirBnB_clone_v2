3761adf902df53be483879c2aaa6cd2521672afa
Tsion Moges <tsionmoges25@gmail.com>
2022-10-24 22:57:20 +0300

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,11 +5,12 @@ Contains the TestAmenityDocs classes
 
 from datetime import datetime
 import inspect
-import models
 from models import amenity
 from models.base_model import BaseModel
 import pep8
 import unittest
+import os
+from sqlalchemy.orm.attributes import InstrumentedAttribute
 Amenity = amenity.Amenity
 
 
@@ -66,15 +67,22 @@ class TestAmenity(unittest.TestCase):
         self.assertTrue(hasattr(amenity, "id"))
         self.assertTrue(hasattr(amenity, "created_at"))
         self.assertTrue(hasattr(amenity, "updated_at"))
-
+       
+     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                     "Testing DBStorage")
     def test_name_attr(self):
         """Test that Amenity has attribute name, and it's as an empty string"""
         amenity = Amenity()
         self.assertTrue(hasattr(amenity, "name"))
-        if models.storage_t == 'db':
-            self.assertEqual(amenity.name, None)
-        else:
-            self.assertEqual(amenity.name, "")
+        self.assertEqual(amenity.name, "")
+        
+     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                     "Testing FileStorage")
+    def test_name_attr_db(self):
+        """Test for DBStorage name attribute"""
+        amenity = Amenity()
+        self.assertTrue(hasattr(Amenity, "name"))
+        self.assertIsInstance(Amenity.name, InstrumentedAttribute)        
 
     def test_to_dict_creates_dict(self):
         """test to_dict method creates a dictionary with proper attrs"""
@@ -82,7 +90,6 @@ class TestAmenity(unittest.TestCase):
         print(am.__dict__)
         new_d = am.to_dict()
         self.assertEqual(type(new_d), dict)
-        self.assertFalse("_sa_instance_state" in new_d)
         for attr in am.__dict__:
             if attr is not "_sa_instance_state":
                 self.assertTrue(attr in new_d)
