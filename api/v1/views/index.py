35c1a778a690f1e41943095a452fadfb7e0dd9e5
NikkyXO <alexisdame2017@gmail.com>
2022-10-24 09:57:17 +0100

    Rest API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,25 @@
 #!/usr/bin/python3
-"""
-Flask route that returns json status response
-"""
+
+"""View for index that handles all default RESTFul API actions"""
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify
 from models import storage
+from models.state import  State
 
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """
-    function for status route that returns the status
-    """
-    if request.method == 'GET':
-        resp = {"status": "OK"}
-        return jsonify(resp)
+@app_views('/status')
+def get_status():
+    """Check status of route"""
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    """
-    function to return the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+@app_views('/stats')
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    objects = {"amenities": 'Amenity', "cities": 'City', 
+               "places": 'Place', "reviews": 'Review',
+               "states": 'State', "users": 'User'}
+
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,60 +1,52 @@
 #!/usr/bin/python3
-"""
-    Flask route that returns json response
-"""
+
+from flask import request, abort, jsonify
 from api.v1.views import app_views
-from flask import abort, jsonify, request
-from models import storage, CNC
-from flasgger.utils import swag_from
+from models import storage, User
 
 
-@app_views.route('/users/', methods=['GET', 'POST'])
-@swag_from('swagger_yaml/users_no_id.yml', methods=['GET', 'POST'])
-def users_no_id(user_id=None):
-    """
-        users route that handles http requests with no ID given
-    """
+@app_views.route('/api/v1/users',
+                 methods=['GET', 'POST'], strict_slashes=False)
+def get_users():
 
     if request.method == 'GET':
-        all_users = storage.all('User')
-        all_users = [obj.to_json() for obj in all_users.values()]
-        return jsonify(all_users)
-
-    if request.method == 'POST':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        if req_json.get('email') is None:
-            abort(400, 'Missing email')
-        if req_json.get('password') is None:
-            abort(400, 'Missing password')
-        User = CNC.get('User')
-        new_object = User(**req_json)
-        new_object.save()
-        return jsonify(new_object.to_json()), 201
+        return jsonify([value.to_dict()
+                        for value in storage.all('User').values()])
+    elif request.method == 'POST':
+        post = request.get_json()
+        if post is None or type(post) != dict:
+            return jsonify({'error': 'Not a JSON'}), 400
+        elif post.get('email') is None:
+            return jsonify({'error': 'Missing email'})
+        elif post.get('password') is None:
+            return jsonify({'error': 'Missing password'}), 400
+        new_user = User(**post)
+        new_user.save()
+        return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>', methods=['GET', 'DELETE', 'PUT'])
-@swag_from('swagger_yaml/users_id.yml', methods=['GET', 'DELETE', 'PUT'])
-def user_with_id(user_id=None):
-    """
-        users route that handles http requests with ID given
-    """
-    user_obj = storage.get('User', user_id)
-    if user_obj is None:
-        abort(404, 'Not found')
+@app_views.route('/api/v1/users/<string:user_id>',
+                 methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
+def get_user_id(user_id):
+    """Retrieves the list of the User objects"""
+    user = storage.get('User', user_id)
+    if user is None:
+        abort(404)
 
     if request.method == 'GET':
-        return jsonify(user_obj.to_json())
+        return jsonify(user.to_dict())
 
-    if request.method == 'DELETE':
-        user_obj.delete()
-        del user_obj
+    elif request.method == 'PUT':
+        post = request.get_json()
+        if post is None or type(post) != dict:
+            return jsonify({'error': 'Not a JSON'})
+        for key, value in post.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(user, key, value)
+                storage.save()
+                return jsonify(user.to_dict()), 200
+
+    elif request.method == 'DELETE':
+        storage.delete(user)
+        storage.save()
         return jsonify({}), 200
-
-    if request.method == 'PUT':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        user_obj.bm_update(req_json)
-        return jsonify(user_obj.to_json()), 200
