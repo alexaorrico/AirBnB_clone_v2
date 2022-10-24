29e53a387a5931ddd6b7667471a1242feb20ec2f
abrhamgg <abrhamgg@gmail.com>
2022-10-24 20:29:31 +0300

    trying to fix

--- api/v1/views/ba.py
+++ api/v1/views/ba.py
@@ -1,70 +1,86 @@
 #!/usr/bin/python3
-"""cities route"""
+""" objects that handle all default RestFul API actions for States """
+from models.state import State
 from models import storage
-from models.user import User
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
+from flask import abort, jsonify, make_response, request
 
 
-@app_views.route('/users/', strict_slashes=False, methods=['GET'])
-def get_users():
-    """Endpoint to retreive all users"""
-    all_users = []
-    users = storage.all(User)
-    for v in users.values():
-        all_users.append(v.to_dict())
-    return jsonify(all_users)
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """
+    Retrieves the list of all State objects
+    """
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
 
 
-@app_views.route('/users/<string:user_id>', strict_slashes=False,)
-def get_user(user_id):
-    """Endpoint to retreive a user"""
-    user = storage.get(User, user_id)
-    if not user:
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ Retrieves a specific State """
+    state = storage.get(State, state_id)
+    if not state:
         abort(404)
-    return jsonify(user.to_dict())
+
+    return jsonify(state.to_dict())
 
 
-@app_views.route('/users/<string:user_id>', strict_slashes=False,
-                 methods=['DELETE'])
-def delete_user(user_id):
-    """Endpoint to delete a user"""
-    user = storage.get(User, user_id)
-    if not user:
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """
+    Deletes a State Object
+    """
+
+    state = storage.get(State, state_id)
+
+    if not state:
         abort(404)
-    storage.delete(user)
+
+    storage.delete(state)
     storage.save()
-    return jsonify({}), 200
+
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route('/users', strict_slashes=False, methods=['POST'])
-def post_user():
-    """Endpoint to create a user"""
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """
+    Creates a State
+    """
     if not request.get_json():
         abort(400, description="Not a JSON")
-    if 'email' not in request.get_json():
-        abort(400, description='Missing email')
-    if 'password' not in request.get_json():
-        abort(400, description='Missing password')
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
     data = request.get_json()
-    user = User(**data)
-    user.save()
-    return jsonify(user.to_dict()), 201
+    instance = State(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
 
 
-@app_views.route('/users/<string:user_id>',
-                 strict_slashes=False, methods=['PUT'])
-def update_user(user_id):
-    """Endpoint to update a user"""
-    user = storage.get(User, user_id)
-    if not user:
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """
+    Updates a State
+    """
+    state = storage.get(State, state_id)
+
+    if not state:
         abort(404)
+
     if not request.get_json():
         abort(400, description="Not a JSON")
+
+    ignore = ['id', 'created_at', 'updated_at']
+
     data = request.get_json()
-    ignore = ['id', 'email', 'created_at', 'updated_at']
-    for k, v in data.items():
-        if k not in ignore:
-            setattr(user, k, v)
-    user.save()
-    return jsonify(user.to_dict()), 200
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
