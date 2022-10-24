5e3ebdfa123c1204474a2177f06fa4ac0e3c7007
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-25 00:24:28 +0300

    removed the prefix last slash

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,4 +9,4 @@ from api.v1.views.index import *
 
 from api.v1.views.amenities import *
 
-app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1/')
+app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
