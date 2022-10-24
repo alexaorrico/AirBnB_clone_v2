d5716d0b4327313d8c3e2324931b82438296c2a4
jacobnzala <63255452+jacobnzala@users.noreply.github.com>
2022-10-24 20:32:38 +0200

    Update amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,71 +1,73 @@
 #!/usr/bin/python3
-"""Contains amenities module"""
+"""
+View for Amenities that handles all RESTful API actions
+"""
 
-from flask import jsonify, abort, request, make_response
-from api.v1.views import app_views
+from flask import jsonify, request, abort
 from models import storage
 from models.amenity import Amenity
+from api.v1.views import app_views
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-@app_views.route('/amenities/<amenity_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_amenity(amenity_id=None):
-    """Retrieves the list of all Amenity objects
-    or a specific Amenity object"""
-    if amenity_id is None:
-        amenities = [amenity.to_dict() for amenity
-                     in storage.all("Amenity").values()]
-        return jsonify(amenities)
+def amenities_all():
+    """ returns list of all Amenity objects """
+    amenities_all = []
+    amenities = storage.all("Amenity").values()
+    for amenity in amenities:
+        amenities_all.append(amenity.to_json())
+    return jsonify(amenities_all)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def amenity_get(amenity_id):
+    """ handles GET method """
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None:
         abort(404)
-    return jsonify(amenity.to_dict())
+    amenity = amenity.to_json()
+    return jsonify(amenity)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def del_amenity(amenity_id=None):
-    """Deletes a Amenity object"""
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def amenity_delete(amenity_id):
+    """ handles DELETE method """
+    empty_dict = {}
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None:
         abort(404)
-    amenity.delete()
+    storage.delete(amenity)
     storage.save()
-    return jsonify({})
+    return jsonify(empty_dict), 200
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-def post_amenity():
-    """Creates a Amenity object"""
-    try:
-        req = request.get_json()
-    except:
-        req = None
-    if req is None:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'name' not in req:
-        return make_response(jsonify({'error': "Missing name"}), 400)
-    amenity = Amenity(**req)
+def amenity_post():
+    """ handles POST method """
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    if 'name' not in data:
+        abort(400, "Missing name")
+    amenity = Amenity(**data)
     amenity.save()
-    return make_response(jsonify(amenity.to_dict()), 201)
+    amenity = amenity.to_json()
+    return jsonify(amenity), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
-                 strict_slashes=False)
-def put_amenity(amenity_id=None):
-    """Updates a Amenity object"""
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def amenity_put(amenity_id):
+    """ handles PUT method """
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None:
         abort(404)
-    try:
-        req = request.get_json()
-    except:
-        req = None
-    if req is None:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    for key, val in req.items():
-        if key not in ('id', 'created_at', 'updates_at'):
-            setattr(amenity, key, val)
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    for key, value in data.items():
+        ignore_keys = ["id", "created_at", "updated_at"]
+        if key not in ignore_keys:
+            amenity.bm_update(key, value)
     amenity.save()
-    return jsonify(amenity.to_dict())
+    amenity = amenity.to_json()
+    return jsonify(amenity), 200
