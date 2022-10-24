162d11c237911f77ad53fcb4012d808b8946af9c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-10-24 13:30:04 +0100

    a better approach

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,8 +13,8 @@ def status():
     function for status route that returns the status
     """
     if request.method == 'GET':
-        response = {"status": "OK"}
-        return jsonify(response)
+        resp = {"status": "OK"}
+        return jsonify(resp)
 
 
 @app_views.route('/stats', methods=['GET'])
@@ -23,8 +23,8 @@ def stats():
     function to return the count of all class objects
     """
     if request.method == 'GET':
-        result = {}
-        class_list = {
+        response = {}
+        PLURALS = {
             "Amenity": "amenities",
             "City": "cities",
             "Place": "places",
@@ -32,6 +32,6 @@ def stats():
             "State": "states",
             "User": "users"
         }
-        for key, value in class_list.items():
-            result[value] = storage.count(key)
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
         return jsonify(response)
