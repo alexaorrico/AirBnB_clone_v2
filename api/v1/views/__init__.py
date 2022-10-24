c961713e4270734dd261dc3c815bc2e8cc38cef0
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-10-25 00:21:47 +0300

    update to pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """Initialize Blueprint views"""
-
-
+from api.v1.views.index import *
+from api.v1.views.states import *
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
-from api.v1.views.states import *
