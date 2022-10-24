7487c2a90459e37123a13f40dda4100902922565
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-25 00:17:09 +0300

    a new view for Amenity objects that handles all default RESTFul API actions

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python3
 """
-    API view related to Amenity objects that handles all the default
-    actions.
+a new view for Amenity objects that handles
+all default RESTFul API actions
 """
-import requests
+
+
+import json
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
-import json
+from flask import Flask, make_response, jsonify, request
 from werkzeug.exceptions import BadRequest, NotFound
-from flask import Flask, request, jsonify, make_response, abort
-from flasgger import swag_from
 
 
 @app_views.route('/amenities', methods=['GET'])
-@swag_from('../swagger_configs/amenities/list.yml')
-def amenities_list() -> json:
+def all_amenities():
     """
-    Retrieves the list of all Amenity objects.
+    Retrieves the list of all Amenity objects
+
     Returns:
-        json: List of Amenity objects with status code 200.
+        json: Wanted Amenity object with status code 200.
     """
     amenities = storage.all(Amenity)
-    list = []
-    for key, amenity in amenities.items():
-        list.append(amenity.to_dict())
-    return make_response(jsonify(list), 200)
+    amenities_list = []
+
+    for amenity in amenities.items():
+        amenities_list.append(amenity.to__dict())
+    return make_response(jsonify(amenities_list), 200)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'])
-@swag_from('../swagger_configs/amenities/show.yml')
-def amenity_show(amenity_id) -> json:
+@app_views.route('/api/v1/amenities/<amenity_id>', methods=['GET'])
+def single_amenity(amenity_id):
     """
     Retrieves a specified Amenity object.
+
     Args:
-        amenity_id : ID of the wanted Amenity object.
+        amenity_id : ID of the specified Amenity object.
+
     Raises:
         NotFound: Raises a 404 error if amenity_id
         is not linked to any Amenity object.
+
     Returns:
         json: Wanted Amenity object with status code 200.
     """
@@ -46,22 +49,25 @@ def amenity_show(amenity_id) -> json:
     if amenity is None:
         raise NotFound
 
-    return make_response(jsonify(amenity.to_dict()), 200)
+    return make_response(jsonify(amenity.to__dict()), 200)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
-@swag_from('../swagger_configs/amenities/delete.yml')
-def amenity_delete(amenity_id) -> json:
+@app_views.route('/api/v1/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
     """
     Deletes a specified Amenity object.
+
     Args:
         amenity_id : ID of the wanted Amenity object.
+
     Raises:
         NotFound: Raises a 404 error if amenity_id
         is not linked to any Amenity object.
+
     Returns:
         json: Empty dictionary with the status code 200.
     """
+
     amenity = storage.get(Amenity, amenity_id)
 
     if amenity is None:
@@ -73,52 +79,55 @@ def amenity_delete(amenity_id) -> json:
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/amenities/', methods=['POST'])
-@swag_from('../swagger_configs/amenities/create.yml')
-def amenity_create() -> json:
+@app_views.route('/api/v1/amenities/', methods=['POST'])
+def add_amenity():
     """
     Creates a new Amenity object.
+
     Error cases:
         BadRequest: If the given data is not a
         valid json or if the key 'name' is not
         present sends status code 400.
+
     Returns:
         json: The new Amenity with the status code 201.
     """
-    if not request.json:
+    amenity = Amenity(**request.get_json())
+
+    if not request.get_json:
         return make_response('Not a JSON', 400)
 
-    if 'name' not in request.get_json().keys():
-        return make_response('Missing name', 400)
+    if 'name' not in request.get_json.keys():
+        return make_response('Missing name', 404)
 
-    amenity = Amenity(**request.get_json())
     amenity.save()
 
     return make_response(jsonify(amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
-@swag_from('../swagger_configs/amenities/update.yml')
-def amenity_update(amenity_id) -> json:
+@app_views.route('/api/v1/amenities/<amenity_id>', methods=['PUT'])
+def update_amenity(amenity_id):
     """
     Update a specified Amenity object.
+
     Args:
         amenity_id : Id of the wanted Amenity object.
+
     Returns:
         json: The updated Amenity object with the status code 200.
     """
     amenity = storage.get(Amenity, amenity_id)
 
+    if not request.get_json:
+        return make_response('Not a JSON', 400)
+
     if amenity is None:
         raise NotFound
 
-    if not request.json:
-        return make_response('Not a JSON', 400)
-
-    for key, value in request.get_json().items():
+    for key, amen in request.get_json().items():
         if key not in ('id', 'created_at', 'updated_at'):
-            amenity.__setattr__(key, value)
+            amenity.__setattr__(key, amen)
 
     amenity.save()
 
-    return make_response(jsonify(amenity.to_dict()), 200)
+    return make_response(jsonify(amenity.to__dict()), 200)
