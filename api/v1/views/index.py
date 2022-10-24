166853cabb80900ddb1bbca3b6f8cbcd53ed144e
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-24 10:48:11 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""index file"""
-
-from api.v1.views import app_views
+"""Retrieves number for each type"""
 from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.place import Place
-from models.city import City
-from models.review import Review
-from models.state import State
-from models.user import User
+from api.v1.views import app_views
 
 
-@app_views.route('/status')
+@app_views.route("/status", strict_slashes=False)
 def status():
-    """returns status"""
-    return jsonify({"status": "OK"})
+    """Returns status"""
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/stats')
+@app_views.route("/stats", strict_slashes=False)
 def stats():
-    """stats"""
-    return jsonify({
-                    "amenities": storage.count(Amenity),
-                    "cities": storage.count(City),
-                    "places": storage.count(Place),
-                    "reviews": storage.count(Review),
-                    "states": storage.count(State),
-                    "users": storage.count(User)
-                    })
+    """Returns stats"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
