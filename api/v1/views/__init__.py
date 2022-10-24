f4cc1ea7b296b4b1000c2f0dcebfdbcd8949a1fb
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-25 00:09:01 +0300

    import on top of the file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,10 +4,9 @@ view Blueprint
 """
 from flask import Blueprint
 
-
-app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1/')
-
 # import views after the site has been defined
 from api.v1.views.index import *
 
 from api.v1.views.amenities import *
+
+app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1/')
