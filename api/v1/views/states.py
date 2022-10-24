4e52df6571e316cc016dcd467eb0f62abdd50730
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-10-24 07:49:32 +0000

    fix typo error

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,17 +2,16 @@
 """create route for states"""
 from api.v1.views import app_views
 from flask import abort, jsonify, request
-from models.state import  State
+from models.state import State
 from models import storage
 
 
 @app_views.route("/states", strict_slashes=False,
                  methods=["GET"])
-def get_states()
+def get_states():
     """Retrieves all states"""
     states = storage.all(States)
-    all_states=[]
+    all_states = []
     for state in states.values():
         all_states.append(state.to_dict())
     return jsonify(all_states)
-
