52ad7378cd23cd31c2adb3c4cdae6bf4d19d0781
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-25 00:02:27 +0300

    Tests

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,4 +7,6 @@ from flask import Blueprint
 
 app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1/')
 
+# import views after the site has been defined
 from api.v1.views.index import *
+from api.v1.views.amenities import *
