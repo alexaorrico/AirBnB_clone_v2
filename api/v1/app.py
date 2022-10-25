da701599f300ab233553ae928373a2a705d2acf9
Pascalchinedu <pascalchinedu2000@gmail.com>
2022-10-25 05:42:11 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,24 +14,24 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
-        """ This method close the session
-            """
-                storage.close()
+    """ This method close the session
+    """
+    storage.close()
 
 
-                @app.errorhandler(404)
-                def error404(error):
-                        """ error 404 handler """
-                            return jsonify({
-                                            'error': 'Not found'
-                                                    }), 404
+@app.errorhandler(404)
+def error404(error):
+    """ error 404 handler """
+    return jsonify({
+            'error': 'Not found'
+        }), 404
 
 
-                            if __name__ == '__main__':
-                                    host = getenv('HBNB_API_HOST')
-                                        port = getenv('HBNB_API_PORT')
-                                            if host is None:
-                                                        host = '0.0.0.0'
-                                                            if port is None:
-                                                                        port = 5000
-                                                                            app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if host is None:
+        host = '0.0.0.0'
+    if port is None:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
