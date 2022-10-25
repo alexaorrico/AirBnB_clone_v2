d5046800cbfbd10b53e27c4768ae853af22d5c4d
smith2eric <ericsmithsonian2@gmail.com>
2022-10-25 16:15:46 +0100

    [fix]: resolved PEP8 validation error

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -27,7 +27,9 @@ def get_state_by_id(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
+@app_views.route(
+    "/states/<state_id>", methods=["DELETE"], strict_slashes=False
+    )
 def delete_state_by_id(state_id):
     """This function delete_state_by_id"""
     state = storage.get("State", state_id)
