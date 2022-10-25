324aa7102a200f6c27ec5983524b7e99a35499a6
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-25 08:41:43 +0300

    Update user.py
    Security improvements!

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """ holds class User"""
 import models
 from models.base_model import BaseModel, Base
+import hashlib
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
@@ -27,3 +28,14 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+    
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        """Hashes a user password with MD5"""
+        encryption = hashlib.md5(password.encode())
+        self._password = encryption.hexdigest()   
+      
