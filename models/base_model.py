bdcceec1db6edd547df22af64857658162368ffe
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-25 08:52:04 +0300

    Update base_model.py
     Security improvements!

--- models/base_model.py
+++ models/base_model.py
@@ -67,9 +67,8 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-         if not password:
+        new_dict.pop('_sa_instance_state', None)
+        if not password:
             new_dict.pop('password', None)
         return new_dict
 
