0decb84ced983348fcc3639844d94878f495ce24
Aysuarex <aysuarex@gmail.com>
2022-10-25 23:53:39 +0100

    AirBnB files update

--- models/user.py
+++ models/user.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-"""
-User Class from Models Module
-"""
+""" holds class User"""
 import hashlib
-import os
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float
-STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """
-        User class handles all application users
-    """
-    if STORAGE_TYPE == "db":
+    """Representation of a user """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-
-        places = relationship('Place', backref='user', cascade='delete')
-        reviews = relationship('Review', backref='user', cascade='delete')
+        email = Column(String(128),
+                       nullable=False)
+        _password = Column('password',
+                           String(128),
+                           nullable=False)
+        first_name = Column(String(128),
+                            nullable=True)
+        last_name = Column(String(128),
+                           nullable=True)
+        places = relationship("Place",
+                              backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review",
+                               backref="user",
+                               cascade="all, delete-orphan")
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+        email = ""
+        _password = ""
+        first_name = ""
+        last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """
-            instantiates user object
-        """
-        if kwargs:
-            pwd = kwargs.pop('password', None)
-            if pwd:
-                User.__set_password(self, pwd)
+        """initializes user"""
         super().__init__(*args, **kwargs)
 
-    def __set_password(self, pwd):
-        """
-            custom setter: encrypts password to MD5
-        """
-        secure = hashlib.md5()
-        secure.update(pwd.encode("utf-8"))
-        secure_password = secure.hexdigest()
-        setattr(self, "password", secure_password)
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        """hashing password values"""
+        self._password = hashlib.md5(pwd.encode()).hexdigest()
