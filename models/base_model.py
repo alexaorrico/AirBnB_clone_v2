a16f7b9b49e42b13c93153d0db2ef3c695a8fc3a
John Matahai Githinji <60788095+johngithinji-cs@users.noreply.github.com>
2022-10-25 08:48:34 +0300

    Update base_model.py
     Security improvements!

--- models/base_model.py
+++ models/base_model.py
@@ -6,6 +6,7 @@ Contains class BaseModel
 from datetime import datetime
 import models
 from os import getenv
+import hashlib
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
@@ -58,7 +59,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, password=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,6 +69,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+         if not password:
+            new_dict.pop('password', None)
         return new_dict
 
     def delete(self):
