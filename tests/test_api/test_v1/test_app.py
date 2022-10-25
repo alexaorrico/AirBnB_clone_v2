fc609f6c3873db444992577edd83dff12ebb5939
GetacherD <getacher.d@gmail.com>
2022-10-25 08:48:31 +0300

    test

--- tests/test_api/test_v1/test_app.py
+++ tests/test_api/test_v1/test_app.py
@@ -13,34 +13,6 @@ class TestIntegrations(TestCase):
         """ remove self.app """
         self.app = None
 
-    def test_statusroute(self):
-        """tests the status route returns JSON-formatted
-        response correctly"""
-        response = self.app.get('/api/v1/status')
-        assert b'{\n  "status": "OK"\n}\n' in response.data
-        self.assertEqual(response.status_code, 200)
-
-    def test_404error(self):
-        """tests the handing of 404 error returns JSON-formatted
-        status code response"""
-        response = self.app.get('/api/v1/nop')
-        self.assertEqual(b'{\n  "error": "Not found"\n}\n', response.data)
-        self.assertEqual(response.status_code, 404)
-
-    def test_stat(self):
-        """tests the stats route"""
-        response = self.app.get('/api/v1/stats')
-        data = response.data
-        self.assertTrue(data in response.data)
-        self.assertEqual(response.status_code, 200)
-
-    # States view
-    def test_get_states(self):
-        """tests that all state objects are returned in JSON"""
-        response = self.app.get('api/v1/states')
-        self.assertEqual(response.status_code, 200)
-        # print(response.data)
-
 
 if __name__ == '__main__':
     unittest.main()
