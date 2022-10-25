6cfe871aa6232a555b26d1808008203dbe7fea44
Pascalchinedu <pascalchinedu2000@gmail.com>
2022-10-25 05:49:55 -0700

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,19 +9,19 @@ from models import storage
 
 @app_views.route("/status", strict_slashes=False)
 def status():
-        """ Return the status of the application
-            """
-                text = {"status": "OK"}
-                    return jsonify(text)
+    """ Return the status of the application
+    """
+    text = {"status": "OK"}
+    return jsonify(text)
 
 
-                @app_views.route("/stats", strict_slashes=False)
-                def stats():
-                        """ Return the stats of the application
-                            """
-                                classes = {"amenities": "Amenity", "cities": "City",
-                                                       "places": "Place", "reviews": "Review",
-                                                                      "states": "State", "users": "User"}
-                                    for key, value in classes.items():
-                                                classes[key] = storage.count(value)
-                                                    return jsonify(classes)
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """ Return the stats of the application
+    """
+    classes = {"amenities": "Amenity", "cities": "City",
+               "places": "Place", "reviews": "Review",
+               "states": "State", "users": "User"}
+    for key, value in classes.items():
+        classes[key] = storage.count(value)
+    return jsonify(classes)
