88bddd98461b24969330436c9376626d7fd656db
root <ucheogbonnak@gmail.com>
2022-10-27 19:46:29 -0700

    fixed errors for status

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,11 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_session(exception):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,8 @@ the index file
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def index():
+def status():
     """ Returns status """
     return jsonify(status="OK")
