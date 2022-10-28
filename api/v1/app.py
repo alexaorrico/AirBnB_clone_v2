4a95f87c1990c6ae761c042fab2925a57977b4b8
root <ucheogbonnak@gmail.com>
2022-10-27 21:32:28 -0700

    added error page messge

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ app """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown_session(exception):
     """ """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Returns error 404 """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
