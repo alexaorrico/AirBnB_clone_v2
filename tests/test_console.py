68d65675ae9956fdc67e5354f2b04fade3699ef8
Alatise Oluwaseun <holynation667@gmail.com>
2022-10-31 02:41:54 +0100

    fix: Error in tests and codebase

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,6 +3,8 @@
 Contains the class TestConsoleDocs
 """
 
+import sys
+from io import StringIO
 import console
 import inspect
 import pep8
@@ -12,6 +14,36 @@ HBNBCommand = console.HBNBCommand
 
 class TestConsoleDocs(unittest.TestCase):
     """Class for testing documentation of the console"""
+    def setUp(self):
+        '''setup for'''
+        self.backup = sys.stdout
+        self.capt_out = StringIO()
+        sys.stdout = self.capt_out
+
+    def tearDown(self):
+        ''''''
+        sys.stdout = self.backup
+
+    def create(self):
+        ''' create an instance of the HBNBCommand class'''
+        return HBNBCommand()
+
+    def test_quit(self):
+        ''' Test quit exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("quit"))
+
+    def test_EOF(self):
+        ''' Test EOF exists'''
+        console = self.create()
+        self.assertTrue(console.onecmd("EOF"))
+
+    def test_all(self):
+        ''' Test all exists'''
+        console = self.create()
+        console.onecmd("all")
+        self.assertTrue(isinstance(self.capt_out.getvalue(), str))
+
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
