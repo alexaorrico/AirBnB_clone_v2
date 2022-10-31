3acf4376f241deb4954c30e48fe368965cb8b832
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-31 07:22:09 +0300

    changed first parameter of app_views BluePrint to  app_views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -16,4 +16,4 @@ from api.v1.views.states import *
 from api.v1.views.places_reviews import *
 
 
-app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
