8edae357b318aed8c58fd18b0395d764159b7f6d
David_Motari <davemotari22@gmail.com>
2022-11-14 18:38:40 +0300

    Style correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,17 +11,11 @@ from models import storage
 from os import getenv
 
 
-
 app = Flask(__name__)
-app.config['JSON_PRETTYPRINT_REGULAR'] = True
+app.config["JSON_PRETTYPRINT_REGULAR"] = True
 
 app.register_blueprint(app_views)
-cors = CORS(
-    app,
-    resources={
-        r"api/v1/*": {"origins": "0.0.0.0"}
-    }
-)
+cors = CORS(app, resources={r"api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_app_context
@@ -37,19 +31,19 @@ def not_found(error):
     """Return error in JSON and status"""
     return make_response(jsonify({"error": "Not Found"}), 404)
 
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone RESTFUL API',
-    'uiversion': 3,
-    
+
+app.config["SWAGGER"] = {
+    "title": "AirBnB clone RESTFUL API",
+    "uiversion": 3,
 }
 
 Swagger(app)
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
+    host = getenv("HBNB_API_HOST")
+    port = getenv("HBNB_API_PORT")
     if not host:
-        host = '0.0.0.0'
+        host = "0.0.0.0"
     if not port:
         port = 5000
     app.run(host=host, port=port, threaded=True)
