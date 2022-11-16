bd3b9ceac2b1fe75f4c1de154080f148b2550e6b
David_Motari <davemotari22@gmail.com>
2022-11-16 04:10:42 +0300

    Task 3 correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,14 +6,16 @@ initialization of app
 from api.v1.views import app_views
 from flask_cors import CORS
 from flask import Flask, jsonify, make_response
-from flasgger import Swagger
 from models import storage
 from os import getenv
 
-
+# Flask app
 app = Flask(__name__)
 
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
+
+# Cross-Origin Resourse Sharing
 cors = CORS(app, resources={r"api/v1/*": {"origins": "0.0.0.0"}})
 
 
@@ -31,10 +33,10 @@ def not_found(error):
     return make_response(jsonify({"error": "Not Found"}), 404)
 
 
-swagger = Swagger(app)
-
-
 if __name__ == "__main__":
+    """
+    flask app
+    """
     host = getenv("HBNB_API_HOST")
     port = getenv("HBNB_API_PORT")
     if not host:
