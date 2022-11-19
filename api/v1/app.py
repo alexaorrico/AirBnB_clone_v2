943a42130ad1e8421064df50e84bc8e73b47611e
ben <benjamineruvieru@gmail.com>
2022-11-19 20:11:17 +0100

    task3 complete

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,21 +4,28 @@
 from api.v1.views import app_views
 from models import storage
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
 
 
 @app.teardown_appcontext
 def close():
     """
-this method calls .close() 
-the current SQLAlchemy Session
-"""
+    this method calls .close() 
+    the current SQLAlchemy Session
+    """
     storage.close()
 
 
 if __name__ == '__main__':
     """
-MAIN Flask App
-"""
+    MAIN Flask App
+    """
     app.run(host=host, port=port)
