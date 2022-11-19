40ebedb68d3850d067b4dcfc8c75dc8bbf5f7780
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-19 13:56:35 +0000

    Corrections

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,7 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
+i
     def get(self, cls, id):
         """ get method returns the value of an id"""
         if cls not in classes.values():
