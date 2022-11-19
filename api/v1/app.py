7090302384e9fdd123ecab81c924cb05aa536c71
ben <benjamineruvieru@gmail.com>
2022-11-19 21:12:41 +0100

    task3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-"""Flask App that integrates with AirBnB static HTML Template"""
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+
 from api.v1.views import app_views
 from models import storage
 from flask import Flask
@@ -13,6 +16,8 @@ app.url_map.strict_slashes = False
 # flask server environmental setup
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
