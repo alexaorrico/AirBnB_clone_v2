03b1151b00c3225d18ae7e1e0bf508e5dd509f2c
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-11-20 13:55:23 +0000

    Users done

---
+++ api/v1/views/users.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" This script provides views of Amenity """
+
+from flask import abort, jsonify, request
+from models import storage
+from models.user import User
+from api.v1.views import app_views
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    data = [
+        user.to_dict() for user in storage.all(User).values()
+    ]
+    return jsonify(data)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_user(user_id):
+    res = storage.get(User, user_id)
+    if res is None:
+        abort(404)
+    return jsonify(res.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_user(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/users', strict_slashes=False, methods=['POST'])
+def create_user():
+    body = request.get_json()
+    if type(body) != dict:
+        return abort(400, {'message': 'Not a JSON'})
+    if 'email' not in body:
+        return abort(400, {'message': 'Missing email'})
+    if 'password' not in body:
+        return abort(400, {'message': 'Missing password'})
+    n_user = User(**body)
+    n_user.save()
+    return jsonify(n_user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', strict_slashes=False,
+                methods=['PUT'])
+def update_user(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    body = request.get_json()
+    if type(body) != dict:
+        return abort(400, {'message': 'Not a JSON'})
+    for k, v in body.items():
+        if k not in ['id', 'created_at', 'updated_at', 'email']:
+            setattr(user, k, v)
+    storage.save()
+    return jsonify(user.to_dict()), 200
