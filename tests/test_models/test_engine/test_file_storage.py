776fd8794048a8788b7f12bc1658f573b91c5c37
bibest <bibhestee@ggmail.com>
2022-11-20 04:15:14 -0800

    Test for get and count outline

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,12 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+
+    def test_get(self):
+        """Test the get method"""
+        pass
+
+    def test_count(self):
+        """Test the count method"""
+        pass
