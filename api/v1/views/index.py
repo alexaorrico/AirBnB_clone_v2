463d0635bab4900a2c7ca74abc513d9f832b4491
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-20 07:56:18 +0000

    index correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,16 +7,18 @@ This module contains some utility functions for the API
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status', methods=['GET'])
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status_okay():
     """
     Returns Status OK code.
     """
-    return jsonify({"status" : "OK"})
+    return jsonify({"status": "OK"})
 
-@app_views.route('/status', methods=['GET'])
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status_okay():
     """
     Returns 200: Status Okay
     """
-    return jsonify({"status" : "OK"})
+    return jsonify({"status": "OK"})
