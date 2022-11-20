26c80f12928180980080072acd1547b2ae050ec4
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-20 14:14:19 +0000

    a handler for 404 errors that returns a JSON-formatted 404 status code response.

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,10 @@
 """
 This module is responsible for setting up the environment for the API
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
