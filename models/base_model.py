297ca8ab742cf5bcbfa4da91c8fe3de8d894cff1
UnfazedAy <ayomidesoniyi@gmail.com>
2022-11-20 23:02:03 +0100

    Resolved Datetime

--- models/base_model.py
+++ models/base_model.py
@@ -44,7 +44,7 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.now()
             self.updated_at = self.created_at
 
     def __str__(self):
