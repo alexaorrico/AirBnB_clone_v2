6083afa525f6647875bb4d553cb5d884ad27c48b
Jemimah Mmboga <mmbogajemimah@gmail.com>
2022-11-20 09:10:57 +0000

    an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,13 @@ This module contains some utility functions for the API
 
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
