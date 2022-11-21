6aeebd5ba8c91521b0a726d1828bec836134c547
Kitesa <kitesamerga@gmail.com>
2022-11-21 04:26:12 -0800

    ADded app file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""
+API for AirBnB_clone_v3
+"""
+
+import os
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
-
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
+def teardown(self):
+    """ handles teardown """
     storage.close()
 
-    
-if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """ handles 404 errors """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
+
+
+if __name__ == '__main__':
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
         host = '0.0.0.0'
-    if not port:
+
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
         port = '5000'
-    app.run(host=host, port=port, threaded=True)
+
+    app.run(host=host, port=port)
