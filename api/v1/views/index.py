a7029fcdfd799cc4527c9305a0aba0cbed71877d
meyota <meyota08@gmail.com>
2022-11-21 18:37:07 +0000

    Status_of Your_Api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,12 +3,17 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
-    return jsonify(status="OK")
+    return jsonify({"status"="OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
