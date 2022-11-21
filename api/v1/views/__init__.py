5f4171971ea6aa620e937ea7204941672ea55d43
jeddaa <fathiato@gmail.com>
2022-11-21 21:50:59 +0100

    API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,7 +9,7 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
- from api.v1.views.users import *
+from api.v1.views.users import *
 """
 if app_views is not None:
     from api.v1.views.index import *
