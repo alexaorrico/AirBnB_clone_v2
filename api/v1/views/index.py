3ef84c53802c7d1bedad70d6d783eb29a6d372b9
VirtualBlakeWillis <bewillis101@gmail.com>
2022-11-21 20:12:38 +0000

    Task 5, no unit tests

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,28 @@ Returns "status": "OK" JSON
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status_route():
     """
-    Returns "status": "OK" 
+    Returns "status": "OK"
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats_route():
+    """
+    Retrieves the number of each objects by type
+    """
+    count_directory = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(count_directory)
