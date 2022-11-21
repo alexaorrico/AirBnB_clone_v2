45f582b1d961c80deae36132bd220d4c72621061
meyota <meyota08@gmail.com>
2022-11-21 18:51:28 +0000

    Status_of Your_Api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,15 +8,4 @@ from models import storage
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
-    return jsonify({"status"="OK"})
-
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """ Returns the number of each instance type """
-    return jsonify(amenities=storage.count("Amenity"),
-                   cities=storage.count("City"),
-                   places=storage.count("Place"),
-                   reviews=storage.count("Review"),
-                   states=storage.count("State"),
-                   users=storage.count("User"))
+    return jsonify({"status"="Ok"})
