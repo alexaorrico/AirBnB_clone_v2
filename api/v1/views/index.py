4a66c056a17fdfe9d240832f97866945ad0a4fda
wilstermanz <wilstermanz@gmail.com>
2022-11-21 23:33:38 -0600

    Added stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,15 @@ Index module that creates routes on app_views objects.
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status')
@@ -19,4 +28,7 @@ def get_stats():
     Endpoint that retrieves the number of ojbects of each
     type.
     """
-    storage.count
+    class_lib = {}
+    for name, clsname in classes.items():
+        class_lib[name] = storage.count(clsname)
+    return jsonify(class_lib)
