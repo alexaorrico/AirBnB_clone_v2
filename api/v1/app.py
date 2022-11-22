a9e14b7dee5109704f151880b6221cc8340cf868
Chalo1996 <emusyoka759@gmail.com>
2022-11-22 16:55:34 +0300

    "Fixed CORS"

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,8 +13,8 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 from models import storage
 import models
 
-my_app = views.app_views
+my_app = app_views
 
 
 @my_app.route('/status', strict_slashes=False)
@@ -23,9 +23,12 @@ def getStatus():
 @my_app.route('/stats', strict_slashes=False)
 def numObj():
     """returns the number of each object by type."""
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify(
+        {"amenities": storage.count("Amenity"),
+         "cities": storage.count("City"),
+         "places": storage.count("Place"),
+         "reviews": storage.count("Review"),
+         "states": storage.count("State"),
+         "users": storage.count("User")
+         }
+    )
