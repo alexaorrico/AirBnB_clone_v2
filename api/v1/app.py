ac652a2389e3161282c96e9647118508ea66dfb0
meyota <meyota08@gmail.com>
2022-11-22 18:40:41 +0000

    HTTP_access_control

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,8 @@ from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
