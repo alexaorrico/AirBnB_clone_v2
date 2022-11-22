2c3d751b20db207799e3a1b18041affe1eb67d4f
mgmtsweni <mtswenigift@gmail.com>
2022-11-22 13:49:41 +0200

    Fix errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -27,11 +27,11 @@ def stats():
                    states=storage.count(State),
                    users=storage.count(User))"""
     class_counts = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User")
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
         }
     return jsonify(class_counts)
