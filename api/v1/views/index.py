0857a278268fb03ba19c80fd2d6e5a762d46f34c
Okorie Jude Ifeanyi <jude58005@gmail.com>
2022-11-22 23:01:38 +0100

    adding /stats route which shows the count of all objects in storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,20 @@
 """ index file which returns json response on /status endpoint"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """ returns json response to the route"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_count():
+    """returns number of each objects by type"""
+    objects = {'amenities': 'Amenities', 'cities': 'Cities',
+               'places': 'Places', 'reviews': 'Reviews', 'states': 'States', 'users': 'Users'}
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
