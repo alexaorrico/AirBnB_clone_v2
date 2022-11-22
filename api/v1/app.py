e665f4752c3083b8c2a6e94e7e6170213bf9bd43
meyota <meyota08@gmail.com>
2022-11-22 18:47:42 +0000

    HTTP_access_control

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,31 +6,11 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
-from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['SWAGGER'] = {
-    "swagger_version": "2.0",
-    "title": "Flasgger",
-    "headers": [
-        ('Access-Control-Allow-Origin', '*'),
-        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
-        ('Access-Control-Allow-Credentials', "true"),
-    ],
-    "specs": [
-        {
-            "version": "1.0",
-            "title": "HBNB API",
-            "endpoint": 'v1_views',
-            "description": 'HBNB REST API',
-            "route": '/v1/views',
-        }
-    ]
-}
-swagger = Swagger(app)
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
