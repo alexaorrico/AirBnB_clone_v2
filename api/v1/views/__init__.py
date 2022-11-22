02c45dee647c5d8eb066ffe71cb151b26ff44194
wilstermanz <wilstermanz@gmail.com>
2022-11-21 23:26:01 -0600

    Fixed circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """initializes views"""
 from flask import Blueprint, jsonify
-from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
