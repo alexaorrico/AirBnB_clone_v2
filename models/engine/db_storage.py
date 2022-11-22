acc9584f3e50640aad51670a3fd5c785c4ba0bbc
chostler24 <connor.hostler@gmail.com>
2022-11-22 11:32:39 -0600

    Adding updates to storage engines in storage_get_count branch to be merged to master

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,13 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """returns the object based on class and id"""
+        if cls in classes.values():
+            return self.__session.query(cls).filter(cls.id == id).first()
+        return None
+
+    def count(self, cls=None):
+        """returns number of objects in storage matching class"""
+        return len(self.all(cls))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """returns object based on class and id"""
+        objects = self.all(cls)
+        for key in objects.keys():
+            if key.find(id) != -1:
+                return objects[key]
+        return None
+
+    def count(self, cls=None):
+        """returns number of objects in storage matching class"""
+        return len(self.all(cls))
