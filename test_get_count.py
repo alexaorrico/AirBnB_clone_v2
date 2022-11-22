ff6878b3874612c6e41f137cf71f01eb5637c0c3
blenassefa2 <blenassefa59@yahoo.com>
2022-11-22 15:13:33 +0300

    Rest

--- test_get_count.py
+++ test_get_count.py
@@ -9,3 +9,26 @@ print("State objects: {}".format(storage.count(State)))
 
 first_state_id = list(storage.all(State).values())[0].id
 print("First state: {}".format(storage.get(State, first_state_id)))
+
+def main():
+    cities, roads, capital = inlt()
+    
+    edges = defaultdict(list)
+  
+    for _ in range(roads):
+        u,v = inlt()
+        edges[u].append(v)
+    def dfs(node,visited):
+        if node in visited:
+            return
+        visited.add(node)
+        for i in edges[node]:
+            dfs(i,visited)
+    visited = set()
+    dfs(capital,visited)
+    disconnected = 0
+    for i in range(1,cities+1):
+        if i not in visited:
+            dfs(i,visited)
+            disconnected += 1
+    print(disconnected)
