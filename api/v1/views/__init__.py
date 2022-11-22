4d09e04b1065bf167f56351e5dc438ad324d680d
Chalo1996 <emusyoka759@gmail.com>
2022-11-23 01:39:36 +0300

    "Imported the Fabrics"

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,6 +11,7 @@ app_views = Blueprint(
     'api_views', __name__, url_prefix='/api/v1'
 )
 
-# from api.v1.views.index import *
-# from api.v1.views.cities import *
-# from api.v1.views.states import *
+from api.v1.views.index import *
+from api.v1.views.cities import *
+from api.v1.views.states import *
+from api.v1.views.amenities import *
