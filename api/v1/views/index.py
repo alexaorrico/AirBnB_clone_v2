be3c3b4809415a034e9767382d198d134694d369
mgmtsweni <mtswenigift@gmail.com>
2022-11-22 12:52:20 +0200

    Fix errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -19,10 +19,19 @@ def status():
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
-    """ Returns the number of each instance type """
+    """ Returns the number of each instance type
     return jsonify(amenities=storage.count(Amenity),
                    cities=storage.count(City),
                    places=storage.count(Place),
                    reviews=storage.count(Review),
                    states=storage.count(State),
-                   users=storage.count(User))
+                   users=storage.count(User))"""
+    class_counts = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+        }
+    return jsonify(class_counts)
