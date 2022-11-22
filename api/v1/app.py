5f2ae4e53ba1738c4877726a36770e55b3fdca25
Chalo1996 <emusyoka759@gmail.com>
2022-11-22 16:08:45 +0300

    "Modified app.py and __init__.py"

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,7 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,6 +8,6 @@ Registers Blueprints.
 from flask import Blueprint
 
 
-app_views = Blueprint('api_views', __name__)
+app_views = Blueprint('api_views', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
