19c813b10eadbc50ef9fc7d447e8f359e3137812
Doro256 <dorothynantumbwe@yahoo.com>
2022-11-22 12:09:50 +0000

    init file edited

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,11 +3,9 @@
 Registers Blueprints.
 """
 
-
-# from api.v1.views.index import *
 from flask import Blueprint
 
 
-app_views = Blueprint('app_views', __name__)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
