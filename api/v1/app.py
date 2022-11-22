787d8e79ec7bae293d86114c4e6676a6109fa18c
Chalo1996 <emusyoka759@gmail.com>
2022-11-22 16:44:26 +0300

    "Added CORS"

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,7 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
