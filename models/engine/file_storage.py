cf90947ee376e98fb0b257384ff141f27afcd933
Finbuka <ebukajoe100@gmail.com>
2022-11-23 03:02:48 +0100

    model_engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,10 +61,28 @@ class FileStorage:
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = obj.__class__.__name__ + "." + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not found"""
+        return self.__objects.get(f"{cls if type(cls) == str else cls.__name__}.{id}")
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        # return (len(self.all(cls)))
+        if cls:
+            return len(
+                [
+                    obj
+                    for key, obj in self.__objects.items()
+                    if key.split(".")[0] == cls.__name__
+                ]
+            )
+        else:
+            return len(self.__objects)
