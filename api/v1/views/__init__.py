e8fc3fb018238ec56995cbf67d9c651af93194d7
clybowl <4941@holbertonschool.com>
2022-11-26 13:28:33 -0800

    typo fixed __init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
-#!/usr/bin/pyhon3
+#!/usr/bin/python3
 """Import blueprint from flask"""
 from flask import Blueprint
 from api.v1.views.index import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-git br
