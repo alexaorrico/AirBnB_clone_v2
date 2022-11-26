1cd39a92f19e064ea3ed3736a1dfda03d6c560b5
clybowl <4941@holbertonschool.com>
2022-11-26 13:30:00 -0800

    removed possible circular import in __init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Import blueprint from flask"""
 from flask import Blueprint
-from api.v1.views.index import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
