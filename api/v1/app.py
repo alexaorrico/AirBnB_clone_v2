8e0adcd1c98f26db87407c5d5b0b14d516b49ae2
claybowl <claybowlproductions@gmail.com>
2022-11-26 22:46:06 -0800

    added 404 error method

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, render_template, make_response, jsonify
 from os import getenv
 
 
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown_app():
     """Tears down by calling storage.close"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
