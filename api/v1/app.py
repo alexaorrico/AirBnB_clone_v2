df9e5b066c2162c545683bbce25476d4c187f46c
samorobo <samgodwin111@gmail.com>
2022-11-27 23:37:39 +0000

    My overall commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,49 @@
 #!/usr/bin/python3
-"""This script represents the Airbnb_clone API"""
-from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
+""" Flask Application """
 from models import storage
-from os import getenv
-
+from api.v1.views import app_views
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """This function handles 404 errors"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
-@app.teardown_appcontext
-def tear_down_context(exception):
-    """This function removes the current SQLAlchemy Session"""
-    storage.close()
-
-
-if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
     if not host:
         host = '0.0.0.0'
     if not port:
         port = '5000'
-
     app.run(host=host, port=port, threaded=True)
