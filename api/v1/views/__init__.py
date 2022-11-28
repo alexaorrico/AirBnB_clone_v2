80b915f98098e668d0d5d7f148ab05b9d7657f5b
claybowl <claybowlproductions@gmail.com>
2022-11-27 22:39:23 -0800

    fixed imports to top of file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """Import blueprint from flask"""
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -11,3 +8,5 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
