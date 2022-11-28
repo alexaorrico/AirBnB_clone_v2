d6123565e76698e3750b9a3f70ac4ed485d0aa70
CalebHouse99 <calebhouse99@gmail.com>
2022-11-28 13:58:37 -0800

    amenities first draft

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -41,3 +41,41 @@ def delete_amenity():
     storage.save()
 
     return make_response(jsonify({}), 200)
+
+
+@app_views.route('/amenities', methods=['POST'],
+                 strict_slashes=False)
+def post_amenity():
+    """creates amenity object"""
+    if not request.get_json():
+        abort(400, descritpion="Not a JSON")
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    data = request.get_json()
+    instance = Amenity(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+
+@app_views.route('/ammenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_amenity(amenity_id):
+    """updates amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+
+    if not amenity:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(amenity, key, value)
+    storage.save()
+    return make_response(jsonify(amenity.to_dict()), 200)
