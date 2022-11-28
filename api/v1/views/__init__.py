b75476e17d10f84c4aa5aaf2ac917e27d9c6cb62
claybowl <claybowlproductions@gmail.com>
2022-11-27 22:41:40 -0800

    whoops that last commit was mistake

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Import blueprint from flask"""
 from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -8,5 +11,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
