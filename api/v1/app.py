c3cbb6238ea53b0fb83426c3d7e0eb266db27dad
claybowl <claybowlproductions@gmail.com>
2022-11-27 21:47:33 -0800

    chngs to app.py/added CORS/fixed methods

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,20 +5,22 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, render_template, make_response, jsonify
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_app():
+def teardown_app(exception):
     """Tears down by calling storage.close"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
+def not_found(exception):
     """404 error"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
