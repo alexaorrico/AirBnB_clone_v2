377a269279329210e1cbd9a252c2be5850fc543f
gallit0 <mateogesede@gmail.com>
2022-12-05 23:40:33 +0000

    count method added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,9 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+
+    def count(self, cls=None):
+        return len(self.all(cls))
+        
+
