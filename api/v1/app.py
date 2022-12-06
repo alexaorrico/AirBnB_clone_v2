152a68fe8925f4260b72a5e7f52a8545e0a34c60
Hardbass1991 <midailsorridente@gmail.com>
2022-12-06 16:28:09 +0000

    added json 404 response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""""""
-from flask import Flask
+"""Module that starts app and runs server"""
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
-from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -12,6 +12,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(
