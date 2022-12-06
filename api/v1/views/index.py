f6c8e9aa017a52bb4fbfe46d81856525bb709d68
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 22:25:47 +0100

    no api.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route('/status', strict_slashes=False)
-def index():
+def status():
     """ok!"""
     return jsonify({'status': 'OK'})
 
