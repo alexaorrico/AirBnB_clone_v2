be9e094addaa247b308985482a0370e6793b6f90
gallit0 <mateogesede@gmail.com>
2022-12-06 02:15:16 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,12 @@
 from api.v1.views import app_views
 import json
 from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 @app_views.route('/status')
@@ -14,9 +20,9 @@ def status():
 
 @app_views.route('/stats')
 def stats():
-    return {"aminities": storage.count('Amenity'),
-            "cities": storage.count('City'),
-            "places": storage.count('Place'),
-            "reviews": storage.count('Review'),
-            "states": storage.count('State'),
-            "users": storage.count('User')}
+    return {"aminities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User')}
