8bcadd9defd827ff95f77ac5574ae0a2154755fb
Luke Hackett <luke-hackett@outlook.com>
2022-12-06 14:26:34 +1100

    moved import to below appviews blueprint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,8 +3,8 @@
 Contains the blueprints for the api url builder.
 '''
 
+
 from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
-
-
-app_views = Blueprint('api', __name__, url_prefix='/api/v1')
