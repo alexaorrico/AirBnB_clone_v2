0e675073ebfd79cc0527aaae62d513be146293a7
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 22:38:35 +0100

    update import api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 from flask import Blueprint, render_template, abort
+from api.v1.views import *
 from api.v1.views.index import *
+from api.v1.views.cities import * 
+from api.v1.views.index import * 
+from api.v1.views.states import *
+from api.v1.views.places_reviews import *
+from api.v1.views.users import *
+from api.v1.views.amenities import *
+from api.v1.views.places import *
 
 app_views = Blueprint('app_view', __name__, url_prefix='/api/v1')
