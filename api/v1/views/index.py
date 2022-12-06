7df294ad3b9dbbeae792cf038ffc1a68cab3376a
gallit0 <mateogesede@gmail.com>
2022-12-06 02:11:21 +0000

    task 5 fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,9 +14,9 @@ def status():
 
 @app_views.route('/stats')
 def stats():
-    return {"aminities": storage.count(Amenity),
-            "cities": storage.count(City),
-            "places": storage.count(Place),
-            "reviews": storage.count(Review),
-            "states": storage.count(State),
-            "users": storage.count(User)}
+    return {"aminities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')}
