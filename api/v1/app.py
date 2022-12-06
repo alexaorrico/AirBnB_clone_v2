8837ced6ae6ce6e95d1d6ff52def0d8272a123b5
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 22:35:39 +0100

    update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -11,6 +11,11 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Returns JSON error repsponse"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
