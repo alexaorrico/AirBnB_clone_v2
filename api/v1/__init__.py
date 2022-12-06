b18e15a7681335d0dac50237a03da187fbb8d138
Noémie Huynh <huynhtieuquy@gmail.com>
2022-12-06 22:33:26 +0100

    update index

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,2 +1,1 @@
 #!/usr/bin/python3
-from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ok!"""
     return jsonify({'status': 'OK'})
 
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """return stats"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
