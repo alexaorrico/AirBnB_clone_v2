6dc2113761f98c969a974c4103f8676ac2d0a714
Luke Hackett <luke-hackett@outlook.com>
2022-12-06 14:20:22 +1100

    move around blueprint import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,8 @@
 Contains the blueprints for the api url builder.
 '''
 
-from api.v1.views.index import *
 from flask import Blueprint
+from api.v1.views.index import *
+
 
 app_views = Blueprint('api', __name__, url_prefix='/api/v1')
