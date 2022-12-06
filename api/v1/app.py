edfeb75423cd5934c2ed329103ac71d9d5f2a3a6
Luke Hackett <luke-hackett@outlook.com>
2022-12-06 14:04:22 +1100

    add app and blueprint

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    '''Closes the connection to storage when it is finished.'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app_host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    app_port = getenv('HBNB_API_PORT', default='5000')
+    app.run(host=app_host, port=app_port, threaded=True)
