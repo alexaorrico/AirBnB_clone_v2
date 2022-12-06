f57541c60f5cdd3871bc8af8a73394b690ea9167
Luke Hackett <luke-hackett@outlook.com>
2022-12-06 14:35:22 +1100

    add class count method to app_views blueprint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,29 @@ Module contains the routes to be used for the API.
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status')
 def app_status():
     '''Returns the status code.'''
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def count():
+    '''Returns the count of objects from storage.'''
+    class_count_dict = {}
+    for key, cls_name in classes:
+        cls_count = storage.count(cls_name)
+        class_count_dict[key] = cls_count
+    return jsonify(class_count_dict)
