fbc978191685c27fe2abbc6de8b8a0feb84d61e5
Elizabeh <eliz01_01@yahoo.com>
2022-12-06 14:52:03 +0100

    A new view for User objects

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -10,17 +10,17 @@ from models.user import User
 def get_all_users():
     """ Function retrieves all Users list"""
     all_users = []
-    for user in storage.all(User).values():
+    for user in storage.all('User').values():
         all_users.append(user.to_dict())
-    return jsonify(all_users)
+    return(jsonify(all_users))
 
 
 @app_views.route('users/<user_id>', methods=['GET'],
                  strict_slashes=False)
 def get_user(user_id):
     """ Function that retrieves a User """
-    user = storage.get(User, user_id)
-    return abort(404) if user is None else jsonify(user.to_dict())
+    user = storage.get('User', user_id)
+    return (abort(404) if user is None else jsonify(user.to_dict()))
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'],
@@ -32,45 +32,45 @@ def delete_user(user_id):
         return abort(404)
     user.delete()
     storage.save()
-    return jsonify({}), 200
+    return(jsonify({}), 200)
 
 
 @app_views.route('/users/', methods=['POST'], strict_slashes=False)
 def post_user():
     """ Function that create a User """
-    dico = request.get_json()
+    res = request.get_json()
 
-    if dico is None:
+    if res is None:
         abort(400, "Not a JSON")
 
-    if dico.get("email") is None:
+    if res.get("email") is None:
         abort(400, "Missing email")
 
-    if dico.get("password") is None:
+    if res.get("password") is None:
         abort(400, "Missing password")
 
-    new_user = User(**dico)
+    new_user = User(**res)
     new_user.save()
 
-    return jsonify(new_user.to_dict()), 201
+    return(jsonify(new_user.to_dict()), 201)
 
 
 @app_views.route('users/<user_id>', methods=['PUT'],
                  strict_slashes=False)
 def put_user(user_id):
     """ Function that update a User """
-    user = storage.get(User, user_id)
+    user = storage.get('User', user_id)
     if user is None:
-        return abort(404)
+        return(abort(404))
 
-    dico = request.get_json()
+    res = request.get_json()
 
-    if dico is None:
+    if res is None:
         abort(400, "Not a JSON")
 
-    for key, value in dico.items():
+    for key, value in res.items():
         if key not in ['id', 'created_at', 'email', 'updated_at']:
             setattr(user, key, value)
     user.save()
 
-    return jsonify(user.to_dict()), 200
+    return(jsonify(user.to_dict()), 200)
