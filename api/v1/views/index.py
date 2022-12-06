afdacae9b07f709648e5a02b3e6efe0e08ad0db9
Elizabeh <eliz01_01@yahoo.com>
2022-12-06 14:37:31 +0100

    correction of syntax

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
-"""
-index.py
-"""
-
+"""Endpoint that retrieves the number of each objects by type"""
 from api.v1.views import app_views
 from flask import jsonify
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
+import models
 
 
-@app_views.route('/status')
-def status_check():
-    """Returns a JSON status message"""
-    text_format = {'status': 'OK'}
-    return jsonify(text_format)
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """Returns status"""
+    return (jsonify({"status": "OK"}))
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Return stats"""
+    my_dict = {
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    }
+    return my_dict
