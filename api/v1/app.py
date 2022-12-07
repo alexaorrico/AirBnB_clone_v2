e3fd6140e51dc281e4a675fdd73964d72c16ec87
Santiago Alves <5102@holbertonstudents.com>
2022-12-07 12:30:07 -0300

    Fix Doc

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,10 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
-
 
 
 @app.teardown_appcontext
