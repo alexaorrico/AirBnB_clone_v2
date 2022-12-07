d09be1b9860811dccaa531412c28d29925e404c4
devmaj <majdi.developer@gmail.com>
2022-12-07 11:42:14 -0500

    try to fix state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -50,3 +50,26 @@ def post_state():
             return jsonify(obj.to_dict()), 201
     except ValueError:
         abort(400, desciption="Not a JSON")
+
+@app_views.route('/states/<state_id>', methods=["PUT"])
+def update_state(state_id):
+    """PUT/UPDATE a single state"""
+    found = storage.get(State, state_id)
+    if not found:
+        abort(404)
+
+    try:
+        req = request.get_json()
+        if req is None:
+            abort(400, description="Not a JSON")
+        else:
+            invalid = ['id', 'created_at', 'updated_at']
+            for key, value in req.items():
+                if key not in invalid:
+                    setattr(found, key, value)
+            storage.save()
+            return jsonify(found.to_dict()), 200
+    except ValueError:
+        abort(400, description="Not a JSON")
+
+
