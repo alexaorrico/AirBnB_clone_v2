bd9678a77dd8f054a433b09a15b9371fc15cd83b
Santiago Alves <5102@holbertonstudents.com>
2022-12-07 12:46:03 -0300

    Fix Docs

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""
-Module documentation
-"""
+""" Create api """
 
-
-import os
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
-
+import os
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
-    """teardown_appcontext"""
+def closedb(exit):
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def not_found(error):
+    """
+    returns a 404 JSON error response
+    jsonify() serializes data to json
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    host = os.getenv("HBNB_API_HOST") or '0.0.0.0'
+    port = os.getenv("HBNB_API_PORT") or '5000'
+    app.run(host=host, port=port, threaded=True)
