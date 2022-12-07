44fb206eeba62132146c6d722f14749b6b551329
HanaOuerghemmi <5004@holbertonschool.com>
2022-12-07 12:01:05 +0100

    count json

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,23 @@ index
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """return status json"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """that retrieves the number of each objects by type:"""
+    classes = {"amenity": "Amenity", "city": "City",
+               "place": "Place", "review": "Review", "state": "State", "user": "User"}
+
+    ct = {}
+    for cls in classes:
+        ct[cls] = storage.count(classes[cls])
+
+    return jsonify(ct)
