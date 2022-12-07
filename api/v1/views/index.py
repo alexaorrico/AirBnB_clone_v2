c0e2b68ee5c7a276efabef928303195c28907aee
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-07 17:14:18 -0300

    fixs index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,18 +11,18 @@ from api.v1.views import app_views
 def api_status():
     """Endpoint (route) will be to return the status of the API"""
     # We can use json.dump() or flask.jsonify()
-    return jsonify(status="OK")
+    return jsonify({"status":"OK"})
 
  
 @app_views.route('/stats')
 def objects_qty():
     """Retrieves the number of each objects by type"""
     from models import storage
-    return jsonify(
-        amenities= storage.count("Amenity"),
-        cities= storage.count("City"),
-        places= storage.count("Place"),
-        reviews= storage.count("Review"),
-        states= storage.count("State"),
-        users= storage.count("User")
-    )
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
