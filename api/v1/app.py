dec8e1f6d8b47c72c845737cde5735bb4c43aaa7
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-07 17:01:13 -0300

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,6 @@
 """start a flask web app"""
 
 
-from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage 
 from os import getenv
@@ -10,7 +9,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+from api.v1.views import app_views
 
 @app.teardown_appcontext
 def storage_close():
