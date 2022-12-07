53684f1c158ff503934b3b665ffa504e92faa4bc
Santiago Alves <5102@holbertonstudents.com>
2022-12-07 13:46:33 -0300

    Adding task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-""" Create api """
+"""
+Module documentation
+"""
 
-from flask import Flask, Blueprint, jsonify, make_response
+
+import os
 from models import storage
 from api.v1.views import app_views
-import os
+from flask import Flask, Blueprint, jsonify, make_response
+
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def closedb(exit):
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """
-    returns a 404 JSON error response
-    jsonify() serializes data to json
-    """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST") or '0.0.0.0'
-    port = os.getenv("HBNB_API_PORT") or '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
