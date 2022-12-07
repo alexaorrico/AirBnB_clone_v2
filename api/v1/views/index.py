a0604e901c238e1bdd911aac7f1607e2d341e4d8
root <piotr62@live.fr>
2022-12-07 07:13:09 -0800

    embettering files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """
     returns an OK Jsonified
