e71e9165c5e7008b2311f9b0d3c01728737302ec
Hardbass1991 <midailsorridente@gmail.com>
2022-12-07 20:46:46 +0000

    added CORS to app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """Module that starts app and runs server"""
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
