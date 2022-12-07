16feaeba45c0de06035131ce57b2364bfcb6724f
mattowsh <mattobelen@gmail.com>
2022-12-07 10:44:55 -0300

    task 5 and status endopint of task 4

---
+++ api/v1/views/index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Endpoints of the blueprint app_views"""
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+@app.route('/status')
+def api_status():
+    """Endpoint (route) will be to return the status of the API"""
+    # We can use json.dump() or flask.jsonify()
+    return jsonify(status="OK")
+
+
+@app.route('/stats')
+def objects_qty():
+    """Retrieves the number of each objects by type"""
+    from models import storage
+    return jsonify(
+        amenities= storage.count("Amenity"),
+        cities= storage.count("City"),
+        places= storage.count("Place"),
+        reviews= storage.count("Review"),
+        states= storage.count("State"),
+        users= storage.count("User")
+    )
