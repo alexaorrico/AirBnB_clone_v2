3de788188209bf998f070436b348098ed087a7ad
ChongLeangUENG <leangcnrp777@gmail.com>
2022-12-07 22:50:24 +0100

    Update API file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
+""" Index """
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """ok!"""
-    return jsonify({'status': 'OK'})
+    """ Returns JSON """
+    return jsonify(status="OK")
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
-    """return stats"""
-    return jsonify({
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    })
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
