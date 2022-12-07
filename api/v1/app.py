524bb86ddc2789124ca2efbe54406bdf142ff9fe
Santiago Alves <5102@holbertonstudents.com>
2022-12-07 12:38:53 -0300

    Fix Docs

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Create method closed and the server"""
+"""Module documentation"""
 
 
-from flask import Flask
+import os
 from models import storage
 from api.v1.views import app_views
-import os
-HOST = os.getenv('HBNB_API_HOST')
-PORT = os.getenv('HBNB_API_PORT')
+from flask import Flask, Blueprint, jsonify, make_response
 
 
 app = Flask(__name__)
@@ -15,16 +13,15 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def closed(exit):
-    """close storage"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
-@app.errorhandler(404)
-def error_404(error):
-    return {"error": "Not found"}
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(host='HBNB_API_HOST' or '0.0.0.0', port='HBNB_API_PORT' or '5000',
-            threaded=True, debug=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=int(os.getenv('HBNB_API_PORT', '5000')))
