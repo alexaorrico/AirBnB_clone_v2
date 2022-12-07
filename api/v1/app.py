970d1a7440d85f678b32c071a37b8b107299791d
Santiago Alves <5102@holbertonstudents.com>
2022-12-07 12:36:16 -0300

    Fix Doc2

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""app.py to connect to API"""
+"""Module documentation"""
 
 
 import os
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
-from flask_cors import CORS
+
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 
 @app.teardown_appcontext
