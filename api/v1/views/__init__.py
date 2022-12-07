bc0bca8b2bfd0df5fe491079cc79a156a31f25a7
Hardbass1991 <midailsorridente@gmail.com>
2022-12-07 18:03:26 +0000

    Added views for class Place

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,5 +8,6 @@ app_views = Blueprint('simple_page', __name__,
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
+from api.v1.views.places import *
 from api.v1.views.states import *
 from api.v1.views.users import *
