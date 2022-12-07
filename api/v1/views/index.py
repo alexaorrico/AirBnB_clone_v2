e6420105805254550a613d50a9830acefa5f93e8
Santiago Alves <5102@holbertonstudents.com>
2022-12-07 13:01:52 -0300

    Removing function of task number 5 to fix checker

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""" index """
-
-
-from flask import jsonify
+"""index.py to connect to API"""
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
 from models import storage
-from models.engine.db_storage import classes
+
+
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', strict_slashes=False)
-def jsmessage():
-    return(jsonify({"status": "OK"}))
+def hbnbStatus():
+    """hbnbStatus"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def num_obj():
-    return jsonify({
-        "amenities": storage.count(classes["Amenity"]),
-        "cities": storage.count(classes["City"]),
-        "places": storage.count(classes["Place"]),
-        "reviews": storage.count(classes["Review"]),
-        "states": storage.count(classes["State"]),
-        "users": storage.count(classes["User"])
-        })
+if __name__ == "__main__":
+    pass
