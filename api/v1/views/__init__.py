7855ef333c14c8a13195d234dd5bef3bc594cccc
Hardbass1991 <midailsorridente@gmail.com>
2022-12-07 20:06:54 +0000

    modified import statements for init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""""
+"""Module that sets a blueprint and prefix for the views"""
 from flask import Blueprint
 
 app_views = Blueprint('simple_page', __name__,
@@ -9,6 +9,6 @@ from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
 from api.v1.views.places import *
-from api.v1.views.reviews import *
+from api.v1.views.places_reviews import *
 from api.v1.views.states import *
-ifrom api.v1.views.users import *
+from api.v1.views.users import *
