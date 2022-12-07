f5c1be05e7666b9321c6d78131b0e3a86165dbf4
santirodriguezzz <santiagorodri130898@gmail.com>
2022-12-07 17:22:56 -0300

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,9 @@ from os import getenv
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
 
 
 @app.teardown_appcontext
