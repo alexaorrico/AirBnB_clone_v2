9871ed87c4c14bb3b8a408632d7adfbf5df6e604
Hardbass1991 <midailsorridente@gmail.com>
2022-12-07 20:03:00 +0000

    added views for class Review

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -9,5 +9,6 @@ from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
 from api.v1.views.places import *
+from api.v1.views.reviews import *
 from api.v1.views.states import *
-from api.v1.views.users import *
+ifrom api.v1.views.users import *
