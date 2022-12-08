efbc511efa0f76e585c658b6c719a0acaaa18926
ChongLeangUENG <leangcnrp777@gmail.com>
2022-12-08 10:00:08 +0100

    Update API file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-""" config app """
+"""Our (single) Flask webapp instance"""
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
-
-host = getenv("HBNB_API_HOST", "0.0.0.0")
-port = getenv("HBNB_API_PORT", "5000")
 
 app = Flask(__name__)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+CORS(app, origins="0.0.0.0")
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes the storage on teardown"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(exception):
-    """ Custom 404 error """
-    return jsonify({"error": "Not found"}), 404
+def handle_notfound(exception):
+    status = {
+        'error': 'Not found'
+    }
+
+    return jsonify(status), 404
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host=host, port=port, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=int(port), threaded=True)
