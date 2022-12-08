6163bb917fcc149960f07043189bf058e438c00c
ChongLeangUENG <leangcnrp777@gmail.com>
2022-12-08 10:36:09 +0100

    Update states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Init file """
 
+from api.v1.views.states import *
+from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
