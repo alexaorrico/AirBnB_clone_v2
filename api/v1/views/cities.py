71cecd8165af319c4417452c84ae0105287f5c44
mattowsh <mattobelen@gmail.com>
2022-12-08 15:44:12 -0300

    task 8 new city, post method

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,9 +6,11 @@ from models.city import City
 from api.v1.views import app_views
 
 
-@app_views.route('/states/<string:state_id>/cities', methods=["GET"])
+@app_views.route('/states/<string:state_id>/cities', methods=["GET", "POST"])
 def cities_by_state(state_id):
-    """Retrieves all cities of a State object by state_id"""
+    """Retrieves all cities of a State object or add a new City by state_id"""
+    # hay codigo repetido, se puede optimizar
+
     if request.method == 'GET':
         detector = 0
         for obj in storage.all("State").values():
@@ -17,13 +19,35 @@ def cities_by_state(state_id):
                 break
             else:
                 pass
-        
+
         if detector != 0:
             results = []
             for city in storage.all("City").values():
                 if city.state_id == state_id:
                     results.append(city.to_dict())
             return jsonify(results)
+        else:
+            abort(404)
+
+    elif request.method == 'POST':
+        detector = 0
+        for obj in storage.all("State").values():
+            if obj.id == state_id:
+                detector = 1
+                break
+            else:
+                pass
+
+        if detector != 0:
+            http_data = request.get_json()
+            if not http_data:
+                abort(400, 'Not a JSON')
+            if "name" not in http_data:
+                abort(400, 'Missing name')
+
+            new_city = City(**http_data)
+            new_city.save()
+            return jsonify(new_city.to_dict()), 201
         else:
             abort(404)
 
@@ -42,5 +66,7 @@ def city_by_id(city_id):
                 storage.delete(obj)
                 storage.save()
                 return jsonify({}), 200
-    
+
     abort(404)
+
+
