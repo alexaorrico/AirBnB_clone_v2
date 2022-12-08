7f5f0407c4b9b266f6fdd385086ea73b74b6f15e
majdi aribi <majdi.developer@gmail.com>
2022-12-08 10:42:30 +0100

    Review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,13 +8,27 @@ from models import storage
 from models.place import Place
 from models.city import City
 from models.user import User
+from models.review import Review
+
 
 @app_views.route('/places/<place_id>/reviews',
                  strict_slashes=False,
                  methods=['GET'])
-def get_places(place_id):
-    """ Get All places"""
+def get_reviews(place_id):
+    """ Get All Reviews of Place"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-    return jsonify([review.to_dict() for review in place.rivews])
+    return jsonify([review.to_dict() for review in place.reviews])
+
+
+@app_views.route('/reviews/<review_id>', strict_slashes=False,
+                 methods=['GET'])
+def get_review(review_id):
+    """Get Review"""
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    return jsonify(review.todict())
+
+
