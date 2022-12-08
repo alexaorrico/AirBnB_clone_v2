a70b9ad136ce6d83b69e3f9fb9f9a83b6d922f83
devmaj <majdi.developer@gmail.com>
2022-12-08 10:36:34 -0500

    places_amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -21,7 +21,10 @@ def get_amenities_places(place_id):
     if place_obj is None:
         abort(404, 'Not found')
     if STORAGE_TYPE == 'db':
-        place_amenities = place_obj.amenities
+        if len(place_obj.amenities) ==  0:
+            place_amenities = place_obj.amenities
+        else:
+            place_amenities = [amen.to_dict() for amen in place_obj.amenities]
     else:
         place_amen_ids = place_obj.amenities
         place_amenities = []
