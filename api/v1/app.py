326b060642a3da4e01388492c57d7991d1560b9f
carloscm2022 <carloscr7cmreal@gmail.com>
2022-12-11 03:02:22 -0500

    modifique file uploading

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,7 +25,8 @@ def not_found(error):
 
 if __name__ == "__main__":
     app.run(
-        host=getenv("HBNB_API_HOST", '0.0.0.0'),
-        port=getenv("HBNB_API_PORT", 5000),
-        threaded=True
-    )
+            host=get.env("HBNB_API_HOST", '0.0.0.0'),
+            port=get.env("HBNB_API_PORT", 5000),
+            threaded=True,
+            debug=True
+           )
