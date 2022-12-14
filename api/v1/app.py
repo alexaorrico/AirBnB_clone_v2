6f7ae5393167cd5afefb39059b4275e5138e2bf4
alejandrodgz <5199@holbertonstudents.com>
2022-12-14 18:28:46 -0500

    task4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-module app with all documentation
-"""
-
+"""immported modules pa"""
 import os
 from api.v1.views import app_views
 from models import storage
 from flask import Flask
-import sys
-sys.path.append('..')
+
 
 if os.getenv("HBNB_API_HOST"):
     HBNB_API_HOST = os.getenv("HBNB_API_HOST")
@@ -27,10 +23,12 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close_this(self):
     """close the storage instance"""
+
     storage.close()
 
 
 if __name__ == "__main__":
     """documented pa"""
+
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT,
             threaded=True)
