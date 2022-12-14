5180a2dc76a20bc1c1fe751f751495e4b8838214
abimont <abimontes00@gmail.com>
2022-12-14 17:43:03 -0500

    Update: Status of the API

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from flask import Flask, request
+from models import storage
+from api.v1.views import app_views
+from os import getenv, environ
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close():
+    """ method that closes a sesion """
+    storage.close()
+
+if __name__ == "__main__":
+    if getenv('HBNB_API_HOST') == None:
+        environ['HBNB_API_HOST'] = '0.0.0.0'
+    if getenv('HBNB_API_PORT') == None:
+        environ['HBNB_API_PORT'] = 5000
+
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True, debug=True)
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pythoon3
+
+from flask import Blueprint
+from api.v1.views.index import *
+
+app_views = Blueprint(url_prefix='/api/v1')
