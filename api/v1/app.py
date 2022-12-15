2f0e2a757d04054b5a2f48e3f622f500bdd6c290
Jehp00 <Jehp01104006@gmail.com>
2022-12-15 11:52:54 -0500

    app task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,34 @@
 #!/usr/bin/python3
-"""
-    module app importing the Blueprinted varieble app_views
-"""
-
+'''
+    app for registering blueprint and starting flask
+'''
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def tearDown(self):
+def tear_down(self):
     '''
-        close the query
+    close query after each session
     '''
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found_route(self):
-    """
-        return json formatted 404 status code response
-    """
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-if __name__ == '__main__':
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', 5000)),threaded=True)
+
+if __name__ == "__main__":
+    port = getenv("HBNB_API_PORT", default=5000)
+    host = getenv("HBNB_API_HOST", default="0.0.0.0")
+    app.run(host=host, port=port, threaded=True, debug=True)
