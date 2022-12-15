4758f251a4e72260a6fb94f57b1f14b16568b644
alejandrodgz <5199@holbertonstudents.com>
2022-12-14 19:34:59 -0500

    task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 import os
 from api.v1.views import app_views
 from models import storage
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,6 +14,10 @@ def close_this(self):
     """close the storage instance"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """404 not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     """documented pa"""
