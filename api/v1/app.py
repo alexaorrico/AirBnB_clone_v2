147e038d0151c4c6b61ef8cdb7709318a9be5dfe
abimont <abimontes00@gmail.com>
2022-12-14 21:54:08 -0500

    Update: pycodestyle app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,13 +9,15 @@ from os import getenv, environ
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close():
     """ method that closes a sesion """
     storage.close()
 
+
 if __name__ == "__main__":
     """ rinnung a server """
     app.run(host=getenv('HBNB_API_HOST', "0.0.0.0"),
-        port=getenv('HBNB_API_PORT', 5000),
-        threaded=True, debug=True)
+            port=getenv('HBNB_API_PORT', 5000),
+            threaded=True, debug=True)
