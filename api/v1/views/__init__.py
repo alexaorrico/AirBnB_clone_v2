8d3fb99530f1ad866b35f4c233bf4a93836bf46a
alejandrodgz <5199@holbertonstudents.com>
2022-12-14 19:05:15 -0500

    task4

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """blueprint init have faith"""
 from flask import Blueprint
-from api.v1.views.index import *
+
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
