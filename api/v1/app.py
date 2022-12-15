1b55d673f08d4e145e79c414db8b43877088128d
alejandrodgz <5199@holbertonstudents.com>
2022-12-14 20:01:23 -0500

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,7 +17,7 @@ def teardown(exception):
 @app.errorhandler(404)
 def not_found(error):
     """404 not found"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    return make_response(jsonify({"error": "Not found"}), 200)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', default="0.0.0.0")
