0016c0951586b8fb0afe4d5e75f7776d28b63c8e
alejandrodgz <5199@holbertonstudents.com>
2022-12-14 19:27:23 -0500

    task23

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
-app_views.route('/status')
+app_views.route('/status', methods=['GET'])
 def app_view():
     """just hoping this is a mistake"""
     
