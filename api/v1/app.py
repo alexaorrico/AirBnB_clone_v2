4b72d001c046a2132d9e6247d7892ad8572da087
Caballero018 <4617@holbertonstudents.com>
2022-12-14 22:56:57 -0500

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,10 @@ from flask import Flask, request
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors  import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views)
 
 
