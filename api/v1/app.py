11cacb60eeca9bf9eea1a18c33cc2fe609154101
alejandrodgz <5199@holbertonstudents.com>
2022-12-14 19:11:02 -0500

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ import os
 from api.v1.views import app_views
 from models import storage
 from flask import Flask
+from flask_cors import CORS
 
 
 if os.getenv("HBNB_API_HOST"):
@@ -17,7 +18,9 @@ else:
     HBNB_API_PORT = 5000
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
+
 
 
 @app.teardown_appcontext
