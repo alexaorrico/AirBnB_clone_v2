cf28656b1603a60d1b261823d472f02389dee4de
Jehp00 <Jehp01104006@gmail.com>
2022-12-15 11:22:29 -0500

    app task 4

---
+++ api/__init__.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+=======
+#!/usr/bin/python3
+""" """
+
+
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+app_views = Blueprint("simple_page", __name__, tamplate_folder='tamplates')
+
+@simple_page.route("/api/v1")
+def index():
+    pass
+>>>>>>> refs/remotes/origin/storage_get_count
