acbd3340184d405050e1a93cfa6294cb59506b28
Caballero018 <4617@holbertonstudents.com>
2022-12-14 22:35:29 -0500

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Doc"""
 from views import app_views
-from flask import jsonify
+from flask import jsonify, Flask
+
+app = Flask(__name__)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def index():
