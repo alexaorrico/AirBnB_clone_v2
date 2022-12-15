e78565ce8a899cfb88f76438d861b818b86c1d0c
Caballero018 <4617@holbertonstudents.com>
2022-12-15 15:39:31 -0500

    Update appy

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ connection  """
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,7 +14,11 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """ Calls storage.close """
     storage.close()
-    
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == "__main__":
     """ running conecction """
     app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
