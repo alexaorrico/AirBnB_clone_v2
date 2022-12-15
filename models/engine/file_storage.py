d3587247b51b675e10d92b662b548bf4b20fb93c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-15 03:33:49 +0000

    create-def-get-def-count-return-id-obj-count-obj

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,13 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ A method to retrieve one object: """
+        if cls and id:
+            return cls.query.get(id)
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage:"""
+        return (len(self.all(cls)))
