5cfe85a267fb39eecd67e3a8fcd0780d3b85a2b2
Jehp00 <Jehp01104006@gmail.com>
2022-12-16 16:32:50 -0500

    task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -35,9 +35,9 @@ def delete_state_obj(state_id):
 def creates_state():
     req_json = request.get_json()
     if not req_json:
-        make_response(jsonify({"error": "Not a JSON"}), 400)
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
     if "name" not in req_json:
-        make_response(jsonify({"erro": "Missing name"}), 400)
+        return make_response(jsonify({"erro": "Missing name"}), 400)
     newState = State()
     for k, v in req_json.items():
         setattr(newState, k, v)
