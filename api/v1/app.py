b292eecab693b82e2082e2c74225d6f13fad0aad
abimont <abimontes00@gmail.com>
2022-12-16 18:25:39 -0500

    Update: HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """ connection  """
 
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+CORS(app, resources={r'/*':{'origins':'0.0.0.0'}})
 
 app.register_blueprint(app_views)
 
