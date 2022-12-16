09b9bb432955b660e517db8ab53f4760efb18024
Caballero018 <4617@holbertonstudents.com>
2022-12-16 09:44:16 -0500

    Create cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,14 +10,17 @@ import requests
 from flask import request
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities(state_id=None):
     """ Retrieves the list of all State objects """
     obj = storage.get(State, state_id)
     if obj is None:
-         return make_response(jsonify({'error': 'Not found'}), 404)
-    cities_objs = [city.to_dict() for city in storage.all(City).values() if city.state_id == state_id]
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    cities_objs = [city.to_dict() for city in storage.all(City).values()
+                   if city.state_id == state_id]
     return make_response(jsonify(cities_objs), 200)
+
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id=None):
@@ -26,6 +29,7 @@ def get_city(city_id=None):
         return make_response(jsonify({'error': 'Not found'}), 404)
     else:
         return make_response(jsonify(obj.to_dict()), 200)
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id=None):
@@ -37,12 +41,14 @@ def delete_city(city_id=None):
     storage.save()
     return make_response(jsonify({}), 200)
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def post_city(state_id=None):
     data = request.get_json(silent=True, force=True)
     obj = storage.get(State, state_id)
     if obj is None:
-         return make_response(jsonify({'error': 'Not found'}), 404)
+        return make_response(jsonify({'error': 'Not found'}), 404)
     if data is None:
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     else:
@@ -53,6 +59,7 @@ def post_city(state_id=None):
     objs.state_id = state_id
     objs.save()
     return make_response(jsonify(objs.to_dict()), 201)
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
