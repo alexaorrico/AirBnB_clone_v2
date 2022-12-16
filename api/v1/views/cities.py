e1760842408ac6aeb61e25d9ab78a3a22c14876d
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-12-16 18:51:08 -0500

    update-cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -25,7 +25,7 @@ def get_cities_id(state_id):
                  strict_slashes=False)
 def get_city(city_id):
     """Retrieves a City object. : GET /api/v1/cities/<city_id>"""
-    city = storage.get("City", city_id)
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
     return jsonify(city.to_dict())
