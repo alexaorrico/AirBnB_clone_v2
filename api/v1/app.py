a9a1791c5b5237590f9a40ffbf90850dea99178f
abimont <abimontes00@gmail.com>
2022-12-16 18:27:16 -0500

    Update: pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-CORS(app, resources={r'/*':{'origins':'0.0.0.0'}})
+CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
 app.register_blueprint(app_views)
 
