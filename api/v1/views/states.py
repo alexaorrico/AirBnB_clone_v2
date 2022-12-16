300068b43f0c8f1a990f940e6c87e3a338ccf56a
abimont <abimontes00@gmail.com>
2022-12-15 21:01:24 -0500

    Update: file structure changed

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -14,27 +14,22 @@ meths = ['GET', 'DELETE', 'POST', 'PUT']
 @app_views.route('/states/<state_id>')
 def get_states(state_id=None):
     """ Retrieves the list of all State objects """
-    if state_id is not None:
-        if request.method == 'GET':
-            """ Method GET """ 
+    if request.method == 'GET':
+        """ Method GET """ 
+        if state_id is None:
+            state_objs = [state.to_dict() for state in storage.all(State).values()]
+            return state_objs
+        else:
             obj = storage.get(State, state_id)
             if obj is None:
                 return make_response(jsonify({'error': 'Not found'}), 404)
             else:
                 return obj.to_dict()
-
-        if request.method == 'DELETE':
-            """ Method DELETE """
-            obj = storage.get(State, state_id)
-            if obj is None:
-                return make_response(jsonify({'error': 'Not found'}), 404)
-            else:
-                key = "{}.{}".format(State.__name__, state_id)
-                storage.delete(obj)
-                storage.save()
-                if key not in storage.all():
-                    return make_response(jsonify('{}'), 200)
-        if request.method == 'PUT':
+    
+    if request.method == 'PUT':
+        if state_id is None:
+            return make_response(jsonify({'error': 'Not a JSON'}), 404)
+        else:
             obj = storage.get(State, state_id)
             if obj is None:
                 return make_response(jsonify({'error': 'Not found'}), 404)
@@ -47,20 +42,26 @@ def get_states(state_id=None):
                     obj.save()
                     return make_response(jsonify(obj), 200)
 
-    else:
-        if request.method == 'GET':
-            """ Method GET """ 
-            state_objs = [state.to_dict() for state in storage.all(State).values()]
-            return state_objs
+    if request.method == 'POST':
+        data = request.get_json()
+        if data is None:
+            return make_response(jsonify({'error': 'Not a JSON'}), 404)
+        else:
+            if 'name' not in data:
+                return make_response(jsonify({'error': 'Missing name'}), 404)
 
-        if request.method == 'POST':
-            data = request.get_json()
-            if data is None:
-                return make_response(jsonify({'error': 'Not a JSON'}), 404)
-            else:
-                if 'name' not in data:
-                    return make_response(jsonify({'error': 'Missing name'}), 404)
+        obj = State(**data)
+        obj.save()
+        return make_response(jsonify(obj.to_dict()), 201)
 
-            obj = State(**data)
-            obj.save()
-            return make_response(jsonify(obj.to_dict()), 201)
+    if request.method == 'DELETE':
+        """ Method DELETE """
+        obj = storage.get(State, state_id)
+        if obj is None:
+            return make_response(jsonify({'error': 'Not found'}), 404)
+        else:
+            key = "{}.{}".format(State.__name__, state_id)
+            storage.delete(obj)
+            storage.save()
+        if key not in storage.all():
+            return make_response(jsonify('{}'), 200)
