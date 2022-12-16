9ba5cfb5fc7f8c5d02248922717a0e26cdf3f1c0
Caballero018 <4617@holbertonstudents.com>
2022-12-16 12:58:26 -0500

    Create place reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -57,9 +57,10 @@ def post_review(place_id=None):
     obj_2 = storage.get(User, data['user_id'])
     if obj is None or obj_2 is None or (obj is None and obj_2 is None):
         return make_response(jsonify({'error': 'Not found'}), 404)
-    objs = Place(**data)
-    objs.place_id = place_id
-    objs.save()
+    else:
+        objs = Place(**data)
+        objs.place_id = place_id
+        objs.save()
     return make_response(jsonify(objs.to_dict()), 201)
 
 
