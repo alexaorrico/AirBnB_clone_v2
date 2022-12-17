354951def4b54c8bc927a854d51a6d76a51a1c1d
Jehp00 <Jehp01104006@gmail.com>
2022-12-16 22:39:14 -0500

    Task 8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-'''
-    app for registering blueprint and starting flask
-'''
-from flask import Flask, make_response, jsonify
+"""Module for Flask REST app"""
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    '''
-    close query after each session
-    '''
+def teardown(exception):
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    '''
-    return JSON formatted 404 status code response
-    '''
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    """404 not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    port = getenv("HBNB_API_PORT", default=5000)
-    host = getenv("HBNB_API_HOST", default="0.0.0.0")
-    app.run(host=host, port=port, threaded=True, debug=True)
+    host = os.getenv('HBNB_API_HOST', default="0.0.0.0")
+    port = os.getenv('HBNB_API_PORT', default=5000)
+    app.run(host=host, port=port, threaded=True)
