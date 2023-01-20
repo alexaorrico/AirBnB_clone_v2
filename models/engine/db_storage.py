9ac9d2c90fee08661d1c850205ae2db06e24b2d5
Robel Pop <robelpopandro@gmail.com>
2023-01-20 18:53:20 +0300

    uniti test added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,7 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-    
+
     def get(self, cls, id):
         """A method to retrieve one object"""
         if cls in classes.values():
