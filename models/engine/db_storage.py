b036713a0954eaf0721a9e2be09b0046e75de452
jude iwuji <judeiwuji@gmail.com>
2023-01-20 16:38:43 +0100

    Add: Task 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,17 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        return self.__session.query(cls).filter_by(id=id).one()
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        if cls is not None:
+            return self.__session.query(cls).count()
+        else:
+            total = 0
+            for k, v in classes.items():
+                total += self.__session.query(v).count()
+            return total
