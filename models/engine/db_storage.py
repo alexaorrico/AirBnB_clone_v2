ca835f6fffa55635b6e7c5acf75ce93f7d0153b5
Sineworknegasi <Sineworknegasi38@gmail.com>
2023-01-20 22:24:53 +0300

    AirBnB

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,16 +76,31 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        '''method to retrieve one object'''
-        if cls and id:
-            tempo = cls, __name__ + "." + id
-            count = self.all(cls)
-            for key in count:
-                if key == tempo:
-                    return count[key]
-        else:
+        """
+        Returns one object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
             return None
 
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+
+        return None
+
     def count(self, cls=None):
-        '''class (optional)'''
-        return (len(self.all(cls)))
+        """
+        Counts and returns the number of objects in storage matching the given class
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
