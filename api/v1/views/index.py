99b2a1b613368c19905e70ac9433a80a85d69f9d
KwameSei <nathanielosei92@gmail.com>
2023-01-20 15:48:41 +0000

    Stats API added to index.py edited

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,11 +22,9 @@ def status_api():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats_api():
     """Endpoint that retrieves the number of each objects by type"""
-    return (jsonify({
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-        }))
+    return (jsonify({"amenities": storage.count(Amenity),
+                     "cities": storage.count(City),
+                     "places": storage.count(Place),
+                     "reviews": storage.count(Review),
+                     "states": storage.count(State),
+                     "users": storage.count(User)}))
