215829d03e1333101eea5bdd814345c3d2e6a343
KwameSei <nathanielosei92@gmail.com>
2023-01-20 13:03:57 +0000

    Requirement fail errors fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Creation of API endpoints"""
 
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status')
+
+@app_views.route('/status', strict_slashes=False)
 def status_api():
     """Returning API status"""
     return (jsonify({"status": "OK"}))
