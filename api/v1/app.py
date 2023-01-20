78419f85dac2be41fd2bc279971b47649da8c6d0
erickadikah <erickadikah2030@gmail.com>
2023-01-20 20:27:12 +0300

    api

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""falsk """
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+@app.teardown_appcontext
+def tear_down(exc):
+    """teardown method"""
+    storage.close()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """404 error"""
+    return jsonify(error="Not found"), 404
+
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = getenv('HBNB_API_PORT') or 5000
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/states.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import jsonify, abort
+from models import storage
+from models.state import State
+from flask import*
+
+@app_views.route('/states', strict_slashes=False)
+def list_states():
+    """list of states"""
+    states = storage.all(State)
+    return jsonify(
+        [state.to_dict() for state in states.values()]
+            )
+
+@app_views.route('/states/<state_id>', strict_slashes=False)
+def get_state(state_id):
+    """get metod for states"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(
+        state.to_dict()
+            )
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=True)
+def delete_state(state_id):
+    """Delete state method"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    state.delete()
+    #del state
+    return jsonify({}), 200
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """create state"""
+    get_json = request.get_json()
+    if get_json is None:
+        abort(404, 'Not a JSON')
+    if get_json['name'] is None:
+        abort(404, 'Missing Name')
+    new_state = State(**get_json)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """updating state"""
+    new_state = storage.get(State, state_id)
+    if new_state is None:
+        abort('404')
+    if request.get_json() is None:
+        abort('404', 'Not s JSON')
+    update = request.get_json()
+    for key, value in update.items():
+        if key != 'id' or key != 'created_at' or key != 'updated_at':
+            setattr(new_state, key,value)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 200
