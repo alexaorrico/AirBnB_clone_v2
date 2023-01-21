c47f8e8e189d06b011bee73bc214f5957aed79ce
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-01-21 21:27:12 +0100

    Stats api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,27 @@
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route("/status")
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats():
+    amenities_cnt = storage.count("Amenity")
+    cities_cnt = storage.count("City")
+    places_cnt = storage.count("Place")
+    reviews_cnt = storage.count("Review")
+    states_cnt = storage.count("State")
+    users_cnt = storage.count("User")
+
+    stats = {
+        "amenities": amenities_cnt, 
+        "cities": cities_cnt, 
+        "places": places_cnt, 
+        "reviews": reviews_cnt, 
+        "states": states_cnt, 
+        "users": users_cnt,
+    }
+
+    return jsonify(stats)
