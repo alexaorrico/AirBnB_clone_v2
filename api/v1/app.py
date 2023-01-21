c8cc6d410d72c94bed528ba552bab9aaf0ebabe4
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-21 10:06:02 +0200

    Add status route

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+First endpoint (route) that will be to return the status of the API
+"""
+
+import os
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """ Return a custom 404 error """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=os.getenv('HBNB_API_PORT', '5000'), threaded=True)
