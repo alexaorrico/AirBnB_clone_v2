7c3ba8a7d45c0459b703aa0eee1b7daf1c4575a3
Rim El Filali <rimo1902@gmail.com>
2023-01-21 15:17:22 +0000

    add

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Flask Api Module"""
+from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+from models import storage
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(self):
+    """teardown"""
+    storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """ returns a JSON-formatted 404 status code response """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = getenv('HBNB_API_PORT', 5000)
+    app.run(host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""index module"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """returns status"""
+    return (jsonify({"status": "OK"}))
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """retrieves the number of each objects by type"""
+    return (jsonify({"amenities": storage.count(Amenity),
+                     "cities": storage.count(City),
+                     "places": storage.count(Place),
+                     "reviews": storage.count(Review),
+                     "states": storage.count(State),
+                     "users": storage.count(User)}))
