8075786489f760c7e0a37b50bb79dff6fda50b9c
Maxwell D. Dorliea <deboy170@gmail.com>
2023-01-21 04:36:41 +0000

    Handled 404 error for the whole application

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """ALX SE Flask Api Module."""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(exception=None):
+    """Handled resource not found error."""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 @app.teardown_appcontext
