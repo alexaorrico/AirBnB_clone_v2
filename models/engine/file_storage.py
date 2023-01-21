9da04d548e67780e02ffb1acc7e958ec04540407
kalibetre <kalbetre@icloud.com>
2023-01-21 11:55:59 +0300

    implement get in file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,8 +12,15 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {
+    "Amenity": Amenity,
+    "BaseModel": BaseModel,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User
+}
 
 
 class FileStorage:
@@ -55,7 +62,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -64,6 +71,21 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
+
+    def get(self, cls, id):
+        """gets the item that matches the specified class and id
+
+        Args:
+            id (str): the id of the object
+            cls (cls): the class of the object
+
+        Returns:
+            cls: obj or None
+        """
+        if (cls is None or id is None):
+            return None
+        objs = list(self.all(cls).values())
+        return next(filter(lambda x: x.id == id, objs), None)
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
