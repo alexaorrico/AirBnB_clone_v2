03b9d2280a860c7bfa9955a5e74537e2f510e592
kendimuthomi <kendijackline42@gmail.com>
2023-01-22 04:27:45 +0000

    Task 3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""init file"""
 from flask import Blueprint, abort, render_template
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
