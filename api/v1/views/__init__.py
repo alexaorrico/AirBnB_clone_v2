8da27dc79b44486a8344c7807af42cc04f4372f7
KwameSei <nathanielosei92@gmail.com>
2023-01-22 16:28:06 +0000

    changes made to amenities.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,4 @@ app_views = Blueprint('index', __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
