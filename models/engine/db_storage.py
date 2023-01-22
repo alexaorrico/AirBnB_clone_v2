c01b346104c3eebe755760a9fe9d8f8260eed906
ehoneahobed <ehoneahobed@hotmail.com>
2023-01-22 09:47:53 +0000

    Update solution to task 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,11 +11,15 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models import storage
-from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+# this helps incase you use a .env file
+try:
+    from decouple import config as getenv
+except ImportError:
+    from os import getenv
 
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -64,6 +68,7 @@ class DBStorage:
         """delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
+        self.save()
 
     def reload(self):
         """reloads data from the database"""
@@ -77,11 +82,14 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """Retrieves one object"""
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
         if cls not in classes.values():
             return None
 
-        all_cls = storage.all(cls)
+        all_cls = models.storage.all(cls)
         for value in all_cls.values():
             if (value.id == id):
                 return value
@@ -89,14 +97,16 @@ class DBStorage:
         return None
 
     def count(self, cls=None):
-        """Counts the number of objects in storage"""
+        """
+        count the number of objects in storage
+        """
         all_class = classes.values()
 
         if not cls:
             count = 0
             for clas in all_class:
-                count += len(storage.all(clas).values())
+                count += len(models.storage.all(clas).values())
         else:
-            count = len(storage.all(cls).values())
+            count = len(models.storage.all(cls).values())
 
         return count
