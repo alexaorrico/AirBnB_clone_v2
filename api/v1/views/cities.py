cd18ebf550c6b4b4b4a37a21be3e1d370193f54a
KwameSei <nathanielosei92@gmail.com>
2023-01-22 14:12:07 +0000

    City objects that handles all default RESTFul API actions edited

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,6 +3,7 @@
 from flask import jsonify, abort, request, make_response
 from models import storage
 from models.state import State
+from models.city import City
 from api.v1.views import app_views
 from models.base_model import BaseModel
 
