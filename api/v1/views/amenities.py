9f4af9227f16e58d2b1662000a15af8eb68d3107
erickadikah <erickadikah2030@gmail.com>
2023-01-22 21:53:31 +0300

    error checked

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,8 +3,7 @@ from api.v1.views import app_views
 from flask import jsonify, abort
 from models.amenity import Amenity
 from models import storage
-from flask import*
-
+from flask import *
 
 
 @app_views.route('/amenities', strict_slashes=False)
@@ -13,7 +12,8 @@ def list_amenities():
     amen = storage.all(Amenity)
     return jsonify(
         [am.to_dict() for am in amen.values()]
-            )
+    )
+
 
 @app_views.route('/amenities/<amenity_id>', strict_slashes=False)
 def get_amenity(amenity_id):
@@ -22,13 +22,16 @@ def get_amenity(amenity_id):
         abort(404)
     return jsonify(amenity.to_dict())
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
     amenity.delete()
     return jsonify({})
+
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
@@ -42,7 +45,9 @@ def create_amenity():
     new_amenity.save()
     return jsonify(new_amenity.to_dict())
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
@@ -52,6 +57,6 @@ def update_amenity(amenity_id):
     update = request.get_json()
     for key, value in update.items():
         if key != 'id' or key != 'created_at' or key != 'updated_at':
-            setattr(amenity, key,value)
+            setattr(amenity, key, value)
     amenity.save()
     return jsonify(amenity.to_dict()), 200
