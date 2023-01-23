747e55e39659f8e5146eabebeaba0f5e824a1d23
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-01-23 21:00:57 +0100

    FEAT: handling error for 404 page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,12 @@ def teardown_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return jsonify(error="Not found"), 404
+
+
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
