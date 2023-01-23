d6a276278e215c9dde938c7346affc2914e82402
Tom-254 <211mseeratiego@gmail.com>
2023-01-23 16:29:54 +0000

    0x05. AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,10 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={"/*": {"origins": '0.0.0.0'}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
