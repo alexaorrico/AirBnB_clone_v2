5506c3c9c4b62b0a59dcec4f07a5bafd06fc4943
mezaya <mesaya@live.com>
2023-01-23 19:26:21 +0200

    V3_Clone

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,4 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        self.password = hashlib.md5(self.password.encode()).hexdigest()
