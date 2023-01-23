1d602118d70a96e4f0fba5a3e29840874b355a1c
apha-pesso <mutiuayo99@gmail.com>
2023-01-23 16:05:59 +0100

    Check init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """init file"""
 from flask import Blueprint, render_template, abort
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -9,3 +8,6 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
