14ee882ab40023452854f8f85e9f827826a02327
erickadikah <erickadikah2030@gmail.com>
2023-01-23 12:39:10 +0300

    reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Review API"""
 from api.v1.views import app_views
-from flask import*
+from flask import *
 from models import storage
 from models.place import Place
 from models.review import Review
