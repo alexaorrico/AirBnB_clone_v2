214f29e625306cb83a82cc7aea71b1820173e955
Wachira007 <dannielmutua00@gmail.com>
2023-01-23 15:09:07 +0300

    Airbnb group project

--- README.md
+++ README.md
@@ -154,9 +154,9 @@ EOF  all  create  destroy  help  quit  show  update
 No known bugs at this time. 
 
 ## Authors
-Alexa Orrico - [Github](https://github.com/alexaorrico) / [Twitter](https://twitter.com/alexa_orrico)  
-Jennifer Huang - [Github](https://github.com/jhuang10123) / [Twitter](https://twitter.com/earthtojhuang)
+Rose Wachuka - [Github](https://github.com/ro61zzy) / [Twitter](https://twitter.com/rosewachukak)
+Daniel Wachira - [Github](https://github.com/Wachira007) [Twitter] (https://twitter.com/wac_h_ira)
 
-Second part of Airbnb: Joann Vuong
+Second part of Airbnb: Rose Wachuka and Daniel Wachira
 ## License
 Public Domain. No copy write protection. 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,26 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls=None, id=None):
+        """Returns obj based on class name and its ID"""
+        if cls is not None and id is not None:
+            for v in self.__objects.values():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    if v.id == id:
+                        return v
+
+        return None
+
+    def count(self, cls=None):
+        """Returns the amount of objects"""
+        count = 0
+        if cls is not None and cls in classes:
+            for v in self.__objects.values():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    count += 1
+        else:
+            for i in self.__objects.values():
+                count += 1
+
+        return count
