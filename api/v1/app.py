2041774cecd5b5b64fac36b14a0f6229e3c03548
kwabenasapong <kwabenasapong@gmail.com>
2023-01-23 15:10:08 +0000

    Revert to old

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """Script to return the status of an API"""
 
-from models import storage
 from flask import Flask, jsonify
+from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views)
+host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
+port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.errorhandler(404)
 def error_page(error):
     """Return error page if specified page is unreachable"""
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
@@ -25,5 +27,4 @@ def teardown_db(self):
 
 
 if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST') or '0.0.0.0',
-            port=getenv('HBNB_API_HOST') or 5000, threaded=True)
+    app.run(host=host, port=port, threaded=True)
