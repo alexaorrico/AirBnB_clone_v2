b6ecf6387e2d2c398337c8739e7d67fad808965f
Ousmane Konate <konatealim@gmail.com>
2023-01-23 18:13:45 +0100

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,12 @@ from models import storage
 from api.v1.views import app_views
 from flask import make_response, jsonify
 import os
+from flask_cors import CORS
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(stiven):
