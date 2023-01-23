d027643b127cba8a0646354d920eb0e4324753b7
erickadikah <erickadikah2030@gmail.com>
2023-01-23 12:13:11 +0300

    pycodestyle

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Place API"""
 from api.v1.views import app_views
-from flask import*
+from flask import *
 from models import storage
 from models.city import City
 from models.place import Place
@@ -29,7 +29,6 @@ def get_place(place_id):
     if place is None:
         abort(404)
     return jsonify(place.to_dict())
-
 
 
 @app_views.route('/places/<place_id>', methods=['DELETE'],
