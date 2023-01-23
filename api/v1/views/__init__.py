f87e4e7fec838339f83818537216c538b4dc7ec3
Ian Otieno <64345032+Ian12467@users.noreply.github.com>
2023-01-24 02:57:37 +0300

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,14 @@
-""" init file for views module
-"""
+#!/usr/bin/python3
+'''An init file'''
+
 from flask import Blueprint
 
-app_views = Blueprint('views', __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-from api.v1.views import (amenities, cities, places, places_amenities,
-                          places_reviews, states, users)
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
