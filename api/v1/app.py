690122343a8632eda744113386a710cc2be44dc1
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-01-23 21:03:13 +0100

    FIX: add make_response to error handler function

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Main Entry Point for the API"""
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -20,7 +20,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
-    return jsonify(error="Not found"), 404
+    return make_response(jsonify(error="Not found"), 404)
 
 
 if __name__ == "__main__":
