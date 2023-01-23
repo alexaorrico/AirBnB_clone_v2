2b0bbe841bb1adc3787f7238aaae28e6e0f4b56a
Tom-254 <211mseeratiego@gmail.com>
2023-01-23 16:17:36 +0000

    0x05. AirBnB clone - RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,2 +1,32 @@
 #!/usr/bin/python3
 
+"""
+Entry point for flask
+"""
+
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    """returns a JSON-formatted 404 status code response."""
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.teardown_appcontext
+def teardown_appcontext(error):
+    """teardown_appcontext"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True, debug=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
+"""
+Creates the Status route for app_views
+"""
 
+from api.v1.views import app_views
+from flask import Flask, jsonify
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'])
+def status_ok():
+    """ returns  json message with status OK"""
+    return jsonify({"status": "OK"}), 200
+
+
+@app_views.route('/stats', methods=['GET'])
+def obj_num():
+    """ retrieves number of objects by type"""
+    objects = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+
+    return jsonify(objects), 200
