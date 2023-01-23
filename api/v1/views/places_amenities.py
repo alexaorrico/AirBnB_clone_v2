755553d199d0267e87de53d3f463539359d584ae
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-01-23 22:59:23 +0200

    Add API routes for Place-Amenity objects

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -13,7 +13,7 @@ from models.amenity import Amenity
 
 @app_views.route('/places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
-def get_amenities(place_id):
+def get_place_amenities(place_id):
     """Retrieves the list of all Amenity objects of a Place"""
     place = storage.get(Place, place_id)
     if place is None:
@@ -23,8 +23,8 @@ def get_amenities(place_id):
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 'DELETE', strict_slashes=False)
-def delete_amenity(place_id, amenity_id):
+                 methods=['DELETE'], strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
     """Deletes a Amenity object"""
     place = storage.get(Place, place_id)
     if place is None:
@@ -40,8 +40,8 @@ def delete_amenity(place_id, amenity_id):
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 'POST', strict_slashes=False)
-def post_amenity(place_id, amenity_id):
+                 methods=['POST'], strict_slashes=False)
+def post_place_amenity(place_id, amenity_id):
     """Link a Amenity object to a Place"""
     place = storage.get(Place, place_id)
     if place is None:
