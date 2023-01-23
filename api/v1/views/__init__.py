a533490928006deb27fc2874b54d12db5b9afe20
erickadikah <erickadikah2030@gmail.com>
2023-01-23 11:39:24 +0300

    passed

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """init file"""
-
-from flask import Blueprint
+from flask import Blueprint, render_template, abort
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
