9e2088ea0fbb10c36a7189e3abc20a047bafdbb0
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-01-23 20:20:48 +0100

    FEAT: added the stats route to get the number of each models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,21 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns a JSON status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Returns stats about the API"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
