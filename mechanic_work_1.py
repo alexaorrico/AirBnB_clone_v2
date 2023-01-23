a8448178e2717a9b1b3e57ecc6a316a0c962e52e
kwabenasapong <kwabenasapong@gmail.com>
2023-01-23 11:05:36 +0000

    getting ready to pull request

--- mechanic_work_1.py
+++ mechanic_work_1.py
@@ -2,11 +2,12 @@
 '''Mechanic Work 1'''
 from models.state import State
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
 from models.amenity import Amenity
 
 
-house_db = FileStorage()
-
+# house_db = FileStorage()
+house_db = DBStorage()
 for _ in range(5):
     house_1 = Amenity()
     house_db.new(house_1)
