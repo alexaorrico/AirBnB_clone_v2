7f8b97257d8908c0aeede64f6a5b6fceb2848332
GodblessMe2 <oshindeinde@gmail.com>
2023-01-23 17:42:17 +0100

    Check for indentation and worked on it

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,6 @@
 """ Blueprint creation
 """
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -13,3 +10,4 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify
+from flask import jsonify, request
 
-@app_views.route('/status')
+
+@app_views.route('/status', methods=['GET'])
 def response():
-  """Status: Ok"""
-  dic = {"status": "OK"}
-  return jsonify(dic)
+    """Status: Ok"""
+    if request.method == 'GET':
+        res = {"status": "Ok"}
+        return jsonify(res)
 
-@app_views.route('/stats')
+
+@app_views.route('/stats', methods=['GET'])
 def class_counter():
-  """Get a dictionary from count method"""
-  dic = {}
-  dic["amenities"] = storage.count("Amenity")
-  dic["city"] = storage.count("City")
-  dic["place"] = storage.count("Place")
-  dic["reviews"] = storage.count("Reviews")
-  dic["states"] = storage.count("States")
-  dic["users"] = storage.count("Users")
-  return jsonify(dic)
+    """Get a dictionary from count method"""
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
