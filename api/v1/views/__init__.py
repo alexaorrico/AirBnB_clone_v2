7d4a30d18c61fb2a8f0c73638ef35ee6c1a69d61
erickadikah <erickadikah2030@gmail.com>
2023-01-23 11:20:21 +0300

    changes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,7 @@
 """init file"""
 
 from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -9,6 +10,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
