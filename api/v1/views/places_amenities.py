46d3b91daa6dbf1702572e70dcc216cf3df512e8
apha-pesso <mutiuayo99@gmail.com>
2023-01-24 00:24:11 +0100

    Add amenity to place

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Amenities API"""
+from api.v1.views import app_views
+from flask import abort, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'],
+                 strict_slashes=False)
+def get_amenities(place_id):
+    """get method for amenities of a place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenities = storage.all(Amenity)
+    place_amenity = []
+    for amenity in amenities.values():
+        if amenity.place_id == place_id:
+            amenity_review.append(amenity.to_dict())
+    return jsonify(place_amenity)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_amenity(place_id, amenity_id):
+    """Delete an amenity"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if (amenity.place_id != place_id):
+        abort(404)
+    amenity.delete()
+    return jsonify({}), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+def link_amenity_to_place(place_id, amenity_id):
+    """Link Amenity to a place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if (amenity.place_id == place_id):
+        return jsonify(amenity.to_dict()), 200
+    else:
+        amenity.place_id = place_id
+    return jsonify(amenity.to_dict()), 201
