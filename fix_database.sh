1b5c6c94c6fb92e3f8c776ecb3fa970e8524efd1
kwabenasapong <kwabenasapong@gmail.com>
2023-01-23 16:33:31 +0000

    Bash script to fix database for mysql 8.*

--- fix_database.sh
+++ fix_database.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # this will fix the database problems
 
-cat setup_mysql_dev.sql | mysql -uroot -p
-cat 100-hbnb.sql | mysql -uroot -p
+echo "DROP DATABASE IF EXISTS hbnb_dev_db;" | mysql -uroot -p
+cmd setup_mysql_dev.sql | mysql -uroot -p
+echo "USE hbnb_dev_db;" | mysql -uroot -p
+cmd 100-hbnb.sql | mysql -uroot -p
 echo "Database hbnb_dev_db and password hbnb_dev_pwd setup and ready for use"
