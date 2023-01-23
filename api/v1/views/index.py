6daa2ef20faec9cefe1c8e1658a587ba5ee9663b
mezaya <mesaya@live.com>
2023-01-23 19:39:49 +0200

    V3_Clone

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,31 @@
 #!/usr/bin/python3
-"""API index views module"""
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models import storage
+"""Initialize flask functions"""
+from flask import jsonify, make_response
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify, request
+
+classes = {"Amenity": "amenities",
+           "City": "cities",
+           "Place": "places",
+           "Review": "reviews",
+           "State": "states",
+           "User": "users"}
 
 
-@app_views.route('/status')
-def status():
-    """
-    Returns json response as the status
-
-    Returns:
-        JSON: json object
-    """
-    status = {
-        "status": "OK"
-    }
-    return jsonify(status)
+@app_views.route('/status', strict_slashes=False)
+def view_status():
+    """Returns a JSON"""
+    response = jsonify({"status": "OK"})
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    """
-    function to return the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+@app_views.route('/stats', strict_slashes=False)
+def storage_stats():
+    """Returns a JSON"""
+    dict = {}
+    for cls, name in classes.items():
+        dict.update({name: storage.count(cls)})
+    response = jsonify(dict)
+    response.headers["Content-Type"] = "application/json"
+    return response
