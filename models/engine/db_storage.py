da4f60d3ef1921f38348ea049af6d6a095298ac8
Ian Otieno <64345032+Ian12467@users.noreply.github.com>
2023-01-23 23:16:46 +0300

    new methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,7 +73,7 @@ class DBStorage:
 
     def close(self):
         """call remove() method on the private session attribute"""
-       self.__session.remove()
+        self.__session.remove()
 
     def get(self, cls, id):
         """
