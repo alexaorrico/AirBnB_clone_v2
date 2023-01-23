4451d2eb2e599d291c9c028505dfaac196891df7
kalibetre <kalbetre@icloud.com>
2023-01-23 15:11:48 +0300

    add place search route

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -2,12 +2,16 @@
 """
 places view routes
 """
+from functools import reduce
+from operator import concat
+
 from flask import abort, jsonify, request
 
 from api.v1.views import app_views
-from models import storage
+from models import storage, storage_t
 from models.city import City
 from models.place import Place
+from models.state import State
 from models.user import User
 
 
@@ -83,3 +87,66 @@ def places(place_id):
 
     else:
         return jsonify(place.to_dict())
+
+
+@app_views.route(
+    "/places_search",
+    methods=["POST"],
+    strict_slashes=False,
+)
+def search_places():
+    """searches places by the given states, cities and amenities
+    """
+    query = request.get_json(silent=True)
+    if query is None:
+        return jsonify(error="Not a JSON"), 400
+
+    state_ids = query.get("states", [])
+    city_ids = query.get("cities", [])
+    amenity_ids = query.get("amenities", [])
+
+    search_result = [place.to_dict() for place in storage.all(Place).values()]
+    if len(query) == 0 or len(state_ids + city_ids + amenity_ids) == 0:
+        return jsonify(search_result)
+
+    search_result = filter_places_by_cities(search_result, city_ids, state_ids)
+    search_result = filter_places_by_amenities(search_result, amenity_ids)
+
+    return jsonify(search_result)
+
+
+def filter_places_by_cities(places, city_ids, state_ids=[]):
+    if len(state_ids + city_ids) > 0:
+        state_cities = reduce(
+            concat,
+            [
+                state.cities for state in storage.all(State).values()
+                if state.id in set(state_ids)
+            ],
+            [],
+        )
+        city_ids.extend([city.id for city in state_cities])
+
+        return [
+            place for place in places
+            if place.get("city_id", None) in set(city_ids)
+        ]
+
+    return places
+
+
+def filter_places_by_amenities(places, amenity_ids):
+    if len(amenity_ids) > 0:
+        amenity_ids = set(amenity_ids)
+        fileted_place_ids = []
+        for place in places:
+            if storage_t == 'db':
+                place_amenity_ids = {amenity.id for amenity in place.amenities}
+            else:
+                place_amenity_ids = place.amenity_ids
+
+            if len(amenity_ids.intersection(place_amenity_ids)) != 0:
+                fileted_place_ids.append(place.id)
+        return [place for place in places if place["id"] in fileted_place_ids]
+
+    return places
