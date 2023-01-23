03d8d756f9ecf181a1928d3f484662e0f9418e0f
kwabenasapong <kwabenasapong@gmail.com>
2023-01-23 15:01:40 +0000

    Update __init__.py in views

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,6 @@ from os import getenv
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
-port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
 app.register_blueprint(app_views)
 
 
@@ -27,4 +25,5 @@ def teardown_db(self):
 
 
 if __name__ == "__main__":
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv('HBNB_API_HOST') or '0.0.0.0',
+            port=getenv('HBNB_API_HOST') or 5000, threaded=True)
