660b59a08308210eaa2572b31d3419821a14ab5b
kwabenasapong <kwabenasapong@gmail.com>
2023-01-23 15:18:56 +0000

    Revert to old

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """init file"""
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+app_views = Blueprint('index', __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
