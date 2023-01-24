80f1ec7af3ea1b37527763b5862afa4f9407e81e
erickadikah <erickadikah2030@gmail.com>
2023-01-24 13:48:31 +0300

    Delete method implemeted

--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
-        # cities = relationship("City", backref="state", cascade="delete")
+        cities = relationship("City", backref="state", cascade="delete")
     else:
         name = ""
 
