254698a3f75f859a6a369605f26e061c5478b510
apha-pesso <mutiuayo99@gmail.com>
2023-01-24 07:08:30 +0100

    Check

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -27,7 +27,7 @@ def get_amenities(place_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
-def delete_amenity(place_id, amenity_id):
+def delete_linked_amenity(place_id, amenity_id):
     """Delete an amenity"""
     place = storage.get(Place, place_id)
     if place is None:
