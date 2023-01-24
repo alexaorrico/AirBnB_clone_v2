866dadd2a45148a4f112217d73d962a5f3a544ef
KwameSei <nathanielosei92@gmail.com>
2023-01-24 02:56:40 +0000

    Continuation code added to amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -17,3 +17,53 @@ def get_amenities():
     for amenity in amenities:
         amenity_list.append(amenity.to_dict())
         return (jsonify(amenity_list))
+
+
+@app_views.route('/amenities', methods=["POST"], strict_slashes=False)
+def create_amenity():
+    """Creating new amenity to the list"""
+    data = request.get_json()
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+    if 'name' not in request.json:
+        abort(400, description="Missing name")
+    amenity = Amenity(**data)
+    amenity.save()
+    amenity.save()
+    return (jsonify(amenity.to_dict(), 201)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=["PUT"],
+                 strict_slashes=False)
+def get_amenity_by_id(amenity_id):
+    """Updating invidual amenities by their ids"""
+    one_amenity = storage.get(Amenity, amenity_id)
+    
+    if not amenity:
+        abort(404)
+
+    if request.method == "GET":
+        result = one_amenity.to_dict()
+        return (jsonify(one_amenity))
+
+    if request.method == "PUT":
+        data = request.get_json()
+        if not request.is_json:
+            abort(400, description="Not a JSON")
+        for key, value in data.items():
+            setattr(amenity, key, value)
+        amenity.save()
+        return (jsonify(amenity.to_dict()), 200)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=["DELETE"],
+                 strict_slashes=False)
+def delete_amenity_by_id(amenity_id):
+    """Deleting an amenity by its id"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    result = make_response(jsonify({}), 200)
+    return result
