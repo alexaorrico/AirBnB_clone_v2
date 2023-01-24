65d20776a82e7e5f764ab493135547233c62555a
apha-pesso <mutiuayo99@gmail.com>
2023-01-24 10:36:37 +0100

    Check delete method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,7 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
+                self.save()
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
