39886a3e65cb22d68f4fb92772b317ad428c77cb
apha-pesso <mutiuayo99@gmail.com>
2023-01-24 10:23:12 +0100

    Check model

--- models/state.py
+++ models/state.py
@@ -14,7 +14,8 @@ class State(BaseModel, Base):
     if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="delete")
+        cities = relationship("City", backref="state")
+        # cities = relationship("City", backref="state", cascade="delete")
     else:
         name = ""
 
