0b5e7352f1f76cd902a901dd12c6a153fbd916ab
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-01-25 09:41:57 +0100

    Refactor code into another file

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Places Amenities API"""
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+from . import app_views
+from flask import jsonify, abort
+
+@app_views.route("/places/<place_id>/amenities")
+def place_amenities(place_id):
+    """Get amenities of a place"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenities = place.amenities
+    amenity_list = []
+    for amenity in amenities:
+        amenity_list.append(amenity.to_dict())
+    return jsonify(amenity_list)
+
+@app_views.route("/places/<place_id>/amenities/<amenity_id>")
+def delete_place_amenity(place_id, amenity_id):
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return jsonify({})
+
+@app_views.route("/places/<place_id>/amenities/<amenity_id>", methods=["POST"])
+def link_place_amenity(place_id, amenity_id):
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if storage.get(Place, amenity.place_id):
+        return jsonify(amenity.to_dict())
+    amenity.place_id = place_id
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
