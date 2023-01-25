5b007324729a0b6e7509e505c705e40049acc846
Ikenna <ikenna@kali.Ikenna-PC>
2023-01-24 23:59:05 -0500

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,26 +7,33 @@ from os import environ
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def close_dataBase(e):
-    """tear down app context"""
-    
+    """
+    tear down app context
+    """
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(e):
+    """
+    404 route
+    """
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
-    host=environ.get('HBNB_API_HOST')
-    port=environ.get('HBNB_API_PORT')
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
     if not host:
         host = '0.0.0.0'
     if not port:
