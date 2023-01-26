cee091de9af64b9c89463d93c4f64956cb53ff35
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-01-26 15:48:18 -0800

    file updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,39 +1,30 @@
 #!/usr/bin/python3
 '''Defines the JSON GET request from the application'''
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+# Importing classes from models directory
 from models.amenity import Amenity
 from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
-from models import storage
-from api.v1.views import app_views
-from models import storage
-from flask import jsonify, request
+from models.review import Review
+from models.place import Place
+from models.state import State
 
 
-@app_views.route('/status')
-def status():
-    '''Returns json response as the status'''
-    status = {
-        "status": "OK"
-    }
-    return jsonify(status)
+@app_views.route('/status', strict_slashes=False)
+def status_api():
+    """Returning API status"""
+    return (jsonify({"status": "OK"}))
 
 
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    '''Returns the count of all class objects'''
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats_api():
+    """Endpoint that retrieves the number of each objects by type"""
+    return (jsonify({"amenities": storage.count(Amenity),
+                     "cities": storage.count(City),
+                     "places": storage.count(Place),
+                     "reviews": storage.count(Review),
+                     "states": storage.count(State),
+                     "users": storage.count(User)}))
