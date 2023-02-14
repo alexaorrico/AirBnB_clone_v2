1482905456bb96b38c68963b72c050d5121d633b
dcode3244 <dagimourgie111@gmail.com>
2023-02-14 17:43:54 +0000

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,24 @@ creates a flask route that returns a json
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def status():
+    ''' responds to /status route and returns status '''
     return (jsonify({'status': 'OK'}))
+
+
+@app_views.route('/stats')
+def stats():
+    ''' responds to /stats route and returns number of objects by type '''
+    ret = {
+      "amenities": storage.count('Amenity'),
+      "cities": storage.count('City'),
+      "places": storage.count('Place'),
+      "reviews": storage.count('Review'),
+      "states": storage.count('State'),
+      "users": storage.count('User')
+    }
+    return (jsonify(ret))
