9ffdaaea01cd4ab4f50f6b7749ce2a5d9cc65d5d
dcode3244 <dagimourgie111@gmail.com>
2023-02-14 13:38:23 +0000

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from os import getenv
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ closes the storage on teardown  """
+    storage.close()
+
+
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0,0')
+    PORT = getenv('HBNB_API_PORT', '5000')
+
+    app.run(host=HOST, port=PORT, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def status():
+    return (jsonify({'status': 'OK'}))
