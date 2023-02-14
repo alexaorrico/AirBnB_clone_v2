3e4fe5bca40e73b9c1988eb01c1e2f1836209a24
reinhard <reinhard2057@gmail.com>
2023-02-14 11:20:07 +0000

    task 3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 """blueprint"""
-
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def index():
     return jsonify({"status": "OK"})
