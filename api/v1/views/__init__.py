043d7e53b37d3a46b46aa60280d189e6d0e14390
dcode3244 <dagimourgie111@gmail.com>
2023-02-14 13:52:34 +0000

    bug fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,5 @@
 ''' flask Bluprint '''
 
 from flask import Blueprint
-from api.v1.views.index import *
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
