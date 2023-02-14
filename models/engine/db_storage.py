18f2c507f735614fba875a794522b13cb9aa413e
bek2518 <bereketb20@hotmail.com>
2023-02-14 20:28:44 +0300

    Created new branch

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,13 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+    
+    def get(self, cls, id):
+        """
+        A method to retrieve one object
+        """
+
+    def count(self, cls=None):
+        """
+        A method to count the number of objects in storage
+        """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,13 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+    
+    def get(self, cls, id):
+        """
+        A method to retrieve one object
+        """
+
+    def count(self, cls=None):
+        """
+        A method to count the number of objects in storage
+        """
