bad67479e0029acc9def1a594a752dd66b598ebc
Okinbird <okinyinka@yahoo.co.uk>
2023-02-14 19:24:57 +0000

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,17 @@ Initiates a Flask app
 """
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({'error': 'Not found'}), 404
 
 
 @app.teardown_appcontext
