81f2fb78bb18f1658d80b8fcd63565d5def6b07d
kingieX <chimakingsley216@gmail.com>
2023-02-15 04:23:48 -0800

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,7 @@ app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashe = False
 app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
@@ -22,6 +23,20 @@ def teardown_flask(exception):
     '''method that handles teardown'''
     # print(exception)
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    '''handles 404 error code'''
+    return jsonify(error='Not found'), 404
+
+@app.errorhandler(400)
+def error_400(error):
+    '''handles 400 error code'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
 
 
 if __name__ == '__main__':
