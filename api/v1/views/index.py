caa197fcf54bc411f7f69000785cc2fb149548c6
kingieX <chimakingsley216@gmail.com>
2023-02-15 04:44:49 -0800

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,16 @@ from api.v1.views import app_views
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    '''Number of each object by type'''
+    return jsonify(
+            amenities=storage.count('Amenity'),
+            cities=storage.count('City'),
+            places=storage.count('Place'),
+            reviews=storage.count('Reviews'),
+            states=storage.count('State'),
+            users=storage.count('User')
+        )
