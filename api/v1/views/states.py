c46a2264035893f82b80b94b3154ced2e6683ec5
Nnachi Kingsley <chimakingsley216@gmail.com>
2023-02-15 08:18:17 -0800

    api

---
+++ api/v1/views/states.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+'''state view for API'''
+from flask import abort, jsinify, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.state import state
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def state():
+    '''list all state object'''
+    objs = storage.all(state)
+    return jsonify([obj.to_dict() for obj in objs.values()])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def single_state(state_id):
+    '''Retrieve state object'''
+    obj = storage.get(state, state_id)
+    if not obj:
+        abort(404)
+    return jsonify(obj.to_dict())
+
+
+@app_views.route('/states/<state_id>',
+        method=['DELETE'], strict_slashes=False)
+def del_state(state_id):
+    '''delete state object'''
+    obj = storage.get(state, state_id)
+    if not obj:
+        abort(404)
+    obj.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    '''return new state'''
+    new_obj = request.get_json()
+    if not new_obj:
+        abort(400, "Not a JSON")
+    if 'name' not in new_obj:
+        abort(400, "Missing name")
+    obj = state(**new_obj)
+    storage.new(obj)
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 201)
+
+
+@app_view.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    '''update state object'''
+    obj = storage.get(state, state_id)
+    if not obj:
+        abort(404)
+
+    req = request.get_json()
+    if not req:
+        abort(400, "Not a JSON")
+
+    for k, v in req.items():
+        if k not in ['id', 'created_at', 'updated_at']:
+            setattr(obj, k, v)
+
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 200)
