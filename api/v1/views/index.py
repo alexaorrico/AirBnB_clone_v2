a7ea684236de345b5dcc48642671a3a5de90ac18
bill omole <omolebill01@gmail.com>
2023-02-15 14:57:40 +0100

    bug fix- circular import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" a route /status on the object app_views that returns 
+""" a route /status on the object app_views that returns
     a JSON: "status": Ok
 """
 from api.v1.views import app_views
 from flask import jsonify
+import json
+
 
 @app_views.route('/status')
 def status():
-    """routes the url status"""
+    """returns the url status"""
     return jsonify({"status": "OK"})
-
