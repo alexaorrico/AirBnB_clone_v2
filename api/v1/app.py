438c7a00516b789221ff1832573bd1df5b7f5186
richardoluwadamilola <oluwatobyrichard@gmail.com>
2023-02-15 07:13:33 -0800

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""
+    This script starts a Flask web application
+"""
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
+
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
-    storage.close()
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    return storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
+def error(e):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 app.config['SWAGGER'] = {
