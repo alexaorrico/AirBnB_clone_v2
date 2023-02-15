1b78a6d133041717df2a61c5c8d9565f8e49ffa7
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-15 17:54:52 +0300

    CORS setup

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,14 @@ from os import environ
 from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+
+# Create a CORS instance that allows all origins for 0.0.0.0
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
