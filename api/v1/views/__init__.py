1afcaa56e8529f27165b270be0161e23907214d1
Joseph Alikah <alikahjoseph@gmail.com>
2023-02-15 01:30:29 +0100

    Add stats route

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,6 @@
 """API v1 view."""
 from flask import Blueprint
 
-app_views = Blueprint("/api/v1", __name__)
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,27 @@
 """Views for the API status."""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route("/status")
 def status():
     """Return the API status."""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """Return the count of the various stored objects."""
+    return jsonify(
+        {key: storage.count(value) for key, value in classes.items()}
+    )
