30a2811df274a6e03ab791ea8d0cc78a2610730b
fabrizia2 <melvinrenish56@gmail.com>
2023-02-15 18:53:36 +0300

    Status

---
+++ api/v1/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""API instanc of Flask"""
+
+from flask import Flask, jsonify, make_response
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """Closes session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
