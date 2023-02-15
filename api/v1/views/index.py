d0be47d4c723ffc7836d8377a577c9f8107c7edb
iakev <kirimikmwongera@gmil.com>
2023-02-15 16:19:50 +0000

    added a route to list number of objects in every category

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,20 @@
 from api.v1.views import app_views
 from flask import jsonify
 import json
+from models import storage
 
-
+print(type(Amenity))
 @app_views.route('/status')
 def status():
     """returns the url status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    """returns number of each objects by type"""
+    objs = {"amenities": 'Amenity', "cities": 'City', "places":
+            'Place', "reviews": 'Review', "states": 'State', "users": 'User'}
+    new_dict = {}
+    for key, val in objs.items():
+        new_dict[key] = storage.count(val)
+    return jsonify(new_dict)
