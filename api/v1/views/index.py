3b15e8ccdee684c41e721f581e28915d04cd9f4f
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-15 15:05:22 +0300

    fixed pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,6 +15,7 @@ def status():
     """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """Retrieves the number of each objects by type"""
