731a9027a4cee50a1b81408487dd9f24b93b91be
Silvester Kilungya <silvesterkilungya@gmail.com>
2023-02-15 15:02:32 +0300

    retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ This module defines a Flask route /status and returns a JSON.
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"])
@@ -13,3 +14,19 @@ def status():
     Return a JSON with "status": "OK"
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    classes = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    count_dict = {}
+    for cls in classes:
+        count_dict[classes[cls]] = storage.count(cls)
+    return jsonify(count_dict)
