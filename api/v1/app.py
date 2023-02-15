f5a202577f8a105fde64772211b6291697f87037
kingieX <chimakingsley216@gmail.com>
2023-02-15 04:37:31 -0800

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,7 +15,7 @@ app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashe = False
 app.register_blueprint(app_views)
-"""CORS(app, resources={'/*': {'origins': app_host}})"""
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
