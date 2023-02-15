a5b976548f8c2fb644b910395c2d5a1db898cfba
ibkdizzu <ibkdizzu@gmail.com>
2023-02-15 23:35:20 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -58,19 +58,15 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, save_fs=None):
-        """returns a dictionary containing all keys/values of the instance"""
+    def to_dict(self, remove_password=True):
+        """Returns a dictionary containing all keys/values of the instance."""
         new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        if save_fs is None:
-            if "password" in new_dict:
-                del new_dict["password"]
+        new_dict.pop("_sa_instance_state", None)
+        if remove_password is True:
+            new_dict.pop("password", None)
         return new_dict
 
     def delete(self):
