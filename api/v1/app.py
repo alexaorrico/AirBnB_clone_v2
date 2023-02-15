5a3e853bcb778df49e2709736fbb2925549a7072
fabrizia2 <melvinrenish56@gmail.com>
2023-02-15 20:15:51 +0300

    Executable

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""API instanc of Flask"""
+"""Status of API"""
 
 from flask import Flask, jsonify, make_response
 from models import storage
@@ -20,20 +20,11 @@ def tear_down(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
+    """page not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    """Runs app on in this module"""
+    app.run(host=os.getenv("HBNB_API_HOST", "0.0.0.0"), port=os.getenv(
+            "HBNB_API_PORT", "5000"), threaded=True)
