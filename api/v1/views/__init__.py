c9bc2e46516aff8dfd82499325a8cb63225ca8eb
fabrizia2 <melvinrenish56@gmail.com>
2023-02-16 14:21:45 +0300

    adjustments

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -14,8 +14,8 @@ from models.review import Review
 
 """import flask views"""
 from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
+"""from api.v1.views.states import *"""
+"""from api.v1.views.cities import *"""
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
