0ea1d60f4a9fc7e85d114bdada2b8c44c46198f9
Mdlalose4life <blessing1059@gmail.com>
2023-02-17 01:57:10 +0200

    The blue prints

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Return the status of the server."""
+    return jsonify({'status': 'ok'})
