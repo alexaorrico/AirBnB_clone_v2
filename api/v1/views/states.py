6b6e628473fc335a61a3ce47dbedcc5c6e55ae00
ru0ya <viktaangi02@gmail.com>
2023-02-16 22:26:15 +0300

    debug

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Creating a new view for State objects that handles all default RESTFul API actions:
+Creating a new view for State objects that
+handles all default RESTFul API actions:
 """
 from models import storage
 from models.state import State
