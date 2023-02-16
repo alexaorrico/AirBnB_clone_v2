d595af6bec1bb8b9ecac90e9733fa90f49dc2b92
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-02-16 13:58:53 +0300

    upd:boom:

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-
-
-from flask import Flask
+"""API setup
+"""
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
 
-HBNB_API_HOST = "0.0.0.0"
-HBNB_API_PORT = 5000
-
-def close_storage(exception=None):
-    """"performs clea up tasks"""
-    storage.close()
 
 @app.teardown_appcontext
-def teardown_appcontext(exception=None):
-    close_storage(exception)
+def end_session(exception):
+    """Calls on storage.close method
+    """
+    return storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
-    """handles 404 not found errors"""
-    return jsonify({"error": "Not found"}), 404
+    """Page not found error handler
+    """
+    return make_response(jsonify({"error": "Not Found"}), 404)
 
 
-if __name__ = "__main__":
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+if __name__ == "__main__":
+    HOST = getenv('HBNB_API_HOST', default='0.0.0.0')
+    PORT = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=HOST, port=PORT, threaded=True)
