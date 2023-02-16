6d22f42796d616104f1736d7c0025b63d4896148
MpiloNM95 <mpilonmafu95@gmail.com>
2023-02-16 01:36:01 -0800

    app added t api directory

---
+++ api/v1/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Contains the FileStorage class
+"""
+from flask import Flask, jsonify
+from models import storage
+from api.vi.views import app_views
+from os import getenv
+from flask_cors import CORS
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+
+if __name__ == "__main__":
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
+    else:
+        host = '0.0.0.0'
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
