8f4ad925151681b38ed3a55b94f91f4eb83f19ac
LoloDum <106752483+LoloDum@users.noreply.github.com>
2023-02-16 10:06:31 +0100

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,5 +87,3 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
-
-    
