0da41790df05ecc77028fd456257250b54b86047
iakev <kirimikmwongera@gmil.com>
2023-02-16 14:06:22 +0000

    setting up CORS for the application

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,14 @@
 This is an endpoint to return the status of the API
 """
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import environ as env
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, origins=["0.0.0.0"])
 
 
 @app.teardown_appcontext
