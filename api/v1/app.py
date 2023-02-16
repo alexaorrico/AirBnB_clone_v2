a2c6df053a68aa56778d6415ee8314a499a93057
MpiloNM95 <mpilonmafu95@gmail.com>
2023-02-16 06:57:17 -0800

    done with mendatory tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,33 +2,28 @@
 """
 Contains the FileStorage class
 """
-from flask import Flask, jsonify
+import os
 from models import storage
-from api.vi.views import app_views
-from os import getenv
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
+
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Remove the current SQLAlchemy session"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Returns a error 404"""
-    return jsonify({'error': 'Not found'}), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    if getenv('HBNB_API_HOST'):
-        host = getenv('HBNB_API_HOST')
-    else:
-        host = '0.0.0.0'
-    if getenv('HBNB_API_PORT'):
-        port = getenv('HBNB_API_PORT')
-    else:
-        port = 5000
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
