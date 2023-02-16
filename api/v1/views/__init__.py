d6a9f870c9c5ed34bc8596612d3afb2eab467047
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-02-16 10:56:38 +0000

    update inits

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,16 +3,6 @@
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-"""import flask views"""
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-
-
 """imort storage engine and classes"""
 from models import storage
 from models.state import State
@@ -21,3 +11,12 @@ from models.amenity import Amenity
 from models.user import User
 from models.place import Place
 from models.review import Review
+
+"""import flask views"""
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
