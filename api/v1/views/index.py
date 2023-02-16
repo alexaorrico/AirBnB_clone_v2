12e793b3a052aba300db7808801ed70597793ee0
MpiloNM95 <mpilonmafu95@gmail.com>
2023-02-16 02:04:20 -0800

    some stats added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,3 +6,13 @@ from api.v1.views import app_views
 def api_status():
     """returns the status of the API"""
     return jsonify(status='OK')
+
+@app_views.route('/stats')
+def api_stats():
+    """checks the API stats of all classes"""
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    for key in classes:
+        classes[key] = storage.count(classes[key])
+    return jsonify(classes)
