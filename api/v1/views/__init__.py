f48a4043d20aaab9d1c6aecf92357a5440995d4e
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-02-16 13:50:58 +0300

    upd

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-
+""" creates app_views which is an instance of  API Blueprint Views Module
+"""
 from flask import Blueprint
-from api/v1/views import 
 
-app_views = ("app_views", __name__, "api/v1")
+app_views = Blueprint('app_views', '__name__', url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
