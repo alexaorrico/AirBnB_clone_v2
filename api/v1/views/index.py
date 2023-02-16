90c88f2683ea0be4276a212775d27958cdaeb71b
MpiloNM95 <mpilonmafu95@gmail.com>
2023-02-16 03:16:33 -0800

    status your API added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,15 +2,22 @@
 """returns the status of the API"""
 import models
 from models import storage
+from models.amenity import Amenity
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
 from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
+
 
 @app_views.route('/status')
 def api_status():
     """returns the status of the API"""
     return jsonify(status='OK')
+
 
 @app_views.route('/stats')
 def api_stats():
