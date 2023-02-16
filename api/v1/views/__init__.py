71fee8243663d5eb9916f57d7da7fb23850df4b1
Nnachi Kingsley <chimakingsley216@gmail.com>
2023-02-16 13:44:29 -0800

    corrections

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+Contains the blueprint for the API
+"""
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
