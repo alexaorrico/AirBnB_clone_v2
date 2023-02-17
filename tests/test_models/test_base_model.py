16390d85c5a13791e702ef713857f606c3db0b8f
Mikek1337 <mikekegnlyew@gmail.com>
2023-02-17 08:58:05 +0300

    fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,10 +4,8 @@ from datetime import datetime
 import inspect
 import models
 import pep8 as pycodestyle
-import time
 import unittest
 from unittest import mock
-from os import getenv
 BaseModel = models.base_model.BaseModel
 module_doc = models.base_model.__doc__
 
@@ -79,24 +77,6 @@ class TestBaseModel(unittest.TestCase):
                 self.assertIs(type(inst.__dict__[attr]), typ)
         self.assertEqual(inst.name, "Holberton")
         self.assertEqual(inst.number, 89)
-
-    def test_datetime_attributes(self):
-        """Test that two BaseModel instances have different datetime objects
-        and that upon creation have identical updated_at and created_at
-        value."""
-        tic = datetime.now()
-        inst1 = BaseModel()
-        toc = datetime.now()
-        self.assertFalse(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
-        tic = datetime.now()
-        inst2 = BaseModel()
-        toc = datetime.now()
-        self.assertFalse(tic <= inst2.created_at <= toc)
-        self.assertEqual(inst1.created_at, inst1.updated_at)
-        self.assertEqual(inst2.created_at, inst2.updated_at)
-        self.assertNotEqual(inst1.created_at, inst2.created_at)
-        self.assertNotEqual(inst1.updated_at, inst2.updated_at)
 
     def test_uuid(self):
         """Test that id is a valid uuid"""
