566fa60f4ee2f9efa6169800f3dd36424255cb97
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-02-17 03:57:36 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,11 +14,9 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def count():
     """returns the number of each objects by type"""
-    classes = [Amenity, City, Place, Review, State, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-
-    return jsonify(num_objs)
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
