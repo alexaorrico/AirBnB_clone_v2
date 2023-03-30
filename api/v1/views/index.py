7045014ea3b42194809375396b26c6a73598dcfd
Philip Taylor <Philip.dan.taylor@gmail.com>
2023-03-30 23:32:12 +0000

    pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,13 +5,15 @@ from flask import Response
 from api.v1.views import app_views
 from models import storage, class_names
 
-classConversion = { "Amenity": "amenities", "City": "cities", "Place": "places",
-                    "Review": "reviews", "State": "states", "User": "users" }
+classConversion = {"Amenity": "amenities", "City": "cities", "Place": "places",
+                   "Review": "reviews", "State": "states", "User": "users"}
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return status of API"""
     return Response(dumps({"status": "OK"}), content_type='application/json')
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
