f0f7f89d626cf83e9ffd7e780057f2443592eb66
MicoBledsoe <micomods@gmail.com>
2023-03-30 18:37:48 -0500

    custom variable names

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -82,12 +82,12 @@ class TestBaseModel(unittest.TestCase):
         """Test that two BaseModel instances have different datetime objects
         and that upon creation have identical updated_at and created_at
         value."""
-        pre_create_time = datetime.now()
+        pre_time = datetime.now()
         inst1 = BaseModel()
-        post_create_time = datetime.now()
+        post_time = datetime.now()
         time.sleep(1e-4)
         inst2 = BaseModel()
-        self.assertTrue(pre_create_time <= post_create_time)
+        self.assertTrue(pre_time <= post_time)
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.created_at, inst2.created_at)
