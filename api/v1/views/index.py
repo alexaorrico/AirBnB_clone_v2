dcc544cd3052acc483fad17b9e5f07d88bb87204
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-30 18:15:18 +0200

    Fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ from flask import jsonify, Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns the status of the API"""
