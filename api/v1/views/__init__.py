15d2379560840b4d56347c6c341e79b6f309c667
Philip Taylor <Philip.dan.taylor@gmail.com>
2023-03-30 21:39:22 +0000

    Corrections for Task 4
    Remember to run and test before submiting a PR.
    Correct __init__ with import order
    Correct content type for index

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 """Define a blueprint for the API v1"""
 from flask import Blueprint
-# Pep8 doesn't like the next line, project said it's okay
-from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Pep8 doesn't like the next line, project said it's okay
+from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Route to index page"""
 from json import dumps
+from flask import Response
 from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return status of API"""
-    return dumps({"status": "OK"})
+    return Response(dumps({"status": "OK"}), content_type='json/application')
