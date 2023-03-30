fb62e116c301f00280092a27a4862adc0dbaf4ad
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-03-30 16:48:08 +0200

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" This file is used to define our Flask application"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    """Closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
