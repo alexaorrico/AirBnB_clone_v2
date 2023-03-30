5a8f847ed5c5496abdf9e9497a1b22e15fe575a5
MicoBledsoe <micomods@gmail.com>
2023-03-30 16:45:43 -0500

    retreving and counting number of objects

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,6 +71,16 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
+    def get(self, cls, id):
+        if cls and id:
+            return self.__session.query(cls).get(id)
+        return None
+
+    def count(self, cls=None):
+        if cls:
+            return self.__session.query(cls).count()
+        return sum(self.__session.query(c).count() for c in classes.values())
+
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
