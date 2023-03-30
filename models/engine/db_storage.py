ca3bd5dd97cad7ccad61609560111f4c9d3fdc9c
Ndi-Shema <f.shema@alustudent.com>
2023-03-30 12:52:58 +0200

    db_storage and FileStorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,70 +1,109 @@
 #!/usr/bin/python3
-'''
-Module for the DbStorage
-'''
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.base_model import Base
+
+"""
+Contains the DBStorage class which interacts with a MySQL database.
+"""
+
+import models
 from models.amenity import Amenity
+from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# Dictionary of all available classes and their corresponding models
+CLASS_MODELS = {"Amenity": Amenity, "City": City, "Place": Place,
+                "Review": Review, "State": State, "User": User}
+
 
 class DBStorage:
-    CNC = {
-        'Amenity': Amenity,
-        'City': City,
-        'Place': Place,
-        'Review': Review,
-        'State': State,
-        'User': User
-    }
-
+    """
+    Represents a database storage engine
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        user = os.environ.get('HBNB_MYSQL_USER')
-        pwd = os.environ.get('HBNB_MYSQL_PWD')
-        host = os.environ.get('HBNB_MYSQL_HOST')
-        db = os.environ.get('HBNB_MYSQL_DB')
+        """
+        Instantiate a DBStorage object with a connection to a MySQL database
+        """
+        MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                      format(user, pwd, host, db),
-                                      pool_pre_ping=True)
-        if os.environ.get('HBNB_ENV') == 'test':
-            Base.metadata.drop_all(bind=self.__engine)
+                                      format(MYSQL_USER, MYSQL_PWD,
+                                             MYSQL_HOST, MYSQL_DB))
+        if ENV == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
+        """
+        Retrieve a dictionary of all objects in the current session
+        """
         obj_dict = {}
-        if cls:
-            for obj in self.__session.query(DBStorage.CNC[cls]):
-                obj_ref = '{}.{}'.format(type(obj).__name__, obj.id)
-                obj_dict[obj_ref] = obj
-            return obj_dict
-
-        for c in DBStorage.CNC.values():
-            for obj in self.__session.query(c):
-                obj_ref = '{}.{}'.format(type(obj).__name__, obj.id)
-                obj_dict[obj_ref] = obj
+        for cls_name, cls_model in CLASS_MODELS.items():
+            if cls is None or cls is cls_model or cls is cls_name:
+                objs = self.__session.query(cls_model).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    obj_dict[key] = obj
         return obj_dict
 
     def new(self, obj):
+        """
+        Add an object to the current session
+        """
         self.__session.add(obj)
 
     def save(self):
+        """
+        Commit all changes made to the current session
+        """
         self.__session.commit()
 
+    def get(self, cls, id):
+        """
+        Retrieve a single object based on class name and object id
+        """
+        obj = self.__session.query(eval(cls)).filter_by(id=id).first()
+        return obj
+
+    def count(self, cls=None):
+        """
+        Count the number of objects in the current session
+        """
+        count = 0
+        obj_dict = self.all(cls)
+        count = len(obj_dict)
+        return count
+
     def delete(self, obj=None):
-        if obj:
+        """
+        Remove an object from the current session
+        """
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        Base.metadata.create_all(bind=self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = Session()
+        """
+        Reload data from the database
+        """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session
 
     def close(self):
-        self.__session.close()
+        """
+        Close the current session
+        """
+        self.__session.remove()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Handles the reading and writing of JSON data for storing all class instances.
+This module defines the FileStorage class.
 """
 
 import json
-from datetime import datetime
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -13,74 +12,105 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+# A dictionary that maps class names to their respective classes
+classes = {
+    "Amenity": Amenity,
+    "BaseModel": BaseModel,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
+
 
 class FileStorage:
+    """
+    This class serializes instances to a JSON file and deserializes
+    back to instances.
+    """
 
-    CNC = {
-        'BaseModel': base_model.BaseModel,
-        'Amenity': amenity.Amenity,
-        'City': city.City,
-        'Place': place.Place,
-        'Review': review.Review,
-        'State': state.State,
-        'User': user.User
-    }
-
-    __file_path = './dev/file.json'
-    __objects = {}
+    __file_path = "file.json"  # The path to the JSON file
+    __objects = {}  # A dictionary to store all objects by class name and ID
 
     def all(self, cls=None):
+        """
+        This method returns the dictionary of objects.
 
+        If the optional argument cls is provided, it returns a dictionary
+        containing only the objects of the specified class.
+        """
         if cls is not None:
-            new_objs = {}
-            for clsid, obj in FileStorage.__objects.items():
-                if type(obj).__name__ == cls:
-                    new_objs[clsid] = obj
-            return new_objs
-        else:
-            return FileStorage.__objects
+            filtered_dict = {}
+            for key, value in self.__objects.items():
+                if value.__class__ == cls or value.__class__.__name__ == cls:
+                    filtered_dict[key] = value
+            return filtered_dict
+        return self.__objects
 
     def new(self, obj):
-
-        bm_id = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[bm_id] = obj
+        """
+        This method sets the object in the __objects dictionary with
+        the key <obj class name>.id
+        """
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-
-        fname = FileStorage.__file_path
-        storage_d = {}
-        for bm_id, bm_obj in FileStorage.__objects.items():
-            storage_d[bm_id] = bm_obj.to_json(saving_file_storage=True)
-        with open(fname, mode='w', encoding='utf-8') as f_io:
-            json.dump(storage_d, f_io)
+        """
+        This method serializes __objects to the JSON file (path: __file_path)
+        """
+        json_dict = {}
+        for key, obj in self.__objects.items():
+            json_dict[key] = obj.to_dict()
+        with open(self.__file_path, "w") as file:
+            json.dump(json_dict, file)
 
     def reload(self):
-
-        fname = FileStorage.__file_path
-        FileStorage.__objects = {}
+        """
+        This method deserializes the JSON file to __objects.
+        """
         try:
-            with open(fname, mode='r', encoding='utf-8') as f_io:
-                new_objs = json.load(f_io)
-        except:
-            return
-        for o_id, d in new_objs.items():
-            k_cls = d['__class__']
-            FileStorage.__objects[o_id] = FileStorage.CNC[k_cls](**d)
+            with open(self.__file_path, "r") as file:
+                json_dict = json.load(file)
+            for key, obj_dict in json_dict.items():
+                class_name = obj_dict["__class__"]
+                self.__objects[key] = classes[class_name](**obj_dict)
+        except Exception:
+            pass
 
     def delete(self, obj=None):
+        """
+        This method deletes an object from the __objects dictionary
+        if it exists.
+        """
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
 
-        if obj:
-            obj_ref = "{}.{}".format(type(obj).__name__, obj.id)
-            all_class_objs = self.all(obj.__class__.__name__)
-            if all_class_objs.get(obj_ref):
-                del FileStorage.__objects[obj_ref]
-            self.save()
+    def close(self):
+        """
+        This method calls the reload() method to deserialize
+        the JSON file to objects.
+        """
+        self.reload()
 
-    def delete_all(self):
+    def get(self, cls, id):
+        """
+        This method retrieves an object based on its class name
+        and ID.
+        """
+        for key, value in self.all(cls).items():
+            if key == "{}.{}".format(cls.__name__, id):
+                return value
 
-        try:
-            with open(FileStorage.__file_path, mode='w') as f_io:
-                pass
-        except:
-            pass
-        del FileStorage.__objects
+    def count(self, cls=None):
+        """
+        This method returns the number of objects in the storage.
+
+        If the optional argument cls is provided, it returns the
+        number of objects of the specified class.
+        """
+        if cls is not None
