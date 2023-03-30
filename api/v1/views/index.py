08ed38edbcb03b0cf54c1f5f8aaffc4f0df49a45
Philip Taylor <Philip.dan.taylor@gmail.com>
2023-03-30 21:50:55 +0000

    Correct content_type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,4 +8,4 @@ from api.v1.views import app_views
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return status of API"""
-    return Response(dumps({"status": "OK"}), content_type='json/application')
+    return Response(dumps({"status": "OK"}), content_type='application/json')
