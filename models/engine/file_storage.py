8f6183976b74fd59e2baf432476280ab834c8416
CStamper <cstampok@gmail.com>
2023-03-30 22:39:24 -0500

    removing print

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,8 +60,7 @@ class FileStorage:
             for key in jo:
                 r = self.class_richard
                 self.__objects[key] = r[jo[key]["__class__"]](**jo[key])
-        except Exception as e:
-            print(e)
+        except Exception:
             pass
 
     def delete(self, obj=None):
