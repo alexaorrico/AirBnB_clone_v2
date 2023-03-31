5b6fd2a2e261d69e9885f83289a244e0ce6192f5
CStamper <cstampok@gmail.com>
2023-03-30 22:59:50 -0500

    model router updates

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""
-    Flask route that returns json respone
-"""
+"""Flask route module for amenities"""
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models import storage, class_richard
 
 
 @app_views.route('/amenities/', methods=['GET'])
-def amenities_no_id(amenity_id=None):
+def amenities_no_id_get(amenity_id=None):
     """amenities route - no id GET scenario"""
     all_amenities = storage.all('Amenity')
     all_amenities = [obj.to_dict() for obj in all_amenities.values()]
     return jsonify(all_amenities)
 
 @app_views.route('/amenities/', methods=['POST'])
-def amenities_no_id(amenity_id=None):
+def amenities_no_id_post(amenity_id=None):
     """amenities route - no id POST scenario"""
     req_json = request.get_json()
     if req_json is None:
@@ -28,9 +26,9 @@ def amenities_no_id(amenity_id=None):
     return jsonify(new_object.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'])
-def amenities_with_id(amenity_id=None):
-    """amenities route - id given scenario"""
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def amenities_with_id_get(amenity_id=None):
+    """amenities route - id given GET scenario"""
     amenity_obj = storage.get('Amenity', amenity_id)
     if amenity_obj is None:
         abort(404, 'Not found')
@@ -38,11 +36,19 @@ def amenities_with_id(amenity_id=None):
     if request.method == 'GET':
         return jsonify(amenity_obj.to_dict())
 
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def amenities_with_id_del(amenity_id=None):
+    """amenities route - id given DELETE scenario"""
+    amenity_obj = storage.get('Amenity', amenity_id)
     if request.method == 'DELETE':
         amenity_obj.delete()
         del amenity_obj
         return jsonify({}), 200
 
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def amenities_with_id_put(amenity_id=None):
+    """amenities route - id given PUT scenario"""
+    amenity_obj = storage.get('Amenity', amenity_id)
     if request.method == 'PUT':
         req_json = request.get_json()
         if req_json is None:
