4f6565112e65dbddab94535c17b72c37fc6201f2
CStamper <cstampok@gmail.com>
2023-03-30 23:13:19 -0500

    amenity update to include validator

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask route module for amenities"""
-from api.v1.views import app_views
+from api.v1.views import app_views, validate_model
 from flask import abort, jsonify, request
 from models import storage, class_richard
 
@@ -29,29 +29,23 @@ def amenities_no_id_post(amenity_id=None):
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 def amenities_with_id_get(amenity_id=None):
     """amenities route - id given GET scenario"""
-    amenity_obj = storage.get('Amenity', amenity_id)
-    if amenity_obj is None:
-        abort(404, 'Not found')
-
-    if request.method == 'GET':
-        return jsonify(amenity_obj.to_dict())
+    amenity_obj = validate_model('Amenity', amenity_id)
+    return jsonify(amenity_obj.to_dict())
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
 def amenities_with_id_del(amenity_id=None):
     """amenities route - id given DELETE scenario"""
-    amenity_obj = storage.get('Amenity', amenity_id)
-    if request.method == 'DELETE':
-        amenity_obj.delete()
-        del amenity_obj
-        return jsonify({}), 200
+    amenity_obj = validate_model('Amenity', amenity_id)
+    amenity_obj.delete()
+    del amenity_obj
+    return jsonify({}), 200
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
 def amenities_with_id_put(amenity_id=None):
     """amenities route - id given PUT scenario"""
-    amenity_obj = storage.get('Amenity', amenity_id)
-    if request.method == 'PUT':
-        req_json = request.get_json()
-        if req_json is None:
-            abort(400, 'Not a JSON')
-        amenity_obj.save(req_json)
-        return jsonify(amenity_obj.to_dict()), 200
+    amenity_obj = validate_model('Amenity', amenity_id)
+    req_json = request.get_json()
+    if req_json is None:
+        abort(400, 'Not a JSON')
+    amenity_obj.save(req_json)
+    return jsonify(amenity_obj.to_dict()), 200
