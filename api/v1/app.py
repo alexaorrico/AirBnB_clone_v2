81a97898c388862f7350db00251ac8134f0e0c4c
Maitreya-today <lessismoreclint@gmail.com>
2023-03-31 22:51:16 -0500

    do it now

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,21 @@
-#!/usr/bin/python3
-""" instance of Flask """
-from flask import Flask, make_response
-from models import storage
+from flask import Flask
 from api.v1.views import app_views
-from os import getenv as env
+from models import storage
 
-
-# create an instance of Flask
+# create Flask instance
 app = Flask(__name__)
+
 # register blueprint
 app.register_blueprint(app_views)
 
-
-@app.errorhandler(404)
-def handle_404(exception):
-    """handles 404 scenario (page not found)"""
-    code = exception.__str__().split()[0]
-    message = {"error": "Not found"}
-    return make_response(message, code)
-
-
+# define teardown method
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ close storage """
+def teardown_storage(exception):
     storage.close()
 
-
-def start_flask():
-    """ start flask """
-    app.run(host=env('HBNB_API_HOST'),
-            port=env('HBNB_API_PORT'),
-            threaded=True)
-
-
+# start Flask server
 if __name__ == "__main__":
-    start_flask()
+    import os
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,14 @@ initialize the models package
 
 from os import getenv
 
-
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
+    class_richard = DBStorage.class_richard
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
+    class_richard = FileStorage.class_richard
     storage = FileStorage()
 storage.reload()
