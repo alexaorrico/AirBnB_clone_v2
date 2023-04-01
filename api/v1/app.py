daa033739ddf4e3415046e12b0224df62033922d
Maitreya-today <lessismoreclint@gmail.com>
2023-03-31 22:55:21 -0500

    update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,37 @@
-from flask import Flask
-from api.v1.views import app_views
+#!/usr/bin/python3
+""" instance of Flask """
+from flask import Flask, make_response
 from models import storage
+from api.v1.views import app_views
+from os import getenv as env
 
-# create Flask instance
+
+# create an instance of Flask
 app = Flask(__name__)
-
 # register blueprint
 app.register_blueprint(app_views)
 
-# define teardown method
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """handles 404 scenario (page not found)"""
+    code = exception.__str__().split()[0]
+    message = {"error": "Not found"}
+    return make_response(message, code)
+
+
 @app.teardown_appcontext
-def teardown_storage(exception):
+def teardown_db(exception):
+    """ close storage """
     storage.close()
 
-# start Flask server
+
+def start_flask():
+    """ start flask """
+    app.run(host=env('HBNB_API_HOST'),
+            port=env('HBNB_API_PORT'),
+            threaded=True)
+
+
 if __name__ == "__main__":
-    import os
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    start_flask()
