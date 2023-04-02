a79a8a1d56149d6b68cd0d2c7a75c98dfc706dfa
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-02 16:29:09 +0200

    Commit message: Update api/v1/views/states.py to accept state_id as an argument, improve DELETE request handling, and add validation for POST request

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,7 +15,7 @@ def states():
     return jsonify([state.to_dict() for state in states])
 
 
-@app_views.route("/states/<string:state_id>", strict_slashes=False)
+@app_views.route("/states/<state_id>", strict_slashes=False)
 def get_state_by_id(state_id):
     """Retrieves a State object"""
     state = storage.get(State, state_id).to_dict()
@@ -24,10 +24,12 @@ def get_state_by_id(state_id):
     return jsonify(state), 200
 
 
-@app_views.route("/states/<state_id>", methods=['DELETE'])
+@app_views.route("/states/<state_id>",
+                 methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state_by_id(state_id):
     """Deletes a State object"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     storage.delete(state)
@@ -37,12 +39,12 @@ def delete_state_by_id(state_id):
 @app_views.route("/states", methods=['POST'], strict_slashes=False)
 def create_state():
     """Creates a State"""
+    if not request.get_json:
+        abort(400, description="Not a JSON")
     data = request.get_json()
-    if not data:
-        abort(400, "Not a JSON")
-    if 'name' not in request.get_json:
-        abort(400, "Missing name")
-    state = State(**request.get_json())
+    if 'name' not in request.data:
+        abort(400, description="Missing name")
+    state = State(**data)
     state.save()
     return jsonify(state.to_dict()), 201
 
@@ -54,7 +56,7 @@ def update_state_by_id(state_id):
     if state is None:
         abort(404)
     if not request.is_json:
-        abort(400, "Not a JSON")
+        abort(400, description="Not a JSON")
     data = request.get_json()
     for key, value in data.items():
         if key not in ["id", "created_at", "updated_at"]:
