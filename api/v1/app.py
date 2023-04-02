1ea5c734687819ff1c9be82126e60bf7847034f2
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-02 13:00:38 +0200

    Update app.py to use os.getenv() for host & port configuration

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 import os
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
+
+
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown(exception):
     """ handles teardown """
     storage.close()
 
@@ -24,14 +27,6 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    try:
-        host = os.environ.get('HBNB_API_HOST')
-    except:
-        host = '0.0.0.0'
-
-    try:
-        port = os.environ.get('HBNB_API_PORT')
-    except:
-        port = '5000'
-
-    app.run(host=host, port=port)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True, debug=True)
