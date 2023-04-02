fbcc042a5614f597bd733730f78e2f3fbaae1d23
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-02 12:36:28 +0200

    fix: Correct body answer

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.user import User
 
 
 @app_views.route('/status')
@@ -17,11 +23,11 @@ def status():
 def stats():
     """ returns number of objects by type """
     result = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(result)
