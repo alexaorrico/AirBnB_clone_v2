3b6c0f9deb369e793856ff85ea4cab64f218ba55
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2023-04-02 01:48:00 +0100

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,165 +1,94 @@
 #!/usr/bin/python3
 """
-Contains the TestDBStorageDocs and TestDBStorage classes
+Contains the class DBStorage
 """
 
-from datetime import datetime
-import inspect
 import models
-from models.engine import db_storage
 from models.amenity import Amenity
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-import json
-import os
-import pep8
-import unittest
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-DBStorage = db_storage.DBStorage
-classes = {"Amenity": Amenity, "City": City, "Place": Place,
-           "Review": Review, "State": State, "User": User}
-
-
-class TestDBStorageDocs(unittest.TestCase):
-    """Tests to check the documentation and style of DBStorage class"""
-
-    @classmethod
-    def setUpClass(cls):
-        """Set up for the doc tests"""
-        cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
-
-    def test_pep8_conformance_db_storage(self):
-        """Test that models/engine/db_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_db_storage(self):
-        """Test tests/test_models/test_db_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_engine/\
-test_db_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_db_storage_module_docstring(self):
-        """Test for the db_storage.py module docstring"""
-        self.assertIsNot(db_storage.__doc__, None,
-                         "db_storage.py needs a docstring")
-        self.assertTrue(len(db_storage.__doc__) >= 1,
-                        "db_storage.py needs a docstring")
-
-    def test_db_storage_class_docstring(self):
-        """Test for the DBStorage class docstring"""
-        self.assertIsNot(DBStorage.__doc__, None,
-                         "DBStorage class needs a docstring")
-        self.assertTrue(len(DBStorage.__doc__) >= 1,
-                        "DBStorage class needs a docstring")
-
-    def test_dbs_func_docstrings(self):
-        """Test for the presence of docstrings in DBStorage methods"""
-        for func in self.dbs_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
-class TestFileStorage(unittest.TestCase):
-    """Test the FileStorage class"""
+class DBStorage:
+    """interaacts with the MySQL database"""
+    __engine = None
+    __session = None
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_all_returns_dict(self):
-        """Test that all returns a dictionaty"""
-        self.assertIs(type(models.storage.all()), dict)
+    def __init__(self):
+        """Instantiate a DBStorage object"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB))
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_all_no_class(self):
-        """Test that all returns all rows when no class is passed"""
+    def all(self, cls=None):
+        """query on the current database session"""
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_new(self):
-        """test that new adds an object to the database"""
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_save(self):
-        """Test that save properly saves objects to file.json"""
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
 
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "skip if  fs")
-class TestDBStorageGet(unittest.TestCase):
-    """Tests get method of the DBStorage class"""
+    def reload(self):
+        """reloads data from the database"""
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
 
-    def setUpClass(self):
-        """Set up for the tests"""
+    def get(self, cls, id):
+        """ get method: returns the object based on the class and its ID or
+            None if not found """
+        if cls and id:
+            obj_dict = models.storage.all(cls)
+            for key, value in obj_dict.items():
+                obj_str = cls.__name__ + '.' + id
+                if key == obj_str:
+                    return (value)
+            return (None)
 
-        self.storage = DBStorage()
-        self.storage.reload()
-        self.new_state = State(name="California")
-        self.new_state.save()
-        self.new_city = City(name="San Francisco", state_id=self.new_state.id)
-        self.new_city.save()
+    def count(self, cls=None):
+        """ count method: Returns the number of objects in storage matching
+            the given class. If no class is passed, returns the count of all
+            objects in storage. """
+        obj_num = models.storage.all(cls)
+        return (len(obj_num))
 
-    def tearDownClass(self):
-        """Tear down after the tests"""
-
-        self.storage.delete(self.new_city)
-        self.storage.delete(self.new_state)
-        self.storage.save()
-        self.storage.close()
-
-    def test_get_existing_object(self):
-        """Test get() with an object that exists"""
-        obj = self.storage.get(City, self.new_city.id)
-        self.assertEqual(obj.id, self.new_city.id)
-
-    def test_get_nonexistent_object(self):
-        """Test get() with an object that does not exist"""
-        obj = self.storage.get(State, "nonexistent")
-        self.assertIsNone(obj)
-
-
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db', "skip if  fs")
-class TestDBStorageCount(unittest.TestCase):
-    """Tests the count() method of the DBStorage class"""
-
-    def setUpClass(self):
-        """Set up for the tests"""
-
-        self.storage = DBStorage()
-        self.storage.reload()
-        self.new_state1 = State(name="California")
-        self.new_state2 = State(name="New York")
-        self.new_state3 = State(name="Texas")
-        self.new_state1.save()
-        self.new_state2.save()
-        self.new_state3.save()
-
-    def tearDownClass(self):
-        """Tear down after the tests"""
-
-        self.storage.delete(self.new_state1)
-        self.storage.delete(self.new_state2)
-        self.storage.delete(self.new_state3)
-        self.storage.save()
-        self.storage.close()
-
-    def test_count_all_objects(self):
-        """Test count() with no arguments"""
-        count = self.storage.count()
-        self.assertEqual(count, 3)
-
-    def test_count_some_objects(self):
-        """Test count() with a class argument"""
-        count = self.storage.count(State)
-        self.assertEqual(count, 3)
-
-    def test_count_nonexistent_class(self):
-        """Test count() with a nonexistent class argument"""
-        count = self.storage.count(Amenity)
-        self.assertEqual(count, 0)
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
