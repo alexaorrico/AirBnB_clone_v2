d28ebac7893df8a71c106f2377f74dabcecba4c6
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-03 22:30:42 +0200

    State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines the views of handling the RESTful API on state objects"""
+
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage, State
