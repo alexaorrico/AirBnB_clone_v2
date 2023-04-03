d1acfcf20db76d4e32b2919c269dbf427d66a545
izzeddin62 <i.ishimwe@alustudent.com>
2023-04-03 16:09:38 +0200

    add comments

--- test_get_count.py
+++ test_get_count.py
@@ -4,9 +4,11 @@
 from models import storage
 from models.state import State
 
+# get the count for all the objects in the db
 print("All objects: {}".format(storage.count()))
+# get the count for all the  states in the db
 print("State objects: {}".format(storage.count(State)))
 
 first_state_id = list(storage.all(State).values())[0].id
-print(first_state_id, '======')
+# print the first state object using get method
 print("First state: {}".format(storage.get(State, first_state_id)))
