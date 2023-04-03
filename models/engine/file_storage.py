24fb1446c63754c7ab5b1a3353ef81c2cfdd17d8
Julia Bullard <5534@holbertonstudents.com>
2023-04-03 15:04:02 -0500

    Upated db_storage.py & file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,8 +12,9 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"Amenity": Amenity, "BaseModel": BaseModel,
+           "City": City, "Place": Place, "Review": Review,
+           "State": State, "User": User}
 
 
 class FileStorage:
@@ -71,3 +72,17 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class
+        and its ID, or None if not found"""
+        key = "{}.{}".format(cls.__name__, id)
+        return self.__objects.get(key, None)
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage."""
+        if cls is not None:
+            return len([obj for obj in self.__objects.values()
+                        if isinstance(obj, cls)])
+        return len(self.__objects)
