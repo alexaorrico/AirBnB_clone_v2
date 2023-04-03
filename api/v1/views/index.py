7cefa6b31114561714306d0db328dd2a1bafbc3f
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-04 00:55:46 +0200

    Revert "fix stats endpoint"
    This reverts commit c0edce02d409f5d98c189762c8a944a9d86e638f.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,7 +13,7 @@ def status():
     return jsonify(status)
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     """ returns number of objects by type """
     result = {}
