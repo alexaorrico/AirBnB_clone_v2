8aad9af77ebdb2a305f7a490f1e7cdcddadc8cc0
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-03 22:52:46 +0200

    States

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,6 +20,15 @@ def get_states():
 def get_state(state_id):
     """
     Retrieves a State object.
+
+    Args:
+        state_id (str): The ID of the State object to retrieve.
+
+    Returns:
+        A JSON response containing a dictionary representing the State object with the given ID.
+
+    Raises:
+        404: If no State object with the given ID is found.
     """
     state = storage.get(State, state_id)
     if not state:
@@ -31,6 +40,15 @@ def get_state(state_id):
 def delete_state(state_id):
     """
     Deletes a State object.
+
+    Args:
+        state_id (str): The ID of the State object to delete.
+
+    Returns:
+        An empty JSON response with a status code of 200.
+
+    Raises:
+        404: If no State object with the given ID is found.
     """
     state = storage.get(State, state_id)
     if not state:
@@ -44,6 +62,12 @@ def delete_state(state_id):
 def create_state():
     """
     Creates a State object.
+
+    Returns:
+        A JSON response containing a dictionary representing the newly created State object.
+
+    Raises:
+        400: If the HTTP body request is not valid JSON or if the dictionary doesn't contain the 'name' key.
     """
     if not request.json:
         abort(400, 'Not a JSON')
@@ -59,6 +83,16 @@ def create_state():
 def update_state(state_id):
     """
     Updates a State object.
+
+    Args:
+        state_id (str): The ID of the State object to update.
+
+    Returns:
+        A JSON response containing a dictionary representing the updated State object.
+
+    Raises:
+        404: If no State object with the given ID is found.
+        400: If the HTTP body request is not valid JSON.
     """
     state = storage.get(State, state_id)
     if not state:
