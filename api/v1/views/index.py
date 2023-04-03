9241dcb4019a4648b71214ebcbadb72b619a121f
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-03 22:02:20 +0200

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,15 @@ from flask import jsonify, Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'] strict_slashes=False)
 def status():
     """Returns the status of the API"""
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
     return jsonify({'status': 'OK'})
