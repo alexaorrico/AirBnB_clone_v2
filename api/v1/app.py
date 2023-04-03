a5bfd6f09a0a736e82bfccb0efd8b63293b1ad76
izzeddin62 <i.ishimwe@alustudent.com>
2023-04-03 23:36:40 +0200

    add /status route

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Flask app
+"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import environ
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_app(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    host = environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = environ.get("HBNB_API_PORT", 5000)
+    app.run(host=host, port=port, threaded=True)
