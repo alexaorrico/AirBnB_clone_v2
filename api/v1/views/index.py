bd3e3e25551acf8d989d2455a10568624cc51408
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-03 23:31:27 +0200

    refactor: API status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,12 +4,6 @@
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.state import State
-from models.review import Review
-from models.user import User
 
 
 @app_views.route('/status')
@@ -22,12 +16,17 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ returns number of objects by type """
-    result = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-    }
+    result = {}
+    classes = {"Amenity": "amenities",
+               "City": "cities",
+               "Place": "places",
+               "Review": "reviews",
+               "State": "states",
+               "User": "users"
+               }
+
+    for cls in classes:
+        count = storage.count(cls)
+        result[classes.get(cls)] = count
+
     return jsonify(result)
