65eeac51f0f2337db2cea424dc9cf5c3e7414e63
root <michaelwblevins@gmail.com>
2023-04-03 23:13:37 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,18 @@
 """Set up routes for apps"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from flask import jsonify
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    stats = {}
+    for cls in storage.classes:
+        stats[cls] = storage.count(cls)
+        return jsonify(stats)
