94d4e6b85b733f91e7ed7fc55eb9cc812beabd5d
root <michaelwblevins@gmail.com>
2023-04-03 23:09:20 +0000

    TAsk 4 new

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Flask instance startup"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Stops instance"""
+    storage.close()
+
+if __name__ == "__main__":
+    import os
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
