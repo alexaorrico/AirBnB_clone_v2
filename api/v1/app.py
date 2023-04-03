e04d58803e1e14adf7b6155cc3d8c38b308e7859
Nachocheeze18 <5581@holbertonstudents.com>
2023-04-03 07:58:21 -0700

    changes to app file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''task 4'''
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -9,6 +9,13 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
+@app.errorhandler(404)
+def page_not_found(exception):
+    """
+    404 Page Not Found Error Handler
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 @app.teardown_appcontext
 def tearitup():
