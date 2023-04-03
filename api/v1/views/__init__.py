9b761ca3f6577640d135fc827b76fd991e2df2d0
Moussa-Kalam <m.amzat@alustudent.com>
2023-04-04 00:07:23 +0200

    Update __init__.py to remove redundant index import and reload storage

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,12 +6,10 @@ from models import storage
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
-from api.v1.views.index import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-
-storage.reload()
