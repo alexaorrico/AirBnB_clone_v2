c136e0ac5d0d4a1b7f85a04e6589851af5eb6f79
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-03 23:06:04 +0200

    States

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" API State module"
+
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage, State
@@ -8,9 +10,6 @@ from models import storage, State
 def get_states():
     """
     Retrieves the list of all State objects.
-
-    Returns:
-        A JSON response containing a list of dictionaries, each representing a State object.
     """
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
@@ -20,15 +19,6 @@ def get_states():
 def get_state(state_id):
     """
     Retrieves a State object.
-
-    Args:
-        state_id (str): The ID of the State object to retrieve.
-
-    Returns:
-        A JSON response containing a dictionary representing the State object with the given ID.
-
-    Raises:
-        404: If no State object with the given ID is found.
     """
     state = storage.get(State, state_id)
     if not state:
@@ -40,15 +30,6 @@ def get_state(state_id):
 def delete_state(state_id):
     """
     Deletes a State object.
-
-    Args:
-        state_id (str): The ID of the State object to delete.
-
-    Returns:
-        An empty JSON response with a status code of 200.
-
-    Raises:
-        404: If no State object with the given ID is found.
     """
     state = storage.get(State, state_id)
     if not state:
@@ -62,12 +43,6 @@ def delete_state(state_id):
 def create_state():
     """
     Creates a State object.
-
-    Returns:
-        A JSON response containing a dictionary representing the newly created State object.
-
-    Raises:
-        400: If the HTTP body request is not valid JSON or if the dictionary doesn't contain the 'name' key.
     """
     if not request.json:
         abort(400, 'Not a JSON')
@@ -83,16 +58,6 @@ def create_state():
 def update_state(state_id):
     """
     Updates a State object.
-
-    Args:
-        state_id (str): The ID of the State object to update.
-
-    Returns:
-        A JSON response containing a dictionary representing the updated State object.
-
-    Raises:
-        404: If no State object with the given ID is found.
-        400: If the HTTP body request is not valid JSON.
     """
     state = storage.get(State, state_id)
     if not state:
