2293cd655906d48799c10f7fb2ce770e3998c8c1
Nachocheeze18 <5581@holbertonstudents.com>
2023-04-03 08:23:47 -0700

    states cities and init file changes

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,6 +5,8 @@ from models.state import State
 from api.v1.views import app_views
 from api.v1.views.cities import *
 
+"""To retrieve the list of all City objects of a State"""
+
 
 @app.route('/api/v1/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def get_cities_by_state(state_id):
@@ -14,6 +16,8 @@ def get_cities_by_state(state_id):
     cities = [city.to_dict() for city in state.cities]
     return jsonify(cities)
 
+"""To retrieve a City object"""
+
 
 @app.route('/api/v1/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
@@ -21,6 +25,8 @@ def get_city(city_id):
     if not city:
         abort(404)
     return jsonify(city.to_dict())
+
+"""To delete a City object"""
 
 
 @app.route('/api/v1/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
@@ -31,6 +37,8 @@ def delete_city(city_id):
     storage.delete(city)
     storage.save()
     return jsonify({}), 200
+
+"""To create a City object"""
 
 
 @app.route('/api/v1/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
@@ -46,6 +54,8 @@ def create_city(state_id):
     city.state_id = state_id
     city.save()
     return jsonify(city.to_dict()), 201
+
+"""To update a City object"""
 
 
 @app.route('/api/v1/cities/<city_id>', methods=['PUT'], strict_slashes=False)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Module for State objects that handles all default Restful API actions"""
+
 from flask import jsonify, abort, request
 from models.state import State
 from api.v1.views import app_views
