8d4638bdaf5f87c78c321b8e335b60faab4cd877
root <michaelwblevins@gmail.com>
2023-04-03 23:10:44 +0000

    PEP8

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Stops instance"""
