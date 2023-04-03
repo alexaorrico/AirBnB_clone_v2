fe0d6b0f23fa5575bd41fc370fbc522dbf824f3e
MaikarfiJesse <j.maikarfi1@alustudent.com>
2023-04-04 00:28:40 +0200

    Fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,11 @@
 """Create a route on the object app_views that returns a JSON: "status":OK """
 from api.v1.views import app_views
 from flask import jsonify, Blueprint
-from models import storage
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/status', methods=['GET'])
 def status():
-  """Returns the status of the API"""
+    """Returns the status of the API"""
     return jsonify({'status': 'OK'})
