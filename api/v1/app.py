52ee4d166a3e0909010a2c09f0959b2ee78a918c
CaseyCJC <knightlaggia@gmail.com>
2023-04-04 07:51:07 +0000

    trying

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Sets up first endpoint that returns stats of the API
-"""
+"""api module"""
 
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
@@ -12,32 +10,24 @@ from os import getenv
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown(exception):
-    """
-    Calls storage.close
-    """
+    """Calls storage.close"""
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(exception):
-    """
-    404 Page Not Found Error Handler
-    """
+    """404 Page Not Found Error Handler"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = "5000"
-    if getenv("HBNB_API_HOST"):
-        host = getenv("HBNB_API_HOST")
-    if getenv("HBNB_API_PORT"):
-        port = getenv("HBNB_API_PORT")
-
-    app.run(host=host, port=port, threaded=True)
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, debug=True, threaded=True)
