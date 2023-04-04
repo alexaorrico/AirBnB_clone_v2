07480170d86c2b8f18a279a38521cdf6f55bbfb6
teylorchapman <5535@holbertonstudents.com>
2023-04-04 07:22:51 +0000

    give stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,22 @@
 from api.v1.views import app_views
 from flask import Response
 from json import dumps
+from models import storage, classes
+
+cc = {"Amenity": "amenities", "City": "cities", "Place": "places",
+      "Review": "reviews", "State": "states", "User": "users"}
 
 
 @app_views.route('/status',  methods=['GET'], strict_slashes=False)
 def status():
     """returns the status of the API"""
     return Response(dumps({"status": "OK"}), content_type='application/json')
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """returns the stats"""
+    data = {}
+    for cls in classes.keys():
+        data[cc[cls]] = storage.count(cls)
+    return Response(dumps(data), content_type='application/json')
