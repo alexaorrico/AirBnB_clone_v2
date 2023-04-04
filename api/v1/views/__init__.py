0f6e30130dd55e07dee1aff391a297964b6b404a
HH <heather.hayes918@gmail.com>
2023-04-04 12:55:14 -0500

    added a few things

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
 """ Blueprint for API v1 """
 from flask import Blueprint, abort, request
-import index
-import users
-import states
-import cities
-import amenities
-import places
-import places_reviews
+from models import storage
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-__all__ = ['index',
-           'user',
-           'states',
-           'cities',
-           'amenities',
-           'places',
-           'places_reviews'
-           ]
+
+
+def valid_model(model, model_id):
+    """ validates if model exists """
+    obj = storage.get(model, model_id)
+    if obj is None:
+        abort(404)
+    return obj
+
+
+def get_json(re_fields=None):
+    """ Get json from request """
+    if re_fields is None:
+        re_fields = []
+    result = request.get_json()
+    if result is None:
+        abort(400, 'Not a JSON')
+    for field in re_fields:
+        if result.get(field) is None:
+            abort(400, 'Missing {}'.format(field))
+    return result
+
+
+# project wants this style
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Index """
+""" index for api """
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
