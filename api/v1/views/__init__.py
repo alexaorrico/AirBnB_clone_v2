c71c3802f541a6e5f73a21cdf0a504320f295495
root <michaelwblevins@gmail.com>
2023-04-04 00:54:45 +0000

    __name__ from name

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,7 @@ Module for API v1 views
 from flask import Blueprint
 
 
-app_views = Blueprint('app_views', name, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
