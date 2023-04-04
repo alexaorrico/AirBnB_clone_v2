bb671815f93bf759365e369b88c684526669bc43
Julia Bullard <5534@holbertonstudents.com>
2023-04-04 16:22:34 -0500

    Deleted test_get_count.py & updated models/init

--- models/__init__.py
+++ models/__init__.py
@@ -4,16 +4,16 @@ initialize the models package
 """
 
 from os import getenv
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
 
 
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
-    from models.engine.db_storage import DBStorage
     classes = DBStorage.classes
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     classes = FileStorage.classes
     storage = FileStorage()
 storage.reload()
