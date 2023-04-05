8289fe320befc1dfe9aa64dfe5fe0ade60ce1021
Manuel Zambrano <mnl.azs@gmail.com>
2023-04-05 14:30:16 -0500

    Test commit

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,7 +2,6 @@
 """
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
-testing.
 """
 
 from fabric.api import env, local, put, run
