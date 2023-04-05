81a03d8652f7413708750022558dacfafd245fe9
kkeas <keaskatrina@gmail.com>
2023-04-05 09:01:32 -0700

    pls work

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -27,3 +27,11 @@ def get_json(required_fields=[]):
             abort(400, 'Missing {}'.format(field))
     return result
 
+# Pep8 doesn't like the next line, project said it's okay
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,22 +3,22 @@
 from json import dumps
 from flask import Response
 from api.v1.views import app_views
-from models import storage
+from models import storage, class_dictionary
 
-cc = {"Amenity": "amenities", "City": "cities", "Place": "places",
-      "Review": "reviews", "State": "states", "User": "users"}
+classConversion = {"Amenity": "amenities", "City": "cities", "Place": "places",
+                   "Review": "reviews", "State": "states", "User": "users"}
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Returns the status of API"""
-    return Response(dumps({'status: "OK"}'}), content_type='application/json')
+    """Return status of API"""
+    return Response(dumps({"status": "OK"}), content_type='application/json')
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
-    """Returns number of objects by type"""
+    """Return number of objects by type"""
     data = {}
-    for cls in cc.keys():
-        data[cc[cls]] = storage.count(cls)
-        return Response(dumps(data), content_type='application/json')
+    for cls in class_dictionary.keys():
+        data[classConversion[cls]] = storage.count(cls)
+    return Response(dumps(data), content_type='application/json')
