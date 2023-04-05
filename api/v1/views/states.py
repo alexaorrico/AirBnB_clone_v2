0d672c71d1bbbbb7b2263eeff04a120edf5e3bea
kkeas <keaskatrina@gmail.com>
2023-04-05 09:04:21 -0700

    work

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""flask route module for states"""
-from api.v1.views import app_views, validate_model, get_json, cc
+"""Flask route module for states"""
+from api.v1.views import app_views, validate_model, get_json
 from flask import jsonify
-from models import storage
+from models import storage, class_dictionary
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -17,7 +17,7 @@ def states_no_id_get():
 def states_no_id_post():
     """states route handling - no id given POST scenario"""
     req_json = get_json(['name'])
-    State = cc.get("State")
+    State = class_dictionary.get("State")
     new_object = State(**req_json)
     new_object.save()
     return jsonify(new_object.to_dict()), 201
