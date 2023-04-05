b8f9fe0ee75fa3ea2a20c4f596e714555f743817
kkeas <keaskatrina@gmail.com>
2023-04-04 19:50:12 -0700

    doc

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Blueprint, abort
+from flask import Blueprint, abort, request
 from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
@@ -11,3 +11,13 @@ def validate_model(model, model_id):
     if obj is None:
         abort(404)
     return obj
+
+def get_json(required_fields=[]):
+    """Get the json from the request"""
+    result = request.get_json()
+    if result is None:
+        abort(400, 'Not a JSON')
+    for field in required_fields:
+        if result.get(field) is None:
+            abort(400, 'Missing {}'.format(field))
+    return result
