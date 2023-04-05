d93669ea3fb4b1ab3c16a5d434ab6a929dbdf750
Your Name <you@example.com>
2023-04-05 15:38:25 +0000

    tested

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -16,7 +16,7 @@ def get_reviews_for_place(place_id):
     if place is None:
         abort(404)
     reviews = [review.to_dict() for review in place.reviews]
-    return jsonify(reviews) if reviews else []
+    return jsonify(reviews)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'],
