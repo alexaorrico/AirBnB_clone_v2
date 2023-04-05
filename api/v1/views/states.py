2cbfa0208558579822715d129a6d1ce70ca7a078
CaseyCJC <knightlaggia@gmail.com>
2023-04-05 02:12:59 +0000

    fixes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@
     RESTful API actions
     """
 from api.v1.views import app_views
-from flask import jsonify, abort, reuest, make_response
+from flask import jsonify, abort, request, make_response, Flask
 from models import storage
 from models.state import State
 
