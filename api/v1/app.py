22215f0d2a72b6f53cf0ccd6f84ce551660c4dc1
kkeas <keaskatrina@gmail.com>
2023-04-05 15:46:42 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,16 +7,18 @@ from api.v1.views import app_views
 from os import getenv as env
 
 
+# create an instance of Flask
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": "0/0/0/0"}})
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+# register blueprint
 app.register_blueprint(app_views)
 
 
 @app.errorhandler(404)
 def handle_404(exception):
-    """handles 404 scenario"""
+    """handles 404 scenario (page not found)"""
     code = exception.__str__().split()[0]
-    message = {"error": "Not Found"}
+    message = {"error": "Not found"}
     return make_response(message, code)
 
 
