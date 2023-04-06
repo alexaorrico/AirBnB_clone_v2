664647e58f89d01ca86abf0e6704cff6b4cef196
izzeddin62 <i.ishimwe@alustudent.com>
2023-04-06 16:20:43 +0200

    update cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_app(exception):
