5294bcda82d3dbff4218f28647c590d006e7ad9a
Ndi-Shema <f.shema@alustudent.com>
2023-04-06 16:07:53 +0200

    db_storage and FileStorage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""module"""
+"""Module"""
+from flask import Flask, jsonify
 from api.v1.views import app_views
-from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models.book import Book
+from models.author import Author
+from models.publisher import Publisher
 
 
-@app_views.route('/status')
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exc):
+    """Close the current SQLAlchemy session"""
+    storage.close()
+
+
+@app.route('/status')
 def status():
     """
     returns a JSON: "status": "OK"
@@ -19,14 +26,15 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
+@app.route('/stats')
 def stats():
     """retrieves the number of each objects by type"""
     return jsonify({
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
+        "books": storage.count(Book),
+        "authors": storage.count(Author),
+        "publishers": storage.count(Publisher)
     })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, threaded=True)
