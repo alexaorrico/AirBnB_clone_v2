0906c8646c67d8ce100e26b334784df60f7b453c
Ndi-Shema <f.shema@alustudent.com>
2023-04-06 15:03:42 +0200

    db_storage and FileStorage

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,24 +6,24 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 
+
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_db(error):
-    """Closes the database again at the end of the request."""
+def tear_down(self):
+    """calls storage.close()"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """404 Error handler"""
+def not_found(error):
+    """error handler for page not found"""
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=getenv("HBNB_API_PORT", "5000"),
-            threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"), port=int(
+        getenv("HBNB_API_PORT", "5000")), threaded=True)
