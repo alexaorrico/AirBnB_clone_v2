2c03e609a01818d4a90d3962e097dd24286dbbdb
izzeddin62 <i.ishimwe@alustudent.com>
2023-04-06 16:22:32 +0200

    resolve pep8 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown_app(exception):
     storage.close()
