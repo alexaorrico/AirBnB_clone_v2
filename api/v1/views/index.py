b221bc9e7b3d296f780df8490e5eefc7d14393f5
Ndi-Shema <f.shema@alustudent.com>
2023-04-07 15:38:58 +0200

    app

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,10 @@ from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
 
+try:
+    from api.v1.views import app_views
+except ImportError:
+    print("Error: Unable to import app_views")
 
 @app_views.route('/status', methods=['GET'])
 def status():
