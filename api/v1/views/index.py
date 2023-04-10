4455ee8b17a6e2471b91a089af55076196f24953
Natha0b <laura.barona04@gmail.com>
2023-04-10 18:08:08 -0500

    add information in the file index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ import app_views from api.v1.views
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -12,3 +13,15 @@ def status():
     Returns the status of the application
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Returns the number of objects by type
+    """
+    counts = {}
+    classes = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+    for cls in classes:
+        counts[cls] = storage.count(cls)
+    return jsonify(counts)
