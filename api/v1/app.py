19104130cb2f5c55999d92298538d787517594b9
Natha0b <laura.barona04@gmail.com>
2023-04-10 17:24:07 -0500

    add information in the directory api and in the files

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application.
+"""
+from flask import Flask
+from api.v1.views import app_views
+from models import storage
+import os
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """
+    Closes the connection to the database
+    """
+    storage.close()
+
+if __name__ == "__main__":
+
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", 5000)
+
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    Returns the status of the application
+    """
+    return jsonify({"status": "OK"})
