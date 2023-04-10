8bdae7530894c35e0faa3f4017857dcb51aff445
Natha0b <laura.barona04@gmail.com>
2023-04-10 17:50:31 -0500

    add changes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
-from api.v1.views.index import *
 
-
-"""wildcard import of everything in the package """
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
