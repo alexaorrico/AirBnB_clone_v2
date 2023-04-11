ae4e5d03cac7f5dc487bcceb9d43c7b7e94adaad
soniangn <nguyen.sonia@gmail.com>
2023-04-11 02:14:14 -0700

    creates the route /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,29 @@
 """ index """
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ return the status of the API """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_obj():
+    """ retrieves the number of objects by type """
+    count_obj = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(count_obj)
