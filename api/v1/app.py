1db06477b06205ba07c6bbae772ad8cf2e8c551e
Arrue Marianne <5608@holbertonstudents.com>
2023-04-11 02:09:25 -0700

    add 404 error route

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ starts a Flask web application """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -17,10 +17,16 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """ 404 error pages """
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST')
     port = os.getenv('HBNB_API_PORT')
-    if not host: 
+    if not host:
         host = '0.0.0.0'
     if not port:
         port = '5000'
