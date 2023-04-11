16fa3804e63a5293097136f96f91b9f454688e8f
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-11 15:54:58 +0200

    fixed again bug

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,19 +4,16 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
 
 @app.teardown_appcontext
 def teardown(self):
     ''' Remove the current SQLAlchemy Session '''
     storage.close()
-
+    
 
 if __name__ == '__main__':
     # return env variable if it exists
