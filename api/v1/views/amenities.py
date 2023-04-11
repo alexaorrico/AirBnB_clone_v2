110da71451871ab7625b03fc6ea8265d147c4fce
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-04-11 06:04:17 -0700

    typo

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,14 +7,15 @@ from models.amenity import Amenity
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-def get_amenity():
+def get_amenities():
     """Retrieves the list of all Amenities objects"""
     amenities = storage.all(Amenity).values()
     amenities_list = [amenity.to_dict() for amenity in amenities]
     return jsonify(amenities_list)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route(
+    '/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieves a Amenity object"""
    amenity = storage.get(Amenity, amenity_id)
