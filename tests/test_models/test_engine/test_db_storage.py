4349d1edca1c10f8eb4834c857087f6ba80d7250
Katrolli <ennio_cano@live.com>
2023-04-11 16:35:39 +0200

    adding

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -102,7 +102,7 @@ class TestDBStorage(unittest.TestCase):
         state = State(name='another')
         state.save()
         self.assertEqual(len(self.storage.all()), self.storage.count())
-        self.assertEqual(len(self.storage.all('State')),
+        self.assertEqual(len(self.storage.all(State)),
                          self.storage.count(State))
 
 
