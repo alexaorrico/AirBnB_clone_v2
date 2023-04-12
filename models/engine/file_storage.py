ce5b6d6d190041d65df211c329846b7a3948bf05
yahdielo <yahdielaz@gmail.com>
2023-04-12 14:25:06 -0400

    added count and get fucntions

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,23 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
+    def get(self, cls, id):
+        if cls and id is not None:
+            obj = self.all(cls)
+            for key, val in obj.items():
+                if val.id == id:
+                    return val
+        return None
+
+
+    def count(self, cls=None):
+        """return count of all objects in storage"""
+        if cls is not None:
+            return (len(self.all(cls)))
+        else:
+            return (len(self.all()))
+
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
