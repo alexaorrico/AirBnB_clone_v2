ff56d406c6af1921e0c97569fc0f3e92144bccf7
soniangn <nguyen.sonia@gmail.com>
2023-04-12 00:37:41 -0700

    correct CORS by adding ressource flag

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
