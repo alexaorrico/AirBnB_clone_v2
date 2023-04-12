336ecb7eab59cf8584d04f4fc030c3b7b6eebc03
bapt2 <5711@holbertonstudents.com>
2023-04-12 12:06:28 +0200

    test2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,7 +4,6 @@ Contains the class DBStorage
 """
 
 import models
-from models import storage
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
