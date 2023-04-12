ede2a73bafc89a96881715bae738cef760edd81a
RedginaldGodeau <5657@holbertonstudents.com>
2023-04-12 12:16:33 +0200

    feat(app.py): add error handler for 404 status code feat(index.py): add docstrings to index and count functions, jsonify return values feat(states.py): add routes for GET, POST, DELETE, and PUT requests for states. Add docstrings to each function.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+    Main file of API,
+    he store the Flask app and run the api
+"""
 
-from flask import Flask, Blueprint
+
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_view
 
@@ -8,5 +13,17 @@ app = Flask(__name__)
 app.register_blueprint(app_view)
 
 
+# Run the app with the default port 5000 threaded true
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', threaded=True)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+        Return a JSON error message. 
+        This is used to display an error when the request cannot be found.
+
+        @param error - The error that was encountered. Can be None.
+        @return The JSON error message with the error message in it
+    """
+    return jsonify({"error": "not found"})
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-
+from flask import jsonify
 from api.v1.app import app
 
 @app.route('/status', strict_slashes=False)
 def index():
-    """returns STATUS OK"""
-    return '{"status": "OK"}'
+    """
+     Return status of API.
+     Used to check if user is allowed to access API
+     
+     @return jsonified version of status
+    """
+    return jsonify({"status": "OK"})
 
 
 @app.route('/stats', strict_slashes=False)
 def count():
-    """returns HOW MANY DATA IN STORAGE"""
-    return '{"status": "OK"}'
+    """
+     Count how many records exist in the database.
+     This is useful for determining if a query is valid or not.
+     
+     @return 200 if everything worked 400 if something went wrong with the
+    """
+    return jsonify({"status": "OK"})
