fe021f2248eb5117501f728b2ec9e729a2df501a
Pauline <paulineparmigiani@gmail.com>
2023-04-13 15:53:48 +0200

    add route /stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,24 +9,19 @@ from flask import jsonify
 from models import storage
 
 
-cls = {
-    "amenities": 47,
-    "cities": 36,
-    "places": 154,
-    "reviews": 718,
-    "states": 27,
-    "users": 31
-}
-
-
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'])
+@app_views.route('/stats', methods=['GET'])
 def retrieve_json():
-    new_dict = {}
-    for k, v in cls.items():
-        new_dict[k] = storage.count(v)
-        return jsonify(new_dict)
+    new_dict = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('cities'),
+        "places": storage.count('palces'),
+        "reviews": storage.count('reviews'),
+        "states": storage.count('states'),
+        "users": storage.count('users'),
+    }
+    return jsonify(new_dict)
