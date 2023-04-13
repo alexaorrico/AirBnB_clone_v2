0b0cb382297d507eb2fa6c6b5b02ede3b1e30ad5
FabioH7 <fhysollari4@gmail.com>
2023-04-13 12:17:44 +0200

    adding amenities and users apis

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,5 +3,9 @@ from flask import Blueprint
 app_views = Blueprint('api', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+from api.v1.views.reviews import *
+from api.v1.views.places import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
