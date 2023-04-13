e1b128f5dc7de2482b4070b9787403800ed45bdd
yahdielo <yahdielaz@gmail.com>
2023-04-13 12:22:59 -0400

    cange response ok to capital letters

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,9 @@
-from flask import Flask
-from models import storage
-from api.v1.views import app_views
 from os import getenv
 
+from flask import Flask
+
+from api.v1.views import app_views
+from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,14 @@
-from api.v1.views import app_views
+#!/usr/bin/python3
 import json
+
 from flask import jsonify
+
+from api.v1.views import app_views
+
 
 @app_views.route("/status")
 def check_status():
     """ return status ok as json"""
-    dict_ = { 'status' : "ok"}
+    dict_ = { 'status' : "OK"}
     
     return jsonify(dict_)
