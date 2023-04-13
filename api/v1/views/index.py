d283bbfcd0891aff348f21ff77aa885d30ce51aa
Pauline <paulineparmigiani@gmail.com>
2023-04-13 15:09:30 +0200

    create endpoint that retrieves number of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,8 +6,27 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+cls = {
+    "amenities": 47,
+    "cities": 36,
+    "places": 154,
+    "reviews": 718,
+    "states": 27,
+    "users": 31
+}
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def retrieve_json():
+    new_dict = {}
+    for k, v in cls.items():
+        new_dict[k] = storage.count(v)
+        return jsonify(new_dict)
