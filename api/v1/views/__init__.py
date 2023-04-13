f6c4d09a0ab6ac4598202c3617b5d5d0e5ff40e3
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-04-13 15:35:40 +0200

    adding docstr

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-
+"""
+    INIT BLUEPRINT AND 
+    IMPORT VIEW
+"""
+# blueprint to import routes and state blueprints for CRUD and JSON - based
 from flask import Blueprint
 
 app_views = Blueprint('api/v1', __name__)
