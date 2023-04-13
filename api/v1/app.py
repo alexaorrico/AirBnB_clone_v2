76ff95cd59a3162f55d8cc2169a36714b9063b92
Pauline <paulineparmigiani@gmail.com>
2023-04-13 16:30:47 +0200

    add error 4004 not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,10 +4,11 @@ create app
 '''
 
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,6 +17,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_appcontext(code):
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
