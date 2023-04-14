55f4023f21be3dfafddca5666260b95b90638211
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-04-14 21:56:13 +0200

    pep8 correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources=r'/*', origins="0.0.0.0")
 
+
 @app.teardown_appcontext
 def teardown(self):
     ''' Remove the current SQLAlchemy Session '''
