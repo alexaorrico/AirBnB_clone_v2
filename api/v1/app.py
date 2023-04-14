f9e4bddfafa7c78d24184be347ed6ea278920354
marlyne <=>
2023-04-14 14:24:59 +0200

    create a variable

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,23 +3,15 @@
     Main file of API,
     he store the Flask app and run the api
 """
-
-
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
-# Run the app with the default port 5000 threaded true
-if __name__ == '__main__':
-    app.run(
-        host= os.environ.get('HBNB_API_HOST', '0.0.0.0'),
-        port= os.environ.get('HBNB_API_PORT', '5000'), threaded=True
-    )
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -30,4 +22,12 @@ def page_not_found(error):
         @param error - The error that was encountered. Can be None.
         @return The JSON error message with the error message in it
     """
-    return jsonify({"error": "not found"})
+    return jsonify({"error": "Not found"})
+
+
+# Run the app with the default port 5000 threaded true
+if __name__ == '__main__':
+    app.run(
+        host= getenv('HBNB_API_HOST', '0.0.0.0'),
+        port= getenv('HBNB_API_PORT', '5000', threaded=True)
+    )
