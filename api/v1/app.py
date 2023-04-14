2e6ce12a342d43bebe8c6d58f9212f6a9e7b4cc3
Marlyne Ihorimbere <113862900+Nelyrma@users.noreply.github.com>
2023-04-14 15:56:28 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,40 +6,28 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-from flask import jsonify
-
+from flask import Flask, jsonify
+from models import storage
+import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    ''' Remove the current SQLAlchemy Session '''
+    storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """
-        Return a JSON error message. 
-        This is used to display an error when the request cannot be found.
-
-        @param error - The error that was encountered. Can be None.
-        @return The JSON error message with the error message in it
-    """
-    return jsonify({"error": "Not found"})
-
-
-def teardown_db(exception):
-    """
-     Clean up after test. This is called at the end of each test and cleans up the database.
-
-     @param exception - The exception that caused the test to fail. This is used to provide context to the failure
-    """
-    storage.close()
+    """Use errorhandler to display 404 error page"""
+    return jsonify({"error": "Not found"}), 404
 
 
 # Run the app with the default port 5000 threaded true
 if __name__ == '__main__':
-    app.run(
-        host= getenv('HBNB_API_HOST', '0.0.0.0'),
-        port= getenv('HBNB_API_PORT', '5000'),
-        threaded=True
-    )
-
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
