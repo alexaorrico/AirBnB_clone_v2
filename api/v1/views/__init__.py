f232af802d6de69e3a2d0618c97ccc306b2177a5
marlyne <=>
2023-04-14 16:14:48 +0200

    import blueprint and create app_views variable

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,13 +3,13 @@
     INIT BLUEPRINT AND 
     IMPORT VIEW
 """
+from flask import Blueprint
 
 
-app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
 # blueprint to import routes and state blueprints for CRUD and JSON - based
-from api.v1.views.states import *
 from api.v1.views.index import *
 # from api.v1.views.states import *
 # from api.v1.views.cities import *
