390beda1f79dc01373a072376205cde05ad5c4c6
marlyne <=>
2023-04-14 14:35:46 +0200

    endpoint that retrieves the number of each objects type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""INDEX.PY
-
-    Route Status
-"""
-
-
+"""create route on the object app_views"""
+from api.v1.views import app_views
 from flask import jsonify
-from api.v1.app import app
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
-@app.route('/status', strict_slashes=False)
+
+@app_views.route('/status', strict_slashes=False)
 def index():
     """
      Return status of API.
@@ -19,12 +22,16 @@ def index():
     return jsonify({"status": "OK"})
 
 
-@app.route('/stats', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def count():
     """
-     Count how many records exist in the database.
-     This is useful for determining if a query is valid or not.
-     
-     @return 200 if everything worked 400 if something went wrong with the
+    retrieve the number of each objects by type
     """
-    return jsonify({"status": "OK"})
+    return jsonify({
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+        })
