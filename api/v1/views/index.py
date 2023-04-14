3ac05658b8226403985e9eb4749e2e1dee51e845
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2023-04-14 11:12:18 +0200

    refactor(index.py): reorder imports alphabetically and group models imports together

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,15 +8,15 @@ Routes:
 """
 
 
-from flask import jsonify
 from api.v1.views import app_views
-from models import storage
-from models.user import User
+from flask import jsonify
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
