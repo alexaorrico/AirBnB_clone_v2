f9000c1f8e315014e24d3b1f15c45d1fcc1605d6
OmarElati <5796@holbertonstudents.com>
2023-04-21 21:10:24 +0100

    test

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,7 +9,8 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
 def get_cities_by_state(state_id):
     """Retrieve all cities related to a given state"""
     state = storage.get(State, state_id)
@@ -39,7 +40,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     """Creates a city"""
     state = storage.get(State, state_id)
