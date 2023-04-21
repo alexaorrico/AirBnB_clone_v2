c9b8aad27110995faa9bbd1cb0b4485dbeaf6f7b
OmarElati <5796@holbertonstudents.com>
2023-04-21 17:06:52 +0100

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
 """
-Index view module.
+Module: index
 """
-from api.v1.views import app_views
+from api.v1.views import app_views, storage
 from flask import jsonify
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status/', strict_slashes=False)
 def status():
-    """
-    GET /status
-    """
-    return jsonify({'status': 'OK'})
+    """ returns status: OK JSON  """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats():
+    """ returns number of objects by type  """
+    class_counts = {}
+    convert_dict = {
+        'Amenity': 'amenities',
+        'State': 'states',
+        'City': 'cities',
+        'User': 'users',
+        'Place': 'places',
+        'Review': 'reviews'
+    }
+
+    for _class in convert_dict.keys():
+        class_counts[convert_dict[_class]] = storage.count(_class)
+
+    return jsonify(class_counts)
