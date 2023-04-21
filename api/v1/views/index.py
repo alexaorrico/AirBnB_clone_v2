4d05c62192cb414560064b9e9d2d3b136786bd8f
OmarElati <5796@holbertonstudents.com>
2023-04-21 17:43:38 +0100

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
 """
-Module: index
+This module defines the index view of the API.
+
+Routes:
+    /status: Returns the status of the API.
+    /stats: Returns the number of objects by type.
 """
+
 from api.v1.views import app_views, storage
 from flask import jsonify
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
+# Define a route to get the status of the API
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_api_status():
+    """
+    Return the status of the API.
 
-@app_views.route('/status/', strict_slashes=False)
-def status():
-    """ returns status: OK JSON  """
+    Returns:
+        A JSON response with a key "status" and value "OK".
+    """
     return jsonify({"status": "OK"})
 
+# Define a route to get the number of objects by type
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_obj_counts():
+    """
+    Return the number of objects by type.
 
-@app_views.route('/stats/', strict_slashes=False)
-def stats():
-    """ returns number of objects by type  """
+    Returns:
+        A JSON response with keys representing object types and values representing
+        the count of objects of each type.
+    """
     class_counts = {}
     convert_dict = {
         'Amenity': 'amenities',
@@ -32,7 +41,8 @@ def stats():
         'Review': 'reviews'
     }
 
-    for _class in convert_dict.keys():
-        class_counts[convert_dict[_class]] = storage.count(_class)
+    for obj_type, route_prefix in convert_dict.items():
+        count = storage.count(obj_type)
+        class_counts[route_prefix] = count
 
     return jsonify(class_counts)
