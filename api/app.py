2fc2328041c1f7066bd316f593e9afa455b49446
OmarElati <5796@holbertonstudents.com>
2023-04-21 17:02:13 +0100

    test

---
+++ api/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Flask app module.
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def root_path():
+    """
+    GET /
+    """
+    return jsonify({'message': 'Hello World'})
+
+
+if __name__ == '__main__':
+    app.run()
--- api/v1/views/app.py
+++ api/v1/views/app.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 """
-Flask app module.
+Flask App that integrates with AirBnb static HTML Template
 """
-from flask import Flask, jsonify
+from flask import Flask
+from api.v1.views import app_views
+from models import storage
+import os
+
 
 app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
-@app.route('/', methods=['GET'])
-def root_path():
+@app.teardown_appcontext
+def teardown(self):
     """
-    GET /
+    Calls storage.close() on app context teardown
     """
-    return jsonify({'message': 'Hello World'})
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+        port=os.getenv('HBNB_API_PORT', '5000'),
+        threaded=True
+    )
