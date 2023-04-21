63270326e42e74357f3b15c855d43e6d97a21d0c
OmarElati <5796@holbertonstudents.com>
2023-04-21 20:37:21 +0100

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage engine"""
