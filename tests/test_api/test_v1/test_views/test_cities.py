a9b58c7c3a2e3856d268a26b0df1bac82b274d66
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-28 16:00:34 +0000

    Adds

---
+++ tests/test_api/test_v1/test_views/test_cities.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Contains tests for the view.index.py file"""
+
+import unittest
+from werkzeug.test import Client
+from api.v1.views import cities
+
+class CitiesTestCase(unittest.TestCase):
+    """Tests cases for the views.index file"""
+    def test_views_app(self):
+        """test views app"""
+        pass
