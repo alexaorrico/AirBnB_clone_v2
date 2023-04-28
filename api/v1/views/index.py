b8946bbde9ed5914681adfa9e69be8a7de4fa081
loyd ngei <loydngei@gmail.com>
2023-04-29 01:14:13 +0300

    Index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app.route('/status'):
-    def app_views():
-        """Return a JSON"""
-        return jsonify({'status': 'OK'})
+@app.route('/status')
+def app_views():
+    """Return a JSON"""
+    return jsonify({'status': 'OK'})
