47b5bf9bdb574eeae62d991263fa768fb95061f8
COLLINS  TONUI <tckiprotich@gmail.com>
2023-04-28 15:44:59 +0000

    fixed 1.. Never fail!

--- tests/test_console.py
+++ tests/test_console.py
@@ -39,3 +39,11 @@ class TestConsoleDocs(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+
+    def test_HBNBCommand_func_docstrings(self):
+        """Test for the presence of docstrings in HBNBCommand methods"""
+        for func in dir(HBNBCommand):
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
