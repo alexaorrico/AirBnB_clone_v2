8736e4facaebab24848bb3c573c10861cfd5f79a
RIKO-99 <rikohssn@gmail.com>
2023-04-28 09:18:24 +0000

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,29 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def api_status():
     """ A function to return status of the API """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def obj_stats():
+    """returns the number of each object"""
+
+    my_dict = {
+            "amenities": storage.count(Amenity), 
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return jsonify(my_dict)
