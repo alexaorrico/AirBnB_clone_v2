3b76593b324bade6347f794e9af85e2cbf53b34f
Martins Ndifon <martinsndifon@gmail.com>
2023-04-28 22:41:39 +0000

    update to task 8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,4 @@ city_views = Blueprint('city_views', __name__, template_folder='templates')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenities import *
