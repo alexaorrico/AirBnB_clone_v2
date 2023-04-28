d0c1434b313ae0d16e66ec7dbf7a6f670dd0c48e
isaaccleopas <isaaccleopas@gmail.com>
2023-04-28 17:58:33 +0000

    improved storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -67,7 +67,6 @@ test_db_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
-    def 
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
