bf4ca9072e0a7116c6ab6f39ab06ec6a50ace2eb
sethdanny <naddulidaniel1994@gmail.com>
2023-04-28 15:26:08 -0700

    created my first endpoint

---
+++ api/v1/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" HBNB flask application """
+
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
