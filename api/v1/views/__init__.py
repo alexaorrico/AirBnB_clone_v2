bac72a6d38534cfac940af22a97437b45fd4d4db
UmarFarukA <talk2ufaz@gmail.com>
2023-04-29 07:14:54 -0700

    update module

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views import states
-from api.v1.views import cities
-from api.v1.views import amenities
-from api.v1.views import users
-
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
+from api.v1.views.state import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
+from api.v1.views.users import *
+from api.v1.views.places_reviews import *
+from api.v1.views.amenities import *
