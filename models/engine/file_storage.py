a9084dedbaaadb2f5a77df3c639d5b8a3dc02f90
TKMshengu <tokocha81@gmail.com>
2023-04-29 22:13:09 +0200

    Update file_storage.py
    Update FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,10 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+ 
+    def count(self, cls=None):
+        """ counts """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
