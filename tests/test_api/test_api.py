0abdeab5606156317baf76fc68b846633d7bf84d
Ebenezer <akhonyaebenezer@gmail.com>
2023-04-29 17:18:36 +0300

    [Chore]: Test API routes

---
+++ tests/test_api/test_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Test the API"""
+
+
+from unittest import TestCase
+
+
+class TestAPI(TestCase):
+    """Unitests for the API"""
+
+    def test_api_status(self):
+        """Initial tests on the API"""
+        pass
+
+    def test_new_api_route(self):
+        """Test the /status route"""
+        pass
+
