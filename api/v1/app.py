8a2c0fd4b07dae3a2e7f550d9222211a65800439
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-29 02:22:30 -0700

    Test cases

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from os import getenv
 from models import storage
 
 app = Flask(__name__)
@@ -16,8 +17,11 @@ def clean_up(exception=None):
 @app.errorhandler(404)
 def not_found_error(error):
     " Handling 404 errors using json """
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = getenv('HBNB_API_HOST', defeault='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
