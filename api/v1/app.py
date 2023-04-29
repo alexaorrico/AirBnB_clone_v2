1af1301e861414ec0f977f52347455841d71b99c
UmarFarukA <talk2ufaz@gmail.com>
2023-04-29 09:11:32 -0700

    add:cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,14 @@
 """A Script that return the status of API"""
 import os
 from flask import Flask
+from flask_cors import CORS, cross_origin
+from werkzeug.exceptions import HTTPException
 from models import storage
 from app.v1.views import app_views
 
 
 app = Flask(__name__)
+cors = CORS(app, resouces={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(app_views)
 
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
