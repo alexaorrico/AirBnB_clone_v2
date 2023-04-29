c0bf38c738243934e188b546b632e309a677c7a7
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-29 03:35:08 -0700

    Tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,116 +1,58 @@
 #!/usr/bin/python3
-"""
-Contains the TestDBStorageDocs and TestDBStorage classes
-"""
 
-from datetime import datetime
-import inspect
-import models
-from models.engine import db_storage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
+""" Handles all restful API actions for State"""
+
+from api.v1.views import app_views
+from flask import request, jsonify, abort
 from models.state import State
-from models.user import User
-import json
-import os
-import pep8
-import unittest
-DBStorage = db_storage.DBStorage
-classes = {"Amenity": Amenity, "City": City, "Place": Place,
-           "Review": Review, "State": State, "User": User}
+from models import storage
 
 
-class TestDBStorageDocs(unittest.TestCase):
-    """Tests to check the documentation and style of DBStorage class"""
-    @classmethod
-    def setUpClass(cls):
-        """Set up for the doc tests"""
-        cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
+@app_views.route('/states',
+                 methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>',
+                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def states(state_id=None):
+    """Retrieves a list of state objects"""
 
-    def test_pep8_conformance_db_storage(self):
-        """Test that models/engine/db_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/engine/db_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    state_objs = storage.all(State)
 
-    def test_pep8_conformance_test_db_storage(self):
-        """Test tests/test_models/test_db_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_engine/\
-test_db_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    states = [obj.to_dict() for obj in state_objs.values()]
+    if not state_id:
+        if request.method == 'GET':
+            return jsonify(states)
+        elif request.method == 'POST':
+            my_dict = request.get_json()
 
-    def test_db_storage_module_docstring(self):
-        """Test for the db_storage.py module docstring"""
-        self.assertIsNot(db_storage.__doc__, None,
-                         "db_storage.py needs a docstring")
-        self.assertTrue(len(db_storage.__doc__) >= 1,
-                        "db_storage.py needs a docstring")
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            if my_dict.get("name") is None:
+                abort(400, 'Missing name')
+            new_state = State(**my_dict)
+            new_state.save()
+            return jsonify(new_state.to_dict()), 201
+    else:
+        if request.method == 'GET':
+            for state in states:
+                if state.get('id') == state_id:
+                    return jsonify(state)
+            abort(404)
+        elif request.method == 'PUT':
+            my_dict = request.get_json()
 
-    def test_db_storage_class_docstring(self):
-        """Test for the DBStorage class docstring"""
-        self.assertIsNot(DBStorage.__doc__, None,
-                         "DBStorage class needs a docstring")
-        self.assertTrue(len(DBStorage.__doc__) >= 1,
-                        "DBStorage class needs a docstring")
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            for state in state_objs.values():
+                if state.id == state_id:
+                    state.name = my_dict.get("name")
+                    state.save()
+                    return jsonify(state.to_dict()), 200
+            abort(404)
 
-    def test_dbs_func_docstrings(self):
-        """Test for the presence of docstrings in DBStorage methods"""
-        for func in self.dbs_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
-
-
-class TestFileStorage(unittest.TestCase):
-    """Test the FileStorage class"""
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_all_returns_dict(self):
-        """Test that all returns a dictionaty"""
-        self.assertIs(type(models.storage.all()), dict)
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_all_no_class(self):
-        """Test that all returns all rows when no class is passed"""
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_new(self):
-        """test that new adds an object to the database"""
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_save(self):
-        """Test that save properly saves objects to file.json"""
-
-class TestNewMethodsDb(unittest.TestCase):
-    """ Testing get and count methods in db_storage """
-
-    def setUp(self):
-        """ set up for db """
-        self.obj_instance = State(name="Vienna")
-
-    def tearDown(self):
-        self.obj_instance.delete()
-
-    def test_count(self):
-        """ Testing for count method """
-
-        obj_count = models.storage.count(State)
-        self.obj_instance.save()
-        obj_second_count = models.storage.count(State)
-
-        self.assetEqual(obj_count + 1, obj_second_count)
-    def test_get(self):
-        """ Testing for get method """
-        self.obj_instance.save()
-        id = self.obj_instance.id
-        get_obj = models.storage.get(State, id)
-
-        self.assertEqual(id, get_obj.id)
-        self.assertInstance(get_obj, State)
-        self.assertEqual(type(id), str)
+        elif request.method == 'DELETE':
+            for obj in state_objs.values():
+                if obj.id == state_id:
+                    storage.delete(obj)
+                    storage.save()
+                    return jsonify({}), 200
+            abort(404)
