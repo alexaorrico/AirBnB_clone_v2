178ea1e531381c17ccd0199feccb95500ffb1894
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-29 14:34:07 +0100

    Task 09: User

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -35,3 +35,42 @@ def delete_user(user_id):
     storage.delete(user)
     storage.save()
     return jsonify({}), 200
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Create a new User object and return its JSON representation"""
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+    if 'email' not in data:
+        return jsonify({'error': 'Missing email'}), 400
+    if 'password' not in data:
+        return jsonify({'error': 'Missing password'}), 400
+
+    user = User(**data)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """Updates User object with the matching id with JSON data"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+
+    for key in ['id', 'email', 'created_at', 'updated_at']:
+        try:
+            data.pop(key)
+        except KeyError:
+            pass
+
+    for key, value in data.items():
+        setattr(user, key, value)
+    storage.save()
+    return jsonify(user.to_dict()), 200
