3190cdf925ab9241ede8aeeaa7bdb04abf56e0f0
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-04-29 20:57:18 +0000

    Modify
    Excludes the password from the to_dict() output on 'db' storage

--- models/base_model.py
+++ models/base_model.py
@@ -68,6 +68,9 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if models.storage_t == 'db':
+            if new_dict.get('password'):
+                del new_dict['password']
         return new_dict
 
     def delete(self):
