13a70fca481c5081e927e5d882cafa008b592d69
Papi <111683116+itsayopapi@users.noreply.github.com>
2023-04-29 16:24:21 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-'''Blueprint - variable name: app_views'''
+'''the blueprint for the API.'''
 from flask import Blueprint
+
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-if app_views is not None:
-    from api.v1.views.index import *
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    from api.v1.views.amenities import *
-    from api.v1.views.users import *
-    from api.v1.views.places import *
-    from api.v1.views.places_reviews import *
-    from api.v1.views.places_amenities import *
+'''blueprint for the AirBnB clone API.'''
+
+
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.index import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
