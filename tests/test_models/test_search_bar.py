08f088defbc500f61e5f1cc39effc7f179a830c0
Decreedblocks <emekky54@gmail.com>
2023-04-29 18:22:22 +0100

    added1

--- tests/test_models/test_search_bar.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-def test_search_bar():
-    homepage = '<body><header><form><input type="text" name="search" placeholder="Search"></form></header></body>'
-    assert '<input type="text" name="search"' in homepage
-
-if name == '__main__':
-    test_search_bar()
