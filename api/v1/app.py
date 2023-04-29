e1568ad41d590b3b24ccc98ef8acea506a66ad81
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-04-29 15:09:18 +0100

    fix: Use env var

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,10 @@
 hbnb airBnB clone
 '''
 
-from models import storage
 from api.v1.views import app_views
+from models import storage
 from flask import Flask
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,4 +19,6 @@ def teardown_db(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
