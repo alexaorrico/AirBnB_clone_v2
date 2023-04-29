af3c6ebfcd0865b84de3fe40366e8c091f8446c7
Bsakwa <sakwabrian23@gmail.com>
2023-04-29 18:09:57 +0300

    Add a count route for models

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,9 @@
 
 '''
 Index file that renders the index.html template
+Routes:
+    /status: Returns a JSON : "status": "OK"
+    /stats: Returns a JSON with the number of objects by type
 '''
 
 from flask import Flask, render_template, jsonify
@@ -15,3 +18,23 @@ def status():
             {
                 "status": "OK"
             })
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    '''Returns a JSON with the number of objects by type'''
+    from models import storage
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.place import Place
+    from models.review import Review
+    from models.user import User
+
+    return jsonify({'amenities': storage.count(Amenity),
+                    'cities': storage.count(City),
+                    'places': storage.count(Place),
+                    'reviews': storage.count(Review),
+                    'states': storage.count(State),
+                    'users': storage.count(User)
+                    })
