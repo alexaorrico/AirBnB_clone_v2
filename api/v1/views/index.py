f249dd6d64b27adc05a148d579cc1f433d2fe426
Nightburn <nightburn2003@gmail.com>
2023-04-29 06:52:48 +0100

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -25,12 +25,13 @@ def get_api_stats():
     """gets the number of each objects"""
 
     stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+       'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
     }
-
+    for key, value in stats.items():
+       stats[key] = storage.count(value)
     return jsonify(stats)
