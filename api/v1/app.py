8f7c691aff346127282e8ab26ebe8842c55acc77
Nightburnn <nightburn2003@gmail.com>
2023-04-29 08:24:12 +0100

    task 5: 404 status

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,19 +4,12 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-# import sys
-# sys.path.insert(0, '/AirBnB_clone_v3/models')
-
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 """ flask web app instance"""
-# app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-# app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-# app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-# CORS(app, resources={'/*': {'origins': app_host}})
 CORS(app, origins='0.0.0.0')
 
 
@@ -29,12 +22,13 @@ def teardown_storage(exception):
 @app.errorhandler(404)
 def not_found(e):
     """handles a 404 error"""
-
-    response = {"error": "Not found"}
-    return jsonify(response)
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=app_host, port=app_port, threaded=True)
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True)
