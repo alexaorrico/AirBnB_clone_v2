59efad7fa9ae085f310eb0c1c4db1481ed68e824
Shobayo Samuel <shobayosamuel62@gmail.com>
2023-04-29 08:31:21 +0000

    fix database conflicts

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from flask import Flask, Blueprint
+from models import storage
+from os import getenv
+from api.v1.views import app_views
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def closeDB():
+    """Close the database"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", default='0.0.0.0')
+    port = getenv("HBNB_API_PORT", default=5000)
+    app.run(host, int(port), threaded=True)
