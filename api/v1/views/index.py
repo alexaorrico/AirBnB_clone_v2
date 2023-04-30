1744abf33aaf5e6a063260a40770a73874f151c1
allenkays <allenkays@tutanota.com>
2023-04-30 09:06:18 -0400

    Add a stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,3 +5,18 @@ from flask import jsonify
 @app_views.route('/status')
 def get_status():
     return jsonify(status="OK")
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """
+    Retrieves the number of each object by type
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
