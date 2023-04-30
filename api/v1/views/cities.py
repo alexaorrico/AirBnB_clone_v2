d1cfcfb9bf578db7bec4b3ca07e61cc133f48292
kadimakeith <keithkadima@gmail.com>
2023-04-30 10:22:34 -0700

    fix: RESTful Actions errors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,12 +5,10 @@ from models.state import State
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/city/cities_by_state.yml', methods=['GET'])
 def get_cities(state_id):
     """
     Retrieves the list of all cities objects
@@ -27,7 +25,6 @@ def get_cities(state_id):
 
 
 @app_views.route('/cities/<city_id>/', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/city/get_city.yml', methods=['GET'])
 def get_city(city_id):
     """
     Retrieves a specific city based on id
@@ -39,7 +36,6 @@ def get_city(city_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
-@swag_from('documentation/city/delete_city.yml', methods=['DELETE'])
 def delete_city(city_id):
     """
     Deletes a city based on id provided
@@ -56,7 +52,6 @@ def delete_city(city_id):
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
                  strict_slashes=False)
-@swag_from('documentation/city/post_city.yml', methods=['POST'])
 def post_city(state_id):
     """
     Creates a City
@@ -77,7 +72,6 @@ def post_city(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/city/put_city.yml', methods=['PUT'])
 def put_city(city_id):
     """
     Updates a City
