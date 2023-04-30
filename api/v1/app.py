46ab978e042307548055627fbf99c9e4340e2aea
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-04-30 20:33:00 +0300

    Initialzinge

---
+++ api/v1/app.py
@@ -0,0 +1,18 @@
+# api/v1/app.py
+
+import os
+from flask import Flask
+from api.v1.views import app_views
+from models import storage
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+@app.teardown_appcontext
+def close_storage(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
