74f7252060031521df4fbe7a400e6816c02f8315
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-04-30 20:38:42 +0300

    Create Flask Blueprint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
+""" api/vi/views/index.py"""
 
-"""
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status', methodes=[GET])
+@app_views.route('/status', methodes=['GET'])
 def status():
-    return jsonify{'status': 'OK'}
+    return jsonify({'status': 'OK'})
