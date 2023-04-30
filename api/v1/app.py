444e6f7bb3fd76f74722526ba0ecc19aa64bfa6d
sethdanny <naddulidaniel1994@gmail.com>
2023-04-30 00:28:26 -0700

    created my first end point

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,4 +9,12 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.teardown_appcontext
+def clean(exception=None):
+    """ close storage sessions """
+    storage.close()
 
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host, port , threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" index file for my flask application """
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status/', strict_slashes=False)
+def status():
+    """return json object status"""
+    return jsonify({"status": "OK"})
