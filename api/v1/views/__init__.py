620af3ad8eb3f1b1d4a21dbc7e6d1c88ca41e67c
attahchisom6 <attahchisom6@gmail.com>
2023-04-30 18:10:10 +0100

    whats wrong: must find it

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,16 +6,6 @@ from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
-
-"""import storage packages"""
-from models import storage
-from models.state import State
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
-
 """Now import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
