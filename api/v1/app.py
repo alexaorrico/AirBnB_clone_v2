4cadaf624317b0f7dff1d5ca5c50196bbc24df13
sethdanny <naddulidaniel1994@gmail.com>
2023-04-30 13:03:25 -0700

    fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,18 +9,22 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
+
 
 @app.teardown_appcontext
 def clean(exception=None):
     """ close storage sessions """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     """handle 404 error"""
     return jsonify({"error": "Not found"}), 404
 
+
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
-    app.run(host, port , threaded=True)
+    app.run(host, port, threaded=True)
