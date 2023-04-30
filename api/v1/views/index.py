937bf6b65acef4191f351b248c997eb30d66a78e
Lawal Damilare <109480017+OLUDAMI08@users.noreply.github.com>
2023-04-30 23:41:40 +0100

    Update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False
 def get_status():
     """ status code"""
     return jsonify(status="ok")
