fd9c94e9e8276e1610060eb450b9d1c2ddfbe953
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-04-30 22:59:14 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
+""" This module contains the routes for the web application
+"""
+
 from api.v1.views import app_views
 from flask import jsonify
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    """ status code"""
     return jsonify(status="ok")
