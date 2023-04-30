84a758ca8eb281fa4502157bdb899d13752f0c01
lucydevonne <wambuilucie7@gmail.com>
2023-04-30 08:05:14 +0000

    sql

--- test_get_count.py
+++ test_get_count.py
@@ -1,15 +1,11 @@
+#!/usr/bin/python3
+""" Test .get() and .count() methods
+"""
 from models import storage
 from models.state import State
 
-def test_get_count():
-    # Test get() method
-    first_state_id = list(storage.all(State).values())[0].id
-    first_state = storage.get(State, first_state_id)
-    assert isinstance(first_state, State)
+print("All objects: {}".format(storage.count()))
+print("State objects: {}".format(storage.count(State)))
 
-    # Test count() method
-    all_count = storage.count()
-    state_count = storage.count(State)
-    assert all_count > 0
-    assert state_count > 0
-
+first_state_id = list(storage.all(State).values())[0].id
+print("First state: {}".format(storage.get(State, first_state_id)))
