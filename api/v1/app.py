8c5a7d9f88e2344f65791f1679fa85b03db173b7
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-04-30 18:48:45 +0100

    update

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    if not host:
+        host="0.0.0.0"
+    if not port:
+        port = "5000"
+    app.run(host=host, port=port, threaded=True, debug=True)
