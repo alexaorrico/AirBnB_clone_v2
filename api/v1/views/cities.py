ecd5286f06fa5f03e10e32948dd542a4f8bbed4c
oneharry <harryofficialchidi@yahoo.com>
2023-04-30 22:40:48 +0000

    Authors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,5 +2,19 @@
 """
 Handle all default RESTFUL API actions
 """
+from models.city import City
+from models.state import State
+from api.v1.views import app_views
 
 
+@app_views.route('/states/<state_id>/cities', methods=('GET'))
+def cities_in_state(state_id):
+    """ Returns all cities in a state id"""
+    all_states = State.to_dict()
+    for key, val in all_states.items():
+	if val['state_id'] == state_id:
+            st_id = all_states[key]
+            break
+        else:
+            st_id = None
+    
