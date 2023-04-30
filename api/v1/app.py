084de40d1bbddf279c14684673b2919475a6648a
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-04-30 23:03:26 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+"""This module contain a simple flask application"""
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -13,8 +15,6 @@ def teardown_storage(exception):
     storage.close()
 
 if __name__ == "__main__":
-    if not host:
-        host="0.0.0.0"
-    if not port:
-        port = "5000"
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True, debug=True)
