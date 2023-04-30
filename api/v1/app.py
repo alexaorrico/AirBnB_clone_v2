6f9ef0368707819c4ba076a86a9bf4fb66d81225
allenkays <allenkays@tutanota.com>
2023-04-30 09:27:27 -0400

    Add changes to state vies

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,10 @@ def close_storage(self):
     """
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = getenv("HBMB_API_HOST", "0.0.0.0")
