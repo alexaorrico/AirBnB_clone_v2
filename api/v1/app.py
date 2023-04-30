c5d617d35719c616b5b33a42d3812f5c22109bad
sethdanny <naddulidaniel1994@gmail.com>
2023-04-30 12:59:36 -0700

    handled 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, jsonify
 from models import storage
+from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
 
@@ -13,6 +14,11 @@ app.register_blueprint(app_views)
 def clean(exception=None):
     """ close storage sessions """
     storage.close()
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """handle 404 error"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', '0.0.0.0')
