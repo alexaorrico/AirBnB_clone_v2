54d1e2e6e2fafe7c46b93a43d063f5429dcc1414
dannyEnagu <danielEnagu@gmail.com>
2023-04-30 00:24:54 +0000

    12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,15 @@
 """creates the app routes"""
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
+
 host = getenv('HBNB_API_HOST', default='0.0.0.0')
 port = getenv('HBNB_API_PORT', default='5000')
 
