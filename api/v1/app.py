f674e476f314a39f1dc9b9969759886c88ae04e1
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-04-30 23:08:28 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,9 +10,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def teardown_storage(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
