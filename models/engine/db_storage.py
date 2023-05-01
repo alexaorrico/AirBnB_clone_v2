55674d6bb98971e14aa69b7693e1829acf99fc51
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-05-01 10:46:02 -0700

    fixed a bug

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,9 +73,9 @@ class DBStorage:
 
     def close(self):
         """call remove() method on the private session attribute"""
-self.__session.remove()
+        self.__session.remove()
 
- def get(self, cls, id):
+    def get(self, cls, id):
         """
         Returns the object based on the class name and its ID, or
         None if not found
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -11,6 +12,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from hashlib import md5
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -44,7 +46,9 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+            if key == "password":
+                json_objects[key].decode()
+            json_objects[key] = self.__objects[key].to_dict(save_fs=1)
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
@@ -66,10 +70,6 @@ class FileStorage:
                 del self.__objects[key]
 
     def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
-        self.reload()
-
- def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
