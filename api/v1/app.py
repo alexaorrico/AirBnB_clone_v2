b111538f337ed9132bd8b7a091afb23e38c5480b
georgeeset <georgeperfect4u@gmail.com>
2023-05-01 06:08:12 +0100

    404 error added to json esponse from api

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,10 +17,12 @@ def teardown(self):
     storage.close()
 
 
-# @app.errorhandler(404)
-# def not_found(error):
-#     """Return json string in case of 404, Not Found"""
-#     return make_response(jsonify({"error": "Not found"}), 404)
+@app.errorhandler(404)
+def not_found(error):
+    """handler for 404 errors that returns a
+    JSON-formatted 404 status code response
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
