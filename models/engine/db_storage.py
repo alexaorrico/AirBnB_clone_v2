583141b188a7a98f08ea8c7a8767f4d03be10b0e
Seife S Demissie <seifesdemissie@Seifes-MacBook-Pro.local>
2023-05-01 19:49:36 +0300

    Updates on db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,23 +76,32 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
+        """A method to retrieve one object:
+        Returns the object based on the class & its ID, or None if not found
         """
-        Returns the object based on the class name and its ID, or None if not
-        found
-        """
-        objects = self.__session.query(classes[cls])
-        for obj in objects:
-            if obj.id == id:
-                return obj
+        if cls not in classes.values():
+            return None
+
+        all_objs_dict = models.storage.all(cls)
+        for value in all_objs_dict.values():
+            if (value.id == id):
+                return value
+
         return None
 
     def count(self, cls=None):
         """
-        Returns the number of objects in storage matching the given class name.
-        If no name is passed, returns the count of all objects in storage.
+        A method to count the number of objects in storage:
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
         """
-        nobjects = 0
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                nobjects += len(self.__session.query(classes[clss]).all())
-        return nobjects
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
