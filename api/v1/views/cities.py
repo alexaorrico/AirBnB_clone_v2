c8cfaeeba7db33aaa517645d47546bce5f8df0a4
oneharry <harryofficialchidi@yahoo.com>
2023-05-01 12:54:15 +0000

    Implememnted task 7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,16 +5,56 @@ Handle all default RESTFUL API actions
 from models.city import City
 from models.state import State
 from api.v1.views import app_views
+from flask import Flask, request, abort, jsonify
+from models import storage
 
 
-@app_views.route('/states/<state_id>/cities', methods=('GET'))
+@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'],
+                 strict_slashes=False)
 def cities_in_state(state_id):
     """ Returns all cities in a state id"""
-    all_states = State.to_dict()
-    for key, val in all_states.items():
-	if val['state_id'] == state_id:
-            st_id = all_states[key]
-            break
-        else:
-            st_id = None
-    
+    my_state = storage.get('State', state_id)
+    if my_state is None:
+        abort(404)
+    cities = []
+    for city in my_state.cities:
+        cities.append(city.to_dict())
+    if request.method == 'GET':
+        return jsonify(cities)
+    elif request.method == 'POST':
+        data = request.get_json()
+        if not data:
+            abort(400, 'Not a JSON')
+        if 'name' not in data:
+            abort(400, 'Missing name')
+        new_city = City(**data)
+        storage.new(new_city)
+        storage.save()
+        return jsonify(new_city.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>', methods=['GET', 'PUT', 'DELETE'],
+                 strict_slashes=False)
+def city(city_id):
+    """ Returns city object of id """
+    city = storage.get('City', city_id)
+    if city is None:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(city)
+    elif request.method == 'PUT':
+        data = request.get_json()
+        if not data:
+            abort(400, 'Not a JSON')
+        for k, v in data.items():
+            ign_attr = ['id', 'created_at', 'updated_at']
+            if k not in ign_attr:
+                city[k] = v
+        storage.save()
+        return jsonify(city.to_dict()), 200
+
+    elif request.method == 'DELETE':
+        storage.delete(city)
+        storage.save()
+        return jsonify({}), 200
