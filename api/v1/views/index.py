b6cf6cf9b43282237993140f521a66d139aa1fba
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-05-01 16:13:58 +0300

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """Defines the API routes for the Flask app"""
 
+from flask import jsonify, Blueprint
 from api.v1.views import app_views
 from models import storage
-from flask import jsonify, Blueprint
 
+
+app_views = Blueprint('app_views', __name__)
+
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Returns the status of the API"""
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/stats', methods=['GET'])
