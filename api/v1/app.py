287be61b5f7c7ae661bd3399d107a6782281c1d8
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-05-01 03:56:51 -0700

    Showing API status

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,10 +9,12 @@ from models import storage
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def clean_up(exception=None):
     """ Closes current session """
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found_error(error):
