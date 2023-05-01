7108a5c652d9eef16f6acddc91a878b3d8e8561c
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-05-01 11:49:49 +0100

    feature: Add custom 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,16 +5,23 @@ hbnb airBnB clone
 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
     '''Tear down seesion: db'''
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    '''Custom 404 error handler'''
+    return make_response(jsonify({"error": "Not found"}))
 
 
 if __name__ == '__main__':
