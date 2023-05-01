f0c2a9eed8cc461664a1ca6872cb390bda890d1b
georgeeset <georgeperfect4u@gmail.com>
2023-05-01 02:04:20 +0100

    boiler plate setup

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+#!/usr/bin/python3
+"""Status of api ApI V1"""
+from flask import Flask, jsonify, make_response
+#mport models
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
 
 app = Flask(__name__)
+# cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.teardown_appcontext
+def teardown(self):
+    """teardown appcontext"""
+    storage.close()
+
+
+# @app.errorhandler(404)
+# def not_found(error):
+#     """Return json string in case of 404, Not Found"""
+#     return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST") or '0.0.0.0'
+    port = getenv("HBNB_API_PORT") or 5000
+    app.run(host=host, port=port, threaded=True)
