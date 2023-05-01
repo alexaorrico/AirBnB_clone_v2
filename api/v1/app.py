d933e42d88e7a9f4f05da4daf0bb39dc1ba8e0f8
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-05-01 23:18:42 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-"""
-    This script starts a Flask web application
-"""
-from os import getenv
-from flask import Flask, jsonify
-from flask_cors import CORS
+""" Flask Application """
 from models import storage
 from api.v1.views import app_views
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
 from flasgger import Swagger
 from flasgger.utils import swag_from
 
-
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
-    return storage.close()
+def close_db(error):
+    """ Close Storage """
+    storage.close()
 
 
 @app.errorhandler(404)
-def error(e):
-    """Handler for 404 errors"""
-    return jsonify({"error": "Not found"}), 404
-
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone Restful API',
@@ -36,7 +38,12 @@ app.config['SWAGGER'] = {
 Swagger(app)
 
 
-if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
-    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
