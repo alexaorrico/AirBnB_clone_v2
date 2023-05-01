de5867dfe164f82938f9fc8d88908faa949c40e0
KayongoYongo <sknsammie10@gmail.com>
2023-05-01 19:27:28 +0000

    app.py_cors_update

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,13 @@
 from api.v1.views import app_views
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 from models import storage
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
