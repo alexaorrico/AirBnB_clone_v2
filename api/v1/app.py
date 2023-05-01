b617720a35a0690757c140365b5570a3efb04899
Bsakwa <sakwabrian23@gmail.com>
2023-05-01 11:45:48 +0300

    Update app with CORS configuration

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@
 if __name__ == "__main__":
 
     from flask import Flask, Blueprint, jsonify
+    from flask_cors import CORS
     from models import storage
     from api.v1.views import app_views
     from os import getenv
@@ -12,6 +13,9 @@ if __name__ == "__main__":
     # Creating the Flask application
     app = Flask(__name__)
     app.register_blueprint(app_views)
+
+    # CORS - Cross Origin Resource Sharing Configuration
+    CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
     @app.teardown_appcontext
     def teardown(self):
