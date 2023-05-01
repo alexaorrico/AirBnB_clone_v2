59833b06c41e32f80b156b217b0a0f332b30db4f
root <saheedolabanji@gmail.com>
2023-05-01 02:36:06 -0700

    updated index.py

--- api/v1/index.py
+++ api/v1/index.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python3
+"""API and Index Module"""
 
+from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    returns a count of all database objects
+    """
+
+    models_avail = {
+        "User": "users",
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+    }
+    mod_objs = [Amenity, City, Place, Review, State, User]
+
+    count = {models_avail[cls]: storage.count(obj) for cls, obj in enumerate(mod_objs)}
+
+    return jsonify(count)
