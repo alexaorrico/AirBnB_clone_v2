d9f658b78e11b227192584c78166e6b6bb69eb4f
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-05-01 10:21:06 -0700

    Adding get and count methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -51,6 +51,18 @@ class DBStorage:
                     new_dict[key] = obj
         return (new_dict)
 
+    def get(self, cls, id):
+        """ Getting an object based on its class and id """
+        my_dict = self.all(cls)
+        for obj in my_dict == id:
+            return obj
+        return None
+
+    def count(self, cls=None):
+        """ counts objects in the database by class """
+        my_dict = self.all(cls)
+        return (len(my_dict))
+
     def new(self, obj):
         """add the object to the current database session"""
         self.__session.add(obj)
