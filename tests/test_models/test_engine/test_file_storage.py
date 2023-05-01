a189347522d158464fdd7ee3bfbdb4e0aa690c3a
Favour Markson <95889759+Markson17@users.noreply.github.com>
2023-05-01 15:21:27 +0100

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,7 @@ Contains the TestFileStorageDocs classes
 from datetime import datetime
 import inspect
 import models
+from models import storage
 from models.engine import file_storage
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -29,6 +30,20 @@ class TestFileStorageDocs(unittest.TestCase):
     def setUpClass(cls):
         """Set up for the doc tests"""
         cls.fs_f = inspect.getmembers(FileStorage, inspect.isfunction)
+
+    def test_get(self):
+        """Tests to check get method"""
+        if storage.all("State").values():
+            first_state_id = list(storage.all("State").values())[0].id
+            first_state = storage.get("State", first_state_id)
+            self.assertTrue(isinstance(first_state, State))
+
+    def test_count(self):
+        """Tests to check count method"""
+        all_objects = storage.count()
+        all_states = storage.count("State")
+        self.assertTrue(isinstance(all_objects, int))
+        self.assertTrue(isinstance(all_states, int))
 
     def test_pep8_conformance_file_storage(self):
         """Test that models/engine/file_storage.py conforms to PEP8."""
@@ -113,3 +128,19 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_db_get(self):
+        """Tests to check get method"""
+        if storage.all("State").values():
+            first_state_id = list(storage.all("State").values())[0].id
+            first_state = storage.get("State", first_state_id)
+            self.assertTrue(isinstance(first_state, State))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_db_count(self):
+        """Tests to check the count method"""
+        all_objects = storage.count()
+        all_states = storage.count("State")
+        self.assertTrue(isinstance(all_objects, int))
+        self.assertTrue(isinstance(all_states, int))
