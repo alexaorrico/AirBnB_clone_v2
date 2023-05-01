3c06c7b06c55a2ca5452d4011ce5f2dedcf6d08a
UmarFarukA <talk2ufaz@gmail.com>
2023-05-01 03:00:41 -0700

    fix pep style

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,7 +75,6 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-
     def get(self, cls, id):
         """
             retrieves one object based on class name and id
