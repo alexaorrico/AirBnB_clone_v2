2e847433080c5b7d83af04f9bc8c795456a769db
Nightburnn <nightburn2003@gmail.com>
2023-05-01 22:50:51 +0100

    amenities object

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,11 +11,19 @@ from model.amenity import Amenity
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities()
+
+
 """ retrieves the list of all Amenity objects """
-amenities_list = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
+
+amenities_list = [
+        amenity.to_dict()
+        for amenity in storage.all(Amenity).values()]
 return jsonify(amenities_list)
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route(
+        '/amenities/<amenity_id>',
+        methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """ retrieves a Amenity object """
     amenity_obj = storage.get(Amenity, amenity_id)
@@ -25,7 +33,9 @@ def get_amenity(amenity_id):
         amenity_dict = amenity_obj.to_dict()
         return jsonify(amenity_dict)
 
-    @app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+    @app_views.route(
+            '/amenities/<amenity_id>',
+            methods=['DELETE'], strict_slashes=False)
     def delete_amenity(amenity_id):
         """ delete an/a Amenity object"""
         amenity_obj = storage.get(Amenity, amenity_id)
@@ -35,7 +45,6 @@ def get_amenity(amenity_id):
             storage.delete(amenity_obj)
             storage.save()
             return jsonify({}), 200
-
 
         @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
         def create_amenity():
@@ -51,7 +60,9 @@ def get_amenity(amenity_id):
 
             return jsonify(new_obj.to_dict()), 201
 
-        @app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+        @app_views.route(
+                '/amenities/<amenity_id>',
+                methods=['PUT'], strict_slashes=False)
         def update_amenity(amenity_id):
             """ updates an/a Amenity object"""
             amenity_obj = storage.get(Amenity, amenity_id)
