70bcaef493c475cf7c43efd7cab22c1c03d75cba
Daniel-Uchay <meetcaxtroh@gmail.com>
2023-05-01 08:42:48 +0100

    done

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''Contains the blueprint for the API.'''
+
 from flask import Blueprint
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
@@ -13,3 +14,12 @@ from api.v1.views.users import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 '''The blueprint for the AirBnB clone API.'''
+
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.index import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
