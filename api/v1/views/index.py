c1473c3ef2b3e7bb2aec7b07c101c10fa5a84863
Judisoft <kumjude09@gmail.com>
2023-05-01 18:16:37 +0000

    modified index.py to conform to pep8 standard

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,46 @@
 #!/usr/bin/python3
+"""
+index
+"""
 
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 
-app_views.route("/status", methods=["GET"], strict_slashes=False)
+from models import storage
 
 
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
-    return jsonify({"status": "OK"})
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
