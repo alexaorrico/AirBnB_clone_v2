3013d0279aa89d562c3550d6e1a3b919787bde9b
oneharry <harryofficialchidi@yahoo.com>
2023-05-01 18:16:46 +0000

    fixed task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,7 +5,7 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.state import storage
+from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False)
