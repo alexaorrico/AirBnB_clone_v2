c4c927388c4c7c5b5a16998be4c7adb8a3a97e34
attahchisom6 <attahchisom@gmail.com>
2023-05-01 23:12:21 +0100

    made some changes

--- work_dir/api/v1/views/cities.py
+++ work_dir/api/v1/views/cities.py
@@ -2,7 +2,7 @@
 """
 This module will help us control and manage state objects using Restful api
 """
-from flask import request, abort, jsonify
+from flask import request, abort, jsonify, make_response
 from models.city import City
 from models.state import State
 from api.v1.views import app_views
@@ -17,7 +17,41 @@ def allCities_inState(state_id):
     """
     c_list = []
     state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
     for city in state.cities:
-        c_list.append(city)
-        c_list = c_list.to_dict()
+        c_list.append(city.to_dict())
     return jsonify(c_list)
+
+
+@app_views.route(
+        "/cities/<city_id>", methods=['GET'], strict_slashes=False)
+def get_a_city(city_id):
+    """returns a single state"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    city = city.to_dict()
+    return jsonify(city)
+
+
+@app_views.route(
+        "/cities/<city_id>", methods=['DELETE'], strict_slashes=False)
+def delete_city_object(city_id):
+    """
+    delete a city object from the list
+    """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    city.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+@app_view.route(
+        '/states/<state_id>/cities', method=['POST'], strict_slashes=False)
+def Mypost(state_id):
+    if not request.get_json():
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if "name" not in request.get_json():
+        return make_response(jsonify({"error": "Missing name"}). 400)
