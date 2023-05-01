ae36bc48730d3432d2a20d2b0d3a35282adc76d3
attahchisom6 <attahchisom6@gmail.com>
2023-05-01 11:28:25 +0100

    debugging

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """
 A new view for state objects that handles all RESTFul api
 """
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -34,9 +34,25 @@ def get_a_state(state_id):
                  methods=['DELETE'], strict_slashes=False)
 def delete_a_state(state_id):
     """deletes a state objects"""
-    state = storage.get(State, state_id)
+    state = storage.get('State', state_id)
     if state is None:
         abort(404)
     state.delete()
     storage.save()
     return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', method=['POST'], strict_slashes=False)
+def create_obj():
+    """
+    Create an instance of the state object
+    """
+    if not request.get_json():
+        return jsonify({"error": "Not a Json"})
+    if "name" not in request.get_json():
+        return Jsonify({"error": "Mising name"}), 400
+
+    js = request.get_json()
+    state = State(**js)
+    state.save()
+    return jsonify(state.to_dict()), 201
