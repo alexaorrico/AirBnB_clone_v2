fb7b50b2c9564cb216e91527535a3e39eab55a69
justo340 <justusnjogu16@gmail.com>
2023-05-01 06:37:14 -0700

    fix api failing

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,7 +5,7 @@ Contains the TestFileStorageDocs classes
 
 from datetime import datetime
 import inspect
-#import models
+import models
 from models.engine import file_storage
 from models.amenity import Amenity
 from models.base_model import BaseModel
