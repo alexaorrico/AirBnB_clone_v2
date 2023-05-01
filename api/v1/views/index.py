c62beb4d8fb5b9998f9271cd69c9cf0f2ea10fee
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-05-01 23:19:42 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-"""This module implements a rule that returns the status of the application"""
-from flask import jsonify
-import models
-from api.v1.views import app_views
+""" Index """
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route("/status", strict_slashes=False)
-def view_status():
-    """View function that return a json message"""
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
-def view_stats():
-    """Veiw function that retrieves the number of each object by type"""
-    return jsonify({
-        "amenities": models.storage.count(Amenity),
-        "cities": models.storage.count(City),
-        "places": models.storage.count(Place),
-        "reviews": models.storage.count(Review),
-        "states": models.storage.count(State),
-        "users": models.storage.count(User)
-    })
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
