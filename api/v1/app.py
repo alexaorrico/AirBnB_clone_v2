ba792be4c5cb8f2fd0bccf00932c0bf9df1825d3
Oluwatobi <amure387@gmail.com>
2023-05-01 08:45:26 +0000

    fix: task 3 pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def destro(error):
     '''method to handle teardown'''
@@ -16,8 +17,14 @@ def destro(error):
 
 @app.errorhandler(404)
 def not_found(error):
+    '''Method for handling errors'''
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
-    my_host = os.getenv("HBNB_API_HOST") if os.getenv("HBNB_API_HOST") else '0.0.0.0'
-    my_port = os.getenv("HBNB_API_PORT") if os.getenv("HBNB_API_PORT") else '5000'
+    '''Main function'''
+    my_host = os.getenv("HBNB_API_HOST") if os.getenv(
+        "HBNB_API_HOST") else '0.0.0.0'
+    my_port = os.getenv("HBNB_API_PORT") if os.getenv(
+        "HBNB_API_PORT") else '5000'
     app.run(host=my_host, port=my_port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''Index module for status'''
 from flask import jsonify
 from models import storage
 from api.v1.views import app_views
@@ -14,11 +15,11 @@ def app_stat():
 def app_stats():
     '''retrieves the number of each objects by type'''
     stats = {
-  "amenities": storage.count('Amenity'),
-  "cities": storage.count('City'),
-  "places": storage.count('Place'),
-  "reviews": storage.count('Review'),
-  "states": storage.count('State'),
-  "users": storage.count('User')
-}
+             "amenities": storage.count('Amenity'),
+             "cities": storage.count('City'),
+             "places": storage.count('Place'),
+             "reviews": storage.count('Review'),
+             "states": storage.count('State'),
+             "users": storage.count('User')
+            }
     return jsonify(stats)
