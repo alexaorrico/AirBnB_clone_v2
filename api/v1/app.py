c205dafedf6672c23e80f8f6f3d60c3484b7b024
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-05-01 15:56:13 +0300

    4 some stats

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,24 @@
-<<<<<<< HEAD
-# api/v1/app.py
-
-=======
 #!/usr/bin/python3
 """
-
+app
 """
->>>>>>> storage_get_count
 import os
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
-<<<<<<< HEAD
-=======
+
 # Register the app_views bluepint to the app
->>>>>>> storage_get_count
+
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 @app.teardown_appcontext
 def close_storage(exception):
-<<<<<<< HEAD
-    storage.close()
-
-=======
     """Close the storage connection at the end of the request"""
     storage.close()
 
 
->>>>>>> storage_get_count
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
-""" api/vi/views/index.py"""
+"""Defines the API routes for the Flask app"""
 
+from flask import jsonify, Blueprint
 from api.v1.views import app_views
-from flask import jsonify
+from models import storage
 
-@app_views.route('/status', methodes=['GET'])
-def status():
+
+app_views = Blueprint('app_views', __name__)
+
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Returns the status of the API"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Returns the number of objects in the data store"""
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
+    }
+    return jsonify(stats)
