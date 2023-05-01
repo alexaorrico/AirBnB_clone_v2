68d88cdf2b704ca960cf44c40de01c6fc93075ec
LadyDammy <oreoluwadammie@gmail.com>
2023-05-01 16:06:10 -0700

    update file and dbstorage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
-
-""" This module contains the routes for the web application
-"""
-
+"""index"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def get_status():
-    """ status code"""
-    return jsonify(status="ok")
+@app_views.route('/status', methods=['GET'])
+def status():
+    ''' routes to status page '''
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    '''retrieves the number of each objects by type'''
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
