529e65c7952e2ce4db5ff3c5dbf62825d6ada307
Daniel-Uchay <meetcaxtroh@gmail.com>
2023-05-01 09:05:56 +0100

    ok

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,50 @@
 #!/usr/bin/python3
-""" Provide high level structure and rules """
-from flask import Flask, Blueprint, jsonify
-from models import storage
-from api.v1.views import app_views
-from os import getenv
+'''Contains a Flask web application API.
+'''
+import os
+from flask import Flask, jsonify
 from flask_cors import CORS
 
+from models import storage
+from api.v1.views import app_views
+
+
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.register_blueprint(app_views, url_prefix='/api/v1')
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """ teardown the app context """
+def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
 
 @app.errorhandler(404)
 def error_404(error):
-    """ return a json 404 error """
-    return jsonify({"error": "Not found"}), 404
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
 
 
-if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=getenv('HBNB_API_PORT', '5000'), threaded=True)
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
+
+
+if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
