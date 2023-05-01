1a45d6cbb221e395b6b7758dbf524379c134a461
sethdanny <naddulidaniel1994@gmail.com>
2023-05-01 09:34:23 -0700

    fix pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,15 +11,16 @@ def status():
     """return json object status"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     """ retrieves the number of each objects by type """
     my_dicts = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User")
-            }
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
     return jsonify(my_dicts)
