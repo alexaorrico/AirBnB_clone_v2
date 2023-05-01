7af682eec48c453757a5ac12db17b09b5bdff7ef
Thulisile Ngomane <thuleen@outlook.com>
2023-05-02 01:14:47 +0200

    states file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,17 @@ from api.v1.views import app_views
     """function to return api status"""
 
         return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def obj_stats():
+    """returns the number of each object"""
+
+    my_dict = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(my_dict)
