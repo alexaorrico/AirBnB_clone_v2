bfbacb0850cb8f6a1c08a2927c4dd652a97b5659
loyd ngei <loydngei@gmail.com>
2023-05-01 21:34:11 +0300

    Updated places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,6 +7,7 @@ from flask import request, jsonify, abort
 from models.city import City
 from models import storage
 from models.place import Place
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places',
