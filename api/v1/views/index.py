3eedd007a3a15de06b4b05bba9dcb6acb1e3e9d7
chineduCoded <chinedujohn172gmail.com>
2023-05-01 14:55:19 -0700

    Displays API status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,19 +16,3 @@ def status():
         return JSON of OK status
     """
     return jsonify({'status': 'OK'})
-
-
-"""@app_views.route("/stats")
-def storage_counts():
-    """
-        return counts of all classes in storage
-    """
-    cls_counts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(cls_counts)"""
