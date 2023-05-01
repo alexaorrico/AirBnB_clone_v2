c1441e55097a3d96b56e6da4a6ed2d70695b38cb
titayi <kitayimike@gmail.com>
2023-05-01 08:40:48 +0000

    Task solution updates

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,74 +1,57 @@
 #!/usr/bin/python3
-"""State module"""
+"""New view for City objects, handles all default RESTFul API actions"""
+
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
-from models import storage
+from flask import request, jsonify, abort
 from models.state import State
+from models import storage
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
-    """
-    Retrieve the list of all states
-    """
-    states = storage.all(State).values()
-    return jsonify([state.to_dict() for state in states])
+@app_views.route('/states',
+                 methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>',
+                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def states(state_id=None):
+    """Retrieves a list of state objects"""
 
+    state_objs = storage.all(State)
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state_id(state_id=None):
-    """
-    Retrieve the state by id
-    """
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    return jsonify(state.to_dict())
+    states = [obj.to_dict() for obj in state_objs.values()]
+    if not state_id:
+        if request.method == 'GET':
+            return jsonify(states)
+        elif request.method == 'POST':
+            my_dict = request.get_json()
 
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            if my_dict.get("name") is None:
+                abort(400, 'Missing name')
+            new_state = State(**my_dict)
+            new_state.save()
+            return jsonify(new_state.to_dict()), 201
+    else:
+        if request.method == 'GET':
+            for state in states:
+                if state.get('id') == state_id:
+                    return jsonify(state)
+            abort(404)
+        elif request.method == 'PUT':
+            my_dict = request.get_json()
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_state(state_id=None):
-    """
-    Delete state with the diven id
-    """
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    storage.delete(state)
-    storage.save()
-    return jsonify({}), 200
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            for state in state_objs.values():
+                if state.id == state_id:
+                    state.name = my_dict.get("name")
+                    state.save()
+                    return jsonify(state.to_dict()), 200
+            abort(404)
 
-
-@app_views.route('/states', methods=['POST'],
-                 strict_slashes=False)
-def create_state():
-    """
-    Create a new state from the json request
-    """
-    if not request.json:
-        abort(400, "Not a JSON")
-    if 'name' not in request.json:
-        abort(400, "Missing name")
-    state = State(**request.json)
-    storage.new(state)
-    storage.save()
-    return jsonify(state.to_dict()), 201
-
-
-@app_views.route('/states/<state_id>', methods=['PUT'],
-                 strict_slashes=False)
-def update_state(state_id):
-    """
-    Update the state of the given id
-    """
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    if not request.json:
-        abort(400, "Not a JSON")
-    for key, value in request.json.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(state, key, value)
-    storage.save()
-    return jsonify(state.to_dict()), 200
+        elif request.method == 'DELETE':
+            for obj in state_objs.values():
+                if obj.id == state_id:
+                    storage.delete(obj)
+                    storage.save()
+                    return jsonify({}), 200
+            abort(404)
