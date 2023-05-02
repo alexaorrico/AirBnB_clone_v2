77566b6b0c6711b2889f5d0ff3e0ebbe420a6320
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-05-02 22:18:30 +0100

    update user.py and basemodel.py to store hashed password

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ holds class User"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -13,17 +14,33 @@ class User(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        _password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
         reviews = relationship("Review", backref="user")
     else:
         email = ""
-        password = ""
+        _password = ""
         first_name = ""
         last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        if kwargs:
+            if len(kwargs['password']) != 32:
+                self._password = hashlib.md5(
+                     kwargs['password'].encode('utf8')).hexdigest()
+            else:
+                self._password = kwargs['password']
+
+    @property
+    def password(self):
+        """ getter to return hashed password """
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        """ hashes the password """
+        self._password = hashlib.md5(value.encode('utf8')).hexdigest()
