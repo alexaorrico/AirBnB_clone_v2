6011b5ffd5949acd406e9a928a37916b6dc7d6ac
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-05-03 22:53:06 +0300

    Arrange the imports

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,9 +3,9 @@
 Module for Amenity objects that handles all default Restful API actions
 """
 from api.v1.views import app_views
+from flask import jsonify, abort, request
 from models import storage
 from models.amenity import Amenity
-from flask import jsonify, abort, request
 
 
 @app_views.route('/amenities', methods=['GET'])
