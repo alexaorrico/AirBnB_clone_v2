088a59a1746d6bfd4881b65c9c05cd3b55d48719
Mre-Dennis-kinuthia <denniskinuthia1105@gmail.com>
2023-05-03 22:50:07 +0300

    Arrange the imports

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,8 @@ from models.state import State
 from models import storage
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
 def get_cities(state_id):
     """Retrieves the list of all City objects of a State"""
     state = storage.get(State, state_id)
@@ -38,7 +39,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def post_city(state_id):
     """Creates a City"""
     state = storage.get(State, state_id)
