8b526697427f228aa190d0cfc742ab9ca0358b69
KrizTech <christiandonatus01@gmail.com>
2023-05-02 18:13:42 -0700

    Updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
-"""main app file for Flask instance in REST API
-"""
-from flask import Flask
-from flask import jsonify
-from flask_cors import CORS
-from models import storage
+"""AirBnB Clone API config file"""
+
 from api.v1.views import app_views
-import os
+from flask import Flask, make_response, jsonify
+from models import storage
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
-
-
-def page_not_found(e):
-    """404 error json response"""
-    return jsonify({'error': "Not found"}), 404
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exc=None):
-    """called on teardown of app contexts of flask
+def teardown_app(exception):
+    """
+    disconnects and ends the db session
+    at the end a request
     """
     storage.close()
 
 
-if __name__ == "__main__":
-    """run the app if the script is not being imported
+@app.errorhandler(404)
+def not_found(error):
     """
-    app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-    app.register_error_handler(404, page_not_found)
-    fetched_host = os.environ.get('HBNB_API_HOST')
-    fetched_port = os.environ.get('HBNB_API_PORT')
-    if fetched_host is None:
-        fetched_host = '0.0.0.0'
-    if fetched_port is None:
-        fetched_port = 5000
-    app.run(host=fetched_host, port=fetched_port, threaded=True)
+    Handle non existing pages
+
+    Args:
+    error: [description]
+
+    Returns:
+    JSON: json object
+    """
+
+    e = {
+        "error": "Not Found"
+    }
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,51 @@
 #!/usr/bin/python3
-"""index file, main view file
-"""
-from api.v1.views import app_views
-from flask import jsonify
+"""API index views module"""
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
+from api.v1.views import app_views
+from models import storage
+from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
-def service_status():
-    """returns the status of the RESTful service"""
-    """ TODO check if this formatting is okay for json response with
-        holberton checker """
-    return jsonify({'status': 'OK'})
+@app_views.route('/status')
+def status():
+    """
+    Returns json response as the status
+
+    Returns:
+        JSON: json object
+    """
+    status = {
+        "status": "OK"
+    }
+    return jsonify(status)
 
 
 @app_views.route('/stats', strict_slashes=False)
-def object_stats():
-    """returns the count of object types"""
+def count():
+    """
+    returns a count of all database objects
+    """
 
-    classes = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User"
-        }
-    for k, v in classes.items():
-        classes[k] = storage.count(v)
-    return jsonify(classes)
+    models_avail = {
+        "User": "users",
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+    }
+    mod_objs = [Amenity, City, Place, Review, State, User]
+
+    count = {}
+    i = -1
+    for cls in models_avail.keys():
+        i += 1
+        count[models_avail[cls]] = storage.count(mod_objs[i])
+
+    return jsonify(count)
