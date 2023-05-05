ff4d5bc3ca0799417b86da79bd09b85a0d2e2380
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-05-05 12:26:04 +0100

    feature: access control allow origin

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,14 @@ hbnb airBnB clone
 
 from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
