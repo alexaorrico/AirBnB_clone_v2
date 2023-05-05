c6b25e0655107e6f1d22597e2433bccea830bfaf
Hamida Mahama <mahamahamida@gmail.com>
2023-05-05 15:26:10 +0000

    new

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods = ['GET'], strict_slashes=False)
 def status():
     """api status"""
     return jsonify({"status": "OK"})
