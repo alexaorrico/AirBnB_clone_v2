9536594f72e92d0153ff9f0be5a439818869d9ae
Cathyrt <cathrinert20@icloud.com>
2023-05-18 19:15:17 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,50 @@
 #!/usr/bin/python3
-"""
-This module contains the principal application
-"""
-from models import storage
-from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
-from os import getenv
+'''Contains a Flask web application API.
+'''
+import os
+from flask import Flask, jsonify
 from flask_cors import CORS
 
+from models import storage
+from api.v1.views import app_views
+
+
 app = Flask(__name__)
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
-def close_db(obj):
-    """ calls methods close() """
+def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_foun(error):
-    """ Loads a custom 404 page not found """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
 
 
-if __name__ == "__main__":
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
 
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
 
-    app.run(host, int(port), threaded=True)
+if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
