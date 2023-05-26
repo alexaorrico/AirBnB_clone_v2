1675f545e0da00ffd694c14bfbda65bec9fe95c3
Frankenstein9585 <donny9585@gmail.com>
2023-05-26 21:00:52 +0100

    to windows

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,7 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        class_dict = self.all(cls)
+        
