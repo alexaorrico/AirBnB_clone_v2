8124c89de00503fe336fe0b11caaf100d50876ff
vpnchengo <charo.alvin@gmail.com>
2023-05-26 23:22:47 +0000

    count instance

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,14 @@ from api.v1.views import app_views
 def get_status():
     """ checking the status of route """
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_of_objects():
+    """ Retrieves the number of each objects by type """
+    objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
+               "reviews": 'Review', "states": 'State', "users": 'User'}
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+    return jsonify(num_objs)
