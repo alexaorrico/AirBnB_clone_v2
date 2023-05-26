74d074494696451e5ca82abaa0ead860adebf21c
Samuel Senerwa <samuelsenerwa@gmail.com>
2023-05-26 16:38:32 -0700

    created an endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ Module contains status endpoint
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
@@ -13,3 +14,18 @@ def get_status():
     Endpoint to get API status
     """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def storage_counts():
+    '''
+    return all the counts of all the classes contain in storage
+    '''
+    cls_counts = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "place": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("Users")
+    }
+    return jsonify(cls_counts)
