7b7f144b3d5e8540922257cffb413f538f5e392b
vpnchengo <charo.alvin@gmail.com>
2023-05-27 01:23:00 +0000

    404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """flask application"""
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify, Blueprint, make_response
 from os import getenv
 from flask_cors import CORS
 
@@ -15,6 +15,12 @@ cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 def close_db(error):
     """ this for slash routing"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """handler for 404 errors that returns a JSON-formatted 404 status code"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
