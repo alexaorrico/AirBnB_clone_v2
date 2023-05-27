6c2d8df83ee0cd2df04b5c8e4eabbec749d27a9d
mugane-dj <muganes25@gmail.com>
2023-05-27 13:30:43 +0000

    CORS config

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 from api.v1.views import app_views
 
 app = Flask(__name__)
-CORS(app, origin="0.0.0.0")
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
