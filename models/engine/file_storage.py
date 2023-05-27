ba10d81b19f152efcb725dc72fdee548d3b42a36
root <root@Yusuf.localdomain>
2023-05-27 11:00:06 +0100

    added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,29 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+   
+    def get(self, cls, id):
+        """ Returns the object based on the class and its ID, or None if not found """
+
+        if type(cls) is str:
+            cls = eval(cls)
+
+        try:
+            obj = self.__session.query(cls).filter_by(id=id).one()
+        except Exception:
+            obj = None
+
+        return obj
+
+    def count(self, cls=None):
+        """ Returns the number of objects in storage """
+
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            objs = self.all(cls)
+        else:
+            objs = self.all()
+
+        return (len(objs))
