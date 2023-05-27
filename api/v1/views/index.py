41e7c3494bc79e406ecad81dc053cad1018a00fd
Slimanyy <aabdulqudduss@gmail.com>
2023-05-27 16:32:23 -0700

    amenties

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,16 @@ from api.v1.views import app_views
 def get_status():
     """Returns HTTP Json status 200"""
     return jsonify({"status": "OK"}), 200
+
+@app_views.route('/stats')
+def get_count():
+    """retrieves the number of each objects by type"""
+    stats = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    return jsonify({name: storage.count(cls) for cls, name in stats.items()})
