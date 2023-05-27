bd4ca3ae256447592e010362b396272896316bd9
vpnchengo <charo.alvin@gmail.com>
2023-05-27 00:18:20 +0000

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@ index
 """
 from flask import jsonify, Blueprint
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -17,7 +24,6 @@ def number_of_objects():
     """ Retrieves the number of each objects by type """
     objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
                "reviews": 'Review', "states": 'State', "users": 'User'}
-    num_objs = {}
-    for i in range(len(classes)):
-        num_objs[names[i]] = storage.count(classes[i])
-    return jsonify(num_objs)
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
