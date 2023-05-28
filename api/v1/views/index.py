14727370a4a7d06d573c33df1e2bdaff3b8c5bbb
GMDLULI <gugulethumdluli98@gmail.com>
2023-05-28 09:56:50 +0000

    retiveing number of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,25 @@
 #!/usr/bin/pyton3
 """ Index.py: return json content"""
 
+
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
 
 @app_views.route("/status")
 def status_ch(self):
     """ Checking status code """
-    return jsonify({"status": "OK"}) 
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats_text():
+    """Retrives number of each object by type"""
+    objects = ({"amenities": storage.count("Amenity"),
+                "cities": storage.count("City"),
+                "places": storage.count("Place"),
+                "reviews": storage.count("Review"),
+                "states": strorage.count("State"),
+                "users": storage.count("User")})
+    return jsonify(objects)
