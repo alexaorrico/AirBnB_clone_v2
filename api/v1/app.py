133371a5aba0db45442a87ec4b555f6e8918e6bd
GMDLULI <gugulethumdluli98@gmail.com>
2023-05-28 04:02:53 +0000

    creating instance of flask

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" app.py """
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os 
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear(self):
+    """tear down app.py"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    if os.getenv("HBNB_API_HOST") and os.getenv("HBNB_API_PORT"):
+        app.run(host=os.getenv("HBNB_API_HOST"),
+                port=os.getenv("HBNB_API_PORT"), threaded=True)
+    else:
+        app.run(host='0.0.0.0', port=5000, threaded=True)
