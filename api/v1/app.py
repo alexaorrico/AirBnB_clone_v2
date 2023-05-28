68b584507cb4db8e9f2ae9a2b489f3131da8f23e
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-28 15:09:21 +0100

    Stated the flask api, with routes(status, stats and one to check Not found errors)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """RESTful API module"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+
 app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Teardown"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == '__main__':
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+"""The module for app_views"""
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'])
 def get_api_status():
