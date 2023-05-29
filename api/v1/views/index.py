aa080a98782ae108b2759bfdda5996e14461a09d
sezfabian <cheruiyotfabian@gmail.com>
2023-05-29 15:46:29 +0300

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -11,3 +12,15 @@ def status():
     Returns a JSON status
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type:
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
