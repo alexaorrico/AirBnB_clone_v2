7c1236f23a856b60d25620caefbeffe21c0563b4
root <sideeqkolade@gmail.com>
2023-05-29 03:37:22 +0100

    task 1

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,16 @@ from flask import jsonify
 def status():
     """status of API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('stats', methods=['GET'], strict_slashes=False)
+def obj_num():
+    """retrieves the number of each objects by type"""
+    return jsonify({
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+        })
