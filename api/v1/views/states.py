0b1f0d39292fa07966b50e370c41750208d468c7
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-05-29 13:15:20 +0100

    update states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -61,9 +61,7 @@ def post_state():
     if 'name' not in request.get_json():
         abort(400, "Missing name")
     state_data = request.get_json()
-    state = State()
-    for key, value in state_data.items():
-        setattr(state, key, value)
+    state = State(**state_data)
     state.save()
     return make_response(jsonify(state.to_dict()), 201)
 
