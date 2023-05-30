7cce72a41105305ab132d45598898883736f01cc
Oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-30 11:46:14 +0100

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,13 +11,13 @@ def get_states():
     states = storage.all(State)
     states_list = []
     for state in states:
-        state_list.append(state.to_dict())
+        states_list.append(state.to_dict())
     return jsonify(states_list)
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state(state_id):
     """ get state """
-    state.storage.get(State, state_id)
+    state = storage.get(State, state_id)
     if state is None:
         return jsonify({'error': 'Not found'}), 404
     return jsonify(state.to_dict())
@@ -35,10 +35,32 @@ def delete_state(state_id):
 def create_state():
     """ create state """
     if not request.is_json:
-        return jsonify({'error': 'Not found'}), 404
-
+        abort(400, 'Not a JSON')
+    
     data = request.get_json()
     if 'name' not in data:
-        return jsonify({'error': 'Not found'}), 404
-
+        abort(400, 'Missing name')
+    
     state = State(**data)
+    storage.new(state)
+    storage.save()
+
+    return jsonify(state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    state =storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+
+    data = request.get_json()
+
+    ignored_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignored_keys:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
