fc9a2fd704402333fd70fca963d7751814f5f11d
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-05-30 01:44:18 +0100

    Resolved pycodestyle error and implemented the place views

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,102 +1,92 @@
 #!/usr/bin/python3
 """
 Module for view for Place objects
-It handles all default RESTFul API actions
+It handles all default RESTful API actions
 """
 
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from models import storage
 from models.place import Place
+from models.city import City
 
 
-@app_views.route('/places', methods=['GET'], strict_slashes=False)
-def get_places_list():
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+def get_places_list(city_id):
     """Retrieves the list of all Place objects"""
-    places_list = []
-    places = storage.all(Place).values()
-    for place in places:
-        places_list.append(place.to_dict())
-    return jsonify(places_list), 200
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    places_list = [place.to_dict() for place in city.places]
+    return jsonify(places_list)
 
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
-def get_places_obj(place_id):
-    """
-    Retrieves a Place object
-
-    Args:
-        place_id: The id of the place object
-    Raises:
-        404: if place_id supplied is not linked to any place object
-    """
-    place = storage.get(Place, place_id)
-    if place:
-        return jsonify(place.to_dict()), 200
-    else:
-        return jsonify({"error": "Not found"}), 404
-
-
-@app_views.route('/places/<place_id>',
-                 methods=['DELETE'],
-                 strict_slashes=False)
-def delete_places_obj(place_id):
-    """
-    Deletes a Place object
-
-    Args:
-        place_id: The id of the place object
-    Raises:
-        404: if place_id supplied is not linked to any place object
-    """
+def get_place(place_id):
+    """Retrieves a Place object"""
     place = storage.get(Place, place_id)
     if not place:
-        return jsonify({"error": "Not found"}), 404
+        abort(404)
+    return jsonify(place.to_dict())
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+def delete_place(place_id):
+    """Deletes a Place object"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
     storage.delete(place)
     storage.save()
-    return jsonify({}), 200
+    return jsonify({})
 
 
-@app_views.route('/places/', methods=['POST'], strict_slashes=False)
-def post_place():
-    """
-    Creates a Place object
+@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+def create_place(city_id):
+    """Creates a Place object"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
 
-    Returns:
-        The new Place with the status code 201
-    """
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+
     data = request.get_json()
-    if not data:
-        return jsonify({'error': 'Not a JSON'}), 400
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+
+    user = storage.get(User, data['user_id'])
+    if not user:
+        abort(404)
+
     if 'name' not in data:
-        return jsonify({'error': 'Missing name'}), 400
+        abort(400, 'Missing name')
+
+    data['city_id'] = city_id
     place = Place(**data)
-    place.save()
+    storage.new(place)
+    storage.save()
+
     return jsonify(place.to_dict()), 201
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
-    """
-    Updates a Place object
-
-    Args:
-        place_id: The id of the Place object
-    Raises:
-        404:
-            If place_id supplied is not linked to any Place object
-            400: If the HTTP body request is not valid JSON
-    """
+    """Updates a Place object"""
     place = storage.get(Place, place_id)
     if not place:
-        return jsonify({'error': 'Not found'}), 404
+        abort(404)
+
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+
     data = request.get_json()
-    if not data:
-        return jsonify({'error': 'Not a JSON'}), 400
-    data.pop('id', None)
-    data.pop('created_at', None)
-    data.pop('updated_at', None)
+    ignored_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+
     for key, value in data.items():
-        setattr(place, key, value)
-    place.save()
-    return jsonify(place.to_dict()), 200
+        if key not in ignored_keys:
+            setattr(place, key, value)
+
+    storage.save()
+
+    return jsonify(place.to_dict())
