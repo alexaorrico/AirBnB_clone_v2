23d5206fde496642605c8dc004db29801dfd541f
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-30 17:09:27 +0000

    Version Update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3xx
-'''api status'''
+#!/usr/bin/python3
+"""api status"""
 import models
 from models import storage
 from models.base_model import BaseModel
@@ -9,16 +9,16 @@ from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def returnstuff():
-    '''return stuff'''
+    """return stuff"""
     return jsonify(status='OK')
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stuff():
-    '''JSON Responses'''
+    """JSON Responses"""
     todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
+             'amenities': Amenity, 'cities': City,
+             'places': Place, 'reviews': Review}
     for key in todos:
         todos[key] = storage.count(todos[key])
     return jsonify(todos)
