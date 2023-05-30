4416777f1087c46cd492a719ac4fdde1fb0ca7cd
GMDLULI <gugulethumdluli98@gmail.com>
2023-05-30 20:49:50 +0000

    index fixes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" app.py """
+''' Creates an instance of flask '''
 
 from flask import Flask, make_response, jsonify
 from models import storage
@@ -7,18 +7,19 @@ from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """teardown app.py"""
+def tear_d(self):
+    ''' Tears down app.py '''
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """ handles 404 errors"""
+    ''' Returns error 404'''
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
