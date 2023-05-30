d310857f5541e822386d82ebb806a9fd24e6b27f
ngwanatuka <ngwanatuka@gmail.com>
2023-05-30 02:28:19 +0200

    adding an end point that retrieves the number of each object

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,22 @@
 """index of a web flack app"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def  get_stats():
+    stats = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(stats)
