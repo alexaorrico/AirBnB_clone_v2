6cb20f46b7469c28d98bfe335867da3f660e6e98
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-30 17:40:30 +0000

    Version Update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,13 +8,13 @@ from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    """return stuff"""
+def returnstatus():
+    """return status code"""
     return jsonify(status='OK')
 
 
 @app_views.route('/stats', strict_slashes=False)
-def stuff():
+def stats():
     """JSON Responses"""
     todos = {'states': State, 'users': User,
              'amenities': Amenity, 'cities': City,
