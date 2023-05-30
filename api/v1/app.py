920225faa4a97ca75eb9737d082388de95f68e6b
Oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-30 15:48:39 +0100

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,10 @@
 Flask App
 """
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 
 """ Flask App """
 app = Flask(__name__)
@@ -19,6 +20,8 @@ port = os.getenv('HBNB_API_PORT', 5000)
 
 # app_views blueprint
 app.register_blueprint(app_views)
+
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 # close storage after rendering
 @app.teardown_appcontext
