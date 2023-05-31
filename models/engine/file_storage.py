6a6eb9a9c6d3f1b44f1de2ca07ab075562c81104
vpnchengo <charo.alvin@gmail.com>
2023-05-31 18:37:21 +0000

    correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -34,6 +34,20 @@ class FileStorage:
             return new_dict
         return self.__objects
 
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
+
     def get(self, cls, id):
         """method to retrieve one object:
         Returns the object based on the class and its ID, or None if not found
@@ -57,21 +71,8 @@ class FileStorage:
             for clas in all_clss:
                 count += len(models.storage.all(clas).values())
         count = len(models.storage.all(cls).values())
+
         return count
-
-    def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
-            self.__objects[key] = obj
-
-    def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        json_objects = {}
-        for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
-            json.dump(json_objects, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
