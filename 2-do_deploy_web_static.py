8af29ee528d6dc5329effd60470863fb78877cb8
solomongegziabher <sage.h22@gmail.com>
2023-06-03 21:27:05 +0000

    AirBnB

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,3 +28,4 @@ def do_deploy(archive_path):
         return True
     except:
         return False
+
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -50,3 +50,4 @@ def deploy():
     if archive_path is None:
         return False
     return do_deploy(archive_path)
+
