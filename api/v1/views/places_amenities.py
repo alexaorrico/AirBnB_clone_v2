48a40892ae01ac7c91c57bc0c933dd8a84be81b6
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-06-15 23:16:07 +0100

    created module that houses the view for the link between Place objects and Amenity objects

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Module that houses the view for the link between
+Place objects and Amenity objects
+
+It handles all default RESTFul API actions
+"""
+
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+
+
+@app_views.route('/places/<place_id>/amenities',
+                 methods=['GET'], strict_slashes=False)
+def get_place_amenities(place_id):
+    """Retrieves the list of all Amenity objects of a Place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenities_list = [amenity.to_dict() for amenity in place.amenities]
+    return jsonify(amenities_list)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    """Deletes an Amenity object from a Place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+def link_place_amenity(place_id, amenity_id):
+    """Links an Amenity object to a Place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if amenity in place.amenities:
+        return jsonify(amenity.to_dict()), 200
+    place.amenities.append(amenity)
+    storage.save()
+    return jsonify(amenity.to_dict()), 201
