8f13b21670f6548fc0f0e9883236f344549e7e72
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-06-18 09:27:48 -0400

    Correcting docstring errors

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-""" holds class City"""
+"""Hold class City."""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -9,7 +9,8 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
+    """Representation of city."""
+
     if models.storage_t == "db":
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
@@ -20,5 +21,5 @@ class City(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes city"""
+        """Initialize city."""
         super().__init__(*args, **kwargs)
