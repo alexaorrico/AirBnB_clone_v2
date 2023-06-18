071b9abe559798971c7ca7a87fe9d070b68287d3
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-06-18 09:51:53 -0400

    Adding ending points to docstrings

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains the TestAmenityDocs classes
-"""
+"""Contains the TestAmenityDocs classes."""
 
 from datetime import datetime
 import inspect
@@ -10,14 +8,15 @@ from models import amenity
 from models.base_model import BaseModel
 import pep8
 import unittest
+
 Amenity = amenity.Amenity
 
 
 class TestAmenityDocs(unittest.TestCase):
-    """Tests to check the documentation and style of Amenity class"""
+    """Tests to check the documentation and style of Amenity class."""
     @classmethod
     def setUpClass(cls):
-        """Set up for the doc tests"""
+        """Set up for the doc tests."""
         cls.amenity_f = inspect.getmembers(Amenity, inspect.isfunction)
 
     def test_pep8_conformance_amenity(self):
@@ -35,21 +34,21 @@ class TestAmenityDocs(unittest.TestCase):
                          "Found code style errors (and warnings).")
 
     def test_amenity_module_docstring(self):
-        """Test for the amenity.py module docstring"""
+        """Test for the amenity.py module docstring."""
         self.assertIsNot(amenity.__doc__, None,
                          "amenity.py needs a docstring")
         self.assertTrue(len(amenity.__doc__) >= 1,
                         "amenity.py needs a docstring")
 
     def test_amenity_class_docstring(self):
-        """Test for the Amenity class docstring"""
+        """Test for the Amenity class docstring."""
         self.assertIsNot(Amenity.__doc__, None,
                          "Amenity class needs a docstring")
         self.assertTrue(len(Amenity.__doc__) >= 1,
                         "Amenity class needs a docstring")
 
     def test_amenity_func_docstrings(self):
-        """Test for the presence of docstrings in Amenity methods"""
+        """Test for the presence of docstrings in Amenity methods."""
         for func in self.amenity_f:
             self.assertIsNot(func[1].__doc__, None,
                              "{:s} method needs a docstring".format(func[0]))
@@ -58,9 +57,10 @@ class TestAmenityDocs(unittest.TestCase):
 
 
 class TestAmenity(unittest.TestCase):
-    """Test the Amenity class"""
+    """Test the Amenity class."""
+
     def test_is_subclass(self):
-        """Test that Amenity is a subclass of BaseModel"""
+        """Test that Amenity is a subclass of BaseModel."""
         amenity = Amenity()
         self.assertIsInstance(amenity, BaseModel)
         self.assertTrue(hasattr(amenity, "id"))
@@ -68,7 +68,7 @@ class TestAmenity(unittest.TestCase):
         self.assertTrue(hasattr(amenity, "updated_at"))
 
     def test_name_attr(self):
-        """Test that Amenity has attribute name, and it's as an empty string"""
+        """Test that Amenity has attribute name, and it's as an empty string."""
         amenity = Amenity()
         self.assertTrue(hasattr(amenity, "name"))
         if models.storage_t == 'db':
@@ -77,7 +77,7 @@ class TestAmenity(unittest.TestCase):
             self.assertEqual(amenity.name, "")
 
     def test_to_dict_creates_dict(self):
-        """test to_dict method creates a dictionary with proper attrs"""
+        """test to_dict method creates a dictionary with proper attrs."""
         am = Amenity()
         print(am.__dict__)
         new_d = am.to_dict()
@@ -89,7 +89,7 @@ class TestAmenity(unittest.TestCase):
         self.assertTrue("__class__" in new_d)
 
     def test_to_dict_values(self):
-        """test that values in dict returned from to_dict are correct"""
+        """test that values in dict returned from to_dict are correct."""
         t_format = "%Y-%m-%dT%H:%M:%S.%f"
         am = Amenity()
         new_d = am.to_dict()
@@ -100,7 +100,7 @@ class TestAmenity(unittest.TestCase):
         self.assertEqual(new_d["updated_at"], am.updated_at.strftime(t_format))
 
     def test_str(self):
-        """test that the str method has the correct output"""
+        """test that the str method has the correct output."""
         amenity = Amenity()
         string = "[Amenity] ({}) {}".format(amenity.id, amenity.__dict__)
         self.assertEqual(string, str(amenity))
