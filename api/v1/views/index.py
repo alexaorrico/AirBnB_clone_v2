e5accd4ee2d2a9b03fc64070f7593b4ed0eb92f0
Enyel019 <el_dominic01@hotmail.com>
2023-06-24 09:36:54 -0400

    new update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Index module."""
+"""
+Index module.
+
+"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -13,4 +16,15 @@ def get_status():
     return jsonify(status="ok")
 
 
-
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    """Retrieve the number of each objects by type."""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
