b8f8e1366189c0c68988ad83684681fa92e1a192
Enyel019 <el_dominic01@hotmail.com>
2023-06-24 09:35:25 -0400

    new update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,15 +13,4 @@ def get_status():
     return jsonify(status="ok")
 
 
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    """Retrieve the number of each objects by type."""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(stats)
+
