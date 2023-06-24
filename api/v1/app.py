58be34de40ada88eee1fe5281c3e2e3376db4d2c
Enyel019 <el_dominic01@hotmail.com>
2023-06-23 21:16:18 -0400

    4. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""It’s time to start your API!."""
+
+from models import storage
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    """Is function "close_db" takes an error as input and does not have any\
+    code implemented within it."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", threaded=True)
