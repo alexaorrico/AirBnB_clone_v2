88755a9dcaed6f513f887a23d254b9d9221a9cee
JimmyHernandez <jimoem24@gmail.com>
2023-06-29 18:59:21 -0400

    second commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""It’s time to start your API!."""
-
+"""app.py to connect to API"""
+import os
 from models import storage
-from flask import Flask, jsonify
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_db(exception):
-    """Is function "close_db" takes an error as input and does not have any\
-    code implemented within it."""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Is description: a resource was not found."""
-    return jsonify({'error': "Not found"}), 404
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
