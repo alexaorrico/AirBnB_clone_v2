6b67d444e5464309026ee24b9ad7aebc444d71c4
Enyel019 <el_dominic01@hotmail.com>
2023-07-01 11:16:56 -0400

    new update

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -15,7 +15,10 @@ def get_reviews_by_place(place_id):
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-    return jsonify([review.to_dict() for review in place.reviews])
+    reviews = storage.all(Review)
+    reviews_by_place = [review.to_dict() for review in reviews.values()
+                        if review.place_id == place_id]
+    return jsonify(reviews_by_place)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
@@ -46,24 +49,19 @@ def create_review(place_id):
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-
-    if not request.get_json():
-        abort(400, "Not a JSON")
-
     data = request.get_json()
-    if "user_id" not in data:
+    if not data:
+        abort(400, "Not a JSON")
+    user_id = data.get("user_id")
+    if not user_id:
         abort(400, "Missing user_id")
-
-    user_id = data["user_id"]
     user = storage.get(User, user_id)
     if not user:
         abort(404)
-
-    if "text" not in data:
+    text = data.get("text")
+    if not text:
         abort(400, "Missing text")
-
-    review = Review(**data)
-    review.place_id = place_id
+    review = Review(user_id=user_id, place_id=place_id, text=text)
     storage.new(review)
     storage.save()
     return jsonify(review.to_dict()), 201
@@ -75,16 +73,12 @@ def update_review(review_id):
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
-
-    if not request.get_json():
-        abort(400, "Not a JSON")
-
     data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
     ignore_keys = ["id", "user_id", "place_id", "created_at", "updated_at"]
-
     for key, value in data.items():
         if key not in ignore_keys:
             setattr(review, key, value)
-
     storage.save()
     return jsonify(review.to_dict()), 200
