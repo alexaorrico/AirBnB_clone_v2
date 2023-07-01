ea2956de083909707920200fd059b4089535e7b4
Enyel019 <el_dominic01@hotmail.com>
2023-07-01 14:29:24 -0400

    new update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,7 +42,8 @@ class FileStorage:
 
     def save(self):
         """Serialize __objects to the JSON file (path: __file_path)."""
-        json_objects = {key: self.__objects[key].to_dict() for key in self.__objects}
+        json_objects = {key: self.__objects[key].to_dict()
+                        for key in self.__objects}
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
