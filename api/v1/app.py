2dedd33e25a01b99c904f13b1e303a22fcb6ac01
OmarElati <5796@holbertonstudents.com>
2023-07-20 07:39:25 +0200

    file Upadte

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,45 @@
 #!/usr/bin/python3
 """
-Module: app
+app
 """
-from flask import Flask, make_response, jsonify
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
+
 from api.v1.views import app_views
 from models import storage
-from os import getenv
-from flask import jsonify
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """ close storage session """
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_404(error):
-    """ returns the 404 request error in JSON format """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
 
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
 
 if __name__ == "__main__":
-    _host = getenv('HBNB_API_HOST', '0.0.0.0')
-    _port = getenv('HBNB_API_PORT', 5000)
-    app.run(host=_host, port=_port)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
