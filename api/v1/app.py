de42f5329bc3f6829670e08c535295bba245c49d
OmarElati <5796@holbertonstudents.com>
2023-07-20 06:18:34 +0200

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,62 +1,32 @@
 #!/usr/bin/python3
 """
-app
-
-This module contains the main Flask application for the API.
+Module: app
 """
-
-from flask import Flask, jsonify
-from flask_cors import CORS
-from os import getenv
-
+from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
 from models import storage
-
+from os import getenv
+from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown function
-
-    This function is called when application context is torn down. It closes
-    the database storage engine to free up resources.
-
-    Args:
-        exception (Exception): The exception that caused the teardown.
-    """
+def teardown(self):
+    """ close storage session """
     storage.close()
 
 
 @app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-
-    This function is called when a 404 error occurs. It returns a JSON response
-    with an "error" key containing message "Not found" and status code of 404.
-
-    Args:
-        exception (Exception): The exception that caused the 404 error.
-
-    Returns:
-        Response: A JSON response with the error message and status code 404.
-    """
-    data = {
-        "error": "Not found"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return resp
+def handle_404(error):
+    """ returns the 404 request error in JSON format """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    _host = getenv('HBNB_API_HOST', '0.0.0.0')
+    _port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=_host, port=_port)
