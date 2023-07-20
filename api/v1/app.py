6a62703218cea78aa4a82ea203727801d274d26d
Nomad-Rob <rfarley89@gmail.com>
2023-07-20 10:59:08 -0500

    pep8 changes on documenting

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ returns status of API """
 
+
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """script to create routes"""
+
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
