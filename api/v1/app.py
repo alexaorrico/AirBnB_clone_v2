355b9f6d22420a70f54aa18a613861cacdea0477
OmarElati <5796@holbertonstudents.com>
2023-07-20 07:25:28 +0200

    file Upadte

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 """
-Module: app.
+Module: app
 """
-
-import os
-from flask import Flask, jsonify
-from models import storage
+from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
+from models import storage
+from os import getenv
+from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown."""
+def teardown(self):
+    """ close storage session """
     storage.close()
 
 
+@app.errorhandler(404)
+def handle_404(error):
+    """ returns the 404 request error in JSON format """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    _host = getenv('HBNB_API_HOST', '0.0.0.0')
+    _port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=_host, port=_port)
