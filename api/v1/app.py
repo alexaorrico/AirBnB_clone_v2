306fd0a7cbdfe761a52de51bea73360f67c50f2b
OmarElati <5796@holbertonstudents.com>
2023-07-20 05:17:07 +0200

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python3
-import sys
-import traceback
-from importlib import import_module
+"""
+app
+"""
+
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
+
+app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    teardown function
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 module_documented.py <module_name>")
-        sys.exit(1)
-
-    module_name = sys.argv[1]
-
-    try:
-        m_imported = import_module(module_name)
-        print("OK")
-    except ImportError as e:
-        print(f"Error: Unable to import module '{module_name}'.")
-        print(f"Reason: {e}")
-        traceback.print_exc()
-        sys.exit(1)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
