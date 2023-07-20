c1c84b2f3c3d3d4d93a55ccc3b8dea74590a788d
OmarElati <5796@holbertonstudents.com>
2023-07-20 05:37:54 +0200

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python3
 """
-    app.py
+app
 """
 
-from flask import Flask
-from models import storage
+from flask import Flask, jsonify
+from flask_cors import CORS
+from os import getenv
+
 from api.v1.views import app_views
-import os
+from models import storage
 
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage engine"""
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port, threaded=True)
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
