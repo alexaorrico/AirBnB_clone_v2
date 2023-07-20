461cb0cc8fc09becffc926a165d7f40247fccf42
OmarElati <5796@holbertonstudents.com>
2023-07-20 05:53:00 +0200

    Update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,16 +7,17 @@ import os
 
 app = Flask(__name__)
 
-"""Register the blueprint app_views to the Flask instance app"""
-app.register_blueprint(app_views)
+def register_blueprints(app):
+    """Register the blueprint app_views to the Flask instance app."""
+    app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):
-    """Closes the storage engine"""
+    """Closes the storage engine when the application context is torn down."""
     storage.close()
 
 if __name__ == "__main__":
-    """Set default values for host and port if not defined in the environment"""
+    """Set default values for host and port if not defined in the environment."""
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True)
