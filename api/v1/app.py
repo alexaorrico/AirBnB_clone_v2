7cf298477194c0b1cb3d519ec7db064ba9870f59
OmarElati <5796@holbertonstudents.com>
2023-07-22 01:51:28 +0200

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 
 
@@ -23,5 +23,4 @@ def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    app.run(host='127.0.0.1', port=int(os.getenv('HBNB_API_PORT', '5000')), threaded=True)
