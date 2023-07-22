3b5d2874c5e4e2399bf8fb66514748e999952ec6
Ben Harper <benjaminitonio@gmail.com>
2023-07-22 14:17:09 -0500

    task 5: Created an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """ this is the index.py file """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def check_status():
     """ returns dict status: OK """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def class_stats():
+    """ return counts of class instances """
+    counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(counts)
