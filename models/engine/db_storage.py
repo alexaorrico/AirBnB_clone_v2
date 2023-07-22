e30c4e51efecf0e25431013443349fa146e39082
christhecoolguy <christopherdiggs1@gmail.com>
2023-07-21 19:50:09 -0700

    pycach/dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,27 +70,37 @@ class DBStorage:
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
-        
-        def count(self, cls=None):
-        """Returns the number of objects
-        in storage matching the given class."""
-        new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    new_dict[key] = obj
-        return len(new_dict)
 
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-    
+
     def get(self, cls, id):
-        """Returns the number of objects
-        in storage matching the given class."""
-        if id is not None and cls is not None:
-            obj = self.__session.query(cls).get(id)
-            return obj
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+
         return None
+
+    def count(self, cls=None):
+        """
+        count the number of objects in storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
