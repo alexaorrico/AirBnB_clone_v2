9da4fb0b23adf42eb83a2c96e50be02546f4324b
OmarElati <5796@holbertonstudents.com>
2023-07-22 02:26:04 +0200

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,6 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
-from os import environ
 
 
 app = Flask(__name__)
@@ -24,11 +23,5 @@ def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host='127.0.0.1', port=int(os.getenv(
+        'HBNB_API_PORT', '5000')))
