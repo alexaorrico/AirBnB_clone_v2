076e184beac6ff012f1a453aa166481866fded62
christhecoolguy <christopherdiggs1@gmail.com>
2023-07-21 19:25:52 -0700

    updatedTestDB

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,46 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count_true(self):
+        """Test that count returns the correct count of objects"""
+        state1 = State(name="California")
+        state2 = State(name="New York")
+        city1 = City(name="San Francisco", state_id=state1.id)
+        city2 = City(name="New York City", state_id=state2.id)
+        models.storage.new(state1)
+        models.storage.new(state2)
+        models.storage.new(city1)
+        models.storage.new(city2)
+        models.storage.save()
+
+        # Check the count of objects for each class
+        count_states = models.storage.count(State)
+        count_cities = models.storage.count(City)
+        count_reviews = models.storage.count(Review)
+        count_amenities = models.storage.count(Amenity)
+
+        # Print the counts
+        print("Count of States:", count_states)
+        print("Count of Cities:", count_cities)
+        print("Count of Reviews:", count_reviews)
+        print("Count of Amenities:", count_amenities)
+
+        # Assert that the counts are accurate
+        self.assertTrue(count_states >= 2)
+        self.assertTrue(count_cities >= 2)
+        self.assertTrue(count_reviews >= 0)
+        self.assertTrue(count_amenities >= 0)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_true(self):
+        """Test that get returns the correct object based on class and ID"""
+        state = State(name="California")
+        models.storage.new(state)
+        models.storage.save()
+
+        # Get the state object from the database using the get method
+        state_obj = models.storage.get(State, state.id)
+
+        # Assert that the retrieved object is the same as the original object
+        self.assertEqual(state_obj, state)
