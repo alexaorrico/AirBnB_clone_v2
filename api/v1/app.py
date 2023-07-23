7c79843f1f205967bc8f56108d17f231b49d7a83
casonbobo <casonsbobo@gmail.com>
2023-07-22 21:17:51 -0700

    this is hopefully all of 4

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Starts up a copy of a flask-app
+"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_appcontext(error):
+    storage.close()
+
+if __name__ =="__main__":
+    import os
+
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_HOST", 5000))
+
+    app.run(host=host, port=port, threaded=True)
