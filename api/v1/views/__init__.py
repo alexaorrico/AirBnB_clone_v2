46b8904c64b9f0d91c7f7f9f7ec2b42b749ea21b
pretzelogical <yingyangyo266@gmail.com>
2023-07-23 11:57:38 -0500

    move imports to after app_views creation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Initializes the api """
 from flask import Blueprint
-from api.v1.views.index import *
 
 app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
