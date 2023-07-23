594bae19179544c07b62d3b358f56157a31e7095
pretzelogical <yingyangyo266@gmail.com>
2023-07-23 12:49:51 -0500

    task 5 first version

--- Makefile
+++ Makefile
@@ -8,6 +8,11 @@ test-fs:
 	python -m unittest \
 	discover -v
 
+test-api:
+	HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost \
+	HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db HBNB_API_HOST=0.0.0.0 \
+	HBNB_API_PORT=5000 python3 -m api.v1.app
+
 test-db:
 	HBNB_ENV=test HBNB_MYSQL_USER=hbnb_test HBNB_MYSQL_PWD=hbnb_test_pwd \
 	HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_test_db \
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,25 @@
 """ Gives the status of the api """
 from flask import jsonify
 from api.v1.views import app_views
-import json
+from api.v1.app import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def app_status():
     """ Returns the status of the app in json format """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def obj_stats():
+    """ Returns the number of each object by type """
+    m_classes = [Amenity, City, Place, Review, State, User]
+    count = {}
+    for c in m_classes:
+        count[c.__name__] = storage.count(c)
+    return jsonify(count)
