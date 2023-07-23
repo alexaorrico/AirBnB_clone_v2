9cbbe6129f6ebb5b4004972e78b6f1eb673c1f7b
pretzelogical <yingyangyo266@gmail.com>
2023-07-23 11:54:14 -0500

    move wildcard import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Initializes the api """
-from api.v1.views.index import *
 from flask import Blueprint
+from api.v1.views.index import *
 
 app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
