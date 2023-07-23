177d77e9f17c0320c3b089c8d42e65ec79a6d9c3
PaintRock <6126@holbertonstudents.com>
2023-07-23 18:26:16 -0500

    branch

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,8 +17,8 @@ def teardown_db(exception):
 
 @app.errorhandler(404)
 def not_found(error):
-    """ Define a handler for 404 errors """
-    return jsonify({"error": "Not found"}), 404
+    """Make of the erroro 404, not found"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     if "HBNB_API_HOST" in environ:
