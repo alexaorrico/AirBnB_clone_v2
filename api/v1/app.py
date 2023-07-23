a585bc33f92bcbbe31142d94a3f2376d42f501cb
sarahmarkland <sarah.markland@icloud.com>
2023-07-23 15:21:01 -0700

    xx

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,7 @@ Starts up a copy of a flask-app
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
-from getenv import env
+from os import getenv as env
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
