0e8327773f051872510dd866a8bcfb40dc9c3608
pretzelogical <yingyangyo266@gmail.com>
2023-07-24 14:55:32 -0500

    PUT working for amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -13,6 +13,7 @@ def get_amenity_no_id():
     amen = storage.all(Amenity).values()
     return jsonify([a.to_dict() for a in amen])
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['GET'],
                  strict_slashes=False)
 def get_amenity_id(amenity_id=None):
@@ -22,6 +23,7 @@ def get_amenity_id(amenity_id=None):
     if a_key not in amen:
         abort(404)
     return(jsonify(amen[a_key].to_dict()))
+
 
 @app_views.route('/amenities', methods=['POST'],
                  strict_slashes=False)
@@ -36,6 +38,7 @@ def new_amenity():
     new_a.save()
     return jsonify(new_a.to_dict()), 201
 
+
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_amenity(amenity_id=None):
@@ -47,3 +50,23 @@ def delete_amenity(amenity_id=None):
     storage.delete(amen[a_key])
     storage.save()
     return jsonify({}), 200
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_amenity(amenity_id=None):
+    """ Updates an amenity based on the amenity id """
+    js_info = request.get_json()
+    amen = storage.all(Amenity)
+    if request.is_json == False:
+        abort(400, 'Not a JSON')
+    js_info.pop('id', 'no_error_pls')
+    js_info.pop('created_at', 'no_error_pls')
+    js_info.pop('updated_at', 'no_error_pls')
+    a_key = "Amenity." + amenity_id
+    if a_key not in amen:
+        abort(404)
+    for key, val in js_info.items():
+        setattr(amen[a_key], key, val)
+    storage.save()
+    return jsonify(amen[a_key].to_dict()), 200
