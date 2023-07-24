0f0240179bf2ea4950d4622b626b5c4176a4498f
pretzelogical <yingyangyo266@gmail.com>
2023-07-24 13:59:18 -0500

    GET with id working

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ View for amenities """
-from flask import jsonify
+from flask import jsonify, abort
 from api.v1.views import app_views
 from models.amenity import Amenity
 from models import storage
@@ -10,9 +10,16 @@ from models import storage
                  strict_slashes=False)
 def get_amenity_no_id():
     """ Gets an amenity if no id has been provided """
-    slist = []
-    states = storage.all(Amenity).values()
-    for state in states:
-        slist.append(state.to_dict())
-    return jsonify(slist)
+    amen_l = []
+    amen = storage.all(Amenity).values()
+    return jsonify([a.to_dict() for a in amen])
 
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_amenity_id(amenity_id=None):
+    """ Gets an amenity when an id is provided """
+    amen = storage.all(Amenity)
+    a_key = "Amenity." + amenity_id
+    if a_key not in amen:
+        abort(404)
+    return(jsonify(amen[a_key].to_dict()))
