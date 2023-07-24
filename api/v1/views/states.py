a8a97afa49ed8609d90c6079598d11fe0a858713
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-24 15:31:21 -0700

    7: -_-

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,7 +9,7 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'])
 def get_states():
     """ retrieves the list of all State objects """
     states = storage.all("State")
@@ -17,7 +17,7 @@ def get_states():
     return jsonify(states_list)
 
 
-@app_views.route('/states/state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('api/v1/views/states/state_id>', methods=['GET'])
 def get_state(state_id):
     """ retrieves a State object (specified with state_id) """
     state = storage.get("State", state_id)
@@ -26,7 +26,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
+@app_views.route('api/v1/views/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
     """ deletes a State object (specified with state_id) """
@@ -51,7 +51,7 @@ def create_state():
     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'],
+@app_views.route('api/v1/views/states/<state_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_state(state_id):
     """ updates a State object (specified with state_id) """
