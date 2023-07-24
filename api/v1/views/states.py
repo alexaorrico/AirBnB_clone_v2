fc35f6a8ff7c45740dca2818b5edb2534c68a0bc
pretzelogical <yingyangyo266@gmail.com>
2023-07-24 16:27:34 -0500

    add strict_slashes=False

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,14 +6,16 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'],
+                 strict_slashes=False)
 def get_states():
     """ Gets the list of all State objects """
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'])
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_state(state_id):
     """ gits the states """
     state = storage.get(State, state_id)
@@ -22,7 +24,8 @@ def get_state(state_id):
     return jsonify(state.to_dict()), 200
 
 
-@app_views.route('/states/<string:state_id>', methods=['DELETE'])
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """ why"""
     state = storage.get(State, state_id)
@@ -33,7 +36,8 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'])
+@app_views.route('/states', methods=['POST'],
+                 strict_slashes=False)
 def create_post():
     if request.is_json is False:
         abort(400, "Not a JSON")
@@ -45,7 +49,8 @@ def create_post():
     return jsonify(state.to_dict()), 201
 
 
-@app_views.route('/states/<string:state_id>', methods=['PUT'])
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_state(state_id):
     if request.is_json is False:
         abort(400, 'Not a JSON')
