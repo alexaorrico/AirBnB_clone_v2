dbf73de979889cc507f375d2e1020c1273e6e71d
Night8Guy <robert@lionsroad.com>
2023-07-24 12:52:29 -0500

    whitespace

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -66,7 +66,7 @@ test_db_storage.py'])
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
-            
+
     def test_dbs_get_method(self):
         """Test for the DBStorage class count"""
         result = db_storage.get(cls="State", id=str(self.state.id))
