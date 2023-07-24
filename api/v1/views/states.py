3cacd9b9e13c948f6f34a06780c87d4abc626df0
PaintRock <6126@holbertonstudents.com>
2023-07-24 19:23:18 +0000

    split

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,5 @@
-#!/usr/bin/python3
-""" Create a new view for states and handle RESTFul API"""
 # api/v1/views/states.py
+""" flask state import blood drive """
 
 from flask import jsonify, request, abort
 from api.v1.views import app_views
@@ -8,16 +7,16 @@ from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'])
 def get_states():
-    """ Gets the list of all State objects"""
+    """ more blood for the drive """
     states = storage.all(State).values()
-    return jsonify([state.to_dict() for state in states])
+    return jsonify([state.to_dict() for state in states]), 200
 
 
 @app_views.route('/states/<string:state_id>', methods=['GET'])
 def get_state(state_id):
-    """ git states"""
+    """ center of his own attention """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
