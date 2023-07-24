1e90ed0f23b702ff9d34f35446a113535a26900c
pretzelogical <yingyangyo266@gmail.com>
2023-07-24 10:25:54 -0500

    trying to fix check on task 5

---
+++ drop_dev_database.sql
@@ -0,0 +1,3 @@
+-- Drop database + User
+DROP DATABASE IF EXISTS hbnb_dev_db;
+DROP USER IF EXISTS hbnb_dev;
---
+++ test_task_5.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+import subprocess
+""" This file is intended to help debug task 5 """
+
+c_ps = subprocess.Popen(['cat', 'drop_dev_database.sql'], stdout=subprocess.PIPE)
+c_out = subprocess.check_output(['mysql', '-uroot', '-p'], stdin=c_ps.stdout)
+c_ps.wait()
+d_ps = subprocess.Popen(['cat', 'create_dev_database.sql'], stdout=subprocess.PIPE)
+d_out = subprocess.check_output(['mysql', '-uroot', '-p'], stdin=d_ps.stdout)
+d_ps.wait()
+
+for i in range(10):
+    a = Amenity(name=f"amenity{i}")
+    a.save()
+    s = State(name=f"state{i}")
+    s.save()
+
+
+# c_ps = subprocess.Popen(['cat', 'drop_dev_database.sql'], stdout=subprocess.PIPE)
+# c_out = subprocess.check_output(['mysql', '-uroot', '-p'], stdin=c_ps.stdout)
+# c_ps.wait()
