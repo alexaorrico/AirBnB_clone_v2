9274a4d3a2b20958e0692d2c51e28d00e4ba6968
pretzelogical <yingyangyo266@gmail.com>
2023-07-24 15:05:59 -0500

    add wildcard import for states (task 7)

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,4 @@ from flask import Blueprint
 app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.amenities import *
+from api.v1.views.states import *
