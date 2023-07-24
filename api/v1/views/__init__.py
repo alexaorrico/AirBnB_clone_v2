59bf410a3710e2eef4208efccb86db96a26ac784
pretzelogical <yingyangyo266@gmail.com>
2023-07-24 15:48:58 -0500

    finalized in branch task-10-pretz

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ app_views = Blueprint('api_v1', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.amenities import *
 from api.v1.views.states import *
+from api.v1.views.users import *
