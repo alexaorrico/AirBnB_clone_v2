d51a2d7e19ef4b4693ab638d7ee1bbc4dbce8733
PaintRock <6126@holbertonstudents.com>
2023-07-24 19:17:10 +0000

    linebreak

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,8 +15,8 @@ def get_states():
     return jsonify([state.to_dict() for state in states])
 
 
-@app_views.route('/states/<string:state_id>',\
-                 methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_state(state_id):
     state = storage.get(State, state_id)
     if not state:
