2201df94280db012a0984f967dc3f40aa9d5abaa
Christopher <christopher@ChristophersMBP.attlocal.net>
2023-07-24 20:31:35 -0500

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 """Index file"""
 
-from api.vi.views import app_views
+from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 # Status route
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    # Returns status
+    """Returns status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Returns count"""
+    stats_dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats_dict)
