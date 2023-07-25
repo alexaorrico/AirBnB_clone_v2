ffa8a492c41fec90db56a0abe6266cbcea04c0c8
Night8Guy <robert@lionsroad.com>
2023-07-25 15:07:08 -0500

    pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,11 +5,13 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 # Status route
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns status"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
