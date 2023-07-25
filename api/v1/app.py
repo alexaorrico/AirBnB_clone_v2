73dac4f60eb5588d4b9ffc4baeaa7905e2a1dfc9
BradenE19 <6115@holbertonstudents.com>
2023-07-25 13:56:27 -0500

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
 """
-Starts up a copy of a flask-app
+app
 """
-from api.v1 import app
-from app import app_views
-from flask import Flask
-from importlib import import_module
-from flask_cors import CORS
-import sys
+
+from flask import Flask, jsonify
+from flask import CORS
+from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 
-"""Allow CORS for all routes on 0.0.0.0"""
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """Closes the storage on teardown"""
-    from models import storage
+def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """404 error handler"""
-    return {"error": "Not found"}, 404
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
 
+    resp = jsonify(data)
+    resp.status_code = 404
 
-def start_flask():
-    """ start flask """
-    from os import getenv as env
-    app.run(host=env('HBNB_API_HOST', default='localhost'),
-            port=env('HBNB_API_PORT'),
-            threaded=True)
-
+    return(resp)
 
 if __name__ == "__main__":
-    start_flask()
+    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
