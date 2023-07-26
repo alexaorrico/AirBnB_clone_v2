272e2d24190977150835808f65497f4ac5e1aaff
Jabulani-N <jabu.94@gmail.com>
2023-07-25 20:06:45 -0700

    pycodestyle improvements

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -114,8 +114,9 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
-    
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "not testing file storage")
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                     "not testing file storage")
     def test_get(self):
         """Test that the get method properly retrievs objects"""
         storage = FileStorage()
@@ -125,7 +126,8 @@ class TestFileStorage(unittest.TestCase):
         new_user.save()
         self.assertIs(storage.get("User", new_user.id), new_user)
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "not testing file storage")
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') ==
+                     'db', "not testing file storage")
     def test_count(self):
         storage = FileStorage()
         initial_length = len(storage.all())
