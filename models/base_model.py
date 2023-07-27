2d164d84b251a9c365ecfbca7c3bca3f70e722e9
samuelselasi <samelsekasi@gmail.com>
2023-07-27 21:48:26 +0000

    Modify User object to store password in hashed MD5 values

--- models/base_model.py
+++ models/base_model.py
@@ -5,8 +5,6 @@ Contains class BaseModel
 
 from datetime import datetime
 import models
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
@@ -58,7 +56,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, pwd=True):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,6 +66,9 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if pwd:
+            if 'password' in new_dict:
+                del new_dict['password']
         return new_dict
 
     def delete(self):
