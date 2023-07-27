838286a39555b36d325ef22b65ab78bcc7f3d341
samuelselasi <samelsekasi@gmail.com>
2023-07-27 14:11:46 +0000

    Add first endpoint that shows status of API

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,16 +29,12 @@ def page_not_foun(error):
 
 app.config['SWAGGER'] = {
     'title': 'AirBnB clone - RESTful API',
-    'description': 'This is the api that was \
-            created for the hbnb restful api project,\
-    all the documentation will be shown below',
+    'description': 'Python back-end API webserver Flask',
     'uiversion': 3}
 
 Swagger(app)
 
 if __name__ == "__main__":
-
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
-
     app.run(host, int(port), threaded=True)
