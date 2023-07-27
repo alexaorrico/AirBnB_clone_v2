388cd784631076f39bcf3f9fd65601a9f7315597
yaekobbonke <yaekobbundure@gmail.com>
2023-07-27 16:14:06 +0000

    db updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        A method to retrieve one object
+        cls: class
+        id: string representing the object ID
+        """
+        classAll = self.all(cls)
+        for obj in classAll.values():
+            if id == str(obj.id):
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        return len(self.all(cls))
