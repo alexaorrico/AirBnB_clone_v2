aa54966802fb820bb2c83137dfe44659621aa46f
yaekobbonke <yaekobbundure@gmail.com>
2023-07-28 20:56:38 +0000

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -88,5 +88,5 @@ class DBStorage:
         return None
 
     def count(self, cls=None):
-        """A method to count the number of objects in storage"""
+        """A method to that count the number of objects in storage"""
         return len(self.all(cls))
