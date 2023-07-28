3d81fdfd0c86d1681ffa1ed5df9f2879216e581d
yaekobbonke <yaekobbundure@gmail.com>
2023-07-28 19:15:56 +0000

    counts objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,9 +7,15 @@ app_views that returns a JSON: "status": "OK"
 """
 from api.v1.views import app_views
 from flak import jsonify
-
+from models.storage import count
 
 @app_views.route("/status")
 def status():
     """ returns a JSON format"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/api/v1/stats")
+def stats():
+    """"""
+    count()
