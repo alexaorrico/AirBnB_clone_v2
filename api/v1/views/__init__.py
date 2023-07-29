5123f8aa3716be2ac23e5d12b342fb70289eae07
Darlington Obioha <epitome011@yahoo.com>
2023-07-29 15:33:38 -0700

    fixed

--- api/v1/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Register Blueprint"""
-
-from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.state import *
-
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
