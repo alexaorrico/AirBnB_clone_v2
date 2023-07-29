44a03ab718451c45cc3ef180dc0adfb07b90aa40
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-29 15:56:55 -0400

    Task 6 completed

--- api/v1/views/state.py
+++ api/v1/views/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """State obj API"""
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 from api.v1.views import app_views
 from models.state import State
 from models import storage
@@ -11,14 +11,16 @@ from models import storage
 def get_state(state_id=None):
     if state_id is None:
         states = storage.all(State).values()
-        states_list = [ state.to_dict() for state in states ]
+        states_list = [state.to_dict() for state in states]
         return jsonify(states_list)
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
@@ -27,9 +29,30 @@ def delete_state(state_id):
     storage.save()
     return make_response(jsonify({}), 200)
 
+
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def post_state(state_id=None):
+def post_state():
+    state = request.get_json()
+    if not state:
+        abort(400, description="Not a JSON")
+    if 'name' not in state:
+        abort(400, description="Missing Name")
+    obj = State(**state)
+    obj.save()
+    return make_response(jsonify(obj.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
     state = storage.get(State, state_id)
+    static_data = ['id', 'created_at', 'updated_at']
     if state is None:
         abort(404)
-
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
+    for key, value in data.items():
+        if key not in static_data:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
