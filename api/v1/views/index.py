d9b887c0dbc762fa11f81fa0521fce216c8ebfa3
MelakuBi <MelakuBirhanu@gmail.com>
2023-07-29 11:20:09 -0700

    Task 4

---
+++ api/v1/views/index.py
@@ -0,0 +1,17 @@
+import app_views from api.v1.views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def get_status():
+    """ returns a JSON """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    clases = {"amenities": Amenity, "cities": City, "places": Place,
+              "reviews": Review, "states": State, "users": User}
+    my_dict = {}
+    for i, j in clases.items():
+        my_dict[i] = storage.count(j)
+    return jsonify(my_dict)
