426cdf5cbc780d0c24ca11c48551bad2ed6f46d9
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-28 20:24:47 -0400

    Start API server

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status")
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """Define route"""
     return jsonify(status='OK')
