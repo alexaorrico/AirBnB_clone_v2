e9c94c28902dbbb02f464937f24b91c9b87c86ec
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-07-29 08:58:10 +0200

    dd

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,10 @@
-#!/usr/bin/python3
-""" Blueprint for API """
 from flask import Blueprint
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places_amenities import *
+from api.v1.views.index import *  # noqa
+from api.v1.views.states import *  # noqa
+from api.v1.views.cities import *  # noqa
+from api.v1.views.amenities import *  # noqa
+from api.v1.views.users import *  # noqa
+from api.v1.views.places import *  # noqa
+from api.v1.views.places_reviews import *  # noqa
+from api.v1.views.places_amenities import *  # noqa
