fc903fa8c108d5830f1c1dcf4daa1336b653e1d9
HassanMunene <awanzihassan@gmail.com>
2023-07-29 15:59:38 +0300

    made some unittest corrections

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -83,9 +83,14 @@ class TestBaseModel(unittest.TestCase):
         and that upon creation have identical updated_at and created_at
         value."""
         inst1 = BaseModel()
+        time.sleep(0.001)
+        inst2 = BaseModel()
+
+        self.assertLessEqual(inst1.created_at, datetime.now())
         self.assertEqual(inst1.created_at, inst1.updated_at)
         self.assertEqual(inst2.created_at, inst2.updated_at)
         self.assertNotEqual(inst1.updated_at, inst2.updated_at)
+        self.assertNotEqual(inst1.created_at, inst2.created_at)
 
     def test_uuid(self):
         """Test that id is a valid uuid"""
