549a998eeef6de783799b0a901d50c3e5461004b
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-29 16:31:58 +0300

    Update DBStorage and FileStorage, adding two new methods get and count

--- models/amenity.py
+++ models/amenity.py
@@ -10,9 +10,12 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity """
+    __tablename__ = 'amenities'
+
     if models.storage_t == 'db':
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
+        __table_args__ = {'mysql_default_charset': 'latin1'}
     else:
         name = ""
 
