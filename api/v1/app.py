e34ad07c583e4c08c1d4643cba01e0bbb47bb63e
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-29 10:49:15 -0400

    Task 4 completed

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,7 +16,7 @@ def close_session(error):
     storage.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,7 +21,7 @@ def status():
     return jsonify(status='OK')
 
 
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """API stats"""
     class_dict = {}
