516b4778f7769adb3c0a8c51c16c0fb2d40d5356
Michael Hudson <mnkotagu@gmail.com>
2023-07-29 13:35:36 +0300

    Fix file name for state view

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,4 +10,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 state_views = Blueprint('app_views', __name__, url_prefix='/api/v1/states')
 
 from api.v1.views.index import *
-from api.v1.views.state import *
+from api.v1.views.states import *
