64e7b7cf4d0df56669c381f722421dbc6944b684
Thecodingyogi <cynthiatonui@gmail.com>
2023-07-29 00:29:04 -0700

    Updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -17,8 +18,9 @@ def teardown_session(exception):
 @app.errorhandler(404)
 def page_not_found(error):
     """Returns a JSON formatted 404 status code response"""
-    return make_response(jsonify(error="Not found"), 404)
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
