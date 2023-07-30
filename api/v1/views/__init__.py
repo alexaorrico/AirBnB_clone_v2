fe9ff5edc975ddefba9dec6ed82c1b924c00cea6
dbaba2011 <dbaba2011@yahoo.com>
2023-07-30 14:38:22 -0700

    api

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+# Create the app_views blueprint instance
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard import of everything in the package api.v1.views.index
+from api.v1.views.index import *
