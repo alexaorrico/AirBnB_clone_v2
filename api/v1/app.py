af3cf37089112e0caf2daf8805d255a9656c3562
Evance Odoyo <odoyoevanc@gmail.com>
2023-07-30 15:39:52 +0300

    add error 404 error handler returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """The Flask application module"""
 from flask import Flask
+from flask import jsonify
+from flask import make_response
 from os import getenv
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handler that returns a JSON-formatted 404 status code response.
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
