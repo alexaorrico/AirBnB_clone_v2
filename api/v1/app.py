4daffc5ce430c95aa0f7b148a0d6a5388110fcdd
davidgregs87 <udohdavid27@gmail.com>
2023-07-30 07:05:32 -0700

    fixed all

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,19 @@ from flask import jsonify
 
 
 @app_views.route('/status')
-def status():
+def view_status():
     """Return the status OK"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def view_stats():
+    """Returns every class and count number of instances"""
+    from models import storage
+    return jsonify({
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'user': storage.count(User)})
