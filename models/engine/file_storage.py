829afa04430b273fd2c2d8695dbe96eae8197f05
papihoudini <ochiengcliff.co@gmail.com>
2023-07-30 07:11:32 -0400

    PEP8 style formatting

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,8 +12,15 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {
+    "Amenity": Amenity,
+    "BaseModel": BaseModel,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
 
 
 class FileStorage:
@@ -45,7 +52,7 @@ class FileStorage:
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
+        with open(self.__file_path, "w") as f:
             json.dump(json_objects, f)
 
     def get(self, cls, id):
@@ -57,29 +64,29 @@ class FileStorage:
         else:
             return None
 
-         def count(self, cls=None):
-             """Count the number of objects in storage"""
-             count = 0
-             if cls in classes.values():
-                 count = len(self.all(cls))
-             else:
-                 count = len(self.__objects)
-                 return count
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        count = 0
+        if cls in classes.values():
+            count = len(self.all(cls))
+        else:
+            count = len(self.__objects)
+            return count
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(self.__file_path, "r") as f:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
-I
+
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = obj.__class__.__name__ + "." + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
