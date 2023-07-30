00f529316a28c247a46ef087a32b031135f5b79c
HassanMunene <awanzihassan@gmail.com>
2023-07-30 13:32:49 +0300

    testing

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models import storage
 from models.user import User
+from flasgger.utils import swag_from
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
