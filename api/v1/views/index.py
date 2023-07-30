dfd0b70e7c9515b363c7db0fe900fe749d868039
Obianuju23 <obianujunmoh@gmail.com>
2023-07-30 15:59:53 -0700

    index file of api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def get_status():
     """Gets the status of the API."""
-    return jsonify("status"="OK")
+    return jsonify("status": "OK")
