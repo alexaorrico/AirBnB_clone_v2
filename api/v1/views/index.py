d4b6e4c2238fb59fe463cf2f0011707a3edc950d
Robert Ruai Mawich <robertruai4@gmail.com>
2023-07-30 14:11:36 +0300

    minor edits

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -26,5 +26,5 @@ def stats():
         "places": storage.count(Place),
         "reviews": storage.count(Review),
         "states": storage.count(State),
-        "users": storage.count(User),
+        "users": storage.count(User)
     })
