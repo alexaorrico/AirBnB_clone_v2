65877e2893260c50fefe75259056c29288121c60
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-30 18:26:01 +0000

    Add .gitignore'

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 """Index view for the web service API"""
 from flask import jsonify
 from api.v1.views import app_views  # Blueprint object
+from models import storage
+from models.engine.file_storage import classes
 
 
 # Create the route "/status" on the Blueprint object
@@ -8,3 +10,17 @@ from api.v1.views import app_views  # Blueprint object
 def status():
     # Return a JSON response with the status "OK"
     return jsonify({"status": "OK"})
+
+
+# stat blueprint
+@app_views.route('/stats')
+def stat(cls=None):
+    """Return number of objects by type"""
+
+    objects = {}
+
+    for key, value in classes.items():
+        if key != 'BaseModel':
+            # count objects by type from storage
+            objects[key.lower()] = storage.count(value)
+    return jsonify(objects)
