68a9aba708675d6e8b5cc0f264a0367ee7e36bb7
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-30 11:01:33 -0400

    Fix task 14 error

--- models/user.py
+++ models/user.py
@@ -29,9 +29,7 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-
-    def __setattr__(self, name, value):
-        """sets a password with md5 encryption"""
-        if name == "password":
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(name, value)
+        if kwargs["password"]:
+            value = kwargs["password"]
+            kwargs["password"] = md5(value.encode()).hexdigest()
+            self.__dict__.update(kwargs)
