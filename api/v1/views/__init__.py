d2f045bd52e82cc2c6ec5626d837b652da376615
davidgregs87 <udohdavid27@gmail.com>
2023-07-30 12:42:21 -0700

    [fixed]

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,3 +5,6 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
+
+app_views.add_url_rule('/status', view_func=view_status)
+app_views.add_url_rule('/stats', view_func=view_stats)
