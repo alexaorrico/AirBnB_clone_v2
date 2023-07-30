cbab28533b5605dcdc269a6ac6e1e71edfb7daaa
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-07-30 14:03:22 +0100

    task 2
    review and merge if no conflict

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -68,3 +69,33 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+    
+    def get(self, cls, id):
+        """"
+        returns the object base on its class name and ID
+        Or return None if not found
+        """
+        if cls not in classes.values():
+            return None
+        
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+        
+        return None
+    
+    def count(self, cls=None):
+        """
+        count the number of objects in the storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for item in all_class:
+                count += len(models.storage.all(item).values())
+        else:
+            count = len(models.storage.all(cls).values())
+        
+        return count
