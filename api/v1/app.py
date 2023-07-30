6af6c654f121a4d1ae57344ba7fa492aad48f276
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-29 20:47:05 -0400

    Update file

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,20 +4,22 @@
 from flask import Flask
 
 import os
-
-app = Flask(__name__)
-
 from models import storage
 from api.v1.views import app_views  # Blueprint
 
+app = Flask(__name__)
+
 # Register app_views as blueprint to app
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def close_storage(error=None):
     """ Called when application context is torn down"""
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host=(os.getenv('HBNB_API_HOST', '0.0.0.0')), 
-            port=(int(os.getenv('HBNB_API_PORT', '5000'))),threaded=True, debug=True)
+    app.run(host=(os.getenv('HBNB_API_HOST', '0.0.0.0')),
+            port=(int(os.getenv('HBNB_API_PORT', '5000'))),
+            threaded=True, debug=True)
