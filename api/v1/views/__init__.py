8e95ad2061828cc61f099924d6f8e9d0da48b5a8
Obianuju23 <obianujunmoh@gmail.com>
2023-07-30 15:40:02 -0700

    created file __init__.py and import Blueprint from flask doc

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Creates a Blueprintinstance with url prefix = /api/v1
+"""
+
+from flask import Blueprint
+ 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+
+"""import flask views"""
+from api.v1.views.index import *
