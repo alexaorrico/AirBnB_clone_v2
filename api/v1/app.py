ba48f58d1a1412029b4bbda1b69252d27457c42c
Obianuju23 <obianujunmoh@gmail.com>
2023-07-29 21:45:38 -0700

    creating app variable with instance of flask

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+contains Flask web application api
+"""
+
+from flask import Flask, jsonify
+from api.v1.views import app_views
+from models import storage
+import os
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def storage_close(self):
+    """closes the storage after each session"""
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", "5000"))
+    app.run(host=host, port=port, threaded=True)
