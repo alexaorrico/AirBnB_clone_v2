c2703f5971f0eaf1cdde8eb79c69d33ab94193dd
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-30 15:59:32 -0700

    Updated proj

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python3
+"""
+ flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 # Define the /status route
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', strict_slashes=False)
 def status():
+    """return JSON of OK status """
     return jsonify({"status": "OK"})
