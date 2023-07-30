e70b26daaaf4597746d2b146c6b24eb009940ca9
Francis Ofori Anane <ofori.anane@gmail.com>
2023-07-30 01:26:15 +0000

    Delete api from master

--- api/v1/views/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Create the Blueprint object with '/api/v1' as url_prefix
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
-
-
