0b3e8a7e07a94b799d262af12f6c52f294d899ca
nasirisreal <nasirisreal@yahoo.com>
2023-07-30 13:17:23 +0000

    AirBnB V3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""
-This module contains endpoint(route) status
-"""
-from models import storage
-from flask import Flask
+""" Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """
-    Returns a JSON status
-    """
-    return jsonify({"status": "OK"})
+    """ Returns JSON """
+    return jsonify(status="OK")
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
-    """
-    Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
