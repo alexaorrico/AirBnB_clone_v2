8138c1f37de627d07ba962a7cb5f87ea55e8c82e
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-07-30 05:30:17 -0400

    Fix task 1 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,14 @@
 """Start server"""
 
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
 from os import getenv
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
