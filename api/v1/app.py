6151366af2e1bd6edeb6109dbcfe25fd13c58a0b
DannySanchez6658 <danieltsega6658@gmail.com>
2023-07-30 23:21:42 +0000

    [Feature]: Add the 404 error handler route method

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,12 @@ def close_it(obj):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """ Returns JSON response with 404 status """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == "__main__":
 
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
