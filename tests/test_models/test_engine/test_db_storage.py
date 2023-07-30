7a4635ee98957653af207d6c4c0a19f26c79ce6d
DannySanchez6658 <danieltsega6658@gmail.com>
2023-07-30 23:57:03 +0000

    [Fix]: Fixed the pycodestyle error

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -89,5 +89,4 @@ class TestFileStorage(unittest.TestCase):
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
-        """Test that count properly counts all objects"""
-        self.assertEqual(len(models.storage.all()), models.storage.count())
+        """Test that count returns the right number of elements in the db"""
