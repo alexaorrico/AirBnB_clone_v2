673b4cb3dacd93e00767c936b165ff1f15a3897e
yaekobbonke <yaekobbundure@gmail.com>
2023-07-30 20:14:46 +0000

    some changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 
 """
-create a variable app, instance of Flask
-import storage from models
-import app_views from api.v1.views
-register the blueprint app_views to your Flask instance app
-declare a method to handle @app.teardown_appcontext that calls storage.close()
-inside if __name__ == "__main__":, run your Flask server (variable app) with:
-host = environment variable HBNB_API_HOST or 0.0.0.0 if not defined
-port = environment variable HBNB_API_PORT or 5000 if not defined
-threaded=True
+creates a flask application
 """
 from flask import Flask
-from models import storage
-from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+from models import storage
+from api.v1.views import app_views
 app.register_blueprint(app_views)
 
 
