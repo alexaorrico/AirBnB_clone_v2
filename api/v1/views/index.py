095e466cefae21ac9e86f412ac9734152d3a2ec4
DannySanchez6658 <danieltsega6658@gmail.com>
2023-07-30 22:31:16 +0000

    [Fix]: Fix pycodestyle error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,12 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
+
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
