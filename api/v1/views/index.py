fdaac8a5109035ac600929e5c3b1add0efb52576
COBIECODES <dibia3527@gmail.com>
2023-07-30 01:16:33 +0000

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,24 @@
-#!/usr/bin/python3
-'''Contains the index view for the API.'''
+#!/usr/bin/python3xx
+'''api status'''
+import models
+from models import storage
+from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
-from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """ Returns JSON """
-    return jsonify(status="OK")
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stat():
-    """returns the number of each objects by type"""
-    return jsonify(
-        amenities=storage.count('Amenity'),
-        cities=storage.count('City'),
-        places=storage.count('Place'),
-        reviews=storage.count('Review'),
-        states=storage.count('State'),
-        users=storage.count('User')
-    )
+@app_views.route('/stats', strict_slashes=False)
+def stuff():
+    '''JSON Responses'''
+    todos = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
