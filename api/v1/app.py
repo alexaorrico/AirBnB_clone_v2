1d1a238adee51cc76f9d1a74f30840ae5081dc78
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-07-30 11:55:01 +0200

    fixd a heavy bug

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,39 +1,68 @@
 #!/usr/bin/python3
-"""Define routes for blueprint
+"""Flask web application
 """
 
-from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify
 from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """Return status of application
+@app.teardown_appcontext
+def teardown(error):
+    """Clean-up method
     """
-    return jsonify({'status': 'OK'})
+    storage.close()
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stats():
-    """Retrieve count of objects in storage
+@app.errorhandler(404)
+def not_found(error):
+    """Custom 404 error
     """
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
+    return jsonify({'error': 'Not found'}), 404
 
-    classes = {"amenities": Amenity, "cities": City,
-               "places": Place, "reviews": Review,
-               "states": State, "users": User}
-    json_dict = {}
+if __name__ == '__main__':
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)#!/usr/bin/python3
+"""Flask web application
+"""
 
-    for name, cls in classes.items():
-        json_dict.update({name: storage.count(cls)})
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
-    return jsonify(json_dict)#!/usr/bin/python3
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown(error):
+    """Clean-up method
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Custom 404 error
+    """
+    return jsonify({'error': 'Not found'}), 404
+
+if __name__ == '__main__':
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)#!/usr/bin/python3
 """Flask web application
 """
 
