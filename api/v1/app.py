15fc0f0bc43a068d5d856ae22bdf26fc036bf9ae
HassanMunene <awanzihassan@gmail.com>
2023-07-30 20:54:46 +0300

    I have the Acces-Control-Allow-Origin header

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
