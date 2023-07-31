9393e1d6130d16c5e9924f7eb58d91bc03d8b953
CyrusNchege <cyruschegecc@gmail.com>
2023-08-01 00:24:46 +0300

    refactoring app file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
-"""
-This module contains the principal application
-"""
-
-from flask import Flask, Blueprint, jsonify
+"""make api"""
 from models import storage
-from api.v1.views import app_views
 from os import getenv
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.config['JSON_SORT_KEYS'] = False
-cors = CORS(app, resources={r"/api/v1/*": {"origins" : "0.0.0.0"}})
+app.register_blueprint(app_views)
+
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage"""
+def teardown_appcontext(self):
+    """teardown method"""
     storage.close()
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """Error handler for 404"""
-    return jsonify({"error": "Not found"}), 404
+    """404 error handler
+
+    args:
+        error status
+    return:
+        json error response
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", default="0.0.0.0'")
-    port = getenv("HBNB_API_PORT", default="5000")
-    app.run(host=host, port=port, threaded=True)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', '5000'))
+    app.run(host, port, threaded=True)
