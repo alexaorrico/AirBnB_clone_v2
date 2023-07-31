4eab5df17561f731aa3b77fe436412ba9900f664
Abdi Berhe <abdi201380@gmail.com>
2023-07-30 18:49:54 -0700

    [Update]: Give rule for CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
 """ Script that imports a Blueprint and runs Flask """
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+# Create a CORS instance allowing /* for 0.0.0.0
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
-
-@app.teardown_appcontext
-def close_it(obj):
-    """ Closes storage session """
-    storage.close()
-
 
 @app.errorhandler(404)
 def not_found(error):
-    """ Returns JSON response with 404 status """
-    return make_response(jsonify({"error": "Not found"}), 404)
-
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
-
-    app.run(host, int(port), threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
