1b33972cc8b31c3fcd6e364c6cbfcfcd44ed2a83
Obianuju23 <obianujunmoh@gmail.com>
2023-07-31 15:40:31 -0700

    trying to fix errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -30,5 +30,5 @@ def not_found(error):
 
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', default='5000'))
+    port = int(os.getenv('HBNB_API_PORT', default=5000))
     app.run(host=host, port=port, threaded=True)
