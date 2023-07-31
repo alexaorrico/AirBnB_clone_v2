de846ccc19dc6ebb76e51639c0c992baf1ad111a
chimekinglsey <onlinekingsley@gmail.com>
2023-07-31 19:45:38 +0000

    changed status in index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ from models import storage
 @app_views.route('/status')
 def status():
     """returns status ok"""
-    return jsonify({"status": "ok"})
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/api/v1/stats')
