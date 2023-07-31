78a60301709c765dfdcaa35407d866ac735a4dbb
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-30 17:05:36 -0700

    index file updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,9 +8,22 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from flask import Flask
+
 
 # Define the /status route
 @app_views.route('/status', strict_slashes=False)
 def status():
     """return JSON of OK status """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """Retrieves the number of each objects by type """
+    return jsonify({"amenities": storage.count("Amenity")
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
