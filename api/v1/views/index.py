881cc2d1f01bb5788c526e094cf12229f173faa2
chimekinglsey <onlinekingsley@gmail.com>
2023-07-31 16:52:16 +0000

    Task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 from . import app_views
 from flask import jsonify
 
 @app_views.route('/status')
 def status():
     """returns status ok"""
-    return jsonify(f'{\n\t"status": "ok"\n}')
+    return jsonify({"status": "ok"})
