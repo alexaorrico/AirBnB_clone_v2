91099d28772137e5fe7b6d2b90dc206f9c8ac15b
Obianuju23 <obianujunmoh@gmail.com>
2023-07-31 15:21:25 -0700

    trying to fix errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,16 +3,18 @@
 contains Flask web application api
 """
 
-from flask import Flask, jsonify
+from flask import Flask, HTTP_response, jsonify
 from api.v1.views import app_views
 from models import storage
-import os
+from os import getenv
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -21,7 +23,13 @@ def tear_down(self):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """handles 404 HTTP errors"""
+    return HTTP_response(jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == '__main__':
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", "5000"))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
