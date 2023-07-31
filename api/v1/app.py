d67b1339a3487d8e8821280c8ad5ce9b3ebf5168
Webamz <chriszxn99@gmail.com>
2023-07-31 10:29:22 +0200

    not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from os import getenv
 
 app = Flask(__name__)
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def close_db(obj):
     """calls the close() method"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    """404 Page not found"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
