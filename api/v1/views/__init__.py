a7e161f091dc36eb6514c6df1f1f67833ca78c8d
Obianuju23 <obianujunmoh@gmail.com>
2023-07-30 23:04:15 -0700

    updated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,9 +4,11 @@ Creates a Blueprintinstance with url prefix = /api/v1
 """
 
 from flask import Blueprint
+from api.v1.views.index import *
+
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
 """import flask views"""
-from api.v1.views.index import *
+'''from api.v1.views.index import *'''
