a58c692d0f9bd22b11dfaea729e13f128c448d2f
yaekobbonke <yaekobbonke@gmail.com>
2023-07-31 03:07:52 +0300

    dender

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
-import Blueprint from flask doc
-create a variable app_views which is an instance of 
-Blueprint (url prefix must be /api/v1)
-wildcard import of everything in the package api.v1.views.index
+declares  Blueprint 
 """
 from flask import Blueprint
 
