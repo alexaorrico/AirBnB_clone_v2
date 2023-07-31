8ce640db5ae0d6bb5aeba7d2557b3f424a3d34ff
Abdi Berhe <abdi201380@gmail.com>
2023-07-30 19:09:58 -0700

    [Update]: Give rule for CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,9 +11,11 @@ app.register_blueprint(app_views)
 # Create a CORS instance allowing /* for 0.0.0.0
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True)
