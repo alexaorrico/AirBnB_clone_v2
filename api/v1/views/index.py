04900359b4faf9afca17d4a865f4a262dbe5ff80
chimekinglsey <onlinekingsley@gmail.com>
2023-07-31 23:01:59 +0000

    refactored index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,38 @@
 #!/usr/bin/python3
-"""
-    Blueprint to all ALX Headache
-"""
+"""The index folder package"""
 
-from models import storage
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 from models.amenity import Amenity
-from models.city import City
 from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
+from models.state import State
+from models.review import Review
+from models.city import City
 
 
 @app_views.route('/status')
 def status():
-    """returns status ok"""
-    return jsonify({"status": "OK"})
+    """Return the status of the page"""
+    status = {
+            'status': 'OK'
+            }
+    return jsonify(status)
 
 
 @app_views.route('/stats')
-def count_objects():
-    """retrieves all objects by classes"""
-    return jsonify({"amenities": storage.count(Amenity),
-                    "states": storage.count(State), "places": storage.count(
-                    Place), "cities": storage.count(City),
-                    "reviews": storage.count(Review),
-                    "users": storage.count(User)})
+def list_counnt():
+    """Return the number of each objects in database"""
+    objects = {
+            'amenities': Amenity,
+            'cities': City,
+            'places': Place,
+            'reviews': Review,
+            'states': State,
+            'users': User
+            }
+    my_dict = {}
+    for key, value in objects.items():
+        my_dict[key] = storage.count(value)
+    return jsonify(my_dict)
