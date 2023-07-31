2dc4c0eb38b741a676d72c2500bf15d1d2b364d3
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-30 19:27:13 -0700

    views updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,9 +21,12 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def count():
     """Retrieves the number of each objects by type """
-    return jsonify({"amenities": storage.count("Amenity")
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    cls_count = {
+        "amenities": storage.count("Amenity")
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+     }
+     return jsonify(cls_count)
