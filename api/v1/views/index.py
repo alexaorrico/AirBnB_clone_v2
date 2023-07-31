e5ee9aba423b6bc843b2d5f65645582a08c5a04d
JaminBenjamins <jaminbenjamins@gmail.com>
2023-08-01 02:19:04 +0300

    Adding the approriate modules

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 that returns a JSON"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=['GET'],strict_slashes=False)
