b35025a58acfa4cf2e514778ff9a00eb861312d8
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-31 05:20:30 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,14 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(code):
