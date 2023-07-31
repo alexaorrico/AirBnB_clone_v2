de811ffec9d95bd0c552fc958e777d6c68478aa9
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-31 20:20:58 +0300

    Adding location obtaining resource

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,15 @@
 import os
 from models import storage
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
-from os import getenv
+from os import environ
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -24,7 +27,7 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    host = environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = environ.get("HBNB_API_PORT", 5000))
 
     app.run(host=host, port=port, threaded=True)
