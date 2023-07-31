9adc71cb018502d8a93d3811a28785fd2de1b821
chimekinglsey <onlinekingsley@gmail.com>
2023-07-31 19:19:46 +0000

    Committing task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,12 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status')
 def status():
     """returns status ok"""
     return jsonify({"status": "ok"})
+
 
 @app_views.route('/api/v1/stats')
 def count_objects():
@@ -16,7 +18,7 @@ def count_objects():
         cls = storage.classes.get(cls_name)
         count = storage.count(cls)
         stats[cls_name.lower()] = count
-    
+
     # Modify the keys to convert singular to plural
     for key, val in stats.items():
         if key.endswith('y'):
@@ -25,4 +27,3 @@ def count_objects():
             stats[key + 's'] = stats.pop(key)
 
     return jsonify(stats)
-
