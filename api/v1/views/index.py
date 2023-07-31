556ac14aefd6539a2f342ee85215cb7e3ec3c504
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-31 20:31:23 +0300

    Setting up the API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,17 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route("/status")
+@app_views.route("/status", methods=['GET'],strict_slashes=False)
 def status():
     """returns a JSON with status OK"""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Return number of each instance type"""
+    return jsonify(amenities=storage.count("Amenity"),
+                    cities=storage.count("City"),
+                    places=storage.count("Place"),
+                    reviews=storage.count("Review"),
+                    states=storage.count("State"),
+                    users=storage.count("User"))
