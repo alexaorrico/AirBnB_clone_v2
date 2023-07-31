fb6e11a8cf60c185dd145aa14eb9fb8d9675dcca
chukssomzzy <chukssomzzy19@gmail.com>
2023-07-31 03:47:57 +0000

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,20 +5,22 @@ from os import getenv
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
 
-@app.teardown_appcontext
+@ app.teardown_appcontext
 def tearDown(exc):
     """Close the session and request a new one once the application context is
     poped"""
     storage.close()
 
 
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def on_error_404(e):
     """Defines error handler  for 404"""
     return make_response(jsonify(error="Not found"), 404)
