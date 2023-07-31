fc82408e74c1375bf27fcebb3920fe16f1e3d9b5
apamjohnhyder <johnapam4@gmail.com>
2023-07-31 15:11:21 -0700

    db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,23 +76,23 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """
-        Returns the object based on the class name and its ID, or None if not
-        found
-        """
-        objects = self.__session.query(classes[cls])
-        for obj in objects:
-            if obj.id == id:
-                return obj
-        return None
+        """retrieve one object with id from current database session"""
+        obj = None
+        if cls and id:
+            try:
+                if isinstance(cls, BaseModel):
+                    cls_name = str(cls).split('.')[-1]
+                if isinstance(cls, str):
+                    cls_name = cls
+                else:
+                    cls_name = cls.__name__
+                obj = self.__session.query(classes[cls_name]).get({'id': id})
+            except sqlalchemy.orm.exc.ObjectDeletedError:
+                obj = None
+        return obj
 
     def count(self, cls=None):
         """
-        Returns the number of objects in storage matching the given class name.
-        If no name is passed, returns the count of all objects in storage.
+            returns the count of all objects in storage
         """
-        nobjects = 0
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                nobjects += len(self.__session.query(classes[clss]).all())
-        return nobjects
+        return (len(self.all(cls)))
