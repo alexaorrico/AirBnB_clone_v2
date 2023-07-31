a7fa3738f15981aa1a40d388ad16768c2b95353d
Ishimwe Sage <72102004+Sagastein@users.noreply.github.com>
2023-07-31 15:36:50 +0200

    updating the db storage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,7 +13,7 @@ from models.state import State
 from models.user import User
 from os import getenv
 import sqlalchemy
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 classes = {"Amenity": Amenity, "City": City,
@@ -51,32 +51,6 @@ class DBStorage:
                     new_dict[key] = obj
         return (new_dict)
 
-    def get(self, cls, id):
-        """Returns the object based on the class and its ID,\
-        or None if not found
-
-        Args:
-            id (str): string representing the object ID
-        """
-        obj = self.__session.query(cls).get(id)
-        return obj
-
-    def count(self, cls=None):
-        """Returns the number of objects in storage matching the given class.
-        If no class is passed, returns the count of all objects in storage.
-
-        Args:
-            cls (_type_): class (optional)
-        """
-        if cls is None:
-            count = 0
-            for k, v in classes.items():
-                result = self.__session.query(func.count(v.id))
-                count = count + result[0][0]
-            return count
-
-        return self.__session.query(func.count(cls.id))[0][0]
-
     def new(self, obj):
         """add the object to the current database session"""
         self.__session.add(obj)
@@ -100,3 +74,33 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+
+        return None
+
+    def count(self, cls=None):
+        """
+        count the number of objects in storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
