fc5912811dd22dc7b3ea059fa9ca901099500185
Fatimah1403 <fattyhassan14@gmail.com>
2023-07-31 15:36:17 -0700

    Model classes updated

--- models/review.py
+++ models/review.py
@@ -11,6 +11,7 @@ class Review(BaseModel, Base):
     """Representation of Review """
     if models.storage_t == 'db':
         __tablename__ = 'reviews'
+        __table_args__ = {"mysql_default_charset": "latin1"}
         place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
