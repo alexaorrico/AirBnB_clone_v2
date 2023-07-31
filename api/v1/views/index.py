8f6a9d900bef6ef678aac2b8107e73d5c471345c
Guy François de Claude NKOUKA <declaudefrancois@gmail.com>
2023-07-31 22:35:57 +0100

    fix: stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,9 +10,9 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 
-classes = {"users": "User", "places": "Place", "states": "State",
-           "cities": "City", "amenities": "Amenity",
-           "reviews": "Review"}
+classes = {"users": User, "places": Place, "states": State,
+           "cities": City, "amenities": Amenity,
+           "reviews": Review}
 
 
 @app_views.route('/status', methods=['GET'])
