397375477cc2c9d89c941e75085db84bd7e4f978
CyrusNchege <cyruschegecc@gmail.com>
2023-08-01 01:03:57 +0300

    Add all other Apis

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,29 @@
 #!/usr/bin/python3
-"""index of views"""
+"""
+This module contains endpoint(route) status
+"""
+from models import storage
+from flask import Flask
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
-from collections import OrderedDict
-
-class_plurals = {'amenities': Amenity, 'cities': City, 'places': Place,
-                 'reviews': Review, 'states': State, 'users': User}
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """status of api v1"""
-    status = {"status": "OK"}
-    return jsonify(status)
+    """
+    Returns a JSON status
+    """
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def stats():
-    """ Returns itemized count of objects in storage by class
-
+def count():
     """
-    stats = OrderedDict()
-    for key in sorted(class_plurals.keys()):
-        count = storage.count(class_plurals[key])
-        if count > 0:
-            stats[key] = count
-    return jsonify(stats)
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
