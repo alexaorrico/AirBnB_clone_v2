241fc0a3eccf03cddff62fb0160d313d66c405d2
pacmanlolo <michael.omotosho7@gmail.com>
2023-07-31 09:28:08 -0700

    stash

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,9 +6,15 @@ Creates a Blueprintinstance with url prefix = /api/v1
 from flask import Blueprint
 
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
 
 """import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
