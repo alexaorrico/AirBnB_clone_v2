21b7f17beae6f9d7667adf2104a6825aeb6d9f71
JEPTUI <alicejeptui@gmail.com>
2023-08-01 12:04:43 -0700

    State

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,3 +4,4 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 # Import flask views defined in this package
 from api.v1.views.index import *
+from api.v1.views.states import *
