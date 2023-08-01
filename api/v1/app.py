87a78a52b400e85bb41710816c948a3d7215fc7c
JEPTUI <alicejeptui@gmail.com>
2023-07-31 19:50:07 -0700

    Hot fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from os import environ
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.url_map.strict_slashes = False
+# app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
