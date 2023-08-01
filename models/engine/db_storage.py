9d2cfe395dc8bebd86590649115b37250bfc2ad6
JEPTUI <alicejeptui@gmail.com>
2023-07-31 19:42:53 -0700

    Db storage update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,7 +66,7 @@ class DBStorage:
 
     def reload(self):
         """reloads data from the database"""
-        Base.metadata.create_all(self.__engine)
+        self.__session = Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
