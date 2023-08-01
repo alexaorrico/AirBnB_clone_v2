70222dd2a4b28b46240730da6a5ca859a5add0d5
CyrusNchege <cyruschegecc@gmail.com>
2023-08-01 22:36:56 +0300

    Refactor Api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,20 @@
-#!/usr/bin/python3
-"""make api"""
+from flask import Flask
 from models import storage
 from os import getenv
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
-from api.v1.views.index import *
+from api.v1.views import app_views
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views)
 
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """teardown method"""
+def teardown_db(exception):
+    """Teardown method"""
     storage.close()
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """404 error handler
-
-    args:
-        error status
-    return:
-        json error response
-    """
-    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', '5000'))
-    app.run(host, port, threaded=True)
+    app.run(host=host, port=port, threaded=True)
