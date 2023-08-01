4156b69c488b707498cbfd6d2d3765f549516281
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-08-01 03:34:17 +0100

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,41 +1,62 @@
 #!/usr/bin/python3
 """states view for the API"""
+
 from flask import jsonify, request
 from api.v1.views import app_views
-from models import storage
 from models.state import State
-from werkzeug.exceptions import NotFound, MethodNotAllowed, BadRequest
+from models import storage
 
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    """Retrieves the list of all State objects"""
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
 
-ALLOWED_METHODS = ['GET', 'DELETE', 'POST', 'PUT']
-"""allowed methods for the states endpoint"""
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    """Retrieves a State object"""
+    state = storage.get(State, state_id)
+    if not state:
+        return jsonify({"error": "Not found"}), 404
+    return jsonify(state.to_dict())
 
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """Deletes a State object"""
+    state = storage.get(State, state_id)
+    if not state:
+        return jsonify({"error": "Not found"}), 404
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
 
-@app_views.route('/states', methods=ALLOWED_METHODS)
-@app_views.route('/states/<state_id>', methods=ALLOWED_METHODS)
-def handle_state(state_id=None):
-    """handles the states endpoint"""
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """Creates a State"""
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Not a JSON"}), 400
+    if "name" not in data:
+        return jsonify({"error": "Missing name"}), 400
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
 
-    handlers = {
-        'GET': get_states,
-        'POST': add_states,
-        'DELETE': rm_states,
-        'PUT': update_state,
-    }
-    
-    if request.method in handlers:
-        return handlers[request.method](state_id)
-    else:
-        raise MethodNotAllowed(list(handlers.keys()))
-    
-def get_states(state_id=None):
-    """
-    gets the state with the given ID or all states
-    """
-    all_states = storage.all(State).values()
-    if state_id:
-        result = list(filter(lambda x: x.id == state_id, all_states))
-        if result:
-            return jsonify(result[0].to_dict())
-        raise NotFound
-    all_states = list(map(lambda x: x.to_dict(), all_states))
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """Updates a State object"""
+    state = storage.get(State, state_id)
+    if not state:
+        return jsonify({"error": "Not found"}), 404
+
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Not a JSON"}), 400
+
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(state, key, value)
+
+    state.save()
+    return jsonify(state.to_dict()), 200
