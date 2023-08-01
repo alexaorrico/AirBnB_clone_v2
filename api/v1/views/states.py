91ddb54679d05e5dfbc3487046d895311e9a1b7b
Rashisky <Rashisky007@gmail.com>
2023-08-01 12:49:08 +0100

    RESTful API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -38,32 +38,32 @@ app = Flask(__name__)
 #             return jsonify(error="Not a JSON"), 400
 
 
-# @app.route('/api/v1/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
-#            strict_slashes=False)
-# def state_id(state_id):
-#     """retrieves or deletes a State Object using its Id"""
-#     for state in states:
-#         if state["id"] == state_id:
-#             if request.method == 'GET':
-#                 return jsonify(state), 200
-#             elif request.method == 'DELETE':
-#                 states.remove(state)
-#                 return jsonify({}), 200
-#             elif request.method == 'PUT':
-#                 try:
-#                     """extract the data passed and checks if it is
-#                     a valid JSON data"""
-#                     data = request.get_json()
-#                     ignore_keys = ['id', 'created_at', 'updated_at']
-#                     new_data = {key: value for key, value in data.items()
-#                                 if key not in ignore_keys}
-#                     state.update(new_data)
-#                     return jsonify(state), 200
+@app.route('/api/v1/states/<state_id>', methods=['GET', 'DELETE', 'PUT'],
+           strict_slashes=False)
+def state_id(state_id):
+    """retrieves or deletes a State Object using its Id"""
+    for state in states:
+        if state["id"] == state_id:
+            if request.method == 'GET':
+                return jsonify(state), 200
+            elif request.method == 'DELETE':
+                states.remove(state)
+                return jsonify({}), 200
+            elif request.method == 'PUT':
+                try:
+                    """extract the data passed and checks if it is
+                    a valid JSON data"""
+                    data = request.get_json()
+                    ignore_keys = ['id', 'created_at', 'updated_at']
+                    new_data = {key: value for key, value in data.items()
+                                if key not in ignore_keys}
+                    state.update(new_data)
+                    return jsonify(state), 200
 
-#                 except Exception as e:
-#                     return jsonify(error=e), 400
+                except Exception as e:
+                    return jsonify(error=e), 400
 
-#     return jsonify(error="Not found"), 404
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
