b2ca48c191a9fe22394cbe8a5d767a99d5803e23
JEPTUI <alicejeptui@gmail.com>
2023-08-01 11:08:39 -0700

    Blueprint instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,17 +4,17 @@ registers the blueprint to your flask instance app
 """
 from models import storage
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-host = os.getenv("HBNB_API_HOST", '0.0.0.0')
+host = os.getenv("HBNB_API_HOST", "0.0.0.0")
 port = os.getenv("HBNB_API_PORT", 5000)
 
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+# cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
