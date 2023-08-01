625285ba3135284822db3d60fe1c96e570833006
Simontagbor <simontagbor360@gmail.com>
2023-08-01 13:32:55 -0700

    Revert "Add the first endpoint (route) to return the status of the API"
    This reverts commit f0a90114773c5ca0a1c38348ec9deebcbc8f7af4.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-from . import *
-from flask import Blueprint
-# from . import states
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Wildcard import of everything in the package api.v1.views.index
+from . import states
