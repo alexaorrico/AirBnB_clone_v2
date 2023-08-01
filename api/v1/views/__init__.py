304951f4e6886fdd9827ea2a90b6a2b1ee4415cf
Simontagbor <simontagbor360@gmail.com>
2023-08-01 13:29:36 -0700

    Add the first endpoint (route) to return the status of the API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-from . import states
+from . import *
+from flask import Blueprint
+# from . import states
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard import of everything in the package api.v1.views.index
