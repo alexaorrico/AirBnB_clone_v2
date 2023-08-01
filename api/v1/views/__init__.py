d1c37d3b5f6207798885996a82d9dd95d9464af2
Rashisky <Rashisky007@gmail.com>
2023-08-01 12:12:04 +0100

    RESTful API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,6 @@
 
 from flask import Blueprint
 from models.state import State
-from api.v1.views import states
+from api.v1.views.states import *
 
 app_views = Blueprint('app_view', __name__, url_prefix='/api/v1')
