4f33da0a8ec93cb875b2e42d52b9c48558019966
JEPTUI <alicejeptui@gmail.com>
2023-08-01 11:38:19 -0700

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,15 +10,3 @@ from models import storage
 def status():
     """returns a JSON with status OK"""
     return jsonify({'status': 'OK'})
-
-
-@app_views.route('/stats')
-def stats():
-    """Return number of each instance type"""
-    return jsonify(
-            amenities=storage.count("Amenity"),
-            cities=storage.count("City"),
-            places=storage.count("Place"),
-            reviews=storage.count("Review"),
-            states=storage.count("State"),
-            users=storage.count("User"))
