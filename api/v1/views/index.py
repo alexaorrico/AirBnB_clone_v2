55b690f775e1bff054a5a3145c70657af19f0d0f
JEPTUI <alicejeptui@gmail.com>
2023-08-01 11:59:16 -0700

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,16 +17,12 @@ def stats():
     '''
         return counts of all classes in storage
     '''
-    if request.method == 'GET':
-        response = {}
-        obj_counts = {
-                "Amenity": "amenities",
-                "City": "cities",
-                "Place": "places",
-                "Review": "reviews",
-                "State": "states",
-                "User": "users"
-                }
-        for key, value in obj_counts.items():
-            response[value] = storage.count(key)
-            return jsonify(response)
+    obj_counts = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(obj_counts)
