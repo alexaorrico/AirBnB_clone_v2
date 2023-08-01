3742e4ef24553ef143df9778736e9866f9caa7ab
CyrusNchege <cyruschegecc@gmail.com>
2023-08-01 23:37:33 +0300

    Refactor my apis

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python3
+'''module api/v1/views/__init__.py:
+creates a Blueprint instance with `url_prefix` set to `/api/v1`
+register the routes for app_views
+'''
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+from api.v1.views.states import *
