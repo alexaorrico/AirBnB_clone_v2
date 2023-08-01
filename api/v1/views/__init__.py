6028f2c950f7f594b39978f5d9005bb6a277304b
chimekinglsey <onlinekingsley@gmail.com>
2023-08-01 00:21:44 +0000

    New Commit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,4 +7,5 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views import states, cities, users, places, places_reviews
+from api.v1.views.places_amenities import *
 from api.v1.views import amenities
