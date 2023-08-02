a9d8c355b6b941f8d633fcf69812b6961f8f6e65
mwangiii <mwangi8kevin@gmail.com>
2023-08-02 13:49:59 +0300

    merges

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 
-""" 
-imports all the functions and classes from 
-states module within the api.v1   views package
-"""
-from api.v1.states import *
-
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
