9cdca8e81794af2b875dee3b0ba0cd7d25df463b
Obianuju23 <obianujunmoh@gmail.com>
2023-08-02 04:34:42 -0700

    update deleted flasger,utils

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,7 +8,6 @@ from models.place import Place
 from models.user import User
 from models.state import State
 from models.amenity import Amenity
-from flasgger.utils import swag_from
 
 
 @app_views.route("/cities/<city_id>/places", methods=["GET"],
