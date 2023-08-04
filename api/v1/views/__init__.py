e1c1818f2c23869d31955551a841b7fadaeb1dac
JEPTUI <alicejeptui@gmail.com>
2023-08-04 02:36:27 -0700

    Hot fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,18 @@ from flask import Blueprint
 # Create a Blueprint instance
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
+
 # Import flask views defined in this package
 from api.v1.views.index import *
 from api.v1.views.states import *
-# from api.v1.views.cities import *
+from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
