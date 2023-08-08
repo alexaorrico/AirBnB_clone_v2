013e5283b001dbf2011b11a757438f84ab8bae06
Ekenimeno Akpaiya <ekenimenoakpaiya@gmail.com>
2023-08-08 08:50:08 -0700

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Flask Application """
-from models import storage
+from models import file_storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
