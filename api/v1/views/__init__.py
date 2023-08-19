d355453b68e55d113c521c28c9f355b05f4e1472
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-19 11:08:23 -0700

    __init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+
 from flask import Blueprint
 """
 create a route /status on the object app_views
 that returns a JSON: "status": "OK"
 """
+from flask import Blueprint
+
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1') 
+
 
 from api.v1.views.index import *
 from api.v1.views.states import *
