1e50c1debf30a62f98a7b82f165ade1a7bfae4d3
andrewgilbert2018 <andrewgilbert2018@gmail.com>
2023-08-19 07:32:58 -0700

    app

---
+++ api/v1/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+create a variable app, instance of Flask
+import storage from models
+import app_views from api.v1.views
+register the blueprint app_views to your Flask instance app
+declare a method to handle @app.teardown_appcontext
+that calls storage.close()
+"""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_app(self):
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """custom page not found msg"""
+    message = {"error": "Not found"}
+    return jsonify(message), 404
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port)
