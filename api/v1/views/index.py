5a2ee1ac70b225704852634b5c44f00d256a4143
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-21 21:29:35 +0000

    task 4 api inits app and index

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Index
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """
+    Returns:
+        Status of API
+    """
+    return jsonify({"status": "OK"})
