1ef54c03c975fdb9bdb329aefbc909fd2ed9d8f6
Patricio-Benglian <6305@holbertonstudents.com>
2023-08-22 09:04:37 -0700

    Stats method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,30 @@
 #!/usr/bin/python3
 """index module"""
 from api.v1.views import app_views
+from flask import jsonify  # Dict is not appearing as dict
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
 def status():
+    """returns status"""
     return {"status": "OK"}
+
+
+@app_views.route("/stats")
+def stats():
+    """returns count of each class"""
+    return {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
