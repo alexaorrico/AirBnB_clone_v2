b2fd7fd9665e3b11feac76550105d051d76545bc
micaelapicco <micaelaabril421@gmail.com>
2023-08-22 09:44:29 -0700

    add count on index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """index"""
 
 from api.v1.views import app_views
+from models import storage
+from models.state import State                                                  
+from models.amenity import Amenity                                              
+from models.city import City                                                    
+from models.user import User                                                    
+from models.place import Place
+from models.review import Review
 
 
 @app_views.route('/status')
@@ -10,3 +17,16 @@ def status():
     return {
         "status": "OK"
         }
+
+@app_views.route('/stats')
+def count():
+    """return counts"""
+    return {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    
