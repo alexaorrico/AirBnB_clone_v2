2aaf8aa6ebabd4d2d1e490b6af1489419697cd73
quentinrouger <quentin_rouger@hotmail.fr>
2023-08-22 09:36:57 +0200

    Add: Get and count to filstorage and dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,20 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class name and its ID, or None"""
+        if cls is None or id is None:
+            return None
+        objs = self.__session.query(classes[cls]).all()
+        for obj in objs:
+            if obj.id == id:
+                return obj
+        return None
+    
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class"""
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
