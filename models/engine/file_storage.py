1524a732f7dcae8664e7926303b91e0b21f2b2b0
nachofen <zorba037@gmail.com>
2023-08-23 07:55:40 -0700

    added get and count functions for dbstorage and filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        for obj in self.all(cls).values():
+            if obj.id == id:
+                return obj
+        return None
+
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        if cls is None:
+            return len(self.all())
+        else:
+            return len(self.all(cls))
