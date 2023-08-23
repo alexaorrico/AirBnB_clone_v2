4fcb1a194b8ab4e1e67e7176f3d87454d6a8cb7b
root <priveiro@gmail.com>
2023-08-23 10:37:48 -0700

    adding documentation in index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+""" views in app_views """
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
 def app_views_status():
+    """ displays a status message """
     return jsonify({"status": "OK"})
