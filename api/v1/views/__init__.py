376faf6fcf0d7ee94e47ec8390a877d03d077ca6
nairbh <houcinenair@proton.me>
2023-08-23 18:35:11 +0200

    add GET methode

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Blueprint for API"""
+
+from api.v1.views.index import index
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import index
-
-app_views.add_url_rule('/index', view_func=index, methods=['GET'], strict_slashes=False)
+app_views.add_url_rule('/index', view_func=index,
+                       methods=['GET'], strict_slashes=False)
 app_views.register_blueprint(state_blueprint)
