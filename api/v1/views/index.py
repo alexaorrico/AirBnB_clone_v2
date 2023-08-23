e900f5a627ea77821b5ccd79f318d9b931bb757c
root <priveiro@gmail.com>
2023-08-23 11:32:51 -0700

    task 5 listing count of elements

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,27 @@
 """ views in app_views """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.engine.db_storage import classes
 
 
 @app_views.route('/status', strict_slashes=False)
 def app_views_status():
     """ displays a status message """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ displays some stats """
+    asked_classes = {
+                    "amenities": storage.count(classes['Amenity']), 
+                    "cities": storage.count(classes['City']), 
+                    "places": storage.count(classes['Place']), 
+                    "reviews": storage.count(classes['Review']), 
+                    "states": storage.count(classes['State']), 
+                    "users": storage.count(classes['User'])
+                    }
+    return jsonify(asked_classes)
+
+
+
