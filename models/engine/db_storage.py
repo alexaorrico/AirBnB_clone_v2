198a1ead40c98f728e57552b9e8930652c463370
Johnatan Ortiz <6403@holbertonstudents.com>
2023-08-23 11:31:34 -0500

    Test for db_storange for the method get and count, and for other files fixed PEP8

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,7 +76,8 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """Returns the object based on the class and its ID, or None if not found"""
+        """Returns the object based on the class and its ID,
+        or None if not found"""
         key = cls.__name__ + "." + id
         if self.all()[key]:
             return self.all()[key]
