ff1b1fc820bab5e9daff1c21f6dc27df31a0b099
nachofen <124267664+nachofen@users.noreply.github.com>
2023-08-23 12:31:08 -0700

    fixed circular import

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_session(arg=None):
+def close_session(exception):
     "Close the session after each request"
     storage.close()
 
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
-from api.v1.views.index import status
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+if app_views is not None:
+    from api.v1.views.index import *
