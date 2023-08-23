a51150f0dcf80bdd4d1216cc615c0a982060db40
nairbh <houcinenair@proton.me>
2023-08-23 19:11:20 +0200

    update init file for circular module

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Blueprint for API"""
+""" Init file """
 
-from api.v1.views.index import index
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-app_views.add_url_rule('/index', view_func=index,
-                       methods=['GET'], strict_slashes=False)
-app_views.register_blueprint(state_blueprint)
+from api.v1.views.index import *
+from api.v1.views.states import *
