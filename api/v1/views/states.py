ad03509ae84d100d52f0d1c6225d9b564b3eecae
nairbh <houcinenair@proton.me>
2023-08-23 18:40:21 +0200

    fixe doc

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ View for State objects that handles default API actions """
+
+
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
