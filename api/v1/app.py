c04b335caeff11c033f1106aaf31a4ba1cc2da34
MateoOlv <6304@holbertonstudents.com>
2023-08-23 07:25:15 -0700

    v1

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from os import environ
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+def close(err):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=environ.get("HBNB_API_HOST", "0.0.0.0"),
+            port=environ.get("HBNB_API_PORT", "5000"), threaded=True)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return {
+        "error": "Not found"
+    }, 404
