ddce9672ca37908776eac2ce4115e48ee097f4d0
RoPallas <rocio.orquestas@gmail.com>
2023-08-23 06:38:39 -0700

    Add Function get on DB_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,11 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+    
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not found"""
+        class_dict = self.all(cls)
+        for obj in class_dict:
+            if obj.id == id:
+                return obj
+        return None
