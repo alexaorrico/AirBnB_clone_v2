1e70388c006a0870722b55cbe95ca41dff3f47f5
nachofen <zorba037@gmail.com>
2023-08-23 10:16:34 -0700

    trying to fix circular importation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """run script"""
 from models import storage
-from api.v1.views import app_views
 from flask import Flask
 from os import getenv
 
@@ -18,6 +17,7 @@ def close_session(arg=None):
 
 if __name__ == "__main__":
     """run the app"""
+    from api.v1.views import app_views
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
