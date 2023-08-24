7865a11865e6b01b03fc8f6b6b29976abdea84ee
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-08-24 17:10:55 -0500

    Update and rename __init.py__ to __init__.py

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,13 @@
+
+from flask import Blueprint
+
+app_views = Blueprint('api', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.users import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.states import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.index import *
