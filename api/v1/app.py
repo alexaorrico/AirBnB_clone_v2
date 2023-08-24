9bdadbb01bbf74ed668db49b6875beec2b6942a8
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-08-24 14:14:46 -0700

    task 5 and 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 starts a Flask web application
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 import os
 from api.v1.views import app_views
 from models import storage
@@ -14,6 +14,10 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """closes the storage on teardown"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,3 +6,14 @@ from models import storage
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    dic = { "amenities": "Amenity", "cities": "City",
+            "places": "Place", "reviews": "Review",
+            "states": "State", "users": "User"}
+    
+    ret = {}
+    for k, v in dic.items():
+        ret[k] = storage.count(v)
+    return jsonify(ret)
