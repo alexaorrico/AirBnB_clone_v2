7fb3a55f563f6abb379c3fa0202effcc0a5db5ff
Axel Malka <axelmalka@outlook.fr>
2023-08-24 12:09:23 +0000

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """A first api app"""
 
-
 from flask import Flask, jsonify, Blueprint
 from api.v1.views import app_views
 from models import storage
@@ -9,6 +8,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def treardown(exeption):
