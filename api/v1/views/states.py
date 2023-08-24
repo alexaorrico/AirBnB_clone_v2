36ba537e5efe4bec8143ec6057a188782493ddb5
TheWhiteMachine <priveiro@gmail.com>
2023-08-24 09:21:46 -0700

    task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,7 @@
 
 from models.state import State
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
 
 
@@ -16,6 +16,7 @@ def get_all_states():
         list_of_states.append(states[state].to_dict())
     return jsonify(list_of_states)
 
+
 @app_views.route('/states/<state_id>', methods=['GET'])
 def get_state_by_id(state_id):
     """ Return a state based on state_id """
@@ -23,6 +24,7 @@ def get_state_by_id(state_id):
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
+
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
@@ -34,3 +36,32 @@ def delete_state(state_id):
     storage.save()
     return jsonify({}), 200
 
+
+@app_views.route('/states/', methods=['POST'])
+def create_state():
+    """ Create new state"""
+    new_state = request.get_json()
+    if not new_state:
+        abort(400, "Not a JSON")
+    if "name" not in new_state:
+        abort(400, "Missing name")
+    state = State(**new_state)
+    storage.new(state)
+    storage.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """ Update an exiting state"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    body = request.get_json()
+    if not body:
+        abort(400, "Not a JSON")
+    for key, value in body.items():
+        if key != 'id' and key != 'created_at' and key != 'updated_at':
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
