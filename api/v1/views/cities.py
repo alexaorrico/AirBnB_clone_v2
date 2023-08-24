5b9a5970d4597358039ecf90e980b8e56018439c
nachofen <zorba037@gmail.com>
2023-08-24 10:03:56 -0700

    added put for city

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -56,3 +56,19 @@ def create_city(state_id):
     storage.new(new_city)
     storage.save()
     return make_response(jsonify(new_city.to_dict()), 201)
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def update_state(city_id):
+    """Updates a State object by ID"""
+    city = storage.get(State, city_id)
+    if city is None:
+        return make_response(jsonify({'error': 'not found'}), 404)
+    request_data = request.get_json()
+    if not request_data:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    for key, value in request_data.items():
+        if key not in ['id', 'state_id' 'created_at', 'updated_at']:
+            setattr(city, key, value)
+    storage.save()
+    return make_response(jsonify(city.to_dict()), 200)
