0409a8d5cb2f695e9191ab9d0bdec2dc3245a080
Houcine <113390396+nairbh@users.noreply.github.com>
2023-08-24 20:47:59 +0200

    Update places_reviews.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -32,12 +32,16 @@ def delete_review(review_id):
 
 @app_views.route('/places/<place_id>/reviews', methods=['POST'], strict_slashes=False)
 def post_review(place_id):
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
     data = request.get_json()
     if not data:
         abort(400, "Not a JSON")
     if 'user_id' not in data:
         abort(400, "Missing user_id")
-    if not storage.get(User, data['user_id']):
+    user = storage.get(User, data['user_id'])
+    if not user:
         abort(404)
     if 'text' not in data:
         abort(400, "Missing text")
@@ -45,6 +49,7 @@ def post_review(place_id):
     new_review = Review(**data)
     new_review.save()
     return jsonify(new_review.to_dict()), 201
+
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def put_review(review_id):
