901acdf763857c118639174c9ba2d56474b42dca
micaelapicco <micaelaabril421@gmail.com>
2023-08-24 08:56:32 -0700

    fix put

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -8,7 +8,7 @@ from models import storage
 
 @app_views.route('/amenities/<amenity_id>', methods=['GET'])
 @app_views.route('/amenities', defaults={'amenity_id': None}, methods=['GET'])
-def retrives_amenit(amenity_id):
+def retrives_amenity(amenity_id):
     """Retrives the list of all amenitys"""
     if amenity_id is None:
         return jsonify([
@@ -17,11 +17,11 @@ def retrives_amenit(amenity_id):
     if storage.get(Amenity, amenity_id) is None:
         abort(404)
 
-    return jsonify([storage.get(Amenity, amenity_id).to_dict()])
+    return jsonify(storage.get(Amenity, amenity_id).to_dict())
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
-def delete_amenit(amenity_id):
+def delete_amenity(amenity_id):
     """Delete amenity"""
     amenitys = storage.get(Amenity, amenity_id)
     if amenitys is None:
@@ -32,21 +32,21 @@ def delete_amenit(amenity_id):
 
 
 @app_views.route('/amenities', methods=['POST'])
-def create_amenit():
+def create_amenity():
     """creates a new amenity"""
     json_data = request.get_json()
     if json_data is None:
         abort(400, 'Not a JSON')
     if 'name' not in json_data:
         abort(400, 'Missing name')
-    amenity = amenity(**json_data)
+    amenity = Amenity(**json_data)
     amenity.save()
     # return a tuple default(data, status)
     return jsonify(amenity.to_dict()), 201
 
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'])
-def update_amenit(amenity_id):
+def update_amenity(amenity_id):
     """update a amenity"""
     json_data = request.get_json()
     if json_data is None:
@@ -56,6 +56,6 @@ def update_amenit(amenity_id):
         abort(404)
     for key, values in json_data.items():
         if key not in ('id', 'created_at', 'updated_at'):
-            setattr(Amenity, key, values)
+            setattr(amenity, key, values)
     amenity.save()
     return jsonify(amenity.to_dict()), 200
