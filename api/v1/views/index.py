80cda243d087b6905a7e6dcad3d61ce973992bfc
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-24 16:44:42 -0500

    update the files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,26 @@ from flask import jsonify
 def get_status():
     """Returns the app status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'],  strict_slashes=False)
+def status():
+    """Retrieves the number of objects per each type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    from models import storage
+    import json
+    dictionary = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    json_dictionary = json.dumps(dictionary, indent=2)
+    return json_dictionary
