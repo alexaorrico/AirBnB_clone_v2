1a439f5112cc7cddc1663e0833d5790a7b62ae32
semaed <figsaned@gmail.com>
2023-08-24 09:04:43 -0400

    tasks 4, 5 and 6: Status of your API, Some stats? and Not found

---
+++ api/v1/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Script to start the API"""
+
+
+from models.storage import storage
+from api.v1.views import app_views
+from os import environ
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close storage"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 error"""
+    return jsonify(error="Not found"), 404
+
+
+if __name__ == "__main__":
+    """Main"""
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, thread=True)
