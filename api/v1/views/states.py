4cb2bf1034b90aeccf5f6f5fb27e0145b6a16295
Houcine <113390396+nairbh@users.noreply.github.com>
2023-08-24 12:16:58 +0200

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,7 +13,6 @@ from flasgger.utils import swag_from
 
 
 @app.route('/api/v1/states', methods=['GET'])
-@swag_from('documentation/state/get_state.yml', methods=['GET'])
 def get_states():
     """ define get states """
     states = storage.all(State)
@@ -21,7 +20,6 @@ def get_states():
 
 
 @app.route('/api/v1/states/<state_id>', methods=['GET'])
-@swag_from('documentation/state/get_id_state.yml', methods=['get'])
 def get_state(state_id):
     """ define id for states """
     state = storage.get(State, state_id)
@@ -31,7 +29,6 @@ def get_state(state_id):
 
 
 @app.route('/api/v1/states/<state_id>', methods=['DELETE'])
-@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
 def delete_state(state_id):
     """ delete request """
     state = storage.get(State, state_id)
@@ -43,7 +40,6 @@ def delete_state(state_id):
 
 
 @app.route('/api/v1/states', methods=['POST'])
-@swag_from('documentation/state/create_state.yml', methods=['POST'])
 def create_state():
     """ create states """
     data = request.get_json()
@@ -58,7 +54,6 @@ def create_state():
 
 
 @app.route('/api/v1/states/<state_id>', methods=['PUT'])
-@swag_from('documentation/state/update_state.yml', methods=['PUT'])
 def update_state(state_id):
     """ update states """
     state = storage.get(State, state_id)
