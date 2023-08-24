0910b41869ee4a310b164d91515a622179170134
Houcine <113390396+nairbh@users.noreply.github.com>
2023-08-24 11:46:14 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,6 +5,6 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from . import index
-from . import states
-from . import cities
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
