e535be3bbd70c26dcacee9c3b5a808983c3a1e65
Axel Malka <axelmalka@outlook.fr>
2023-08-24 10:11:12 +0000

    start API using flask

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A first api app"""
+
+from flask import Flask, jsonify
+from api.v1.views import app_views
+from models import storage
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def treardown(exeption):
+    storage.close()
+
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/view/index.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    return jsonify({"status": "OK"})
