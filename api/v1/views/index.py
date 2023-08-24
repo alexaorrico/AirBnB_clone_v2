a94e52551250bad430d77749305db0c76d0a4823
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-08-24 17:30:44 -0500

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""Flask application that retrieves information"""
+""" This is the index file for  flask app"""
+
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
-def status():
-    """Returns the app status"""
+@app_views.route('/status', strict_slashes=False)
+def api_status():
+    """a function to return api status"""
+
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", methods=["GET"], strict_slashes=False)
-def stats():
-    """Retrieves the number of objects per each type"""
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-    from models import storage
-    import json
-    dic = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
+@app_views.route('/stats', strict_slashes=False)
+def obj_stats():
+    """returns the number of each object"""
+
+    my_dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
     }
-    json_dict = json.dumps(dic, indent=2)
-    return json_dict
+    return jsonify(my_dict)
