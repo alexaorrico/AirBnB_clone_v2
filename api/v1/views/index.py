4855d4d996df98853a5c51296da78069be640dae
YanielRamos <yanielramos0504@gmail.com>
2023-08-24 16:40:04 -0400

    returning a {} with amount of objects of each clas

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,29 @@
 """Module that have the route of app_views"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def hbnbstatus():
     """function that return json on status route"""
     return jsonify({'status': "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats_hbnb():
+    """function: retrieves a dictionary with the amount of objects"""
+    return{
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Place),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
