16848b4f02c715aac4ad5a547697f51dea1f60fe
DarianGrabino <dariangra@gmail.com>
2023-08-24 16:57:24 -0700

    new view for State objects

---
+++ api/v1/views/states.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""State views for API"""
+
+
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'])
+def get_states():
+    """Return states list in format JSON"""
+    states = storage.all(State)
+    dict_states = []
+    for state in states.values():
+        dict_states.append(state.to_dict())
+    return jsonify(dict_states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state(state_id):
+    """Return a specific State object"""
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    """Delete state id"""
+    dict_states = storage.all(State)
+    for state in dict_states.values():
+        if state.id == state_id:
+            storage.delete(state)
+            storage.save()
+            return jsonify({}), 200
+    abort(404)
+
+
+@app_views.route('/states', methods=['POST'])
+def create_state():
+    """Creates a State"""
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    new_state = State(**data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """Updates given state"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    storage.save()
+    return jsonify(state.to_dict())
