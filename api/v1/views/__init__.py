3c166650bb5244c4a677a825bc44c7e05a808655
nairbh <houcinenair@proton.me>
2023-08-24 14:38:23 +0200

    init file init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,11 +7,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.cities import *
 from api.v1.views.states import *
 from api.v1.views.index import *
-<<<<<<< HEAD
 from api.v1.views.amenity import *
-from api.v1.views.users import *
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-=======
-from api.v1.views.amenities import *
->>>>>>> 35ab43aa11081adaf409a1af7a92bf5daabfe3ab
