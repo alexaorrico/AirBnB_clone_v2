4843929ee1fe429fba847703241b8d72ac71dbf8
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-24 17:48:13 -0500

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,7 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
     """a function to return api status"""
 
