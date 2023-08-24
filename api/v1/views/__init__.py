cedf16993d142e9676571caf0412cbf22797e80f
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-08-24 17:37:58 -0500

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-"""initialization file for views module"""
-
+"""Script that creates the Blueprint Flask Class and import modules"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
