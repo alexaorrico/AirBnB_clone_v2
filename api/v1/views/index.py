ab025b82a6064945fa495c2997e76d1f61ed3c29
Gaburon <124399167+Gaburon@users.noreply.github.com>
2023-08-24 17:13:08 -0500

    Create index.py

---
+++ api/v1/views/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""First route to display a json object"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route('/status')
+def status():
+    '''status route'''
+    return jsonify(status="OK")
