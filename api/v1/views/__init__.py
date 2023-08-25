57c1144f9dc303e15ef315522a71aad548832f9b
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-08-25 14:56:15 -0700

    task 12

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,3 +8,4 @@ from api.v1.views.states import *  # noqa:E402
 
 from api.v1.views.users import *  # noqa:E402
 from api.v1.views.places import *  # noqa:E402
+from api.v1.views.places_reviews import *  # noqa:E402
