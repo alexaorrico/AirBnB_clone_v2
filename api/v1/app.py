463e91ae7365e2ec1e46679627c9163b9e12b6ca
Patricio-Benglian <6305@holbertonstudents.com>
2023-08-25 04:14:50 -0700

    Fix values

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
-app_cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+app_cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
