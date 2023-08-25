9bf35f12319a8be84a76e4b515900aaf659f9d92
DarianGrabino <dariangra@gmail.com>
2023-08-25 10:40:13 -0700

    import request

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,7 @@
 
 
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 from models import storage
 from models.state import State
 
