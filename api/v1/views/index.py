2b0b2bf5e9d885c553be523904a7d742ea92b650
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-24 22:14:06 -0700

    Task 04

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Flask App"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status", strict_slashes=False)
+def statrus():
+    return jsonify({"status": "OK"})
