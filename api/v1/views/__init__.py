7740d1156ac604a707ce4d08244d4690421b6125
MateoOlv <6304@holbertonstudents.com>
2023-08-25 01:32:36 -0700

    Task 7

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,5 +2,7 @@
 from flask import Blueprint
 
 
-app_views = Blueprint("/api/v1", __name__)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.amenities import *
---
+++ api/v1/views/states.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+from models.state import State
+from flask import abort, jsonify, request
+from api.v1.views import app_views
+from models import storage
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def getstatelist():
+    """
+    Return State list
+    """
+    states = []
+    states_list = storage.all(State)
+    states_list = [state.to_dict() for state in states.values()]
+    return jsonify(states_list)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def getstate1(state_id):
+    """
+    Return 404 if there is not a state object
+    """
+    state = State.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(storage.get(State, state_id).to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def deletestate(state_id):
+    """
+    Delete state by id
+    """
+    state = State.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(states)
+    storage.save()
+    return {
+
+    }, 200
+
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def create_state():
+    """
+    Create a new State
+    """
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    new_state = State(**data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put(self, state_id):
+    state = State.query.get(state_id)
+    if state is None:
+        abort(404)
+        
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+        
+    ignored_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignored_keys:
+            setattr(state, key, value)
+        
+    db.session.commit()
+    return jsonify(state.to_dict()), 200
