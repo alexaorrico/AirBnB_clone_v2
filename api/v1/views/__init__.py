b0abdfabafc9d6c338f645c1fe86e2a0eb860fd1
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-25 15:37:50 -0400

    adding count routes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,6 @@ Blueprint instance
 """
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__,
                       url_prefix='/api/v1')
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,30 @@
 Status route on app_views
 """
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
+    """Returns json status of route"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """"Retrieves dictionary obj count"""
+    return {
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'place': storage.count(State),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+    }
