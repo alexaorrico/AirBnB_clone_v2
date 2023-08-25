711977e2b64bed13a8194c09289e923989e0a619
semaed <figsaned@gmail.com>
2023-08-25 14:43:25 -0400

    Update and correction of Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Script to start the API"""
 
-
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, jsonify
-from flask_cors import CORS
-
+from flask import Flask
+"""from flask_cors import CORS"""
 
 
 app = Flask(__name__)
@@ -16,28 +14,21 @@ app.register_blueprint(app_views)
 
 
 # Create a CORS instance with permissive settings (for demonstration purposes)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
+"""cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})"""
 
 
 @app.teardown_appcontext
-def close_db(error):
+def close_db(exception):
     """Close storage"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
+def not_found(exception):
     """404 error"""
-    return jsonify(error="Not found"), 404
+    return {"error": "Not found"}
 
 
 if __name__ == "__main__":
     """Main"""
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, thread=True)
+    app.run(host='0.0.0.0', port=5000)
