a3169c1c94b8d30620415731b73db0602f008066
grem62 <gremontmatheo22@gmail.com>
2023-08-25 10:59:21 +0200

    message de validation

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """
-Blueprint for API
+Initialize views package
 """
 from flask import Blueprint
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+app_views = Blueprint('app_views', __name__)
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.user import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
