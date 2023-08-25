6e4fcaa0a6506f466d81d97436cf489d2423c178
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-25 09:41:45 +0000

    task 13 plus docs

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,16 +2,22 @@
 """
 Flask App
 """
-
 from models import storage
 from api.v1.views import app_views
+from os import environ
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+# Register the blueprint containing the API routes
+app.register_blueprint(app_views)
+# Setup CORS to allow requests from any origin
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
+# Teardown app context to close database connection
 @app.teardown_appcontext
 def close_db(error):
     """
@@ -23,7 +29,7 @@ def close_db(error):
 @app.errorhandler(404)
 def not_found():
     """
-
+    404 error handler
     """
     return make_response(jsonify({"error": "Not found"}), 404)
 
@@ -34,10 +40,9 @@ if __name__ == "__main__":
     """
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
-
+    # set default host and port
     if not host:
         host = '0.0.0.0'
-
     if not port:
         port = '5000'
 
