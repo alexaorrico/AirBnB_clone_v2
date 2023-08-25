dc0508612acd87f0192190d6b4ce77115c6deb0a
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-08-25 12:06:01 -0700

    fix pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,18 +4,20 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Get status"""
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     """Get stats"""
-    dic = { "amenities": "Amenity", "cities": "City",
+    dic = {"amenities": "Amenity", "cities": "City",
             "places": "Place", "reviews": "Review",
             "states": "State", "users": "User"}
-    
+
     ret = {}
     for k, v in dic.items():
         ret[k] = storage.count(v)
