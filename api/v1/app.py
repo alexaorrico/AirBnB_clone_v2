ddc4a8169eaaad4aa2e649778df47d032bdeedc3
Washx23 <5731@holbertonstudens.com>
2023-08-25 15:37:19 -0700

    fix errno

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """module"""
 
+
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
-def teardown_app_context(exception):
-    """Closes the storage on teardown."""
+def teardown_db(exception):
+    """Close the current SQLAlchemy session after each request."""
     storage.close()
 
+
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,4 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Wildcard import for views in this package
 from api.v1.views.index import *
