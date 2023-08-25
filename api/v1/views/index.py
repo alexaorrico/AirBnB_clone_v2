7af87ebbba359869737a73a574e08d6a188f8bd5
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-08-24 22:32:05 -0700

    Task 05

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,22 @@
 """ Flask App"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", strict_slashes=False)
 def statrus():
     return jsonify({"status": "OK"})
+
+@app_views.route("/api/v1/stats", strict_slashes=False)
+def stats():
+    """ """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
