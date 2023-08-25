d89c74efa740aab9c54e06924f9f846cc718c495
Spark4545 <lerouxpaulhenri@gmail.com>
2023-08-25 12:27:02 +0000

    c'mon

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,35 +3,35 @@
 Index
 """
 from api.v1.views import app_views
-from flask import jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.state import State
-from models.review import Review
-from models.place import Place
-from models.user import User
+from flask import jsonify, request
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
     """
     Returns:
         Status of API
     """
-    return jsonify({"status": "OK"})
+    if request.method == 'GET'
+        response = {"status": "OK"}
+        return jsonify(response)
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objs():
     """
-
+    Endpoint
     """
-    classes = [Amenity, City, State, Review, Place, User]
-    names = ["amenities", "cities", "places", "reviews", "states", "users"]
-
-    objs = {}
-    for index in range(len(classes)):
-        objs[names[index]] = storage.count(classes[index])
-
-    return jsonify(objs)
+    response = {}
+    NAME = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    for key, value in NAME.items():
+        response[value] = storage.count(key)
+    return jsonify(response)
