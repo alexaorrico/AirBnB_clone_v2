bfff480ecdfd58eb518011f2944d22cec548ebf6
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-08-25 13:30:27 -0700

    task 10

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,3 +4,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *  # noqa:E402
 from api.v1.views.states import *  # noqa:E402
+from api.v1.views.users import *  # noqa:E402
