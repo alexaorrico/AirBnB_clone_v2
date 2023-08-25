a8124db6569acabcf0ba5fc6653e707e0307e3b2
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-25 14:03:12 -0400

    fixing typo

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, resources={r'/api/v1/':{"origins": "*"}})
+CORS(app, resources={r'/api/v1/*':{"origins": "*"}})
 app.register_blueprint(app_views)
 
 
