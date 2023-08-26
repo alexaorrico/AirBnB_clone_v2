9d3c342090e32532ee623d45fc18d7f7d43789e5
EmilianoGarin <60578409+EmilianoGarin@users.noreply.github.com>
2023-08-26 14:11:38 -0700

    task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,14 @@
 starts a Flask web application
 """
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 import os
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
