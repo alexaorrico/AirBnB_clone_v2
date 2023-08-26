853d5929cb34258fd8ff1c043cffd04682253b30
YanielRamos <yanielramos0504@gmail.com>
2023-08-25 21:50:02 -0400

    updating amenity

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Module that creates the blueprint of app_views"""
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
