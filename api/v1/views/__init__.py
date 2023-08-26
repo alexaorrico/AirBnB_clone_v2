9111a4f6018c83ad35e94362e892c106c8c60cba
DarianGrabino <dariangra@gmail.com>
2023-08-25 17:57:39 -0700

    add import cities and amenities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,8 +4,8 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *  # noqa:E402
 from api.v1.views.states import *  # noqa:E402
-
-
+from api.v1.views.cities import *  # noqa:E402
+from api.v1.views.amenities import *  # noqa:E402
 from api.v1.views.users import *  # noqa:E402
 from api.v1.views.places import *  # noqa:E402
 from api.v1.views.places_reviews import *  # noqa:E402
