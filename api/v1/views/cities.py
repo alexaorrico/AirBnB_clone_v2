fcb641b2f94c14096bba57560e1c2f70d0d56a78
Santiago E <santy.e@gmail.com>
2023-08-27 13:32:12 -0600

    Fixes PEP8 validations

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,7 @@ from models.city import City
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'],
-                    strict_slashes=False)
+                 strict_slashes=False)
 def get_cities(state_id):
     """Retrieves the list of all City objects of a State"""
     state = storage.get(State, state_id)
@@ -20,7 +20,7 @@ def get_cities(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'],
-                    strict_slashes=False)
+                 strict_slashes=False)
 def get_city(city_id):
     """Retrieves a City object"""
     city = storage.get(City, city_id)
@@ -30,7 +30,7 @@ def get_city(city_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'],
-                    strict_slashes=False)
+                 strict_slashes=False)
 def delete_city(city_id):
     """Deletes a City object"""
     city = storage.get(City, city_id)
@@ -42,7 +42,7 @@ def delete_city(city_id):
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
-                    strict_slashes=False)
+                 strict_slashes=False)
 def create_city(state_id):
     """Creates a City"""
     state = storage.get(State, state_id)
@@ -60,7 +60,7 @@ def create_city(state_id):
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'],
-                    strict_slashes=False)
+                 strict_slashes=False)
 def update_city(city_id):
     """Update a City"""
     city = storage.get(City, city_id)
