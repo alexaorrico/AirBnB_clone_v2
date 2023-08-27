0c0850199bc6426d0246ce2cff9eeabf579c6bf6
Santiago E <santy.e@gmail.com>
2023-08-27 13:25:36 -0600

    Fix codestyle and add shebang

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,33 +1,42 @@
+#!/usr/bin/python3
 from flask import request, jsonify, abort
 from api.v1.views import app_views
 from models.city import City
 from models.state import State
 
+
 @app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def get_state_cities(state_id):
+    """Retrieves the list of all City objects of a State"""
     state = State.get(state_id)
     if not state:
         abort(404)
     cities = state.cities
     return jsonify([city.to_dict() for city in cities]), 200
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
+    """Retrieves a City object"""
     city = City.get(city_id)
     if not city:
         abort(404)
     return jsonify(city.to_dict()), 200
 
+
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
+    """Deletes a City object"""
     city = City.get(city_id)
     if not city:
         abort(404)
     city.delete()
     return jsonify({}), 200
 
+
 @app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def create_city(state_id):
+    """Creates a City"""
     state = State.get(state_id)
     if not state:
         abort(404)
@@ -41,8 +50,10 @@ def create_city(state_id):
     city.save()
     return jsonify(city.to_dict()), 201
 
+
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
+    """Updates a City object"""
     city = City.get(city_id)
     if not city:
         abort(404)
