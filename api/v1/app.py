b984f945aa8634da873446b8f9ed311b28f269e4
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-28 11:58:59 -0400

    fixing typo"

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,7 +12,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 # restrictions access for api
-CORS(app, resources={r'/api/v1/*': {"origins": "*"}})
+CORS(app, resources={r'/api/v1/*': {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
