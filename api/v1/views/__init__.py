5be7fce233ea15c33c574d7081df0318232bce77
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-28 11:48:56 -0400

    fixing typo"

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -11,6 +11,6 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.places import *
-from api.v1.views.user import *
+from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
