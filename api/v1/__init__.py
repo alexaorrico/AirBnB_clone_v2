f9d50fa187224c8f6c43faffea6ad62cbaedf288
Alvarez0Jose <josealejandro220@gmail.com>
2023-08-28 11:34:34 -0400

    fixing the __init__

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -5,11 +5,14 @@ init file
 
 
 from flask import Blueprint
-from api.v1.views.index import *
-import api.v1.views.states
-import api.v1.views.cities
-import api.v1.views.amenities
-import api.v1.views.users
-import api.v1.views.places
-import api.v1.views.places_reviews
+
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
