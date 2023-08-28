463677367d0ac38933fe9f2cddb7db80d6295d5e
Nashalys Fernandez <nashalysf@gmail.com>
2023-08-28 11:55:31 -0400

    fixing typo"

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Server file for HBNB version 3
 """
 
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
@@ -25,7 +25,7 @@ def teardown(exception):
 @app.errorhandler(404)
 def not_found(exception):
     """Handles 404 error"""
-    return {'error': "Not found"}
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
