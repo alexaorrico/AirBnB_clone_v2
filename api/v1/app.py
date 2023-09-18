fa39668075977a7064a52c7076a4d720ec746ab4
thomaskitaba <thomaskitaba@gmail.com>
2023-09-18 16:20:02 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,8 +17,6 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flasgger import Swagger
-from flasgger.utils import swag_from
 
 
 app = Flask(__name__)
@@ -36,15 +34,6 @@ def teardown(self):
 def error(e):
     """Handler for 404 errors"""
     return jsonify({"error": "Not found"}), 404
-
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
-
-Swagger(app)
-
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
