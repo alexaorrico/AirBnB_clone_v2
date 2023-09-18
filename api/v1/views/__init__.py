d0ff282dbdec668e85539eb11304e3a22c9013a6
thomaskitaba <thomaskitaba@gmail.com>
2023-09-18 15:50:26 +0300

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,10 +8,6 @@ don’t worry, it’s normal and this file (v1/views/__init__.py)
 won’t be check.
 """
 from flask import Blueprint
-
-
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -20,3 +16,6 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+
+
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
