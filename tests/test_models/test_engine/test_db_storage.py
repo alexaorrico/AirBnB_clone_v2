706776d63b2696b1ccf7223ae93e9739002e1bd6
MiguelBG12 <miguel.bg12@gmail.com>
2023-09-24 21:08:40 -0500

    Added unit test for DBStorage's get method, ensuring object retrieval functionality

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import inspect
 import models
 from models.engine import db_storage
+from models.engine.db_storage import DBStorage
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -67,6 +68,26 @@ test_db_storage.py'])
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
+class TestDBStorageGet(unittest.TestCase):
+    """Test the get method in DBStorage"""
+    def test_get(self):
+        """Test the get method"""
+        # Create an instance of DBStorage
+        storage = DBStorage()
+
+        # Create a test object (e.g., an Amenity instance)
+        test_obj = Amenity(name="Test Amenity")
+        test_obj.save()
+
+        # Use the get method to retrieve the test object
+        retrieved_obj = storage.get(Amenity, test_obj.id)
+
+        # Assert that the retrieved object is not None
+        self.assertIsNotNone(retrieved_obj)
+
+        # Assert that the retrieved object has the correct class and ID
+        self.assertIsInstance(retrieved_obj, Amenity)
+        self.assertEqual(retrieved_obj.id, test_obj.id)
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
