85391b25030c46e8b93d2def855bdedfeba2e3ee
MiguelBG12 <miguel.bg12@gmail.com>
2023-09-26 16:06:50 -0500

    Update pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ This module defines a Flask blueprint for handling status requests.
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
 def api_status():
     """a function to return api status"""
