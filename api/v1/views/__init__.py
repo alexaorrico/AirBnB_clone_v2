0606e0b5225a705c226c33eaf4acc38afb701c81
MiguelBG12 <miguel.bg12@gmail.com>
2023-09-26 16:52:26 -0500

    Update File to initialize for the views module

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-This module defines a Flask Blueprint for handling API version 1 views.
+File to initialize for the views module
 """
 from flask import Blueprint
 
 # Create a Blueprint instance with URL prefix '/api/v1'
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/stats')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/')
 
-# Wildcard import from 'index' module within 'views'
-from . import index
+from api.v1.views.index import *
+from api.v1.views.states import *
