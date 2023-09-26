2998403ace042b33d8a7dc73c6e83aa9fc4e2d52
Denzel18rb <74129875rb@gmail.com>
2023-09-26 16:02:17 -0500

    Changes to the index.py and __init__.py files inside the views folder

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,15 +3,16 @@
 This module defines a Flask blueprint for handling status requests.
 """
 
+from api.v1.views import app_views
 from flask import jsonify
-from . import app_views
-from models import storage
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    counts = {}
-    classes = storage.classes()
-    for cls in classes:
-        counts[cls] = storage.count(cls)
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
+def api_status():
+    """a function to return api status"""
+    data = {
+        "status": "OK"
+    }
 
-    return jsonify(counts), 200
+    shojo = jsonify(data)
+    shojo.status_code = 200
+    return shojo
