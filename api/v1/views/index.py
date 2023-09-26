d68df8f0c45ceaedd100b94a738dc9f58dc2b59f
Denzel18rb <74129875rb@gmail.com>
2023-09-25 21:31:57 -0500

    comments correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,13 +2,10 @@
 """
 This module defines a Flask blueprint for handling status requests.
 """
-from flask import jsonify
 
-# We import the route set 'app_views' from the current module.
+from flask import jsonify
 from . import app_views
 
-# We define a route '/status' that responds to GET requests.
 @app_views.route('/status', methods=['GET'])
 def get_status():
-    # We create a JSON response with an "OK" status message.
     return jsonify({"status": "OK"})
