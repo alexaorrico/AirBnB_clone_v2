f202ab4d1b550af5a9c9cea590095dce33f840f9
Denzel18rb <74129875rb@gmail.com>
2023-09-26 16:40:58 -0500

    PEP8 validation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,6 @@ from models.state import State
 from models.user import User
 
 
-
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
 def api_status():
     """a function to restore the state of the API"""
