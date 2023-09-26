84da61c2c3f54c533163c33e2cbaec12bb391b97
MiguelBG12 <miguel.bg12@gmail.com>
2023-09-26 17:16:13 -0500

    Script to initialize Flask app and API routes for managing state objects

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,17 +3,21 @@
 This script initializes a Flask application and sets up the API routes.
 """
 
-from flask import Flask
-import os
-from models import storage
+
 from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+from models import storage
+from models.state import State
+from os import getenv
+
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def close_route(error):
+    "Close the SQLAlchemy Session"
     storage.close()
 
 if __name__ == "__main__":
