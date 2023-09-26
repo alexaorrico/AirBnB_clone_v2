77c2d3c3f2093bf52edff28c6777f437f4ba3e77
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-09-25 20:02:56 -0500

    Fix: Implementation of descriptive comments in the code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 from flask import jsonify
+"""
+    We import the 'jsonify' function from Flask
+    to format the responses in JSON format.
+"""
+# We import the route set 'app_views' from the current module.
 from . import app_views
 
+# We define a route '/status' that responds to GET requests.
 @app_views.route('/status', methods=['GET'])
 def get_status():
+    # We create a JSON response with an "OK" status message.
     return jsonify({"status": "OK"})
