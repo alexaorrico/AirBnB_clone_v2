2d06a9217526407b611b27b54103dcd675bca1fe
Denzel18rb <74129875rb@gmail.com>
2023-09-25 19:07:07 -0500

    Added Initialize DBStorage and load database; commonly used at the beginning of the program.

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -136,3 +136,8 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+
+if __name__ == "__main__":
+    storage = DBStorage()
+    storage.reload()
