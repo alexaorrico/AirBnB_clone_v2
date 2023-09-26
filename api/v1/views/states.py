aac00c16b03319d3bf49d0b00aa23f5b244ec619
MiguelBG12 <miguel.bg12@gmail.com>
2023-09-26 16:58:27 -0500

    Endpoint to retrieve a list of all state objects as JSON

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,3 +4,13 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models.state import State
 from models import storage
+
+
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
+def get_states():
+    """Retrieves the list of all state objects"""
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
