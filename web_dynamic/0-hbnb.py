45645fc5c652e0320eb3a5f50aad6b26520bb2d1
Nashalys Fernandez <nashalysf@gmail.com>
2023-09-27 12:56:09 -0400

    directory setup

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" Starts a Flask Web Application """
 
-
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from os import environ
 from flask import Flask, render_template
 import uuid
 
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def close_db(error):
-    """ Close SQLAlchemy Session """
-    storage.close()
-
 
 @app.route('/0-hbnb', strict_slashes=False)
 def hbnb():
-    """ Render Template """
+  """ Render Template """
 
-    return render_template('0-hbnb.html', cache_id=str(uuid.uuid4()))
+  return render_template('0-hbnb.html', cache_id=str(uuid.uuid4()))
 
 
 if __name__ == "__main__":
-    """ Main Function """
+    """ Run port """
     app.run(host='0.0.0.0', port=5001)
