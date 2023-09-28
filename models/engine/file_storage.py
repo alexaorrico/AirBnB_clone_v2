685ff67dbe778db9ef7364e82ceed859aade4326
Mohamed Amine <ma.abahmane@gmail.com>
2023-09-28 13:46:50 +0100

    RESTful API

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,26 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ 
+            A method to retrieve one object [GET]
+                cls:  class
+                id :  string representing the object ID
+        """
+        if cls in classes.values() and id:
+            objs = self.all(cls)
+
+            for key, value in objs.items():
+                objID = key.split('.')[1]
+                # match ID
+                if (objID == id):
+                    return value
+        return None
+    
+    def count(self, cls=None):
+        """ 
+            A method to count the number of objects in storage
+                cls: class (optional) 
+        """
+        return (len(self.all(cls)))
