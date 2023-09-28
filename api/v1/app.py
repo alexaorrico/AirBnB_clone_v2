b16c37fe9c5641e8aa11449dce7ffa24ae6d13cf
Adooz <kndonake@gmail.com>
2023-09-28 21:19:17 +0100

    added 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,6 +25,12 @@ def teardown_flask(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
+
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     app_port = int(os.getenv('HBNB_API_PORT', '5000'))
