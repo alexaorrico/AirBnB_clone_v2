b7e533b41a26639adbab5b6ec2a5ed14082eb127
EllaRepo <eliaberango@gmail.com>
2023-09-28 16:43:25 -0400

    Update DBStorage and FileStorage, adding two new methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,23 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not
+           found
+        """
+        objs = self.__session.query(classes[cls])
+        for obj in objs:
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+           If no class is passed, returns the count of all objects in storage
+        """
+        count = 0
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                count += len(self.__session.query(classes[clss]).all())
+        return count
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,25 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID, or None if not
+           found
+        """
+        key = "{}.{}".format(cls, id)
+        if key in self.__objects.keys():
+            return self.__objects[key]
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+           If no class is passed, returns the count of all objects in storage
+        """
+        if cls:
+            count = 0
+            for obj in self.__objects.values():
+                if obj.__class__.__name__ == cls:
+                    count += 1
+            return count
+        else:
+            return len(self.__objects)
