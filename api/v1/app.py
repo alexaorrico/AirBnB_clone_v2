1a4c0efa0585536ac19055531b9b453fe8673227
Imukua <ianmukuaa@gmail.com>
2023-09-29 16:12:02 +0300

    added 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """API calls handled here"""
 from flask import Flask
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 from api.v1.views import app_views
@@ -21,3 +22,9 @@ if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors
+       Returns a json formatted error message."""
+    return jsonify({"error": "Not found"}), 404
