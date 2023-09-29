5b25f013fbabb5096dbe2a2fa2bf5060faa7fe39
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-09-30 01:40:08 +0300

    Create index.py
    - Create a route /status on the object app_views that returns a JSON:
      "status": "OK"

---
+++ api/v1/views/index.py
@@ -0,0 +1,16 @@
+"""
+Module for API status view.
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """
+    Retrieves the status of the API.
+    ---
+    responses:
+    status: 200
+    """
+    return jsonify({"status": "OK"})
