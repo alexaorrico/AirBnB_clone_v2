3653135ad10b49f5a472121021480174c9cb9e35
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-09-30 01:38:26 +0300

    Create __init__.py
    - Package initializer for API v1 views
    - Import everything in the package

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,14 @@
+"""
+Package initializer for API v1 views.
+"""
+
+from flask import Blueprint
+from os import path
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# Automatic import of all modules in the current package
+modules = [path.splitext(module)[0] for module in os.listdir(path.dirname(__file__)) if module != '__init__.py']
+__all__ = modules
+
+from api.v1.views import *
