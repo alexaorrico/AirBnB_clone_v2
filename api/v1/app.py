44288fcb057ba7c45599fad4329a8cae8494220e
Imukua <ianmukuaa@gmail.com>
2023-09-29 18:50:35 +0300

    updated 404 error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,14 +17,13 @@ def teardown_appcontext(exception):
     """Terminates the database connection after handling the request"""
     storage.close()
 
-
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=port, threaded=True)
-
 @app.errorhandler(404)
 def not_found(error):
     """Handles 404 errors
        Returns a json formatted error message."""
     return jsonify({"error": "Not found"}), 404
+
+if __name__ == "__main__":
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
