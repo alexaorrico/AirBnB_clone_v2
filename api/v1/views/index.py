29988a22b2dc3e6da8d6f55cd7d7b8c7fc63bf22
Bigiizc <olalekanisaac75@gmail.com>
2023-09-29 23:16:50 +0100

    Update api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,29 @@
 
 
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
 def status():
     return {"status": "OK"}
+
+
+@app_views.route('/stats')
+def stats():
+    result = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return result
