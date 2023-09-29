28a979bc85729909e7acaf7cb60d2e7dcf40ac90
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2023-09-29 17:41:59 +0100

    a

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""first endpoint (route) will be to return the status of your API"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+
+# app_views BluePrint defined in api.v1.views
+app.register_blueprint(app_views)
+
+
+# flask server environmental setup
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+
+@app.route('/hello')
+def hello():
+return 'Hello, World!'
+
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    """Start flask app"""
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""index file"""
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    function for status route that returns the status
+    """
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
