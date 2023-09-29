a833264b7570c3f371323f47d5288534af0e564f
Thomas Collins <collinskthomas@gmail.com>
2023-09-29 14:30:11 +0000

    Fix flask compile bug. Reposition __init__.py, change stat route logic to use classes with count() method instead of strings

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,11 +14,18 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stat():
     """returns the number of each objects by type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
     return jsonify(
-        amenities=storage.count('Amenity'),
-        cities=storage.count('City'),
-        places=storage.count('Place'),
-        reviews=storage.count('Review'),
-        states=storage.count('State'),
-        users=storage.count('User')
+        amenities=storage.count(Amenity),
+        cities=storage.count(City),
+        places=storage.count(Place),
+        reviews=storage.count(Review),
+        states=storage.count(State),
+        users=storage.count(User)
     )
