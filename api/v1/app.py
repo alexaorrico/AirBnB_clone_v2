31fa8576bfce87590e0d39fac39c01b73461f9bd
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-29 19:51:45 +0000

    Add task#5: handle 404 error

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Web server
 """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -17,6 +17,13 @@ def close_storage(exception):
     """Close the storage session."""
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(err):
+    """Handle the 404 not found error."""
+    return jsonify(error="Not found")
+
+
 if __name__ == "__main__":
     HBNB_API_HOST = getenv('HBNB_API_HOST')
     HBNB_API_PORT = getenv('HBNB_API_PORT')
@@ -24,4 +31,4 @@ if __name__ == "__main__":
         HBNB_API_HOST = '0.0.0.0'
     if not HBNB_API_PORT:
         HBNB_API_PORT = '5000'
-    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True, debug=True)
