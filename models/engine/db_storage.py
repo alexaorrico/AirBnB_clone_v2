ed07235df9ab1eb4fb6ebc05388cbc33306af9ef
== <==>
2023-09-29 12:09:21 +0000

    revert to master

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,9 +74,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def get(self, cls, id):
-        """Method to retrieve one object"""
-        if cls is None or id is None:
-            return None
-        
