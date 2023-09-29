197846cb937929081fb1e4910e29163dbe95dbe0
== <==>
2023-09-29 15:10:23 +0000

    API v1 setup

---
+++ api/v1/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+HBNB_API_HOST = '0.0.0.0'
+HBNB_API_PORT = 5000
+app = Flask(__name__)
+app.register_blueprint
+
+
+
+if __name__ == '__main__':
+    app.run(host='HBNB_API_HOST', port=HBNB_API_PORT)
