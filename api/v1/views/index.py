e364e21fcf21ff6c3e62a7fe573a09e3e7f4269a
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-29 19:38:26 +0000

    Add task#4: implement /stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,29 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Return the status of the web server."""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Return the number of each objects by type."""
+    response = {}
+    response['amenities'] = storage.count(Amenity)
+    response['cities'] = storage.count(City)
+    response['places'] = storage.count(Place)
+    response['reviews'] = storage.count(Review)
+    response['states'] = storage.count(State)
+    response['users'] = storage.count(User)
+    return jsonify(response)
