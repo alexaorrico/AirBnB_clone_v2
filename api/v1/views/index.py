ea8e8cd6c63305e1062df244bdc97da9577eb10e
== <==>
2023-09-29 16:05:06 +0000

    Return status code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+@app.route('/status')
+def return_status(app_views):
+    """Return status code"""
+    response = jsonify(app_views)
+    return response['status']
