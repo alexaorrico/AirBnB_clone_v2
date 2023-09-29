39996df1fc18cc62a4c30421553bbe2887fac8e5
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-09-29 10:38:38 +0300

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,34 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import json
+
+classes = {"amenities": Amenity,"cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
+
 
 @app_views.route('/status', methods=['GET'])
 def status():
     data = {
         "status": "OK"
     }
-    response = jsonify(data)
+    response = json.dumps(data, indent=2)
+    response += '\n'
+    return response
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    count_dict = {}
+
+    for key, value in classes.items():
+        count_dict[key] = storage.count(value)
+    
+    response = json.dumps(count_dict, indent=2)
+    response += '\n'
     return response
