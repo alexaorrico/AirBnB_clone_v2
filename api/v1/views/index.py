0490a7e0ac5d06cf0f7cc4b1a9e2236260500531
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-09-30 02:01:16 +0300

    Update api/v1/views/index.py
    - Add new line to follow pycodestyle guidelines

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ Module for API status view.
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """
