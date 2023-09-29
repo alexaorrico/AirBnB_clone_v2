c3df660e1d27f086ea48e5256e22356108042428
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-29 20:15:07 +0000

    fix 404 response

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Web server
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -21,7 +21,7 @@ def close_storage(exception):
 @app.errorhandler(404)
 def not_found(err):
     """Handle the 404 not found error."""
-    return jsonify(error="Not found")
+    return make_response(jsonify(error="Not found"), 404)
 
 
 if __name__ == "__main__":
