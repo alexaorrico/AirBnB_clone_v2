06bbae74fbf3d07ae08ca495dcc4c8417de2bcd1
Meryeme <dobli.meryeme@gmail.com>
2023-09-29 06:22:36 -0700

    fix index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" module index """
+"""
+module index
+"""
 
 from api.v1.views import app_views
 from flask import jsonify
@@ -8,4 +10,4 @@ from flask import jsonify
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ return status """
-    return jsonify(status='ok')
+    return jsonify(status='OK')
