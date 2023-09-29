0b2e0d008e70a6c393c2c633e634c5ee02865d39
israel-09 <israelinene91@gmail.com>
2023-09-29 11:20:31 +0000

    removed blank line

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,9 +13,9 @@ app.register_blueprint(app_views)
 if __name__ == '__main__':
     host = getenv('HBHB_API_HOST')
     port = getenv('HBHB_API_PORT')
-    
     if host is None:
         host = '0.0.0.0'
     if port is None:
         port = 5000
+
     app.run(debug=True, host=host, port=port)
