8fdd1300e8e5755c2fbe15b5a067b2ab5453e5b1
Ukpono <ukpono9@gmail.com>
2023-09-29 14:00:12 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,14 +8,23 @@ from os import getenv
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
+CORS(app, origins=["0.0.0.0"])
+host = getenv("HBNB_API_HOST", "0.0.0.0")
+port = getenv("HBNB_API_PORT", "5000")
 
-if __name__ == '__main__':
-    host = getenv('HBHB_API_HOST')
-    port = getenv('HBHB_API_PORT')
-    if host is None:
-        host = '0.0.0.0'
-    if port is None:
-        port = 5000
 
-    app.run(debug=True, host=host, port=port)
+@app.teardown_appcontext
+def teardown(exception):
+    """Cleanup operations"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    app.run(host, port, threaded=True, debug=True)
