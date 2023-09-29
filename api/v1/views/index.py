00e02ea3214613e772eca040eecb7e5b01c7579f
Imukua <ianmukuaa@gmail.com>
2023-09-29 15:57:05 +0300

    added a stats api route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,21 @@
 """Defines the status route for our API"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status')
 def status():
     """Returns JSON response for status OK """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def stats():
+    """Returns stats of the number of each object by type"""
+    return jsonify({
+        "amenities": storage.count("amenities"),
+        "cities": storage.count("cities"),
+        "place": storage.count("place"),
+        "reviews": storage.count("reviews"),
+        "states": storage.count("states"),
+        "users": storage.count("users")
+    })
