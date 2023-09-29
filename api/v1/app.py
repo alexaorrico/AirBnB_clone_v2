590e2adc2842469506e67e4c9cccc1a21bbf8ea2
Meryeme <dobli.meryeme@gmail.com>
2023-09-29 06:12:33 -0700

    fix pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from models import storage
 from api.v1.views import app_views
 from flask import Flask
@@ -6,15 +8,14 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def downtear(self):
     """ Close the storage when the app context is torn down  """
     storage.close()
 
 
-
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True, debug=True)
-
