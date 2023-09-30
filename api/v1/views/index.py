9890da3e6c5adc594c298d000b0fd78e4d24a015
Pontuagi <benepontuagi@gmail.com>
2023-10-01 02:25:37 +0300

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,9 @@ Creating a blueprint with the URL prefix
 """
 
 from flask import Blueprint, jsonify
+from models import storage
+from models.state import State
+from models.city import City
 
 
 # Create a blueprint with the URL
@@ -16,3 +19,14 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 def get_status():
     """Returns a JSON response indicating the status is OK."""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Retrieve the number of objects by type"""
+    counts = {
+        'State': storage.count(State),
+        'City': storage.count(City),
+    }
+
+    return jsonify(counts)
