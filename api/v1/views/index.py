64a3644e9e0a6aa1dce5b4e94f95c787417d710b
Mohamed Amine <ma.abahmane@gmail.com>
2023-09-30 19:33:22 +0100

    index update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,8 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
-from models import *
+import models
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
