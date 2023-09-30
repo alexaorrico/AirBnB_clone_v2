f565c2f1edd6693814db9c0b74167b8d6e11c5c6
Keith koki <kokikeith@gmail.com>
2023-09-30 15:16:56 -0700

    file fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,10 +6,13 @@ Blueprint for index
 from api.v1.views import app_views
 from flask import jsonify, Blueprint
 from models import storage
-from models.state import State
 
-objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
-           "reviews": 'Review', "states": 'State', "users": 'User'}
+from models.state import State
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
+from models.user import User
+from models.city import City
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -25,6 +28,9 @@ def get_stats():
     """
     Retrieves the number of each object type
     """
+    objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
+               "reviews": 'Review', "states": 'State', "users": 'User'}
+
     objects = {key: storage.count(value) for key, value in objects.items()}
 
     return jsonify(objects)
