bb4846cd008d56487b96e43149cbe9710fa2c94f
Reem-degais <reem.degais.33@gmail.com>
2023-09-30 04:24:43 -0700

    Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,3 +16,17 @@ def status():
     status: 200
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ retrieves the number of each objects by type"""
+    sta = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(sta)
