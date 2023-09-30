c3a5c5bfe09313cec2274f50e6b200d5f9b69311
Keith koki <kokikeith@gmail.com>
2023-09-30 14:33:16 -0700

    file updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,16 @@ def status():
     json object with app status as return
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """
+    Retrieves the number of each object type
+    """
+    objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
+               "reviews": 'Review', "states": 'State', "users": 'User'}
+
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
