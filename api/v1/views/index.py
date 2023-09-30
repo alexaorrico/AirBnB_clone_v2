51d803094d99d9f9084d3b24816c9755cbd443dc
Modeste Ciira <ciiramodeste23@gmail.com>
2023-09-30 18:16:44 +0300

    new api methods

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,29 @@
 """creates status"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+classes = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
 
 
 @app_views.route('/status', strict_slashes=False)
 def hbnb_status():
     """hbnb status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    """hbnb_stats"""
+    obj_stats = {}
+    for k, v in classes.items():
+        obj_stats[k] = storage.count(v)
+    return jsonify(obj_stats)
