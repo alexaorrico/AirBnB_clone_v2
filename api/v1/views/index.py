7f2cdb9f6753a6ca33eaa00bcbe74883a0a72f11
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-30 14:51:28 +0100

    removed global variable

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,15 +7,6 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
-classes = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User"
-        }
-
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
@@ -26,6 +17,15 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """retrieves the number of each objects by type"""
+    classes = {
+            "amenities": "Amenity",
+            "cities": "City",
+            "places": "Place",
+            "reviews": "Review",
+            "states": "State",
+            "users": "User"
+            }
+
     objs = {}
 
     for key, value in classes.items():
