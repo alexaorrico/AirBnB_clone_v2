b8a92e555bc688030b7cb8b55010d20435088183
Bigiizc <olalekanisaac75@gmail.com>
2023-09-30 22:55:16 +0100

    Update api

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -11,15 +11,16 @@ from api.v1.views import app_views
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_all_user():
-    """Retrives all User objects in the storage"""
+    """Retrives all User objects in the storage
+    """
     users = [u.to_dict() for u in storage.all(User).values()]
     return jsonify(users)
 
 
-@app_views.route('/users/<user_id>', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user_based_on_id(user_id):
-    """Retrives an User object given it's id else return 404"""
+    """Retrives an User object given it's id else return 404
+    """
     user = storage.get(User, user_id)
     if user:
         return jsonify(user.to_dict())
@@ -27,10 +28,10 @@ def get_user_based_on_id(user_id):
         abort(404)
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user_object(user_id):
-    """Deletes an User object if found otherwise return 404"""
+    """Deletes an User object if found otherwise return 404
+    """
     user = storage.get(User, user_id)
     if user:
         storage.delete(user)
@@ -42,7 +43,8 @@ def delete_user_object(user_id):
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user_object():
-    """Creates an User object returns the created user object"""
+    """Creates an User object returns the created user object
+    """
     data = request.get_json()
     if not data:
         return jsonify({"error": "Not a JSON"}), 400
@@ -55,10 +57,10 @@ def create_user_object():
     return jsonify(user.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'],
-                 strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
-    """Updates an User object based on the user id"""
+    """Updates an User object based on the user id
+    """
     fetch_user = storage.get(User, user_id)
     if not fetch_user:
         abort(404)
