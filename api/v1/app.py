d1c8e49de95a7759253f85bd6736641b1baa5d52
Imukua <ianmukuaa@gmail.com>
2023-09-30 13:58:26 +0300

    removed redudndant app variable initialisation

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,19 +9,21 @@ import os
 
 
 app = Flask(__name__)
-app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Terminates the database connection after handling the request"""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """Handles 404 errors
        Returns a json formatted error message."""
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
