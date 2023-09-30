88ef87f4a4fe2ed21f2e42c042e43767f651df2c
wughangar <wughangar.rose@gmail.com>
2023-09-30 15:12:43 +0300

    pycode

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,4 @@
 from flask import Blueprint, jsonify
-
+from api.v1.views.index import *
 # Create a Blueprint object
 app_views = Blueprint('app_views', __name__)
-
-from api.v1.views.index import *
-
