fc1d800ce335dd7e9d5f02aa5a578a4dbfeb3c1a
raymond <stancillousray@gmail.com>
2023-09-30 19:10:40 +0300

    replaced json.dumps with jsonify

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,7 +5,7 @@ from models import storage
 from api.v1.views import app_views
 import json
 
-from flask import request
+from flask import request, jsonify
 
 all_states = storage.all(State)  # is a dict
 states = []
@@ -38,28 +38,29 @@ def delete_state_obj(state_id):
     state_key = "State.{}".format(state_id)
 
     if state_key not in all_states.keys():
-        return json.dumps({"error": "Not found"}), 404
+        return jsonify({"error": "Not found"}), 404
     
     state_to_delete = all_states[state_key] # gives us the instance
     storage.delete(state_to_delete)
-    return json.dumps({}), 200
+    return jsonify({}), 200
 
 
-# @app_views.route('/states', methods=['POST'], strict_slashes=False)
-# def create_new_state():
-#     """creates a State"""
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_new_state():
+    """creates a State"""
 
-#     data_entered = request.get_json()
-#     print("data is ", data_entered)
-#     print("type is ", type(data_entered))
-    # if data_entered is None:
-    #     return "Not a JSON", 400
+    data_entered = request.get_json()
+    print("data is ", data_entered)
+    print("type is ", type(data_entered))
+    if data_entered is None:
+        return "Not a JSON", 400
     
-    # # if name not in dict
-    # if data_entered.get('name') is None:
-    #     return "Missing name", 400
-#     storage.new(data_entered)
-#     print("all states", storage.all(State))
+    # if name not in dict
+    if data_entered.get('name') is None:
+        return "Missing name", 400
+    # storage.new(data_entered)
+    new_state = State(name=data_entered.get('name'))
+    return jsonify(new_state.to_dict())
 
     
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
@@ -69,7 +70,7 @@ def update_state_obj(state_id):
     state_key = "State.{}".format(state_id)
 
     if state_key not in all_states.keys():
-        return json.dumps({"error": "Not found"}), 404
+        return jsonify({"error": "Not found"}), 404
     
     data_entered = request.get_json()  # method returns None if fails
     if data_entered is None:
@@ -81,4 +82,4 @@ def update_state_obj(state_id):
     # REQUEST DICT, THEN USE THAT TO UPDATE THE VALUES
     # NOT AS DONE BELOW (SHOULD BE DYNAMIC)
     state_to_update.name = data_entered['name']
-    return json.dumps(state_to_update.to_dict()), 200
+    return jsonify(state_to_update.to_dict()), 200
