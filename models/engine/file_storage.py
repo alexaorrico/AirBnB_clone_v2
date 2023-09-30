b9f6fee79fa0448eb1afd30df7c8e14afd9e8684
Castro Hamiltone <castromiltone@gmail.com>
2023-09-30 11:54:19 +0300

    adds file storage get and count

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,22 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns object based on class and its ID, or None if not found"""
+        obj = self.__objects["{}.{}".format(cls.__name__, id)]
+        return obj
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
+        count = 0
+        if cls is not None:
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    count += 1
+            return count
+        return len(self.__objects.keys())
+
