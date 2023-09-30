52354283a0ba3cf35693fc0897a451c79caa8b47
Reem-degais <reem.degais.33@gmail.com>
2023-09-30 09:37:38 -0700

    update app.py and indx.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ Module for API status view.
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
