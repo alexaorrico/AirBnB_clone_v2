ff0bddea38c953aee99ef77bd9fb53040401129c
Mohamed Amine <ma.abahmane@gmail.com>
2023-09-30 19:30:50 +0100

    Update stats Function

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,11 +6,30 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
+from models import *
+
 
 @app_views.route('/status', methods=['GET'])
 def statCode():
     """
-        a function that return a json string
+        a function that return a json string containing
+        the status code of the site
     """
     js = {'status': 'OK'}
+    return jsonify(js)
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def objStats():
+    """
+        a function that retrieves the number of each objects by type
+    """
+    js = {
+        "amenities": storage.count('amenities'),
+        "cities": storage.count('cities'),
+        "places": storage.count('places'),
+        "reviews": storage.count('reviews'),
+        "states": storage.count('states'),
+        "users": storage.count('users')
+    }
     return jsonify(js)
