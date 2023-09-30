7b1fab6d9afbb049d72706b981c93ef135ecffc2
0marSalah <omar.salah1597@gmail.com>
2023-09-30 21:07:20 +0300

    tssts

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,6 +19,10 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
+classes_plural = {"amenities": Amenity, "cities": City,
+                  "places": Place, "reviews": Review,
+                  "states": State, "users": User}
+
 
 class DBStorage:
     """interaacts with the MySQL database"""
@@ -74,3 +78,33 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return value
+
+        return None
+
+    def count(self, cls=None):
+        """
+        count the number of objects in storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
