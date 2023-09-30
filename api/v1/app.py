7c99d125c6737c2c09b0d54b708305af2455d889
FabPaul <paulfopa0@gmail.com>
2023-09-30 09:52:01 +0100

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,7 +19,7 @@ def create_app():
         """Handles 404 not found"""
         result = {"error": "Not found"}
         return jsonify(result)
-    
+
     @app.teardown_appcontext
     def close_staorage(error):
         """close the app storage "DBStorage or FileStorage" """
@@ -28,7 +28,7 @@ def create_app():
     return app
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app = create_app()
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(getenv("HBNB_API_PORT", "5000"))
