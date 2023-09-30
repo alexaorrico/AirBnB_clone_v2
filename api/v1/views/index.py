7533ad7ae4a4c8816e8914b63ec009ffaa8f8b5e
Huming-bird <ahmedolalekan2@gmail.com>
2023-09-30 09:24:39 -0700

    api tasks

---
+++ api/v1/views/index.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""index.py to connect to API"""
+
+from api.v1.views import app_views
+
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """this method returns stats of object count"""
+    dic = {}
+    for key, value in classes.items():
+        dic[key] = storage.count(value)
+    return jsonify(dic)
