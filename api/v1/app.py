dc00605a309c031ed53bee4808e4df82022d67d1
Christine-design-web <wakuthiichristine5@gmail.com>
2023-09-30 14:03:17 -0700

    damn

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,14 +4,25 @@ app
 """
 
 from flask import Flask, jsonify
-from models import storage
-from api.v1.views import app_views
+from flask_cors import CORS
 from os import getenv
+
+from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
+
+
 @app.teardown_appcontext
 def teardown(exception):
+    """
+    teardown function
+    """
     storage.close()
 
 if __name__ == "__main__":
