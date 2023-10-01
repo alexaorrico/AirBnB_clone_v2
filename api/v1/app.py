fe55f3319a9cbb156930bbdf2659650063abd984
Imukua <ianmukuaa@gmail.com>
2023-10-01 21:24:50 +0300

    pycodestyle fixes

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """terminates the database connection after handling the request"""
