e74421e48baf9762a23e1e1a9215975c8d2bd141
IgbeAjaga <ajagaigbe144@gmail.com>
2023-10-01 21:48:01 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,34 +2,21 @@
 """Flask server (variable app)
 """
 
-
-from flask import Flask, jsonify
+from flask import Flask
 from models import storage
-from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
+# Register the blueprint
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
+def teardown_appcontext(exception):
+    """Teardown app context"""
     storage.close()
 
-
-@app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Not found'), 404
-
-
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
-from api.v1.views.index import *
