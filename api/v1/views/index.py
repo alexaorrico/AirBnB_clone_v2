da794c62d77de1b48c0c1437ab9689ad7283ed5e
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-10-01 17:25:07 +0300

    Revert "Update api/v1/views/index.py"
    This reverts commit 0490a7e0ac5d06cf0f7cc4b1a9e2236260500531.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """
