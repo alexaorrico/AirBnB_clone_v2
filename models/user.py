3c335c50d66b7b2dcc4853f4f7786e9bdb095ae4
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-10-01 18:10:28 +0300

    security for password

--- models/user.py
+++ models/user.py
@@ -29,8 +29,24 @@ class User(BaseModel, Base):
         """initializes user"""
         super().__init__(*args, **kwargs)
 
-    def __setattr__(self, name, value):
-        """sets a password with md5 encryption"""
-        if name == "password":
-            value = md5(value.encode()).hexdigest()
-        super().__setattr__(name, value)
+        if 'password' in kwargs:
+            self.hash_password(kwargs['password'])
+
+    def hash_password(self, password):
+        '''
+            Hash the user's password using SHA-256.
+        '''
+        salt = uuid.uuid4().hex  # Generate a random salt
+        hashed_password = hashlib.sha256((salt + password).encode()).hexdigest()
+        self.password = salt + ':' + hashed_password  # Store salt:hashed_password
+
+    def is_valid_password(self, password):
+        '''
+            Check if a given password is valid for the user.
+        '''
+        if ':' not in self.password:
+            # Password is not hashed properly
+            return False
+
+        salt, hashed_password = self.password.split(':')
+        return hashed_password == hashlib.sha256((salt + password).encode()).hexdigest()
