41b51fa47b800b836941a3393ebddf53147bfef9
Christine-design-web <wakuthiichristine5@gmail.com>
2023-10-01 11:58:11 -0700

    deleted+

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,10 +25,20 @@ def teardown(exception):
     """
     storage.close()
 
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
 if __name__ == "__main__":
     app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
-
-@app.errorhandler(404)
-def not_found(error):
-    """ Returns JSON response with 404 status """
-    return make_response(jsonify({"error": "Not found"}), 404)
