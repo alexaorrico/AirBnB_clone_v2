6d4f645cc33904f1d98fd17bc181a2ff888e4495
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-10-01 17:35:02 +0300

    security for password

--- models/base_model.py
+++ models/base_model.py
@@ -68,6 +68,8 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if models.storage_t == "db" and "password" in new_dict:
+            del new_dict["password"]
         return new_dict
 
     def delete(self):
