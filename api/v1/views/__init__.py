4c11ef50fb5f061efb90588a472838fef96a4221
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-10-02 01:19:02 +0300

    Update api/v1/views/__init__.py
    - Import states for api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ Package initializer for API v1 views.
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *  # noqa
+from api.v1.views.states import *  # noqa
