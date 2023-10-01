b0686d5967d81b0c8528d0d5379c91a9dede7b8a
Favourokerri <favourokerri767@gmail.com>
2023-10-01 00:00:59 +0000

    index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,21 @@ from models import storage
 def show_status():
     """returns a JSON string of the status in a 200 response"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def getstats():
+    """retrieves the number of each objects by type"""
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        }
+    return jsonify(stats)
+
+
+if __name__ == "__main__":
+    pass
