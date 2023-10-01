c830bbe55595978ca7cf577cffa609f5d89b84a6
Herman100 <hermankwamebour30@gmail.com>
2023-10-01 09:27:39 +0000

    getting count of classes created

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,6 +8,13 @@ When a GET request is sent to /status, the status function is executed.
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -19,3 +26,20 @@ def status():
     key-value pair. The key is "status" and the value is "OK".
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    Retrieves the number of each objects by type
+    Return the server status as JSON.
+
+    When this function is called, it returns a JSON response with a multiple
+    key-value pairs of objects by type.
+    """
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User}
+    counts = {}
+    for key, cls in classes.items():
+        counts[key] = storage.count(cls)
+    return jsonify(counts)
