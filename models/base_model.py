1797a38bbad51023dca581893e595881442221ac
Richard.G <77013205+richardHaggioGwati@users.noreply.github.com>
2023-10-01 11:32:12 +0000

    Never fail!

--- models/base_model.py
+++ models/base_model.py
@@ -4,12 +4,13 @@ Contains class BaseModel
 """
 
 from datetime import datetime
-import models
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import models
+
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
