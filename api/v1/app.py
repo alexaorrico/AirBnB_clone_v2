ceefea40a54cf7fa762070bf2e1f0e502b7f2423
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-10-01 08:32:24 +0300

    Undo refactor done in previous commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python3
-"""Main file"""
 
-import os
-from flask import Flask, jsonify
+"""this is a script that serves a flask app"""
+
+from flask import Flask, abort, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+import os
+import json
 
+# assign host and post values
+host = "0.0.0.0"
+port = 5000
+
+if "HBNB_API_HOST" in os.environ:
+    host = os.environ.get("HBNB_API_HOST")
+if "HBNB_API_PORT" in os.environ:
+    port = int(os.environ.get("HBNB_API_PORT"))
+
+# instance of Flask
 app = Flask(__name__)
+
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def cleanup_app_context(arg):
+    """Remove SQLAlchemy  Session"""
+    # print("removing session")
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    '''Handle not found error'''
-    return jsonify({"error": "Not found"}), 404
+def not_found_error(error):
+    """404 Not found"""
+    data = {
+        "error": "Not found"
+    }
+
+    return make_response(jsonify(data), 404)
+
+@app.errorhandler(400)
+def bad_request(e):
+    """400 Bad request"""
+    custom_message = str(e.description if e.description else "Bad Request")
+    return  jsonify(custom_message), 400
 
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    # getenv returns a string and port is an int
-    # THREADED is set to true so it can serve multiple requests at once
+    # run flask app
     app.run(host=host, port=port, threaded=True)
