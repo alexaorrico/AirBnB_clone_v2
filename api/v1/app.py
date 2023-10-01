8bffcf64df2f96db3a19cde9024755152390393b
Mike-chege <mikechege93@gmail.com>
2023-10-01 06:23:29 +0000

    Fixed documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,15 @@
 """
 Main module for the Flask instance
 """
+
+
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
