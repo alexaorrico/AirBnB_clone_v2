08d18a1744d9aa349b981d57fa44a7db3d9b345d
michaelmensa <michaelmensa@live.com>
2023-10-01 04:31:38 +0000

    added tests for get and cound methods on both test_storage files

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,11 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        ''' Tests that gets an item in db properly '''
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        ''' Tests that counts; properly returns the no of elements in db '''
