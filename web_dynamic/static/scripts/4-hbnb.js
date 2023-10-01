ce8bcc2de4e2507b7f66a1f59c660a67d04daf2c
Luis <luisayala323@gmail.com>
2023-09-30 23:47:23 -0400

    Filter places by Amenity

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,54 @@
+$(function() {
+    const amenityDict = {};
+    $(':checkbox').change(function() {
+        if (this.checked) {
+            amenityDict[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenityDict[$(this).data('id')];
+        }
+        if ($.isEmptyObject(amenityDict)) {
+            $('.amenities h4').html('&nbsp');
+        } else {
+            $('.amenities h4').text(Object.values(amenityDict).join(', '));
+        }
+    });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+    if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+    } else {
+        $('div#api_status').removeClass('available');
+    }
+});
+
+$.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function(data) {
+        alert(data);
+        for (let i = 0; i < data.length; i++) {
+            const place = data[i];
+            $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+        }
+    }
+});
+
+$('button').click(function() {
+            $.ajax({
+                    type: 'POST',
+                    url: 'http://0.0.0.0:5001/api/v1/places_search',
+                    contentType: 'application/json',
+                    dataType: 'json',
+                    data: JSON.stringify({ amenities: Object.keys(amens) }),
+                    success: function(data) {
+                        $('article').remove();
+                        for (const place of Object.values(data)) {
+                            $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+                        },
+                    }
+                }
+            });
