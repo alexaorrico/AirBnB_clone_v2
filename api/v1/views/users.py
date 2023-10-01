640ad9a2d37bffe3e74b9e670bf0116d0d37a2f5
== <==>
2023-10-01 21:47:30 +0000

    User handler methods

---
+++ api/v1/views/users.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'])
+def get_users():
+    """List all users"""
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'])
+def get_user(user_id):
+    """Retrieve a User by ID"""
+    user = storage.get(User, user_id)
+    if user:
+        return jsonify(user.to_dict())
+    abort(404)
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """Delete a user by ID"""
+    user = storage.get(User, user_id)
+    if user:
+        storage.delete(user)
+        storage.save()
+        return jsonify({}), 200
+    abort(404)
+
+
+@app_views.route('/users', methods=['POST'])
+def create_user():
+    """Create New User"""
+    data = request.get_json()
+    if not data:
+        abort(400, description='Not a JSON')
+    if 'email' not in data:
+        abort(400, description='Missing email')
+    if 'password' not in data:
+        abort(400, description='Missing password')
+    new_user = User(**data)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'])
+def update_user(user_id):
+    """Update a User by ID"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, description='Not a JSON')
+    for key, value in data.items():
+        if key not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200
