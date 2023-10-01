1e18ddf90057588885572b6723727ac11ec65c98
Keith koki <kokikeith@gmail.com>
2023-10-01 01:13:14 -0700

    corrections

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,10 @@
 """
 Module for flask Blueprint
 """
+
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
