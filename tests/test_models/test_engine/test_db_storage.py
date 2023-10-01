af19bc01db46df14764c4a024b4531e168b9700f
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-10-01 11:52:07 +0300

    Fix code style
    - Edit code style to follow pycodestyle guidelines

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,6 +87,7 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
+
 class TestDBStorage(unittest.TestCase):
     """Test the DBStorage class"""
 
