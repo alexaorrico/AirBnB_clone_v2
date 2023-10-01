df61348acb820c9d947d8d0eeeb0300504fddbd9
Bigiizc <olalekanisaac75@gmail.com>
2023-10-01 19:58:52 +0100

    Update api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,6 @@
 
 
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
@@ -11,3 +10,4 @@ from api.v1.views.places import *
 from api.v1.views.states import *
 from api.v1.views.users import *
 from api.v1.views.places_reviews import *
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
