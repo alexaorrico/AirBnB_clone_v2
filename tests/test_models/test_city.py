33158052ca0887aba21ad2891cde06d26034ce5f
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-10-01 22:20:41 +0300

    Replace not equal with !=

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -59,6 +59,7 @@ class TestCityDocs(unittest.TestCase):
 
 class TestCity(unittest.TestCase):
     """Test the City class"""
+
     def test_is_subclass(self):
         """Test that City is a subclass of BaseModel"""
         city = City()
@@ -92,7 +93,7 @@ class TestCity(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in c.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
