7bb493a073756ef606b114f924e39cfac97e695a
root <root@DESKTOP-KUT53MR>
2023-10-01 15:48:39 +0000

    app flask

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/python3
+"""AirBnb flask APP"""
+from flask import Flask
+from models import storage
+import os
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def closeStorage(error):
+    """Method to close storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.debug = True
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
