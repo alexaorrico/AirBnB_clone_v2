6f1cd748608c3690ede1b3361cb6835eb184c111
IgbeAjaga <ajagaigbe144@gmail.com>
2023-10-01 21:35:54 +0000

    update

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,116 +3,43 @@
 Contains the TestDBStorageDocs and TestDBStorage classes
 """
 
-from datetime import datetime
-import inspect
-import models
-from models.engine import db_storage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-import json
-import os
-import pep8
 import unittest
-DBStorage = db_storage.DBStorage
-classes = {"Amenity": Amenity, "City": City, "Place": Place,
-       	"Review": Review, "State": State, "User": User}
+from models import storage
+from models.state import State
 
+class TestDBStorageMethods(unittest.TestCase):
 
-class TestDBStorageDocs(unittest.TestCase):
-	"""Tests to check the documentation and style of DBStorage class"""
-	@classmethod
-	def setUpClass(cls):
-    	"""Set up for the doc tests"""
-    	cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
+    def test_get_existing_object(self):
+        # Test retrieving an existing object
+        new_state = State(name="Test State")
+        storage.new(new_state)
+        storage.save()
+        retrieved_state = storage.get(State, new_state.id)
+        self.assertEqual(retrieved_state, new_state)
 
-	def test_pep8_conformance_db_storage(self):
-    	"""Test that models/engine/db_storage.py conforms to PEP8."""
-    	pep8s = pep8.StyleGuide(quiet=True)
-    	result = pep8s.check_files(['models/engine/db_storage.py'])
-    	self.assertEqual(result.total_errors, 0,
-                     	"Found code style errors (and warnings).")
+    def test_get_non_existing_object(self):
+        # Test retrieving a non-existing object
+        non_existent_id = "non_existent_id"
+        retrieved_state = storage.get(State, non_existent_id)
+        self.assertIsNone(retrieved_state)
 
-	def test_pep8_conformance_test_db_storage(self):
-    	"""Test tests/test_models/test_db_storage.py conforms to PEP8."""
-    	pep8s = pep8.StyleGuide(quiet=True)
-    	result = pep8s.check_files(['tests/test_models/test_engine/\
-test_db_storage.py'])
-    	self.assertEqual(result.total_errors, 0,
-                     	"Found code style errors (and warnings).")
+    def test_count_all_objects(self):
+        # Test counting all objects in storage
+        initial_count = storage.count()
+        new_state = State(name="Test State")
+        storage.new(new_state)
+        storage.save()
+        updated_count = storage.count()
+        self.assertEqual(updated_count, initial_count + 1)
 
-	def test_db_storage_module_docstring(self):
-    	"""Test for the db_storage.py module docstring"""
-    	self.assertIsNot(db_storage.__doc__, None,
-                     	"db_storage.py needs a docstring")
-    	self.assertTrue(len(db_storage.__doc__) >= 1,
-                    	"db_storage.py needs a docstring")
+    def test_count_specific_objects(self):
+        # Test counting specific objects in storage
+        initial_count = storage.count(State)
+        new_state = State(name="Test State")
+        storage.new(new_state)
+        storage.save()
+        updated_count = storage.count(State)
+        self.assertEqual(updated_count, initial_count + 1)
 
-	def test_db_storage_class_docstring(self):
-    	"""Test for the DBStorage class docstring"""
-    	self.assertIsNot(DBStorage.__doc__, None,
-                     	"DBStorage class needs a docstring")
-    	self.assertTrue(len(DBStorage.__doc__) >= 1,
-                    	"DBStorage class needs a docstring")
-
-	def test_dbs_func_docstrings(self):
-    	"""Test for the presence of docstrings in DBStorage methods"""
-    	for func in self.dbs_f:
-        	self.assertIsNot(func[1].__doc__, None,
-                         	"{:s} method needs a docstring".format(func[0]))
-        	self.assertTrue(len(func[1].__doc__) >= 1,
-                        	"{:s} method needs a docstring".format(func[0]))
-
-
-class TestFileStorage(unittest.TestCase):
-	"""Test the FileStorage class"""
-	@unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-	def allReturnDict(self):
-    	"""Test that all returns a dictionaty"""
-    	self.assertIs(type(models.storage.all()), dict)
-
-	@unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-	def allNoClass(self):
-    	"""Test that all returns all rows when no class is passed"""
-
-	@unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-	def new(self):
-    	"""test that new adds an object to the database"""
-
-	@unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-	def save(self):
-    	"""Test that save properly saves objects to file.json"""
-
-
-class TestDBStorage(unittest.TestCase):
-	"""Test the DBStorage class"""
-
-	@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                 	"not testing db storage")
-	def test_get(self):
-    	"""Test that get returns specific object, or none"""
-    	newState = State(name="New York")
-    	newState.save()
-    	newUser = User(email="bob@foobar.com", password="password")
-    	newUser.save()
-    	self.assertIs(newState, models.storage.get("State", newState.id))
-    	self.assertIs(None, models.storage.get("State", "blah"))
-    	self.assertIs(None, models.storage.get("blah", "blah"))
-    	self.assertIs(newUser, models.storage.get("User", newUser.id))
-
-	@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                 	"not testing db storage")
-	def test_count(self):
-    	"""add new object to db"""
-    	startCount = models.storage.count()
-    	self.assertEqual(models.storage.count("Blah"), 0)
-    	newState = State(name="Montevideo")
-    	newState.save()
-    	newUser = User(email="ralexrivero@gmail.com.com", password="dummypass")
-    	newUser.save()
-    	self.assertEqual(models.storage.count("State"), startCount + 1)
-    	self.assertEqual(models.storage.count(), startCount + 2)
+if __name__ == '__main__':
+    unittest.main()
