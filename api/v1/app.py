d4d3326c4f79f2aebd07d38cb72c9b01cd671a56
stephenkiai <stephenkiai23@gmail.com>
2023-10-02 01:45:23 +0300

    test_api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,13 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """close running SQLAlchemy session"""
     storage.close()
+
+
+#handler for 404 errors
+@app.errorhandler(404)
+def not_found(error):
+    """a not found page"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
---
+++ tests/test_api/test_app.py
@@ -0,0 +1,50 @@
+# tests/test_app.py
+
+import unittest
+from flask import Flask
+from api.v1.app import app
+from models import storage,City, Place, Amenity
+
+class TestApp(unittest.TestCase):
+    """Test cases for the Flask application."""
+
+    def setUp(self):
+        """Set up the test environment."""
+        self.app = app.test_client()
+
+    def test_hello_world_route(self):
+        """Test the / route (hello world)."""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode("utf-8"), "Hello, World!")
+
+
+    def test_status_route(self):
+        """Test the /status route."""
+        response = self.client.get("/api/v1/status")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"status": "OK"})
+
+
+    def test_stats_route(self):
+        """Test the /stats route."""
+        # Simulate adding objects to storage for testing
+        storage.create(Amenity(name="TestAmenity"))
+        storage.create(City(name="TestCity"))
+        storage.create(Place(name="TestPlace"))
+        
+        response = self.client.get("/api/v1/stats")
+        self.assertEqual(response.status_code, 200)
+        # Check response JSON for expected keys and values
+        self.assertIn("Amenity", response.json)
+        self.assertIn("City", response.json)
+        self.assertIn("Place", response.json)    
+
+    def test_404_error_handler(self):
+        """Test the 404 error handler."""
+        response = self.app.get("/non_existent_route")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json, {"error": "Not found"})
+
+if __name__ == "__main__":
+    unittest.main()
