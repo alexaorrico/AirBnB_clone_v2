56fdddb1e97b5ac47f1177604da22a7b46e0b64e
rodgersxy <rodgersny99@gmail.com>
2023-09-30 22:25:52 -0700

    bug fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,8 @@ Blueprint for index
 """
 
 from api.v1.views import app_views
-from flask import jsonify, Blueprint
+from flask import jsonify
 from models import storage
-from models.state import State
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -17,14 +16,15 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    """
-    Retrieves the number of each object type
-    """
-    objects = {"amenities": 'Amenity', "cities": 'City', "places": 'Place',
-               "reviews": 'Review', "states": 'State', "users": 'User'}
-
-    objects = {key: storage.count(value) for key, value in objects.items()}
-
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    """Retrieves the no of each object by type"""
+    objects = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User'),
+            }
     return jsonify(objects)
