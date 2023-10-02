ec7316cc925217d6298b200cad174f978baa1f0f
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-02 17:03:06 +0100

    added places_amenities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,7 @@
 
 from flask import Blueprint
 
+# start blueprint session
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
@@ -13,3 +14,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
