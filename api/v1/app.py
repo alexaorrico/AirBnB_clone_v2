d7fe79bb789a49193789a1d2264497c7490e949e
root <tawaifrancis@gmail.com>
2023-10-02 13:05:50 -0700

    fix pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
 """Flask application"""
 
+
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
 from flask_cors import CORS
 
-#flask app instance
+
 app = Flask(__name__)
-
-#blueprint registration
 app.register_blueprint(app_views)
-
-
-# Enable CORS
 CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
-#teardown method
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """close running SQLAlchemy session"""
     storage.close()
 
 
-#handler for 404 errors
 @app.errorhandler(404)
 def not_found(error):
     """a not found page"""
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
