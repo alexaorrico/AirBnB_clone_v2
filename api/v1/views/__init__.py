63feacf3e6a12af65f6592f3e74fee521cc5b16f
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-10-02 22:58:04 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""
-Initializes the API views package
-"""
-
+"""create blueprint"""
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+if app_views is not None:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.places_reviews import *
+    from api.v1.views.places_amenities import *
