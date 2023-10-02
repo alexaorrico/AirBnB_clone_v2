891c6a3d7a5242d1765eef53873c936a5226b7a7
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-10-02 09:12:14 +0100

    refactored app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,6 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 
-host = getenv("HBNB_API_HOST", '0.0.0.0')
-port = getenv("HBNB_API_PORT", 5000)
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
@@ -29,4 +26,7 @@ def not_found(error):
 
 
 if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", '0.0.0.0')
+    port = getenv("HBNB_API_PORT", 5000)
+
     app.run(host=host, port=port, threaded=True)
