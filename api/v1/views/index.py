6cb0a2abd4c3eeb3f137fa0a86a9f2269fa5b314
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-10-02 16:43:01 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,22 @@
 import models
 from models import storage
 from models.base_model import BaseModel
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """Returns the status of the API"""
-    return jsonify({"status": "OK"})
+
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stuff():
+    '''JSON Responses'''
+    todos = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
