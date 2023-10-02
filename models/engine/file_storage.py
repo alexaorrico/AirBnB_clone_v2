f4bd70abe1c4c4adf2f2ee5a34a1d7df7fabe1b8
Ntuthuko <ntuthukolu@gmail.com>
2023-10-03 00:30:12 +0200

    indentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,7 +69,7 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
-        def get(self, cls, id):
+    def get(self, cls, id):
         """
         Returns the object based on the class and its ID, or `None` if not
         found.
