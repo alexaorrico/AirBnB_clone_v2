8e85272814954d86d99ae1566001f198b0f65a83
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-10-02 15:00:01 +0100

    reverting changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,51 +1,29 @@
 #!/usr/bin/python3
-
 """ Index module """
-
 from flask import jsonify
-
 from api.v1.views import app_views
-
 from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-
 def status():
-
     """returns a json"""
-
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-
 def stats():
-
     """retrieves the number of each objects by type"""
-
     classes = {
-
             "amenities": "Amenity",
-
             "cities": "City",
-
             "places": "Place",
-
             "reviews": "Review",
-
             "states": "State",
-
             "users": "User"
-
             }
-
     objs = {}
-
     for key, value in classes.items():
-
         count = storage.count(value)
-
         objs[key] = count
-
     return jsonify(objs)
