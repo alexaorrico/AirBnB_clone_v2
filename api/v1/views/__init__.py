b78e3cd13cc975512f08f6fa42dac5006760a6cb
jeshi2 <jamesvean19@gmail.com>
2023-10-02 21:02:12 +0300

    mmmh

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""Initializing Blueprint views snops"""
-
-"""import blueprint class from flask"""
+"""Initialize Blueprint views"""
 from flask import Blueprint
 
-"""create blueprint objects"""
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-"""imports for other views"""
 from api.v1.views.index import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""index for connecting to APIs"""
+"""index.py to connect to API"""
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
 
-"""dictionary to map endpoint names to their object types"""
-endpoint_to_object_type = {
+
+hbnbText = {
     "amenities": "Amenity",
     "cities": "City",
     "places": "Place",
@@ -14,25 +14,20 @@ endpoint_to_object_type = {
     "users": "User"
 }
 
-"""Define a route for the /status endpoint"""
+
 @app_views.route('/status', strict_slashes=False)
-def hbnb_status():
-    """Return Status"""
+def hbnbStatus():
+    """hbnbStatus"""
     return jsonify({"status": "OK"})
 
-"""Define a route for the /stats endpoint"""
+
 @app_views.route('/stats', strict_slashes=False)
-def hbnb_stats():
-    """Return Stats"""
-    stats_dict = {}
-    
-    """Loop through the endpoint-to-object-type mapping"""
-    for endpoint, object_type in endpoint_to_object_type.items():
-        """Use the storage.count method to get 
-        the count of objects for each type"""
-        stats_dict[endpoint] = storage.count(object_type)
-    
-    return jsonify(stats_dict)
+def hbnbStats():
+    """hbnbStats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
 
 if __name__ == "__main__":
     pass
