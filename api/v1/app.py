d3dd64c5c1cbabf610b1ec6e8b628308e6536794
vanessaokumu <okumuvanessa@gmail.com>
2023-10-02 00:37:14 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,24 +3,20 @@
 """create a variable app, instance of Flask"""
 
 import os
-from flask import Flask, jsonify
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception):
     """Closes the database connection on teardown."""
     storage.close()
 
-if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
-    
-    app.run(host=host, port=port, threaded=True)
 
 @app.errorhandler(404)
 def not_found_error(error):
@@ -29,11 +25,7 @@ def not_found_error(error):
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    
     app.run(host=host, port=port, threaded=True)
