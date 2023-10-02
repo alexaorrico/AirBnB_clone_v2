30e03074b50cc671b4073608428ae5ed40326e6c
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-10-02 09:48:13 +0100

    reverting changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-""" Index module """
-from flask import jsonify,Flask
+"""List of states"""
 from api.v1.views import app_views
+from flask import jsonify, request
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """
-    Returns a JSON status
-    """
+@app_views.route('/status')
+def api_status():
+    """api_status"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def count():
+@app_views.route('/stats', methods=['GET'])
+def stats():
     """
-    Retrieves the number of each objects by type
+    returns the count of all the class objects.
     """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    if request.method == 'GET':
+
+        response = {}
+
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+
+        for key, value in PLURALS.items():
+            response[value] = len(storage.all(key))
+
+        return jsonify(response)
