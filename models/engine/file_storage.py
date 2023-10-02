fed559e79322b83da70766569da4dccec6fe1f50
Navi212 <josephchristo615@gmail.com>
2023-10-02 16:10:20 +0100

    Adds db and fs storage files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,6 +12,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
@@ -55,7 +56,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +69,26 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID, or None if not found
+        """
+        key = f"{cls.__class__.__name__}.{str(id)}"
+        for k, v in self.__objects.items():
+            if (k == key):
+                return (self.__objects[key])
+            return (None)
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage.
+        """
+        counts = 0
+        if (cls):
+            for obj in self.__objects.values():
+                if (obj.__class__.__name__ == cls):
+                    counts += 1
+            return (counts)
+        return (len(self.__objects))
