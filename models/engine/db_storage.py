557309a8eacd72a4c11eb7bc9dc8311da4858503
Ntuthuko <ntuthuko@ubuntu.stukie>
2023-10-02 22:35:52 +0200

    "Improve storage Task 2"

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+        def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID, or `None` if not
+        found.
+        """
+        if cls in classes.values():
+            return self.__session.query(cls).filter(cls.id == id).first()
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class,
+        or if no class is passed, the total count of all objects
+        """
+        return len(self.all(cls))
