ff9dcd6067817f364d3dba074521fd9cc8cb40be
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-10-02 19:04:26 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-"""Create a Flask API"""
-
-from flask import Flask, jsonify, make_response
-import os
+"""app.py to connect to API"""
+from os import getenv
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, jsonify
 from flask_cors import CORS
 
-"""Instantiate a flask app by calling the Flask class"""
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
-def close_storage(exception=None):
-    """method to handle app context storage"""
+@app.teardown_appcontext
+def teardown_appcontext(exc):
+    """teardown_appcontext"""
     storage.close()
 
 
-"""Register the teardown function"""
-app.teardown_appcontext(close_storage)
-
-
 @app.errorhandler(404)
-def not_found(error):
-    """Returns the page Not Found in JSON format"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+def page_not_found(error):
+    return jsonify(error='Not found'), 404
 
 
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
-
-    app.run(host=host, port=port, debug=True, threaded=True)
+    host = getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = getenv('HBNB_API_PORT') or 5000
+    app.run(host=host, port=port, threaded=True)
