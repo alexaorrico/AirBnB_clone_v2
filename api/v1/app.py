4a986ecab7fa313940d4d0e7aba81371638e19ab
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-02 15:06:52 +0100

    app fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -35,7 +35,8 @@ def error_404(error):
         a handler for 404 errors that returns a JSON-formatted
         404 status code response
     """
-    return jsonify({"error": "Not found"}), 404
+    js = {"error": "Not found"}
+    return jsonify(js), 404
 
 
 if __name__ == '__main__':
