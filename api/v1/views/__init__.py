c8ebc6f441cb7a093d029972bde2988a2cf8910f
kim-thuita <kevinkoech265@gmail.com>
2023-10-02 23:04:29 +0300

    "Testing out task 3"

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,14 @@
-#!/bin/usr/python3
-"""
-init file with the following
-"""
+#!/usr/bin/python3
+"""create blueprint"""
 from flask import Blueprint
-from api.v1.views.index import *
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+if app_views:
+    from api.v1.views.index import *
+    from api.v1.views.states import *
+    from api.v1.views.cities import *
+    from api.v1.views.amenities import *
+    from api.v1.views.users import *
+    from api.v1.views.places import *
+    from api.v1.views.places_reviews import *
