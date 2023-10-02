ab6b9d64f2d7e5225c814373c258591c7235a1a2
papaskripto <bygeoffreyouma@gmail.com>
2023-10-02 20:55:26 +0000

    App

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
+""" App module
 """
-A flask app to serve REST api service
-"""
-from .views import app_views
-from flask import Flask, make_response, jsonify
-from models import storage
+
+
 import os
+from flask import Flask
+from flask import jsonify
+from models import storage
+from api.v1.views import app_views
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def tear_down(exception):
+def tear_down(self):
+    """ Calls storage.close()
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found_err(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def page_not_found_err(error):
+    """ Return 404 not found error page
+    """
+    return jsonify(error='Not found'), 404
 
 
 if __name__ == '__main__':
