d85f8f0cbe20a8e0b18e3647469ba9d5f2490e03
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-10-02 17:28:04 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,16 +4,20 @@
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     '''Get the count of each object type'''
     count_dict = {}
     
-    # Define a list of object classes
     object_classes = ["Amenity", "City", "Place", "Review", "State", "User"]
     
-    # Calculate the count for each object class
     for class_name in object_classes:
         count = storage.count(class_name)
         count_dict[class_name.lower() + 's'] = count
