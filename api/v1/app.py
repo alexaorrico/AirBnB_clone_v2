e5e776e69a4d5f6003fa5574d20ce563495e1331
KOBBY-IM <nyarkotheo2@gmail.com>
2023-10-02 11:23:43 +0000

    added the blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,4 +17,4 @@ def teardown_appcontext(exception):
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)Z
+    app.run(host=host, port=port, threaded=True)
