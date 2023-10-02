de166b0d5ca0106cfae8782ce67604232e636528
wughangar <wughangar.rose@gmail.com>
2023-10-02 05:14:08 +0300

    style

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,14 +8,15 @@ import os
 from api.v1.views.index import api_v1_stats
 
 app = Flask(__name__)
-#app.register_blueprint(app_views, url_prefix='/api/v1')
 app.register_blueprint(api_v1_stats)
 app.register_blueprint(app_views)
+
 
 @app.errorhandler(404)
 def handle_error(error):
     """ handles 404 error"""
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -23,6 +24,7 @@ def teardown(exception):
     calls the close() method
     """
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
