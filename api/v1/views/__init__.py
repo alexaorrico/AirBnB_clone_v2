13fabe154f21b58699965956b669f8cd377332f2
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-10-02 14:02:06 +0300

    Update api/v1/views/__init__.py
    - Add comments to ignore pycodestyle check for importing modules within
    the code
    - Import users views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,5 +7,6 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *  # noqa
 from api.v1.views.states import *  # noqa
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
+from api.v1.views.cities import *  # noqa
+from api.v1.views.amenities import *  # noqa
+from api.v1.views.users import *  # noqa
