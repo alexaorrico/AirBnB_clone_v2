17ed8a761234b651c6ac535b695c5ae392e03664
sarahadeks <sarahopeyemi@yahoo.com>
2023-10-02 14:07:53 -0700

    create an endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,3 +11,17 @@ def status_message():
     '''Returns status code'''
     return jsonify({"status": "OK"})
 
+@app_views.route("/stats")
+def get_stats():
+    """
+       endpoint that retrieves the number of each objects
+    """
+    return jsonify({
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    })
+
