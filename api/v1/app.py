3c21e32551386e20855fbf172218a225c7bc198f
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-10-02 17:00:53 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,18 +2,34 @@
 """Flask server (variable app)
 """
 
-import os
+
 from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
-def teardown(exception):
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
+
+
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
