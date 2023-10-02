d70c5f8701cf04497f4ada487d56ea2c444fd338
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-10-02 09:06:34 +0100

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-""" HBNB flask application """
+"""Status API"""
 
-from flask import Flask, jsonify
+from flask import Flask, Blueprint
+from flask_cors import CORS
 from models import storage
+from flask import jsonify
 from api.v1.views import app_views
-from os import getenv
+import os
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {'origins': ['0.0.0.0']}})
 
 
 @app.teardown_appcontext
-def clean(exception=None):
-    """ close storage sessions """
+def close_s(x=None):
+    """Close session at the end"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found_error(error):
-    """handle 404 error"""
+def not_found(error):
+    """Not found"""
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = getenv('HBNB_API_PORT', '5000')
-    app.run(host, port, threaded=True)
+    host = os.getenv('HBNB_API_HOST')
+    port = os.getenv('HBNB_API_PORT')
+
+    app.run(host=host, port=port, threaded=True)
