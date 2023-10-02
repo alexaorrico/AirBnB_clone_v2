72a55c906f564b3fcbfd705832cebc0a9fbfaf6d
vanessaokumu <okumuvanessa@gmail.com>
2023-10-02 02:12:10 -0700

    task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,27 +4,30 @@ create a route /status on the object app_views that returns a JSON
 and an endpoint that retrieves the number of each objects by type
 """
 
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, request
 from api.v1.views import app_views
 from models import storage
 
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
-classes = {"amenites": Amenity, "places": Place, "city": City,
-           "state": State, "user": User, "review": Review}
 
 @app_views.route('/status', methods=['GET'])
 def status():
+    """function that returns the status"""
+    if request.method == "GET":
     return jsonify({"status": "OK"})
 
 @app_views.route('/status', methods=['GET'])
 def get_stats():
-    counts = {}
-    for clss in classes:
-        counts[clss] = storage.count(classes[clss])
-    return jsonify(counts)
+    """function that returns count of all class objects"""
+    if request.method == 'GET':
+        response = {}
+        texthbnb = {
+            'Amenity': 'amenities',
+            'City': 'cities',
+            'Place': 'places',
+            'Review': 'reviews',
+            'State': 'states',
+            'User': 'users'
+        }
+        for key, value in texthbnb.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
