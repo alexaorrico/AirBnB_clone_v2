9124f1722064191201822e38045e7e6a3ab28a13
Huming-bird <ahmedolalekan2@gmail.com>
2023-10-02 13:38:09 -0700

    api tasks

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,9 +21,11 @@ def teardown_appcontext(code):
     """teardown_appcontext"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
