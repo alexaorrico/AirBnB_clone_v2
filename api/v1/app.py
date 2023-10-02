6dd7ad663e83debd2a445e4b4ffb70ea4fcc0766
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-10-02 09:03:34 +0100

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-""" Script that imports a Blueprint and runs Flask """
-from flask import Flask, make_response, jsonify
-from flask_cors import CORS
+""" HBNB flask application """
+
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
-host = getenv("HBNB_API_HOST", '0.0.0.0')
-port = getenv("HBNB_API_PORT", 5000)
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """closes the current session"""
+def clean(exception=None):
+    """ close storage sessions """
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """errors that returns a JSON-formatted
-       404 status code response.
-    """
-    return make_response(jsonify({"error": "Not found"}), 404)
+def not_found_error(error):
+    """handle 404 error"""
+    return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == "__main__":
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
+    app.run(host, port, threaded=True)
