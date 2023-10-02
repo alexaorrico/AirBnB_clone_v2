c56ddc99ffbfa2c493832ddcb1250c23a2c392ca
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-10-02 17:25:25 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,15 +8,17 @@ from models import storage
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     '''Get the count of each object type'''
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(stats)
+    count_dict = {}
+    
+    # Define a list of object classes
+    object_classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    
+    # Calculate the count for each object class
+    for class_name in object_classes:
+        count = storage.count(class_name)
+        count_dict[class_name.lower() + 's'] = count
+    
+    return jsonify(count_dict)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
