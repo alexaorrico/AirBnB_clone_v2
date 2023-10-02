3cc432549c6d70a93d9532682fccc1ec311cf68d
primeace7 <osaruonamenjeffery@gmail.com>
2023-10-02 19:51:07 +0000

    Add task 7 file

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,13 +10,16 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def tear_down(exception):
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found_err(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == '__main__':
     if not os.getenv('HBNB_API_HOST'):
