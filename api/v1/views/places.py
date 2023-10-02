32be9d55d41031f238aa32360e7ccd87a99ef9aa
sarahadeks <sarahopeyemi@yahoo.com>
2023-10-02 15:16:23 -0700

    final update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """
-    cities.py file in v1/views
+    states.py file in v1/views
 """
 from flask import abort, Flask, jsonify, request
 from api.v1.views import app_views
 from models import storage
+from models.place import Place
 from models.city import City
-from models.state import State
+from models.user import User
 
 
-@app_views.route("/states/<state_id>/cities", methods=["GET", "POST"], strict_slashes=False)
-def handle_cities(state_id):
+@app_views.route("/cities/<city_id>/places", methods=["GET", "POST"], strict_slashes=False)
+def handle_places(city_id):
     """
-        Method to return a JSON representation of cities by state
+        Method to return a JSON representation of all states
     """
-    state_by_id = storage.get(State, state_id)
-    if state_by_id is None:
+    city_by_id = storage.get(City, city_id)
+    if city_by_id is None:
         abort(404, 'Not found')
 
     if request.method == 'GET':
-        city_list = []
-        for city in state_by_id.cities:
-            city_list.append(city.to_dict())
-        return jsonify(city_list)
+        place_list = []
+        for place in city_by_id.places:
+            place_list.append(place.to_dict())
+        return jsonify(place_list)
 
     elif request.method == 'POST':
         post = request.get_json()
@@ -30,29 +31,35 @@ def handle_cities(state_id):
             return jsonify({'error': 'Not a JSON'}), 400
         elif post.get('name') is None:
             return jsonify({'error': 'Missing name'}), 400
+        elif post.get('user_id') is None:
+            return jsonify({'error': 'Missing user_id'}), 400
 
-        state_by_id = storage.get(State, state_id)
-        if not state_by_id:
+        user_id = post['user_id']
+
+        user_by_id = storage.get(User, user_id)
+        city_by_id = storage.get(City, city_id)
+
+        if not user_by_id or not city_by_id:
             abort(404)
 
-        new_state = City(**post)
-        new_state.save()
-        return jsonify(new_state.to_dict()), 201
+        new_place = Place(**post)
+        new_place.save()
+        return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route("/cities/<city_id>", methods=["GET", "PUT", "DELETE"],
+@app_views.route("/places/<place_id>", methods=["GET", "PUT", "DELETE"],
                  strict_slashes=False)
-def handle_cities_by_id(city_id):
+def handle_place_by_id(place_id):
     """
-        Method to return a JSON representation of a city
+        Method to return a JSON representation of a state
     """
-    city_by_id = storage.get(City, city_id)
-    if city_by_id is None:
+    place_by_id = storage.get(Place, place_id)
+    if place_by_id is None:
         abort(404)
     elif request.method == 'GET':
-        return jsonify(city_by_id.to_dict())
+        return jsonify(place_by_id.to_dict())
     elif request.method == 'DELETE':
-        storage.delete(city_by_id)
+        storage.delete(place_by_id)
         storage.save()
         return jsonify({}), 200
     elif request.method == 'PUT':
@@ -61,6 +68,6 @@ def handle_cities_by_id(city_id):
             return jsonify({'message': 'Not a JSON'}), 400
         for key, value in put.items():
             if key not in ['id', 'created_at', 'updated_at']:
-                setattr(city_by_id, key, value)
+                setattr(place_by_id, key, value)
         storage.save()
-        return jsonify(city_by_id.to_dict()), 200
+        return jsonify(place_by_id.to_dict()), 200
