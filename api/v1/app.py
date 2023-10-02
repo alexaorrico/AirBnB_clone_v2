c660b5db5935833b028f6d983b860899ae82ddac
sarahadeks <sarahopeyemi@yahoo.com>
2023-10-02 14:54:07 -0700

    final update

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,11 @@ def teardown(code):
     """
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """Handler for 404 Not found errors"""
+    return ({'error': 'Not found'}), 404
+
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
