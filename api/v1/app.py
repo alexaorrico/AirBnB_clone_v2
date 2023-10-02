88a9b093f02f0795ba4921b705e0dc64b42a740e
Chinenye Nmoh <chinenyeumeaku@gmail.com>
2023-10-02 11:13:59 +0100

    reverting changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-"""make api"""
-import os
-from models import storage
-from api.v1.views import app_views
+""" Script that imports a Blueprint and runs Flask """
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+host = getenv("HBNB_API_HOST", '0.0.0.0')
+port = getenv("HBNB_API_PORT", 5000)
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """teardown method"""
+def teardown(exception):
+    """closes the current session"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """404 error handler
-
-    args:
-        error status
-    return:
-        json error response
+def not_found(error):
+    """errors that returns a JSON-formatted
+       404 status code response.
     """
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    app.run(host=host, port=port, threaded=True)
