4d07b152b521813183dc7759b18d049eda499e87
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2023-10-02 19:26:15 +0100

    aa

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 import os
 from flask import Flask, jsonify, make_response, render_template, url_for
 from werkzeug.exceptions import HTTPException
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
@@ -17,6 +18,10 @@ app.register_blueprint(app_views)
 # flask server environmental setup
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
+
+
+# Cross-Origin Resource Sharing
+cors = CORS(app, resources={r'/*': {'origins': host}})
 
 
 # global strict slashes
