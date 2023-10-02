a4b53da4444243622b7d2f225184775aca9517b7
jeshi2 <jamesvean19@gmail.com>
2023-10-02 20:00:41 +0300

    confirmed

--- models/review.py
+++ models/review.py
@@ -1,23 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ holds class Review"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 
 class Review(BaseModel, Base):
     """Representation of Review """
-    if models.storage_t == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
+        text = Column(String(1024),
+                      nullable=False)
+        place_id = Column(String(60),
+                          ForeignKey('places.id'),
+                          nullable=False)
+        user_id = Column(String(60),
+                         ForeignKey('users.id'),
+                         nullable=False)
     else:
+        text = ""
         place_id = ""
         user_id = ""
-        text = ""
 
     def __init__(self, *args, **kwargs):
         """initializes Review"""
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -5,9 +5,9 @@ Contains the TestReviewDocs classes
 
 from datetime import datetime
 import inspect
-import models
 from models import review
 from models.base_model import BaseModel
+import os
 import pep8
 import unittest
 Review = review.Review
@@ -67,42 +67,39 @@ class TestReview(unittest.TestCase):
         self.assertTrue(hasattr(review, "created_at"))
         self.assertTrue(hasattr(review, "updated_at"))
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                     "Testing DBStorage")
     def test_place_id_attr(self):
         """Test Review has attr place_id, and it's an empty string"""
         review = Review()
         self.assertTrue(hasattr(review, "place_id"))
-        if models.storage_t == 'db':
-            self.assertEqual(review.place_id, None)
-        else:
-            self.assertEqual(review.place_id, "")
+        self.assertEqual(review.place_id, "")
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                     "Testing DBStorage")
     def test_user_id_attr(self):
         """Test Review has attr user_id, and it's an empty string"""
         review = Review()
         self.assertTrue(hasattr(review, "user_id"))
-        if models.storage_t == 'db':
-            self.assertEqual(review.user_id, None)
-        else:
-            self.assertEqual(review.user_id, "")
+        self.assertEqual(review.user_id, "")
 
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
+                     "Testing DBStorage")
     def test_text_attr(self):
         """Test Review has attr text, and it's an empty string"""
         review = Review()
         self.assertTrue(hasattr(review, "text"))
-        if models.storage_t == 'db':
-            self.assertEqual(review.text, None)
-        else:
-            self.assertEqual(review.text, "")
+        self.assertEqual(review.text, "")
 
     def test_to_dict_creates_dict(self):
         """test to_dict method creates a dictionary with proper attrs"""
         r = Review()
         new_d = r.to_dict()
         self.assertEqual(type(new_d), dict)
-        self.assertFalse("_sa_instance_state" in new_d)
         for attr in r.__dict__:
             if attr is not "_sa_instance_state":
-                self.assertTrue(attr in new_d)
+                with self.subTest(attr=attr):
+                    self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
     def test_to_dict_values(self):
