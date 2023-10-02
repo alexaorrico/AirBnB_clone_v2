5e903e65cb12e4edf288fd099da66c29764e714d
Mohamed Amine <ma.abahmane@gmail.com>
2023-10-02 17:22:54 +0100

    feature added

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -39,7 +39,7 @@ def get_place_amenities(place_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                   methods=['DELETE'], strict_slashes=False)
-def del_place_amenities(place_id, amenity_id):
+def del_place_amenity(place_id, amenity_id):
     """
         A function that Deletes a Amenity object to a Place:
         DELETE /api/v1/places/<place_id>/amenities/<amenity_id>
@@ -67,5 +67,39 @@ def del_place_amenities(place_id, amenity_id):
 
         storage.save()
         return make_response(jsonify({}), 200)
+    else:
+        abort(404)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                  methods=['POST'], strict_slashes=False)
+def add_place_amenity(place_id, amenity_id):
+    """
+        A function that Link a Amenity object to a Place:
+        POST /api/v1/places/<place_id>/amenities/<amenity_id>
+    """
+    # get place by id
+    place = storage.get(Place, place_id)
+    if (not place):
+        abort(404)
+
+     # get amenities by id
+    amenity = storage.get(Amenity, amenity_id)
+
+    if (amenity):
+        # get environment type then add place amenity
+        if (env.get('HBNB_TYPE_STORAGE') == 'db'):
+            if (amenity in place.amenities):
+                return make_response(jsonify(amenity.to_dict()), 200)
+            else:
+                place.amenities.append(amenity)
+        else:
+            if (amenity_id in place.amenity_ids):
+                return make_response(jsonify(amenity.to_dict()), 200)
+            else:
+                place.amenity_ids.append(amenity_id)
+
+        storage.save()
+        return make_response(jsonify(amenity.to_dict()), 201)
     else:
         abort(404)
