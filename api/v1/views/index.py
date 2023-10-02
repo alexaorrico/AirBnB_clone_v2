4d094d31958bdaa89a4f66a516a5dd6af170440f
WalalaMarren <marrenanyangonyagaya@gmail.com>
2023-10-02 20:54:16 +0300

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-"""Retrieve numnber of objects by type"""
+"""Index page"""
 
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = [Amenity, City, Place, Review, State, User]
 
 
-@app.views.route('/stats', methods=['GET'], strict_slashes=False)
-def statistics():
-    """Retrieve number of each object by type"""
+@app_views.route('/status', strict_slashes=False)
+def index_status():
+    """Index route"""
+    return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
+    """Get status"""
     return jsonify(
-        {"amenities": storage.count("Amenity"),
-         "cities": storage.count("City"),
-         "places": storage.count("Place"),
-         "reviews": storage.count("Review"),
-         "states": storage.count("State"),
-         "users": storage.count("User")})
+        amenities=storage.count(Amenity),
+        cities=storage.count(City),
+        places=storage.count(Place),
+        reviews=storage.count(Review),
+        states=storage.count(State),
+        users=storage.count(User)
+            )
