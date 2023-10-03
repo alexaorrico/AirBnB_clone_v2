531f57f5c5ea97c2a48f828ad0a2e1b6b5bd5777
Belove <beloveolusola@gmail.com>
2023-10-03 01:51:48 +0100

    Api

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Flask application
+"""
+from flask import Blueprint
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Flask application
+"""
+from api.v1.views import app_views
+from flask import Flask, jsonify
+@app_views.route('/status')
+def status():
+    return jsonify({"status": "OK"})
