6318ef86e9ed2aca87c9530987fd0176a763eb5f
Belove <beloveolusola@gmail.com>
2023-10-03 02:19:20 +0100

    New route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,8 @@
 Flask application
 """
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -12,3 +13,16 @@ def status():
     route for status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """
+    Returns the number of each instance type
+    """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
