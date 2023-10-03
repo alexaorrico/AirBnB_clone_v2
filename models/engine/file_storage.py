44595f26de0e06d23daee8d8b7882b3cb456fa53
EllaRepo <eliaberango@gmail.com>
2023-10-03 04:03:48 -0400

    Update file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -73,7 +73,7 @@ class FileStorage:
         """Returns the object based on the class and its ID, or None if not
            found
         """
-        key = "{}.{}".format(cls, id)
+        key = "{}.{}".format(cls.__name__, id)
         if key in self.__objects.keys():
             return self.__objects[key]
         return None
