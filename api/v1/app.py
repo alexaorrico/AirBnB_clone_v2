bb74b75d355fef2250b36109aed2283d82f8a374
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-10-03 07:37:41 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,50 +1,36 @@
 #!/usr/bin/python3
-'''Contains a Flask web application API.
-'''
-import os
-from flask import Flask, jsonify
+"""
+This script runs a Flask API.
+"""
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
+host = getenv('HBNB_API_HOST', '0.0.0.0')
+port = getenv('HBNB_API_PORT', '5000')
 
 app = Flask(__name__)
-'''The Flask web application instance.'''
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
-app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
+app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_flask(exception):
-    '''The Flask app/request context end event listener.'''
-    # print(exception)
+def close_storage(self):
+    """
+    This closes the database session
+    """
     storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
-    '''Handles the 404 HTTP error code.'''
-    return jsonify(error='Not found'), 404
-
-
-@app.errorhandler(400)
-def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
+def not_found(error):
+    """
+    Returns a customized error message for 404 Not Found.
+    """
+    return make_response(jsonify({"error": 'Not found'}), 404)
 
 
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
-"""
-This Module handles the status route endpoint
-"""
-from api.v1.views import app_views
+'''Contains the index view for the API.'''
 from flask import jsonify
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.user import User
-from models.review import Review
 
-classes = {
-        "states": State,
-        "cities": City,
-        "amenities": Amenity,
-        "places": Place,
-        "users": User,
-        "reviews": Review
-        }
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
-def status():
-    """returns the status of a request"""
-    return jsonify({"status": "OK"})
+def get_status():
+    '''Gets the status of the API.
+    '''
+    return jsonify(status='OK')
 
 
 @app_views.route('/stats')
-def object_no():
-    """ retrieves the number of each objects based on its type """
-    objs_count = {}
-    for key, cls in classes.items():
-        n = storage.count(cls)
-        objs_count[key] = n
-    return jsonify(objs_count)
+def get_stats():
+    '''Gets the number of objects for each type.
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
