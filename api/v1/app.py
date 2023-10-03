2564ed056a8ec55cfd098f4c6fa2c15e158b901a
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-10-02 21:35:53 -0700

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,30 +1,50 @@
 #!/usr/bin/python3
-"""
-This module runs my API
-"""
-from flask import Flask, make_response, jsonify
+'''Contains a Flask web application API.
+'''
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 
-host = getenv('HBNB_API_HOST', '0.0.0.0')
-port = getenv('HBNB_API_PORT', '5000')
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
-def close_storage(self):
-    """This closes the database session"""
+def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """returns a customized error message"""
-    return make_response(jsonify({"error": 'Not found'}), 404)
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
+
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
 
 
 if __name__ == '__main__':
-    app.run(host=host, port=port, threaded=True)
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
