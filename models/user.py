ac5af7e375a18111896beb8ec49622b404b54322
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-10-02 21:27:45 -0700

    update

--- models/user.py
+++ models/user.py
@@ -25,5 +25,14 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """initializes users"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, __name: str, __value) -> None:
+        '''Sets an attribute of this class to a given value.'''
+        if __name == 'password':
+            if type(__value) is str:
+                m = hashlib.md5(bytes(__value, 'utf-8'))
+                super().__setattr__(__name, m.hexdigest())
+        else:
+            super().__setattr__(__name, __value)
