7a5bf62134179341cc58266bc69d8743758da4f7
Navi212 <josephchristo615@gmail.com>
2023-10-03 01:04:06 +0100

    Adds task4 file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,20 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route("/status", methods=["GET"], strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Checks status of route `/status`"""
-    dit = {"status": "OK"}
-    return jsonify(dit)
+    return jsonify(status='OK')
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieves the number of each objects by type"""
+    return jsonify(amenities=storage.count('Amenity'),
+                  cities=storage.count('City'),
+                  places=storage.count('Place'),
+                  reviews=storage.count('Review'),
+                  states=storage.count('State'),
+                  users=storage.count('User'))
