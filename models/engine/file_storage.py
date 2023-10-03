4c2f5ec79fa651791723249154b60fc5bb580bde
wadenya <alexwadenya001@gmail.com>
2023-10-03 00:38:58 +0000

    bnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,7 +69,7 @@ class FileStorage:
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
 
-     def get(self, cls, id):
+    def get(self, cls, id):
         """method to retrieve one object:"""
         key = "{}.{}".format(cls, id)
         if key in self.__objects.keys():
