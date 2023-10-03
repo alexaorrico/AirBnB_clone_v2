bbd89b3821b3910e8af88564c20084a23a0560f9
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-10-03 19:19:11 +0000

    no 6

---
+++ app.py
@@ -0,0 +1,10 @@
+# save this as app.py
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
