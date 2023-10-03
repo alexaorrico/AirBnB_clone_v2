eef3ef9b2f717238a44ffe91f6d4dc791eade1c1
Mbokola <tonypeaks@gmail.com>
2023-10-03 09:21:40 +0000

    Strict slashes false

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,7 +9,7 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 
 
-@app_views.route("/status", methods=["GET"])
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
     """Returns status of the api"""
     return jsonify({"status": "OK"})
