0290f64e9fc45a3959b2f49f2fa0caaaca99fb9c
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2023-10-03 16:38:11 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""API Stats"""
-
-from flask import Flask, jsonify
-from models import storage
+"""This module implement a rule that returns the status of the application"""
+from flask import jsonify
+import models
 from api.v1.views import app_views
-
-app = Flask(__name__)
-
-
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    """Get the count of each object type"""
-    count_dict = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(count_dict)
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app_views.route("/status", strict_slashes=False)
+def view_status():
+    """View function that return a json message"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def view_stats():
+    """Veiw function that retrieves the number of each object by type"""
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
