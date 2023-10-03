58112675c42474dcf19d6b3a1dd43d5b37addb54
Grace_ngigi <gracengigi859@gmail.com>
2023-10-02 17:42:29 -0700

    add get and count methods in file storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -70,19 +70,17 @@ class DBStorage:
         Returns:
             The object based on the class and its ID, or None if not found.
         """
-        try:
-            # Query the database to retrieve the object by ID
-            obj = self.__session.query(cls).filter_by(id=id).first()
-            return obj
-        except Exception as e:
-            print(f"Error: {e}")
+        if cls is None or id is None:
             return None
-        finally:
-            # Close the session
-            self.__session.close()
+        if type(cls) is str:
+            if cls not in classes:
+                return None
+            cls = classes[cls]
+        save = models.storage.all(cls)
+        return save.get("{}.{}".format(cls.__name__, id)) 
             
             
-    def get(self, cls, id):
+    def count(self, cls=None):
         """
         Count the number of objects in storage matching the given class.
         
@@ -93,20 +91,9 @@ class DBStorage:
             The number of objects in storage matching the given class.
             If no class is passed, returns the count of all objects in storage.
         """
-        try:
-            if cls:
-                # Count objects of the specified class
-                count = self.__session.query(cls).count()
-            else:
-                # Count all objects in storage
-                count = self.__session.query(cls).count()
-            return count
-        except Exception as e:
-            print(f"Error: {e}")
-            return 0
-        finally:
-            # Close the session
-            self.__session.close()
+        if cls is None:
+            return len(models.storage.all())
+        return len(models.storage.all(cls)) 
 
     def delete(self, obj=None):
         """delete from the current database session obj if not None"""
