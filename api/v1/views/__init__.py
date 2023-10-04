f79adb7c375bd3137091660fbb49ceb1069bb03e
Huming-bird <ahmedolalekan2@gmail.com>
2023-10-04 01:13:07 -0700

    updates

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,4 +10,4 @@ from api.v1.views.places_reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
