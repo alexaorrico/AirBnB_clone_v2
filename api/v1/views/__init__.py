d8c7f943f21957a30e7b632780a67faf8ae4754b
Tshupane Morake <tshupane@outlook.com>
2023-10-04 20:02:08 +0200

    module documentation added

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-
+""" Blueprint for API """
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" Index """
 
 from api.v1.views import app_views
 from flask import jsonify
