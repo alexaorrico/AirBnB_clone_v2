a419997c2cf47b1e917f3a59ec9d002d74385804
KOBBY-IM <nyarkotheo2@gmail.com>
2023-10-08 19:56:28 +0000

    udated app

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,14 +18,29 @@ def teardown_appcontext(self):
     storage.close()
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """ Custom error handle for 404 errors"""
-    response = {
-        "error": "Not found"
-    }
-    return jsonify(response), 404
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
+
 
 if __name__ == "__main__":
-    HOST = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    PORT = os.getenv("HBNB_API_PORT", '5000')
-    app.run(host=HOST, port=PORT, threaded=True)
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
