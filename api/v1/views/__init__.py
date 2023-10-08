7b9d11480edb1db455ded18a9ead411cc0a75f18
KOBBY-IM <nyarkotheo2@gmail.com>
2023-10-08 22:14:47 +0000

    pycode error

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,6 @@
 """ Blueprint for API """
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.places import *
