11c010135252d5d9fb44604fb5b9aaef9f3bcea3
Grace-ngigi <gracengigi859@gmail.com>
2023-10-21 17:19:05 +0300

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,17 @@
 Index module
 """
 from flask import jsonify
-from api.v1.views import app_views
+from views import app_views
+from models import storage
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/api/v1/stats', methods=['GET'])
 def status():
     """
     Returns status: OK in JSON format
     """
-    return jsonify({"status": "OK"})
+    stats = {}
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    for class_name in classes:
+        count = storage.count(class_name)
+        stats[class_name] = count
+    return jsonify(stats)
