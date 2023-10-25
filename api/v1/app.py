8bd392c2390d5c3fe01766380d6b744f4b88822c
H. Sabah <sabahhasbi00@gmail.com>
2023-10-25 13:56:48 +0000

    Code fix: changed order of import, fixed missing data

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 from flask import Flask
-from models import storage
 from api.v1.views import app_views
 from os import getenv
 
-app = Flask()
+app = Flask(__name__)
 app.register_blueprint(app_views)
 host = getenv("HBNB_API_HOST")
 port = getenv("HBNB_API_PORT")
 
 @app.teardown_appcontext
 def teardown(err):
+    from models import storage
     storage.close()
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,6 @@
 
 from api.v1.views import app_views
 
-@app_views('/status')
+@app_views.route('/status')
 def status():
     return {"status": "OK"}
