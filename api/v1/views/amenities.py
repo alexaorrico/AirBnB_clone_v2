a12fa94f2c7b8bed7020d7913f125a41278074e7
H. Sabah <sabahhasbi00@gmail.com>
2023-10-25 18:27:44 +0000

    Files added: task 8 plus some mods

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,43 +5,41 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
 import json
 
 
-@app_views.route('/states/<id_state>/cities', methods=['GET'])
-def get_cities(id_state):
-    """retrieves all cities by state id object"""
-    state = storage.get(State, id_state)
-    citiesList = []
-    if not state:
-        abort(404)
-    for city in state.cities:
-        citiesList.append(city.to_dict())
-    res = citiesList
-    response = make_response(json.dumps(res), 200)
+@app_views.route('/amenities', methods=['GET'])
+def get_amenities():
+    """retrieves all amenities"""
+    allAmenities = storage.all(Amenity).values()
+    amenitiesList = []
+    for amenity in allAmenities:
+        amenitiesList.append(amenity.to_dict())
+    response = make_response(json.dumps(amenitiesList), 200)
     response.headers['Content-Type'] = 'application/json'
     return response
 
 
-@app_views.route('/cities/<id>', methods=['GET'])
-def get_city(id):
-    """retrieves cities object with id"""
-    city = storage.get(City, id)
-    if not city:
+@app_views.route('/amenities/<id>', methods=['GET'])
+def get_amenity(id):
+    """retrieves amenity object with id"""
+    amenity = storage.get(Amenity, id)
+    if not amenity:
         abort(404)
-    response_data = city.to_dict()
+    response_data = amenity.to_dict()
     response = make_response(json.dumps(response_data), 200)
     response.headers['Content-Type'] = 'application/json'
     return response
 
 
-@app_views.route('/cities/<id>', methods=['DELETE'])
-def delete_city(id):
-    """delets city with id"""
-    city = storage.get(City, id)
-    if not city:
+@app_views.route('/amenities/<id>', methods=['DELETE'])
+def delete_amenity(id):
+    """delets amenity with id"""
+    amenity = storage.get(Amenity, id)
+    if not amenity:
         abort(404)
-    storage.delete(city)
+    storage.delete(amenity)
     storage.save()
     res = {}
     response = make_response(json.dumps(res), 200)
@@ -49,21 +47,23 @@ def delete_city(id):
     return response
 
 
-@app_views.route('/states/<id_state>/cities', methods=['POST'])
-def create_city(id_state):
+@app_views.route('/amenities', methods=['POST'])
+def create_amenity():
     """inserts city if its valid json amd has correct key and state id"""
+    abortMSG = "Not a JSON"
     missingMSG = "Missing name"
-    state = storage.get(State, id_state)
-    if not state:
-        abort(404)
+    if not request.get_json():
+        abort(
+            400,
+            description=abortMSG
+        )
     if 'name' not in request.get_json():
         abort(
             400,
             description=missingMSG
         )
     data = request.get_json()
-    instObj = City(**data)
-    instObj.state_id = id_state
+    instObj = Amenity(**data)
     instObj.save()
     res = instObj.to_dict()
     response = make_response(json.dumps(res), 201)
@@ -71,13 +71,13 @@ def create_city(id_state):
     return response
 
 
-@app_views.route('/cities/<id>', methods=['PUT'])
-def put_city(id):
+@app_views.route('/amenities/<id>', methods=['PUT'])
+def put_amenity(id):
     """update a city by id"""
     abortMSG = "Not a JSON"
-    city = storage.get(City, id)
-    ignoreKeys = ['id', 'state_id', 'created_at', 'updated_at']
-    if not city:
+    amenity = storage.get(Amenity, id)
+    ignoreKeys = ['id', 'created_at', 'updated_at']
+    if not amenity:
         abort(404)
     if not request.get_json():
         abort(
@@ -87,9 +87,9 @@ def put_city(id):
     data = request.get_json()
     for key, value in data.items():
         if key not in ignoreKeys:
-            setattr(city, key, value)
+            setattr(amenity, key, value)
     storage.save()
-    res = city.to_dict()
+    res = amenity.to_dict()
     response = make_response(json.dumps(res), 200)
     response.headers['Content-Type'] = 'application/json'
     return response
