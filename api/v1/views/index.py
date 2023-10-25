993403ef964412ca461155a63f03e61bdbdb1699
H. Sabah <sabahhasbi00@gmail.com>
2023-10-25 14:26:10 +0000

    Code fix: count for task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,3 +5,22 @@ from api.v1.views import app_views
 @app_views.route('/status')
 def status():
     return {"status": "OK"}
+
+@app_views.route('/stats')
+def stats():
+    from models import storage
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    count = {
+            'amenities': storage.count(Amenity),
+            'cities': storage.count(City),
+            'places': storage.count(Place),
+            'reviews': storage.count(Review),
+            'states': storage.count(State),
+            'users': storage.count(User)
+            }
+    return count
