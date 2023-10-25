f2fb6ea1beae9a3837bed25b48734093f530204b
YassineRKB <yassine@arcraven.com>
2023-10-25 17:08:21 +0200

    taks 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """api states"""
+from flask import abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -13,3 +14,13 @@ def get_states():
     for state in allStates:
         statesList.append(state.to_dict())
     return statesList
+
+
+@app_views.route('/states/<id>', methods=['GET'])
+def get_state(id):
+    """retrieves State object with id"""
+    state = storage.get(State, id)
+    if not state:
+        abort(404)
+    data = state.to_dict()
+    return data
