bcad7b97fda730e7cdf765c3a257a8af8a3e5dcd
H. Sabah <sabahhasbi00@gmail.com>
2023-10-25 11:10:02 +0000

    Code fix: added get() and count() methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,13 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        cls_objs = self.all(cls)
+        return cls_objs.get(cls.__name__ + '.' + id)
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        cls_objs = self.all(cls)
+        return len(cls_objs)
