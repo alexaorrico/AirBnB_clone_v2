4b335c84b878e3b333f42d0c30eb2491e22681cb
YassineRKB <yassine@arcraven.com>
2023-10-25 21:35:52 +0200

    task 14 security improvments user.py setattr from text to hash

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,7 +40,7 @@ class FileStorage:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
-    def save(self):
+    def save(self, fsmode=200):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
