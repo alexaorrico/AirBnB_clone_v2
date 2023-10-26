6ea619a1e778a12df7bc71f7ef5e04c1c3330436
aycrown1 <ayoadeadedamola198@gmail.>
2023-10-26 23:29:55 +0100

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,11 @@
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def check_status():
     """
     returns the status
     """
-    return jsonify({"status": "OK"})
+    if request.method == "GET":
+        response = {"status": "OK"}
+        return jsonify(response)
