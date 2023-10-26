33d89ff03c48ca45505db15c72f8891c19a6fba2
niyioo <niyiolagbegi@gmail.com>
2023-10-26 18:12:38 +0100

    nn

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -21,3 +21,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Import the views to include in the blueprint
 from api.v1.views.index import *
+from api.v1.views.states import *
