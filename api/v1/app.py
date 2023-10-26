f27375911e4c8da89e918be73259f5ee10ddb3ab
niyioo <niyiolagbegi@gmail.com>
2023-10-26 17:40:55 +0100

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,16 +5,19 @@ Main application module for the AirBnB API.
 This module creates a Flask application, registers blueprints,
 and handles app context teardown.
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from os import environ
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
+
 
 # Create a Flask app
 app = Flask(__name__)
 
 # Register the blueprint app_views with a URL prefix
 app.register_blueprint(app_views, url_prefix='/api/v1')
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 # Define a method to handle app context teardown
 @app.teardown_appcontext
@@ -36,12 +39,9 @@ def not_found(error):
 
     :param error: The 404 error object.
     """
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     # Get the host and port from environment variables or use defaults
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', 5000))
-
-    # Run the Flask app
-    app.run(host=host, port=port, threaded=True)
+    app.run(host = environ.get('HBNB_API_HOST', '0.0.0.0')
+            port = int(environ.get('HBNB_API_PORT', 5000)))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,20 +14,41 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+stats = {
+    "amenities": storage.count("Amenity"),
+    "cities": storage.count("City"),
+    "places": storage.count("Place"),
+    "reviews": storage.count("Review"),
+    "states": storage.count("State"),
+    "users": storage.count("User")
+}
+
+
+@app_views.route('/status', strict_slashes=False)
 def get_status():
+    """
+    Get the status of the API.
+
+    Returns:
+        JSON response containing the status.
+
+    Example:
+        {"status": "OK"}
+    """
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def get_stats():
     """
     Get the number of each object type.
 
     Returns:
         JSON response containing the count of each object type.
     """
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(stats)
+    d_dict = {}
+    for key, value in stats.items():
+        d_dict[key] = storage.count(value)
+    return jsonify(d_dict)
+
+if __name__ == "__main__":
+    pass
