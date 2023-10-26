d543fa0ec0a6965d362b5d2aca0d2ab49fc3a923
H. Sabah <sabahhasbi00@gmail.com>
2023-10-26 13:52:30 +0000

    Code fix: still half baked, but doesn't break

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -21,8 +21,13 @@ def get_place_amenities(id_place):
     amenitiesList = []
     if not place:
         abort(404)
-    for amenity in place.amenities:
-        amenitiesList.append(amenity.to_dict())
+    if storage_t == 'db':
+        for amenity in place.amenities:
+            amenitiesList.append(amenity.to_dict())
+    else:
+        for id in place.amenity_ids:
+            amenity = storage.get(Amenity, id)
+            amenitiesList.append(amenity.to_dict())
     res = amenitiesList
     response = make_response(json.dumps(res), 200)
     response.headers['Content-Type'] = 'application/json'
@@ -38,11 +43,11 @@ def delete_place_amenity(place_id, amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)        
-    if amenity not in place.amenities:
-        abort(404)
-    storage.delete(amenity)
     if storage_t != 'db':
+        if amenity not in place.amenity_ids:
+            abort(404)
         place.amenity_ids.remove(amenity_id)
+    storage.delete(amenity)
     storage.save()
     res = {}
     response = make_response(json.dumps(res), 200)
@@ -59,8 +64,12 @@ def create_place_amenity(place_id, amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
-    if amenity in place.amenities:
-        return amenity, 200
     if storage_t != 'db':
-        place.amenities.append(amenity_id)
-    return amenity
+        if amenity_id in place.amenity_ids:
+            return amenity, 200
+        place.amenity_ids.append(amenity_id)
+    print(place.to_dict())
+    storage.save()
+    response = make_response(json.dumps(amenity.to_dict()), 201)
+    response.headers['Content-Type'] = 'application/json'
+    return response
--- models/place.py
+++ models/place.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #!/usr/bin/python
 """ holds class Place"""
 import models
