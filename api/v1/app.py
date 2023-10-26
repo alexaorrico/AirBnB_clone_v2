089a6dfbdce1682c3aee93c64ba59155230e00fb
aycrown1 <ayoadeadedamola198@gmail.>
2023-10-26 23:24:48 +0100

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,6 @@ def teardown(self):
     storage.close()
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', 5000)
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
