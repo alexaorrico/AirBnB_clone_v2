2e145083dde17302480af134f0bafa04e58b0bf7
kumdan job <redeks123456@gmail.com>
2023-10-27 20:37:31 +0100

    task 3 not tested

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,8 +2,10 @@
 """for the following files"""
 from flask import Flask
 from models import storage
+from api.v1.views import app_views
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
