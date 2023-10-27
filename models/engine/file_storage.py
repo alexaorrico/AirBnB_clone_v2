af48471e59e17f58dbd2006256fc86d2d075ff66
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-27 08:16:25 +0000

    task 2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,30 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """ A method to retrieve one object
+            Args:
+            cls: class passed
+            id: string representing the object ID
+            Return: object based on the class and its ID, or None if not found
+        """
+        objs = self.all(cls)
+        for obj in objs.values():
+            if obj.id == id:
+                return obj
+
+    def count(self, cls=None):
+        """ count the number of objects in storage
+        Args:
+            cls: class passed
+        Return: the number of objects in storage matching the given class.
+        """
+        count = 0
+        if cls is None:
+            objs = self.all()
+        else:
+            objs = self.all(cls)
+        for obj in objs:
+            count += 1
+        return count
