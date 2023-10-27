935d7d50d7ff7ba86a6ba752094917c9bf141252
kumdan job <redeks123456@gmail.com>
2023-10-28 00:33:47 +0100

    task 4 not tested

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
 """for the following files"""
 from flask import jsonify
+from models import storage
 
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {
+    "Amenity": Amenity,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
 
 
 @app_views.route("/status")
 def status():
     """return the status of the application"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """return the stats of the application"""
+    new_d = {val.lower(): storage.count(val) for val in classes.keys()}
+
+    return jsonify(new_d)
