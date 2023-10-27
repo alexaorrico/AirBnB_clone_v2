340b30caafc23bc890cd964b410aafc9a39304c6
amalsayedm <amal.sayedm2@gmail.com>
2023-10-28 01:34:44 +0200

    amal

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,4 +25,5 @@ def page_not_found(error):
 
 if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+            port=int(os.getenv('HBNB_API_PORT', '5000')),
+            threaded=True)
