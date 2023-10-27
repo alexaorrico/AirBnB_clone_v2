0b672c0c13820a9ce3f675583ccd0987ffaa36ae
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-28 00:10:35 +0100

    pycodestyle fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,10 +9,12 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_(exception):
     """ close the storage """
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
