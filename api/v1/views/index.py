5476d611898c853e98cd26271ae8899953d1619b
besufikad <07yilmabese@gmail.com>
2023-10-27 17:04:58 +0300

    stats edited

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.review import Review
+from models.place import Place
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
@@ -21,11 +27,11 @@ def status():
 def get_stats():
     """Return /status api route"""
     stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
     }
     return jsonify(**stats)
