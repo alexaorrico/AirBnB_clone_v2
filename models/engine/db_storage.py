58d2a40dba6219d1d332031c9cda0dcc5fd5a789
Yvette Achieng <achiengyvette@gmail.com>
2023-10-27 03:05:24 -0700

    ammending files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieves one object"""
+        if cls and id:
+            fetch = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+            return all_obj.get(fetch)
+        return None
+
+    def count(self, cls=None):
+        """returns the count of objects in storage"""
+        return (len(self.all(cls)))
