c7db46833ea0ae9eb6dbc8c64bb043a420710786
MNatiki <natiart07@gmail.com>
2023-10-27 13:38:30 +0000

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """returning status of the api"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
@@ -14,3 +15,17 @@ def status():
         "status": "OK"
           }
     return jsonify(**arg)
+
+@app_views.route('/stats', methods=["GET"])
+def stats():
+    """Return /status api route"""
+    d = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    d = {k: storage.count(v) for k, v in d.items()}
+    return jsonify(d)
