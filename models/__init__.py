c8036d66670a23bed5359fd6843ffe58207341ec
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-28 01:44:45 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,15 @@
 initialize the models package
 """
 
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.engine import file_storage
+from models.engine import db_storage
 from os import getenv
 
 
