19b8666a25681fc13b1acbf57231209453cdbeaa
patrice02 <patricedev02@gmail.com>
2023-10-27 23:04:41 +0000

    setup repo

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,32 +4,34 @@ starts a Flask web application
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returns HBNB"""
-    return 'HBNB'
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def cisfun(text):
     """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+    return "C " + text.replace("_", " ")
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythoniscool(text="is cool"):
     """display “Python ”, followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+    return "Python " + text.replace("_", " ")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
