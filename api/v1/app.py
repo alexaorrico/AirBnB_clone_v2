957d2b070106f6a2b9e2ffdb918a6211759d14c7
Anas Ribki <anasribki147@gmail.com>
2023-10-27 21:49:23 +0100

    Add task 5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,6 +13,15 @@ app.register_blueprint(app_views)
 def close_db(err):
     """Close db Storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handler for 404 errors that returns a
+    JSON-formatted 404 status code response
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
