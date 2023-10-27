1373e404c247e8c7a50e431915731c2b138a6f29
codeabuu <abdulngui718@gmail.com>
2023-10-27 16:05:12 -0700

    updates

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -22,7 +22,7 @@ def get_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    return jsonify(amenities.to_dict())
+    return jsonify(amenity.to_dict())
 
 
 @app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'],
