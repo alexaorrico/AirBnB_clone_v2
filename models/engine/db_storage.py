e1fb486be2892023ac7075a16d4aac4f2a593402
sabrallah <sabrallah1991@gmail.com>
2023-10-27 13:01:42 -0700

    task1

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,12 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ Gets an object from DB """
+        cls_name = f"{cls.__name__}.{id}"
+        return self.all(cls).get(cls_name, None)
+
+    def count(self, cls=None):
+        """ Counts the number of objects in storage """
+        return len(self.all(cls)) if cls else len(self.all())
