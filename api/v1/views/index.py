f1ce4e12866bb35cebe029fb3a586cfd9ef7435a
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-27 16:42:33 +0000

    edit index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Returns JSON """
