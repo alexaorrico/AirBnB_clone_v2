5e8a11a1d75ce8a9918d4eea654d1ff964913efa
Fikayomi31 <fikayoogundijo@gmail.com>
2023-10-27 21:31:29 +0000

    improve storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,29 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Return the object based on the class and it ID, or None
+        if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        else:
+            all_cls = self.all(cls)
+            for value in all_cls.values():
+                if (value.id == id):
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """Return the count of all object in storage"""
+        all_class = classes.values()
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(model.storage.all(clas).values())
+        else:
+            count = len(model.storage.all(cls).values())
+
+        return count
