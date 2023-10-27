6e61db0043f27bdd68f810a6a4bb8f88bc88a8e6
niyioo <niyiolagbegi@gmail.com>
2023-10-27 13:00:03 +0100

    n

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
+
 # Define a method to handle app context teardown
 @app.teardown_appcontext
 def teardown(exception):
@@ -31,6 +32,7 @@ def teardown(exception):
     """
     storage.close()
 
+
 # Define a handler for 404 errors
 @app.errorhandler(404)
 def not_found(error):
@@ -41,7 +43,8 @@ def not_found(error):
     """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
+
 if __name__ == "__main__":
     # Get the host and port from environment variables or use defaults
-    app.run(host = environ.get('HBNB_API_HOST', '0.0.0.0'),
-            port = int(environ.get('HBNB_API_PORT', 5000)))
+    app.run(host=environ.get('HBNB_API_HOST', '0.0.0.0'),
+            port=int(environ.get('HBNB_API_PORT', 5000)))
