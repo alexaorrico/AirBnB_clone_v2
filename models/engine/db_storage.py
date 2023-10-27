665ab30114ee965588a203d36b9a52fb0757f68d
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-27 01:37:46 +0000

    update db and file storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,16 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        for object in self.all(cls).values():
+            if object.id == id:
+                return object
+        return None
+
+    def count(self, cls=None):
+        """A method to count the number of objects in storage"""
+        return len(self.all(cls).values())
+
+
