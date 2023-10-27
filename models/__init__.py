4d6a84b040986ef678d241aef960507ace9bc80b
patrice02 <patricedev02@gmail.com>
2023-10-27 23:04:41 +0000

    setup repo

--- models/__init__.py
+++ models/__init__.py
@@ -10,8 +10,10 @@ storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
+
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
+
     storage = FileStorage()
 storage.reload()
--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -6,16 +6,17 @@ starts a Flask web application
 from flask import Flask, render_template
 from models import *
 from models import storage
+
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
+@app.route("/hbnb_filters", strict_slashes=False)
 def filters():
     """display a HTML page like 6-index.html from static"""
     states = storage.all("State").values()
     amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+    html = "10-hbnb_filters.html"
+    return render_template(html, states=states, amenities=amenities)
 
 
 @app.teardown_appcontext
@@ -23,5 +24,6 @@ def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
