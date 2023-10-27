9bcccbeb4ac794cf4ba54a7dcebcccefa4b0406a
Olamide Alokan <alokanolamide@gmail.com>
2023-10-27 14:18:23 +0100

    added Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ def teardown(self):
     """
     
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
