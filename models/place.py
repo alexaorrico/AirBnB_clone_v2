da0840e546ff5cf68d6eb27cb82d55656f70c332
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-28 01:57:03 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -7,7 +7,7 @@ import sqlalchemy
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
-if models.storage_t == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60),
                                  ForeignKey('places.id', onupdate='CASCADE',
