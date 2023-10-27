89d0c85a58f5a368e8b1d0ecee19b2ff3d68322a
stepholo <oloobrian89@gmail.com>
2023-10-27 04:15:20 -0700

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from api.v1.views import app_views
 from flask import jsonify, make_response
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Returns json rep of response code"""
     return make_response(jsonify({"status": "OK"}), 200)
+
+
+@app_views.route('/stats')
+def count_objects():
+    """counts the number of respective object"""
+    classes = ['Amenities', 'Cities', 'Places', 'Reviews', 'States', 'Users']
+    objs = {'amenities', 'cities', 'places', 'reviews', 'states', 'users'}
+    i = 0
+    for key in objs:
+        objs[key] = storage.count(classes[i])
+        i+=1
+
+    return make_response(jsonify({objs}))
+
