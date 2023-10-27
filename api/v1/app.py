8dc5be67b6ed60d51b2f594e465f405b12af576c
SHmohamed229 <SHmohamed229@gmail.com>
2023-10-27 08:09:05 -0700

    u

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,10 @@ this for API for AirBnB_clone_v3
 
 import os
 from flask import Flask, jsonify, Response
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
