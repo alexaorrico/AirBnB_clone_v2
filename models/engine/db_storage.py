20d1547664f24c3dd04baf0fbced67d324161ebc
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-28 01:32:23 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,10 +66,10 @@ class DBStorage:
 
     def reload(self):
         """reloads data from the database"""
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Base.metadata.create_all(self.__engine)
-        Session = scoped_session(sess_factory)
-        self.__session = Session
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        # Session = scoped_session(sess_factory)
+        # self.__session = Session
 
     def close(self):
         """call remove() method on the private session attribute"""
