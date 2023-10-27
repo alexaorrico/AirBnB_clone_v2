ec06845515b928f9435d7b96802ce62a80068de9
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-28 01:51:46 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -11,14 +11,15 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
 if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
     Base = declarative_base()
 else:
     Base = object
 
-"""time = "%Y-%m-%dT%H:%M:%S.%f"
 
-if models.storage_t == "db":
+
+"""if models.storage_t == "db":
     Base = declarative_base()
 else:
     Base = object"""
@@ -26,7 +27,7 @@ else:
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
-    if models.storage_t == "db":
+    if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
