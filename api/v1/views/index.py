9ecac0e6964578b40a2834ba487017e4c254a34e
kumdan job <redeks123456@gmail.com>
2023-10-27 21:21:35 +0100

    task 3 not tested

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """for the following files"""
-from api.v1.views import app_views
 from flask import jsonify
+
+from api.v1.views import app_views
 
 
 @app_views.route("/status")
 def status():
     """return the status of the application"""
-    return jsonify(status="OK")
+    return jsonify({"status": "OK"})
