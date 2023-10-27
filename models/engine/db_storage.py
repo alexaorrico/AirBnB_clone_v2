c4830a41fd1d6ed079a82d7f21a3eda1e4a16a87
Akwesi-bonah <arhinbonnah@gmail.com>
2023-10-27 08:38:41 +0000

    error fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -49,7 +49,7 @@ class DBStorage:
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
-        return new_dict
+        return (new_dict)
 
     def new(self, obj):
         """add the object to the current database session"""
@@ -76,33 +76,16 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """
-        Returns the object based on the class name and its ID, or
-        None if not found
-        """
-        if cls not in classes.values():
+        """A method to retrieve one object"""
+        if cls is None or id is None:
             return None
-
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if value.id == id:
-                return value
+        objs = models.storage.all(cls)
+        for obj in objs.values():
+            if obj.id == id:
+                return obj
         return None
 
     def count(self, cls=None):
-<<<<<<< HEAD
-        """
-        count the number of objects in storage
-        """
-        all_class = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
-=======
         """returns the number of objects 
         in storage matching the given class name. 
         If no name is passed, returns the count 
@@ -113,5 +96,4 @@ class DBStorage:
             count = 0
             for value in all_cls:
                 count += len(models.storage.all(value).values())
->>>>>>> storage_get_count
         return count
