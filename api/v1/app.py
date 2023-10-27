e94a2b3311e6325bc14d1cffd230ab9845c88749
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-27 16:34:55 +0000

    edit task 3, 4, 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ Flask Application """
 from models import storage
-from api.v1.views import app_views
+from api.v1.views.index import app_views
 from os import environ
-from flask import Flask, render_template, make_response, jsonify
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Index """
-from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-
+from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
