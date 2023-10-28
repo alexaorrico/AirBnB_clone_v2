6c125b53eefe41bc5f06445a96086c8e7b945db0
amisoni70 <amisoni7014@gmail.com>
2023-10-28 06:57:00 -0700

    task 2 refined

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -78,13 +78,13 @@ class DBStorage:
     def get(self, cls, id):
         """A method to retrieve one object"""
         if cls and id:
-            temp = cls, __name__ + "." + id
-            count = self.all(cls)
-            for key in count:
-                if key == temp:
-                    return count[key]
-        else:
-            return None
+            # Check if the class exists in storage
+            if cls.__name__ in self.__objects:
+                objects_of_class = self.__objects[cls.__name__]
+                for obj in objects_of_class.values():
+                    if obj.id == id:
+                        return obj  # Return the object if found
+        return None
 
     def count(self, cls=None):
         """A method to count the no. of objects in storage"""
