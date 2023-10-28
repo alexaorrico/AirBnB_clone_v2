413878a26e323c4b40b11e4b66e75437158633cf
Abodawoud <yousefabodawoud@gmail.com>
2023-10-28 22:02:34 +0200

    Add route 'api/v1 /stats'

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,13 @@
 """index.py that returns a JSON"""
 
 from api.v1.views import app_views, jsonify
-
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route("/status")
 def status():
@@ -11,3 +17,18 @@ def status():
         'status': 'OK'
     }
     return (jsonify(data))
+
+
+@app_views.route('/stats')
+def stats():
+
+    data = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+
+    return jsonify(data)
