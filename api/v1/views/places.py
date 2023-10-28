ba7e05e4656d104945b9154e54799db52816ca83
MNatiki <natiart07@gmail.com>
2023-10-28 13:49:20 +0000

    api

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -70,7 +70,8 @@ def update_place(place_id):
     if not request.get_json():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
     data = request.get_json()
-    if 'name' in data:
-        place.name = data['name']
+    for key, value in data.items():
+        if key not in ('id', 'user_id', 'city_id', 'created_at', 'updated_at'):
+            setattr(place, key, value)
     place.save()
     return jsonify(place.to_dict())
