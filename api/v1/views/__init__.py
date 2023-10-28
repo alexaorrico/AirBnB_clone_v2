57491222faf7d37f31cc9fc4c25e5d125097d5ca
besufikad <07yilmabese@gmail.com>
2023-10-28 14:00:44 +0300

    api dir

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,7 +7,7 @@ app_views = Blueprint('api', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places import *
+# from api.v1.views.amenities import *
+# from api.v1.views.users import *
+# from api.v1.views.places_reviews import *
+# from api.v1.views.places import *
