3293ec28181c8f4bfcaaec6b1764a7359015800e
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-28 02:22:27 +0200

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,9 +16,14 @@ import sqlalchemy
 from sqlalchemy import create_engine, func
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
+classes = {
+    'Amenity': Amenity,
+    'City': City,
+    'Place': Place,
+    'State': State,
+    'Review': Review,
+    'User': User
+}
 
 class DBStorage:
     """interaacts with the MySQL database"""
