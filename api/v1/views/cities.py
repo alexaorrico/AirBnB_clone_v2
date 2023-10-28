725ee61c1b1824d894992fd250cc9c7b9a4af09e
JanviaveTharwat <janviavetharwat@gmail.com>
2023-10-28 21:15:10 +0000

    7

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,28 +11,28 @@ from models.state import State
                  methods=['GET'], strict_slashes=False)
 def cities(state_id):
     """Retrieves the list of all City objects of a State"""
-    obj_state = storage.get(State, state_id)
-    if not obj_state:
+    all_cities = storage.get(State, state_id)
+    if not all_cities:
         abort(404)
-    return jsonify([city.to_dict() for city in obj_state.cities])
+    return jsonify([city.to_dict() for city in all_cities.cities])
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def single_city(city_id):
     """Retrieves a City object"""
-    obj = storage.get(City, city_id)
-    if not obj:
+    one_city = storage.get(City, city_id)
+    if not one_city:
         abort(404)
-    return jsonify(obj.to_dict())
+    return jsonify(one_city.to_dict())
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def del_city(city_id):
     """Returns an empty dictionary with the status code 200"""
-    obj = storage.get(City, city_id)
-    if not obj:
+    city_deleted = storage.get(City, city_id)
+    if not city_deleted:
         abort(404)
-    obj.delete()
+    city_deleted.delete()
     storage.save()
     return make_response(jsonify({}), 200)
 
@@ -41,8 +41,8 @@ def del_city(city_id):
                  methods=['POST'], strict_slashes=False)
 def post_city(state_id):
     """Returns the new City with the status code 201"""
-    obj_state = storage.get(State, state_id)
-    if not obj_state:
+    city_state = storage.get(State, state_id)
+    if not city_state:
         abort(404)
 
     new_city = request.get_json()
@@ -61,8 +61,8 @@ def post_city(state_id):
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
     """Returns the City object with the status code 200"""
-    obj = storage.get(City, city_id)
-    if not obj:
+    new_city = storage.get(City, city_id)
+    if not new_city:
         abort(404)
 
     req = request.get_json()
@@ -71,7 +71,7 @@ def put_city(city_id):
 
     for k, v in req.items():
         if k not in ['id', 'created_at', 'update_at', 'state_id']:
-            setattr(obj, k, v)
+            setattr(new_city, k, v)
 
     storage.save()
-    return make_response(jsonify(obj.to_dict()), 200)
+    return make_response(jsonify(new_city.to_dict()), 200)
