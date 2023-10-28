eae86f4a0916ae000075374f339f3cdfdcc85538
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-28 21:52:19 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -28,12 +28,7 @@ def not_found(error):
 
 
 if __name__ == '__main__':
-    if getenv("HBNB_API_HOST") is None:
-        HBNB_API_HOST = '0.0.0.0'
-    else:
-        HBNB_API_HOST = getenv("HBNB_API_HOST")
-    if getenv("HBNB_API_PORT") is None:
-        HBNB_API_PORT = 5000
-    else:
-        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+   app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
