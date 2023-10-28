3c9e0efe3d6dd9c84b4e9067ba4846fed0df8cc5
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-28 02:00:20 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -22,7 +22,7 @@ class State(BaseModel, Base):
         """initializes state"""
         super().__init__(*args, **kwargs)
 
-    if models.storage_t != "db":
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
