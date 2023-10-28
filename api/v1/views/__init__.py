e57c528bb8efd68ab25c08eac44b8034b00e7843
bion-slmn <bionsol25@gmail.com>
2023-10-28 03:56:47 -0700

    init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,8 +8,6 @@ app_views = Blueprint(
     __name__,
     url_prefix='/api/v1'
 )
-
-# imported at the bottom to avoid circular import error
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
