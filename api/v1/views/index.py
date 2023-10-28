8f2b5599c751a1641fa84222d74a2361be8bf233
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-28 19:40:32 +0100

    api to retrieve number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,12 @@ all associated methods with the blueprint
 """
 from api.v1.views import app_views
 from flask import jsonify
-from models import Amenity, City, Place, Review, State, User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
 
 
@@ -17,4 +22,19 @@ def api_ok():
     """
     return jsonify({"status": "OK"})
 
-# @app_views.route("/stats")
+
+@app_views.route("/stats")
+def number_per_object():
+    """
+    return the total number of objects
+    per class
+    """
+    number_per_object = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return jsonify(number_per_object)
