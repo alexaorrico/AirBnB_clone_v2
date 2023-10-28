8aa55037b475cf3ca43b72150ac80e43fe5991b9
Pritchad25 <pricts29@gmail.com>
2023-10-28 06:51:47 -0700

    importing Blueprint from flask and creating a varable which is an instance of Blueprint called app_views. Then creating a route on the object app_views that returns a JSON.

---
+++ api/v1/views/index.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This Python script creates a route `/status` on the object app_views.
+"""
+
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'])
+def api_status():
+	"""
+	This method returns a JSON response for the RESTful API health.
+	"""
+	response = {'status': 'OK'}
+	return jsonify(response)
