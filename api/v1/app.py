8fcf85f9b457d1524fdda565f86b85bb3b7bb366
amisoni70 <amisoni7014@gmail.com>
2023-10-28 14:21:09 -0700

    Task 4 and 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,15 @@ Creation of a variable app(using flask)
 """
 
 
-from flask import Flask
+from flask import Flask, jsonify
+from werkzeug.exceptions import NotFound
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -18,6 +20,14 @@ def downtear(self):
     """A method to handle the app
     teardown"""
     storage.close()
+
+
+@app.errorhandler(NotFound)
+def not_found_error(e):
+    '''return render_template'''
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 """Status of the api"""
 
-
+import models
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.base_model import BaseModel
 
 
 @app_views.route('/status', strict_slashes=False)
 def returnstuff():
     '''return stuff'''
     return jsonify(status='OK')
+
+@app_views.route('/stats', strict_slashes=False)
+def statistics():
+    '''Method that returns the statistics
+    about various stuff in JSON format'''
+    todo = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in todo:
+        todo[key] = storage.count(todo[key])
+    return jsonify(todo)
