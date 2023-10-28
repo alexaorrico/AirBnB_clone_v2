a50b6a0372f9376c8a9b9f6b28fe66b6d190068d
Tim Ngeno <timngeno424@gmail.com>
2023-10-28 15:44:01 +0300

    create init files

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Init module"""
+
+from flask import Blueprint
+
+# Initialize blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# Import views
+from api.v1.views import *
