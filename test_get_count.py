7c84a7cb41eec845493712170791ed29fd567228
patrice <patricedev02@gmail.com>
2023-10-28 19:47:23 +0000

    Status of your API

--- test_get_count.py
+++ test_get_count.py
@@ -8,6 +8,5 @@ print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
 
 first_state_id = list(storage.all(State).values())[0].id
-print(first_state_id, "iddddddddd")
 print(first_state_id in str(storage.get(State, first_state_id)))
 print("First state: {}".format(storage.get(State, first_state_id)))
