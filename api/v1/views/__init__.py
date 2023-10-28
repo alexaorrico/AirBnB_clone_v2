c72595775e01bbf7a42a49c3dc7caf31c81b377a
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-28 11:09:53 -0700

    API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Wildcard import of everything in package api.v1.views.index
 from api.v1.views.index import *
+from api.v1.views.states import *
