def7713473d544811c1ed18b190eb940fcc7ebdf
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-28 12:26:15 +0000

    get port and host from environment variables

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,6 +6,7 @@ from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 import json
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -24,4 +25,7 @@ def page_not_found(e):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+
+    app.run(host=host, port=port, threaded=True)
