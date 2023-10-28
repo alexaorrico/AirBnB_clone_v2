d8c0d2c57394800d3874d0661afa7847f42b04dc
salihabrik <briksaliha@gmail.com>
2023-10-28 22:05:08 +0200

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-""" Create a Route -> (/status) on the Object """
-from flask import Response, jsonify
+"""Returns a Json response"""
+
+from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.engine.db_storage import classes
 
 
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """ Status ->  That Return a JSON -> ('OK') """
-    return jsonify({'status': 'OK'})
+@app_views.route('/status')
+def status_check():
+    '''Returns status code'''
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def stats():
-    """ In Storage -> Retrieve the count of objects """
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
-
-    classes = {"amenities": Amenity, "cities": City, "places": Place,
-               "reviews": Review, "states": State, "users": User}
-    json_dict = {}
-
-    for name, clas in classes.items():
-        json_dict.update({name: storage.count(clas)})
-    return jsonify(json_dict)
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    """Retrieves the no of each object by type"""
+    objects = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User'),
+            }
+    return jsonify(objects)
