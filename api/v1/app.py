ea33442174f8024ddaacd0006ad646603650db83
root <ohamdi776@gmail.com>
2023-10-28 05:58:53 -0700

    Completed 5. Not found task

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """An app for registering a blueprint and starting a server"""
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown(self):
     """Close query"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Return JSON 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
