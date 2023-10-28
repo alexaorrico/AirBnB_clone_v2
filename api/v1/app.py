0ae33aedd3a1cd5cd1c217dddb89a51d19201e6c
abdelmalek <abdelmalek@gmail.com>
2023-10-28 01:45:24 -0700

    fix output for task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import os
-from flask import Flask, jsonify, Blueprint, make_response, jsonify
+from os import getenv
+from flask import Flask, jsonify, Blueprint, make_response
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 from flask_cors import CORS
 
 # Create a Flask instance
@@ -13,13 +13,13 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 # Cross-Origin Resource Sharing
-CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
 def not_found(error):
     """Handles 404 errors by returning a JSON response."""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,12 @@ Flask route that returns json status response
 from flask import jsonify, request
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
