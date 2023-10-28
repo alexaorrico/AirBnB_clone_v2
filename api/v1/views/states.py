669a34d6459ada54515f844ab16526c8bd95a125
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-10-28 23:22:37 +0200

    6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,27 +9,27 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def state():
     """Retrieves the list of all State objects"""
-    all_sts = storage.all(State)
-    return jsonify([st.to_dict() for ost in all_sts.values()])
+    objs = storage.all(State)
+    return jsonify([obj.to_dict() for obj in objs.values()])
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def single_state(state_id):
     """Retrieves a State object"""
-    one_st = storage.get(State, state_id)
-    if not one_st:
+    obj = storage.get(State, state_id)
+    if not obj:
         abort(404)
-    return jsonify(one_st.to_dict())
+    return jsonify(obj.to_dict())
 
 
 @app_views.route('/states/<state_id>',
                  methods=['DELETE'], strict_slashes=False)
 def del_state(state_id):
     """Deletes a State object"""
-    st_deleted = storage.get(State, state_id)
+    obj = storage.get(State, state_id)
     if not obj:
         abort(404)
-    st_deleted.delete()
+    obj.delete()
     storage.save()
     return make_response(jsonify({}), 200)
 
@@ -37,28 +37,31 @@ def del_state(state_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
     """Returns the new State with the status code 201"""
-    new_st = request.get_json()
-    if not new_st:
+    new_obj = request.get_json()
+    if not new_obj:
         abort(400, "Not a JSON")
     if 'name' not in new_obj:
         abort(400, "Missing name")
-    st = State(**new_st)
-    storage.new(st)
+    obj = State(**new_obj)
+    storage.new(obj)
     storage.save()
-    return make_response(jsonify(st.to_dict()), 201)
+    return make_response(jsonify(obj.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     """ Updates a State object """
-    st = storage.get(State, state_id)
-    if not st:
+    obj = storage.get(State, state_id)
+    if not obj:
         abort(404)
+
     req = request.get_json()
     if not req:
         abort(400, "Not a JSON")
+
     for k, v in req.items():
         if k not in ['id', 'created_at', 'updated_at']:
-            setattr(st, k, v)
+            setattr(obj, k, v)
+
     storage.save()
     return make_response(jsonify(obj.to_dict()), 200)
