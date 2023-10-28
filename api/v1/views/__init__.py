e57b6c28a9bc8617fb62df933944e2bd7a150bf2
achrafezzaam <achraf.ezzaam@gmail.com>
2023-10-28 17:57:19 +0100

    Correctinng the path to the reviews views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -16,4 +16,4 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.places import *
-from api.v1.views.reviews import *
+from api.v1.views.places_reviews import *
