6b79908734099ae94d378d58b0bffd96d918a7d7
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-28 14:07:13 +0300

    repaired pycodestyle

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """API endpoints for places"""
+from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
-from models import storage
 from flask import abort, jsonify, request
 from api.v1.views import app_views
 
