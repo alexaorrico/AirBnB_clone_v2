410b52e193352347d1fd6198328bd57949170921
root <root@DESKTOP-T0EB176.localdomain>
2023-10-28 10:00:05 +0100

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,18 @@ def ok_status():
     return an ok json status
     """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats/')
+def stats():
+    """
+    endpoint returns stats
+    returns a number of objects of each class
+    """
+    models = {"User": "users",
+              "Amenity": "amenities", "City": "cities",
+              "Place": "places", "Review": "reviews",
+              "State": "states"}
+    stats = {}
+    for cls in models.keys():
+        stats[models[cls]] = storage.count(cls)
+    return jsonify(stats)
