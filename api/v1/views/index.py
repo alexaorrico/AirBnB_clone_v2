60c3b21882191482e6ba60982b67779484f93b05
Noransaber <noransaber685@gmail.com>
2023-10-28 18:25:04 +0200

    Editing the style

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
-	""" Return OK"""
-	return jsonify(status='OK')
-
-
+    """ Return OK"""
+    return jsonify(status='OK')
