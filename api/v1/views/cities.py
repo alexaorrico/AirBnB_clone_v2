744ec57abf7faef4e6315e2a8b7beaeedc7594d8
kaleabendrias <kaleabendrias1212@gmail.com>
2023-10-28 11:23:43 +0300

    trial1

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -29,31 +29,30 @@ def get_city(city_id):
 @app_views.route("/cities/<city_id>", methods=["DELETE"])
 def delete_city(city_id):
     """Deletes a City object"""
-    empty_dict = {}
-    city = storage.get("City", city_id)
+    city = storage.get(City, city_id)
     if city is None:
         abort(404)
     storage.delete(city)
     storage.save()
-    return jsonify(empty_dict), 200
+    return jsonify({}), 200
 
 
 @app_views.route("/states/<state_id>/cities", methods=['POST'])
 def post_city(state_id):
     """Creates a City"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     data = request.get_json()
-    if not data:
-        return jsonify({"error": "Not a JSON"}), 400
-    if "name" not in data:
-        return jsonify({"error": "Missing name"}), 400
+    if data is None:
+        abort(400, "Not a JSON")
+    if 'name' not in data:
+        abort(400, "Missing name")
     city = City(**data)
     city.state_id = state_id
-    storage.new(city)
-    storage.save()
-    return jsonify(city.to_dict()), 201
+    city.save()
+    city = city.to_json()
+    return jsonify(city), 201
 
 
 @app_views.route("/cities/<city_id>", methods=['PUT'])
