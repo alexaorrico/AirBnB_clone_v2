8be765b4b0852ea6acdda4bd84fbfc5088ee2500
AckimJnr <acklongwe@gmail.com>
2023-10-28 22:01:26 +0200

    Fix module level import in api/v1/views/__init__.py file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,10 +2,6 @@
 """ Blueprint module """
 
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -14,3 +10,6 @@ from api.v1.views.amenities import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
