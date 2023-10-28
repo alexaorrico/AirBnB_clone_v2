12c23d08b355c2f2dfbb30def9809a457dab3878
kemelite1 <osikemelite42@gmail.com>
2023-10-28 12:02:38 +0100

    a handler for 404 errors that returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,14 @@ def teardown(err):
     """Teardown the application"""
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """loads a custom 404 page not found"""
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, threaded=True)
