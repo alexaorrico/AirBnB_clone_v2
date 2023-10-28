d405ac19ef1b7ac9f523aa5097aaead7b87b275f
kumdan job <redeks123456@gmail.com>
2023-10-28 20:55:22 +0100

    task 5 not tested

--- models/city.py
+++ models/city.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 """ holds class City"""
-import models
-from models.base_model import BaseModel, Base
 from os import getenv
+
 import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
+
+import models
+from models.base_model import Base, BaseModel
 
 
 class City(BaseModel, Base):
-    """Representation of city """
+    """Representation of city"""
+
     if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        places = relationship("Place", backref="cities", cascade="all, delete")
     else:
         state_id = ""
         name = ""
