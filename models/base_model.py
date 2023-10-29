ca795c1e62998e89717eba1b92de4ca4835c9b5f
Basant-Adel <37676351+Basant-Adel@users.noreply.github.com>
2023-10-29 17:16:25 +0200

    Update the method to_dict() of BaseModel to remove the password :D

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains class BaseModel
-"""
+""" Contains class BaseModel """
 
 from datetime import datetime
 import models
@@ -20,14 +18,14 @@ else:
 
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """ BaseModel class from which future classes will be derived """
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """ Initialization of base model """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -48,18 +46,18 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation of the BaseModel class"""
+        """ String representation of BaseModel class """
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                                          self.__dict__)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        """ Updates attribute 'updated_at' with current datetime """
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """ Returns dictionary containing all keys/values of instance """
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
@@ -68,8 +66,10 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if "password" in new_dict:
+            del new_dict["password"]
         return new_dict
 
     def delete(self):
-        """delete the current instance from the storage"""
+        """ Delete the current instance from storage """
         models.storage.delete(self)
