4e189b5dd8f59c385e854d6789abb61b2e6db5db
Tim Ngeno <timngeno424@gmail.com>
2023-10-29 21:46:17 +0300

    retrieve number of objects in storage by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,30 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
 def check_status():
     """Returns status of the flask app"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def fetch_stats():
+    """Retrieve objects info from storage"""
+    stats = {}
+    stats['amenities'] = storage.count(Amenity)
+    stats['cities'] = storage.count(City)
+    stats['places'] = storage.count(Place)
+    stats['reviews'] = storage.count(Review)
+    stats['states'] = storage.count(State)
+    stats['users'] = storage.count(User)
+
+    return jsonify(stats)
