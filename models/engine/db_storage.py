c994e6dfd62780534fe902680746efe24ebfcb4e
jabulane <jabumokoena85@gmail.com>
2023-10-29 11:32:46 -0700

    Revert "updating file storage and database storage, also unittests for the new updates"
    This reverts commit b7f84ec3fab16c7840b302d2580a9def9a960a19.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,22 +74,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def get(self, cls, id):
-        """
-        A method to retrieve one object, and
-        return the object based on the class and its ID, or None if not found
-        """
-        if cls and id:
-            retrieved_objects = "{}.{}".format(cls, id)
-            all_objects_db = self.all(cls)
-            return all_objects_db.get(retrieved_objects)
-        return None
-
-    def count(self, cls=None):
-        """
-        A method to count the number of objects in storage
-        Returns the number of objects in storage matching the given class.
-        If no class is passed, returns the count of all objects in storage.
-        """
-         return (len(self.all(cls)))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,22 +68,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
-    def get(self, cls, id):
-        """
-        A method to retrieve one object, and
-        return the object based on the class and its ID, or None if not found
-        """
-        if cls and id:
-            retrieved_objects = "{}.{}".format(cls, id)
-            all_objects_file = self.all(cls)
-            return all_objects_file.get(retrieved_objects)
-        return None
-
-    def count(self, cls=None):
-        """
-        A method to count the number of objects in storage
-        Returns the number of objects in storage matching the given class.
-        If no class is passed, returns the count of all objects in storage.
-        """
-        return len(self.all(cls))
