411c0dea27158450824181a5db1cf5ebd91231d5
biitopher <biitopher@github.com>
2023-10-29 10:51:38 -0700

    [Fix]Task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,47 +9,58 @@ from datetime import datetime
 import uuid
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def get_states():
-    states = State.all()
-    return jsonify([state.to_dict() for state in states])
+def list_states():
+    """retrieves list of all State"""
+    states = [state.to_dict() for state in storage.all("State").values()]
+    return jsonify(states)
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
-    state = State.get(state_id)
+    """retrieves a State"""
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
+
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
-    state = State.get(state_id)
+    """deletes a State"""
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
-    state.delete()
+    storage.delete(state)
+    storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
+    """creates state"""
     data = request.get_json()
     if data is None:
-        abort(400, "Not a JSON")
+        return jsonify({"error": "Not a JSON"}), 400
     if "name" not in data:
-        abort(400, "Missing name")
-    state = State(**data)
-    state.save()
-    return jsonify(state.to_dict()), 201
+        return jsonify({"error": "Missing name"}), 400
+    new_state = State(**data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    state = State.get(state_id)
+    """updates State"""
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     data = request.get_json()
     if data is None:
-        abort(400, "Not a JSON")
+        return jsonify({"error": "Not a JSON"}), 400
     for key, value in data.items():
-        if key not in ('id', 'created_at', 'updated_at'):
+        if key not in ["id", "created_at", "updated_at"]:
             setattr(state, key, value)
 
-    state.save()
+    storage.save()
     return jsonify(state.to_dict()), 200
