8b9c530ae77230b9ff579cf07ebbdcc0b6665249
CourageT1 <tcgwizhi@gmail.com>
2023-10-29 04:06:20 -0700

    correcting3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """api views & blueprint imports"""
 from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,4 +11,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
