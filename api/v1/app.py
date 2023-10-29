7a33969ac4044ae43b4496edfa50ec0493c27eb1
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2023-10-29 11:07:15 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,15 @@
-fig
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_storage(exception):
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
