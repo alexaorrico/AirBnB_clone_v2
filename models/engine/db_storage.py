2ea130347133763c11c6b8423de82dc602666b9d
yassine rahhou <125989457+yassinerahhou@users.noreply.github.com>
2023-10-29 17:14:25 +0100

    fix db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,10 +75,39 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
         # task badya hna 
-    def get(self, cls, id):
-        """
-        Returns one object based on the 
-        """
-        if cls not in classes.values():
-            return None
+def get(self, cls, id):
+    """
+    Returns one object based on the class name and its ID, or
+    None if not found
+    """
+    # Check if the provided class is valid
+    if cls not in self.classes.values():
+        return None
 
+    # Retrieve all objects of the specified class
+    all_cls = self.all(cls)
+
+    # Iterate through the objects to find a match by ID
+    for value in all_cls.values():
+        if value.id == id:
+            return value
+
+    # If no matching object is found, return None
+    return None
+
+
+
+def count(self, cls=None):
+        """
+        Counts and ret
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
