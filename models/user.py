87521dbf6ac5132890513c55d93355965a2e9b9e
chukwucyprian <chukwucyprian262@gmail.com>
2023-10-29 13:10:56 +0100

    models_correction

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
+
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
