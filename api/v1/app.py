337c1c089398401981bc0c125c2bd735fa6eff15
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-29 16:49:30 -0700

    createdd handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ A script that creates a Flask web server. """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,14 @@ app.register_blueprint(app_views)
 def close_db(error):
     """ Close storage on teardown. """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    error handler for 404 errors that returns a JSON-formatted 404 response.
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
