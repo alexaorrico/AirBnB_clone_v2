b8eff600752c97d0becece909a8727a0c7ebf37b
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-10-29 11:58:23 +0300

    Update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,34 @@
-from api.v1.views import app_views
-from flask import jsonify
+#!/usr/bin/python3
+'''
+Create a route `/status` on the object app_views.
+'''
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    return jsonify({"status": "OK"})
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'])
+def api_status():
+    '''
+    Returns a JSON response for RESTful API health.
+    '''
+    response = {'status': 'OK'}
+    return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    '''
+    Retrieves the number of each objects by type.
+    '''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
