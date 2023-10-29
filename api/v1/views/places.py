1c14a51e9719de2c7dfa06c0ff4128dac4fed6cd
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-29 11:47:06 -0700

    API

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -51,15 +51,15 @@ def create_place(city_id):
 
     data = request.get_json()
     if not data:
-        abort(400, description="Not a JSON")
-    if 'user_id' not in request.get_json():
-        abort(400, description="Missing user_id")
+        abort(400, "Not a JSON")
+    if 'user_id' not in data:
+        abort(400, "Missing user_id")
 
     user = storage.get(User, data['user_id'])
     if not user:
         abort(404)
-    if 'name' not in request.get_json():
-        abort(400, description="Missing name")
+    if 'name' not in data:
+        abort(400, "Missing name")
 
     data["city_id"] = city_id
     new = Place(**data)
@@ -76,7 +76,7 @@ def update_place(place_id):
 
     data = request.get_json()
     if not data:
-        abort(400, description="Not a JSON")
+        abort(400, "Not a JSON")
 
     ignore_key = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
 
