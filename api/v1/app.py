17575cabff98377af440be118bbe4a0c9b351771
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-29 16:39:59 +0000

    404: error not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,6 @@
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
-import jsonify
 import os
 
 app = Flask(__name__)
@@ -19,7 +18,7 @@ def teardown(exception):
 # Defines a custom 404 error handler
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Not found"}), 404
+    return {"error": "Not found"}, 404
 
 if __name__ == "__main__":
     env_host = 'HBNB_API_HOST'
