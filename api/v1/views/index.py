9f964513e5c23de53e9bb8b36cb9df42b3ae9d6f
Akwesi-bonah <arhinbonnah@gmail.com>
2023-10-29 15:48:53 +0000

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,14 +6,12 @@ index of API
 from api.v1.views import app_views
 from flask import jsonify, make_response
 from models import storage
-dic_data = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 
 @app_views.route('/status', methods=['GET'],  strict_slashes=False)
@@ -22,9 +20,14 @@ def status():
     return make_response(jsonify({"status": "OK"}))
 
 
-@app_views.route('/stats',methdos=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def app_status():
-    """ app status"""
-    for key, value in dic_data.items():
-        dic_data[key] = storage.count(value)
-    return jsonify(dic_data)
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
