fae7c94fab7d506e699895cb15c25439733bc245
Pritchad25 <pricts29@gmail.com>
2023-10-28 21:15:08 -0700

    importing a variable that is an instance of Blueprint called app_views from the current path.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""
+'''
 This Python script creates a route `/status` on the object app_views.
-"""
-
+'''
 
 from flask import jsonify
 from api.v1.views import app_views
@@ -11,8 +10,8 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def api_status():
-	"""
-	This method returns a JSON response for the RESTful API health.
-	"""
-	response = {'status': 'OK'}
-	return jsonify(response)
+    '''
+    This method returns a JSON response for the RESTful API health.
+    '''
+    response = {'status': 'OK'}
+    return jsonify(response)
