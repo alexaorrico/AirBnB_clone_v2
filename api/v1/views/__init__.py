afbc4871d07e70a48d1d369b443829a0725807de
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-29 16:35:50 -0700

    an endpoint that retrieves the number of each objects by type

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,4 +6,4 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # wildcard import of everything in the package api.v1.views.index
-from api.v1.views.index import *
+from api.v1.views.index import * 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,33 @@
 """ A module defines a rule that returns the current state of the app """
 from api.v1.views import app_views
 from flask import jsonify
+import models
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ This function returns the app's status as JSON. """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ A function that displays the quantity of each kind of object """
+    return jsonify({
+        "amenities": models.storage.count(Amenity),
+        "cities": models.storage.count(City),
+        "places": models.storage.count(Place),
+        "reviews": models.storage.count(Review),
+        "states": models.storage.count(State),
+        "users": models.storage.count(User)
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
