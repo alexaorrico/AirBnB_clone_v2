7c268290a5ce9538640c00f2506d6ebd1b609dff
yassine rahhou <125989457+yassinerahhou@users.noreply.github.com>
2023-10-29 18:31:35 +0100

    fix app py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,16 @@
-from flask import Flask 
+from flask import Flask
 from flask import Blueprint
 from models import storage
 from api.v1.views import app_views
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
-# app_views = Blueprint('app_views', __name__)
 
-@app.teardown_appcontext()
-def teardown_()
+@app.teardown_appcontext
+def teardown_(_):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000,threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
+
 
