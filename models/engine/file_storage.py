bd5936e5c1918d41c6e018a88796dcbe1bd3924d
Harry Muriithi <hharryjjack@gmail.com>
2023-10-29 17:40:13 +0300

    trial 2 Improve storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,16 +71,25 @@ class FileStorage:
 
     def get(self, cls, id):
         """Retrieve an object based on the class and its ID"""
-        if cls and id:
-            return self.__session.query(cls).get(id)
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if (value.id == id):
+                return Value
+
         return None
 
     def count(self, cls=None):
         """Count the number of objects in storage matching the given class."""
-        if cls:
-            return self.__session.query(cls).count()
-        else:
+        all_class = classes.values()
+
+        if not cls:
             count = 0
-            for model_cls in classes.values():
-                count += self.__session.query(model_cls).count()
-            return count
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
