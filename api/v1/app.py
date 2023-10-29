6439a28ebf208c5605216589b9b90539b920c5b0
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-29 15:31:46 +0000

    task 12 CORS..

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-CORS(app, origin='0.0.0.0')
+CORS(app, origins='0.0.0.0')
 
 
 @app.teardown_appcontext
