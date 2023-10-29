0f2a2d644d776f453b075cb86c3ba89eaf0d2698
Pritchad25 <pricts29@gmail.com>
2023-10-28 21:32:24 -0700

    An endpoint that retrieves the number of each objects by type.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,3 +15,19 @@ def api_status():
     '''
     response = {'status': 'OK'}
     return jsonify(response)
+
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
+    '''
+    This method retrieves the number of each objects by type.
+    '''
+    stats = {
+            'amenities': storage.count('Amenity'),
+            'cities': storage.count('City'),
+            'places': storage.count('Place'),
+            'reviews': storage.count('Review'),
+            'states': storage.count('State'),
+            'users': storage.count('User')
+    }
+    return jsonify(stats)
+
