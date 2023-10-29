a04b96ef77c31e6869a0d305c5a9837495f1b3fa
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-29 18:58:17 +0100

    fix commit insertions in init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,9 +12,5 @@ if app_views:
     from api.v1.views.index import *
     from api.v1.views.states import *
     from api.v1.views.cities import *
-<<<<<<< HEAD
-    from api.v1.views.ameities import *
     from api.v1.views.users import *
-=======
     from api.v1.views.amenities import *
->>>>>>> 97e69c3d4d10ad114ac929dad78ca8163a0e258c
