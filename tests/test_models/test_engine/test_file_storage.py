ba9b042705d62f1f1a4be05740db781068ad55de
babafemiolatona <femiolatona@gmail.com>
2023-10-29 23:02:50 +0100

    Added get and count methods

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,10 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test count method."""
+        storage = FileStorage()
+        count = len(storage.all())
+        self.assertEqual(count, storage.count())
