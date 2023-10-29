c0cf64c712f5cbf6447078055861e9699c413c73
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-10-29 13:09:37 +0300

    Update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""
-Creates a Blueprint instance with the url_prefix set /api/vi
-"""
+'''
+Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
+'''
+
 
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.vi.views.index import *
-from api.vi.views.states import *
-from api.vi.views.cities import *
-from api.vi.views.amenities import *
-from api.vi.views.users import *
-from api.vi.views.places import *
-from api.vi.views.places_reviews import *
-from api.vi.views.places_amenities import *
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
