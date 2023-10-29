b127260c88c9ced1b37b0b958cbaf00217d01d64
Mumbi69 <winfriendmumbi69@gmail.com>
2023-10-29 16:34:33 +0000

    404: error not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,6 +14,10 @@ def teardown(exception):
     """teardown"""
     storage.close()
 
+# Defines a custom 404 error handler
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = "0.0.0.0" if host is None
