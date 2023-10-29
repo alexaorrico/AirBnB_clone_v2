82084f3de0b874edff39c25053a0c44ea596253d
shazaaly <shaza.aly@gmail.com>
2023-10-29 11:34:52 +0200

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@
 from api.v1.views import app_views
 
 
-@app_views.route("/status", methods=['GET'])
+@app_views.route("/status", strict_slashes=False, methods=['GET'])
 def status():
     return {
         "status": "OK",
