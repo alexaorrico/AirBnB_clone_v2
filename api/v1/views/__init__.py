b2bd58ef02fe93590ae68254c0dd7f281bd4f95e
Harry Muriithi <hharryjjack@gmail.com>
2023-10-29 13:35:21 +0300

    Status of your API It’s time to start your API!

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+""" The blueprint for API """
 
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" index """
 
 from api.v1.views import app_views
 from flask import jsonify
@@ -6,4 +7,7 @@ from flask import jsonify
 
 @app_views.route("/status", methods=["GET"])
 def status():
+    """
+    returns status of API
+    """
     return jsonify({"status": "OK"})
