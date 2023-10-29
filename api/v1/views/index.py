e5fdf9221fda6c43629cb9d4ab0237a631cf78f0
AckimJnr <acklongwe@gmail.com>
2023-10-29 08:54:13 +0200

    Add get object stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status_json():
     """returns the status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def get_number_of_objects_by_type():
+    """Gets then number of objects per type and returns the stats"""
+    stats = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+            }
+    return jsonify(stats)
