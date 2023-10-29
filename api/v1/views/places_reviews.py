2c0dce6b3b832f20985857e1a361845e5a5d2e78
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-29 14:20:31 -0700

    changes

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request, abort
 from models import storage
 from models.place import Place
+from models.user import User
 from models.review import Review
 from api.v1.views import app_views
 
