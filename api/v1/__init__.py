9689a76d68fad52793efd65a9969a5ef3ea1fa51
Evans Sese <sese_evans@yahoo.com>
2023-10-29 10:52:04 +0300

    [feature] Task 3

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -2,9 +2,9 @@
 
 from flask import Blueprint
 
+
+app_views = Blueprint('app_views', __name__)
+
 # This wildcard import is used to include views from other files.
 # PEP8 may complain about it, but it's normal and can be ignored.
 from api.v1.views.index import *
-
-
-app_views = Blueprint('app_views', __name__)
--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 import os
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
+
 
 app = Flask(__name__)
 
