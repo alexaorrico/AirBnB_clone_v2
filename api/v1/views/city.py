67db6a81eac6378c12357987f1387f98cc1d8b52
biitopher <biitopher@github.com>
2023-10-29 11:41:57 -0700

    Task 7

---
+++ api/v1/views/city.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Creates cities"""
+
+from flask import request, jsonify, abort
+from api.v1.views import app_views
+from models.city import City
+from models import storage
+
+
+@app_views.route('/cities', methods=['GET'], strict_slashes=False)
+def list_cities():
+    """retrieves list of all City"""
+    cities = [city.to_dict() for city in storage.all(City).values()]
+    return jsonify(cities)
+
+
+@app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+def get_city(city_id):
+    """retrieves a City"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_city(city_id):
+    """deletes a City"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/cities', methods=['POST'], strict_slashes=False)
+def create_city():
+    """creates city"""
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if "name" not in data:
+        return jsonify({"error": "Missing name"}), 400
+    new_state = City(**data)
+    storage.new(new_city)
+    storage.save()
+    return jsonify(new_city.to_dict()), 201
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def update_city(city_id):
+    """updates City"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    for key, value in data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(city, key, value)
+
+    storage.save()
+    return jsonify(city.to_dict()), 200
