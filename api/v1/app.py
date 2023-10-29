099fb1cf3f2cb61613131a152cd26e7866a8914e
Evans Sese <sese_evans@yahoo.com>
2023-10-29 15:41:03 +0300

    [feature] Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 from flask import Blueprint
 
-app_views = Blueprint(url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, template_folder='views')
 from api.v1.views.index import *
