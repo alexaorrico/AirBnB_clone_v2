8968d4d310f772ae7d92996e59b966862b5bbf30
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-29 16:40:20 -0700

    some stats

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,20 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID,
+        or None if not found
+        """
+        if cls and id:
+            return self.__session.query(cls).filter_by(id=id).first()
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class
+        """
+        if cls:
+            return self.__session.query(cls).count()
+        return self.__session.query(cls).count()
