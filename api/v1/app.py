10c429126a96f312b87e7d65ee2edeacba51cafb
talent <124376251+CourageT1@users.noreply.github.com>
2023-10-29 22:17:36 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,63 +2,46 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
-import json
+from werkzeug.exceptions import NotFound
 from api.v1.views import app_views
 from os import getenv
+import json
+import os
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-host = getenv("HBNB_API_HOST")
-port = getenv("HBNB_API_PORT")
-cors = CORS(
-    app,
-    resources={r"/*": {"origins": "0.0.0.0"}}
-    )
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown(err):
-    """api teardown"""
+    """API teardown function to close the storage"""
     from models import storage
-
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(err):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
+    """Error handler for 404 Not Found"""
     res = {'error': "Not found"}
     response = make_response(json.dumps(res), 404)
     response.headers['Content-Type'] = 'application/json'
     return response
 
 
-if __name__ == "__main__":
-    """api entrypoint"""
-    host = "0.0.0.0" if host is None else host
-    port = "5000" if port is None else port
-    app.run(host=host, port=port, threaded=True)
-
-    from flask import Flask, jsonify
-from werkzeug.exceptions import NotFound
-
-app = Flask(__name)
-
 @app.errorhandler(NotFound)
 def handle_404_error(e):
+    """Error handler for 404 Not Found using Werkzeug"""
     response = jsonify({"error": "Not found"})
     response.status_code = 404
     return response
 
+
 if __name__ == "__main__":
+    """API entry point"""
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
