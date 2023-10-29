10371e3640b4b5392c719aead7780692c3fb3bd5
KhotsoSelading <k.selading007@gmail.com>
2023-10-29 16:13:06 +0200

    views

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-A view dedicated to State entities, overseeing all default RESTful API functions.
+A view dedicated to State entities, overseeing all default RESTful API
+functions.
 
 Author:
 Khotso Selading and Londeka Dlamini
