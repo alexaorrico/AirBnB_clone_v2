0aefe0aae4c53ccc234230aa6ba847ff3070fc83
Evans Sese <sese_evans@yahoo.com>
2023-10-29 23:41:00 +0300

    [Feature] Task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ Flask API """
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
