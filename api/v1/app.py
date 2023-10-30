57748b952e4a4b9933e4faafeb97f13cb1d30c7a
ZAKARIA <zakariaelaro@gmail.com>
2023-10-30 16:20:40 -0700

    Check task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,19 @@
-from flask import Flask
-import os
-from models import storage
+from flask import Flask, jsonify
+from werkzeug.exceptions import HTTPException
 from api.v1.views import app_views
 
-app = Flask(__name__)
+app = Flask(__name)
+app.url_map.strict_slashes = False
 
-@app.teardown_appcontext
-def teardown(exception):
-    storage.close()
+# Register your blueprint (app_views) here
+
+# Define a handler for 404 errors
+@app.errorhandler(HTTPException)
+def handle_404_error(e):
+    response = {
+        "error": "Not found"
+    }
+    return jsonify(response), 404
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host='0.0.0.0', port=5000)
