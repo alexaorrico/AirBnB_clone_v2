4c0e8d73e67cbdf0c1b7269a03267919b47ab7c1
jabulane <jabumokoena85@gmail.com>
2023-10-30 13:45:09 -0700

    error handler modification

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,9 +3,10 @@
 A Flask App that integrates with AirBnB static HTML Template
 """
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify,  make_response
 from models import storage
 import os
+from werkzeug.exceptions import HTTPException
 
 
 app = Flask(__name__)
@@ -23,16 +24,30 @@ def teardown_db(exception):
     """
     storage.close()
 
-@app.errorhandler(404)
-def not_found(error):
+@app.errorhandler(Exception)
+def handle_exception(err):
+    is_http_exception = isinstance(err, HTTPException)
+
+    err_description = err.description if is_http_exception else str(err)
+    code = err.code if is_http_exception else 500
+
+    message = {'error': 'Not found' if is_http_exception and
+            isinstance(err, NotFound) else err_description}
+
+    return make_response(jsonify(message), code)
+
+def global_error_handler():
     """
-    a method to handle page not found error
+    Global Route to handle All Error Status Codes
     """
-    return jsonify({'error': 'Not found'}), 404
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, handle_exception)
 
 if __name__ == "__main__":
     """
     MAIN Flask App starter
     """
+    # start global error handling
+    global_error_handler()
     # start Flask app
     app.run(host=host, port=port, threaded=True))
