890175409c474a88d4a553614f134e85a9455197
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-10-30 01:30:01 +0000

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,7 +3,6 @@
 Contains the class DBStorage
 """
 
-import models
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -12,7 +11,6 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from os import getenv
-import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
