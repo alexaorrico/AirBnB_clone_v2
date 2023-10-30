c4b587d6e5aef768a95fda44ebcecbaa29fe5d08
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-30 16:50:32 -0700

    changes

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for Places """
+"""Create a new view for Place objects."""
 from models.state import State
 from models.city import City
 from models.place import Place
@@ -15,9 +15,7 @@ from flasgger.utils import swag_from
                  strict_slashes=False)
 @swag_from('documentation/place/get_places.yml', methods=['GET'])
 def get_places(city_id):
-    """
-    Retrieves the list of all Place objects of a City
-    """
+    """Retrieves the list of all Place objects."""
     city = storage.get(City, city_id)
 
     if not city:
@@ -31,9 +29,7 @@ def get_places(city_id):
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/place/get_place.yml', methods=['GET'])
 def get_place(place_id):
-    """
-    Retrieves a Place object
-    """
+    """Retrieves a Place object"""
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
@@ -45,9 +41,7 @@ def get_place(place_id):
                  strict_slashes=False)
 @swag_from('documentation/place/delete_place.yml', methods=['DELETE'])
 def delete_place(place_id):
-    """
-    Deletes a Place Object
-    """
+    """Deletes a Place Object"""
 
     place = storage.get(Place, place_id)
 
@@ -64,9 +58,7 @@ def delete_place(place_id):
                  strict_slashes=False)
 @swag_from('documentation/place/post_place.yml', methods=['POST'])
 def post_place(city_id):
-    """
-    Creates a Place
-    """
+    """Creates a Place"""
     city = storage.get(City, city_id)
 
     if not city:
@@ -96,9 +88,7 @@ def post_place(city_id):
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 @swag_from('documentation/place/put_place.yml', methods=['PUT'])
 def put_place(place_id):
-    """
-    Updates a Place
-    """
+    """This is a Place Updates"""
     place = storage.get(Place, place_id)
 
     if not place:
@@ -120,10 +110,7 @@ def put_place(place_id):
 @app_views.route('/places_search', methods=['POST'], strict_slashes=False)
 @swag_from('documentation/place/post_search.yml', methods=['POST'])
 def places_search():
-    """
-    Retrieves all Place objects depending of the JSON in the body
-    of the request
-    """
+    """Retrieves all Place objects"""
 
     if request.get_json() is None:
         abort(400, description="Not a JSON")
