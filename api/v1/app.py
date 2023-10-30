e946f68500d5fec645e864361c571725202acebc
HossamMahmoudKhedr <nhem318@gmail.com>
2023-10-31 01:11:12 +0200

    modify app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 from os import getenv
 from models import storage
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -15,12 +16,9 @@ def teardown(self):
 
 
 @app.errorhandler(404)
-def pageNotFound():
-    error = jsonify({
-        "error": "Not found"
-    })
-    error.status_code = 404
-    return error
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
