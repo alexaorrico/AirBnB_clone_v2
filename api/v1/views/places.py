3328771c47f39fd5a638b5db852e7ec7a9085385
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-30 16:16:31 -0700

    Place - Amenity

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,7 +8,7 @@ from models.city import City
 from models.user import User
 from models.state import State
 from models.amenity import Amenity
-
+import os
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
