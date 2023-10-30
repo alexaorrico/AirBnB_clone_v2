0bd2d1b084682997d97e03843cdfb09d2fdf7012
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-30 16:54:57 -0700

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
