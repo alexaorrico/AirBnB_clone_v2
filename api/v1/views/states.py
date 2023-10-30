18416b7b49b0b05f059731f140fa3989b4ff9fa2
cartermusee <cartermusee@gmail.com>
2023-10-29 23:37:12 -0700

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """states module"""
 from api.v1.views import app_views
-from flask import jsonify, Flask, abort, request
+from flask import jsonify, Flask, abort, request, make_request
 from models import storage
 from models.state import State
 
