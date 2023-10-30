984d4af932975596e3e559b5ce5aa99c2c3bc4e6
2Kelvin <rocketmancodes02@gmail.com>
2023-10-30 14:01:54 +0300

    fix test_users err testcase

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -78,12 +78,14 @@ class TestUser(unittest.TestCase):
 
     def test_password_attr(self):
         """Test that User has attr password, and it's an empty string"""
+        import hashlib
         user = User()
         self.assertTrue(hasattr(user, "password"))
         if models.storage_t == 'db':
             self.assertEqual(user.password, None)
         else:
-            self.assertEqual(user.password, "")
+            newPswd = hashlib.md5().hexdigest()
+            self.assertEqual(user.password, newPswd)
 
     def test_first_name_attr(self):
         """Test that User has attr first_name, and it's an empty string"""
@@ -110,7 +112,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in u.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
