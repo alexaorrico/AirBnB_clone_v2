6de7e3858ac922bef8d60223436d37ae5edfed98
ZAKARIA <zakariaelaro@gmail.com>
2023-10-30 15:26:35 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,20 +1,16 @@
-from flask import jsonify
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
 
-@app_views.route('/status', methods=['GET'])
-def get_status():
-    response = {"status": "OK"}
-    return jsonify(response)
-
-@app_views.route('/stats', methods=['GET'])
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
-    counts = {
+    """Retrieves the number of objects by type"""
+    stats = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
-        "users": storage.count("User")
+        "users": storage.count("User"),
     }
-    return jsonify(counts)
+    return jsonify(stats)
