b49da05f929df8d41a7475091ca45efd5c040968
EvansSese <sese_evans@yahoo.com>
2023-10-30 14:55:06 +0300

    [Feature] Task 14

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ holds class User"""
 import models
+import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -27,3 +28,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        self.password = self.hash_password(self.password)
+
+
+    def hash_password(self, password):
+        hashed_password = hashlib.md5(password.encode()).hexdigest()
+        return hashed_password
