34653cfbe85299ceabe50f65413cb945c9c7f136
Temiloluwa22 <113702411+Temiloluwa22@users.noreply.github.com>
2023-10-30 11:51:07 +0100

    Code fix

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -110,13 +110,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in u.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
     def test_to_dict_values(self):
         """test that values in dict returned from to_dict are correct"""
-        t_format = "%Y-%m-%dT%H:%M:%S.%f"
+        t_format = "%Y-%m-%dT%H:%M:%S"
         u = User()
         new_d = u.to_dict()
         self.assertEqual(new_d["__class__"], "User")
