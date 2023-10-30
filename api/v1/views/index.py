2a69c2c909b792077e612c9e4bc181d2983be43a
ZAKARIA <zakariaelaro@gmail.com>
2023-10-30 16:00:20 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,15 +2,27 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    """Retrieves the number of objects by type"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+# Define the count method here
+def count(cls=None):
+    all_class = storage.all()
+    count = 0
+    if not cls:
+        for obj_class in all_class.values():
+            count += len(obj_class)
+    else:
+        count = len(all_class.get(cls, {}).values())
+
+    return count
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def stats():
+    stats_data = {
+        "amenities": count("Amenity"),
+        "cities": count("City"),
+        "places": count("Place"),
+        "reviews": count("Review"),
+        "states": count("State"),
+        "users": count("User")
     }
-    return jsonify(stats)
+
+    return jsonify(stats_data)
