53bba96ae43a7face037fb3acd2a59b2a8b49237
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-30 15:05:50 -0700

    changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """It creates a route /status on the object app_views"""
-from flask import jsonify
+
+from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
