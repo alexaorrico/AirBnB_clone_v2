de68b4160ec7d943ce089fdea822c56df3cb0e9d
Toby2507 <oluwatobisalau2000@gmail.com>
2023-10-30 09:38:51 +0100

    Update the docs for the retrieve_object function in the Places_review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -11,7 +11,7 @@ from models.user import User
 
 
 def retrive_object(cls, id):
-    """Retrives a resource based on given class and id."""
+    """Retrive a resource based on given class and id."""
     obj = storage.get(cls, escape(id))
     if obj is None:
         abort(404)
