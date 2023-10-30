61c73266cc4370600d5c5e39922e2c8ec6dae2e9
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-30 15:53:34 -0700

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,10 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -28,4 +26,4 @@ def ops_404(error):
 if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(environ.get('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
