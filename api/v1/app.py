440eeaaaefc4d0a63d314f3befb73f9a558cb7ab
abimbola-mmichael <abimbolamichael100@gmail.com>
2023-10-30 13:56:43 +0100

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,14 +7,38 @@ from flask import Flask, render_template, jsonify, url_for, make_response
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+from werkzeug.exceptions import HTTPException
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown_db(e):
+def teardown_db(exception):
     """Close storage"""
     storage.close()
+
+
+@app.errorhandler(Exception)
+def error_handler(error):
+    """Handles 404 error"""
+    if isinstance(error, HTTPException):
+        if type(error).__name__ == "NotFound":
+            error.description = "Not found"
+            msg = error.description
+            code = error.code
+        else:
+            msg = error
+            code = 500
+        return make_response(jsonify({"error": msg}), code)
+
+def setup_global_errors():
+    """
+    updates HTTPException class with error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, error_handler)
 
 if __name__ == "__main__":
     """Flask runner"""
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Flask route for index model"""
+
 from api.v1.views import app_views
 from flask import request, jsonify
 from models import storage
@@ -5,4 +8,19 @@ from models import storage
 @app_views.route("/status", method=["GET"])
 def status():
     """request status route"""
-    return jsonify({"status": "OK"})
+    if request.method == "GET":
+        return jsonify({"status": "OK"})
+
+
+app_views.route("/stats", method=["GET"])
+def stats():
+    """an endpoint that retrieves the number of each objects by type:"""
+    if request.method == "GET":
+        result = {}
+        request["amenities"] = storage.count("Amenity")
+        request["cities"] = storage.count("City")
+        request["places"] = storage.count("Place")
+        request["reviews"] = storage.count("Review")
+        request["states"] = storage.count("State")
+        request["users"] = storage.count("User")
+        return jsonify(result)
