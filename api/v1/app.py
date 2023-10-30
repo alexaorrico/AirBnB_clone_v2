96b13e70604e6830943688ee854af8eec6409aa2
CourageT1 <tcgwizhi@gmail.com>
2023-10-30 12:58:24 -0700

    corrections

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,11 +9,13 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
+
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})  # Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
 def close_storage(exception):
@@ -22,12 +24,14 @@ def close_storage(exception):
     """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """
     Custom error handler for 404 Not Found.
     """
     return {"error": "Not found"}, 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
