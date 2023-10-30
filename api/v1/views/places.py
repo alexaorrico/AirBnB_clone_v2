3d5c1d5419745306fbef88fdf41d83b9eb714fbd
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-30 13:55:46 -0700

    Place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -2,7 +2,10 @@
 """ Place objects that handles all default RESTFul API actions """
 from flask import Flask, request, jsonify, abort
 from api.v1.views import app_views
-from models import storage, Place, City, User
+from models import storage
+from models.place import Place
+from models.city import City
+from models.user import User
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
