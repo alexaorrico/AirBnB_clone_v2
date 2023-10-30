4167ae6acac2f8907066bd11a97791b22eae3865
cartermusee <cartermusee@gmail.com>
2023-10-30 01:25:18 -0700

    user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -41,8 +41,10 @@ def post_user():
     """return json"""
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'name' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing name'}), 400)
+    if 'email' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing email'}), 400)
+    if 'password' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing password'}), 400)
     user = request.get_json()
     us = User(**user)
     us.save()
