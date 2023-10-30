8123d1a96a59a5b258fa3dec3229583aed4d13bc
yasoug <ougouisyassine@gmail.com>
2023-10-30 12:55:30 -0700

    task 7 : commit 1

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,4 +7,6 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from models import storage
 from models.state import State
+from models.city import City
 from api.v1.views.states import *
+from api.v1.views.cities import *
