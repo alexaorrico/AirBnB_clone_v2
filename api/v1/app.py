6d4f0e091ca426e4ccbb6aff2d5daac38bd35b4e
yasoug <ougouisyassine@gmail.com>
2023-10-30 14:39:10 -0700

    task 12 : commit 1

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Application module"""
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 host = getenv("HBNB_API_HOST")
 port = getenv("HBNB_API_PORT")
