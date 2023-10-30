713472a22f11806c787bcc46c40e83bdfa87ce37
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-30 09:46:11 -0700

    debugg

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,6 +13,6 @@ app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 
 from api.v1.views.index import *
-from api.v1.views.city import *
-from api.v1.views.amenity import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
 from api.v1.views.places_reviews import *
