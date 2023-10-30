c6d0328524f9c0ac0774b774ac9b66cb0641b47c
ZAKARIA <zakariaelaro@gmail.com>
2023-10-30 16:09:12 -0700

    flask task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,30 @@
+#!/usr/bin/python3
+"""connect to API"""
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
-# Define the count method here
-def count(cls=None):
-    all_class = storage.all()
-    count = 0
-    if not cls:
-        for obj_class in all_class.values():
-            count += len(obj_class)
-    else:
-        count = len(all_class.get(cls, {}).values())
 
-    return count
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
+    return jsonify({"status": "OK"})
 
-@app_views.route('/api/v1/stats', methods=['GET'])
-def stats():
-    stats_data = {
-        "amenities": count("Amenity"),
-        "cities": count("City"),
-        "places": count("Place"),
-        "reviews": count("Review"),
-        "states": count("State"),
-        "users": count("User")
-    }
 
-    return jsonify(stats_data)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
--- api/v1/views/stats.py
+++ api/v1/views/stats.py
@@ -1,28 +1,91 @@
-from api.v1.views import app_views
-from flask import jsonify
+#!/usr/bin/python3
+""" States """
+from models.state import State
 from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from flasgger.utils import swag_from
 
-# Define the count method here
-def count(cls=None):
-    all_class = storage.all()
-    count = 0
-    if not cls:
-        for obj_class in all_class.values():
-            count += len(obj_class)
-    else:
-        count = len(all_class.get(cls, {}).values())
 
-    return count
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_state.yml', methods=['GET'])
+def get_states():
+    """
+    Retrieves the list of all State objects
+    """
+    all_states = storage.all(State).values()
+    list_states = []
+    for state in all_states:
+        list_states.append(state.to_dict())
+    return jsonify(list_states)
 
-@app_views.route('/api/v1/stats', methods=['GET'])
-def stats():
-    stats_data = {
-        "amenities": count("Amenity"),
-        "cities": count("City"),
-        "places": count("Place"),
-        "reviews": count("Review"),
-        "states": count("State"),
-        "users": count("User")
-    }
 
-    return jsonify(stats_data)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_id_state.yml', methods=['get'])
+def get_state(state_id):
+    """ Retrieves a specific State """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    return jsonify(state.to_dict())
+[12:03 AM]
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
+def delete_state(state_id):
+    """
+    Deletes a State Object
+    """
+
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    storage.delete(state)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/state/post_state.yml', methods=['POST'])
+def post_state():
+    """
+    Creates a State
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    data = request.get_json()
+    instance = State(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/state/put_state.yml', methods=['PUT'])
+def put_state(state_id):
+    """
+    Updates a State
+    """
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    ignore = ['id', 'created_at', 'updated_at']
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ignore:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
