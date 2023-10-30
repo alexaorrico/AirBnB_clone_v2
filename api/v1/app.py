4ae8053ca5aff49d7ffbb29e24534707ab5808af
jabulane <jabumokoena85@gmail.com>
2023-10-30 13:07:57 -0700

    adding a method to handle status errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,10 @@
 A Flask App that integrates with AirBnB static HTML Template
 """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 import os
-
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
@@ -23,10 +23,27 @@ def teardown_db(exception):
     """
     storage.close()
 
+def handle_exception(err):
+    is_http_exception = isinstance(err, HTTPException)
 
+    err_description = err.description if is_http_exception else str(err)
+    code = err.code if is_http_exception else 500
+
+    message = {'error': 'Not found' if is_http_exception and
+            isinstance(err, NotFound) else err_description}
+
+    return make_response(jsonify(message), code)
+
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """
+    Global Route to handle All Error Status Codes
+    """
+    return handle_exception(err)
+    
 if __name__ == "__main__":
     """
     MAIN Flask App starter
     """
     # start Flask app
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True))
