6de7928c6762ae697e977e7b8a8967b249b3fc6a
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-30 06:50:54 -0700

    API

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,11 +5,11 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_response, jsonify
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
