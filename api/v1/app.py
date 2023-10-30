bb5a0810598cffd9d40f3702e650a08a41960fc6
washucode <estherw8525@gmail.com>
2023-10-31 00:55:13 +0300

    fix some errors from app in api

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,17 +4,12 @@ Createw Flask app; and register the blueprint app_views to Flask instance app.
 
 from os import getenv
 from flask import Flask, jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
-# enable CORS and allow for origins:
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
-
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
+
 
 
 @app.teardown_appcontext
