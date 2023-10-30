c395428b733b7b4e0af419e1b89d85b2472c94bf
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-30 16:53:53 -0700

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, make_response, jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
