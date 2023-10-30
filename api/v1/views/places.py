b322ed70ec9220b9d05f971d0ff7b46eda6368e6
ikede-divine-keno <ikededivine@gmail.com>
2023-10-30 15:13:26 -0700

    imported Amenity

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,6 +4,7 @@ from models.state import State
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, request
