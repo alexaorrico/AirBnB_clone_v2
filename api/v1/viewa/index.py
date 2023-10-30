a52ad0b7d9eb1bd345d6a601b7f965f29bf4fbbd
abimbola-mmichael <abimbolamichael100@gmail.com>
2023-10-30 12:30:07 +0100

    Update

--- api/v1/viewa/index.py
+++
@@ -1,8 +0,0 @@
-from api.v1.views import app_views
-from flask import request, jsonify
-from models import storage
-
-@app_views.route("/status", method=["GET"])
-def status():
-    """request status route"""
-    return jsonify({"status": "OK"})
