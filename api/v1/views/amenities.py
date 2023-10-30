a4aa9df0f82b157727d07ab792705b6dd8fe37a7
cartermusee <cartermusee@gmail.com>
2023-10-30 00:57:40 -0700

    amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""amenity module"""
+"""states module"""
 from api.v1.views import app_views
 from flask import jsonify, Flask, abort, request, make_response
 from models import storage
@@ -7,7 +7,7 @@ from models.amenity import Amenity
 
 
 @app_views.route("/amenities", methods=['GET'], strict_slashes=False)
-def all_amenity():
+def all_states():
     """return json"""
     for amenity in storage.all(Amenity).values():
         all_amenity = [amenity.to_dict()]
@@ -26,10 +26,10 @@ def all_amenity_by_id(amenity_id):
 
 @app_views.route("/amenities/<string:amenity_id>", methods=['DELETE'],
                  strict_slashes=False)
-def del_amenity_by_id(amenity_id):
+def del_amenities_by_id(amenity_id):
     """return json"""
     amenity = storage.get(Amenity, amenity_id)
-    if Amenity is None:
+    if amenity is None:
         abort(404)
     amenity.delete()
     storage.save()
@@ -44,20 +44,22 @@ def post_amenity():
     if 'name' not in request.get_json():
         return make_response(jsonify({'error': 'Missing name'}), 400)
     amenity = request.get_json()
-    am = Amenity(**amenity)
-    am.save()
-    return make_response(jsonify(am.to_dict()), 201)
+    ame = Amenity(**amenity)
+    ame.save()
+    return make_response(jsonify(ame.to_dict()), 201)
 
 
 @app_views.route("/amenities/<string:amenity_id>", methods=['PUT'],
                  strict_slashes=False)
 def put_amenity_by_id(amenity_id):
     """return json"""
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
+
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+
     for key, value in request.get_json().items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(amenity, key, value)
