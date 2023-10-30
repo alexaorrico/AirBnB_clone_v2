0100ca473b49ffdbf6e74c4ff4c143ad6b1864f1
mamesareagan <mamesareagan0@gmail.com>
2023-10-30 10:52:55 +0000

    makes a cross-origin HTTP request

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,12 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
