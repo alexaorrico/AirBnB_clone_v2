80f7c1143579ec29f14238899e0853afc7720a91
ZAKARIA <zakariaelaro@gmail.com>
2023-10-30 12:52:09 -0700

    Trying to fix task

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,15 @@
 from flask import Flask
-from flask import Blueprint
+import os
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_(_):
+def teardown(exception):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
-
-
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,5 @@
-from flask import Flask
 from flask import Blueprint
-from api.v1.views import *
 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-
-
-
-pp = Flask(__name__)
-app_views = Blueprint('app_views', __name__)
+from api.v1.views.index import *
