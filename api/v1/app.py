4764a4316a8600418ed1e100d735df9d6873f8de
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-10-31 20:15:59 +0300

    added an error handler method for 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,10 @@ def close_db(obj):
     """This function calls the close() func"""
     storage.close()
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """ This function returns a 404 error """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
 
