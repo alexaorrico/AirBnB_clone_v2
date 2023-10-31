2656317293dd68860b5304ef45cce805a4f01aee
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-10-31 11:49:15 -0700

    changes

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for Place - Amenity """
+"""Create a new view for the link between Place object and Amenity object."""
 from models.place import Place
 from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
 
 
 @app_views.route('places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/place_amenity/get_places_amenities.yml',
-           methods=['GET'])
 def get_place_amenities(place_id):
-    """
-    Retrieves the list of all Amenity objects of a Place
-    """
+    """Retrieves the list of all Amenity"""
     place = storage.get(Place, place_id)
 
     if not place:
@@ -33,12 +28,8 @@ def get_place_amenities(place_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
-@swag_from('documentation/place_amenity/delete_place_amenities.yml',
-           methods=['DELETE'])
 def delete_place_amenity(place_id, amenity_id):
-    """
-    Deletes a Amenity object of a Place
-    """
+    """Deletes a Amenity object"""
     place = storage.get(Place, place_id)
 
     if not place:
@@ -64,12 +55,8 @@ def delete_place_amenity(place_id, amenity_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
                  strict_slashes=False)
-@swag_from('documentation/place_amenity/post_place_amenities.yml',
-           methods=['POST'])
-def post_place_amenity(place_id, amenity_id):
-    """
-    Link a Amenity object to a Place
-    """
+def link_place_amenity(place_id, amenity_id):
+    """Link a Amenity object to a place"""
     place = storage.get(Place, place_id)
 
     if not place:
