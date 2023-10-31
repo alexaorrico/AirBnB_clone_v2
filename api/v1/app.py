ed4601d34274577d2bc72dc2c977dee2b82a1da1
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-10-31 13:19:24 +0300

    Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
 '''
-Createw Flask app; and register the blueprint app_views to Flask instance app.
+Create a Flask app and register the blueprint app_views to Flask instance app.
 '''
 
+# Import necessary modules
 from os import getenv
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
 
+# Create a Flask application instance
 app = Flask(__name__)
 
-
+# Enable Cross-Origin Resource Sharing (CORS) for the app
 CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 
-
+# Register the app_views blueprint with the Flask app
 app.register_blueprint(app_views)
+
+# Disable strict slashes at the end of routes
 app.url_map.strict_slashes = False
 
-
+# Define a teardown function for cleaning up after each request
 @app.teardown_appcontext
 def teardown_engine(exception):
     '''
@@ -26,18 +30,19 @@ def teardown_engine(exception):
     '''
     storage.close()
 
-
-# Error handlers for expected app behavior:
+# Define an error handler for 404 Not Found errors
 @app.errorhandler(404)
 def not_found(error):
     '''
-    Return errmsg `Not Found`.
+    Return error message 'Not Found'.
     '''
     response = {'error': 'Not found'}
     return jsonify(response), 404
 
-
 if __name__ == '__main__':
+    # Get the host and port values from environment variables or use default values
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
+    
+    # Run the Flask app
     app.run(host=HOST, port=PORT, threaded=True)
