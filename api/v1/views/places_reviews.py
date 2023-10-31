be585ffd754915c8af704644a4908540e82f168d
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-10-31 05:05:38 +0300

    corrected reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -14,34 +14,35 @@ places_reviews = Blueprint('places_reviews', __name__)
 @places_reviews.route('/places/<place_id>/reviews', methods=['GET', 'POST'])
 def handle_place_reviews(place_id):
     # Check if the place_id is linked to a Place object
-    place = Place.get(place_id)
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
 
     if request.method == 'GET':
         # Retrieve the list of all Review objects of a Place
-        reviews = Review.get_reviews_by_place(place_id)
-        return jsonify([review.to_dict() for review in reviews])
+        reviews = storage.all(Review).values()
+        get_all = []
+        for rev in reviews:
+            if rev.place_id == place_id:
+                get_all.append(rev.to_dict())
+        return jsonify(get_all)
 
     elif request.method == 'POST':
         # Create a new Review
+        if not request.get_json():
+            return make_response(jsonify({"error": "Not a JSON"}), 400)
         data = request.get_json()
-        if not data:
-            abort(400, 'Not a JSON')
-        if 'user_id' not in data:
-            abort(400, 'Missing user_id')
-
-        user_id = data['user_id']
-        user = User.get(user_id)
+        if "user_id" not in data:
+            return make_response(jsonify({"error": "Missing user_id"}), 400)
+        user = storage.get(User, data["user_id"])
         if user is None:
             abort(404)
-
-        if 'text' not in data:
-            abort(400, 'Missing text')
-
-        review = Review(user_id=user_id, place_id=place_id, text=data['text'])
-        review.save()
-        return jsonify(review.to_dict()), 201
+        if "text" not in data:
+            return make_response(jsonify({"error": "Missing text"}), 400)
+        data["place_id"] = place_id
+        new = Review(**data)
+        new.saave()
+        return make_response(jsonify(new.to_dict()), 201)
 
 
 @places_reviews.route('/reviews/<review_id>', methods=['GET', 'DELETE', 'PUT'])
@@ -58,7 +59,8 @@ def handle_review(review_id):
     elif request.method == 'DELETE':
         # Delete a Review object
         review.delete()
-        return jsonify({}), 200
+        review.save()
+        return make_response(jsonify({}), 200)
 
     elif request.method == 'PUT':
         # Update a Review object
@@ -73,4 +75,4 @@ def handle_review(review_id):
             setattr(review, key, value)
 
     review.save()
-    return jsonify(review.to_dict()), 200
+    return make_response(jsonify(review.to_dict()), 200)
