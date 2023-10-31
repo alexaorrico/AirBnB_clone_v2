d938d1704aa62710ce0757617016efc23467b5de
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-10-31 15:21:14 +0300

    added a route called /stats to represent available objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 
 @app_views.route('status')
 def return_status():
     """ returns a json response """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def return_objects_by_type():
+    """
+    Retrieves the number of each objects by type
+    """
+
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
