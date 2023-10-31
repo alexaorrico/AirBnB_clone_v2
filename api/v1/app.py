2bdc00f7b91f946bc0b3da54dbb74d3090b37ccc
ZAKARIA <zakariaelaro@gmail.com>
2023-10-31 10:59:13 -0700

    trying to solve task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,49 @@
-from flask import Flask, jsonify
-from werkzeug.exceptions import HTTPException
 from api.v1.views import app_views
+from flask import Flask, jsonify, request
+from models.state import State
+from models import storage
 
-app = Flask(__name)
-app.url_map.strict_slashes = False
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    states = storage.all(State)
+    return jsonify([state.to_dict() for state in states.values()])
 
-# Register your blueprint (app_views) here
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
 
-# Define a handler for 404 errors
-@app.errorhandler(HTTPException)
-def handle_404_error(e):
-    response = {
-        "error": "Not found"
-    }
-    return jsonify(response), 404
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({})
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    if not request.json:
+        abort(400, "Not a JSON")
+    if "name" not in request.json:
+        abort(400, "Missing name")
+    state = State(**request.json)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    if not request.json:
+        abort(400, "Not a JSON")
+    for key, value in request.json.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict())
+
