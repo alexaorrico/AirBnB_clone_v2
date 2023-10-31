68f483a907cfc0e6fc75d1ea803e83b594ed9596
Shajoshed20 <ugobelle@gmail.com>
2023-10-31 04:58:26 +0100

    modified

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-This is the endpoint (route) status
-"""
+"""This is the endpoint (route) status"""
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
@@ -10,17 +8,13 @@ from flask import jsonify
 
 @app_views.route('/status', strict_slashes=False)
 def get_status():
-    """
-    Route to return a JSON status
-    """
+    """Route to return a JSON status"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('stats', strict_slashes=False)
 def count():
-    """
-    Retrieves objects by their type
-    """
+    """Retrieves objects by their type"""
     count = {}
     classes = ["User", "State", "City", "Amenity", "Place", "Review"]
 
