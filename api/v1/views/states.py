a4496a5c9fe6be19f1df3cb1bbd20034e63a6fae
Netrobe Ubuntu <netrobepy@gmail.com>
2023-10-31 05:38:00 +0100

    Added states crud

---
+++ api/v1/views/states.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+""" Index """
+from flask import abort, jsonify, request
+
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
+def get_states():
+    """Retrieves the list of all State"""
+    return jsonify([state.to_dict() for state in storage.all(State).values()])
+
+
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
+def delete_state(state_id):
+    """Deletes a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    state.delete()
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
+def create_state():
+    """Create a State object"""
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    state = State(**data)
+    state.save()
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route("/states/<state_id>", methods=["put"], strict_slashes=False)
+def update_state(state_id):
+    """Updates a State object"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    data = request.get_json()
+    if not data:
+        abort(400, "Not a JSON")
+    if "name" not in data:
+        abort(400, "Missing name")
+    for key, value in data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict()), 200
