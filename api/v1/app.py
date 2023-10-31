1b38aa258f2014576c3260f926dd06d55b9a4dcd
Shajoshed20 <ugobelle@gmail.com>
2023-10-31 01:47:28 +0100

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,12 +12,14 @@ from flasgger import Swagger
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def storage_close(obj):
-  """ Close Storage"""
+    """ Close Storage"""
     storage.close()
 
+
 if __name__ == "__main":
-   host = getenv('HBNB_API_HOST', default='0.0.0.0')
-   port = getenv('HBNB_API_PORT', default=5000)
-   app.run(host, int(port), threaded=True)
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host, int(port), threaded=True)
