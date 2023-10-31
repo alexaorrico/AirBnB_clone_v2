2ce479629a3af93df5db36a6dc8ec971fac54671
adonijah01 <adonijahsingoei@gmail.com>
2023-10-31 17:04:27 +0300

    state5

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -48,3 +48,48 @@ class DBStorage:
         """query on the current database session"""
         new_dict = {}
         for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reloads data from the database"""
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
+
+    def close(self):
+        """
+        call remove() method on the private session attribute
+        """
+        self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve one object by class and id."""
+        if cls and id:
+            obj_key = "{}.{}".format(cls.__name__, id)
+            return self.__session.query(cls).get(obj_key)
+    
+    def count(self, cls=None):
+        """Count objects in storage, optionally filtered by class."""
+        if cls:
+            return self.__session.query(cls).count()
+        return sum(1 for cls in self.__classes.values())
+
