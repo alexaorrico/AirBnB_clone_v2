3306ecfd370f9f1adb05d518775cc3a70a431da3
Brian-thee-shovi <brian14198@gmail.com>
2023-10-31 17:15:04 +0300

    api updates innit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,36 @@
 #!/usr/bin/python3
-"""
-Flask route that returns json status response
-"""
+"""App index"""
 from api.v1.views import app_views
-from flask import jsonify, request
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models import storage
+from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """
-    function for status route that returns the status
-    """
-    if request.method == 'GET':
-        resp = {"status": "OK"}
-        return jsonify(resp)
+@app_views.route('/status', strict_slashes=False)
+def index():
+    """this returns the api status"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    """
-    function to return the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """counts the number of objs by type"""
+    classes = {
+            "amenities": Amenity,
+            "cities": City,
+            "places": Place,
+            "reviews": Review,
+            "states": State,
+            "users": User
+            }
+    dic_t = {}
+    for key, value in classes.items():
+        number = storage.count(value)
+        dic_t[key] = number
+    return jsonify(dic_t)
