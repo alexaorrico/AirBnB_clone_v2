da75752f21b5b58be05e60972878e63c865e143f
Shajoshed20 <ugobelle@gmail.com>
2023-10-31 01:53:16 +0100

    amended

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,9 @@
 """Initialization files for Modules"""
 from flask import Blueprint
 
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 
 from api.v1.views.index import *
 from api.v1.views.states import *
