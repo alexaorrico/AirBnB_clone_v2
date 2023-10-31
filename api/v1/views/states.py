3b19f3e597b5f17b5bcd4b06bf8dca95de900458
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-10-31 22:23:02 +0300

    created state views to control manipulation of States

---
+++ api/v1/views/states.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+"""
+view for State objects that handles all
+default RESTFul API actions
+"""
+from flask import jsonify, abort, request, make_response
+from models.state import State
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_all():
+    """ get all by id """
+    all_list = [obj.to_dict() for obj in storage.all(State).values()]
+    return jsonify(all_list)
+
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
+def states(state_id):
+    """ returns all states """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_states(state_id):
+    """ delete state by id"""
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(400)
+    state.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """ creates a new state instance """
+    json_obj = request.get_json()
+    if not json_obj:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if 'name' not in json_obj:
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    obj = State(**json_obj)
+    obj.save()
+    return jsonify(obj.to_dict()), 201
+
+
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def post_method(state_id):
+    """ post method """
+    if not request.get_json():
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    obj = storage.get(State, state_id)
+    if obj is None:
+        abort(404)
+    for key, value in request.get_json().items():
+        if key not in ['id', 'created_at', 'updated']:
+            setattr(obj, key, value)
+    storage.save()
+    return jsonify(obj.to_dict())
