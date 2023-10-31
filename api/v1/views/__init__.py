82fd68e411e2960200b54abee046a0947dcdac0f
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-10-31 14:07:39 +0300

    imported index file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ This module creates a blueprint for app views """
 
+from flask import Flask
 from flask import Blueprint
+from api.v1.views.index import *
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
