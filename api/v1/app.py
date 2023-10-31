8aabcfc2b7e669b204b9d70aab1d814cf82d6da3
Sos <sosmogendi@gmail.com>
2023-10-31 13:55:04 -0700

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-"""Flask server (variable app)
-"""
-
-
-from flask import Flask, jsonify
+"""app"""
+from flask import Flask, make_response, jsonify
 from models import storage
-from os import getenv
 from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
+def close_storage(self):
+    '''Closes the storage engine'''
     storage.close()
 
-
-@app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify('error='Not found'), 404
-
-
-if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+if __name__ == '__main__':
+    api_host = getenv("HBNB_API_HOST", '0.0.0.0')
+    api_port = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=api_host, port=api_port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-'''API status'''
-from models import storage
-from models.state import State
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from flask import jsonify
+"""index"""
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def get_status():
-    '''Returns the API status.'''
-    return jsonify(status="OK")
+# Mapping of class names to custom identifiers
+class_name_map = {
+    "users": "User",
+    "places": "Place",
+    "states": "State",
+    "cities": "City",
+    "amenities": "Amenity",
+    "reviews": "Review"
+}
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    '''Returns the count of each type of object.'''
-    classes = {
-        "states": State,
-        "users": User,
-        "amenities": Amenity,
-        "cities": City,
-        "places": Place,
-        "reviews": Review
-    }
-    stats = {key: storage.count(value) for key, value in classes.items()}
-    return jsonify(stats)
+@app_views.route('/status', methods=['GET'])
+def status():
+    '''Route to return status "OK"'''
+    return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    '''Retrieves the number of objects by type'''
+    object_count = {}
+    for endpoint, class_name in class_name_map.items():
+        object_count[endpoint] = storage.count(class_name)
+    return jsonify(object_count)
