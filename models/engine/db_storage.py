cea656432a01a4813343fe4fe396bf4795cfe17a
Innocent <innocentbern22@gmail.com>
2023-10-31 19:03:35 +0000

    engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,25 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID
+        Or None if not found
+        """
+        if cls in classes.values() and id and type(id) == str:
+            cls_obj = self.all(cls)
+            for k, v in cls_obj.items():
+                if k.split(".")[1] == id:
+                    return v
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class. 
+        If no class is passed, returns the count of all objects in storage.
+        """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)            
