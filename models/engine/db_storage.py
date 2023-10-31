3f4374d1438345c4d541511b207c5f2c79813f5f
Ayowale <haydexwisdom@gmail.com>
2023-10-31 01:16:30 +0100

    Updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,8 +16,14 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {
+        "Amenity": Amenity,
+        "City": City,
+        "Place": Place,
+        "Review": Review,
+        "State": State,
+        "User": User
+        }
 
 
 class DBStorage:
