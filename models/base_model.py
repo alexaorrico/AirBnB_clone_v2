33a48e56191956c7486d3884746d91c3da27405e
Thami911 <122853011+Thami911@users.noreply.github.com>
2023-10-31 23:49:38 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Contains class BaseModel
-"""
-
+"""Contains class BaseModel"""
+import inspect
 from datetime import datetime
 import models
 from os import getenv
@@ -68,6 +66,14 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        frame = inspect.currentframe().f_back
+        func_name = frame.f_code.co_name
+        class_name = ''
+        if 'self' in frame.f_locals:
+            class_name = frame.f_locals["self"].__class__.__name__
+        is_fs_writing = func_name == 'save' and class_name == 'FileStorage'
+        if 'password' in new_dict and not is_fs_writing:
+            del new_dict['password']
         return new_dict
 
     def delete(self):
