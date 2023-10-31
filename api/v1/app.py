0a060efe1667b86e8b090c49572f0b3016b2f8de
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2023-10-31 20:17:38 +0300

    ran the file against autopep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,10 +15,12 @@ def close_db(obj):
     """This function calls the close() func"""
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     """ This function returns a 404 error """
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
 
