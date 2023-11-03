ec7b08d2f11d9450a02a6ace356622ee1f8c13c8
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-11-03 10:51:08 +0200

    Add tasdk 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,12 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    ''' Error 404 '''
+    return {"error": "Not found"}, 404
+
+
 if __name__ == "__main__":
     host = environ.get("HBNB_API_HOST", "0.0.0.0")
     port = int(environ.get("HBNB_API_PORT", 5000))
