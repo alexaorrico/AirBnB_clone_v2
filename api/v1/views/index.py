db100cc094b7a8f8ca41e14a2b39c638f25caf38
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-11-04 21:01:57 +0100

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,5 +6,5 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    " Status of API "
+    """ Status of API """
     return jsonify({"status": "OK"})
