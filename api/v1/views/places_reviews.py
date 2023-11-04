3e9c36f4e054c4e8863f17a26e3378ffe85cc4ff
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-11-04 07:18:49 +0200

    Add task 11

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -34,41 +34,41 @@ def get_review(review_id=None):
             abort(404)
 
 
-# @app_views.route('/places/<place_id>', methods=['DELETE'],
-#                  strict_slashes=False)
-# def delete_place(place_id=None):
-#     '''delete place'''
-#     place = storage.get(Place, place_id)
-#     if place:
-#         place.delete()
-#         storage.save()
-#         return jsonify({}), 200
-#     else:
-#         abort(404)
-#
-#
-# @app_views.route('/cities/<city_id>/places', methods=['POST'],
-#                  strict_slashes=False)
-# def post_place(city_id=None):
-#     '''post place'''
-#     if not request.get_json():
-#         return jsonify({'error': 'Not a JSON'}), 400
-#     if 'user_id' not in request.get_json():
-#         return jsonify({'error': 'Missing user_id'}), 400
-#     if 'name' not in request.get_json():
-#         return jsonify({'error': 'Missing name'}), 400
-#     user = storage.get(User, request.get_json()['user_id'])
-#     if not user:
-#         abort(404)
-#     city = storage.get(City, city_id)
-#     if city:
-#         place = Place(**request.get_json())
-#         place.city_id = city.id
-#         place.save()
-#         return jsonify(place.to_dict()), 201
-#     abort(404)
-#
-#
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_review(review_id=None):
+    '''delete review'''
+    review = storage.get(Review, review_id)
+    if review:
+        review.delete()
+        storage.save()
+        return jsonify({}), 200
+    else:
+        abort(404)
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
+def post_review(place_id=None):
+    '''post review'''
+    if not request.get_json():
+        return jsonify({'error': 'Not a JSON'}), 400
+    if 'user_id' not in request.get_json():
+        return jsonify({'error': 'Missing user_id'}), 400
+    if 'text' not in request.get_json():
+        return jsonify({'error': 'Missing text'}), 400
+    user = storage.get(User, request.get_json()['user_id'])
+    if not user:
+        abort(404)
+    place = storage.get(Place, place_id)
+    if place:
+        review = Review(**request.get_json())
+        review.place_id = place.id
+        review.save()
+        return jsonify(review.to_dict()), 201
+    abort(404)
+
+
 # @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 # def update_place(place_id=None):
 #     '''UPdate place'''
