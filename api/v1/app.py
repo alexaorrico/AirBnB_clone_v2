b126f4e379a33886ae8ffad85c86035bc80e9265
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-11-04 08:04:08 +0200

    Add task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,13 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import environ
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
