4aa0fe6b12124b1b057ea0cf2fbf9ddc33173f76
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-11-04 21:08:07 +0100

    updated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,6 @@
 """ Blueprint for API """
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, template_folder='/api/v1')
 
 from api.v1.views.index import *
