2d9ccfe313051e69eb7d005171aa979d9e3af2b7
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-11-04 23:37:43 +0200

    Add tassk 14

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        if models.storage_t != 'db' and 'password' in kwargs.keys():
+            password = kwargs['password']
+            h = hashlib.md5()
+            h.update(password.encode())
+            self.password = h.hexdigest()
+
