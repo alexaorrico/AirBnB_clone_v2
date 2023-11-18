82139c6c548d9cbabff463f62c748c28d3f2a76c
LeeWest89 <6683@holbertonstudents.com>
2023-11-18 08:46:42 -0800

    updated index adding stats method and importing classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,17 @@
 """ API Status Route """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status')
@@ -9,3 +20,12 @@ def status():
     """ Status Route Method """
     api_status = {"status": "OK"}
     return jsonify(api_status)
+
+@app_views.route('/stats')
+def stats():
+    """Stats route methods"""
+    info_dict = {}
+    for key, values in classes.items():
+        info_dict[key] = storage.count(values)
+    return jsonify(info_dict)
+
