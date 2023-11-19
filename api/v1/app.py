d6c4a74d75957d6d95395d345b272349af82c8a5
michaellgans <michaellgans@Victoria>
2023-11-19 09:51:32 -0600

    M - 11/19 8:10 PM - creates API

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Michael edited 11/19 8:04 PM
+"""API"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app_views = Blueprint("app_views", __name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown():
+    """closes application"""
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.environ.et("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", "5000"))
+    app.run(host=host, port=port, threaded=True)
