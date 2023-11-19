31570aee6afdd9cd7c453998693bf08d202f4a80
michaellgans <michaellgans@Victoria>
2023-11-19 10:24:59 -0600

    M - 11/19 8:41PM - importing os

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,10 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 if __name__ == "__main__":
-    host = os.environ.et("HBNB_API_HOST", "0.0.0.0")
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port = int(os.environ.get("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
 
@@ -17,6 +18,6 @@ app_views = Blueprint("app_views", __name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown():
+def teardown(error):
     """closes application"""
     storage.close()
