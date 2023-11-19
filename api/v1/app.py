6e9e1ee3d3d3056abc53595cfa125df017f8bbff
michaellgans <michaellgans@Victoria>
2023-11-19 10:19:26 -0600

    M - 11/19 8:23PM - working on documentation error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-#Michael edited 11/19 8:21 PM
+#Michael edited 11/19 8:36 PM
 """API"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
 
 if __name__ == "__main__":
     host = os.environ.et("HBNB_API_HOST", "0.0.0.0")
     port = int(os.environ.get("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
 
-from flask import Flask
-from models import storage
-from api.v1.views import app_views
+    app = Flask(__name__)
+    app_views = Blueprint("app_views", __name__)
 
-app = Flask(__name__)
-app_views = Blueprint("app_views", __name__)
+    app.register_blueprint(app_views)
 
-app.register_blueprint(app_views)
-
-@app.teardown_appcontext
-def teardown():
-    """closes application"""
-    storage.close()
+    @app.teardown_appcontext
+    def teardown():
+        """closes application"""
+        storage.close()
