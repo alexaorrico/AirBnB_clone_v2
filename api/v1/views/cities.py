3481359030fc1773f6bec49c4fb39275681280a0
Allison <allisonabinger@gmail.com>
2023-11-20 16:40:07 -0600

    A - 11/20 04:39 PM - states has documentation for updates, cities methods are updated

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,13 +11,14 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities(state_id):
     """retrieves the list of all city objects based off of state id"""
     city_list = []
     state = storage.get(State, state_id)
     if not state:
-        abort (404)
+        abort(404)
     for city in state.cities:
         city_list.append(city.to_dict())
 
@@ -34,44 +35,49 @@ def one_city(city_id):
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'],
                  strict_slashes=False)
-def del_city(state_id):
+def del_city(city_id):
     """deletes city object specified by city_id, returns a 404 error
         if city is not found, returns empty dictionary with status code 200"""
-    state = storage.get(State, state_id)
-    if not state:
+    city = storage.get(City, city_id)
+    if not city:
         abort(404)
-    storage.delete(state)
+    storage.delete(city)
     storage.save()
 
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def new_state():
-    """Creates a new state - transforms the HTTP body request to a dictionary
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
+def new_city(state_id):
+    """Creates a new city for state based off of state id provided
+    transforms the HTTP body request to a dictionary
     handles error raises, returns new state with status code 201"""
 
-    new_data = request.get_json()
-
-    if new_data is None:
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    if not request.get_json:
         abort(400, description="Not a JSON")
-    if 'name' not in new_data:
+    if 'name' not in request.get_json:
         abort(400, description="Missing name")
 
-    new_state = State(**new_data)
+    new_info = request.get_json()
+    city = City(**new_info)
     """** -> double asterisks unpacks a dictionary and passes
     the key-value pairs as arguments to the state constructor!"""
-    storage.new(new_state)
-    storage.save()
+    city.state_id = state.id
+    city.save()
+    return make_response(jsonify(city.to_dict()), 201)
 
-    return jsonify(new_state.to_dict()), 201
 
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+def update_da_state(city_id):
+    """updates a city based off of city id given in request, returns the
+    city object and status code 200"""
+    city = storage.get(City, city_id)
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_da_state(state_id):
-    state = storage.get(State, state_id)
-
-    if not state:
+    if not city:
         abort(404)
     if not request.get_json():
         abort(400, description="Not a JSON")
@@ -81,7 +87,7 @@ def update_da_state(state_id):
     new_data = request.get_json()
     for key, value in new_data.items():
         if key not in key_ignore:
-            setattr(state, key, value)
+            setattr(city, key, value)
 
     storage.save()
-    return make_response(jsonify(state.to_dict()), 200)
+    return make_response(jsonify(city.to_dict()), 200)
