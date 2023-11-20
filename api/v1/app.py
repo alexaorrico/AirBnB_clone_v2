ceb41988fb42b022089cc1e9e6bfc24472a8ba79
michaellgans <michaellgans@Victoria>
2023-11-19 18:17:43 -0600

    M - 11/20 9:40 AM - documentation test

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,6 @@ from models import storage
 from api.v1.views import app_views
 import os
 
-if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", "5000"))
-    app.run(host=host, port=port, threaded=True)
-
 app = Flask(__name__)
 app_views = Blueprint("app_views", __name__)
 
@@ -19,5 +14,10 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(error):
-    """closes application"""
+    """ Method that closes application """
     storage.close()
+
+if __name__ == "__main__":
+    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.environ.get("HBNB_API_PORT", "5000"))
+    app.run(host=host, port=port, threaded=True)
