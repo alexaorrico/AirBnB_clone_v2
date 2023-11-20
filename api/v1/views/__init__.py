9fbbb36d5675d9906814a9d4c6a7f4cb9d619ff2
michaellgans <michaellgans@Victoria>
2023-11-19 18:29:44 -0600

    M - 11/20 9:51 AM - Adds documentation to innit

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-#Michael edited 11/19 8:21 PM
 """ Blueprint """
+# Michael edited 11/19 8:21 PM
+
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
