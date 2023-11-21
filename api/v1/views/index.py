54248dbf21431ac56fd6c4737e9724139c844c0c
connortrue <connor.true.94@gmail.com>
2023-11-21 15:19:03 -0600

    first time using my branch

---
+++ api/v1/views/index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This returns all the classes."""
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    return jsonify({
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    })
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,6 +71,20 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
+    def get(self, cls, id):
+        """Retrieve one object"""
+        for obj in self.all(cls).values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """Count number of objects in storage"""
+        if cls:
+            return len(self.all(cls))
+        else:
+            return len(self.all())
+
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
