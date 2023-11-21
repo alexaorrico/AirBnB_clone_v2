7d872c7d2e221a9d6117578a56dc9cf527f34ee3
mokasp <kasperbouldin@gmail.com>
2023-11-21 15:30:18 -0600

    kasper | 11-21 | 3:30 pm | add get method for single object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,3 +16,11 @@ def get_all_states():
     for item in states_dict:
         state_list.append(states_dict[item].to_dict())
     return jsonify(state_list)
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_one_states(state_id):
+    """ tbc """
+    states_dict = storage.all(State)
+    for item in states_dict:
+        if states_dict[item].to_dict()['id'] == state_id:
+            return jsonify(states_dict[item].to_dict())
