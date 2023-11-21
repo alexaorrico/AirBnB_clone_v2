0a0c701f7c3eae3a33e59c267f562ab2f29ef27f
mokasp <kasperbouldin@gmail.com>
2023-11-21 15:07:38 -0600

    kasper | 11-21 | 3:07pm | add get method to states view for all states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+""" tbc """
+import json
+from flask import Flask, request, jsonify
+from models.state import State
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+@app_views.route('/states/', methods=['GET'])
+def get_all_states():
+    """ tbc """
+    state_list = []
+    states_dict = storage.all(State)
+    for item in states_dict:
+        state_list.append(states_dict[item].to_dict())
+    return jsonify(state_list)
