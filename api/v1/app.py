342b0e8b0994c7cee128976e1aa730f050edc2ac
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-11-21 11:41:47 -0800

    fixed app.py and index.py for PEP8

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """api app"""
 
+
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """closes storage"""
     storage.close()
+
 
 @app.errorhandler(404)
 def handle_not_found_error(e):
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """index creates route for status"""
 
+
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
@@ -12,10 +13,12 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status')
 def get_status():
     """returns status"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats')
 def get_count():
