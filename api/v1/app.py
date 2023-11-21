710a9475bf83910b5b9fc014365a71b4456dac12
mokasp <kasperbouldin@gmail.com>
2023-11-20 22:34:56 -0600

    kasper | 10-20 | 10:35pm | add code to run app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+""" tbc """
+from flask import Flask
+from models import storage 
+from api.v1.views import app_views
+from os import getenv
+from werkzeug.exceptions import HTTPException
+import json
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+@app.teardown_appcontext
+def teardown(e=None):
+    """ tbc """
+    storage.close()
+
+if __name__ == '__main__':
+    if getenv('HBNB_API_HOST') is not None:
+        the_host = getenv('HBNB_API_HOST')
+    else:
+        the_host = '0.0.0.0'
+    if getenv('HBNB_API_PORT') is not None:
+        the_port = getenv('HBNB_API_PORT')
+    else:
+        the_port = 5000
+    app.run(host=the_host, port=the_port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+from flask import jsonify
+
+@app_views.route('/status')
+def index():
+    """ tbc """
+    return jsonify({
+        "status": "OK"
+        })
