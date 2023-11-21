b85cf40547a96b8213b6c06b114f18587a9f4ac7
mokasp <kasperbouldin@gmail.com>
2023-11-20 21:16:42 -0600

    kasper | 11-20 | 9:16pm | remove changes, wrong branch

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,9 +14,6 @@ from models.user import User
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
-c_lasses = {Amenity: "Amenity", BaseModel: "BaseModel", City: "City",
-           Place: "Place", Review:  "Review", State: "State", User:  "User"}
-
 
 
 class FileStorage:
@@ -71,19 +68,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
-    def get(self, cls, id):
-        """ get method """
-        key = c_lasses[cls] + '.' + id
-        return self.__objects[key]
-
-    def count(self, cls=None):
-        """ count method """
-        if cls is None:
-            return len(self.__objects)
-        else:
-            count_list = []
-            for item in self.__objects.values():
-                if item.to_dict()['__class__'] == c_lasses[cls]:
-                    count_list.append(item)
-            return len(count_list)
