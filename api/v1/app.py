ee08cd5245c5aded9b87885f6b8a407571ee4189
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-21 19:08:09 -0800

    updated app.py to enable HTTP access control CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """api app"""
-
-
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 import os
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
