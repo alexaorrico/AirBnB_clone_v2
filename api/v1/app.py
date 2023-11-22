59a456c24d89d95748a057046e563dcb202505f0
root <root@MSI>
2023-11-21 19:13:29 -0600

    last

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,24 +7,22 @@ import flask
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 app.register_blueprint(app_views, url_prefix="/api/v1")
-
 
 @app.teardown_appcontext
 def teardown_app_context(exception):
     storage.close()
-
 
 @app.errorhandler(404)
 def page_not_found(e):
     '''Return this page when a 404 error appear'''
     data = {"error": "Not found"}
     return flask.jsonify(data), 404
-
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
