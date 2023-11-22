8c06b7a1cb7f6567acf3bee93bba6319ff3ebec3
colbertamir <colbertamir1516@gmail.com>
2023-11-21 17:40:35 -0800

    Docstrings!

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" tbc """
+"""
+This script sets up a Flask 
+application with API endpoints.
+"""
+
 from flask import Flask
 from models import storage 
 from api.v1.views import app_views
@@ -12,26 +16,46 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 
 @app.teardown_appcontext
 def teardown(e=None):
-    """ tbc """
-    storage.close()
+   """
+   Closes the storage after each request.
+
+   Args:
+       e (Exception, optional): An exception that 
+       was raised during the request.
+   """
+   storage.close()
 
 @app.errorhandler(HTTPException)
 def not_found(e):
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "error": "Not found"
-    })
-    response.content_type = "application/json"
-    return response
+   """
+   Handles HTTP exceptions by returning a 
+   JSON response with an error message.
+
+   Args:
+       e (HTTPException): The exception that was raised.
+
+   Returns:
+       Response: A Flask Response object with a 
+       JSON body containing the error message.
+   """
+   response = e.get_response()
+   # replace the body with JSON
+   response.data = json.dumps({
+       "error": "Not found"
+   })
+   response.content_type = "application/json"
+   return response
 
 if __name__ == '__main__':
-    if getenv('HBNB_API_HOST') is not None:
-        the_host = getenv('HBNB_API_HOST')
-    else:
-        the_host = '0.0.0.0'
-    if getenv('HBNB_API_PORT') is not None:
-        the_port = getenv('HBNB_API_PORT')
-    else:
-        the_port = 5000
-    app.run(host=the_host, port=the_port, threaded=True)
+   """
+   Runs the Flask application.
+   """
+   if getenv('HBNB_API_HOST') is not None:
+       the_host = getenv('HBNB_API_HOST')
+   else:
+       the_host = '0.0.0.0'
+   if getenv('HBNB_API_PORT') is not None:
+       the_port = getenv('HBNB_API_PORT')
+   else:
+       the_port = 5000
+   app.run(host=the_host, port=the_port, threaded=True)
