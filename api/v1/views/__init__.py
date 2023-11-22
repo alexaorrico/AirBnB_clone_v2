4961a49a19fc468d3c7caa774f52ff66a008c2fb
chris85gillis <chris85gillis@gmail.com>
2023-11-21 19:43:07 -0800

    tweaking files for task 10

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""index script"""
+"""Index script"""
 
 from models import storage
 from api.v1.views import app_views
@@ -8,13 +8,13 @@ from flask import jsonify
 
 @app_views.route("/status")
 def status():
-    """retrieve the status of the API"""
+    """Retrieve the status of the API"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats")
 def objects():
-    """retrieve statistics on various objects"""
+    """Retrieve statistics on various objects"""
     return jsonify({
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
