983b6c3f2602999139c755252731f6afcd9b8ef2
chris85gillis <chris85gillis@gmail.com>
2023-11-21 19:23:43 -0800

    updating files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
+"""index - end point"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def opbjects():
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
