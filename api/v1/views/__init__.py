5c5eacfbc11f66097558c2cf699059aeeb62ce7e
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2023-11-22 16:26:39 +0000

    working on init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""Index script"""
+"""init"""
+from flask import Blueprint
 
-from models import storage
-from flask import jsonify
 
-from api.v1.views import app_views
-@app_views.route("/status")
-def status():
-    """Retrieve the status of the API"""
-    return jsonify({"status": "OK"})
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views import app_views
-@app_views.route("/stats")
-def objects():
-    """Retrieve statistics on various objects"""
-    from api.v1.views import app_views
-    return jsonify({
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    })
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.users import *
+from api.v1.views.places import * 
+from api.v1.views.places_reviews import *
