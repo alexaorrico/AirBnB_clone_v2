a9d518e96523a88fa068f01618913b5f7d7c7f0c
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2023-11-22 19:26:55 +0000

    working on getting code fixed

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""index - end point"""
-=======
-"""This returns all the classes."""
->>>>>>> 2f785afc83268335f06aedd42646c8c0ff3ef96d
-from flask import jsonify
-from api.v1.views import app_views
+"""endpoint"""
+
 from models import storage
+from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route("/status")
 def status():
     return jsonify({"status": "OK"})
 
 
-<<<<<<< HEAD
 @app_views.route("/stats")
-def opbjects():
+def objects():
+    """retrice"""
     return jsonify({
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
@@ -24,15 +21,4 @@ def opbjects():
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
         "users": storage.count("User")
-=======
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    return jsonify({
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
->>>>>>> 2f785afc83268335f06aedd42646c8c0ff3ef96d
     })
