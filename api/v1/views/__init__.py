bde09dafc935d29c90647f08e1895c32ba5b8ec0
michaellgans <michaellgans@Victoria>
2023-11-22 09:30:37 -0600

    M - 11/22 9:30 AM - adds import statements

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Blueprint """
-# Allison edited 11/20 3:45 PM
+# Michael edited 11/22 9:29 AM
 
 from flask import Blueprint
 
@@ -9,3 +9,6 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
+from api.v1.views.amenity import *
+from api.v1.views.user import *
+from api.v1.views.place import *
