9dfce63801b0ccd0362be23d18bfd5ad4c63a3b5
mokasp <kasperbouldin@gmail.com>
2023-11-26 19:18:13 -0600

    calling get method/relationship

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,16 +11,15 @@ from werkzeug.exceptions import HTTPException
 app = Flask(__name__)
 
 
-@app_views.route('/states/<state_id>/cities/', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def get_all_cities(state_id):
     """ tbc """
     the_state = storage.get(State, state_id)
     if the_state is None:
         abort(404)
+    states_cities = the_state.cities
     cities_list = []
-    cities_dict = storage.all(City)
-    for item in cities_dict:
-        if cities_dict[item].to_dict()['state_id'] == state_id:
-            cities_list.append(cities_dict[item].to_dict())
-    return cities_list
+    for item in states_cities:
+        cities_list.append(item.to_dict())
+    return jsonify(cities_list)
 
