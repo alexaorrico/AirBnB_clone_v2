3cfab07308fb8b6324cd13fc5258255b9a4e6db2
Nimo08 <nimomohamed800@gmail.com>
2023-12-01 12:21:39 +0300

    Attempting task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,17 @@
 """ The index module"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Return the status of the API."""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_obj():
+    """Retrieves the number of each objects by type"""
+    number_obj = storage.count()
+    return jsonify(number_obj)
