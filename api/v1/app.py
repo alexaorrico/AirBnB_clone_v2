9663838e68bbf3f6814a3f4a28126e4a09df6efc
AmalNAdifi <amal.nadifi9@gmail.com>
2023-12-01 12:36:11 -0800

    app file created

---
+++ api/v1/app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+The following script creates a Flask app,
+and registers the blueprint 'api_views' to the Flask instance app
+"""
+
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+# Enabling CORS and allowing for origins:
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """
+    The following method removes the current SQLAlchemy
+    Session object after each request
+    """
+    storage.close()
+
+# Updated comment for error handler
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    The following method returns the error msg “Not Found”
+    for custom 404 error handling
+    """
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
+
+
+if __name__ == '__main__':
+    HBNB_API_HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    HBNB_API_PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
