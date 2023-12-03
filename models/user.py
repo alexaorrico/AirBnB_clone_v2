f81990f5991d729a953022e8c549443df5281a3d
nadiaMahfoud <nadiamahfoud48@gmail.com>
2023-12-03 08:11:19 -0800

    user

--- models/user.py
+++ models/user.py
@@ -6,12 +6,13 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
     """Representation of a user """
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    __tablename__ = 'users'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
@@ -26,4 +27,15 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        # If keyword arguments (kwargs) are provided,
+        # it checks if the 'password' key is present.
+        # If yes, it hashes the password using MD5 before storing it.
+        if kwargs:
+            pwd = kwargs.pop('password', None)
+            if pwd:
+                # Hash the password using MD5
+                secure = hashlib.md5()
+                secure.update(pwd.encode("utf-8"))
+                secure_password = secure.hexdigest()
+                kwargs['password'] = secure_password
         super().__init__(*args, **kwargs)
