f76d3954945be7d372b6546fe42ef6fcccb877b5
CaramelInvestor <caramelInvestor@gmail.com>
2023-12-03 22:18:33 +0100

    API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a flask application serving our API"""
+"""Flask application serving our API"""
 
-from flask import Flask
+from os import getenv
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from flask import Flask, Blueprint, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage"""
+def handle_close(err):
+    """Closes the storage connection"""
     storage.close()
 
 
-if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", default="0.0.0.0")
-    port = getenv("HBNB_API_PORT", default=5000)
+@app.errorhandler(404)
+def handle_page_not_found(err):
+    """Handles 404 errors by returning a JSON response"""
+    return jsonify({"error": "Not found"}), 404
 
-    app.run(host=host, port=port, threaded=True)
+
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=getenv("HBNB_API_PORT", "5000"), threaded=True)
