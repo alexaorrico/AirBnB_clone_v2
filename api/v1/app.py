21492e67dd23b41a6d186e32fb0ab95a8a469ce6
CaramelInvestor <caramelInvestor@gmail.com>
2023-12-03 21:34:30 +0100

    Status of an API

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage"""
@@ -16,7 +17,5 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", default="0.0.0.0")
-    port = getenv("HBNB_API_PORT", default=5000)
-
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=getenv("HBNB_API_PORT", "5000"), threaded=True)
