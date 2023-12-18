2092edd0deaba28fc70e9ec2c5d9e69c76df4e45
electrikbox <electrikbox@icloud.com>
2023-12-18 17:59:22 +0100

    add count and alt command syntax

--- console.py
+++ console.py
@@ -21,6 +21,9 @@ class HBNBCommand(cmd.Cmd):
     """ HBNH console """
     prompt = '(hbnb) '
 
+    # Quit commands and  Emptyline
+    # ============================================================ #
+
     def do_EOF(self, arg):
         """Exits console"""
         return True
@@ -46,13 +49,16 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     try:
                         value = int(value)
-                    except:
+                    except Exception:
                         try:
                             value = float(value)
-                        except:
+                        except Exception:
                             continue
                 new_dict[key] = value
         return new_dict
+
+    # Create
+    # ============================================================ #
 
     def do_create(self, arg):
         """Creates a new instance of a class"""
@@ -68,6 +74,9 @@ class HBNBCommand(cmd.Cmd):
             return False
         print(instance.id)
         instance.save()
+
+    # Show instance
+    # ============================================================ #
 
     def do_show(self, arg):
         """Prints an instance as a string based on the class and id"""
@@ -87,23 +96,40 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
-    def do_destroy(self, arg):
-        """Deletes an instance based on the class and id"""
-        args = shlex.split(arg)
-        if len(args) == 0:
+    # Destroy
+    # ============================================================ #
+
+    def do_destroy(self, line):
+        """ Deletes an instance based on class name + ID """
+        if not line:
             print("** class name missing **")
-        elif args[0] in classes:
-            if len(args) > 1:
-                key = args[0] + "." + args[1]
-                if key in models.storage.all():
-                    models.storage.all().pop(key)
-                    models.storage.save()
-                else:
-                    print("** no instance found **")
-            else:
-                print("** instance id missing **")
-        else:
+            return
+
+        args = shlex.split(line)
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        class_name = args[0]
+        instance_id = args[1]
+
+        if class_name not in models.classes:
             print("** class doesn't exist **")
+            return
+
+        all_objects = models.storage.all()
+        key = "{}.{}".format(class_name, instance_id)
+
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+
+        del all_objects[key]
+        models.storage.save()
+
+    # Show all instances
+    # ============================================================ #
 
     def do_all(self, arg):
         """Prints string representations of instances"""
@@ -122,43 +148,126 @@ class HBNBCommand(cmd.Cmd):
         print(", ".join(obj_list), end="")
         print("]")
 
-    def do_update(self, arg):
-        """Update an instance based on the class name, id, attribute & value"""
-        args = shlex.split(arg)
-        integers = ["number_rooms", "number_bathrooms", "max_guest",
-                    "price_by_night"]
-        floats = ["latitude", "longitude"]
-        if len(args) == 0:
+    # Update instance
+    # ============================================================ #
+
+    def do_update(self, line):
+        """Update an instance and save it"""
+        if not line:
             print("** class name missing **")
-        elif args[0] in classes:
-            if len(args) > 1:
-                k = args[0] + "." + args[1]
-                if k in models.storage.all():
-                    if len(args) > 2:
-                        if len(args) > 3:
-                            if args[0] == "Place":
-                                if args[2] in integers:
-                                    try:
-                                        args[3] = int(args[3])
-                                    except:
-                                        args[3] = 0
-                                elif args[2] in floats:
-                                    try:
-                                        args[3] = float(args[3])
-                                    except:
-                                        args[3] = 0.0
-                            setattr(models.storage.all()[k], args[2], args[3])
-                            models.storage.all()[k].save()
-                        else:
-                            print("** value missing **")
-                    else:
-                        print("** attribute name missing **")
-                else:
-                    print("** no instance found **")
-            else:
-                print("** instance id missing **")
-        else:
+            return
+
+        args = shlex.split(line)
+
+        class_name = args[0]
+
+        if class_name not in models.classes:
             print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        instance_id = args[1]
+        key = "{}.{}".format(class_name, instance_id)
+        all_objects = models.storage.all()
+
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+
+        # loop step per 2
+        for idx in range(2, len(args), 2):
+            attribut_name = args[idx].replace("{", "").replace(":", "")
+            attribut_value = args[idx + 1].replace("}", "")
+
+            if attribut_name in models.int_attrs:
+                setattr(all_objects[key], attribut_name, int(attribut_value))
+
+            elif attribut_name in models.float_attrs:
+                setattr(all_objects[key], attribut_name, float(attribut_value))
+
+            else:
+                setattr(all_objects[key], attribut_name, attribut_value)
+
+            all_objects[key].save()
+
+    # Count instances in json (all or per class)
+    # ============================================================ #
+
+    def do_count(self, line):
+        """Count instances"""
+        if not line:
+            print("** class name missing **")
+            return
+
+        args = shlex.split(line)
+        class_name = args[0]
+
+        if class_name not in models.classes:
+            print("** class doesn't exist **")
+            return
+
+        all_objects = models.storage.all()
+
+        count = 0
+
+        for key in all_objects.keys():
+            name = key.split('.')[0]
+            if name == class_name:
+                count += 1
+
+        print(count)
+
+    # Alternative command syntax
+    # ============================================================ #
+
+    def precmd(self, line):
+        """handle alt syntax"""
+        if not line:
+            return line
+
+        if "." not in line:
+            return line
+
+        class_and_cmd = line.split("(")[0]
+        class_name = class_and_cmd.split(".")[0]
+
+        if class_name not in models.classes:
+            return line
+
+        command = class_and_cmd.split(".")[1]
+        new_line = f"{command} {class_name}"
+
+        # get all attributs inside ()
+        args = line.split("(")[1].replace(")", "").split(",")
+
+        if len(args) > 0:
+            instance_id = args[0]
+            new_line += f" {instance_id}"
+
+        if len(args) > 1:
+            for idx in range(1, len(args)):
+                new_line += f" {args[idx]}"
+            print(new_line)
+
+        return new_line
+
+    # Default error message
+    # ============================================================ #
+
+    def default(self, line):
+        print(f"*** Unknown syntax: {line}")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
