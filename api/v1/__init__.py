0fe2bae1024f085afa55d135bd5a3585c8b9c830
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-19 18:14:45 -0400

    fixed pycode

--- api/v1/__init__.py
+++
@@ -1,7 +0,0 @@
-from api.v1.views.index import *
-from flask import Blueprint
-
-# Create a variable app_views which is an instance of Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Wildcard import of everything in the package api.v1.views.index
