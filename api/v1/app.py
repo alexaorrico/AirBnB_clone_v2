cb7dd22bac8f6f04d6daa7744a06cbcfdb6e7364
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-19 19:36:17 -0400

    verifying output

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,10 +21,6 @@ def teardown_db(exception):
 # Inside if __name__ == "__main__":, run your Flask server (variable app) with:
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = environ.get('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
