d12d065534d62e650de3adf8d701a974a80e139e
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-19 18:38:57 -0400

    Verifying requirements

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""Creates an intance in flask class named app"""
-
-from flask import Flask
-import os
+""" Flask Application """
 from models import storage
 from api.v1.views import app_views
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Teardown app context"""
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-""" Blueprint for our API """
-
-
-# api/v1/views/__init__.py
+""" Blueprint for API """
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Wildcard import for circular dependency (PEP8 will complain, but it's intentional)
 from api.v1.views.index import *
