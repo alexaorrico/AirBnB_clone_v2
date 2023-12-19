6d431de1f69753f5dd3188f4b59bb01ee144b092
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-19 18:35:09 -0400

    Verifying requirements

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,10 @@
 """ Blueprint for our API """
 
 
+# api/v1/views/__init__.py
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Wildcard import for circular dependency (PEP8 will complain, but it's intentional)
 from api.v1.views.index import *
