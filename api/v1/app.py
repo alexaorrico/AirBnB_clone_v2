0216b719c5f3eb7abdca03389e583baf40232c6a
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-19 18:51:20 -0400

    verifying output

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
 """ Flask Application """
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
+def teardown_appcontext(exception):
     storage.close()
 
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
