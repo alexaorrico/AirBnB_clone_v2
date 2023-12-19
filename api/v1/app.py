84937ca46ffbc6db67d97e95501af80d7b48f9fa
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-19 19:28:05 -0400

    verifying output

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-""" Flask Application """
 from flask import Flask
 from os import environ
 from models import storage
@@ -11,18 +10,16 @@ app = Flask(__name__)
 # Register the blueprint app_views to your Flask instance app
 app.register_blueprint(app_views)
 
-
 # Declare a method to handle @app.teardown_appcontext that calls storage.close()
 @app.teardown_appcontext
 def teardown_db(exception):
-    """ Close Storage """
+    """Close the storage on teardown."""
     storage.close()
-
 
 # Inside if __name__ == "__main__":, run your Flask server (variable app) with:
 if __name__ == "__main__":
     # host = environment variable HBNB_API_HOST or 0.0.0.0 if not defined
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
     # port = environment variable HBNB_API_PORT or 5000 if not defined
     port = environ.get('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
