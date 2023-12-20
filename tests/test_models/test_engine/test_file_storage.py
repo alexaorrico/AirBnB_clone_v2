50ab910b51c9f09a5aec7de2ef4c774afe172167
ThatFireBoi <gcf2007@hotmail.com>
2023-12-20 17:18:58 -0400

    Test cases for file storage and db storage in regards to get and count

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -16,7 +16,7 @@ from models.state import State
 from models.user import User
 import json
 import os
-import pep8
+import pycodestyle
 import unittest
 FileStorage = file_storage.FileStorage
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
@@ -32,14 +32,14 @@ class TestFileStorageDocs(unittest.TestCase):
 
     def test_pep8_conformance_file_storage(self):
         """Test that models/engine/file_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
+        pep8s = pycodestyle.StyleGuide(quiet=True)
         result = pep8s.check_files(['models/engine/file_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_file_storage(self):
         """Test tests/test_models/test_file_storage.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
+        pep8s = pycodestyle.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_models/test_engine/\
 test_file_storage.py'])
         self.assertEqual(result.total_errors, 0,
@@ -113,3 +113,29 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    def test_get(self):
+        """Test that get returns the correct object"""
+        storage = FileStorage()
+        new_state = State(name="California")
+        storage.new(new_state)
+        storage.save()
+        self.assertIs(storage.get(State, new_state.id), new_state)
+        self.assertIs(storage.get(State, "fake_id"), None)
+        self.assertIs(storage.get("State", new_state.id), new_state)
+        self.assertIs(storage.get("State", "fake_id"), None)
+        self.assertIs(storage.get(None, new_state.id), None)
+        self.assertIs(storage.get(None, "fake_id"), None)
+        self.assertIs(storage.get(State, None), None)
+        self.assertIs(storage.get(None, None), None)
+
+    def test_count(self):
+        """Test that count returns the correct number of objects"""
+        storage = FileStorage()
+        new_state = State(name="California")
+        storage.new(new_state)
+        storage.save()
+        self.assertEqual(storage.count(), 1)
+        self.assertEqual(storage.count(State), 1)
+        self.assertEqual(storage.count("State"), 1)
+        self.assertEqual(storage.count(None), 0)
