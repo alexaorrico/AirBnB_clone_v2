004d91fb7a199a7ca519adad4b630b9cfeb2d3b1
mickaeltartar <mickael.tartar@hotmail.com>
2023-12-20 11:28:14 +0100

    add stats for each object to count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,29 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns a json for the state of the API"""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ number of each object types """
+    object_count = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City), 
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State), 
+        "users": storage.count(User)
+    }
+    return jsonify(object_count)
