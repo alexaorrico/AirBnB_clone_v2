c1df8bffe16986ff5afdc8873a4851c10415b332
Mizuinu30 <juansilva.dvm@gmail.com>
2023-12-19 21:54:03 -0400

    fixed pycode

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -21,7 +21,8 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
