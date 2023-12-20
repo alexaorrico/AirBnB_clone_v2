877acc252511633db2753146cffbd727293e2a37
electrikbox <electrikbox@icloud.com>
2023-12-20 19:26:45 +0100

    add comments

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,6 +7,9 @@ from models.amenity import Amenity
 from flask import jsonify, abort, request
 
 
+# GET all amenities
+# ============================================================================
+
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """ get amenities object """
@@ -14,6 +17,9 @@ def get_amenities():
     list_amenities = [amenity.to_dict() for amenity in amenities]
     return jsonify(list_amenities)
 
+
+# GET 1 amenity
+# ============================================================================
 
 @app_views.route('/amenities/<amenity_id>',
                  methods=['GET'], strict_slashes=False)
@@ -26,6 +32,9 @@ def get_amenity(amenity_id):
 
     return jsonify(amenity.to_dict())
 
+
+# DELETE an amenity
+# ============================================================================
 
 @app_views.route('/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
@@ -42,8 +51,10 @@ def delete_amenity(amenity_id):
     return jsonify({}), 200
 
 
-@app_views.route('/amenities',
-                 methods=['POST'], strict_slashes=False)
+# CREATE an amenity
+# ============================================================================
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def post_amenity():
     """ create an amenity """
 
@@ -60,23 +71,25 @@ def post_amenity():
     return jsonify(amenity.to_dict()), 201
 
 
-@app_views.route("/amenities/<amenity_id>", methods=["PUT"],
-                 strict_slashes=False)
-def put_amenity(amenity_id):
-    """ Updates an amenity by its id """
-    amenity = storage.get(Amenity, amenity_id)
+# UPDATE an amenity
+# ============================================================================
 
-    if amenity is None:
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
+def update_amenity(amenity_id):
+    """update city by id"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
         abort(404)
 
     data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
 
-    if data is None:
-        abort(400, description="Not a JSON")
+    ignore_keys = ['id', 'state_id', 'create_at', 'updated_at']
 
     for key, value in data.items():
-        if key not in ["id", "created_at", "updated_at"]:
+        if key not in ignore_keys:
             setattr(amenity, key, value)
-
-    amenity.save()
+    storage.save()
     return jsonify(amenity.to_dict()), 200
