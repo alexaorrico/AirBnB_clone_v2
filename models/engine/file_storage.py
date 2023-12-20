4b282f547d5cc6c90353298437cff000c0b7281c
mickaeltartar <mickael.tartar@hotmail.com>
2023-12-20 10:43:18 +0100

    add file for get & count in db & file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,14 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        key = cls.__name__ + "." + id
+        if key in self.all(cls):
+            return self.all(cls)[key]
+        return None
+
+    def count(self, cls=None):
+        if cls is None:
+            return len(self.__objects())
+        return len(self.all(cls.__name__))
