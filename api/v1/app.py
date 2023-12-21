35c78f7a313cb848376f032a90b6a69a2af29b18
AaronEGH16 <aezegonz@gmail.com>
2023-12-21 02:14:43 +0000

    created a prototype of API app

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""this module contains a Flask APP"""
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def tear_down(self):
+    """APP teardown method"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Initialize APP"""
+    app.run(
+        host=getenv("HBNB_API_HOST", "0.0.0.0"),
+        port=int(getenv("HBNB_API_PORT", "5000")),
+        threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this module contains flask app routes
+    flask APP routes:
+        /status:    print jsonify "status"
+"""
+
+from api.v1.views import app_views 
+from flask import jsonify
+
+@app_views.route("/status")
+def status():
+    "returns a serialized json data"
+    return jsonify({"status": "OK"})
