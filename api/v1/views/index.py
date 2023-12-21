4079e9352ae567441911843023d1d9481641a576
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-12-21 15:41:57 -0400

    task 5 fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,24 @@
 """Index"""
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """status"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/status", strict_slashes=False)
+def stats():
+    """get stats"""
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    
+    return jsonify(data)
