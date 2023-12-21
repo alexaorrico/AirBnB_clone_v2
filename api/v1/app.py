62fcaeafcdcec19ca7e11cb001b35c9d280b8e82
LuciaPuppo897 <alerome897@gmail.com>
2023-12-21 08:22:34 -0800

    fixed connection problem

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" a Flask app"""
+""" a Flask app """
 from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views
 from os import getenv
-
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,17 +12,16 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_storage(self):
     """closes storage if it exist"""
-    if Storage is not None:
-        storage.close()
+    storage.close()
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    """error message for page not found"""
-    return jsonify({"error": "Not found"}), 404
+# @app.errorhandler(404)
+# def page_not_found(error):
+# """error message for page not found"""
+# return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=getenv('HBNB_API_PORT', '5000'),
-            threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host=host, port=port, threaded=True)
