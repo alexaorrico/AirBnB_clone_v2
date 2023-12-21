2bafdc1312093e9ea9cd8c62b6f52dc6750424d9
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-21 11:30:45 -0400

    Add comments to explain the logic of the code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,18 +11,18 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-
+# route to return JSON status
 @app_views.route("/status")
 def status():
     """Returns status of API"""
     return jsonify({"status": "OK"})
 
-
+# route to return JSON stats
 @app_views.route("/stats")
 def stats():
     """Retrieves number of each objects by type"""
 
     classes = {"amenities": Amenity, "cities": City, "places": Place,
                "reviews": Review, "states": State, "users": User}
-
+# return dictionary of each object type and number of objects
     return jsonify({key: storage.count(value) for key, value in classes.items()})
