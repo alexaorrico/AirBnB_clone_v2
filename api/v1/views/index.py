721f085b2b22d6f37385b8629e81488c4cd51048
LuciaPuppo897 <alerome897@gmail.com>
2023-12-21 08:58:19 -0800

    check new method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,15 @@ and stats"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status')
@@ -17,16 +26,8 @@ def status():
 def count():
     """use count method to count obj"""
     dict = {}
-    clases = {
-        "Amenety": amenities,
-        "City": cities,
-        "Place": places,
-        "Review": reviews,
-        "State": states,
-        "User": users
-    }
 
-    for cls, class_list in clases.items():
+    for cls, class_list in classes.items():
         counter = storage.count(cls)
         dict[cls] = counter
 
