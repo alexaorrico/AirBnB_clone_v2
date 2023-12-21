6f0ad0f3740e0281e1edfdd44deb6c6344b2f887
LuciaPuppo897 <alerome897@gmail.com>
2023-12-20 21:09:18 -0800

    add task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,30 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """returns the API status"""
-    return jsonify({'status': 'OK'})
+    return jsonify({"status": "OK"})
+
+
+@app_views.stats('/stats')
+def count():
+    """use count method to count obj"""
+    dict = {}
+    clases = {
+        "Amenety": amenities,
+        "City": cities,
+        "Place": places,
+        "Review": reviews,
+        "State": states,
+        "User": users
+    }
+
+    for cls, class_list in clases.items():
+        counter = storage.count(cls)
+        dict[cls] = counter
+
+    return jsonify(dict)
