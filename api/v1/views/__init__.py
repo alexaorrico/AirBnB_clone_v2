923189f1fb3fc51e42b1777a2f98bc7ff8914908
LuciaPuppo897 <alerome897@gmail.com>
2023-12-20 19:24:11 -0800

    fix typo

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ initializates blueprint"""
 
-from Flask import Blueprint
+from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
