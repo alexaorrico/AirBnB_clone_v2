d3220f0419637c665c3aee1c381b96be730ea62a
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-21 12:03:59 -0400

    adding comments

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
 """Index view for api v1"""
 
+# Import necessary modules and classes
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
@@ -11,18 +11,19 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-# route to return JSON status
+# Define route to return JSON status
 @app_views.route("/status")
 def status():
     """Returns status of API"""
+    # Return a JSON response with the status
     return jsonify({"status": "OK"})
 
-# route to return JSON stats
+# Define route to return JSON stats
 @app_views.route("/stats")
 def stats():
     """Retrieves number of each objects by type"""
-
+    # Define a dictionary with the classes to count
     classes = {"amenities": Amenity, "cities": City, "places": Place,
                "reviews": Review, "states": State, "users": User}
-# return dictionary of each object type and number of objects
+    # Return a JSON response with the count of each class
     return jsonify({key: storage.count(value) for key, value in classes.items()})
