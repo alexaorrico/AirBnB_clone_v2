c0f0225db3291fba1d8e244e579e6cb42ae6dc8d
LuciaPuppo897 <alerome897@gmail.com>
2023-12-20 21:33:23 -0800

    fixed documentation problem

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ a Flask app"""
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,13 +4,7 @@ and stats"""
 
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from models import storage 
 
 @app_views.route('/status')
 def status():
