edf58d6dc06b9274f0a6c0a07a29c1f8a23bbe65
JCRoooD <juancarlos-99@live.com>
2023-12-21 13:51:02 -0400

    checkers checkers

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,6 @@ def teardown(self):
 
 
 if __name__ == "__main__":
-    HBNB_API_HOST = os.getenv("HBNB_API_HOST", default='0.0.0.0')
-    HBNB_API_PORT = os.getenv("HBNB_API_PORT", default='5000')
+    HBNB_API_HOST = os.getenv('HBNB_API_HOST', default='0.0.0.0')
+    HBNB_API_PORT = os.getenv('HBNB_API_PORT', default='5000')
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
