06ea8dcee738b5e49bf71b840df0a01538642438
LuciaPuppo897 <alerome897@gmail.com>
2023-12-20 20:46:32 -0800

    fixed pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,14 +20,10 @@ def teardown_storage(self):
 @app.errorhandler(404)
 def page_not_found(error):
     """error message for page not found"""
-    return jsonify({"error":"Not found"}), 404
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    host = '0.0.0.0'
-    if getenv('HBNB_API_HOST'):
-        host = getenv('HBNB_API_HOST')
-    port = 5000
-    if getenv('HBNB_API_PORT'):
-        port = getenv('HBNB_API_PORT')
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'),
+            threaded=True)
