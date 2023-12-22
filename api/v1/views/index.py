a15d8084b527fc6463b81a0ba552af24cf26f4eb
JoseMilans <josemilans@gmail.com>
2023-12-21 23:49:23 -0300

    fix: Correct object count in /api/v1/stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -15,14 +21,12 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def stats():
     """Returns the count of various objects by type"""
-    classes = {
-        "Amenity": "amenities",
-        "City": "cities",
-        "Place": "places",
-        "Review": "reviews",
-        "State": "states",
-        "User": "users"
+    counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
-    counts = {cls_name: storage.count(cls_obj) for cls_obj,
-              cls_name in classes.items()}
     return jsonify(counts)
