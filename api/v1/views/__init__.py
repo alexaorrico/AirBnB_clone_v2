2899c050057e89a0d7307e94cc793fa9f3b1e768
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2023-12-22 14:23:19 -0400

    task 11

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,7 +4,7 @@ from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.amenities import *
 from api.v1.views.places import *
+from api.v1.views.amenities import *
+from api.v1.views.states import *
+from api.v1.views.index import *
