c0a436bef8881dc624adcfa2b4151f8112ff0731
JCRoooD <juancarlos-99@live.com>
2023-12-22 10:10:50 -0400

    bad imports bad

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""init file in api/v1/views"""
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
+from api.v1.views.states import *
