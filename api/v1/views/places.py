b84038224d9f7f2b5b805648aca77aa344f8fe9a
Fabiana Tellechea <85513647+fabianatellechea@users.noreply.github.com>
2023-12-23 20:37:24 +0000

    added: changed post method for task 11

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -47,7 +47,8 @@ def create_place(city_id):
         abort(404)
     if 'name' not in data:
         abort(400, 'Missing name')
-
+    data['city_id'] = storage.get(User, city_id)
+    data['user_id'] = storage.get(User, data['user_id'])
     new_place = Place(**data)
     storage.new(new_place)
     storage.save()
