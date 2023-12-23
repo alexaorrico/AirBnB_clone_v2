f00fe3646a9f384825678aff366504140444cc8e
LuciaPuppo897 <alerome897@gmail.com>
2023-12-23 13:05:14 -0800

    feat: task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from models import storage
 from os import getenv
 from api.v1.views import app_views
 from flask_cors import CORS
+
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
