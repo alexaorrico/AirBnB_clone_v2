5b7b97f87632b195e6411ccb609b23299f081446
AaronEGH16 <aezegonz@gmail.com>
2023-12-23 21:18:19 +0000

    added all users methods ('GET, DELETE, POST and PUT')

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,10 @@
-from flask import Blueprint
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 # import flask app views modules
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.users import *
+
+# creates a Blueprint to execute app
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
