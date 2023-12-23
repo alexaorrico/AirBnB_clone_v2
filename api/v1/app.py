3eba2d7817cfa46733b6d039938fbbe52daa077c
JCRoooD <juancarlos-99@live.com>
2023-12-22 21:32:42 -0400

    if this doesnt work gitrivert

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from models import storage
 from flask import Blueprint
-import os
+from os import environ
 
 
 # set up app_views blueprint and flask instance
@@ -25,6 +25,12 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+    """Main """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
+
