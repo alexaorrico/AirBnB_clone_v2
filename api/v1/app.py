cccfe191a0aa24b77f689fce6f65a0f525c926c8
LuciaPuppo897 <alerome897@gmail.com>
2023-12-23 13:04:28 -0800

    feat: task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,8 +4,9 @@ from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
-
+from flask_cors import CORS
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
