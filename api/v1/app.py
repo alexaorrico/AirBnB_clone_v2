7749b83bf957ed578ed64f3bfb61c99b1b871059
Mariana Echeto <echetomariana@gmail.com>
2023-12-23 23:25:46 -0300

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,6 +21,6 @@ if __name__ == '__main__':
     """
         main function
     """
-    app.run(host=os.getenv('HBNB_API_HOTS', '0.0.0.0'),
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=os.getenv('HBNH_API_PORT', '5000'),
             threaded=True, debug=False)
