a1ccee588e7cb65cbfe053d25f7a0f9baccab0e6
Fabiana Tellechea <85513647+fabianatellechea@users.noreply.github.com>
2023-12-24 01:14:29 +0000

    pycode fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
