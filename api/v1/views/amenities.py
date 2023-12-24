e36704073e54d770615f351a9960fca3e33488bc
FrancoMuVa <muvafranco@gmail.com>
2023-12-24 21:06:27 +0000

    feat: added new functions

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,13 +7,16 @@ from models import storage
 from models.amenity import Amenity
 from api.v1.views import app_views
 
+
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """Retrieves the list of all Amenity objects"""
     amenities = storage.all(Amenity).values()
     return jsonify([amenity.to_dict() for amenity in amenities])
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_amenity(amenity_id):
     """Retrieves a Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -21,7 +24,9 @@ def get_amenity(amenity_id):
         abort(404)
     return jsonify(amenity.to_dict())
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_amenity(amenity_id):
     """Deletes a Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -30,6 +35,7 @@ def delete_amenity(amenity_id):
     amenity.delete()
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
@@ -43,7 +49,9 @@ def create_amenity():
     amenity.save()
     return jsonify(amenity.to_dict()), 201
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """Updates a Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,9 @@ from models.state import State
 from models.city import City
 from api.v1.views import app_views
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
 def get_cities(state_id):
     """Retrieves the list of all City objects of a State"""
     state = storage.get(State, state_id)
@@ -16,6 +18,7 @@ def get_cities(state_id):
         abort(404)
     cities = [city.to_dict() for city in state.cities]
     return jsonify(cities)
+
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
@@ -25,7 +28,9 @@ def get_city(city_id):
         abort(404)
     return jsonify(city.to_dict())
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """Deletes a City object"""
     city = storage.get(City, city_id)
@@ -35,7 +40,36 @@ def delete_city(city_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     """Creates a City"""
-    state = storage.g
+    state = storage.get(State, state_id)
+    if state in None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(404, description="Not a JSON")
+    if "name" not in data:
+        abort(400, description="Missing name")
+    city = City(**data)
+    city.save()
+    return jsonify(city.to_dict()), 201
+
+
+@app_views.route("/cities/<city_id>",
+                 mathods=["PUT"], strict_slashes=False)
+def update_city(city_id):
+    """ Update a City object """
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, description="Not a JSON")
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(city, key, value)
+    city.save()
+    return jsonify(city.to_dict()), 200
