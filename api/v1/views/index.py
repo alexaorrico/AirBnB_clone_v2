4e055eee213e9eb33ae513c05b90d8ae4ab503e9
rhf007 <rahofahazem512@gmail.com>
2024-01-04 13:52:22 -0800

    nice

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,8 @@
 
 
 from api.v1.views import app_views
-
+from flask import jsonify
+from models.storage import count
 
 @app_views.route('/status')
 def status():
@@ -11,3 +12,19 @@ def status():
     stat = {"status": "OK"}
     json_response_stat = jsonify(stat)
     return json_response_stat
+
+
+@app_views.route('/api/v1/stats')
+def stats():
+    """retrieves the number of each objects by type"""
+    obj_count = {
+        "amenities": count("Amenity"),
+        "cities": count("City"),
+        "places": count("Place"),
+        "reviews": count("Review"),
+        "states": count("State"),
+        "users": count("User")
+    }
+
+    json_obj_count = jsonify(obj_count)
+    return (json_obj_count)
