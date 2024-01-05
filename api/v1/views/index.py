980551e4802a01b18878a62ad5992133f048072c
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-06 00:44:11 +0100

    fix: add strict_slashes=False

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def get_status():
     """get api status"""
     return jsonify({"status": "OK"})
