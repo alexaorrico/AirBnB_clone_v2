bd052ea0faa8da797ac46f3cf90b9bafc726c388
raniachikhi <raniach0653@gmail.com>
2024-01-05 14:54:54 -0800

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,30 +7,21 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Remove the current SQLAlchemy session"""
     storage.close()
-
 
 @app.errorhandler(404)
 def not_found(error):
-    """Returns a error 404"""
+    """Returns a 404 JSON-formatted response"""
     return jsonify({'error': 'Not found'}), 404
 
-
 if __name__ == "__main__":
-    if getenv('HBNB_API_HOST'):
-        host = getenv('HBNB_API_HOST')
-    else:
-        host = '0.0.0.0'
-    if getenv('HBNB_API_PORT'):
-        port = getenv('HBNB_API_PORT')
-    else:
-        port = 5000
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
