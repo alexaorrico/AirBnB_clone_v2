2b1b84f3a0ce70839eaed42ba0ae8cc39ca2946e
memoe <memoemukoe@gmail.com>
2024-01-06 01:35:35 +0200

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module for index blueprints
+"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Returns status Ok"""
+    return jsonify({"status": "OK"})
