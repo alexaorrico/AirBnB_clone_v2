486d5144be5a55d1b33ab60ee0fe07210bb7552b
raniachikhi <raniach0653@gmail.com>
2024-01-05 13:32:48 -0800

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,46 @@
 #!/usr/bin/python3
-'''api status'''
-import models
-from models import storage
-from models.base_model import BaseModel
+"""
+index
+"""
+
 from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route('/status', strict_slashes=False)
-def returnstuff():
-    '''return stuff'''
-    return jsonify(status='OK')
+from models import storage
 
-@app_views.route('/stats', strict_slashes=False)
-def stuff():
-    '''JSON Responses'''
-    todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
