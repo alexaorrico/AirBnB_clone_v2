9d44c9536ef57c867d2dea1448839b865beea68b
wisdomwest <harmonykoome@gmail.com>
2024-01-05 14:48:58 +0300

    lol simple mistake had done State

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,11 +9,11 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<int:state_id>/cities', methods=['GET'],
+@app_views.route('/states/<string:state_id>/cities', methods=['GET'],
                  strict_slashes=False)
 def all_cities(state_id):
     """get all cities in a state"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     data = []
