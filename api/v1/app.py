ed3931453cc2d1720fd2fe47ffe36b9ce9f4a3b1
Emil dawood <edawood997@gmail.com>
2024-01-06 01:38:51 +0200

    d

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,16 +5,23 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(e):
+def teardown(self):
     """teardown method"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    host = getenv("HBNB_API_HOST")
+    port = getenv("HBNB_API_PORT")
+    if not host:
+        host = "0.0.0.0"
+    if not port:
+        port = "5000"
+    app.run(host=host, port=port, threaded=True)
