64e3f3bfd4c9339dacc1e1412316b870744be6cc
raniachikhi <raniach0653@gmail.com>
2024-01-05 14:51:42 -0800

    add

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,7 @@ views
 
 from flask import Blueprint
 
-app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.states import *
