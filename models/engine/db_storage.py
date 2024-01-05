95c578c3e43f44f307bb3589b292ece1bf98599e
cdJohnEl <easykelchimdikejohn@gmail.com>
2024-01-05 08:32:14 +0100

    updated DBStorage, FileStorage and wrote tests

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieves an object"""
+        if cls in classes.value() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+    
+    def count(self, cls=None):
+        """counts the number of objects in storage"""
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
