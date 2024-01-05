0ac5ec07f9dbfaaa56c8788a07f460c331efbb5c
Mohammed Mubarak <modymu9@gmail.com>
2024-01-05 16:40:21 +0200

    added task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,5 +23,8 @@ def tear_down(exception=None):
 if __name__ == '__main__':
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
+    debug = bool(os.environ.get('DEBUG', 0))
+    # to enable debugging locally
+    # export DEBUG=1
 
-    app.run(host=host, port=port, debug=True, threaded=True)
+    app.run(host=host, port=port, debug=debug, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,9 +5,20 @@ set up route for status endpoint
 
 
 from api.v1.views import app_views
+from flask import jsonify
+from models.engine.db_storage import classes
+import models
 
 
 @app_views.route('/status')
 def get_status():
     """ send status api"""
-    return {"status": "OK"}
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def get_count():
+    classes_counts = dict(
+        (cls.__name__, models.storage.count(cls)) for cls in classes.values()
+    )
+    return jsonify(classes_counts)
