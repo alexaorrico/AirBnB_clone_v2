739d5f9df0540047b7cfcaaedae8231dd8347ec5
DAVENJAGI <njagidave39@gmail.com>
2024-01-05 16:52:14 +0300

    Restful_API_more_tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -18,6 +18,7 @@ import json
 import os
 import pep8
 import unittest
+from models import storage_t
 from unittest.mock import patch
 DBStorage = db_storage.DBStorage
 classes = {"Amenity": Amenity, "City": City, "Place": Place,
@@ -30,6 +31,7 @@ class TestDBStorageDocs(unittest.TestCase):
     def setUpClass(cls):
         """Set up for the doc tests"""
         cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
+        cls.storage = DBStorage()
 
     def test_pep8_conformance_db_storage(self):
         """Test that models/engine/db_storage.py conforms to PEP8."""
@@ -67,11 +69,9 @@ test_db_storage.py'])
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method ineeds a docstring".format(func[0]))
-
+g
     def test_count_empty(self):
         """tests foe method count when empty"""
-        storage_t = 'db'
-
         self.assertEqual(self.storage.count(), 0)
 
     def test_count_after_creating(self):
@@ -85,7 +85,7 @@ test_db_storage.py'])
     def test_count_nonexistent_class(self):
         """test for count when we parse a nonexistent class"""
         with self.assertRaises(ValueError):
-            self.storage.count(NonExistentClass)
+            cls.storage.count(NonExistentClass)
 
     def test_count_after_deleting(self):
         """test for count number after deleting"""
