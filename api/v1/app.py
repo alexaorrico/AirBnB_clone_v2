047ff7d11b642852d3f44657881d8624cb631af5
John Gaitho <johngaitho05@gmail.com>
2024-01-05 17:56:08 +0300

    404

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,7 +21,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def not_found(error):
     """404 handler"""
-    return jsonify({'error': 'Not Found'}), 404
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == '__main__':
