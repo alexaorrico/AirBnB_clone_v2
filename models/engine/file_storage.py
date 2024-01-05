93ad367dad30f8817535bf530b83f4af695120f1
John Gaitho <johngaitho05@gmail.com>
2024-01-05 13:59:09 +0300

    add file storage tests

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,6 +50,7 @@ class FileStorage:
 
     def reload(self):
         """deserializes the JSON file to __objects"""
+        self.__objects = {}
         try:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
