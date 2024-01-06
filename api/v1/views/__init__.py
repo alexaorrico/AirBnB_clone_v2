86f31a5e9e05cf7562b29408f6698869c727861b
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 16:59:05 +0200

    ahmed eid v0.035

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 from flask import Blueprint
-from AirBnB_clone_v3.api.v1.views.index import *
-
+from api.v1.views.index import *
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
