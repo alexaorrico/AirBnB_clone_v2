3fc2a6fac0b03742e74de75d4a2e3c97a66a2eab
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-01-06 14:05:10 +0200

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 
 """
 creating blueprints for app.views
 """
-=======
-"""Contains Blueprint object"""
->>>>>>> 6338784aeb158049a5574267bcaba8774a776573
 
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
-<<<<<<< HEAD
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
-=======
->>>>>>> 6338784aeb158049a5574267bcaba8774a776573
+
