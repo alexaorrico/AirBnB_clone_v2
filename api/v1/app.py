3d6a77d227167675289a1fc1a13ac19c087c0e66
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-06 19:06:49 +0000

    app.py updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 """ Flask application for aibrnb clone"""
+
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response, render_template, url_for
 from models import storage
 import os
-
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -26,5 +25,5 @@ def page_not_found(exception):
 if __name__ == "__main__":
     """Main function for flask app"""
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', '5000')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
     app.run(host=host, port=port, threaded=True, debug=True)
