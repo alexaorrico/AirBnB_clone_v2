7aaf3e41b6d022a44b67c926b6f6a35e6c3c808c
Jukunye <jukunyes@gmail.com>
2024-01-06 21:28:53 +0300

    “404 page”, a “Not found”

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask import make_response
 from os import getenv
 
 
@@ -15,6 +16,12 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def close_con(error):
     """closes the database connection"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """creates a 'Not found'"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
