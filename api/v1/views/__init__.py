1af2233c1f3f04b7465517a53c04a7c2fe789763
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-06 18:20:40 +0000

    6. State

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,6 @@
 """Contains Blueprint object for initial start of Flask app"""
 
 from flask import Blueprint
-
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 from api.v1.views.index import *
+from api.v1.views.states import *
