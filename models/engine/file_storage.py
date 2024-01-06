bd1735a297e22f2b80051c330cab75b69d7600f0
Chareeef <youssef.charif.h@gmail.com>
2024-01-06 11:58:01 +0000

    Save password in FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,7 +44,8 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+            json_objects[key] = self.__objects[key].\
+                                to_dict(hide_password=False)
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
