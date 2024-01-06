ab2ce255cc0a9e9394d92f16bd092dffb85c2df6
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-06 12:18:30 +0000

    updated index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,4 @@
 #!/usr/bin/python3
-"""module for index blueprints"""
-
-from api.v1.views import app_views
-from flask import jsonify
-from models import storage
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    """Returns status Ok"""
-    return jsonify({"status": "OK"})
-def count():
-    """Retrieves the number of objects"""
-    obj_type = [Amenity, State, City, Review, Place, User]
-    stats = {}
-    for obj in obj_type:
-        stats_result = storage.count(obj)
-        stats[obj] = stats_result
-    return jsonify(stats)
-    
 """Includes Flask routes for airbnb clone"""
 
 from api.v1.views import app_views
