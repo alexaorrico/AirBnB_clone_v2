45f0009f6d9e976a5e2f534e89b47a67775b05de
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 18:34:14 +0200

    ahmed eid v0.03D

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,11 +10,13 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
-	"""teardown"""
-	storage.close()
+    """teardown"""
+    storage.close()
 
 
 if __name__ == "__main__":
-	app.run(host=os.getenv("HBNB_API_HOST"), port=os.getenv("HBNB_API_PORT"), threaded=True)
+    app.run(host=os.getenv("HBNB_API_HOST"),
+            port=os.getenv("HBNB_API_PORT"), threaded=True)
