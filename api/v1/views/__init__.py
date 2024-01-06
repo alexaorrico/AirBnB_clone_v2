6338784aeb158049a5574267bcaba8774a776573
Habiba Adam <119977316+habibaadam@users.noreply.github.com>
2024-01-06 11:39:38 +0000

    shebangs added

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""Contains Blueprint object"""
+
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Includes Flask routes for airbnb clone"""
+
 from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
