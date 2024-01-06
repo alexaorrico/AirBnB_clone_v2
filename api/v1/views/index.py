ce79c1fec125bff6cc862b240226c061cba09591
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 18:26:31 +0200

    ahmed eid v0.03B

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def status():
     """Returns a JSON string"""
     return jsonify({"status": "OK"})
