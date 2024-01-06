93e0c313a26bf430da23834766f8927a523877ff
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2024-01-06 20:07:19 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""
+   a Blueprint instance with the name 'app_views'
+"""
+
 from flask import Blueprint
+
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views import *
