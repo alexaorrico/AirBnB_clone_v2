6d304dd1d60c1bd4340089ae1f8c9e4a54884835
memoe <memoemukoe@gmail.com>
2024-01-06 09:08:12 -0800

    removing true debug

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,4 +27,3 @@ if __name__ == "__main__":
     """Main function for flask app"""
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True, debug=True)
