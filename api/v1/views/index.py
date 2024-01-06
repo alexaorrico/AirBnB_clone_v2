5186dcb0db8a85e367d0635b082b5e0f02000c40
memoe <memoemukoe@gmail.com>
2024-01-05 16:40:39 -0800

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,14 @@ from models import storage
 def status():
     """Returns status Ok"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/api/v1/stats', methods=['GET'],
+                 strict_slashes=False)
+def count():
+    """Retrieves the number of objects"""
+    obj_type = [Amenity, State, City, Review, Place, User]
+    stats = {}
+    for obj in obj_type:
+        stats_result = storage.count(obj)
+        stats[obj] = stats_result
+    return jsonify(stats)
