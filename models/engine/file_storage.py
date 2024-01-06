7011394ab3ecffbcd7b928ae70fdafc01066986c
lisapowell1991 <lisapowell46@gmail.com>
2024-01-06 04:43:45 -0800

    fixed storage file and deleted file_storage_backup file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,34 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Retrieve one object.
+
+        Args:
+            cls: Class
+            id: String representing the object ID
+
+        Returns:
+            Object based on the class and its ID, or None if not found
+        """
+        key = "{}.{}".format(cls.__name__, id)
+        return self.__objects.get(key, None)
+
+    def count(self, cls=None):
+        """
+        Count the number of objects in storage.
+
+        Args:
+            cls: Class (optional)
+
+        Returns:
+            The number of objects in storage matching the given class.
+            If no class is passed, returns the count of all objects in storage.
+        """
+        if cls:
+            count = sum(1 for obj in self.__objects.values() if isinstance(obj, cls))
+        else:
+            count = len(self.__objects)
+        return count
