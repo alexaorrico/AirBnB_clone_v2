51000623b04a35c18081b0428967af6bd4ec08f1
Sadson <sadsonmkumira@yahoo.com>
2024-01-06 16:59:29 +0000

    Improved file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception as ep:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,22 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieves one object"""
+        for key in classes.values():
+            if cls == key:
+                key = f'{cls.__name__}.{id}'
+                if key in self.__objects:
+                    return self.__objects[key]
+
+    def count(self, cls=None):
+        """counts the number of objects in storage"""
+        number_objects = 0
+        if cls is not None:
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or value.__class__.__name__:
+                    number_objects += 1
+            return number_objects
+        else:
+            return len(self.__objects)
