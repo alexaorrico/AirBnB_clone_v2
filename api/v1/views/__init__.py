0772aad42938e114a55766dac5a62f59e905ab99
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 17:05:08 +0200

    ahmed eid v0.038

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,3 @@
 from flask import Blueprint
-import *
+from api.v1.views.index import *
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
 
