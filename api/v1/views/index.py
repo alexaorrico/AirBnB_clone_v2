e2f251f0592eb06bfe35c14fce4bccacab599bf9
Madu-f0r0 <duherbert@gmail.com>
2024-01-06 21:29:47 +0100

    Initial set up for flask WSGI
    Set up v1 drectory hierarchy, initialized flask instance, created app_view blueprint, handled ,  routes, handled 404 error

---
+++ api/v1/views/index.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Blueprint for api status"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+@app_views.route("/status")
+def status():
+    """Returns the api status in JSON format"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """Returns the number of items in storage in JSON format"""
+    from models import storage
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+               "reviews": Review, "states": State, "users": User
+               }
+
+    all_cls_stats = {key: storage.count(val) for key, val in classes.items()}
+    return jsonify(all_cls_stats)
