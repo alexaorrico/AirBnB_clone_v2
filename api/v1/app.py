7d8022635edb5d66e2b128de6e393f06ae358d87
Allan <allancheruiyot003@gmail.com>
2024-01-06 07:14:38 -0500

    ALX SE

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from flask import Flask
+from os import getenv
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
