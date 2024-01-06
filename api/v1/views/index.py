df15f10589a404a8f4257eb526519117d32005d2
Zaim211 <yousseftitazaim@gmail.com>
2024-01-06 07:17:31 -0800

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,3 +12,23 @@ def status():
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
