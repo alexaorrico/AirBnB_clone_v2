3fab06a4be8bc3d0a981d3f1887839b73c6bb5b0
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 18:30:22 +0200

    ahmed eid v0.03C

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ app.py """
+
+
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
@@ -7,10 +9,12 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 @app.teardown_appcontext
 def teardown_db(exception):
 	"""teardown"""
 	storage.close()
+
 
 if __name__ == "__main__":
 	app.run(host=os.getenv("HBNB_API_HOST"), port=os.getenv("HBNB_API_PORT"), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" index.py """
+
+
 from api.v1.views import app_views
 from flask import jsonify
-
 
 @app_views.route('/status', methods=['GET'])
 def status():
