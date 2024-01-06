80ce59182d211248d9a8d05ba43298250efe346a
Mcrymbo <croco2023@gmail.com>
2024-01-06 21:25:31 +0300

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ creating an app file for defining end points """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -10,6 +10,14 @@ app = Flask(__name__)
 
 
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ handles 404 error """
+
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == '__main__':
     host = environ.get('HBNB_API_HOST')
