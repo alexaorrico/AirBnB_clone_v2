467f84e6d1d6e32ed3faa39b7fa99d3583eccefe
Mcrymbo <croco2023@gmail.com>
2024-01-06 23:05:11 +0300

    update state

---
+++ api/v1/views/states.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+""" creates a new view for State object """
+from models.state import State
+from models import storage
+from api.v1.views import app_views
+from flask import make_response, jsonify, abort, request
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+
+def get_state():
+    """ get list of states """
+    states = storage.all(State).values()
+    states_all = []
+    for state in states:
+        states_all.append(state.to_dict())
+    return jsonify(states_all)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state_by_id(state_id):
+    """ get list by id """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """ deletes a state """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """ post method for adding State """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    if 'name' not in request.get_json():
+        abort(400, description="Missing name")
+
+    res = request.get_json()
+    state = State(**res)
+    state.save()
+    return make_response(jsonify(state.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """ updates state based on id """
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    fields = ['id', 'created_at', 'updated_at']
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in fields:
+            setattr(state, key, value)
+    storage.save()
+    return make_response(jsonify(state.to_dict()), 200)
