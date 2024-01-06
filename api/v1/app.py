713956845a9bcd6c8df6f593090598e7c4e34081
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-01-06 05:21:48 -0800

    Code updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,10 +10,7 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-# Enable CORS
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 app.register_blueprint(app_views)
-app.url_map.strict_slashes=False
 
 
 @app.teardown_appcontext
@@ -23,15 +20,6 @@ def teardown_appcontext(exception):
     """
     storage.close()
     
-# Error handlers for expected app behavior:
-@app.errorhandler(404)
-def not_found(error):
-    """
-    Return errmsg `Not Found`.
-    """
-    response = {'error': 'Not found'}
-    return jsonify(response), 404
-
 
 if __name__ == '__main__':
     HOST = '0.0.0.0' if getenv('HBNB_API_HOST') is None else getenv('HBNB_API_HOST')
