bdf97de452869368edfd38e7195681c70e109ded
Ote Ian <oteian9@gmail.com>
2024-01-06 21:48:57 +0300

    New: Test cases for get() and count() and all previously defined methods

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,11 +86,72 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_no_class(self):
         """Test that all returns all rows when no class is passed"""
+        storage = models.storage
+        all_objs = storage.all()
+        self.assertEqual(len(all_objs), storage.count())
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_new(self):
         """test that new adds an object to the database"""
+        storage = models.storage
+        initial_count = storage.count(User)
+
+        new_user = User(name="Freddie McKenzie")
+        storage.new(new_user)
+        storage.save()
+
+        updated_count = storage.count(User)
+        self.assertEqual(updated_count, initial_count + 1)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """Test the get() method for retrieving an object by class and id"""
+        storage = models.storage
+
+        obj = User(name='Freddie McKenzie')
+        self.assertEqual(obj, storage.get(User, obj.id))
+        self.assertEqual(obj.id, storage.get(User, obj.id).id)
+        self.assertEqual(obj.name, storage.get(User, obj.id).name)
+        self.assertIsNot(obj, storage.get(User, obj.id + 'fu'))
+        self.assertIsNone(storage.get(User, obj.id + 'fu'))
+        self.assertIsNone(storage.get(User, 100))
+        self.assertIsNone(storage.get(None, obj.id))
+
+        with self.assertRaises(TypeError):
+            storage.get(User, obj.id, 'fu')
+            storage.get(User)
+            storage.get()
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        storage = models.storage
+        review1 = Review(text="Review 1")
+        review2 = Review(text="Review 2")
+        review3 = Review(text="Review 3")
+
+        storage.new(review1)
+        storage.new(review2)
+        storage.new(review3)
+        storage.save()
+
+        self.assertGreater(storage.count(), storage.count(Review))
+        self.assertIs(type(storage.count()), int)
+        self.assertIs(type(storage.count(None)), int)
+        self.assertIs(type(storage.count(int)), int)
+        self.assertIs(type(storage.count(State)), int)
+
+        with self.assertRaises(TypeError):
+            storage.count(State, 'fu')
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+        storage = models.storage
+        initial_count = storage.count(User)
+
+        new_user = User(name="Freddie McKenzie")
+        storage.new(new_user)
+        storage.save()
+
+        updated_count = storage.count(User)
+        self.assertEqual(updated_count, initial_count + 1)
