1be6ab27f4a1e113082554267f6aab102a9a4784
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-01-06 02:50:22 -0800

    Commit to new branch

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -79,15 +79,8 @@ class FileStorage:
         id -- The string representing the id
         Return: The object based on the class and its id
         """
-        if cls not in classes.values():
-            return None
-
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
-
-        return None
+        key = cls.__name__ + '.' + id
+        return self.__objects.get(key, None)
     
     def count(self, cls=None):
         """The method counts the number of objects in storage
@@ -96,12 +89,8 @@ class FileStorage:
         cls -- The class
         Return: The number of objects in storage
         """
-        all_class = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
+        if cls:
+            count = sum(1 for key in self.__objects if cls.__name__ in key)
         else:
-            count = len(models.storage.all(cls).values())
+            count = len(self.__objects)
         return count
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -114,7 +114,7 @@ class TestFileStorage(unittest.TestCase):
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
     
-    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+     @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
     def test_get(self):
         """
         Test method for obtaining an instance
