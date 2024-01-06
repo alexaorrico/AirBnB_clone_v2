48f79b0f05905af27e0128fb6695ba4c9eefd16d
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 08:51:29 +0200

    ahmed eid v0.015

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -108,7 +108,7 @@ class test_db_storage(unittest.TestCase):
     def test_get(self):
         """Test that get properly retrieves objects from database"""
         key = "Amenity.20c0604b-d687-4e8b-b5a1-666b2b002077"
-        self.assertEqual(models.storage.all(Amenity)[key], models.storage.get(Amenity, "20c0604b-d687-4e8b-b5a1-666b2b002077"))
+        self.assertEqual(models.storage.all(Amenity).items()[key], models.storage.get(Amenity, "20c0604b-d687-4e8b-b5a1-666b2b002077"))
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
