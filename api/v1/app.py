39e1204997f478414a4821b6689360954348e98a
eyobsis <ehtube2020@gmail.com>
2024-01-06 15:01:28 -0800

    api....

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,9 +9,11 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST") or "0.0.0.0"
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns the status of your API"""
