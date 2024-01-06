dc43b6ad466f03de7bb5ab5ed6aa98df938ce220
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2024-01-06 20:05:02 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+"""
+  route ib object that returns a json
+"""
+
 from api.v1 import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """
+    Return the status of the API
+    """
     return jsonify({"status": "OK"})
