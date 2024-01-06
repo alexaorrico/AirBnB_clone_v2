ec283caa95020354318745ec2b73bbcfd9fac467
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 08:19:54 +0200

    ahmed eid v0.00D

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -118,6 +118,8 @@ class test_db_storage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
         """Test that count properly counts objects in database"""
+        models.storage.reload()
+        models.storage.save()
         self.assertEqual(models.storage.count(State), 32)
         self.assertEqual(models.storage.count(), 32)
         print(models.storage.count())
