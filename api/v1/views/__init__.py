68a88fd2a6c15ecf889813a00c166cf0574e74b8
Wanjiruwanjiku-tech <natalieshiro2016@gmail.com>
2024-01-06 21:00:05 +0300

    code updates for __init__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Creates a blueprint instance with url-prefix"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
