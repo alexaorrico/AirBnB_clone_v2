dc0605a8c002fdc1b436066fa95c54337f8e3459
Zaim211 <yousseftitazaim@gmail.com>
2024-01-06 07:20:08 -0800

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 """Flask route that returns json status response"""
 from api.v1.views import app_views
 from flask import jsonify, request
-
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
