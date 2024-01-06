a8b54db0cffec6505ee875dd92ca31dfa58ee7b8
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 07:54:01 +0200

    ahmed eid v0.005

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,7 @@ initialize the models package
 from os import getenv
 
 
-storage_t = "db"
+storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
