db9327768f95493dcf532d2c3225e806678deb22
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-06 18:36:46 +0200

    ahmed eid v0.03F

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns a JSON string"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Returns a JSON string"""
+    from models import storage
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
