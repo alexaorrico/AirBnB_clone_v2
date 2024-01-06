de16745b49ae3a1da42cc95f293c5787f5b70441
MarkMusili <musiliyrn@gmail.com>
2024-01-06 23:59:19 +0300

    Import error fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,7 +9,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def display_status():
     """
     Route: /status
