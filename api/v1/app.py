1edbf41de485e87510d96c11705500d7cd9fdfda
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-01-06 14:09:41 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """starting a Flask application"""
 
-from os import getenv
-from flask import Flask, jsonify
-from models import storage
-from api.v1.views import app_views
-
 app = Flask(__name__)
-
-@app.teardown_appcontext
-def storage_close(Exception):
-    """Closes storage"""
-    storage.close()
-
-if __name__ == '__main__':
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-=======
 """ Flask app for aibrnb clone"""
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response, render_template, url_for
@@ -41,5 +25,4 @@ port = os.getenv('HBNB_API_PORT', '5000')
 
 if __name__ == "__main__":
     """Main function for flask app"""
->>>>>>> 6338784aeb158049a5574267bcaba8774a776573
     app.run(host=host, port=port, threaded=True)
