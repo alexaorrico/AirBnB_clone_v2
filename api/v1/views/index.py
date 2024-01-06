68c6b74ebe0224149dd9aa918320fe45a5cca771
Emil dawood <edawood997@gmail.com>
2024-01-06 02:31:41 +0200

    d

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,15 @@
 """index"""
 
 
+from models.amenity import Amenity
+from models.state import State
+from models.place import Place
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
+from models import *
+from models.city import City
+from models.review import Review
+from models.user import User
 
 
 @app_views.route('/status')
@@ -15,16 +21,16 @@ def status():
     return json_response_stat
 
 
-@app_views.route('/api/v1/stats')
+@app_views.route('/stats')
 def stats():
     """retrieves the number of each objects by type"""
     obj_count = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
 
     json_obj_count = jsonify(obj_count)
