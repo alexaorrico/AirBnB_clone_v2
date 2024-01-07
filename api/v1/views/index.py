48a9562bf146b1b48b63e465d29f4752d74cbd51
octocatblain <blainmasaku2@gmail.com>
2024-01-07 17:24:31 +0300

    task 4 correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ Index """
+from models.amenity import Amenity
 from models.city import City
+from models.place import Place
+from models.review import Review
 from models.state import State
+from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
