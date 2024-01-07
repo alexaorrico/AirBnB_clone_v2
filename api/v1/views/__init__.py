cfc1a0d954fe71faf4dd82a85a6ad581ffa0c789
HazelDaniel <hazeldaniel856@gmail.com>
2024-01-07 13:48:20 +0100

    commented out the import statement on the package initializer file of the api package to avoid circular import, also imported modules into the functions that need them to avoid potential circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """Initializes blueprints"""
 
 from flask import Blueprint
-from api.v1.views.index import *
+# from api.v1.views.index import *
 
 app_views = Blueprint("app_views", __name__,
                       url_prefix="/api/v1")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,13 +3,6 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route("/status")
@@ -20,6 +13,13 @@ def status():
 
 @app_views.route("/stats")
 def stats():
+    from models.amenity import Amenity
+    from models.base_model import BaseModel
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
     """Returns the number of items in storage in JSON format"""
     from models import storage
     classes = {"amenities": Amenity, "cities": City, "places": Place,
