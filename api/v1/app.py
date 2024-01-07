a141f83bbe5762a6931d1572c806ee24a109bb7e
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-01-07 15:59:44 -0800

    . HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,14 +3,14 @@
 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify
 from flask_cors import CORS
 from os import getenv
 
 
 app = Flask(__name__)
 
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 app.register_blueprint(app_views)
 
