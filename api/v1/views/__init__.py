4f67eb884603b98623a39ced8de2cc317f52def4
MarkMusili <musiliyrn@gmail.com>
2024-01-07 15:14:19 +0300

    last trial

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,6 @@
 """Blueprint for API"""
 # from api.v1.views.index import *
 from flask import Blueprint
-
+from .index import app_views
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
