ef6dce4327796bbcd6af5e231fbf50c2e465e26b
Edward_Odey <edwardodey04@gmail.com>
2024-01-07 09:35:31 -0800

    update directory structure

--- test_get_count.py
+++ test_get_count.py
@@ -5,8 +5,8 @@ from models import storage
 from models.state import State
 
 print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count(State)))
+print("State objects: {}".format(storage.count("value")))
 
 first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+print("First state: {}".format(storage.get(State, "first_state_id")))
 
