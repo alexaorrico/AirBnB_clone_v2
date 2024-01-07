ad4b33aef524a7b82d917b9ead06e4b5eb6a8352
Mcrymbo <croco2023@gmail.com>
2024-01-07 11:40:52 +0300

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -14,11 +14,10 @@ def get_cities(state_id):
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    cities = storage.all(City).values()
-    cities_all = []
-    for city in cities:
-        cities_all.append(city.to_dict())
-    return jsonify(cities_all)
+    cities = []
+    for city in state.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
@@ -30,7 +29,8 @@ def get_city(city_id):
     return jsonify(city.to_dict())
 
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/cities/<city_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """ deletes a city """
     city = storage.get(City, city_id)
@@ -41,22 +41,28 @@ def delete_city(city_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/cities', methods=['POST'], strict_slashes=False)
-def post_city():
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
+def post_city(state_id):
     """ post method for adding city """
-    if not request.get_json():
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    res = request.get_json()
+    if not res:
         abort(400, description="Not a JSON")
-    if 'name' not in request.get_json():
+    if 'name' not in res:
         abort(400, description="Missing name")
 
-    res = request.get_json()
     city = City(**res)
-    city.save()
+    setattr(city, 'state_id', state_id)
+    storage.new(city)
+    storage.save()
     return make_response(jsonify(city.to_dict()), 201)
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
-def put_state(city_id):
+def put_city(city_id):
     """ updates city based on id """
     city = storage.get(City, city_id)
     if not city:
