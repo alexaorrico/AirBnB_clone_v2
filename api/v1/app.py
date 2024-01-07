38f6a594860529788d1b724aa32a2f483af5369d
MphoCM <christian.mofokeng@gmail.com>
2024-01-07 13:03:34 -0800

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""server configuration"""
-from api.v1.views import app_views
-from flask import Flask, jsonify
-from models import storage
+"""app.py to connect to API"""
 import os
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """cleanup operation at the end of a request"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
-@app.errorhandler(404)
-def error_msg(error):
-    """returns error 404 in JSON"""
-    err = jsonify({"error": "Not found"})
-    err.status_code = 404
-    return err
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
