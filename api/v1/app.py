ebfd6409d0905caf5b38a805c578598dfde36553
dev-EstherKubania <eskury@gmail.com>
2024-01-07 10:43:59 -0800

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from os import getenv
+import os
 
 
 app = Flask(__name__)
@@ -20,5 +20,4 @@ def teardown(exception):
 if __name__ == '__main__':
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
-
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,12 @@
 """
   route in object that returns a json
 """
+
 from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
     """
     Return the status of the API
