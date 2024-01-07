adf6ca65d4e21dee7101fd690f249ef306cf4430
Ote Ian <oteian9@gmail.com>
2024-01-07 21:20:39 +0300

    Fix: Documentation

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,14 +11,35 @@ from api.v1.views import app_views
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def list_state_objects():
-    """ """
+    """
+    Retrieve a list of all State objects.
+
+    Returns:
+    - JSON representation of the list of State objects.
+
+    Example:
+    $ curl http://127.0.0.1:5000/api/v1/states
+    Output: JSON representation of all State objects
+    """
     all_objs = storage.all(State)
     return jsonify([obj.to_dict() for obj in all_objs.values()])
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state_objects(state_id):
-    """ """
+    """
+    Retrieve a specific State object based on its ID.
+
+    Parameters:
+    - state_id: The ID of the State object to retrieve.
+
+    Returns:
+    - JSON representation of the specified State object.
+
+    Example:
+    $ curl http://127.0.0.1:5000/api/v1/states/1
+    Output: JSON representation of the State object with ID 1
+    """
     obj = storage.get(State, state_id)
     if not obj:
         abort(404)
@@ -28,7 +49,19 @@ def get_state_objects(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state_objects(state_id):
-    """ """
+    """
+    Delete a specific State object based on its ID.
+
+    Parameters:
+    - state_id: The ID of the State object to delete.
+
+    Returns:
+    - Empty JSON response with HTTP status code 200 upon successful deletion.
+
+    Example:
+    $ curl -X DELETE http://127.0.0.1:5000/api/v1/states/1
+    Output: {}
+    """
     obj = storage.get(State, state_id)
     if obj is None or not obj:
         abort(404)
@@ -41,7 +74,19 @@ def delete_state_objects(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    """ """
+    """
+    Create a new State object.
+
+    Returns:
+    - JSON representation of the newly created State object.
+    - HTTP status code 201 upon successful creation.
+
+    Example:
+    $ curl -X POST -H "Content-Type: application/json" \
+      -d '{"name": "New State"}' http://127.0.0.1:5000/api/v1/states
+    Output: JSON representation of the newly created State object
+    """
+
     data = request.get_json()
 
     if not data:
@@ -58,7 +103,21 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state_objects(state_id):
-    """ """
+    """
+    Update a specific State object based on its ID.
+
+    Parameters:
+    - state_id: The ID of the State object to update.
+
+    Returns:
+    - JSON representation of the updated State object.
+    - HTTP status code 200 upon successful update.
+
+    Example:
+    $ curl -X PUT -H "Content-Type: application/json" \
+      -d '{"name": "Updated State"}' http://127.0.0.1:5000/api/v1/states/1
+    Output: JSON representation of the updated State object
+    """
     obj = storage.get(State, state_id)
 
     if obj is None or not obj:
