3ea267de91a11ac0cbdf43d6ece9913c6361341a
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-07 08:19:37 +0200

    ahmed eid v0.04F test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -104,21 +104,3 @@ class TestFileStorage(unittest.TestCase):
         self.assertIn(key, models.storage.all().keys())
         models.storage.delete(state)
         models.storage.save()
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_get(self):
-        """Test that get properly retrieves objects from database"""
-        states = models.storage.all(State).values()
-        getCheck = None
-        if len(states) > 0:
-            getCheck = models.storage.get(State, list(states)[0].id)
-            self.assertIn(getCheck, states)
-        self.assertEqual(getCheck, None)
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-    def test_count(self):
-        """Test that count properly counts objects in database"""
-        length = len(models.storage.all(State).values())
-        self.assertEqual(models.storage.count(State), length)
-        length = len(models.storage.all().values())
-        self.assertEqual(models.storage.count(), length)
