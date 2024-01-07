378537368b1f00c08c7aca65735065cb4ee6f4c2
KelvinoKing <kelvinoking@outlook.com>
2024-01-07 14:19:25 +0000

    State

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """app for registering blueprint and starting flask"""
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -14,6 +14,11 @@ app.register_blueprint(app_views)
 def tearDown(self):
     """close query after each session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
