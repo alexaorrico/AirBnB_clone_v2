49abd671bbbfcbcf47690f08ca320ff6bb167d3b
obimbasmart <obimbasmartchukwunenye@gmail.com>
2024-01-07 04:00:14 +0100

    fix: use ISO format for datetime conversion

--- models/base_model.py
+++ models/base_model.py
@@ -62,9 +62,9 @@ class BaseModel:
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+            new_dict["created_at"] = new_dict["created_at"].isoformat()
         if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+            new_dict["updated_at"] = new_dict["updated_at"].isoformat()
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
