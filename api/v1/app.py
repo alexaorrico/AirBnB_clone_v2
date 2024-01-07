eb04da5467466e33aae836691bf5ef3241f3d025
lisapowell1991 <lisapowell46@gmail.com>
2024-01-07 07:41:17 -0800

    404 response as JSON

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 Starts the flask app.py
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -19,6 +19,11 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def close_con(error):
     """closes the database connection"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
