ab532d6350482726fddf3757946e70d55bad6aca
MarkMusili <musiliyrn@gmail.com>
2024-01-07 21:47:24 +0300

    Cities

---
+++ api/v1/views/cities.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Cities routes for the API"""
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+from flask import abort, jsonify, request
+
+app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'])
+def get_cities(state_id):
+    """Gets all the cities for a given state"""
+    if request.method == 'POST':
+        state =  storage.get("State", state_id)
+        if state is None:
+            abort(404)
+        cities = [city.to_dict() for city in state.cities]
+        return jsonify(cities)
+
+    if request.method  == 'POST':
+        data =  request.get_json()
+        if data is None:
+            abort(400, 'Not a JSON')
+        if 'name' not in data:
+            abort(400, 'Missing name')
+        state = storage.get("State", state_id)
+        if state is None:
+            abort(404)
+        city = City(**data)
+        city.state_id = state_id
+        city.save()
+        return jsonify(city.to_dict()), 201
+
+
+app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT', 'POST'])
+def method_city(city_id):
+    """Method for cities"""
+    city = storage.get('City', city_id)
+    if city is None:
+        abort(404)
+    if request.method == 'GET':
+        return jsonify(city.to_dict())
+    if request.method == 'DELETE':
+        storage.delete(city)
+        storage.save()
+        return jsonify({}), 200
+    if request.method == 'PUT':
+        data = request.get_json()
+        if data is None:
+            abort(400, 'Not a JSON')
+        for key, value in data.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(city, key, value)
+        storage.save()
+        return jsonify(city.to_dict()), 200
