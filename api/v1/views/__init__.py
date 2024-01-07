2cf7bf02761d583fb8fb3afb32560c0e493d6157
MarkMusili <musiliyrn@gmail.com>
2024-01-07 14:48:31 +0300

    import fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Blueprint for API"""
-# from api.v1.views.index import *
 from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
