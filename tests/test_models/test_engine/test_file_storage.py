946f84f055fece055d5b1760a1dd88e36efeafaa
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-07 06:53:52 +0200

    ahmed eid v0.050 test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -147,4 +147,5 @@ class TestFileStorage(unittest.TestCase):
                 instance_key = instance.__class__.__name__ + "." + instance.id
                 search[instance_key] = instance
         self.assertEqual(storage.count(User), len(search))
+        print(storage.count(User))
         FileStorage._FileStorage__objects = save
