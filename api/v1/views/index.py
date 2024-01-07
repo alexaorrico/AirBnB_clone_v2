76a5548de7e251b779cfccfdd0dbab5c4c382a8c
octocatblain <blainmasaku2@gmail.com>
2024-01-07 17:21:45 +0300

    task 4 correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,8 +15,8 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
     """ Retrieves the number of each objects by type """
-    classes = [ City, State]
-    names = ["cities", "states"]
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
 
     num_objs = {}
     for i in range(len(classes)):
