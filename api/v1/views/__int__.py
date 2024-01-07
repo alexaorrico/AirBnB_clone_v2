9de01c9a017582df8ac99f606f7fd718a6737979
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2024-01-07 15:07:59 -0800

    states.py and __int__.py update

---
+++ api/v1/views/__int__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Initialize the views module """
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+
