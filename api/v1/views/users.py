855a98156eebaea9525eff46619bb8e7f0fc87f1
Mcrymbo <croco2023@gmail.com>
2024-01-07 16:38:59 +0300

    correction

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" creates a new view for Amenity object """
+""" creates a new view for user object """
 from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import make_response, jsonify, abort, request
 
 
-@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@app_views.route('/users',
+                 methods=['GET'], strict_slashes=False)
 def get_users():
     """ get list of users """
     users = storage.all(User).values()
-    users_all = []
+    if not users:
+        abort(404)
+
+    all_users = []
     for user in users:
-        users_all.append(user.to_dict())
-    return jsonify(users_all)
+        all_users.append(user.to_dict())
+    return jsonify(all_users)
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
@@ -37,17 +41,20 @@ def delete_user(user_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
+@app_views.route('/users',
+                 methods=['POST'], strict_slashes=False)
 def post_user():
     """ post method for adding a user """
-    if not request.get_json():
+    res = request.get_json()
+    if not res:
         abort(400, description="Not a JSON")
-    if 'email' not in request.get_json():
+
+    if 'email' not in res:
         abort(400, description="Missing email")
-    if 'password' not in request.get_json():
+
+    if 'password' not in res:
         abort(400, description="Missing password")
 
-    res = request.get_json()
     user = User(**res)
     storage.new(user)
     storage.save()
