3f979a1bf8d450543cf2941043918763122291e1
Norberts <mainanorbert@gmail.comm>
2024-01-07 22:22:29 +0300

    line endings

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,8 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = '52.205.210.57'
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
