81f2adf820d6c3ac2bc6b093b6e92576e4532fd3
Norberts <mainanorbert@gmail.comm>
2024-01-07 07:35:09 +0300

    api status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,11 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', method=['GET'])
+@app_views.route('/status', methods=['GET'])
 def status():
     """returning a json string"""
     return jsonify({"status": "OK"})
+
+# @app_views.route('/stats')
+# def method_name():
+#     pass
