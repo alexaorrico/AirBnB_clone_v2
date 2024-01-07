6f40ab9f8bb49b126f1399064ddc7a7e40eb29c9
aicha652 <aichaazlf652@gmail.com>
2024-01-07 03:49:46 -0800

    fix pycodestyle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,11 +2,10 @@
 """ create a Blueprint of the apps """
 
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
