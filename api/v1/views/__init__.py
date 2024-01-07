fa41ec09a0cb64b8c78b5192219f19d4cd61c0b9
itsoluwatobby <oluwasamuel050@gmail.com>
2024-01-07 11:33:03 +0100

    working on places

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from . import index
 from api.v1.views import states
 from api.v1.views import amenities
+from api.v1.views import places
