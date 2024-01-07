11ff9b4fdc9518daa4e3f20e0f22e05355675956
MarkMusili <musiliyrn@gmail.com>
2024-01-07 15:16:37 +0300

    importing routes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Blueprint for API"""
-# from api.v1.views.index import *
 from flask import Blueprint
-from .index import app_views
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
