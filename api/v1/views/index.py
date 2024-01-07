b2367e8721c0bd9c4232f718439fc373ae5bfd68
Norberts <mainanorbert@gmail.comm>
2024-01-07 19:13:58 +0300

    return no. of objs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-module for api index page
+module that is used for api index page
 """
 from api.v1.views import app_views
 from flask import jsonify
@@ -8,7 +8,24 @@ from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
-def status():
-    """ returns json string """
+def api_status():
+    """ returns json string when request api status"""
     return jsonify({"status": "OK"})
 
+
+@app_views.route('/stats', methods=['GET'])
+def returns_no():
+    """returning count for all objects by type"""
+    if request.method == 'GET':
+        res = {}
+        clss = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+            }
+        for k, v in clss.items():
+            res[v] = storage.count(k)
+        return jsonify(res)
