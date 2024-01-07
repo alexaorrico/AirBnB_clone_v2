0321e175b3c14e666301d3dbd86ffb0986f665e2
ahmedeid533 <ahmedeidgoma333@gmail.com>
2024-01-07 10:44:07 +0200

    ahmed eid v0.063

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" holds class Amenity"""
+
+from models.amenity import Amenity
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities():
+	"""Returns a JSON string"""
+	amenities = [amenity.to_dict() for amenity in storage.all("Amenity").values()]
+	return jsonify(amenities)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def get_amenity(amenity_id):
+	"""Returns a JSON string"""
+	amenity = storage.get(Amenity, amenity_id)
+	if amenity is None:
+		abort(404)
+	return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+	"""Returns a JSON string"""
+	amenity = storage.get(Amenity, amenity_id)
+	if amenity is None:
+		abort(404)
+	amenity.delete()
+	storage.save()
+	return jsonify({}), 200
+
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def post_amenity():
+	"""Returns a JSON string"""
+	if not request.get_json():
+		abort(400, "Not a JSON")
+	if "name" not in request.get_json():
+		abort(400, "Missing name")
+	amenity = Amenity(**request.get_json())
+	amenity.save()
+	return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def put_amenity(amenity_id):
+	"""Returns a JSON string"""
+	amenity = storage.get(Amenity, amenity_id)
+	if amenity is None:
+		abort(404)
+	if not request.get_json():
+		abort(400, "Not a JSON")
+	for key, value in request.get_json().items():
+		if key not in ['id', 'created_at', 'updated_at']:
+			setattr(amenity, key, value)
+	amenity.save()
+	return jsonify(amenity.to_dict()), 200
