f69beabd0d6093864c5c406e209e10f19b1c70ca
alban-okoby <albanokoby225@gmail.com>
2024-01-07 14:17:57 -0800

    Storage last update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python3
+
 from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """Returns a JSON response with status OK."""
+    """Returns a JSON response with status OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Returns a JSON response with the count of objects"""
+    total_objects = storage.count()
+    return jsonify({"status": "OK", "count": total_objects})
