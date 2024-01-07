9cd53575a0219d06da2cd31053230b14acaa9793
Charaf eddahhani <125513039+HONORPRIDE@users.noreply.github.com>
2024-01-07 19:52:43 +0100

    add  hashlib to improve security for users pswrd

--- models/user.py
+++ models/user.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 """ holds class User"""
-import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
+import hashlib
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """Representation of a user """
-    if models.storage_t == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
-        first_name = ""
-        last_name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+@property
+def password(self):
+    """Getter for the password attribute """
+    return self.__password
+
+@password.setter
+def password(self, value):
+    """Setter for the password attribute """
+    self.__password = hashlib.md5(value.encode()).hexdigest()
