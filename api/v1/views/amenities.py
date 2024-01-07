34964214690de34d705115e09527b1f9f634ce85
hordunlarmy <hordunlarmy@gmail.com>
2024-01-07 05:18:09 +0000

    Refactor get_amenity function to return amenity as a dictionary

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -27,7 +27,7 @@ def get_amenity(amenity_id):
     amenity = storage.get(Amenity, amenity_id)
     if not amenity:
         abort(404)
-    return jsonify(amenity)
+    return jsonify(amenity.to_dict())
 
 
 @app_views.route("/amenities/<string:amenity_id>", strict_slashes=False,
