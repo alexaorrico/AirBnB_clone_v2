cdc0497dc9dd382e5a7fb38369bea80853b99980
MarkMusili <musiliyrn@gmail.com>
2024-01-07 14:06:36 +0300

    Another route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,7 @@ The route returns a JSON response indicating the status as "OK"
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -21,3 +22,18 @@ def display_status():
     Output: {"status": "OK"}
     """
     return jsonify(status="OK")
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def show_data():
+    """
+    Retrieves the number of each objects by type.
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
