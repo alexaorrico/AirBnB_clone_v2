643a01d2fad13666b3f904fb15aee27cb5fff852
octocatblain <blainmasaku2@gmail.com>
2024-01-07 16:04:58 +0300

    task 2: update models

--- models/city.py
+++ models/city.py
@@ -14,7 +14,9 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        places = relationship("Place",
+                              backref="cities",
+                              cascade="all, delete, delete-orphan")
     else:
         state_id = ""
         name = ""
