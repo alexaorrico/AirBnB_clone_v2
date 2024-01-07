5363f135fb72373d7e18c1f2668536dfa93e90c4
Ote Ian <oteian9@gmail.com>
2024-01-07 20:49:16 +0300

    Fix: Strictslashes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,16 +9,14 @@ from flask import Flask, abort, jsonify, request, make_response
 from api.v1.views import app_views
 
 
-@app_views.route('/api/v1/states', method=['GET'],
-                 strictslashes=False)
+@app_views.route('/api/v1/states', method=['GET'])
 def list_state_objects():
     """ """
     all_objs = storage.all(State)
     return [obj.to_dict() for obj in all_objs.values()]
 
 
-@app_views.route('/api/v1/states/<state_id>', method=['GET'],
-                 strictslashes=False)
+@app_views.route('/api/v1/states/<state_id>', method=['GET'])
 def get_state_objects(state_id):
     """ """
     obj = storage.get(State, state_id)
@@ -27,8 +25,7 @@ def get_state_objects(state_id):
     return jsonify(obj.to_dict())
 
 
-@app_views.route('/api/v1/states/<state_id>', method=['DELETE'],
-                 strictslashes=False)
+@app_views.route('/api/v1/states/<state_id>', method=['DELETE'])
 def delete_state_objects(state_id):
     """ """
     obj = storage.get(State, state_id)
@@ -41,8 +38,7 @@ def delete_state_objects(state_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('/api/v1/states', method=['POST'],
-                 strictslashes=False)
+@app_views.route('/api/v1/states', method=['POST'])
 def create_state():
     """ """
     data = request.get_json()
@@ -59,8 +55,7 @@ def create_state():
     return make_response(jsonify(obj), 201)
 
 
-@app_views.route('/api/v1/states/<state_id>', method=['PUT'],
-                 strictslashes=False)
+@app_views.route('/api/v1/states/<state_id>', method=['PUT'])
 def update_state_objects(state_id):
     """ """
     obj = storage.get(State, state_id)
