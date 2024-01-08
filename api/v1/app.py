2003fc6e1a051c4d484b3f10e95f8a6083b6433e
mefdev <mohamedmeftah2018@gmail.com>
2024-01-08 13:15:48 +0100

    Configure the main app

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    HBNB_API_HOST = getenv('HBNB_API_HOST')
+    HBNB_API_PORT = getenv('HBNB_API_PORT')
+    app.run(host=HBNB_API_HOST or "0.0.0.0",
+            port=HBNB_API_PORT or "5000", threaded=True)
