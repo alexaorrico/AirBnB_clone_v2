c11e384221ee180bf97c2d9b204daad90d83e89a
Ayo <talktojmcvibes@gmail.com>
2024-01-08 12:33:41 -0800

    task4 fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""flask  api views endpoint module"""
+"""
+This module contains endpoint(route) status
+"""
 from models import storage
+from flask import Flask
 from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
-    """return json object"""
+    """
+    Returns a JSON status
+    """
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats')
-def stat_count():
-    """Returns json object of
-    each class"""
-    return jsonify({'amenities': storage.count('Amenity')
-                    'cities': storage.count('City'),
-                    'places': storage.count('Place'),
-                    'reviews': storage.count('Review'),
-                    'states': storage.count('State'),
-                    'users': storage.count('User')})
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
