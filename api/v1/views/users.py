b9fd3afee4d9c8ce54d99d0410fcc51293849b78
Jukunye <jukunyes@gmail.com>
2024-01-08 23:07:12 +0300

    users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""
+a new view for User objects that handles all default RESTFul API actions.
+"""
+
+from flask import abort, jsonify, request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
