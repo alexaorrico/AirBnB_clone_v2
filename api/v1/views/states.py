8a773f31d07acb29a15336e49b4b225eff3abd1b
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-01-09 00:55:08 +0100

    ok

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,7 @@ from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
 
-@app_views.route('/states', strict_slashes=False, methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/state/get_state.yml', methods=['GET'])
 def get_states():
     """
@@ -20,13 +20,15 @@ def get_states():
     return jsonify(list_states)
 
 
-@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/state/get_id_state.yml', methods=['get'])
 def get_state(state_id):
+    """ Retrieves a specific State """
     state = storage.get(State, state_id)
-    if state is not None:
-        return jsonify(state.to_dict())
-    abort(404)
+    if not state:
+        abort(404)
+
+    return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>',
