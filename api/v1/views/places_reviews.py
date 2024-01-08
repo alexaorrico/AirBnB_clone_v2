edbe11c20ed997df47c983ac4b3b46292816da22
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-08 17:06:53 +0300

    task 11

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -82,7 +82,8 @@ def update_review(review_id):
         abort(400, "Not a JSON")
 
     for key, value in data.items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at', 'updated_at']:
+        if key not in [
+            'id', 'user_id', 'place_id', 'created_at', 'updated_at']:
             setattr(review, key, value)
     review.save()
     return jsonify(review.to_dict())
