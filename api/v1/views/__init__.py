bdea5ff0e7ad8d2c6dd54c5af2264ad06f32eafc
fardosa001 <fardosaadow@gmail.com>
2024-01-07 21:15:27 -0800

    Amenity

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
@@ -12,4 +14,3 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
