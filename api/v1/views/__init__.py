7b8aad673861cb3bde6c2eec3454b1e822004ba2
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-01-09 00:08:10 +0300

    resolve circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,8 +3,9 @@
 This module declares the blueprint of the system
 """
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.states import *
