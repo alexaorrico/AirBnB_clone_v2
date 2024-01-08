8391d25683eec040166d1ab0c675b37b5b51c146
Norberts <mainanorbert@gmail.comm>
2024-01-08 04:47:04 +0300

    pycode style compliant

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,6 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
     def get(self, cls, id):
         """retrieving one object"""
         """key = "{}.{}".format(cls.__name__, id)"""
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,13 +50,10 @@ class FileStorage:
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-        try:
-            with open(self.__file_path, 'r') as f:
-                jo = json.load(f)
+        with open(self.__file_path, 'r') as f:
+            jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
-            pass
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
