9f90b2e7592310fad429ebaa223739a2cdef38a3
HazelDaniel <hazeldaniel856@gmail.com>
2024-01-08 15:02:14 +0100

    added support for all available methods

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,4 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
+from api.v1.views.places_reviews import *
---
+++ api/v1/views/places_reviews.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""blueprint for the reviews"""
+
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+from markupsafe import escape
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
+
+@app_views.get('/places/<place_id>/reviews', strict_slashes=False)
+def get_reviews_of_place(place_id):
+    """this is the view for the /api/v1/places/[SLUG]/reviews
+        endpoint"""
+    res = storage.get(Place, escape(place_id))
+    if not res:
+        abort(404)
+    res = storage.all(Review).values()
+    res = [x.to_dict() for x in res if x.place_id == place_id]
+    return jsonify(res)
+
+
+@app_views.get('/reviews/<review_id>', strict_slashes=False)
+def get_review(review_id):
+    """this is the view for the /api/v1/reviews/[SLUG]
+        endpoint"""
+    res = storage.get(Review, escape(review_id))
+    if not res:
+        abort(404)
+    res = res.to_dict()
+    return jsonify(res)
+
+
+@app_views.delete('/reviews/<review_id>', strict_slashes=False)
+def delete_review(review_id):
+    """this is the view for the /api/v1/reviews/[SLUG]
+        endpoint"""
+    res = storage.get(Review, escape(review_id))
+    if not res:
+        abort(404)
+    storage.delete(res)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.post('/places/<place_id>/reviews', strict_slashes=False)
+def post_review_of_places(place_id):
+    """this is the view for the /api/v1/places/[SLUG]/reviews
+        endpoint"""
+    res = storage.get(Place, escape(place_id))
+    if not res:
+        abort(404)
+    try:
+        body = request.get_json()
+        if 'text' not in body.keys():
+            return make_response(jsonify("Missing text"), 400)
+        if 'user_id' not in body.keys():
+            return make_response(jsonify("Missing user_id"), 400)
+        new_review = Review(**body)
+        new_review.place_id = place_id
+        storage.new(new_review)
+        storage.save()
+        return make_response(jsonify(new_review.to_dict()), 201)
+    except Exception as e:
+        # print(f"exception is : {e}")
+        return make_response(jsonify("Not a JSON"), 400)
+
+
+@app_views.put('/reviews/<review_id>', strict_slashes=False)
+def put_review(review_id):
+    """this is the view for the /api/v1/reviews/[SLUG]
+        endpoint"""
+    res = storage.get(Review, escape(review_id))
+    ignore_keys = ["id", "created_at", "updated_at"]
+    if not res:
+        abort(404)
+    try:
+        body = request.get_json()
+        for key in body:
+            if key not in ignore_keys:
+                res.__dict__[key] = body[key]
+        res.save()
+        storage.save()
+        return make_response(jsonify(res.to_dict()), 200)
+    except Exception as e:
+        return make_response(jsonify("Not a JSON"), 400)
