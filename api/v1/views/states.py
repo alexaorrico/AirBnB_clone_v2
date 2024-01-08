28670ebf03b53ee8a038a3805a5173cbeaa888d4
kevkatam <kevkatam@gmail.com>
2024-01-08 12:30:26 -0800

    modified states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -24,7 +24,7 @@ def state_id(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """ Deletes a State object """
     state = storage.get("State", state_id)
