87e13c0fd8a05c1c9a149afb569acb11bde03b88
Mulle21 <mulukenab95@gmail.com>
2024-01-08 04:58:19 -0800

    task model

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,20 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+        def get(self, cls, id):
+       '''get:
+       retrive an obiect from the dile storage by class and id '''
+       if cls in classes.values() and id and type(id) == str:
+           d_obj =self.all(cls)
+           for key, value in d_obj.items():
+               if key.split(".") == id:
+                   return value
+               return None
+           def count(self, cls=None):
+               '''count the number of object in astroge maching the give class
+               '''
+               data = self.all(cls)
+               if cls in classes.values():
+                   data = self.all(cls)
+                   return len(data)
