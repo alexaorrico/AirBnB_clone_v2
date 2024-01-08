76a4c6d3a3285c4c83f93ffd1b6f20dd1b12505a
Abokiey <sweneysam@gmail.com>
2024-01-08 17:53:55 +0300

    amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
+    """get amenities"""
     amenities = storage.all(Amenity).values()
     return jsonify([amenity.to_dict() for amenity in amenities])
 
@@ -15,6 +16,7 @@ def get_amenities():
 @app_views.route('/amenities/<amenity_id>', methods=['GET'],
                  strict_slashes=False)
 def get_amenity(amenity_id):
+    """get amenity by amenit id"""
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
@@ -24,6 +26,7 @@ def get_amenity(amenity_id):
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_amenity(amenity_id):
+    """delete amenity by amenit id"""
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
@@ -34,6 +37,7 @@ def delete_amenity(amenity_id):
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
+    """create amenity"""
     data = request.get_json()
     if data is None:
         abort(400, description="Not a JSON")
@@ -49,6 +53,7 @@ def create_amenity():
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_amenity(amenity_id):
+    """update amenity by amenit id"""
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
