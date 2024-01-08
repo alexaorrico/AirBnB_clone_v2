5db044b0e9100109415a2f047ae8b2a4185f6d7b
mefdev <mohamedmeftah2018@gmail.com>
2024-01-08 14:47:43 +0100

    Hot fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Initialize blueprint"""
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
