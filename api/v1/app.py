ecda082fe054ca08cf2711df70450f56c0140ec8
Ayo <talktojmcvibes@gmail.com>
2024-01-08 12:51:21 -0800

    import make response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask app entry point"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
