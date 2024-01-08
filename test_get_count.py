52f6c0efe29b156e42bfd67006aa68ce97f6678f
Mikias Gedlu <addiserp@gmail.com>
2024-01-08 17:49:21 +0300

    0x05. AirBnB clone - RESTful API

--- test_get_count.py
+++ test_get_count.py
@@ -9,4 +9,3 @@ print("State objects: {}".format(storage.count(State)))
 
 first_state_id = list(storage.all(State).values())[0].id
 print("First state: {}".format(storage.get(State, first_state_id)))
-
