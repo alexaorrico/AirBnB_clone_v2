734675aba34f28a149f53ff3568c704592765bf8
Emil dawood <edawood997@gmail.com>
2024-01-09 00:35:38 +0200

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,13 @@
 
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
