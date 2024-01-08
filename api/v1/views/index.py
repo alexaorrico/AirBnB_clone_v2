45288fe11eeebec90674378db662c743f4a9fb37
Justine-ini <inijustine4040@gmail.com>
2024-01-07 17:04:48 -0800

    API Directory

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Contains the index view for the API.'''
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def get_status():
+    '''Gets the status of the API.
+    '''
+    return jsonify(status='OK')
