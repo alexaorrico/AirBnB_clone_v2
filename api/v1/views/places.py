6dd72e7edc033c50427e80c09f5895529b595d75
KelvinoKing <kelvinoking@outlook.com>
2024-01-08 16:56:55 +0000

    Place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -11,7 +11,8 @@ from models.amenity import Amenity
 from api.v1.views import app_views
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'])
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+        strict_slashes=False)
 def get_places(city_id):
     """Retrives the list of all Place objects of a City
     """
@@ -23,7 +24,8 @@ def get_places(city_id):
     return jsonify(places)
 
 
-@app_views.route('/places/<place_id>', methods=['GET'])
+@app_views.route('/places/<place_id>', methods=['GET'],
+        strict_slashes=False)
 def get_place(place_id):
     """Retrive a place object
     """
@@ -34,7 +36,8 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'])
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+        strict_slashes=False)
 def delete_place(place_id):
     """Delete a Place object
     """
@@ -48,7 +51,8 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'])
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+        strict_slashes=False)
 def create_place(city_id):
     """Creates a Place object
     """
@@ -69,11 +73,13 @@ def create_place(city_id):
             abort(404)
         data["city_id"] = city_id
         new_place = Place(**data)
-        new_place.save()
+        storage.new(new_place)
+        storage.save()
         return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'])
+@app_views.route('/places/<place_id>', methods=['PUT'],
+        strict_slashes=False)
 def update_place(place_id):
     """Updates a Place object
     """
