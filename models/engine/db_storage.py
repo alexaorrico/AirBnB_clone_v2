6985a8b588d39aab3018adff1190320df0cef874
Ermi24et <lilermi24@gmail.com>
2024-01-08 12:56:04 +0300

    updated the file storage and db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -71,6 +71,20 @@ class DBStorage:
         Session = scoped_session(sess_factory)
         self.__session = Session
 
+    def get(self, cls, id):
+        """ retrieve one object """
+        if self.all(cls):
+            for val in self.all(cls).values():
+                if value.id = id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """ count the number of objects in storage """
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
+
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,6 +65,20 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
+    def get(self, cls, id):
+        """ get or retrieve one object """
+        if self.all(cls):
+            for val in self.all(cls).values():
+                if value.id = id:
+                    return val
+        return None
+
+    def count(self, cls=None):
+        """ count the number of objects in storage """
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
