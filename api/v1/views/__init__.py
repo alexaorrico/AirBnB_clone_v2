8f31e478d9e48ca3b22d970fa0dca1f190d6500e
Jukunye <jukunyes@gmail.com>
2024-01-08 14:59:56 +0300

    import states

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,3 +8,4 @@ app_views = Blueprint('/api/v1', __name__)
 from api.v1.views.index import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.states import *
