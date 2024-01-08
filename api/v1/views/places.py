26756f5b00d2c1f0460dbe851f5f94adc3faf1c9
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-08 16:58:49 +0300

    task 10

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -10,7 +10,9 @@ from models.place import Place
 from models.city import City
 from models.user import User
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+
+@app_views.route(
+        '/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def get_places_by_city(city_id):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
@@ -19,7 +21,9 @@ def get_places_by_city(city_id):
     places = [place.to_dict() for place in city.places]
     return jsonify(places)
 
-@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+
+@app_views.route(
+        '/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
     """Retrieves a Place object"""
     place = storage.get(Place, place_id)
@@ -27,7 +31,9 @@ def get_place(place_id):
         abort(404)
     return jsonify(place.to_dict())
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route(
+        '/places/<place_id>', methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
@@ -37,7 +43,9 @@ def delete_place(place_id):
     storage.save()
     return jsonify({})
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+
+@app_views.route(
+        '/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     """Creates a Place"""
     city = storage.get(City, city_id)
@@ -60,7 +68,9 @@ def create_place(city_id):
     new_place.save()
     return jsonify(new_place.to_dict()), 201
 
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route(
+        '/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """Updates a Place object"""
     place = storage.get(Place, place_id)
