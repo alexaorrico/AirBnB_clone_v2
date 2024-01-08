308e66e960af3c58f04382695ffbe0f4e632c668
Allan <allancheruiyot003@gmail.com>
2024-01-08 04:33:56 -0500

    ALX SE

--- models/__init__.py
+++ models/__init__.py
@@ -2,20 +2,14 @@
 """
 initialize the models package
 """
-
 from os import getenv
 
 
 storage_t = getenv("HBNB_TYPE_STORAGE")
-from models.engine.db_storage import DBStorage
-storage = DBStorage()
-storage.reload()
-
-"""if storage_t == "db":
+if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
-"""
--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,7 @@ time = "%Y-%m-%dT%H:%M:%S.%f"
 if models.storage_t == "db":
     Base = declarative_base()
 else:
+    print()
     Base = object
 
 
