085d5755af87ad4917234b9055e98d51180fe1d5
Nmcleon <nmcleon10@gmail.com>
2024-01-09 00:50:36 +0200

    update amenities

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""holds class Amenity
+"""
+from flask import Blueprint, jsonify, request, abort
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+
+# Define route for amenities handling GET and POST requests
+@app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
+def get_post_amenities():
+    """Handles GET (retrieve all amenities) and POST (create new amenity) requests"""
+    if request.method == 'GET':
+        # Retrieve all amenities and return in JSON format
+        all_amenities = [amenity.to_dict() for amenity in storage.all('Amenity').values()]
+        return jsonify(all_amenities)
+    elif request.method == 'POST':
+        # Create a new amenity based on POST data in JSON format
+        request_data = request.get_json()
+        if request_data is None or type(request_data) != dict:
+            return jsonify({'error': 'Invalid JSON'}), 400
+        elif 'name' not in request_data:
+            return jsonify({'error': 'Missing name parameter'}), 400
+        new_amenity = Amenity(**request_data)
+        new_amenity.save()
+        return jsonify(new_amenity.to_dict()), 201
+
+# Endpoint to handle GET, PUT, and DELETE requests for a specific amenity by ID
+@app_views.route('/amenities/<string:amenity_id>', methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def get_put_delete_amenity(amenity_id):
+    """Handles GET (retrieve), PUT (update), and DELETE (remove) requests for a specific amenity"""
+    amenity = storage.get('Amenity', amenity_id)
+    if amenity is None:
+        abort(404)  # Return 404 if amenity with given ID doesn't exist
+    elif request.method == 'GET':
+        # Return details of the amenity in JSON format
+        return jsonify(amenity.to_dict())
+    elif request.method == 'DELETE':
+        storage.delete(amenity)  # Delete the specified amenity
+        storage.save()  # Save changes
+        return jsonify({}), 200  # Return empty JSON with 200 status after deletion
+    elif request.method == 'PUT':
+        # Update attributes of the amenity based on PUT data in JSON format
+        put_data = request.get_json()
+        if put_data is None or type(put_data) != dict:
+            return jsonify({'error': 'Invalid JSON'}), 400
+        for key, value in put_data.items():
+            if key != 'id' and key != 'created_at' and key != 'updated_at':
+                setattr(amenity, key, value)
+        storage.save()  # Save changes
+        return jsonify(amenity.to_dict()), 200  # Return updated amenity details
