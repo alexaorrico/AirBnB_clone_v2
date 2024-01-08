308fb399cecbeacc0c487d7cfd4e83f4dd3a7b1f
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-08 22:44:54 +0300

    set us free

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -9,10 +9,11 @@ from models import storage
 from models.place import Place
 from models.city import City
 from models.user import User
+from models.state import State
+from models.amenity import Amenity
 
 
-@app_views.route(
-        '/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
 def get_places_by_city(city_id):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
@@ -22,8 +23,7 @@ def get_places_by_city(city_id):
     return jsonify(places)
 
 
-@app_views.route(
-        '/places/<place_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
 def get_place(place_id):
     """Retrieves a Place object"""
     place = storage.get(Place, place_id)
@@ -32,8 +32,7 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route(
-        '/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
@@ -44,8 +43,7 @@ def delete_place(place_id):
     return jsonify({})
 
 
-@app_views.route(
-        '/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
 def create_place(city_id):
     """Creates a Place"""
     city = storage.get(City, city_id)
@@ -69,8 +67,7 @@ def create_place(city_id):
     return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route(
-        '/places/<place_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """Updates a Place object"""
     place = storage.get(Place, place_id)
@@ -86,3 +83,46 @@ def update_place(place_id):
             setattr(place, key, value)
     place.save()
     return jsonify(place.to_dict())
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def places_search():
+    """Searches for places based on the criteria provided."""
+    data = request.get_json()
+
+    if not data:
+        abort(400, "Not a JSON")
+
+    states = data.get("states", [])
+    cities = data.get("cities", [])
+    amenities = data.get("amenities", [])
+
+    all_places = storage.all(Place).values()
+    places = []
+
+    if not states and not cities and not amenities:
+        places = all_places
+    else:
+        for state_id in states:
+            state = storage.get(State, state_id)
+            if state:
+                for city in state.cities:
+                    places.extend(city.places)
+
+        for city_id in cities:
+            city = storage.get(City, city_id)
+            if city and city not in places:
+                places.extend(city.places)
+
+        if amenities:
+            amenities = [storage.get(Amenity, a_id) for a_id in amenities]
+            places = [
+                place for place in places
+                if all(amenity in place.amenities for amenity in amenities)
+            ]
+
+    return jsonify([place.to_dict() for place in places])
+
+
+if __name__ == "__main__":
+    app_views.run(host="0.0.0.0", port=5000)
