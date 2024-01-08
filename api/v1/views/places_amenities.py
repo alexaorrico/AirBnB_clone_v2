e8e88643bf5e03557c2426b40c940f71fb3641c7
Abokiey <sweneysam@gmail.com>
2024-01-08 18:42:14 +0300

    amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -10,6 +10,7 @@ from api.v1.views import app_views
 @app_views.route('/places/<place_id>/amenities', methods=['GET'],
                  strict_slashes=False)
 def get_amenities_by_place(place_id):
+    """get amenities by place id"""
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
@@ -26,6 +27,7 @@ def get_amenities_by_place(place_id):
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
 def delete_amenity_from_place(place_id, amenity_id):
+    """delete amenities by place id and amenity id"""
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
 
@@ -48,6 +50,7 @@ def delete_amenity_from_place(place_id, amenity_id):
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
                  strict_slashes=False)
 def link_amenity_to_place(place_id, amenity_id):
+    """update amenities by place id and amenity id"""
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
 
