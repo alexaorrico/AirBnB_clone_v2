04ab69dde54d59ed3f6abb5a47c389447b84e55a
itsoluwatobby <oluwasamuel050@gmail.com>
2024-01-08 13:31:43 +0100

    task 12 concerning CORS completed and also corrected some of the errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, resources={"api/*": {"origin": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
