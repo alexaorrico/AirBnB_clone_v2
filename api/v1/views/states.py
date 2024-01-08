1fa8f7154faecbc1b206cd28a5e31074d8151a93
TechieGold <techiegold101@gmail.com>
2024-01-08 13:44:47 +0100

    implement pycodestyle

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,12 +7,13 @@ from flask import jsonify, request, abort
 from models import storage
 from models.state import State
 
-@app_views.route("/states", methods=['GET'], strict_slashes=False)
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    """ Retrieves the list of all State objects """
-    all_state = storage.all(State).values()
-    for state in all_state:
-        return jsonify(state.to_dict())
+    """ Retrieve the list of all State objects """
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
 
 @app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
 def get_state(state_id):
@@ -22,7 +23,9 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route("/states/<state_id>", methods=['DELETE'], strict_slashes=False)
+
+@app_views.route("/states/<state_id>", methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """ Deletes a State object """
     state = storage.get(State, state_id)
@@ -45,6 +48,7 @@ def create_state():
     new_state.save()
     return jsonify(new_state.to_dict()), 201
 
+
 @app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """ Updates a State object """
@@ -55,7 +59,7 @@ def update_state(state_id):
     if not data:
         abort(400, description='Not a JSON')
     ignore = ['id', 'created_at', 'updated_at']
-    for key,value in data.items():
+    for key, value in data.items():
         if key not in ignore:
             setattr(state, key, value)
     storage.save()
