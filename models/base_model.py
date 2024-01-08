3e0692c20bef4709070088e702f2d9f160d1d5f2
Charaf eddahhani <125513039+HONORPRIDE@users.noreply.github.com>
2024-01-08 18:25:42 +0100

    handle the exclusion of the 'password' key based on the storage type

--- models/base_model.py
+++ models/base_model.py
@@ -68,6 +68,10 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if exclude_password and 'password' in new_dict and models.storage_t != 'db':
+            del new_dict['password']
+
+        new_dict["__class__"] = self.__class__.__name__
         return new_dict
 
     def delete(self):
