9ed4bb3f0b2cfc4c25094a7063be423482eb1940
KelvinoKing <kelvinoking@outlook.com>
2024-01-08 16:46:57 +0000

    Place

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,6 +5,9 @@ from flask import jsonify, abort, request
 from models import storage
 from models.place import Place
 from models.city import City
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
 from api.v1.views import app_views
 
 
