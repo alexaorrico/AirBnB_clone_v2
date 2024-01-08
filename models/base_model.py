4d4afe1bb6301f58db13acc92c4f37c8021909ea
Charaf eddahhani <125513039+HONORPRIDE@users.noreply.github.com>
2024-01-08 18:16:34 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -58,17 +58,19 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-        new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        return new_dict
+    def to_dict(self, exclude_password=True):
+    """returns a dictionary containing all keys/values of the instance"""
+    new_dict = self.__dict__.copy()
+    if "_sa_instance_state" in new_dict:
+        del new_dict["_sa_instance_state"]
+    if "created_at" in new_dict:
+        new_dict["created_at"] = new_dict["created_at"].strftime(time)
+    if "updated_at" in new_dict:
+        new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+    if exclude_password and 'password' in new_dict:
+        del new_dict['password']
+    new_dict["__class__"] = self.__class__.__name__
+    return new_dict
 
     def delete(self):
         """delete the current instance from the storage"""
