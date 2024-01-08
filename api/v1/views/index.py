43eb8a10593906a410f7f47db3aa9e8948547d06
golphat1 <golphat@yahoo.com>
2024-01-08 02:23:43 -0800

    ap

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,30 +5,19 @@
         /status:    displays"status":"OK"
         /stats:     displays total for all classes
 '''
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
 
-
-@app_views.route("/status")
-def status():
-    '''
-        return JSON of OK status
-    '''
-    return jsonify({'status': 'OK'})
-
-
-@app_views.route("/stats")
-def storage_counts():
-    '''
-        return counts of all classes in storage
-    '''
-    cls_counts = {
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieve the number of each object by type"""
+    stats = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
-        "users": storage.count("User")
+        "users": storage.count("User"),
     }
-    return jsonify(cls_counts)
+    return jsonify(stats)
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -23,7 +23,7 @@ class TestAmenity(unittest.TestCase):
         """Test that Amenity has attribute name, and it's as an empty string"""
         amenity = Amenity()
         self.assertTrue(hasattr(amenity, "name"))
-        if models.storage_type == 'db':
+        if hasattr(models, 'storage_type') and models.storage_type == 'db':
             self.assertEqual(amenity.name, None)
         else:
             self.assertEqual(amenity.name, "")
@@ -36,7 +36,7 @@ class TestAmenity(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in am.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
