1666c97e49fba5ff2d4750433fad96da82632d5a
Norberts <mainanorbert@gmail.comm>
2024-01-08 22:08:47 +0300

    users api

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -20,3 +20,46 @@ def get_list_od_users(user_id=None):
             abort(404)
         else:
             return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', strict_slashes=False,
+                 methods=['DELETE'])
+def del_user_based_on_id(user_id):
+    """ deletes a user object based on id given"""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return (jsonify({})), 200
+
+
+@app_views.route('/users', strict_slashes=False, methods=['POST'])
+def add_user_to_db():
+    """ adds new user to db """
+    json_data = request.get_json()
+    if not js_data:
+        abort(400, 'Not a JSON')
+    if 'email' not in json_data:
+        return jsonify({"error": "Missing email"}), 400
+    if 'password' not in json_data:
+        return jsonify({"error": "Missing password"}), 400
+    user = User(**json_data)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', strict_slashes=False, methods=['PUT'])
+def update_user_in_db(user_id):
+    """ updates a user based on id """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    json_data = request.get_json()
+    if not json_data:
+        abort(400, 'Not a JSON')
+    for k, v in json_data.items():
+        if k not in ["id", "email", "created_at", "updated_at"]:
+            setattr(user, k, v)
+    user.save()
+    return jsonify(user.to_dict()), 200
