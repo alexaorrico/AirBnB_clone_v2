82f1bbdb236778af99754a76b62d8c0e8bc3c80d
Jukunye <jukunyes@gmail.com>
2024-01-08 23:16:30 +0300

    arrange import in alphabetic order

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,17 +6,10 @@ from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.user import User
-from models.place import Place
-from models.review import Review
-
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
 from api.v1.views.amenities import *
-from api.v1.views.users import *
+from api.v1.views.cities import *
+from api.v1.views.index import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
