262399be301a31a87f72cc104c4dbf704a967434
TechieGold <techiegold101@gmail.com>
2024-01-09 00:58:01 +0100

    delete pycache files

--- api/v1/app.py
+++ api/v1/app.py
@@ -35,3 +35,4 @@ if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', '5000'))
     app.run(host, port, threaded=True)
+    app.run(debug=True)
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Objects that handles all default RESTful API actions
-"""
+""" Objects that handles all default RESTful API actions """
 from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models import storage
