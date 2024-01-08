5bcb37eb2c382a0085315e03ca0df8c5a5ed8761
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-01-08 12:18:04 +0100

    ok

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,10 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
-def index():
+def status_api():
+    """home screen of the app"""
     return jsonify({"status": "OK"})
