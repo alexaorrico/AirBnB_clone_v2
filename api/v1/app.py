a8614bc1ca5718aa9590aef02cbade1fd0a4911f
Nmcleon <nmcleon10@gmail.com>
2024-01-08 15:55:25 -0800

    test

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Created on Tue Sep  1 14:42:23 2020
-@authors: Robinson Montes
-          Mauricio Olarte
-          """
-          from os import getenv
-          from flask import Flask, jsonify, Blueprint
-          from models import storage
-          from api.v1.views import app_views
-          from flask_cors import CORS
+Status of your API
+setting up a Flask API with specific endpoints
+"""
 
-          app = Flask(__name__)
-          CORS(app, origins="0.0.0.0")
-          app.register_blueprint(app_views)
-          CORS(app_views)
+from flask import Flask, jsonify, Blueprint
+from models import storage
+from api.v1.views import app_views
+from flask_cors import CORS
+import os
 
+app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 
-          @app.teardown_appcontext
-          def close_db_sesion(error):
-                  """ this for slash routing"""
-                      storage.close()
+app.register_blueprint(app_views)
 
 
-                      @app.errorhandler(404)
-                      def page_not_found(e):
-                              """handler for 404 errors that returns a JSON-formatted
-                                  404 status code response.
-                                      """
-                                          return ({'error': 'Not found'}), 404
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close SQLAlchemy session"""
+    storage.close()
 
 
-                                      if __name__ == "__main__":
-                                              HBNB_API_HOST = getenv('HBNB_API_HOST')
-                                                  HBNB_API_PORT = getenv('HBNB_API_PORT')
-                                                      app.run(host=HBNB_API_HOST, port=HBNB_API_PORT,
-                                                                          threaded=True, debug=True)
+@app.errorhandler(404)
+def page_not_found(error):
+    """Handler for 404 errors."""
+    return jsonify({'error': 'Not found'}), 404
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port,
+            threaded=True, debug=True)
