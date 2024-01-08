b4dbc1b53c25b8a2495da8c89b9768e94ad291ea
Andrew <m.maina.andrew@gmail.com>
2024-01-08 07:15:06 +0000

    Add: CORS configurations to API
    - Added CORS to allow client applicatins to request resources from the
    API.
    - Allowed origin '0.0.0.0' for resources using the regex pattern "/*"

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,13 @@
 import os
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 from api.v1 import app_views
 from models import storage
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
