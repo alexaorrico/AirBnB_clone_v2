a8edb1ab72a5bbb0e0751423ef956108908ea5d0
Ayo <talktojmcvibes@gmail.com>
2024-01-08 12:21:54 -0800

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,4 +6,17 @@ from flask import jsonify
 
 @app_views.route('/status')
 def status():
+    """return json object"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stat_count():
+    """Returns json object of
+    each class"""
+    return jsonify({'amenities': storage.count('Amenities'),
+                    'cities': storage.count('Cities'),
+                    'places': storage.count('Places'),
+                    'reviews': storage.count('Reviews'),
+                    'states': storage.count('States'),
+                    'users': storage.count('Users')})
