a0d9cacd69f072183b636363b9f1cfb4821fb5d9
HazelDaniel <hazeldaniel856@gmail.com>
2024-01-08 10:51:09 +0100

    #added the route handler (view) for the cities endpoint to support all available request types . (issue: the PUT is not persistent)

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,4 +5,5 @@ from flask import Blueprint
 app_views = Blueprint("app_views", __name__,
                       url_prefix="/api/v1")
 from api.v1.views.index import *
-from api.v1.views.state import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
