5d8b200a79db4d2de364ff6f556a0bd406f5717c
mefdev <mohamedmeftah2018@gmail.com>
2024-01-08 23:19:31 +0100

    Use hashing for password

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,8 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import hashlib
+
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -65,6 +67,14 @@ class BaseModel:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        if "password" in new_dict and models.storage_t == "db":
+            del new_dict["password"]
+            new_dict["password"] = hashlib.md5(
+                new_dict["password"].encode()).hexdigest()
+        if "password" in new_dict and models.storage_t == "file":
+            new_dict["password"] = hashlib.md5(
+                new_dict["password"].encode()).hexdigest()
+
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
