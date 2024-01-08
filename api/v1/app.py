d044cd9d9c646ad5adf5669b515c9d9d5a9d6dd5
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-01-08 22:46:02 +0300

    page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ this module is to use CORs for api testing
 """
 from api.v1.views import app_views
 import sys
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 
 
@@ -16,6 +16,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Calls storage.close() upon teardown."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors and returns a JSON-formatted response."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
