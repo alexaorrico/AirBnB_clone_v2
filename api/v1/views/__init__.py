d1048ebeec74ebecdc1377c764147d8b105f4e89
TechieGold <techiegold101@gmail.com>
2024-01-08 14:57:15 +0100

    implement pycodestle

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,7 @@
 """create views"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views',
-                      __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.states import *
