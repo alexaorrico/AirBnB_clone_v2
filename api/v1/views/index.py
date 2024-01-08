bc25cbf5151563649fd802f73c769ae58508af72
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-08 15:41:54 +0300

    task 4 adding stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,13 +1,45 @@
 #!/usr/bin/python3
-"""Defining the modulee"""
+"""
+Flask route that returns json status response
+"""
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
+from models import storage
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Method that returns the status"""
+    """
+    function for status route that returns the status
+    """
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
 
-    return jsonify({
-                    "status": "OK"
-                    })
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(eval(key))
+        return jsonify(response)
