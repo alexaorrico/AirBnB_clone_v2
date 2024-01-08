89e26a9a27da995a45ff3ae1b226f940c83a1eb0
Abokiey <sweneysam@gmail.com>
2024-01-08 15:39:01 +0300

    passwd

--- models/base_model.py
+++ models/base_model.py
@@ -68,6 +68,9 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        if without_pwd:
+            new_dict.pop("password", None)
+
         return new_dict
 
     def delete(self):
