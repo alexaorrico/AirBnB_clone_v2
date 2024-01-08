c3b5eec5ebd19c39f2424bd88765adac8313637f
Josh-dev69 <ebubejoshua69@gmail.com>
2024-01-08 10:52:39 +0100

    404 page

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,19 +7,25 @@ and sets up the teardown_appcontext to close the database connection.
 
 """
 
-from flask import Flask
+from flask import Flask, Blueprint, jsonify
 from models import storage
 import os
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
-    """Teardown appcontext method to close the database connection."""
+    """Close the storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 errors and returns a JSON-formatted 404 response."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
