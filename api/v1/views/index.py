80498ffa43cf7529eb4ae65d01a573d69a00c093
Tamara Lumumba <tlumumba@gmail.com>
2024-01-08 17:42:17 +0000

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify
 #from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status')
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
