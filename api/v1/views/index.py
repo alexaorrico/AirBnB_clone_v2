cf11c7f502773306a0ba7ef23001fd2a241b25e8
Justine-ini <inijustine4040@gmail.com>
2024-01-08 06:28:36 -0800

    Added request method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def get_status():
     '''Gets the status of the API.
     '''
