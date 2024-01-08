3ef7bed3a66826a0f8ed0ae652e5c51ea989f4fa
castykawira <murithicasty@gmail.com>
2024-01-08 06:34:39 -0800

    question 3

---
+++ api/v1/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This is the main application file for the API"""
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
+from os import getenv
+from flasgger import Swagger
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Close storage session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors"""
+    return jsonify({'error': 'Not found'}), 404
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone RESTful API',
+    'uiversion': 3}
+
+Swagger(app)
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000'))
+    app.run(host, int(port), threaded=True)
