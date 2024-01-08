80fd81b3ae76ce855476fee67b1a0636c488d99c
JuneMbuvi <mbuvimbatha@gmail.com>
2024-01-08 03:36:34 -0800

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,12 +8,7 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
-    response = {
-            'status': 'OK'
-            }
-    formatted_json = json.dumps(response, indent=2)
-    print(formatted_json)
-    return jsonify(response)
+    return jsonify(status="OK")
 
 
 @app_views.route('/stats', methods=['GET'])
