1c6a6f4fc05ef08865bd3d4075958c6a03f2fc33
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-01-08 03:57:07 +0300

    Q3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,7 @@
 create a variable app_views which is an instance of Blueprint
 (url prefix must be /api/v1)
 """
-import Blueprint from flask
+from flask import Blueprint
 
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
