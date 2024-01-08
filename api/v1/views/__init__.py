8721aa60f8d2d360d91c4e1c3b42d58cf6bf5878
hordunlarmy <hordunlarmy@gmail.com>
2024-01-08 23:26:40 +0000

    Add routes for place amenities in views/__init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,7 +10,7 @@ from api.v1.views.index import *  # noqa
 from api.v1.views.amenities import *  # noqa
 from api.v1.views.cities import *  # noqa
 from api.v1.views.places import *  # noqa
-# from api.v1.views.places_amenities import *  # noqa
+from api.v1.views.places_amenities import *  # noqa
 from api.v1.views.places_reviews import *  # noqa
 from api.v1.views.states import *  # noqa
 from api.v1.views.users import *  # noqa
--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,1 +1,57 @@
+#!/usr/bin/python3
+"""
+contains endpoints(routes) for place_amenities objects
+"""
+import os
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
 
+
+@app_views.route("/places/<string:place_id>/amenities", strict_slashes=False)
+def get_amenities(place_id):
+    """
+    Retrieves the list of all amenities objects of a place
+    """
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenities = [obj.to_dict() for obj in place.amenities]
+    return jsonify(amenities)
+
+
+@app_views.route("/places/<string:place_id>/amenities/<string:amenity_id>",
+                 strict_slashes=False, methods=['DELETE'])
+def del_amenity(place_id, amenity_id):
+    """
+    Deletes a amenity object from place
+    """
+
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if not place or not amenity:
+        abort(404)
+    if amenity not in place.amenities:
+        abort(404)
+    place.amenities.remove(amenity)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route("/places/<string:place_id>/amenities/<string:amenity_id>",
+                 strict_slashes=False, methods=['POST'])
+def create_amenity2(place_id, amenity_id):
+    """
+    Creates an amenity instance
+    """
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if not place or not amenity:
+        abort(404)
+    if amenity in place.amenities:
+        return make_response(jsonify(amenity.to_dict()), 200)
+    place.amenities.append(amenity)
+    storage.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
