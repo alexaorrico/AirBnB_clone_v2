a9dfe9a3cb09eae37790d9f5510f5419d9adb919
kdshola <khidirshola@gmail.com>
2024-01-09 10:15:53 +0100

    update base model

--- models/base_model.py
+++ models/base_model.py
@@ -60,7 +60,7 @@ class BaseModel:
 
     def to_dict(self, disk_save=False):
         """returns a dictionary containing all keys/values of the instance"""
-        new_dict = self.__dict__.copy()i
+        new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
@@ -68,8 +68,6 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if not disk_save and new_dict["__class__"] is 'User':
-            new_dict.pop('_password', None)
         return new_dict
 
     def delete(self):
