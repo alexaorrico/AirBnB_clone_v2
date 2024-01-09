f66afdaaafe0f3f410dce54f45adeb498583c2dd
bennet489 <bennetasare@outlook.com>
2024-01-08 19:16:58 -0500

    Revert "Fix pycodestyle for __init.py__"
    This reverts commit 186efaf9e9c97467ba01fedd445c38dc12e3f5d2.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """create views"""
-import api.v1.views.index import *
-import api.v1.views.states import *
-import api.v1.views.cities import *
-import api.v1.views.amenities import *
-import api.v1.views.users import *
-import api.v1.views.places import *
-
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
