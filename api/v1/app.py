fc11a1bb43d102beba95546481e1b1d10c3d7cf1
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-08 19:00:18 -0800

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """Returns the status as JSON."""
     return jsonify({"status": "OK"})
