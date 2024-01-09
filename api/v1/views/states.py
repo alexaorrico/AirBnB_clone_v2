100c2cbe65b02bbd42d95d1f4eee4d3770a50e10
Mahama Hamida <mahamahamida@gmail.com>
2024-01-09 01:55:17 +0000

    fix: pycodestyle fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -20,7 +20,8 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """deletes state by id"""
     state = storage.get(State, state_id)
