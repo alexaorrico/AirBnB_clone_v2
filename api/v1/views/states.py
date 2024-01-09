bf84bb440a77d25deca546c619a03e9d4d207feb
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-09 02:06:29 -0800

    states.py file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -21,17 +21,17 @@ from models.state import State
 from api.v1.app import not_found
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__)
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects."""
     states = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(states)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/api/v1/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """Retrieves a State object by state_id."""
     state = storage.get(State, state_id)
@@ -40,7 +40,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'],
+@app_views.route('/api/v1/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object by state_id."""
@@ -52,7 +52,7 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route('/api/v1/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Creates a new State."""
     data = request.get_json()
@@ -65,7 +65,7 @@ def create_state():
     return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/api/v1/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Updates a State object by state_id."""
     state = storage.get(State, state_id)
