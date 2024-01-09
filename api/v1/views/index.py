20c2f9a7ba8f5221daab8b3dc00b3afad4697254
Willmuseve <willmuseve@gmail.com>
2024-01-08 17:32:13 -0800

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+has all needed dependencies
+"""
+from models import storage
+from flask import Flask
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """
+    returns a json "status":"ok"
+    """
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def count():
+    """
+    Retrieves the objects type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
