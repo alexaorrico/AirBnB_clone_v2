f9075fa66947c945d4b65c44bea0da64c10819d4
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-09 15:53:14 +0100

    improved storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """returns the object based on the class and it's id"""
+        all_data = self.all(cls)
+        for key, value in all_data.items():
+            if id in key:
+                return value
+        return None
+
+    def count(self, cls=None):
+        """count the number of cls in storage"""
+        all_data = self.all(cls)
+        count = 0
+        for data in all_data:
+            count += 1
+        return count
