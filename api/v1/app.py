0cfec44389d9dd43539aba3991eb3490f248f5a9
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-09 08:26:06 +0300

    add

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-Flask App that integrates with AirBnB static HTML Template
+Flask App that integrates with AirBnB static HTML Template.
+This module sets up a Flask application with CORS configuration.
 """
+
+# Importing necessary modules and packages
 from api.v1.views import app_views
-from flask import Flask, jsonify, make_response, render_template, url_for
-from flask_cors import CORS
+from flask import Flask, jsonify, make_response, render_template
+from flask_cors import CORS  # Import CORS module
 from flasgger import Swagger
 from models import storage
 import os
@@ -14,6 +17,9 @@ from werkzeug.exceptions import HTTPException
 app = Flask(__name__)
 swagger = Swagger(app)
 
+# Global CORS Configuration: Allow /* for 0.0.0.0
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 # global strict slashes
 app.url_map.strict_slashes = False
 
@@ -21,29 +27,22 @@ app.url_map.strict_slashes = False
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 
-
-# Cross-Origin Resource Sharing
-cors = CORS(
-    app, resources={r"/*": {"origins": "0.0.0.0"}})  # Allow /* for 0.0.0.0
-
 # app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
-
-# begin flask page rendering
+# Teardown context function
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
+    After each request, this method calls .close() on the current SQLAlchemy Session.
     """
     storage.close()
 
-
+# Global error handler function
 @app.errorhandler(Exception)
 def global_error_handler(err):
     """
-        Global Route to handle All Error Status Codes
+    Global Route to handle All Error Status Codes.
     """
     if isinstance(err, HTTPException):
         if type(err).__name__ == 'NotFound':
@@ -55,20 +54,19 @@ def global_error_handler(err):
         code = 500
     return make_response(jsonify(message), code)
 
-
+# Setup global errors
 def setup_global_errors():
     """
-    This updates HTTPException Class with custom error function
+    Update HTTPException Class with custom error function.
     """
     for cls in HTTPException.__subclasses__():
         app.register_error_handler(cls, global_error_handler)
 
-
 if __name__ == "__main__":
     """
-    MAIN Flask App
+    MAIN Flask App.
     """
-    # initializes global error handling
+    # Initializes global error handling
     setup_global_errors()
-    # start Flask app
+    # Start Flask app
     app.run(host=host, port=port)
