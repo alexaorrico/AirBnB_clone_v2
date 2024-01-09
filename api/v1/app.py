7e2f578c8b6bfe25861855f586c93a841b8eb2e6
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-08 17:58:57 -0800

    Status of the API

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Closes the storage on teardown."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
