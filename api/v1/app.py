a04e0ee84ce64b0fbc89f5ea4d1cf585adabcb5e
Menna Hassan <h.menna85@gmail.com>
2024-01-09 02:40:57 +0200

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def close_storage(exception=None):
     storage.close()
