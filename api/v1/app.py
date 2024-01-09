0e5e895ab7130a6ce10c866bdfc9576ad11e10d9
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-09 04:14:19 -0800

    app.py file

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,8 @@ Functions:
 """
 
 import os
-from flask import Flask, jsonify
+from flask import Flask
+from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -21,12 +22,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 
 
-app.register_blueprint(app_views)
-
-
-def teardown_appcontext(exception):
-    """Closes the storage on teardown."""
-    storage.close()
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
