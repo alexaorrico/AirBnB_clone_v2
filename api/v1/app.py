e52c2b321881867cb601201aae6d9c92b6ba86ef
Menna Hassan <h.menna85@gmail.com>
2024-01-09 03:41:03 +0200

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,13 +5,15 @@ import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
-def pagenotfound(e):
+def page_not_found(e):
     return {"error": "Not found"}, 404
 
 
@@ -32,7 +34,7 @@ else:
     host = "0.0.0.0"
 
 if os.getenv("HBNB_API_PORT"):
-    port = int(os.getenv("HBNB_API_PORT "))
+    port = int(os.getenv("HBNB_API_PORT"))
 else:
     port = 5000
 
