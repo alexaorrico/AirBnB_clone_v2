b1064aa5a2524a2c76b295768df598c88623fcc4
castykawira <murithicasty@gmail.com>
2024-01-08 18:53:53 -0800

    updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,4 +20,9 @@ def count():
     """
     Return JSON stats response
     """
-    return jsonify({"states": storage.count("State")})
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
