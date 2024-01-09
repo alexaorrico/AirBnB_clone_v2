4ffebdd8116964a95cb267fe0da3303c9d9e59a4
castykawira <murithicasty@gmail.com>
2024-01-08 16:44:12 -0800

    updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""This module defines routes related to the status of the API"""
+"""
+This module defines routes related to the status of the API
+"""
 from api.v1.views import app_views
-from flask import jsonify
-from models import storage
 from flask import Flask
+from models import storage
+from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """Return JSON status response"""
-    statuse = {"status": "OK"}
-    return jsonify(statuse)
+    """
+    Return JSON status response
+    """
+    return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
 def count():
-    """Return JSON stats response"""
-    stats_data = {
-            'amenities': storage.count('Amenity'),
-            'cities': storage.count('City'),
-            'places': storage.count('Place'),
-            'reviews': storage.count('Review'),
-            'states': storage.count('State'),
-            'users': storage.count('User')
-            }
-    return jsonify(stats_data)
+    """
+    Return JSON stats response
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
