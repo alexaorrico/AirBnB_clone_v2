226ad32cab7e8ecf52abc37314ed80b74ef8f66f
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-01-09 05:54:47 +0100

    places

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Create a new view for User object that handles all default RESTFul API actions
-"""
+""" objects that handle all default RestFul API actions for Users """
 from models.user import User
 from models import storage
 from api.v1.views import app_views
@@ -9,19 +7,22 @@ from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
 
-@app_views.route('/users', strict_slashes=False, methods='GET')
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 @swag_from('documentation/user/all_users.yml')
-def all_users():
-    """REtrieves all user from storage"""
-    users = storage.all(User).values()
-    ls_users = []
-    for user in users:
-        ls_users.append(user.to_dict())
-    return jsonify(ls_users)
+def get_users():
+    """
+    Retrieves the list of all user objects
+    or a specific user
+    """
+    all_users = storage.all(User).values()
+    list_users = []
+    for user in all_users:
+        list_users.append(user.to_dict())
+    return jsonify(list_users)
 
 
-@app_views.route('/users/<user_id>', strict_slashes=False, methods='GET')
-@swag_from('documentation/user/get_user.yml', methods='GET')
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/user/get_user.yml', methods=['GET'])
 def get_user(user_id):
     """ Retrieves an user """
     user = storage.get(User, user_id)
@@ -40,8 +41,10 @@ def delete_user(user_id):
     """
 
     user = storage.get(User, user_id)
+
     if not user:
         abort(404)
+
     storage.delete(user)
     storage.save()
 
