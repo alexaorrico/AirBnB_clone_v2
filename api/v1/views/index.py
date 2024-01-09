88bae7c4ccec782b1b3cd2710597149fb2974c72
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-09 17:03:49 +0100

    integrate api, status of my Api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""api index"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route("/status")
 def status():
+    """blueprints route"""
     return jsonify({"status": "OK"})
