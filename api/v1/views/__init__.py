290c038b1d199fd6779ee5be53f08c187ec90d27
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-09 20:41:17 +0100

    pushed init configuration

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """init file"""
-from flask import Blueprint
+from flask import Blueprint 
 
 
 app_views = Blueprint("init", __name__, url_prefix="/api/v1")
 
 
-from api.v1.views.index import *
+from api.v1.views.states import *
