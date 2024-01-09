7396088997b220a4b6980ec8edadb26c7d9b79be
castykawira <murithicasty@gmail.com>
2024-01-08 17:25:05 -0800

    updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 """
 This module defines routes related to the status of the API
 """
 from api.v1.views import app_views
-from flask import jsonify
 from models import storage
+from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,14 +15,21 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
+@app_views.route("/stats")
 def count():
     """
     Return JSON stats response
     """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    objs = {
+            'amenities': storage.count('Amenity'),
+            'cities': storage.count('City'),
+            'places': storage.count('Place'),
+            'reviews': storage.count('Review'),
+            'states': storage.count('State'),
+            'users': storage.count('User')
+            }
+    return jsonify(objs)
+
+
+if __name__ == "__main__":
+    pass
