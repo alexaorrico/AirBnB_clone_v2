e6c3d8e9f500eb6c63f6bd721efedad6822113ad
muriukialex-n <muriukialex.n@gmail.com>
2024-01-08 18:33:54 -0800

    sure

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,53 +1,24 @@
 #!/usr/bin/python3
-
-"""Creating index page"""
+""" API test stats"""
 from api.v1.views import app_views
-from flask import Flask, render_template, jsonify
+from flask import jsonify
 from models import storage
-from models.state import State
-from models.amenity import Amenity
-from models.place import Place
-from models.city import City
-from models.review import Review
-from models.user import User
 
 
-@app_views.route("/status", strict_slashes=False)
-def index():
-    """create a route /status on the object app_views that returns a JSON:"""
-    status_dict = {"status": "OK"}
-    return (jsonify(status_dict))
+@app_views.route('/status')
+def status():
+    """status"""
+    return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
+@app_views.route('/stats')
 def stats():
-    """Returns counts for objects"""
-    objs_dict = storage.all()
-    count_dict = {}
-
-    classes = [Amenity, City, Place, Review, State, User]
-
-    for i in classes:
-        count = storage.count(i)
-
-        if count == 0:
-            continue
-        if i == Amenity:
-            count_dict["amenities"] = count
-
-        elif i == City:
-            count_dict["cities"] = count
-
-        elif i == Place:
-            count_dict["places"] = count
-
-        elif i == Review:
-            count_dict["reviews"] = count
-
-        elif i == State:
-            count_dict["states"] = count
-
-        elif i == User:
-            count_dict["users"] = count
-
-    return (jsonify(count_dict))
+    """stats"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        })
