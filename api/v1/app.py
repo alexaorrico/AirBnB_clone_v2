29374684c4b843a147524eace4538a884950b5a9
Froches <michaelfavour418@gmail.com>
2024-01-09 09:26:40 -0800

    not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,3 +27,4 @@ if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True)
+    app.run(debug=True)
