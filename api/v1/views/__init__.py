3f7c85f2c94f345d9371b450c4d9a6dfd2bc4205
bennet489 <bennetasare@outlook.com>
2024-01-08 19:31:36 -0500

    pycode error fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """create views"""
+from flask import Blueprint
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.places import *
 from api.v1.views.users import *
-from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
