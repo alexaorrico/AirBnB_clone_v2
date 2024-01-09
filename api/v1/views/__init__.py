3c042c728ac693b36f4b52b5820428227f96d4ac
amoskarugo <karugoamos@gmail.com>
2024-01-10 00:21:08 +0300

    #

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """Initialize module"""
 
 from flask import Blueprint
-from api.v1.views.index import *
+from api.v1.views.ind import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
