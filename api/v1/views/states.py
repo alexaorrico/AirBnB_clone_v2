f507a2acd8b7810438dca6d17e01de4463b3a1a1
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-09 01:46:35 -0800

    states.py file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -15,7 +15,6 @@ Routes:
 
 from flask import Blueprint
 from flask import jsonify
-from flask import abort
 from flask import request
 from models import storage
 from models.state import State
@@ -35,7 +34,7 @@ def get_state(state_id):
     """Retrieves a State object by state_id."""
     state = storage.get(State, state_id)
     if state is None:
-        abort(404)
+        not_found(404)
     return jsonify(state.to_dict())
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
@@ -43,7 +42,7 @@ def delete_state(state_id):
     """Deletes a State object by state_id."""
     state = storage.get(State, state_id)
     if state is None:
-        abort(404)
+        not_found(404)
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
@@ -53,9 +52,9 @@ def create_state():
     """Creates a new State."""
     data = request.get_json()
     if not data:
-        abort(400, 'Not a JSON')
+        not_found(400, 'Not a JSON')
     if 'name' not in data:
-        abort(400, 'Missing name')
+        not_found(400, 'Missing name')
     new_state = State(**data)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
@@ -65,10 +64,10 @@ def update_state(state_id):
     """Updates a State object by state_id."""
     state = storage.get(State, state_id)
     if state is None:
-        abort(404)
+        not_found(404)
     data = request.get_json()
     if not data:
-        abort(400, 'Not a JSON')
+        not_found(400, 'Not a JSON')
     for key, value in data.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
