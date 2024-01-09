2e370b9979516d28cc6ab01ccfb081b9337204e7
castykawira <murithicasty@gmail.com>
2024-01-08 17:10:02 -0800

    updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,8 @@ This module defines routes related to the status of the API
 """
 from api.v1.views import app_views
 from flask import Flask
-from models import storage
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -16,7 +16,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def count():
     """
     Return JSON stats response
@@ -27,7 +27,3 @@ def count():
                     "reviews": storage.count("Review"),
                     "states": storage.count("State"),
                     "users": storage.count("User")})
-
-
-    if __name__ == "__main__":
-         pass
