eb15247fc545abe031c177f01af22646b87755c3
Engr Ugwu <fugwungene@gmail.com>
2024-01-09 15:47:11 +0000

    Add task 6
    add states.py file

---
+++ api/v1/views/states.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify, abort
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+@app_views.route("/api/v1/states", methods=["GET"], strict_slashes=False)
+def get_states():
+    states = request.get(storage.all(State).values())
+    return jsonify([state.to_dict() for state in states])
+
+
+@app_views.route("/api/v1/states/<state_id>", methods=["GET"], strict_slashes=False)
+def state_id(state_id):
+    state = storage.get(state, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.ti_dict())
+
+@app_views.route("/api/v1/states/<state_id>", methods=["DELETE"], strict_slashes=False)
+def delete_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
+
+@app_views.route("/api/v1/states", methods=["POST"], strict_slashes=False)
+def create_state():
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    if "name" not in date:
+        abort(400,"Missing name")
+    new_state = State(**data)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+@app_views.route("/api/v1/states/<state_id>", methods=["PUT"], strict_slashes=False)
+def update_state(state_id):
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(401)
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    for key, value in data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(State, key, value)
+    storage.save()
+    return jsonify(state.to_dict()), 200
