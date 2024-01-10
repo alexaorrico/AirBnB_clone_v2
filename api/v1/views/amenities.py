d1e5857b505fb4f8c94a1f1f0cb6fefbb1f40023
Froches <michaelfavour418@gmail.com>
2024-01-10 14:42:12 -0800

    amenities fixed

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -4,7 +4,8 @@ Amenities file
 """
 from flask import jsonify, abort, request
 from api.v1.views import app_views
-from models import storage, Amenity
+from models import storage
+from models.amenity import Amenity
 
 
 @app_views.route('/amenities', methods=['GET'])
