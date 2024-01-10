defc779ee9643e20e5d4fd99647ffe52f32d90e3
Willmuseve <willmuseve@gmail.com>
2024-01-09 19:02:04 -0800

    new

--- api/v1/app.py
+++ api/v1/app.py
@@ -26,4 +26,4 @@ def page_not_found(error):
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=Trueg)
