5474051776714cc7e9ef6d63d2d13fdfc7a9231a
Froches <michaelfavour418@gmail.com>
2024-01-10 02:22:23 -0800

    amenities

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,7 @@ app.register_blueprint(app_views)
 
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def handle_application(application):
     """handle application"""
