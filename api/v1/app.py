63190b9f96b57bc0e1100a0f9ebc729ec8b82c77
Linda Ihuoma Nwachukwu <102322910+Lyndha@users.noreply.github.com>
2024-01-10 15:38:22 +0100

    Error handling: 404 Not found  (#8)
    * Stats endpoint: retrieve objects count
    
    * Error status: error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """return the status of your API
 """
 
-from flask import Flask
+from flask import Flask, make_response
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -16,6 +16,13 @@ def teardown_appcontext(exception):
     """close storage
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """handles a JSON-formatted 404 status code response
+    """
+    return make_response({"error": "Not found"}, 404)
 
 
 if __name__ == "__main__":
