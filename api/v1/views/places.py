d112c3d17bbd4cee2c5f81e06d69d4d5552780d1
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-01-14 22:48:42 +0000

    TASK 15

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -79,3 +79,47 @@ def put_place(place_id):
             setattr(place, attr, val)
     place.save()
     return jsonify(place.to_dict())
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def place_search():
+    """retrieves all Place objects depending of the JSON
+    in the body of the request."""
+    body = request.get_json()
+    if not body:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    places = storage.all('Place').values()
+    if not len(places):
+        return jsonify([])
+    if not len(body) or not len([x for x in body if len(body[x])]):
+        return jsonify([x.to_dict() for x in places])
+
+    result = []
+    state_ids = body['states'] if body.get('states') else []
+    city_ids = body['cities'] if body.get('cities') else []
+    amenity_ids = body['amenities'] if body.get('amenities') else []
+
+    cities = []
+    if len(state_ids):
+        all_states = storage.all('State').values()
+        states = [x for x in all_states if x.id in state_ids]
+        for state in states:
+            cities.extend(state.cities)
+    if len(city_ids):
+        all_cities = storage.all('City').values()
+        _cities = [x for x in all_cities if x.id in city_ids]
+        _cities = [x for x in _cities if x not in cities]
+        cities.extend(_cities)
+
+    amenities = []
+    if len(amenity_ids):
+        all_amenities = storage.all('Amenity').values()
+        amenities = [x for x in all_amenities if x.id in amenity_ids]
+
+    for place in places:
+        place_amenities = place.amenities
+        if place.city_id in [x.id for x in cities]:
+            if all(x in place_amenities for x in amenities):
+                result.append(place.to_dict())
+
+    return jsonify(result)
