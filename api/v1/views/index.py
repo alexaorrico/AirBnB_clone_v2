0b872e09db066dfb726bb4de8324189c959589ca
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-16 05:26:04 -0800

    index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Module for defining API routes related to index.
+""" Module for defining API routes related to index.
 
-This module defines the API route for status and stats.
+    This module defines the API route for status and stats.
 
-Functions:
-    - get_status: Returns the status as JSON.
-    - get_stats: Retrieves the number of each object type.
-
+    Functions:
+        - get_status: Returns the status as JSON.
+        - get_stats: Retrieves the number of each object type.
 """
 
 from api.v1.views import app_views
@@ -16,13 +15,17 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
-    """Returns the status as JSON."""
+    """
+        Returns the status as JSON.
+    """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
-    """Retrieve the number of each object type."""
+    """
+        Retrieve the number of each object type.
+    """
     stats = {}
     classes = ["User", "State", "City", "Amenity", "Place", "Review"]
 
