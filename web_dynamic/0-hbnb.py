3b3be8b41bb92402f71102f72935fb693e0ea4cc
Olutayo0910 <olutayoogunlade0910@gmail.com>
2024-01-15 18:42:07 -0800

    task 1

---
+++ web_dynamic/0-hbnb.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Flask app to generate complete html page"""
+from flask import Flask, render_template
+from models import storage
+import uuid
+app = Flask('web_dynamic')
+app.url_map.strict_slashes = False
+
+
+@app.route('/0-hbnb')
+def display_hbnb():
+    """Generate page with popdown menu of states/cities"""
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    cache_id = uuid.uuid4()
+    return render_template('8-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
+
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Close database"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
