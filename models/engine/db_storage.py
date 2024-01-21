63177016c2fe2c7fc4317760f2428d3f42936ed0
victormc13 <viktorsb14@gmail.com>
2024-01-20 23:59:50 -0800

    update: Creates two new methods 'def get(self, cls, id)' and to retrieve one object and 'def count(self, cls=None)' to count number of objects in storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,25 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve an object"""
+        if cls is not None and type(cls) is str and id is not None and\
+           type(id) is str and cls in classes:
+            cls = classes[cls]
+            result = self.__session.query(cls).filter(cls.id == id).first()
+            return result
+        else:
+            return None
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        total = 0
+        if type(cls) == str and cls in classes:
+            cls = classes[cls]
+            total = self.__session.query(cls).count()
+        elif cls is None:
+            for cls in classes.values():
+                total += self.__session.query(cls)
+
+        return total
