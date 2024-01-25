ad870eda902eefa23ca53b2da3fbec43416af9a1
muriukialex-n <muriukialex.n@gmail.com>
2024-01-26 00:22:11 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python3
-'''A minimal Flask Application'''
+# web_flask/0-hello_route.py
 from flask import Flask
+
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    '''home'''
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
     return 'Hello HBNB!'
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
