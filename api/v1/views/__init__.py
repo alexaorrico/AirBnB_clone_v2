b439d18cf7c54572e5b331036bbb8cfbf933a8e7
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-02-01 23:30:20 +0000

    Adding RESTful actions for city objects

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,3 +13,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Import views from the 'index' module to include in the Blueprint
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
