3b79806e1e0154f95959b3d1a7b09bcbb48f4ff5
Dragonixj <delafiado@gmail.com>
2024-02-01 17:17:32 +0000

    working on task 3

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Starts a flask application
+"""
+import os
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exc):
+    """
+    method that calls storage.close()
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app_host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    app_port = int(os.getenv("HBNB_API_PORT", "5000"))
+    app.run(host=app_host, port=app_port, threaded=True)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ Contains the FileStorage class
 """
 
 import json
+
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -12,8 +13,15 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {
+    "Amenity": Amenity,
+    "BaseModel": BaseModel,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State,
+    "User": User,
+}
 
 
 class FileStorage:
@@ -45,13 +53,13 @@ class FileStorage:
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, 'w') as f:
+        with open(self.__file_path, "w") as f:
             json.dump(json_objects, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(self.__file_path, "r") as f:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
@@ -61,7 +69,7 @@ class FileStorage:
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
         if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
+            key = obj.__class__.__name__ + "." + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
@@ -71,26 +79,16 @@ class FileStorage:
 
     def get(self, cls, id):
         """Returns object based on class and its ID, or None if not found"""
-        if cls not in classes.values():
-            return None
+        if cls is not None:
+            res = list(
+                filter(lambda x: type(x) is cls and x.id == id, self.__objects.values())
+            )
+            if res:
+                return res[0]
 
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
-            if (value.id == id):
-                return value
-
-            return None
+        return None
 
     def count(self, cls=None):
         """Returns no of objects of class if found,
         if not return all objects of storage"""
-        all_classes = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_classes:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
-
-        return count
+        return len(self.all(cls))
