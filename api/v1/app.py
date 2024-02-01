112f66229beccef0f7318c71b28dda00496cfd1b
Ayomide Kayode <ayomidekay7@gmail.com>
2024-02-01 18:41:15 +0100

    Create handler for 404 errors.❌

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,12 +13,21 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """
     Closes the storage connection after each request
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_error404(error):
+    """
+    Handler for 404 errors
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
