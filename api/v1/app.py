dbf32a85aa70cc3817ea6dfedb364cc859d270ed
Ayomide Kayode <ayomidekay7@gmail.com>
2024-02-01 16:54:57 +0100

    Ensured that app_views is defined before importing views in index.py to address circular import issues.

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,8 +11,7 @@ import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 @app.teardown_appcontext
 def teardown(exception):
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,10 +4,11 @@
 """ Initialization of Flask Blueprint for API views
 """
 
-# Import views from the 'index' module to include in the Blueprint
-from api.v1.views.index import *
 from flask import Blueprint
 
 
 # Create a Blueprint named 'app_views' with a URL prefix '/api/v1'
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import views from the 'index' module to include in the Blueprint
+from api.v1.views.index import *
