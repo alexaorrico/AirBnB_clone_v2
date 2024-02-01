83ffe5ce73a3e0b7c74f4ac127bd01f013cda2bf
sarora2200 <sarora2200@gmail.com>
2024-02-01 10:31:07 -0800

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ Flask route that returns json status response
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status/")
@@ -12,3 +13,19 @@ def status():
     function for status route that returns the status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views_route("/stats")
+def stats():
+    """
+    Create an endpoint that retrieves the number of each objects by type
+    """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(stats)
