c830cf0db6fc40b61798bd2f98a651896b649268
Nkemjika1divine <cndivine@gmail.com>
2024-02-01 00:57:39 -0800

    new branch

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on class & its ID, or None if not found"""
+
