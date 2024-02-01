11ec1c46ce9576a3dd993b9a691a5fd3c1ec71a5
Nkemjika1divine <cndivine@gmail.com>
2024-02-01 01:40:09 -0800

    filesorage get() and count() fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,26 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on class & its ID, or None if not found"""
+        objs = self.all()
+        class_id = "{}.{}".format(cls, id)
+        if class_id in objs:
+            return objs[class_id]
+        else:
+            return
+
+    def count(self, cls=None):
+        """Counts the occurence of a class in the database"""
+        objs = self.all()
+        count = 0
+
+        if cls:
+            for key, value in objs:
+                obj_cls = key.split(".")[0]
+                if obj_cls == cls:
+                    count += 1
+            return count
+        else:
+            return len(objs)
