cd63ba9b204c20f012441c78ab89c8b0782cdbac
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-01 17:26:18 +0100

    task 2 : Improve storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except e:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,22 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID,
+        or None if not found
+        """
+        dict = self.all(cls)
+        for key, value in dict.items():
+            if (value.id == id):
+                return value
+
+        return None
+
+    def count(self, cls=None):
+        """counts the number of objects in storage"""
+        if not cls:
+            return len(self.all())
+
+        return len(self.all(cls))
