42bef276aa45c1d2ac3ff1d68fde8bb7aea8adc3
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-02-01 22:17:10 +0100

    task 4 :fire:

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,25 @@
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.get('/status')
 def status():
     """display, 'status': 'ok'"""
     return {'status': "ok"}
+
+
+@app_views.get('/stats')
+def stats():
+    """retrieves the number of each objects by type"""
+    classes = [Amenity, City, Place, Review, State, User]
+    stats = {}
+    for cls in classes:
+        stats[cls.__tablename__] = storage.count(cls)
+
+    return stats
