4fff91594971defe2132153ef733bb7bf5d8ea8a
Ayomide Kayode <ayomidekay7@gmail.com>
2024-02-01 22:53:42 +0100

    Created views for State objects to handle default RESTful API actions.🚧

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,3 +12,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Import views from the 'index' module to include in the Blueprint
 from api.v1.views.index import *
+from api.v1.views.states import *
