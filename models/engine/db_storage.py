03a898ee2dba61618f1b9c0c84b1ec9315de23d7
Muliro <mulirokhaemba@gmail.com>
2024-02-01 21:13:56 +0300

    db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,23 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+    
+    def get(self, cls, id):
+        '''get:
+        retrieve an object from the file storage by class and id.
+        '''
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+    
+    def count(self, cls=None):
+        '''count:
+        count the number of objects in storage matching the given class.
+        '''
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
