b4c581c686e79f4ffbb1bdc42d20fda8f986651f
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-02 18:37:17 +0100

    Add stats endpoint to return count of all objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,35 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models.state import State
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """ test status OK """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """ return the count of all objects"""
+    aminities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    return jsonify(
+        {"amenities": aminities,
+         "cities": cities,
+         "places": places,
+         "reviews": reviews,
+         "states": states,
+         "users": users}
+    )
