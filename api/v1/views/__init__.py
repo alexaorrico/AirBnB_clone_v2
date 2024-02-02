f46233af9a703352500e182d71a568d46ad63e3f
Muigai-ben <muigaibenson37@gmail.com>
2024-02-02 06:00:39 -0800

    Status of your API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Creates a Blueprint instance with `url_prefix` set to `/api/v1`."""
+
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
