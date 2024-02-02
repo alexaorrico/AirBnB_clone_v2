2577fc0395d71335051f11df3223ac226872e570
abdelhadia72 <thisisavnb@gmail.com>
2024-02-02 17:37:09 +0100

    Fixing pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,8 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
     """ test status OK """
-    return jsonify({ "status": "OK" })
+    return jsonify({"status": "OK"})
