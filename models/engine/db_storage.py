319af896d3ab05fffbe3ef5ed044c616d5676530
RevolverSnake412 <oussag@hotmail.fr>
2024-02-02 17:41:37 +0000

    Update DBStorage and FileStorage, adding two new methods.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieves one object"""
+        if cls is not None and id is not None:
+            obj = self.all(cls)
+            return obj.get("{}.{}".format(cls, id))
+        else:
+            return None
+
+    def count(self, cls=None):
+        """counts the number of objects in storage"""
+        return (len(self.all(cls)))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieves one object"""
+        if cls is not None and id is not None:
+            obj = self.all(cls)
+            return obj.get("{}.{}".format(cls, id))
+        else:
+            return None
+
+    def count(self, cls=None):
+        """counts the number of objects in storage"""
+        return (len(self.all(cls)))
