af797bcf4f760480256e7fc479aa494f73d32521
abdelhadia72 <thisisavnb@gmail.com>
2024-02-02 15:20:45 +0100

    storage_get_count

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieve an object from the database by class and id"""
+        return self.all(cls).get(cls.__name__ + '.' + id)
+
+    def count(self, cls=None):
+        """count the number of objects in the database"""
+        if cls:
+            return len(self.all(cls))
+        else:
+            return sum(len(self.all(c)) for c in classes.values())
+
