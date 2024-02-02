5490598699c9539140bb0fc9f959c2795668c818
Mohamed Ayman <mohayman2007@gmail.com>
2024-02-02 22:01:34 +0000

    0x05. AirBnB clone (RESTful API) - Adding the stats route and error 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 from os import getenv
 
-from flask import Flask
+from flask import Flask, jsonify
 
 from models import storage
 from api.v1.views import app_views
@@ -16,6 +16,12 @@ app.register_blueprint(app_views)
 def close(eexception):
     """Closes the storage"""
     storage.close()
+
+from flask import jsonify
+@app.errorhandler(404)
+def error_handler_404(error):
+    '''Returns the JSON {"error": "Not found"} if resource wasn't found'''
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
 """API main routes"""
 
-import json
+from flask import  jsonify
 
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """Returns the status in JSON format"""
-    return json.dumps({"status": "OK"})
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    stats_dict = {}
+    for cls_name, cls in classes.items():
+        stats_dict[cls_name] = storage.count(cls)
+    return jsonify(stats_dict)
+    
