db51e14159abc76fc176b5c363763fecfdbedb78
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-02-02 23:22:39 +0300

    .

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -78,14 +78,18 @@ class DBStorage:
     def get(self, cls, id):
         """Method to retrieve one object"""
         if cls and id:
-            key = f"{cls.__name__}.{id}"
-            return self.__objects.get(key, None)
+            try:
+                result = [v for k, v in self.all(cls).items() if k.split(".")[1] == id]
+                if result != []:
+                    return result[0]
+            except Exception:
+                pass
+        return None
 
     def count(self, cls=None):
         """Counts the number of objects in storage"""
         if cls:
-            return sum(1 for obj in self.__objects.values() if isinstance)
+            # return sum(1 for obj in self.__objects.values() if isinstance)
+            return len(self.all(cls))
         else:
-            return len(self.__objects)
-
-        # return len[v for k, v in self.all().items if k.split(" ")[1] == id]
+            return len(self.all())
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -72,14 +72,18 @@ class FileStorage:
     def get(self, cls, id):
         """Method to retrieve one object"""
         if cls and id:
-            key = f"{cls._name_}.{id}"
-            return self.__objects.get(key, None)
+            try:
+                result = [v for k, v in self.all(cls).items() if k.split(".")[1] == id]
+                if result != []:
+                    return result[0]
+            except Exception:
+                pass
+        return None
 
     def count(self, cls=None):
         """Counts the number of objects in storage"""
         if cls:
-            return sum(1 for obj in self.__objects.values() if isinstance)
+            # return sum(1 for obj in self.__objects.values() if isinstance)
+            return len(self.all(cls))
         else:
-            return len(self.__objects)
-
-        # return len[v for k, v in self.all().items if k.split(" ")[1] == id]
+            return len(self.all())
