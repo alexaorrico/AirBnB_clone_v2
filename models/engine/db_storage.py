11fe44d8409786005ee547fa32429e747cf19f1e
AnsaYong <ansahmbomyong@gmail.com>
2024-02-02 16:32:59 +0200

    added the get() method to the DBstorage class

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieves an object based on its class name and ID"""
+        return self.__session.query(cls).filter(cls.id)
