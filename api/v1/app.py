a33a030d477a5339dbf2bcd355c981c658d5112a
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-02-02 16:40:33 +0100

    task 3

---
+++ api/v1/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Starting Flask app """
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import environ
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Close Storage """
+    storage.close()
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
