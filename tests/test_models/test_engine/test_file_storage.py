723731cd6de4d8e32ed9bcceae8b7235dfb001b3
OthmanSALAHI <ootmansalhi.24@gmail.com>
2024-02-02 16:09:06 +0100

    Remove unnecessary imports from test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -115,14 +115,6 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-
 class TestFileStorageGet(unittest.TestCase):
     """Tests for the get method of FileStorage class"""
 
