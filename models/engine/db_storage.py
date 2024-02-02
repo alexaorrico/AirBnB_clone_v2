397946b34d05526508685e6f2a57c8e19d4e78c3
ZafCode14 <zafarwp14@gmail.com>
2024-02-02 08:11:14 +0200

    commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,15 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """retrieves one object"""
+        all_obj = self.all(cls)
+        for key, value in all_obj.items():
+            if key == "{}.{}".format(cls.__name__, id):
+                return value
+
+    def count(self, cls=None):
+        """counts the number of objects in storage"""
+        all_obj = self.all(cls)
+        return len(all_obj)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieves one object"""
+        all_obj = self.all(cls)
+        for key, value in all_obj.items():
+            if key == "{}.{}".format(cls.__name__, id):
+                return value
+
+    def count(self, cls=None):
+        """counts the number of objects in storage"""
+        all_obj = self.all(cls)
+        return len(all_obj)
