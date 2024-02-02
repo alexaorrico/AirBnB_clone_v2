a8ab06891d6b65b56d15df28872af3777fd23190
Ayomide Kayode <ayomidekay7@gmail.com>
2024-02-02 14:11:04 +0100

    Add CORS support to Flask application and allow request from any origin.🔗

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,13 @@
 
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 import os
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
