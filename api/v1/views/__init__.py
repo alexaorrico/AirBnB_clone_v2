5bbf1be90f0bb487e0968884b9ab75fd6eb04283
bakiizese <bereketzeselassie@gmail.com>
2024-02-02 20:58:44 -0800

    fix

--- api/v1/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-'''blueprint for api app'''
-
-
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
