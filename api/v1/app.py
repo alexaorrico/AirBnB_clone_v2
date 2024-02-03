fd97368a61533c51296de6c6869822efcc5de9f0
bakiizese <bereketzeselassie@gmail.com>
2024-02-02 20:53:31 -0800

    branch

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,4 +20,4 @@ def teardown(self):
 if __name__ == '__main__':
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = os.environ.get('HBNB_API_PORT', 5000)
-    app.run(host=host, port=int(port), threaded=True)
+    app.run(host=host, port=int(port), threaded=True, debug=1)
