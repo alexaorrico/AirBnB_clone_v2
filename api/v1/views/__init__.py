fc1af7f0de934b1564a259213e39eed8bde37b4b
Olakunle64 <olakunleisiaq@gmail.com>
2024-02-03 20:07:41 +0000

    add more tasks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,5 +3,10 @@
 
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# import all blueprints that uses the app_views
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,70 +2,66 @@
 """ This module contains a blue print for a web app """
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models import storage
 from models.state import State
 
-# print("debugging............................")
-@app_views.route('/states', methods=['GET'])
-def all_states_obj():
-    """ return the all state objects """
-    # print("debugging............................")
-    state_objects = storage.all(State)
-    object_list = []
-    for obj in state_objects.values():
-        object_list.append(obj.to_dict())
-    return jsonify(object_list)
+
+@app_views.route('/states/', methods=['GET', 'POST'])
+@app_views.route('/states', methods=['GET', 'POST'])
+def post_get_state_obj():
+    """ This function contains two http method handler
+
+        GET:
+            return the all state objects
+        POST:
+            create a new state object
+    """
+    if request.method == 'GET':
+        state_objects = storage.all(State)
+        object_list = []
+        for obj in state_objects.values():
+            object_list.append(obj.to_dict())
+        return jsonify(object_list)
+    elif request.method == 'POST':
+        try:
+            state_dict = request.get_json()
+        except Exception:
+            abort(400, description="Not a JSON")
+        if "name" not in state_dict:
+            abort(400, description="Missing name")
+        new_state = State(**state_dict)
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['GET'])
-def get_state_obj(state_id):
-    """ get the state object with the respective id """
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+def delete_put_get_state_obj(state_id):
+    """ This function contains two http method handler
+
+        GET:
+            get the state object with the respective id
+        DELETE:
+            delete the state object with the respective id
+        PUT:
+            update the state object with the respective id
+    """
     state = storage.get(State, state_id)
     if not state:
         abort(404)
-    else:
+    elif request.method == 'GET':
         return jsonify(state.to_dict())
-
-
-@app_views.route('/states/<state_id>', methods=['DELETE'])
-def delete_state_obj(state_id):
-    """ delete the state object with the respective id """
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    else:
+    elif request.method == 'DELETE':
         storage.delete(state)
         storage.save()
         return jsonify({}), 200
-
-
-@app_views.route('/states', methods=['POST'])
-def create_state_obj():
-    """ create a state object """
-    print(request.get_json())
-    try:
-        state_dict = request.get_json()
-    except Exception:
-        abort(400, description="Not a JSON")
-    if "name" not in state_dict:
-        abort(400, description="Missing name")
-    new_state = State(**state_dict)
-    return jsonify(new_state.to_dict()), 201
-
-
-@app_views.route('/states/<state_id>', methods=['PUT'])
-def update_state_obj(state_id):
-    """ update the state with the respective id """
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-    try:
-        state_dict = request.get_json()
-    except Exception:
-        abort(400, description="Not a JSON")
-    for key, value in state_dict.items():
-        if key != "id" or key != "created_at" or key != "updated_at":
-            setattr(state, key, value)
-    state.save()
-    return jsonify(state.to_dict()), 200
+    elif request.method == 'PUT':
+        try:
+            state_dict = request.get_json()
+            for key, value in state_dict.items():
+                if key != "id" and key != "created_at" and key != "updated_at":
+                    setattr(state, key, value)
+            state.save()
+            return jsonify(state.to_dict()), 200
+        except Exception:
+            abort(400, description="Not a JSON")
