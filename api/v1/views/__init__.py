7949bd5cfae0f977ba04d4b02a6eeb3b32731499
Etsybt <hmidouchnouhaila2@gmail.com>
2024-02-03 19:39:51 +0000

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,12 +5,11 @@ innit file
 
 
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 from api.v1.views import index
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
