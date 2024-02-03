62d8da3b1b50be1e071ce92a2ee0ff9ce8253e9f
esraaashraf023 <esraaashraf780567@gmail.com>
2024-02-03 06:34:42 +0200

    add file

---
+++ api/v1/index.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""this module handles status and stats routs"""
+
+from flask import jsonify
+from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
+@app_views.route('/status')
+def status():
+    """API"""
+    return jsonify({"status": "OK"}), 200
+
+@app_views.route('/stats')
+def stats():
+    """objects by type"""
+    classes = {"amenities": Amenity, "cities": City, "places": Place,
+            "reviews": Review, "states": State, "users": User}
+    count_dict = {}
+    for key, value in classes.items():
+        count_dict[key] = storage.count(value)
+    return jsonify(count_dict)
