14141a4b5856f4210a0724b4bcda12245f257012
Emedo586 <emedopeace99@gmail.com>
2024-02-03 14:21:09 -0800

    APIs

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Contains the blueprint for the API.'''
+# import Blueprint from flask
+from flask import Blueprint
+
+# create a variable app_views which is an instance of Blueprint
+# url prefix must be /api/v1
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
+
+
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.index import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
