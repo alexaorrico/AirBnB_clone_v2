651bbeedc7c03c6f75e7ba286d5a627a005d92cf
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-04 00:29:22 +0100

    creating status endpoint

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """Returns a JSON response with status OK"""
+    return jsonify({"status": "OK"})
