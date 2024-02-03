0839e93c2c92ab254bea1b38d89bb6b66aa76e26
AhmedEhab2022 <am8122004@gmail.com>
2024-02-03 12:56:32 -0800

    Add two methods in each file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,25 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class and its ID,
+        or None if not found
+        """
+
+        if cls and id:
+            objs_dict = self.all(cls)
+            for key, value in obj_dict.items():
+                if key.split(".")[1] == id:
+                    return value
+
+        return None
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage
+        """
+
+        return len(self.all(cls))
