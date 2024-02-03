45bc1ef7f3a6d750831c4470262b2378d9c74661
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-04 00:38:41 +0100

    task 4 => adding an endpoint that retrives the count of each object by its type

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """endpoint for staatus"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -13,6 +12,20 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """Closes storage"""
     storage.close()
+
+
+@app_views.route("/stats", strict_slashes=False, methods=["GET"])
+def stats():
+    """Retrieves the number of each object type"""
+    stats_dict = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(stats_dict)
 
 
 if __name__ == "__main__":
