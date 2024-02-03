ef2828ec6e4bd02284b64eb9a9c98aea8b44f81c
Ouazane <mohamedouazane@yahoo.com>
2024-02-03 03:01:34 -0800

    3. Status of your API

---
+++ api/v1/view/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views import states
+from api.v1.views import cities
+from api.v1.views import amenities
+from api.v1.views import users
