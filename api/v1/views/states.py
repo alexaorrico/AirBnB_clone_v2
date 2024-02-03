d79a0dad6152ad2811a864e812baeae67eeecc97
zaynom <ouaziz0626@gmail.com>
2024-02-03 08:59:50 +0000

    task 6: fixing State checker issues

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """states view"""
-from flask import abort, request
+from flask import abort, request, jsonify
 
 from api.v1.views import app_views
 from models import storage
@@ -10,7 +10,7 @@ from models.state import State
 @app_views.route('/states', methods=['GET'])
 def states():
     """display all states"""
-    return [obj.to_dict() for obj in storage.all(State).values()]
+    return jsonify([obj.to_dict() for obj in storage.all(State).values()])
 
 
 @app_views.route('/states/<id>', methods=['GET'])
