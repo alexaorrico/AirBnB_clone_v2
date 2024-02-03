1580355884694861e3abc50e233a7a52c3baea12
bakiizese <bereketzeselassie@gmail.com>
2024-02-02 21:01:01 -0800

    added api

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''index for the api app'''
+
+
+from api.v1.views import app_views
+from flask import jsonify
+
+jb = {"status": "OK"}
+
+
+@app_views.route('/status')
+def status():
+    '''returns json to the route'''
+    return jsonify(jb)
