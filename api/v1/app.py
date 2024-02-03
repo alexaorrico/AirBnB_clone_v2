c4a136295ab111998b2ec7dda975b474f18361eb
dev-tch <tijeni.chaabeni@gmail.com>
2024-02-02 17:40:36 -0800

    add handler error

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,7 @@
 """
 controller web flask app
 """
+from flask import jsonify
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
@@ -16,6 +17,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """close the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """return 404 json for uknown ressources"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
