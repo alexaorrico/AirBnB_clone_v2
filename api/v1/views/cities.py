483e6df2a53d7bf791b12edacebef43ae9946000
Loubna <loubna.alfidi@gmail.com>
2024-02-04 00:49:54 +0100

    fixing pycodestyle

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,8 @@ from models.city import City
 from models.state import State
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities_by_state(state_id):
     """Retrieves the list of all City objects of a State"""
     state = storage.get(State, state_id)
@@ -38,7 +39,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """Creates a City"""
     state = storage.get(State, state_id)
