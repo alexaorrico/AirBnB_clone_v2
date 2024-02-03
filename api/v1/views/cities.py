9f1266e15607d251e7d3155ee991a463db37dbeb
Etsybt <hmidouchnouhaila2@gmail.com>
2024-02-03 16:16:49 +0000

    update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,7 +9,11 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models import storage
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+
+@app_views.route(
+    '/states/<state_id>/cities',
+    methods=['GET'],
+    strict_slashes=False)
 def get_cities_by_state(state_id):
     state = storage.get(State, state_id)
     if not state:
@@ -17,12 +21,14 @@ def get_cities_by_state(state_id):
     cities = [city.to_dict() for city in state.cities]
     return jsonify(cities)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
@@ -33,7 +39,11 @@ def delete_city(city_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route(
+    '/states/<state_id>/cities',
+    methods=['POST'],
+    strict_slashes=False)
 def create_city(state_id):
     state = storage.get(State, state_id)
     if not state:
@@ -47,6 +57,7 @@ def create_city(state_id):
     city = City(**data)
     city.save()
     return jsonify(city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
@@ -63,9 +74,11 @@ def update_city(city_id):
     city.save()
     return jsonify(city.to_dict()), 200
 
+
 @app_views.errorhandler(404)
 def not_found(error):
     return jsonify({'error': 'Not found'}), 404
+
 
 @app_views.errorhandler(400)
 def bad_request(error):
