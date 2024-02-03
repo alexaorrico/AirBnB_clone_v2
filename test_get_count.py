3c63cb659be2d5ed6ba67f7e65b0668d42b7f5a3
sallyMohamed <engsally@gmail.com>
2024-02-03 01:40:41 -0800

    done

--- test_get_count.py
+++ test_get_count.py
@@ -3,8 +3,19 @@
 Test .get() and .count() methods 
 Test .get() and .count() methods
 """
+import os
+from sqlalchemy import create_engine
 from models import storage
 from models.state import State
+
+# Get the environment variables
+user = os.environ.get('HBNB_MYSQL_USER')
+password = os.environ.get('HBNB_MYSQL_PWD')
+host = os.environ.get('HBNB_MYSQL_HOST')
+database = os.environ.get('HBNB_MYSQL_DB')
+
+# Create the SQLAlchemy engine
+engine = create_engine(f'mysql://{user}:{password}@{host}/{database}')
 
 print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
