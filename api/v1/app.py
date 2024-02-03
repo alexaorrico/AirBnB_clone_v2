87d872437c5642647ba0121219303e155f29e537
nourister <nouristerjuma@gmail.com>
2024-02-03 10:53:27 +0000

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """
 This module contains the principal application
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-
+"""Init file for views module"""
 from flask import Blueprint
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-"""import storage engine and classes"""
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.user import User
-from models.place import Place
-from models.review import Review
 
-"""import flask views"""
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -20,3 +13,4 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
