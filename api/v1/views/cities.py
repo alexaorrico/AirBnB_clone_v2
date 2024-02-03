ad5d4dd6647d58a5b4ab692dd90e497217faa84d
zaynom <ouaziz0626@gmail.com>
2024-02-03 10:24:12 +0000

    task 7: fixing checker issues

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """"Cities views"""
-from flask import abort, request
+from flask import abort, request, jsonify
 
 from api.v1.views import app_views
 from models import storage
@@ -13,9 +13,9 @@ def cities_of_state(state_id):
     "Get a list of all cities of a state"
     state = storage.get(State, state_id)
     if state:
-        return [city.to_dict() for city in state.cities]
+        return jsonify([city.to_dict() for city in state.cities])
 
-    return abort(404)
+    abort(404)
 
 
 @app_views.route('/cities/<id>', methods=['GET'])
@@ -25,7 +25,7 @@ def city_by_id(id):
     if city:
         return city.to_dict()
 
-    return abort(404)
+    abort(404)
 
 
 @app_views.route('/cities/<id>', methods=['DELETE'])
@@ -37,17 +37,35 @@ def delete_city(id):
         storage.save()
         return {}
 
-    return abort(404)
+    abort(404)
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'])
 def create_city(state_id):
     "Create a new city"
     if not storage.get(State, state_id):
-        return abort(404)
-    if request.is_json:
-        data = request.get_json()
-        if not data.get('name'):
-            return 'Missing name', 400
-        new_city = City(state_id=state_id, **data)
-    return 'Not a JSON', 400
+        abort(404)
+    if not request.is_json:
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    if not data.get('name'):
+        abort(400, 'Missing name')
+    new_city = City(state_id=state_id, **data)
+    new_city.save()
+    return new_city.to_dict(), 201
+
+
+@app_views.route('/cities/<id>', methods=['PUT'])
+def update_city(id):
+	city = storage.get(City, id)
+	if not city:
+		abort(404)
+	if not request.is_json:
+		abort(400, 'Not a JSON')
+	data = request.get_json()
+	for k, v in data.items():
+		if k not in ['id', 'state_is', 'created_at', 'updated_id']:
+			setattr(city, k, v)
+	city.save()
+	return city.to_dict()
+
