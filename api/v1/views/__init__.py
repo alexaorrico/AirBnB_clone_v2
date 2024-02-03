e3a648cebb2f1551c81311dd1dd725232482b1cd
Olakunle64 <olakunleisiaq@gmail.com>
2024-02-03 02:02:00 +0000

    add a restful api

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains a flask blueprint for a web application"""
+
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+from api.v1.views.index import *
+from api.v1.views.states import *
