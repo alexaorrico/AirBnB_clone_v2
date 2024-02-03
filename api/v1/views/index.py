66602a5e8c9ebcdd108f184226b8a5ccfba9e1b6
Lamin Jawla <laminjawla1001@gmail.com>
2024-02-03 14:51:32 +0000

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,28 @@
 """Flask RESTful application"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    result = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+    return jsonify(result)
