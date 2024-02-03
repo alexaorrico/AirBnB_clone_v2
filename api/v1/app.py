783e712f29420f4fe0a44e4efb5c95ffd7157a1a
Lamin Jawla <laminjawla1001@gmail.com>
2024-02-03 13:11:57 +0000

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,17 +8,18 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
+# app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
 def teardown(self):
+    """Close the storage"""
     storage.close()
 
 
 if __name__ == "__main__":
     app.run(
         host=getenv("HBNB_API_HOST", "0.0.0.0"),
-        port=getenv("HBNB_API_PORT", 5000),
+        port=getenv("HBNB_API_PORT", "5000"),
         threaded=True,
     )
