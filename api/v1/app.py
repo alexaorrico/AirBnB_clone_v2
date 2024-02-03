88219b610df43ec353cfaad450a53428679ffdc9
bakiizese <bereketzeselassie@gmail.com>
2024-02-02 23:47:00 -0800

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 '''api main app run'''
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,11 @@ app.register_blueprint(app_views)
 def teardown(self):
     '''closes storage after done'''
     storage.close()
+
+
+@app.errorhandler(404)
+def er_404(error):
+    return jsonify({"error": "Not found"})
 
 
 if __name__ == '__main__':
