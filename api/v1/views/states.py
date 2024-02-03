2463c8008fb5e633c38b3d29740379daae4e4c6b
bakiizese <bereketzeselassie@gmail.com>
2024-02-03 11:50:46 -0800

    fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''routes'''
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.state import State
 import json
@@ -10,18 +10,20 @@ import json
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     '''returns json to the route'''
-    obj = [obj.to_dict() for obj in storage.all('State').values()]
+    obj = [obj.to_dict() for obj in storage.all(State).values()]
     return jsonify(obj)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_states_id(state_id):
     '''return by id'''
-    obj = [obj.to_dict() for obj in storage.all('State').values()]
-    for j in obj:
-        if j['id'] == state_id:
-            return j
-    abort(404)
+    st = storage.get(State, state_id)
+
+    if st is None:
+        abort(404)
+    st = st.to_dict()
+
+    return jsonify(st)
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'],
@@ -31,9 +33,9 @@ def del_by_id(state_id):
     st = storage.get(State, state_id)
     if st is None:
         abort(404)
-    storage.delete(st)
+    st.delete()
     storage.save()
-    return {}, 200
+    return (jsonify({}), 200)
 
 
 @app_views.route('/states', methods=['POST'],
@@ -43,15 +45,15 @@ def create_state():
     try:
         data = request.get_json()
     except Exception:
-        return jsonify({"error":  "Not a JSON"}), 400
-    create = State()
-    for key, value in data.items():
-        if key != 'name':
-            return jsonify({"error": "Missing name"}), 400
-        setattr(create, key, value)
-    storage.new(create)
-    storage.save()
+        return make_response(jsonify({"error":  "Not a JSON"}), 400)
+    if 'name' not in data:
+        return make_response(jsonify({"error": "Missing name"}), 400)
+
+    create = State(**data)
+
+    create.save()
     create = create.to_dict()
+
     return jsonify(create), 201
 
 
@@ -61,15 +63,18 @@ def update_state(state_id):
     '''update state by id'''
     try:
         data = request.get_json()
-    except json.JSONDecodeError:
-        return jsonify({"Not a JSON"}), 400
+    except Exception:
+        return make_response(jsonify({"Not a JSON"}), 400)
 
     stat = storage.get(State, state_id)
+
     if stat is None:
         abort(404)
     for key, value in data.items():
         if key == 'id' or key == 'created_at' or key == 'updated_at':
             continue
         setattr(stat, key, value)
+    storage.save()
     stat = stat.to_dict()
+
     return jsonify(stat), 200
