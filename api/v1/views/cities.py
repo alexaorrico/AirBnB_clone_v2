594df9cfa4867b6eaf1747cd1764e48d265bd93e
abdelhadia72 <thisisavnb@gmail.com>
2024-02-03 16:15:47 +0100

    Cities part one fetch all cities by the <state_id>

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-"""cities"""
+""" Import the app_views blueprint for cities API endpoints """
 from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
 from models.city import City
+
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'])
+@app_views.route('/states/<state_id>/cities/', methods=['GET'])
+def get_all_cities(state_id):
+    """ Get all cities objects """
+    all_status = storage.all("State").values()
+    state_objs = [state.to_dict() for state in all_status if state_id == state.id]
+    if state_objs == []:
+        abort(404)
+    all_cities = storage.all("City").values()
+    list_cities = [city.to_dict() for city in all_cities if state_id == city.state_id]
+    return jsonify(list_cities)
