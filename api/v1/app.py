dfa30accbff35f8dbb5c31a4790d0024432378d5
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-02-04 00:42:38 +0100

    task 4 => adding an endpoint that retrives the count of each object by its type v2

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,20 +14,6 @@ def teardown(exception):
     storage.close()
 
 
-@app_views.route("/stats", strict_slashes=False, methods=["GET"])
-def stats():
-    """Retrieves the number of each object type"""
-    stats_dict = {
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User')
-    }
-    return jsonify(stats_dict)
-
-
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', '5000'))
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """rout blueprints"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns a JSON response with status OK"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", strict_slashes=False, methods=["GET"])
+def stats():
+    """Retrieves the number of each object type"""
+    stats_dict = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    }
+    return jsonify(stats_dict)
