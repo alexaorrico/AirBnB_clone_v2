6562e6a7dbb0dd90d455aecba8da116ca6656482
bakiizese <bereketzeselassie@gmail.com>
2024-02-03 00:30:57 -0800

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
 
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    '''returns json to the route'''
+    obj = [obj.to_dict() for obj in storage.all('State').values()]
+    return jsonify(obj)
