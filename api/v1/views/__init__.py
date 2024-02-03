131f729029eba72c629739a8ba74fd2f5690d952
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-03 21:18:38 +0100

    task 4

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Flask App"""
 
-
-from api.v1.views.index import *
+from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from flask import Blueprint
+from api.v1.views.index import *
+from api.v1.views.states import *
