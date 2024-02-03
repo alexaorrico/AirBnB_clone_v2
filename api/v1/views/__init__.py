4170f807d06c16c59897ecba8b8d78742839ea30
himza25 <himza.of.theking95@gmail.com>
2024-02-03 12:53:21 -0800

    update api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 '''Contains the blueprint for the API.'''
 from flask import Blueprint
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-'''The blueprint for the AirBnB clone API.'''
-
-
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
@@ -15,3 +9,7 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
