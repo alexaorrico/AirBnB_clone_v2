78092cdb2da5f7aadbc991ce214a3c53be8bc860
KofiGhana <KofiGhana>
2024-02-04 14:12:47 -0800

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-"""
-API for AirBnB_clone_v3
+"""Flask server (variable app)
 """
 
-import os
-from flask import Flask, jsonify, Response
-from flask_cors import CORS
+
+from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """ handles teardown """
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """ handles 404 errors """
-    status = {"error": "Not found"}
-    return jsonify(status), 404
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
 
 
-if __name__ == '__main__':
-    try:
-        host = os.environ.get('HBNB_API_HOST')
-    except:
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
         host = '0.0.0.0'
-
-    try:
-        port = os.environ.get('HBNB_API_PORT')
-    except:
+    if not port:
         port = '5000'
-
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,24 @@
-#!/usr/bin/python3
-"""
-App views for AirBnB_clone_v3
-"""
-
-from flask import jsonify
+#!/usr/bin/python3xx
+'''api status'''
+import models
 from models import storage
+from models.base_model import BaseModel
+from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
-def status():
-    """ returns status """
-    status = {"status": "OK"}
-    return jsonify(status)
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
 
 
-@app_views.route('/stats')
-def count():
-    """ returns number of each objects by type """
-    total = {}
-    classes = {"Amenity": "amenities",
-               "City": "cities",
-               "Place": "places",
-               "Review": "reviews",
-               "State": "states",
-               "User": "users"}
-    for cls in classes:
-        count = storage.count(cls)
-        total[classes.get(cls)] = count
-    return jsonify(total)
+@app_views.route('/stats', strict_slashes=False)
+def stuff():
+    '''JSON Responses'''
+    todos = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
