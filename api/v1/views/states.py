d52071ebcf58df70fa72aa6bf49d6e41a979b7e3
Mally13 <mallymutuku13@gmail.com>
2024-02-04 19:30:09 +0300

    Api State endpoint

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,6 +6,7 @@ Handles RESTFul API actions for states
 from flask import abort
 from api.v1.views import app_views
 from flask import jsonify
+from flask import make_response
 from flask import request
 from models.state import State
 from models import storage
@@ -43,7 +44,7 @@ def delete_state(state_id):
     state = storage.get("State", state_id)
     if state:
         storage.delete(state)
-        return jsonify({}), 200
+        return make_response(jsonify({}), 200)
     abort(404)
 
 
@@ -54,14 +55,14 @@ def add_state():
     """
     data = request.get_json(silent=True)
     if not data:
-        abort(400,'Not a JSON')
+        abort(400, 'Not a JSON')
     if 'name' not in data:
-        abort(400,"Missing name")
+        abort(400, "Missing name")
 
     name = data['name']
     new_state = State(name=name)
     new_state.save()
-    return jsonify(new_state.to_dict()), 201
+    return make_response(jsonify(new_state.to_dict()), 201)
 
 
 @app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
@@ -73,12 +74,12 @@ def update_state(state_id):
     if state:
         data = request.get_json(silent=True)
         if not data:
-            abort(400,"Not a JSON")
+            abort(400, "Not a JSON")
         keys_to_ignore = ["created_at", "id", "updated_at"]
         for k, v in data.items():
             if k not in keys_to_ignore:
                 state.__dict__.update({k: v})
         storage.save()
-        return jsonify(state.to_dict()), 200
+        return make_response(jsonify(state.to_dict()), 200)
 
     abort(404)
