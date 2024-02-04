31d24057532797a13ab965f62a1ad5c223a6b27e
sarora2200 <sarora2200@gmail.com>
2024-02-04 11:03:12 -0800

    fix

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -109,3 +109,7 @@ class TestFileStorage(unittest.TestCase):
         storage.save()
         c = storage.count()
         self.assertEqual(len(storage.all()), c)
+
+
+if __name__ == "__main__":
+    unittest.main()
