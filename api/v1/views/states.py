090d53384594e5df31c766654d5d68c33d12dab8
tarie100 <tarie.school@gmail.com>
2024-02-04 14:37:04 -0800

    edit states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask import abort, jsonify, request
-from models import storage
+from models.__init__ import storage
 from models.state import State
 from api.v1.views import app_views
 
