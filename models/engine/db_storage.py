5095d009f307338480d8324c7ae1f661be1e6a97
faith2221 <edithbanda014@gmail.com>
2024-02-04 03:33:00 -0800

    deleted

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,15 +74,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def get(self, cls, id):
-        """retrieves an object."""
-        if cls and id:
-            key = "{}.{}".format(cls, id)
-            all_obj = self.all(cls)
-            return all_obj.get(key)
-        return (None)
-
-    def count(self, cls=None):
-        """count the number of objects in the storage."""
-        return (len(self.all(cls)))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,15 +69,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
-    def get(self, cls, id):
-        """retrieves an object."""
-        if cls and id:
-            key = "{}.{}".format(cls, id)
-            all_obj = self.all(cls)
-            return all_obj.get(key)
-        return (None)
-
-    def count(self, cls=None):
-        """count the number of objects in the storage."""
-        return (len(self.all(cls)))
