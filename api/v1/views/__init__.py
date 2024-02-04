88d5d32ef8b6aa784e3e8f1b9225557d77f92621
KennyChukwuebuka <134069456+KennyChukwuebuka@users.noreply.github.com>
2024-02-04 04:06:05 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,7 +6,7 @@ instance of Blueprint (url prefix must be /api/v1)
 wildcard import of everything in the package
 api.v1.views.index => PEP8 will complain
 about it, don’t worry, it’s normal and
-this file (v1/views/__init__.py) won’t be check.
+this file (v1/views/__init__.py) won’t be check
 """
 
 from flask import Blueprint
