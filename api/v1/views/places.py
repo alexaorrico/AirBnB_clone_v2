b8e76b7a68f4554208460cf754aa849422ee0bfe
0'Brien <abucheriobrien@gmail.com>
2024-02-03 19:14:57 -0800

    Search functionality

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -7,6 +7,7 @@ from models import storage
 from models.city import City
 from models.place import Place
 from models.user import User
+from models.amenity import Amenity
 import json
 
 
@@ -95,3 +96,39 @@ def update_place(place_id):
 
     storage.save()
     return jsonify(place.to_dict()), 200
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def places_search():
+    '''Search for places based on JSON request'''
+    try:
+        data = request.get_json()
+    except Exception:
+        return jsonify({"error": "Not a JSON"}), 400
+
+    states = data.get("states", [])
+    cities = data.get("cities", [])
+    amenities = data.get("amenities", [])
+
+    places = []
+
+    if not states and not cities and not amenities:
+        places = [place.to_dict() for place in storage.all(Place).values()]
+    else:
+        for state_id in states:
+            state = storage.get(State, state_id)
+            if state:
+                places.extend([place.to_dict() for city in state.cities
+                              for place in city.places])
+
+        for city_id in cities:
+            city = storage.get(City, city_id)
+            if city:
+                places.extend([place.to_dict() for place in city.places])
+
+    if amenities:
+        amenities_set = set(amenities)
+        places = [place for place in places
+                  if amenities_set.issubset(place.get("amenities", []))]
+
+    return jsonify(places)
