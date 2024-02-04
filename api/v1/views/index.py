094c1c9d486d6315d60197dd639ab862a91f31a2
Muliro <mulirokhaemba@gmail.com>
2024-02-04 17:28:27 +0300

    pep

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def api_status():
     ''' Returns a JSON response for RESTful API health.
@@ -12,16 +13,17 @@ def api_status():
     response = {'status': 'OK'}
     return jsonify(response)
 
+
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     ''' Retrieves the number of each objects by type.
-   '''
+    '''
     stats = {
-    'amenities': storage.count('Amenity'),
-    'cities': storage.count('City'),
-    'places': storage.count('Place'),
-    'reviews': storage.count('Review'),
-    'states': storage.count('State'),
-    'users': storage.count('User')
-} 
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+            }
     return jsonify(stats)
