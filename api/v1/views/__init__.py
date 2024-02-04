bd567c397af00b8d84a7deea06396f116f6c2215
Karenmiano <mianokaren1798@gmail.com>
2024-02-04 11:44:59 +0300

    adding files

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from flask import Blueprint
-from api.v1.views import *
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
+from api.v1.views.states import *
