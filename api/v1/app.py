cc0628a9e20526eb665eac9128dfdc9150d64523
0'Brien <abucheriobrien@gmail.com>
2024-02-03 17:47:41 -0800

    implemented CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,14 @@
 from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+# Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
