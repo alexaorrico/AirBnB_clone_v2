b6849f5762983b1cd86be58d166982f97adefc52
sarora2200 <sarora2200@gmail.com>
2024-02-04 15:52:55 -0800

    done

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,9 +3,6 @@
 Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
 """
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
@@ -13,3 +10,5 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
