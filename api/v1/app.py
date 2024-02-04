08534a3f7d0953d91a311e85f7baa1f8aeb95224
Etsybt <hmidouchnouhaila2@gmail.com>
2024-02-04 14:10:27 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,10 @@ from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
