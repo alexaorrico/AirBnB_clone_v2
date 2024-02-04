a276a0b5d1cb015bd61c1a31256f674733a4a2b5
bakiizese <bereketzeselassie@gmail.com>
2024-02-04 13:26:58 -0800

    fixed

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''blueprint for api app'''
+from flask import Blueprint
 
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.places_amenities import *
 from api.v1.views.places_reviews import *
@@ -10,6 +12,3 @@ from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
 from api.v1.views.index import *
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
