07b35787fd287de337859fa363ab79c5e407873a
Etsybt <hmidouchnouhaila2@gmail.com>
2024-02-04 12:50:45 +0000

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,12 @@
-#!/usr/bin/python3
-"""
-innit file
-"""
+#!/usr/bin/python
+"""creates a Blueprint instance"""
 
 
 from flask import Blueprint
-from api.v1.views import index
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
