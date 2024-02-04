6d2e00f136e644e14812c99bfc3151c3b4423751
sarora2200 <sarora2200@gmail.com>
2024-02-04 15:54:58 -0800

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,7 +15,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats")
+@app_views.route("/stats", strict_slashes=False)
 def stats():
     """
     Create an endpoint that retrieves the number of each objects by type
