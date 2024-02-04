46d3ec4938ef8ed4422d6e8bc469c8b56c9a37e4
sarora2200 <sarora2200@gmail.com>
2024-02-04 11:20:26 -0800

    fix

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,6 +87,7 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         """Tests method for obtaining an instance db storage"""
         storage = DBStorage()
@@ -97,6 +98,7 @@ class TestFileStorage(unittest.TestCase):
         get_instance = storage.get(State, instance.id)
         self.assertEqual(get_instance, instance)
 
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
         """Tests count method db storage"""
         storage = DBStorage()
