bf5597b89923f09a8a1bacf4a769d52cf02e60b7
KofiGhana <KofiGhana>
2024-02-04 13:07:42 -0800

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,50 @@
 #!/usr/bin/python3
-'''Flask server (variable app)'''
+'''Contains a Flask web application API.
+'''
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
 
-from flask import Flask
 from models import storage
-from os import getenv
 from api.v1.views import app_views
-from flask import jsonify
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+'''The Flask web application instance.'''
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
-def downtear(self):
-    '''Status of your API'''
+def teardown_flask(exception):
+    '''The Flask app/request context end event listener.'''
+    # print(exception)
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    '''return render_template'''
-    return jsonify({'error': 'Not found'}), 404
+def error_404(error):
+    '''Handles the 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
 
 
-if __name__ == "__main__":
-    if getenv('HBNB_API_HOST'):
-        host = getenv('HBNB_API_HOST')
-    else:
-        host = '0.0.0.0'
-    if getenv('HBNB_API_PORT'):
-        port = getenv('HBNB_API_PORT')
-    else:
-        port = 5000
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(400)
+def error_400(error):
+    '''Handles the 400 HTTP error code.'''
+    msg = 'Bad request'
+    if isinstance(error, Exception) and hasattr(error, 'description'):
+        msg = error.description
+    return jsonify(error=msg), 400
+
+
+if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
