74312e34f4bb50276fb9a335ea651b2d10ec7a7b
himza25 <himza.of.theking95@gmail.com>
2024-02-03 16:11:31 -0800

    update tests and api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-'''Contains the blueprint for the API.'''
+"""
+Initialize views
+"""
 from flask import Blueprint
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.index import *
-from api.v1.views.places_amenities import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.states import *
-from api.v1.views.users import *
-
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-'''The blueprint for the AirBnB clone API.'''
+
+# PEP8 will complain about wildcard import here, but it is needed to make the
+# endpoints accessible.
+from api.v1.views.index import *
+
--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -68,56 +68,74 @@ test_db_storage.py'])
                             "{:s} method needs a docstring".format(func[0]))
 
 
-@unittest.skipIf(models.storage_t != 'db', "not testing db storage")
-class TestDBStorage(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
         self.assertIs(type(models.storage.all()), dict)
 
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_no_class(self):
         """Test that all returns all rows when no class is passed"""
 
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_new(self):
         """test that new adds an object to the database"""
 
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
-    def test_get(self):
-        """test that get returns an object of a given class by id."""
-        storage = models.storage
-        obj = State(name='Michigan')
-        obj.save()
-        self.assertEqual(obj.id, storage.get(State, obj.id).id)
-        self.assertEqual(obj.name, storage.get(State, obj.id).name)
-        self.assertIsNot(obj, storage.get(State, obj.id + 'op'))
-        self.assertIsNone(storage.get(State, obj.id + 'op'))
-        self.assertIsNone(storage.get(State, 45))
-        self.assertIsNone(storage.get(None, obj.id))
-        self.assertIsNone(storage.get(int, obj.id))
-        with self.assertRaises(TypeError):
-            storage.get(State, obj.id, 'op')
-        with self.assertRaises(TypeError):
-            storage.get(State)
-        with self.assertRaises(TypeError):
-            storage.get()
 
-    def test_count(self):
-        """test that count returns the number of objects of a given class."""
-        storage = models.storage
-        self.assertIs(type(storage.count()), int)
-        self.assertIs(type(storage.count(None)), int)
-        self.assertIs(type(storage.count(int)), int)
-        self.assertIs(type(storage.count(State)), int)
-        self.assertEqual(storage.count(), storage.count(None))
-        State(name='Lagos').save()
-        self.assertGreater(storage.count(State), 0)
-        self.assertEqual(storage.count(), storage.count(None))
-        a = storage.count(State)
-        State(name='Enugu').save()
-        self.assertGreater(storage.count(State), a)
-        Amenity(name='Free WiFi').save()
-        self.assertGreater(storage.count(), storage.count(State))
-        with self.assertRaises(TypeError):
-            storage.count(State, 'op')
+class TestDBStorageGetCount(unittest.TestCase):
+    """Test the get and count methods of the DBStorage class."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the tests."""
+        models.storage.reload()
+        cls.state = State(name="TestState")
+        models.storage.new(cls.state)
+        models.storage.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up actions after tests."""
+        models.storage.delete(cls.state)
+        models.storage.save()
+
+    def test_get_method_valid_id(self):
+        """Test retrieval of object by valid ID."""
+        obj = models.storage.get("State", self.state.id)
+        self.assertEqual(obj, self.state)
+
+    def test_get_method_invalid_id(self):
+        """Test retrieval with an invalid ID returns None."""
+        obj = models.storage.get("State", "invalid_id")
+        self.assertIsNone(obj)
+
+    def test_get_method_invalid_class(self):
+        """Test retrieval with an invalid class returns None."""
+        obj = models.storage.get("InvalidClass", self.state.id)
+        self.assertIsNone(obj)
+
+    def test_count_method_with_class(self):
+        """Test counting objects of a specific class."""
+        initial_count = models.storage.count("State")
+        new_state = State(name="AnotherTestState")
+        models.storage.new(new_state)
+        models.storage.save()
+        self.assertEqual(models.storage.count("State"), initial_count + 1)
+        models.storage.delete(new_state)
+        models.storage.save()
+
+    def test_count_method_no_class(self):
+        """Test counting all objects in storage."""
+        initial_count = models.storage.count()
+        new_state = State(name="YetAnotherTestState")
+        models.storage.new(new_state)
+        models.storage.save()
+        self.assertTrue(models.storage.count() > initial_count)
+        models.storage.delete(new_state)
+        models.storage.save()
