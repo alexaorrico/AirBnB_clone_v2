3790d67266b9991b841e6d8cbf721330c6450b1d
Mally13 <mallymutuku13@gmail.com>
2024-02-04 14:47:14 +0300

    Task 2: fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,7 +80,7 @@ class DBStorage:
         for obj in all.values():
             if obj.id == id:
                 return obj
-            return None
+        return None
 
     def count(self, cls=None):
         """
