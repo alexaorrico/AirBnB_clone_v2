ae55fd41c0392a2c818e467821183e366e159b2f
KofiGhana <KofiGhana>
2024-02-04 08:17:52 -0800

    updated api index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-"""
-a route /status on the object app_views
-returns a JSON: "status": "OK" (see example)
-"""
-from api.v1.views import app_views
-from flask import jsonify
+'''api status'''
+import models
 from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from flask import jsonify
+from api.v1.views import app_views
 
 
-@app_views.route('/status',  methods=['GET'])
-def status():
-    return jsonify({"status": "OK"})
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
 
 
-@app_views.route('/stats',  methods=['GET'])
-def stats():
-    """an endpoint that retrieves the number of each objects by type:"""
-    response = {}
-    key_val = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-
-    for key, val in key_val.items():
-        response[val] = storage.count(key)
-    return jsonify(response)
+@app_views.route('/stats', strict_slashes=False)
+def stuff():
+    '''JSON Responses'''
+    todos = {
+        'states': State, 'users': User,
+        'amenities': Amenity, 'cities': City,
+        'places': Place, 'reviews': Review
+    }
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
