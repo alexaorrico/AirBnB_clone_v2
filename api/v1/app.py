62fb0a03f35e6c92fc59b6b2915ff985bfb1dcc4
sarora2200 <sarora2200@gmail.com>
2024-02-04 15:39:00 -0800

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,6 @@
 Starts a flask application
 """
 import os
-
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
@@ -11,11 +10,9 @@ from werkzeug.exceptions import NotFound
 from flask_cors import CORS
 
 app = Flask(__name__)
-
-CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r'/api/*': {'origins': '0.0.0.0'}})
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
