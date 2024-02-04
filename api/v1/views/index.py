c45bd5d918b02ddbc7bd8faa8858f509d227d686
Mally13 <mallymutuku13@gmail.com>
2024-02-04 14:11:47 +0300

    Task 4: doc strings

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-defines the route /status
+Defines the routes: /status
+                    /stats
 """
 
 from api.v1.views import app_views
@@ -10,19 +11,19 @@ from models import storage
 
 @app_views.route("/status", strict_slashes=False)
 def status():
-    """returns status of response"""
+    """Returns status of response"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats", strict_slashes=False)
 def stats():
-    """returns the count of each object in storage"""
+    """Returns the count of each object in storage"""
     obj_stats = {
         "amenities": storage.count("Amenity"),
         "cities": storage.count("City"),
         "places": storage.count("Place"),
         "reviews": storage.count("Review"),
         "states": storage.count("State"),
-        "users": storage.count("User")
+        "users": storage.count("User"),
     }
     return jsonify(obj_stats)
