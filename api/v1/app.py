f4fc48380bd44d219853e84c8b8466460a85329c
KofiGhana <KofiGhana>
2024-02-04 13:18:35 -0800

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,50 +1,40 @@
 #!/usr/bin/python3
-'''Contains a Flask web application API.
-'''
-import os
-from flask import Flask, jsonify
-from flask_cors import CORS
+"""
+API for AirBnB_clone_v3
+"""
 
+import os
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-
-
 app = Flask(__name__)
-'''The Flask web application instance.'''
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_flask(exception):
-    '''The Flask app/request context end event listener.'''
-    # print(exception)
+def teardown(self):
+    """ handles teardown """
     storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
-    '''Handles the 404 HTTP error code.'''
-    return jsonify(error='Not found'), 404
-
-
-@app.errorhandler(400)
-def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
+def page_not_found(e):
+    """ handles 404 errors """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
 
 
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
+        host = '0.0.0.0'
+
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
+        port = '5000'
+
+    app.run(host=host, port=port)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
-'''Contains the index view for the API.'''
+"""
+App views for AirBnB_clone_v3
+"""
+
 from flask import jsonify
-from api.v1.views import app_views
 from models import storage
+from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status')
 def status():
-    """ Returns JSON """
-    return jsonify(status="OK")
+    """ returns status """
+    status = {"status": "OK"}
+    return jsonify(status)
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stat():
-    """returns the number of each objects by type"""
-    return jsonify(
-        amenities=storage.count('Amenity'),
-        cities=storage.count('City'),
-        places=storage.count('Place'),
-        reviews=storage.count('Review'),
-        states=storage.count('State'),
-        users=storage.count('User')
-    )
+@app_views.route('/stats')
+def count():
+    """ returns number of each objects by type """
+    total = {}
+    classes = {"Amenity": "amenities",
+               "City": "cities",
+               "Place": "places",
+               "Review": "reviews",
+               "State": "states",
+               "User": "users"}
+    for cls in classes:
+        count = storage.count(cls)
+        total[classes.get(cls)] = count
+    return jsonify(total)
