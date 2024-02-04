b0921bc951e4cb85231d5d1816c5f309ad3779a9
Olakunle64 <olakunleisiaq@gmail.com>
2024-02-04 18:49:57 +0000

    debugging....

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,40 +1,28 @@
 #!/usr/bin/python3
-""" This module contains route for status of the api
-    and statistic of all classes
 """
-
-from api.v1.views import app_views
+This module contains the index
+"""
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
-from models.state import State
 from models.amenity import Amenity
-from models.user import User
-from models.review import Review
-from models.place import Place
 from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+classes = {"amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User}
 
 
 @app_views.route('/status', strict_slashes=False)
-def status():
-    """ return the status of the API """
+def api_status():
+    """returns the status of the api"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    """ return the statistic of all class objects in the storage """
-    amenities_count = storage.count(Amenity)
-    cities_count = storage.count(City)
-    places_count = storage.count(Place)
-    reviews_count = storage.count(Review)
-    states_count = storage.count(State)
-    users_count = storage.count(User)
-
-    all_classes_stat = {'amenities': amenities_count,
-                        'cities': cities_count,
-                        'places': places_count,
-                        'reviews': reviews_count,
-                        'states': states_count,
-                        'users': users_count
-                        }
-    return jsonify(all_classes_stat)
+    """returns the stats of all classes"""
+    stats_dict = {k: storage.count(v) for k, v in classes.items()}
+    return jsonify(stats_dict)
