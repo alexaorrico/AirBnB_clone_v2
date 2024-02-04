55d4ada96cf434a35cf9d5517de5dde7e2813b55
KofiGhana <KofiGhana>
2024-02-04 13:27:37 -0800

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Contains the blueprint for the API
+Views for AirBnB_clone_v3
 """
-from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from flask import Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -13,4 +13,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
