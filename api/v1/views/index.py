ce89a7ac13cfab823d57160234aa6e957ece54fe
bakiizese <bereketzeselassie@gmail.com>
2024-02-05 01:02:47 -0800

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify, Flask
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,9 +20,9 @@ def status():
 @app_views.route('/stats', strict_slashes=False)
 def stat():
     '''returns number of each obj by type'''
-    return jsonify({"amenities": storage.count('Amenity'),
-                    "cities": storage.count('City'),
-                    "places": storage.count('Place'),
-                    "reviews": storage.count('Review'),
-                    "states": storage.count('State'),
-                    "users": storage.count('User')})
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
