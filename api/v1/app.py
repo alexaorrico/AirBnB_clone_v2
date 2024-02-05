6e299b46476716a800af0a17d200f485a413e688
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-02-05 22:18:06 +0000

    API setup

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-"""start flask api"""
-from flask import Flask
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
+
 
 app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def close_storage():
-    """closes storage"""
+def tear_down(self):
+    '''
+    close query after each session
+    '''
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
-    port = os.getenv('HBNB_API_PORT') if os.getenv('HBNB_API_PORT') is not None else '0.0.0.0'
-    host = os.getenv('HBNB_API_HOST') if os.getenv('HBNB_API_HOST') is not None else '5000'
-    app.run(threaded=True, port=port, host=host)
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,34 @@
-i#!/usr/bin/python3
-"""index status blueprint"""
+#!/usr/bin/python3
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
 from api.v1.views import app_views
-from flask import jasonify
+from flask import jsonify
 from models import storage
+
 
 @app_views.route("/status")
 def status():
-    """check status"""
-    return jasonify(status='OK')
+    '''
+        return JSON of OK status
+    '''
+    return jsonify({'status': 'OK'})
 
-@app_views.route("/api/v1/stats")
-def stats():
-    """checks the stats of each object by its type"""
-    return jasonify(storage.count())
 
-@app_views.errorhandler(404)
-def not_found():
-    """handles not found error"""
-    response = jasonify(error='Not Found')
-    response.status_code = 404
-    return response
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(cls_counts)
