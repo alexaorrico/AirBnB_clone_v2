7d6021648eef85072a41beee0ba9199cbd62cbc7
Mohamed Ayman <mohayman2007@gmail.com>
2024-02-05 13:56:11 +0000

    0x05. AirBnB clone (RESTful API) - Updating BaseModel.__init__()

--- models/base_model.py
+++ models/base_model.py
@@ -34,13 +34,13 @@ class BaseModel:
                     setattr(self, key, value)
             if kwargs.get("created_at", None) and type(self.created_at) is str:
                 self.created_at = datetime.strptime(kwargs["created_at"], time)
-            else:
+            elif getattr(self, "created_at") is None:
                 self.created_at = datetime.utcnow()
             if kwargs.get("updated_at", None) and type(self.updated_at) is str:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            else:
+            elif getattr(self, "updated_at") is None:
                 self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
+            if kwargs.get("id") is None and getattr(self, "id") is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
