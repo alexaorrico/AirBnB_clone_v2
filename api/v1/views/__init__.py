0d4c059e5ccb6d9fef2d9ee7f9ce1bb164358805
root <hakeemabdullah87@gmail.com>
2024-02-05 09:01:16 -0800

    commit all files

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Blue print for flask document"""
 from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.reviews import *
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
