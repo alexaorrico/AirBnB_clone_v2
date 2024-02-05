469bc9cb85c9baf2e6696b32cda01fb1df9f9d05
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-02-05 20:35:26 +0200

    task 9 ):

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,84 +1,65 @@
 #!/usr/bin/python3
-"""
-Module for handling RESTful API actions for User objects.
-"""
+"""user route"""
 
-from flask import Flask, jsonify, abort, request
 from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
 from models import storage
 from models.user import User
 
 
-@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@app_views.route('/users', methods=['GET'])
 def get_users():
-    """
-    Retrieves the list of all User objects.
-    """
-    users = storage.all(User).values()
-    return jsonify([user.to_dict() for user in users])
+    """get a list of users"""
+    users = [obj.to_dict() for obj in storage.all(User).values()]
+    return make_response(jsonify(users), 200)
 
 
-@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def get_user(user_id):
-    """
-    Retrieves a User object.
-    """
+@app_views.route('/users/<user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+    """get user by id"""
     user = storage.get(User, user_id)
-    if user is None:
-        abort(404, description=f"User with ID {user_id} not found")
-    return jsonify(user.to_dict())
+    return make_response(jsonify(user.to_dict()),
+                         200) if user else abort(404)
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def delete_user(user_id):
-    """
-    Deletes a User object.
-    """
-    user = storage.get(User, user_id)
-    if user is None:
-        abort(404, description=f"User with ID {user_id} not found")
-
-    storage.delete(user)
-    storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/users', methods=['POST'], strict_slashes=False)
+@app_views.route('/users', methods=['POST'])
 def create_user():
-    """
-    Creates a User object.
-    """
-    data = request.get_json()
-    if not data:
-        abort(400, description="Not a JSON")
-    if 'email' not in data:
-        abort(400, description="Missing email")
-    if 'password' not in data:
-        abort(400, description="Missing password")
-
-    new_user = User(**data)
-    storage.new(new_user)
-    storage.save()
-    return jsonify(new_user.to_dict()), 201
+    """create a new user"""
+    user = request.get_json()
+    if not user:
+        return make_response("Not a JSON", 400)
+    if not user.get('email'):
+        return make_response("Missing email", 400)
+    if not user.get('password'):
+        return make_response("Missing password", 400)
+    new_user = User(**user)
+    new_user.save()
+    return make_response(jsonify(new_user.to_dict()), 201)
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
-    """
-    Updates a User object.
-    """
-    user = storage.get(User, user_id)
-    if user is None:
-        abort(404, description=f"User with ID {user_id} not found")
-
-    data = request.get_json()
-    if not data:
-        abort(400, description="Not a JSON")
-
-    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
-    for key, value in data.items():
-        if key not in ignore_keys:
-            setattr(user, key, value)
-
+    """update a user by id"""
+    cur_user = storage.get(User, user_id)
+    if not cur_user:
+        abort(404)
+    new_user = request.get_json()
+    if not new_user:
+        return make_response("Not a JSON", 400)
+    ignored_keys = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in new_user.items():
+        if key not in ignored_keys:
+            setattr(cur_user, key, value)
     storage.save()
-    return jsonify(user.to_dict()), 200
+    return make_response(cur_user.to_dict(), 200)
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    """delete user by id"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    user.delete()
+    storage.save()
+    return make_response({}, 200)
