1b64945d1a3c29af790219b4c254507a95802740
LusterPearl <Salamioluwatosin90@gmail.com>
2024-02-05 12:48:53 -0800

    9thpush

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,8 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
+import hashlib
+
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -58,7 +60,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self,  save_to_disk=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -66,6 +68,8 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
+        if not save_to_disk:
+            new_dict.pop("password", None)
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
         return new_dict
