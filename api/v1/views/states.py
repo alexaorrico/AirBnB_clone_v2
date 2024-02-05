4f5b1e795e63a10c2c03fbd13def708376094f84
bakiizese <bereketzeselassie@gmail.com>
2024-02-05 09:01:11 -0800

    i

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''routes'''
+from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 from models import storage
 from models.state import State
-from api.v1.views import app_views
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
