b8cc4b22a1fd8a0aac99b9aa5b7137b92d8a2a79
7bernie <bernardbassie@gmail.com>
2024-02-05 00:08:56 -0800

    Status of your API

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+        def get(self, cls, id):
+        """
+            retrieves one object based on class name and id
+        """
+        if cls and id:
+            fetch = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+            return all_obj.get(fetch)
+        return None
+
+    def count(self, cls=None):
+        """
+            returns the count of all objects in storage
+        """
+        return (len(self.all(cls)))
