39c27d6dec2326dd743cdcba24000f0ad35f5377
essaiydy <imaneessaiydy2@gmail.com>
2024-02-05 15:51:34 -0800

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 import os
 
@@ -12,6 +12,11 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def terddown():
     """function to close"""
     storage.close()
+
+@app.errorhandler(404)
+def error(error):
+    """return error page"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
