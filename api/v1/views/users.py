91f220eb09199be397765baf0e76b17408669ae4
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-02-05 14:06:13 +0200

    task 9

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,20 +2,20 @@
 """This module handles the RESTful API actions for User objects"""
 
 
-from api.v1.views import app_views
 from flask import jsonify, abort, request
+from api.v1.views import app_views
 from models import storage
 from models.user import User
 
 
-@app_views.route('/users', methods=['GET'])
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_all_users():
     """Retrieves the list of all User objects"""
     users = storage.all(User).values()
     return jsonify([user.to_dict() for user in users])
 
 
-@app_views.route('/users/<user_id>', methods=['GET'])
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """Retrieves a User object by user_id"""
     user = storage.get(User, user_id)
@@ -24,7 +24,7 @@ def get_user(user_id):
     return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'])
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """Deletes a User object by user_id"""
     user = storage.get(User, user_id)
@@ -35,7 +35,7 @@ def delete_user(user_id):
     return jsonify({}), 200
 
 
-@app_views.route('/users', methods=['POST'])
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
 def post_user():
     """Creates a new User"""
     request_body = request.get_json()
@@ -51,7 +51,7 @@ def post_user():
     return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'])
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def put_user(user_id):
     """Updates a User object by user_id"""
     user = storage.get(User, user_id)
