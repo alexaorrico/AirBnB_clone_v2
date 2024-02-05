7bd84820d4d02f23e5e199c1181c838632d7115b
Olakunle64 <olakunleisiaq@gmail.com>
2024-02-05 00:22:57 +0000

    allow control allow origin

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,11 @@ import os
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
