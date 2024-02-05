b83e35633040cbce50c42a6345d8a94093dc27f9
bakiizese <bereketzeselassie@gmail.com>
2024-02-05 08:29:52 -0800

    try

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-'''index for the api app'''
-from api.v1.views import app_views
-from flask import jsonify, Flask
+"""
+This module contains endpoint(route) status
+"""
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
+from flask import Flask
+from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    '''returns json to the route'''
+    """
+    Returns a JSON status
+    """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
-def stat():
-    '''returns number of each obj by type'''
-    return jsonify({"amenities": storage.count(Amenity),
-                    "cities": storage.count(City),
-                    "places": storage.count(Place),
-                    "reviews": storage.count(Review),
-                    "states": storage.count(State),
-                    "users": storage.count(User)})
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
