f08989509f0eb6856f325a3c13d5021ade0826ae
himza25 <himza.of.theking95@gmail.com>
2024-02-05 06:52:18 -0800

    update api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,7 +6,13 @@ retrieving the number of objects by type.
 """
 from flask import jsonify
 from api.v1.views import app_views
-from models import storage, Amenity, City, Place, Review, State, User
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
