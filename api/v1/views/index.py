615f438a8149eb5a3a1737fc8fed65f0d62ff0e5
EmyJuly <farsimane00@gmail.com>
2024-02-05 14:10:00 +0000

    index modif

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
-'''Contains the index view for the API.'''
-from flask import jsonify
-
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
-@app_views.route('/status')
-def get_status():
-    '''Gets the status of the API.
+@app_views.route("/status")
+def status():
     '''
-    return jsonify(status='OK')
-
-
-@app_views.route('/stats')
-def get_stats():
-    '''Gets the number of objects for each type.
+        return JSON of OK status
     '''
-    objects = {
-        'amenities': Amenity,
-        'cities': City,
-        'places': Place,
-        'reviews': Review,
-        'states': State,
-        'users': User
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
     }
-    for key, value in objects.items():
-        objects[key] = storage.count(value)
-    return jsonify(objects)
+    return jsonify(cls_counts)
