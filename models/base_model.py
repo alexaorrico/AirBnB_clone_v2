48f4cfae6b36f23d3ed2e24c22da2f165d151d09
Matrix30 <Matrixboy30@gmail.com>
2024-02-05 22:20:06 +0100

    fix basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
                 self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
+            if kwargs.get("id") is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
@@ -49,8 +49,13 @@ class BaseModel:
 
     def __str__(self):
         """String representation of the BaseModel class"""
+        obj_dict = self.__dict__.copy()
+
+        if obj_dict.get("_sa_instance_state") is not None:
+            del obj_dict["_sa_instance_state"]
+
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+                                         obj_dict)
 
     def save(self):
         """updates the attribute 'updated_at' with the current datetime"""
@@ -58,7 +63,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, storage_type="db"):
+    def to_dict(self, storing_data=False):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -68,8 +73,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if models.storage_t == "db" and "password" in new_dict:
-            # new_dict.pop("password")
+        if not storing_data and "password" in new_dict:
             del new_dict["password"]
         return new_dict
 
