e7a98fd15db2c20d335d946cb65d8220f3f5f97d
EmyJuly <farsimane00@gmail.com>
2024-02-05 16:48:11 +0000

    correct app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,102 +1,35 @@
 #!/usr/bin/python3
-'''Contains the cities view for the API.'''
-<<<<<<< HEAD
-ntains the cities view for the API.'''
-=======
->>>>>>> 3007e7ceb55b1b6d0012b287e4a0344339979045
-from flask import jsonify, request
-from werkzeug.exceptions import NotFound, MethodNotAllowed, BadRequest
-
+'''
+    app for registering blueprint and starting flask
+'''
+flask import Flask, make_response, jsonify
+from flask_cors import CORS
+from models import storage
 from api.v1.views import app_views
-from models import storage, storage_t
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
+from os import getenv
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET', 'POST'])
-@app_views.route('/cities/<city_id>', methods=['GET', 'DELETE', 'PUT'])
-def handle_cities(state_id=None, city_id=None):
-    '''The method handler for the cities endpoint.
+app = Flask(__name__)
+CORS(app, origins="0.0.0.0")
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def tear_down(self):
     '''
-    handlers = {
-        'GET': get_cities,
-        'DELETE': remove_city,
-        'POST': add_city,
-        'PUT': update_city,
-    }
-    if request.method in handlers:
-        return handlers[request.method](state_id, city_id)
-    else:
-        raise MethodNotAllowed(list(handlers.keys()))
-
-
-def get_cities(state_id=None, city_id=None):
-    '''Gets the city with the given id or all cities in
-    the state with the given id.
+    close query after each session
     '''
-    if state_id:
-        state = storage.get(State, state_id)
-        if state:
-            cities = list(map(lambda x: x.to_dict(), state.cities))
-            return jsonify(cities)
-    elif city_id:
-        city = storage.get(City, city_id)
-        if city:
-            return jsonify(city.to_dict())
-    raise NotFound()
+    storage.close()
 
 
-def remove_city(state_id=None, city_id=None):
-    '''Removes a city with the given id.
+@app.errorhandler(404)
+def not_found(error):
     '''
-    if city_id:
-        city = storage.get(City, city_id)
-        if city:
-            storage.delete(city)
-            if storage_t != "db":
-                for place in storage.all(Place).values():
-                    if place.city_id == city_id:
-                        for review in storage.all(Review).values():
-                            if review.place_id == place.id:
-                                storage.delete(review)
-                        storage.delete(place)
-            storage.save()
-            return jsonify({}), 200
-    raise NotFound()
-
-
-def add_city(state_id=None, city_id=None):
-    '''Adds a new city.
+    return JSON formatted 404 status code response
     '''
-    state = storage.get(State, state_id)
-    if not state:
-        raise NotFound()
-    data = request.get_json()
-    if type(data) is not dict:
-        raise BadRequest(description='Not a JSON')
-    if 'name' not in data:
-        raise BadRequest(description='Missing name')
-    data['state_id'] = state_id
-    city = City(**data)
-    city.save()
-    return jsonify(city.to_dict()), 201
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-def update_city(state_id=None, city_id=None):
-    '''Updates the city with the given id.
-    '''
-    xkeys = ('id', 'state_id', 'created_at', 'updated_at')
-    if city_id:
-        city = storage.get(City, city_id)
-        if city:
-            data = request.get_json()
-            if type(data) is not dict:
-                raise BadRequest(description='Not a JSON')
-            for key, value in data.items():
-                if key not in xkeys:
-                    setattr(city, key, value)
-            city.save()
-            return jsonify(city.to_dict()), 200
-    raise NotFound()
+if __name__ == "__main__":
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
