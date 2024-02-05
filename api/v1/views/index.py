f5a1a7662b187a7995883e6271099b85d4098aa1
essaiydy <imaneessaiydy2@gmail.com>
2024-02-05 15:20:00 -0800

    task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,13 +10,20 @@ from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
 
-@app_views.rote('/status')
+@app_views.route('/status')
 def get_status():
     """returns a JSON: "status": 'OK'"""
     rep = {"status": "OK"}
 
     return jsonify(rep)
 
-@app_views.rote('/api/v1/stats')
+@app_views.route('/api/v1/stats')
 def nub_obj():
+    """Create an endpoint that retrieves the number of each objects"""
+    dictt = {}
+    lista = [State, City, User, Place, Review, Amenity]
 
+    for clase in lista:
+        count = storage.count(clase)
+        dictt[clase.__name__] = count
+    return jsonify(dictt)
