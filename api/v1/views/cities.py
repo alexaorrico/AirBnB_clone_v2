026384f3bd9a95de22629ea4d86207737ee59cd4
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-02-05 12:05:55 +0000

    fixing more errors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -18,8 +18,8 @@ def get_cities_by_state(state_id):
     if state is None:
         abort(404)
 
-        cities = [city.to_dict() for city in state.cities]
-        return jsonify(cities)
+    cities = [city.to_dict() for city in state.cities]
+    return jsonify(cities), 200
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
