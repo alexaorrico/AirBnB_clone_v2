727e86cf5ef2cc0e81bfc1b3f6c33d9dfefaaa05
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-05 20:58:45 +0100

    install flask cors and update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from os import getenv
 from models import storage
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 if getenv('HBNB_API_HOST'):
     HBNB_HOST = getenv('HBNB_API_HOST')
