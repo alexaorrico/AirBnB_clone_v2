e6daa03a1afa5ea0da5e11be073c77bcc0bcc6c2
Mohamed Ayman <mohayman2007@gmail.com>
2024-02-05 15:17:53 +0000

    0x05. AirBnB clone (RESTful API) - Style fix

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
                 self.updated_at = datetime.strptime(kwargs["updated_at"], time)
             else:
                 self.updated_at = datetime.utcnow()
-            if kwargs.get("id") is None and getattr (self, "id") is None:
+            if kwargs.get("id") is None and getattr(self, "id") is None:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
