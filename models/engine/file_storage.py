8c3a061382c26569da186d1c10b157e03c7f9801
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-02-05 18:53:10 +0100

    refactor(file_storage): Refactor documentations
    Change documentation to be PEP8 compliant.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,7 +24,7 @@ classes = {
 
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
+    """Serializes instances to a JSON file & deserializes back to instances."""
 
     # string - path to the JSON file
     __file_path = "file.json"
@@ -32,7 +32,7 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
+        """Returns the dictionary __objects."""
         if cls is not None:
             new_dict = {}
             for key, value in self.__objects.items():
@@ -42,13 +42,13 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """Sets in __objects the obj with key <obj class name>.id."""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """Serializes __objects to the JSON file (path: __file_path)."""
         json_objects = {}
         for key in self.__objects:
             json_objects[key] = self.__objects[key].to_dict()
@@ -56,7 +56,7 @@ class FileStorage:
             json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """Deserializes the JSON file to __objects."""
         try:
             with open(self.__file_path, "r") as f:
                 jo = json.load(f)
@@ -66,27 +66,28 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
+        """Delete obj from __objects if it’s inside."""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
     def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
+        """Call reload() method for deserializing the JSON file to objects."""
         self.reload()
 
     def get(self, cls, id):
-        """Retrives an object from the local storage by id"""
+        """Retrives an object from the local storage by id."""
         if cls not in classes.values():
             return
         key = cls.__name__ + "." + id
         return self.__objects.get(key, None)
 
     def count(self, cls=None):
-        """
-        Counts the number of objects ideentifed by the
-        cls or all objects if cls is None.
+        """ Counts the number of objects in storage
+
+        Objects are identifed by the cls.
+        If cls is none all objects are returned.
         """
         if cls is None:
             return len(self.__objects)
