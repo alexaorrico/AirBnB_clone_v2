c80dec2a78ae9ccc1bd9241920e56dec0575feb8
August-IE <Aeromosele9@gmail.com>
2024-02-05 10:13:03 +0000

    Improve storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,72 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+
+@unittest.skipIf(STORAGE_TYPE != 'db', 'skip if environ is not db')
+class TestCountGet(unittest.TestCase):
+    """testing Count and Get methods"""
+
+    @classmethod
+    def setUpClass(cls):
+        """sets up the class for this round of tests"""
+        print('\n\n....................................')
+        print('.......... Testing DBStorage .......')
+        print('. State, City, User, Place Amenity .')
+        print('....................................')
+        storage.delete_all()
+        cls.s = State(name="California")
+        cls.c = City(state_id=cls.s.id,
+                     name="San Francisco")
+        cls.u = User(email="betty@holbertonschool.com",
+                     password="pwd")
+        cls.p1 = Place(user_id=cls.u.id,
+                       city_id=cls.c.id,
+                       name="a house")
+        cls.p2 = Place(user_id=cls.u.id,
+                       city_id=cls.c.id,
+                       name="a house two")
+        cls.a1 = Amenity(name="Wifi")
+        cls.a2 = Amenity(name="Cable")
+        cls.a3 = Amenity(name="Bucket Shower")
+        objs = [cls.s, cls.c, cls.u, cls.p1, cls.p2, cls.a1, cls.a2, cls.a3]
+        for obj in objs:
+            obj.save()
+
+    def setUp(self):
+        """initializes new user for testing"""
+        self.s = TestCountGet.s
+        self.c = TestCountGet.c
+        self.u = TestCountGet.u
+        self.p1 = TestCountGet.p1
+        self.p2 = TestCountGet.p2
+        self.a1 = TestCountGet.a1
+        self.a2 = TestCountGet.a2
+        self.a3 = TestCountGet.a3
+
+    def test_all_reload_save(self):
+        """... checks if all(), save(), and reload function
+        in new instance.  This also tests for reload"""
+        actual = 0
+        db_objs = storage.all()
+        for obj in db_objs.values():
+            for x in [self.s.id, self.c.id, self.u.id, self.p1.id]:
+                if x == obj.id:
+                    actual += 1
+        self.assertTrue(actual == 4)
+
+    def test_get_pace(self):
+        """... checks if get() function returns properly"""
+        duplicate = storage.get('Place', self.p1.id)
+        expected = self.p1.id
+        self.assertEqual(expected, duplicate.id)
+
+
+    def test_count_all(self):
+        """... checks if count() functions with no class"""
+        count_all = storage.count()
+        expected = 8
+        self.assertEqual(expected, count_all)
+
+if __name__ == '__main__':
+    unittest.main
