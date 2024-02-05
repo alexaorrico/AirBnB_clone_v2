f3a8f76b90c6e1e4834028fdf09f5097ce5ef971
essaiydy <imaneessaiydy2@gmail.com>
2024-02-05 13:49:22 -0800

    t

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+import storage
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 @app_views.rote('/status')
 def get_status():
@@ -8,3 +16,7 @@ def get_status():
     rep = {"status": "OK"}
 
     return jsonify(rep)
+
+@app_views.rote('/api/v1/stats')
+def nub_obj():
+
