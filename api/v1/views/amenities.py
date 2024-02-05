378eb382057dbb4c74899480c58f496743032397
bakiizese <bereketzeselassie@gmail.com>
2024-02-05 02:59:09 -0800

    fix

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -40,18 +40,17 @@ def delete_amenity(amenity_id):
     am.delete()
     storage.save()
 
-    return (jsonify({}), 200)
+    return jsonify({})
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
 def create_amenity():
     '''creates an amenity instance'''
-    try:
-        data = request.get_json()
-    except Exception:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    data = request.get_json()
 
-    if 'name' not in data:
+    if not data:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    elif 'name' not in data:
         return make_response(jsonify({'error': 'Missing name'}), 400)
 
     create = Amenity(**data)
@@ -66,21 +65,20 @@ def create_amenity():
                  strict_slashes=False)
 def update_amenity(amenity_id):
     '''update amenity by id'''
+    data = request.get_json()
+    
+    if not data:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+
     am = storage.get(Amenity, amenity_id)
 
     if am is None:
         abort(404)
 
-    try:
-        data = request.get_json()
-    except Exception:
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-
     for key, value in data.items():
-        if key == 'id' or key == 'created_at' or key == 'updated_at':
-            continue
-        setattr(am, key, value)
-
+        if key not in {'id', 'created_at', 'updated_at'}:
+            setattr(am, key, value)
+ 
     storage.save()
     am = am.to_dict()
 
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -64,15 +64,15 @@ def create_state():
                  strict_slashes=False)
 def update_state(state_id):
     '''update state by id'''
-    stat = storage.get(State, state_id)
-
-    if stat is None:
-        abort(404)
-
     data = request.get_json()
 
     if not data:
         return make_response(jsonify({"error": "Not a JSON"}), 400)
+
+    stat = storage.get(State, state_id)
+
+    if stat is None:
+        abort(404)
 
     for key, value in data.items():
         if key not in {'id', 'created_at', 'updated_at'}:
