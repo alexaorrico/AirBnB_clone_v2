1f6e08c530dff6ba26740b43c724832338550b16
Hazem Magdy <hhazem0010@gmail.com>
2024-02-05 04:06:26 +0200

    fix server error when call /status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """a module as an API"""
+from api.v1.views import app_views
 from flask import Flask
 from models import storage
-from api.v1.views import app_views
 from os import getenv
 
 
 app = Flask(__name__)
-
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def close_storage():
+def close_storage(error):
     """a function to call storage.close()"""
-    storage.close()
+    return storage.close()
 
 
 if __name__ == "__main__":
     api_host = getenv("HBNB_API_HOST", "0.0.0.0")
-    api_port = int(getenv("HBNB_API_PORT", "5000"))
+    api_port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=api_host, port=api_port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """a module as flask blueprint"""
-from flask import Blueprint, render_template, abort
-from api.v1.views.index import *
+from flask import Blueprint
+
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
