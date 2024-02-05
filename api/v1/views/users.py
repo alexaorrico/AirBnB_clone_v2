1730ea3a80b7ca80c304c8ad6a9f3a44e8d555d7
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-02-06 01:15:33 +0200

    user fix

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -51,7 +51,7 @@ def users(user_id=None):
 
         data = request.get_json()
         if not data:
-            return make_response(jsonify({'error': 'not a json'}), 400)
+            return make_response(jsonify({'error': 'Not a JSON'}), 400)
 
         for key, value in data.items():
             if key not in ['id', 'email', 'created_at', 'updated_at']:
