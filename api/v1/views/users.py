406264a692e2a86e69494e12213fab16e92d431c
HichamLuffy <imalexvssteve2020@gmail.com>
2024-02-05 20:17:19 +0100

    update user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,7 +1,69 @@
 #!/usr/bin/python3
-"""amenities view"""
+"""users view"""
 
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.user import User
+from models.state import State
+
+
+@app_views.route("/users", strict_slashes=False, methods=["GET"])
+@app_views.route("/users/<users_id>", strict_slashes=False,
+                 methods=["GET"])
+def get_users(user_id=None):
+    """retrives the list of all users object of state"""
+    list_user = []
+    if user_id in None:
+        all_objs = storage.all(User).values()
+        for v in all_objs:
+            list_user.append(v.to_dict())
+        return jsonify(list_user)
+    else:
+        result = storage.get(User, user_id)
+        if result is None:
+            abort(404)
+        return jsonify(result.to_dict())
+
+
+@app_views.route("/users/<user_id>", strict_slashes=False,
+                 methods=["DELETE"])
+def delete_user(user_id):
+    """deletes a user object"""
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    storage.delete(user)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/users", strict_slashes=False, methods=["POST"])
+def create_user():
+    """create a new post reqs"""
+    data = request.get_json(force=True, silent=True)
+    if not data:
+        abort(400)
+    if "name" not in data:
+        abort(400, "Missing name")
+    new_user = User(**data)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route("/users/<user_id>", strict_slashes=False,
+                 methods=["PUT"])
+def update_user(user_id):
+    """updates a amenity object"""
+    user_obj = storage.get(User, user_id)
+    if not user_obj:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    data = request.get_json(force=True, silent=True)
+    ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(user_obj, key, value)
+    user_obj.save()
+    return jsonify(user_obj.to_dict()), 200
