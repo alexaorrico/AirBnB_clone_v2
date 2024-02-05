1f546ff7a52c4e37376541c1f64d896ba19bd91b
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-02-06 01:06:57 +0200

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -35,8 +35,10 @@ def users(user_id=None):
         data = request.get_json()
         if not data:
             return make_response(jsonify({'error': 'Not a JSON'}), 400)
-        elif 'name' not in data:
-            return make_response(jsonify({'error': 'Missing name'}), 400)
+        elif 'email' not in data:
+            return make_response(jsonify({'error': 'Missing email'}), 400)
+        elif 'password' not in data:
+            return make_response(jsonify({'error': 'Missing password'}), 400)
         else:
             user = User(**data)
             user.save()
@@ -52,7 +54,7 @@ def users(user_id=None):
             return make_response(jsonify({'error': 'not a json'}), 400)
 
         for key, value in data.items():
-            if key not in ['id', 'created_at', 'updated_at']:
+            if key not in ['id', 'email', 'created_at', 'updated_at']:
                 setattr(user, key, value)
         user.save()
         return make_response(jsonify(user.to_dict()), 200)
