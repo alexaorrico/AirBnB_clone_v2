9d66da801b5de85fe878d9058fcd45fbdeb98f78
Lamin Jawla <laminjawla1001@gmail.com>
2024-02-05 09:00:47 +0000

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,12 @@ from api.v1.views import app_views
 from flask import Flask, make_response, jsonify
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
