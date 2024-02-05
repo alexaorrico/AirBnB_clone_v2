33afe6dc45794c78699057e5ef663b2ee4804559
Mohamed Ayman <mohayman2007@gmail.com>
2024-02-05 10:59:59 +0000

    0x05. AirBnB clone (RESTful API) - Adding Not found (Error 404) handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,12 @@ def close(eexception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(e):
+    '''Returns the JSON {"error": "Not found"} if resource wasn't found'''
+    return jsonify({"error": "Not found"}), e.code
+
+
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST", '0.0.0.0'),
             port=getenv("HBNB_API_PORT", '5000'),
