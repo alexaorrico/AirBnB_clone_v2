9cfd1721d6a7685d4fa9c02bb76512b6ada205f5
root <hakeemabdullah87@gmail.com>
2024-02-05 05:59:10 -0800

    FEAT: update the app.py to handle error page not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
 """AirBnB Clone API configuration file"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, make_reponse
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+
+
 app = Flask(__name__)
 
 
 # Register the blueprint
 app.register_blueprint(app_views)
-
+app.url_map.strict_slashes = False
 
 @app.teardown_appcontext
 def teardown_app(exception):
     """disconnect the db session """
     storage.close()
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """route for not found page"""
+    err = {
+        "error": "Not Found"
+    }
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
