29590b40111696c750a26ad8cc8111adc51a0cff
EmyJuly <farsimane00@gmail.com>
2024-02-05 19:42:59 +0000

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,20 +1,17 @@
+#!/usr/bin/python3
+'''Contains the blueprint for the API.'''
 from flask import Blueprint
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-"""import storage engine and classes"""
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.user import User
-from models.place import Place
-from models.review import Review
 
-"""import flask views"""
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
+
+
 from api.v1.views.amenities import *
-from api.v1.views.users import *
+from api.v1.views.cities import *
+from api.v1.views.index import *
+from api.v1.views.places_amenities import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
