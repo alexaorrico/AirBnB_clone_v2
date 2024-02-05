1183110e1dd8411fa08a68bc0ac31e04bda99749
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2024-02-05 18:52:03 +0000

    added get & count functionality to engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class
+        and its ID, or None if not found
+        """
+        k = "{}.{}".format(cls, id)
+        return (self.all(cls).get(k))
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching
+        the given class. If no class is passed,
+        returns the count of all objects in storage.
+        """
+        return (len(self.all(cls)))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Returns the object based on the class
+        and its ID, or None if not found
+        """
+        k = "{}.{}".format(cls, id)
+        return (self.all(cls).get(k))
+
+    def count(self, cls=None):
+        """
+        Returns the number of objects in storage matching
+        the given class. If no class is passed,
+        returns the count of all objects in storage.
+        """
+        return (len(self.all(cls)))
