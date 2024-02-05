38633e59c3d647ea7ef4ff2df56a72f19731a178
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-02-05 23:33:13 +0200

    CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Contains the status of the API  """
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
