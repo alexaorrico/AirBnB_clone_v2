9927b5d7a103574b600771e3136102e8ab6e46ed
Pop2323 <omar4500455@gmail.com>
2024-02-05 17:24:20 -0800

    AirBnB_Clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""start API"""
 from flask import Flask, render_template, jsonify
 from models import storage
 from api.v1.views import app_views
@@ -9,21 +8,19 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
-port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else '5000'
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
+cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
 
 
 @app.teardown_appcontext
-def close(self):
-    """close session"""
+def teardown_db(self):
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(err=None):
-    """404 Not Found"""
+def page_not_found(e):
+    """404 Page Not Found"""
     return jsonify({"error": "Not found"}), 404
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host=host, port=port, threaded=True)
