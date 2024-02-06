9fac1aa5d420b952f8687346d95481f4f840bdaa
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-02-06 12:35:11 +0000

    Task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """This Creates a Flask app and registers the blueprint app_views
 to Flask instance app"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from .views import app_views
 import os
@@ -15,6 +15,11 @@ app.url_map.strict_slashes = False
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
