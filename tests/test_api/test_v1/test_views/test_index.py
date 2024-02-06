9036992f07c1b8f7f977f3d74a4830663d9afdec
omotomiwa26 <omotomiwa26@gmail.com>
2024-02-06 01:42:49 +0000

    all pep8 errors fixed in test directory

--- tests/test_api/test_v1/test_views/test_index.py
+++ tests/test_api/test_v1/test_views/test_index.py
@@ -13,13 +13,12 @@ storage = getenv("HBNB_TYPE_STORAGE")
 
 class TestIndex(unittest.TestCase):
     '''test index'''
-    def test_status(self):        
+    def test_status(self):
         '''test status function'''
         with app.test_client() as c:
             resp = c.get('/api/v1/status')
             data = json.loads(resp.data.decode('utf-8'))
             self.assertEqual(data, {'status': 'OK'})
-
 
     def test_count(self):
         '''test count'''
