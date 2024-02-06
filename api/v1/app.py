d82dfba835aa6f8bdca1698fdf41ddded44b4d41
Olakunle64 <olakunleisiaq@gmail.com>
2024-02-06 23:06:27 +0000

    adjust the app.py documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""This module contain a flask web application framework
-    for the airbnb website
-
-    It contains two functions
+"""
+This module contain a flask web application framework for the airbnb website
 """
 import os
 from flask import Flask, jsonify
