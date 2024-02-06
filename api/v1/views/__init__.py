1ecc48f27ab91a96c00da97e7138050d930d265a
njine <brikab@gmail.com>
2024-02-07 00:47:23 +0300

    revamp

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python3
+'''
+Create a Blueprint instance configured with a url_prefix of /api/v1.
+'''
+
+
 from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
@@ -10,3 +13,5 @@ from api.v1.views.states import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
