f1484e2d9b7337c1ca2b84734a4879f0448bc003
root <hakeemabdullah87@gmail.com>
2024-02-05 20:30:02 -0800

    FEAT: add the init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,9 @@
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.reviews import *
+from api.v1.views.amenities import *
+from api.v1.views.places import *
+from api.v1.views.users import *
 
 from flask import Blueprint
 
