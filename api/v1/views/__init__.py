6841ec160c9bb92837719e8811ce98a0044fe696
njine <brikab@gmail.com>
2024-02-07 00:59:52 +0300

    revamp

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-'''
-Create a Blueprint instance configured with a url_prefix of /api/v1.
-'''
-
+"""
+Create Blueprint instance with `url_prefix` set to '/api/v1'.
+"""
 
 from flask import Blueprint
+
+# Create a Blueprint instance with the specified url_prefix
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import views from the respective modules
 from api.v1.views.index import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.users import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
