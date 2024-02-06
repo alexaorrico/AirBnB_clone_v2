fb92f655a67a5dbcc2798a618c4a5bbf8c14a47e
HazemUsama <hazemosama681@gmail.com>
2024-02-06 02:37:32 +0200

    Fixes

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,7 +10,7 @@ from models.city import City
 
 
 @app_views.route('/states/<string:state_id>/cities', strict_slashes=False,
-                 methods=['GET', 'PUT'])
+                 methods=['GET', 'POST'])
 def city_state(state_id=None):
     """Retrieves and Creates a City given state_id"""
     if request.method == 'GET':
@@ -20,24 +20,21 @@ def city_state(state_id=None):
         cities = [city.to_dict() for city in state.cities]
         return jsonify(cities)
 
-    elif request.method == 'PUT':
-        city = storage.get(City, city_id)
-        if city is None:
-            abort(404)
-
+    elif request.method == 'POST':
         data = request.get_json()
         if not data:
-            return make_response(jsonify({'error': 'not a json'}), 400)
-
-        for key, value in data.items():
-            if key not in ['id', 'created_at', 'updated_at']:
-                setattr(City, key, value)
-        city.save()
-        return make_response(jsonify(city.to_dict()), 200)
+            return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        elif 'name' not in data:
+            return make_response(jsonify({'error': 'Missing name'}), 400)
+        else:
+            data['state_id'] = state_id
+            city = City(**data)
+            city.save()
+            return make_response(jsonify(city.to_dict()), 201)
 
 
 @app_views.route('/cities/<string:city_id>', strict_slashes=False,
-                 methods=['GET', 'DELETE', 'POST'])
+                 methods=['GET', 'DELETE', 'PUT'])
 def city(city_id=None):
     if request.method == 'GET':
 
@@ -54,13 +51,18 @@ def city(city_id=None):
         storage.save()
         return make_response(jsonify({}), 200)
 
-    elif request.method == 'POST':
+    elif request.method == 'PUT':
+        city = storage.get(City, city_id)
+        if city is None:
+            abort(404)
+
         data = request.get_json()
         if not data:
-            return make_response(jsonify({'error': 'Not a JSON'}), 400)
-        elif 'name' not in data:
-            return make_response(jsonify({'error': 'Missing name'}), 400)
-        else:
-            city = City(**data)
-            city.save()
-            return make_response(jsonify(city.to_dict()), 201)
+            return make_response(jsonify({'error': 'not a json'}), 400)
+
+        for key, value in data.items():
+            if key not in ['id', 'created_at', 'updated_at']:
+                setattr(city, key, value)
+        city.save()
+        return make_response(jsonify(city.to_dict()), 200)
+
