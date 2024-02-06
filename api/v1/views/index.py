adb4b9fc6376419ecc72c7e709ce0243618795b6
0'Brien <abucheriobrien@gmail.com>
2024-02-06 12:46:05 -0800

    test run_1

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -31,9 +31,9 @@ def stat():
     Returns:
         A JSON response containing the counts of different object types.
     '''
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
