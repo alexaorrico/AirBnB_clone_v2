e1ece1828f06dd746d50c7e2dd94f9985bae8317
esraaashraf023 <esraaashraf780567@gmail.com>
2024-02-06 23:26:30 +0200

    UPdate

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,7 @@
-#!/usr/bin/python3
-"""This module initializes the blueprint app_views"""
-
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
-from models import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
