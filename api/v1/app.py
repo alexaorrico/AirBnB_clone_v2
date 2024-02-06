dcb9b0a7f356e67a553d6b72b23077e278d5c7c4
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-02-06 19:23:57 +0000

    Updating API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
 '''
-    app for registering blueprint and starting flask
+Create Flask app
 '''
-from flask import Flask, make_response, jsonify
+
+from os import getenv
+from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import getenv
-
 
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
+
+# enable CORS and allow for origins:
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
+
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def tear_down(self):
+def teardown_engine(exception):
     '''
-    close query after each session
+    Removes the current SQLAlchemy Session object after each request.
     '''
     storage.close()
 
 
+# Error handlers for expected app behavior:
 @app.errorhandler(404)
 def not_found(error):
     '''
-    return JSON formatted 404 status code response
+    Return errmsg `Not Found`.
     '''
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    response = {'error': 'Not found'}
+    return jsonify(response), 404
 
 
-if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
 '''
-    flask with general routes
-    routes:
-        /status:    display "status":"OK"
-        /stats:     dispaly total for all classes
+Create a route `/status` on the object app_views.
 '''
-from api.v1.views import app_views
+
+
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route("/status")
-def status():
+@app_views.route('/status', methods=['GET'])
+def api_status():
     '''
-        return JSON of OK status
+    Returns a JSON response for RESTful API health.
     '''
-    return jsonify({'status': 'OK'})
+    response = {'status': 'OK'}
+    return jsonify(response)
 
 
-@app_views.route("/stats")
-def storage_counts():
+@app_views.route('/stats', methods=['GET'])
+def get_stats():
     '''
-        return counts of all classes in storage
+    Retrieves the number of each objects by type.
     '''
-    cls_counts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
     }
-    return jsonify(cls_counts)
+    return jsonify(stats)
