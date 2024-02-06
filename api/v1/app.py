9f2f0db2a33c534c8714290cba3f745708443463
August-IE <Aeromosele9@gmail.com>
2024-02-06 10:17:29 +0000

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,15 @@
 Flask App that integrates with AirBnB static HTML Template
 """
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response, render_template, url_for
+from flask_cors import CORS, cross_origin
 from models import storage
 import os
+from werkzeug.exceptions import HTTPException
 
 # Global Flask Application Variable: app
 app = Flask(__name__)
+swagger = Swagger(app)
 
 # global strict slashes
 app.url_map.strict_slashes = False
@@ -28,13 +31,63 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    This method calls storage.close()
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(400)
+def handle_404(exception):
+    """
+    handles 400 errros, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+@app.errorhandler(Exception)
+def global_error_handler(err):
+    """
+        Global Route to handle All Error Status Codes
+    """
+    if isinstance(err, HTTPException):
+        if type(err).__name__ == 'NotFound':
+            err.description = "Not found"
+        message = {'error': err.description}
+        code = err.code
+    else:
+        message = {'error': err}
+        code = 500
+    return make_response(jsonify(message), code)
+
+
+def setup_global_errors():
+    """
+    This updates HTTPException Class with custom error function
+    """
+    for cls in HTTPException.__subclasses__():
+        app.register_error_handler(cls, global_error_handler)
+
 
 if __name__ == "__main__":
     """
     MAIN Flask App
     """
+    # initializes global error handling
+    setup_global_errors()
     # start Flask app
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,8 +10,28 @@ from models import storage
 @app_views.route('/status', methods=['GET'])
 def status():
     """
-    function for status route
+    function for status route that returns the status
     """
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
