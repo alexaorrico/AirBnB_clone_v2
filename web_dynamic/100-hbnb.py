5a97dbf43e681f53fc7e3335fb358f473621e5f2
EngrBassey <basseyleads001@gmail.com>
2024-02-11 06:10:59 +0000

    Cash only

---
+++ web_dynamic/100-hbnb.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+from flask import Flask, render_template, url_for
+from models import storage
+import uuid;
+
+# flask setup
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
+
+
+# begin flask page rendering
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route('/100-hbnb')
+def hbnb_filters(the_id=None):
+    """
+    handles request to custom template with states, cities & amentities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([state.name, state] for state in state_objs)
+    amens = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                 for user in storage.all('User').values())
+    return render_template('100-hbnb.html',
+                           cache_id=uuid.uuid4(),
+                           states=state_objs,
+                           amens=amens,
+                           places=places,
+                           users=users)
+
+if __name__ == "__main__":
+    """
+    MAIN Flask App"""
+    app.run(host=host, port=port)
