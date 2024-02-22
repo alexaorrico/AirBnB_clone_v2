80c8a00788e3317598ec4fe80c62e89de5ade072
DouibaOmar <douibaomar40@gmail.com>
2024-02-22 16:04:37 +0100

    Done

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,12 +6,13 @@ from flask import Flask
 import models
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-HBNB_API_HOST = '0.0.0.0'
-HBNB_API_PORT = 5000
+HBNB_API_HOST = getenv('HBNB_API_PORT')
+HBNB_API_PORT = getenv('HBNB_API_PORT')
 
 
 @app.teardown_appcontext
@@ -21,5 +22,9 @@ def close_db(self):
 
 
 if __name__ == "__main__":
-    app.run(HBNB_API_HOST, HBNB_API_PORT,
-            debug=True, threaded=True)
+    if HBNB_API_HOST and HBNB_API_PORT:
+        app.run(HBNB_API_HOST, HBNB_API_PORT,
+                debug=True, threaded=True)
+    else:
+        app.run(host='0.0.0.0', port=5000,
+                debug=True, threaded=True)
