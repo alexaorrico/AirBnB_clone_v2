602c779f07c572453d8144515cd8e050748ea156
Emran <ouakouriimran@gmail.com>
2024-02-22 20:10:13 +0100

    2-task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,15 +3,18 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
 @app.teardown_appcontext
-def session_close():
+def session_close(exception):
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
 
 
 @app_views.route('/status')
