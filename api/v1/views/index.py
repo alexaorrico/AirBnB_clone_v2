87bdb0342304e21544e3caf1a5774a858d742f02
Hatim Selmun <selmun13@gmail.com>
2024-02-22 14:17:12 +0100

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,26 @@ Index with one route
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", methods=['GET'])
 def status():
     return jsonify({"status": "ok"})
+
+
+@app_views.route("/stats", methods=['GET'])
+def num_of_obj():
+    dict = {}
+    classes = {'states': State, 'amenities': Amenity,
+               'cities': City, 'places': Place,
+               'reviews': Review, 'users': User}
+    for name, clas in classes.items():
+        dict[name] = storage.count(clas)
+    return jsonify(dict)
