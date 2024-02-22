b4f9f00881d090f2a355c5f822a9ccfc70bed551
Hatim Selmun <selmun13@gmail.com>
2024-02-22 18:55:03 +0100

    working on states

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ import os
 app = Flask(__name__)
 
 
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 @app.teardown_appcontext
 def tear(error):
     storage.close()
@@ -17,7 +17,7 @@ def tear(error):
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({"error": "Not found"}))
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,5 +3,6 @@
 Package
 """
 from flask import Blueprint
-app_views = Blueprint("views", __name__)
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
+from api.v1.views.states import *
