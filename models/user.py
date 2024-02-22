6de4abd9e8d6238ff13bf72e49ff0e472c39206d
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-22 23:34:04 +0200

    Some import adjusments

--- models/user.py
+++ models/user.py
@@ -3,7 +3,6 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
