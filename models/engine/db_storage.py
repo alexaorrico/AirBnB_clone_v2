b7cb4d05c08aea20d2b1641a6905c5c6b6f15228
Evarmedia <mishakmanuel@gmail.com>
2024-02-23 19:46:29 +0100

    task#2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -51,6 +51,17 @@ class DBStorage:
                     new_dict[key] = obj
         return (new_dict)
 
+    def get(self, cls, id):
+        ''' A method to retrieve one object '''
+        obj = None
+        if cls is not None and issubclass(cls, BaseModel):
+            obj = self.__session.query(cls).filter(cls.id == id).first()
+	return obj
+
+    def count(self, cls=None):
+        ''' Returns the number of objects in storage matching the given class '''
+	    return len(self.all(cls))
+
     def new(self, obj):
         """add the object to the current database session"""
         self.__session.add(obj)
