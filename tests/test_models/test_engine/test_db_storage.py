f75616f4219d312f6d3ca1b15233ca5aa1330619
fatimazaarate <fatima.zaarate@gmail.com>
2024-02-23 13:46:54 +0100

    test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -18,6 +18,7 @@ import json
 import os
 import pep8
 import unittest
+from models import storage
 DBStorage = db_storage.DBStorage
 classes = {"Amenity": Amenity, "City": City, "Place": Place,
            "Review": Review, "State": State, "User": User}
@@ -86,3 +87,25 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+    def test_get(self):
+        """ Test get method based on cls and id """
+        state1 = State(name="California")
+        storage.new(state1)
+        storage.save()
+        result = storage.get(State, state1.id)
+        self.assertEqual(result, state1)
+
+    def test_count(self):
+        """ test for counting objects for db """
+        s1 = State(name="New York")
+        s2 = State(name="Arizona")
+        city = City(name="Albany", state_id=st1.id)
+        storage.new(s1)
+        storage.new(s2)
+        storage.new(city)
+        storage.save()
+        all_objs = storage.count()
+        self.assertEqual(all_objs, len(storage.all()))
+        city_objects = storage.count(City)
+        self.assertEqual(city_objects, len(storage.all(City)))
