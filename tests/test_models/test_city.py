1a2a8342ddaa8d57042b2beab6b838f88d62464c
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-23 22:15:22 +0200

    :"(

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -92,7 +92,7 @@ class TestCity(unittest.TestCase):
         self.assertEqual(type(new_d), dict)
         self.assertFalse("_sa_instance_state" in new_d)
         for attr in c.__dict__:
-            if attr is not "_sa_instance_state":
+            if attr != "_sa_instance_state":
                 self.assertTrue(attr in new_d)
         self.assertTrue("__class__" in new_d)
 
