bb5e3acbd635de5cb706cb426266424ae90bad2c
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-23 22:06:20 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" api/v1/views/index.py """
+""" API Status and Object Statistics """
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 from models.amenity import Amenity
@@ -7,10 +9,20 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-from models.user import User
-from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
+def api_status():
+    """ Endpoint to get the status of the API """
     return jsonify({"status": "OK"})
 
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def objects_statistics():
+    """ Endpoint to retrieve the number of each object type """
+    object_types = [Amenity, City, Place, Review, State, User]
+    type_names = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    object_counts = {}
+    for i in range(len(object_types)):
+        object_counts[type_names[i]] = storage.count(object_types[i])
+
+    return jsonify(object_counts)
