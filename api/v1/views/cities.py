47a5e2f11dfb25d8d562eadfb7240fa516b7483f
DouibaOmar <douibaomar40@gmail.com>
2024-02-23 15:37:15 +0100

    Done

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -3,14 +3,14 @@
 text
 """
 from models import storage
-import models
+from api.v1.views import app_views
+from models import storage
 from models.state import State
 from models.city import City
-from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, abort, request
 
 
-@app_views('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>/cities', methods=['GET'], strict_slashes=False)
 def states(state_id=None):
     states = storage.get(State, state_id)
     if states is None:
@@ -22,7 +22,7 @@ def states(state_id=None):
         return jsonify(cities)
 
 
-@app_views('/cities/<city_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<string:city_id>', methods=['GET'], strict_slashes=False)
 def cities(city_id=None):
     cities = storage.get(City, city_id)
     if cities is None:
