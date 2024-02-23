903eee8aff5b81ee429c8fda4b83d258c3526476
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-23 21:12:23 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,8 @@
+# api/v1/views/index.py
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    return jsonify({"status": "OK"})
 
