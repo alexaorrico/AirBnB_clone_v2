635f841d50f16aa3a4f163e2cdcae85a34ff8b14
mostafashraf18 <ma2963629@gmail.com>
2024-02-23 15:36:16 +0200

    fix the base model

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
-
-if models.storage_t == "db":
+if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
     Base = declarative_base()
 else:
     Base = object
@@ -21,7 +20,7 @@ else:
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
-    if models.storage_t == "db":
+    if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
