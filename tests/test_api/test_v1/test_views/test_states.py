0492d556d1d3361f49f510ada09929ae874b59f0
Garrick Larson Potts <ricki.glp@gmail.com>
2024-02-23 05:46:47 -0800

    added from test files only for structure and base code to work from didnt test all faccests yet

---
+++ tests/test_api/test_v1/test_views/test_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is the unittest for the States module"""
+
+import unittest
+
+
+class TestStatesAPI(unittest.TestCase):
+    def test_pass(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
