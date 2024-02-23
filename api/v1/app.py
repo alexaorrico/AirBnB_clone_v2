1f2d4edadf97fb3acbc9dc7c633ca4df16725a93
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-23 21:34:22 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # api/v1/app.py
-from flask import Flask
+from flask import Flask, jsonify
+from werkzeug.exceptions import NotFound
 from api.v1.views import app_views
 from models import storage
 
@@ -11,7 +12,13 @@ app.register_blueprint(app_views)
 def teardown(exception):
     storage.close()
 
+# Custom 404 error handler
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = int(os.getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
+
