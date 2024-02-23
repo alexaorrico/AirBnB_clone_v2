2c1f08e92cd92db82c9271ef1016e17bf88b9c0a
Cha1mae <chaimastaifa@gmail.com>
2024-02-23 10:46:35 -0800

    added content for app file

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This code sets up a Flask web application with API views
+it ensures the database connection closes properly
+and runs the app on a specified host and port"""
+from os import getenv
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close storage on teardown."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=port, threaded=True)
