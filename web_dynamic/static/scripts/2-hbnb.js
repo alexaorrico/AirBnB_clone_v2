d0f98167562ebdfd91ed8b554dcb49dd96a3b82a
yassminee00 <yassmine00elgh@gmail.com>
2024-02-23 12:09:32 +0000

    updated

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,24 +1,45 @@
-$('document').ready(function () {
-  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
-  $.get(url, function (response) {
-    if (response.status === 'OK') {
-      $('DIV#api_status').addClass('available');
-    } else {
-      $('DIV#api_status').removeClass('available');
-    }
-  });
+$(document).ready(init);
 
-  let amenities = {};
-  $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(':checked')) {
-      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-      delete amenities[$(this).attr('data-id')];
-    }
-    if (Object.values(amenities).length === 0) {
-      $('.amenities H4').html('&nbsp;');
-    } else {
-      $('.amenities H4').text(Object.values(amenities).join(', '));
-    }
-  });
+// The host URL for the API
+const HOST = 54.88.134.53;
+
+// Initialization function
+function init() {
+        // An object to store selected amenities
+        const amenityObj = {};
+
+        // Listen for changes on checkboxes inside the ".amenities .popover" element
+        $('.amenities .popover input').change(function () {
+                // If the checkbox is checked, add its data-name and data-id to the amenityObj
+                if ($(this).is(':checked')) {
+                        amenityObj[$(this).attr('data-name')] = $(this).attr('data-id');
+                } else if ($(this).is((':not(:checked)')) {
+                        // If the checkbox is unchecked, remove its data-name from the amenityObj
+                        delete amenityObj[$(this).attr('data-name')];
+                }
+
+
+                // Get the names of selected amenities, sort them, and update the content of the H4 element inside ".amenities"
+                const names = Object.keys(amenityObj);
+                $('.amenities h4').text(names.sort().join(', '));
+        });
+
+        // Call the apiStatus function to check the API status
+        apiStatus();
+}
+
+// Function to check the API status
+function apiStatus() {
+        // The API endpoint URL
+        const API_URL = 'http://${HOST}:5001/api/v1/status/';
+
+        // send a GET request to the API to get its status
+        $.get(API_URL, (data, textStatus) => {
+                // If the request is successful and the API status is 'OK', add the 'available' class to the element with the ID 'api_status'
+                $('#api_status').addClass('available');
+        } else {
+                // If the API status is not 'OK', remove the 'available' class from the element with ID 'api_status'
+                $('#api_status').removeClass('available');
+        }
 });
+}
