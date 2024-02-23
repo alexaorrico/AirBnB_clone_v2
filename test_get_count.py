c47f2d6cf7ef1617d7d896b3e2bb27ac236d4388
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-23 17:37:41 +0200

    changed the test file

--- test_get_count.py
+++ test_get_count.py
@@ -8,4 +8,4 @@ print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
 
 first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get("State", first_state_id)))
+print("First state: {}".format(storage.get(State, first_state_id)))
