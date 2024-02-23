5c0b5656f05b5f0f02322cd42f1c23a0b0296bd5
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-23 21:42:06 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-# api/v1/views/index.py
+""" api/v1/views/index.py """
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
