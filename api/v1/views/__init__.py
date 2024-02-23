5c2549f0de51c33d0167b190b513e4056efc5e28
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-23 21:11:58 +0200

    Update __init__.py

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,7 @@
+# api/v1/views/__init__.py
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Wildcard import is intentional; PEP8 may complain, but it's normal.
+from api.v1.views.index import *
