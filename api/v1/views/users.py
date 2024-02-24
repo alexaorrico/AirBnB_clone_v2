6c98d1eccb61a361012b2443a3efedc7eaa1c723
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-24 16:41:26 +0300

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+"""
+Routes for handling User objs and operations
+"""
+from flask import jsonify, abort, request
+from api.v1.views import app_views, storage
+from models.user import User
 
+
+@app_views.route("/users", methods=["GET"], strict_slashes=False)
+def user_get_all():
+  """
+  Retrives all User objects
+  :Return: json of all users
+  """
+  user_list = []
+  user_obj = storage.all("User")
+  for obj in user_obj.values():
+    user_list.append(obj.to_json())
+  
+  return jsonify(user_list)
