838d2a3e6b7ebb11369834f3a73b2b0893d7c3bb
oussama-el1 <elhadraoui284@gmail.com>
2024-02-24 21:38:43 +0100

    from storage_get_count

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,7 +5,7 @@ Contains the class DBStorage
 
 import models
 from models.amenity import Amenity
-from models.base_model import BaseModel, Base
+from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
