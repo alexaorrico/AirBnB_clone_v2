58cd2d1620ffc02eab236ecdbb4bdc2d6ba08c9c
DouibaOmar <douibaomar40@gmail.com>
2024-02-24 15:37:02 +0100

    Done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,18 +18,11 @@ def status():
     return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'],
-                 strict_slashes=False)
-def get_stats():
-    """
-    Retrieves the number of objects by type.
-    """
-    stats = {
-        'amenities': storage.count(Amenity),
-        'cities': storage.count(City),
-        'places': storage.count(Place),
-        'reviews': storage.count(Review),
-        'states': storage.count(State),
-        'users': storage.count(User)
-    }
-    return jsonify(stats)
+@app_views.route('/stats', strict_slashes=False)
+# This code has the advantage of not returning non-existing objects
+def stats():
+    """display the number of each objects by type"""
+    all_classes = {"Amenity": "amenities", "City": "cities", "Place": "places",
+                   "Review": "reviews", "State": "states", "User": "users"}
+    return jsonify({v: storage.count(k) for k, v in all_classes.items()
+                    if storage.count(k)})
