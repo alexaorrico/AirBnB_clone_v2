42c688574cf18f1b320d0eefdd3d9bf20cb397a8
MDIMACat <romanmdima@gmail.com>
2024-02-24 19:12:12 +0200

    Task 6, States views

---
+++ api/v1/views/states.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+States view
+"""
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+from flask import jsonify, abort, request
+
+
+@app_views.route('/states', methods=["GET"])
+def get_state():
+    """ Gets a single state
+    """
+    state_list = []
+    state_obj = storage.all("State")
+    for obj in state_obj.values():
+        state_list.append(obj.to_json())
+
+    return jsonify(state_list)
+
+
+@app_views.route("/states/<state_id>", methods=["GET"])
+def get_states(state_id):
+    """ Gets state based on id passed"""
+    state_obj = storage.get("State", str(state_id))
+    if state_obj is None:
+        abort(404)
+
+    return jsonify(state_obj.to_json())
+
+
+@app_views.route("/states/<state_id>", methods=["DELETE"])
+def del_state(state_id):
+    """ Deletes a state using ID"""
+    state_obj = storage.get('State', str(state_id))
+    if state_obj is None:
+        abort(404)
+    else:
+        storage.delete(state_obj)
+        storage.save()
+    return jsonify({})
+
+
+@app_views.route("/states", methods=["POST"])
+def create_state():
+    """Create a new post"""
+    new_state = request.get_json()
+    if new_state is None:
+        abort(400, "Not a JSON")
+
+    if "name" is None:
+        abort(400, "Missing name")
+
+    state_instance = State(**new_state)
+    storage.new(state_instance)
+    storage.save()
+    resp = jsonify(state_instance.to_json())
+    resp.status_code = 201
+
+    return resp
+
+
+@app_views.route("/states/<state_id>", methods=["PUT"])
+def update_states(states_id):
+    """Updates an existing state"""
+    state_obj = storage.get("State", str(states_id))
+    if state_obj is None:
+        abort(404)
+
+    new_state = request.get_json()
+    if new_state is None:
+        abort(400, "Not a JSON")
+
+    for key, value in new_state.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(state_obj, key, value)
+    state_obj.save()
+    resp = jsonify(state_obj.to_json())
+    resp.status_code = 200
+    return resp
