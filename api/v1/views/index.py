5a35620f8c013a32d73a021612d66252f15ced72
mohanned-sameh <mohanned2678@gmail.com>
2024-02-24 14:16:01 +0200

    Delete API v1 module and views

--- api/v1/views/index.py
+++
@@ -1,8 +0,0 @@
-from api.v1.views import app_views
-from flask import jsonify
-
-
-@app_views.route("/status", methods=["GET"], strict_slashes=False)
-def status():
-    """Returns a JSON"""
-    return jsonify(status="OK")
