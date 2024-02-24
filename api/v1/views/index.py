3b933071833550de864aa7c29eb77452475d8e1d
DouibaOmar <douibaomar40@gmail.com>
2024-02-24 11:43:36 +0100

    Done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -25,11 +25,11 @@ def get_stats():
     Retrieves the number of objects by type.
     """
     stats = {
-        'amenities': storage.count(Amenity),
-        'cities': storage.count(City),
-        'places': storage.count(Place),
-        'reviews': storage.count(Review),
-        'states': storage.count(State),
-        'users': storage.count(User)
+        'amenities': storage.count("Amenity"),
+        'cities': storage.count("City"),
+        'places': storage.count("Place"),
+        'reviews': storage.count("Review"),
+        'states': storage.count("State"),
+        'users': storage.count("User")
     }
     return jsonify(stats)
