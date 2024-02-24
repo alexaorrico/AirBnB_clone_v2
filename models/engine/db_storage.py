241d363c9a7886509b8d1e139186556b5c29f52f
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-25 00:49:39 +0100

    branch 2

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,14 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """get object by class name and id"""
+        if cls and id:
+            obj = "{}.{}".format(cls, id)
+            objs = self.all(cls)
+        return objs.get(obj)
+
+    def count(self, cls=None):
+        """get count of objects stored"""
+        return (len(self.all(cls)))
