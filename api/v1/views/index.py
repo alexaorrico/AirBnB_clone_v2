1fa90a04e10b9b80ee1f6b67492cbc39b7aa4358
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-24 15:44:12 +0200

    Removed GET method from app_review route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def show_status():
     """ Shows the api response status """
     return jsonify(status="OK")
