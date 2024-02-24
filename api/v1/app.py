d7e7edd1292af5a86b12217f7f374f160d961ace
lisacharuma <charumalisah@gmail.com>
2024-02-24 06:21:08 -0800

    Added a custom 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
     create a file app.py
 """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,12 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 def teardown(exception):
     """Teardown app context"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Returns a custom 404"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
