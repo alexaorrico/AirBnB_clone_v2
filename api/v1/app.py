e4339a1380a6aa071c263cb4b0d5a7d6e348e383
MDIMACat <romanmdima@gmail.com>
2024-02-24 15:08:15 +0200

    Made files execuatble and added shebang

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+""" Application file API
+"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+Index file
+"""
 from api.v1.views import app_views
 from flask import jsonify, make_response
 
