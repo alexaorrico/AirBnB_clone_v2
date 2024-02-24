8bcdc11081bad3cfbbe90d80e4dd17f5a354f0fa
khalildahbane <kelldani100@gmail.com>
2024-02-24 08:01:22 -0800

    Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,17 +7,40 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 
+# creating a Flask app
 app = Flask(__name__)
-app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return {"error": "Not found"}, 404
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+    message = e.description
+    return message, 400
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """Close storage on teardown."""
+def close(ctx):
     storage.close()
 
 
+if os.getenv("HBNB_API_HOST"):
+    host = os.getenv("HBNB_API_HOST")
+else:
+    host = "0.0.0.0"
+
+if os.getenv("HBNB_API_PORT"):
+    port = int(os.getenv("HBNB_API_PORT"))
+else:
+    port = 5000
+
+
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" this responds with a message """
+"""define routes of blueprint"""
+
 from api.v1.views import app_views
-from flask import jsonify
+from models import storage
 
-
-@app_views.route('/status', methods=['GET'])
+@app_views.route("/status", strict_slashes=False, methods=["GET"])
 def status():
-    return jsonify({"status": "OK"})
+    return {
+        "status": "OK",
+    }
