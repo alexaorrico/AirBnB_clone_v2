370841c57894a2b77e12a13707189708463afa27
MDIMACat <romanmdima@gmail.com>
2024-02-24 14:54:14 +0200

    Revert "Created api in task 3"
    This reverts commit 87c4a002c281c5020e3c9e88928486f004560e80.

--- api/v1/views/index.py
+++
@@ -1,8 +0,0 @@
-from api.v1.views import app_views
-from flask import jsonify, make_response
-
-
-@app_views.route('/status')
-def status():
-    """Gives status 200 message"""
-    return make_response(jsonify({"status": "OK"}), 200)
