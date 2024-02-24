9374f1898b0e0f57752c999fa9c49776122bcb79
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-24 10:11:25 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Flask Application """
+""" This is the Flask Application module """
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -31,8 +31,6 @@ Swagger(app)
 
 if __name__ == "__main__":
     """ Main Function """
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', 5000))
+    host = environ.get('HBNB_API_HOST') or '0.0.0.0'
+    port = int(environ.get('HBNB_API_PORT') or 5000)
     app.run(host=host, port=port, threaded=True)
-
-
