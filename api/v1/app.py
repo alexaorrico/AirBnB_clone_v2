0710fbf9f4c737cc5b2b67156410e797f211c701
theChosenDevop <tobbey.adesanya@gmail.com>
2024-02-23 19:31:11 -0800

    Task4 routes to stats It routes to /api/v1/stats Need to populate the data to show count for

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
@@ -15,6 +15,12 @@ cors = CORS(app)
 def teardown(exception):
     """ Teardown  function """
     storage.close()
+
+@app.errorhandler(404)
+def not_found_error(error):
+    """ returns a JSON-formatted 404 status code response """
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = "0.0.0.0"
