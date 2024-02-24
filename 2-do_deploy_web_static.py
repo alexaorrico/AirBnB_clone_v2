c79b9658838a5edcd6a853755510e4f91cdc3b53
David <obododavidsop@gmail.com>
2024-02-24 01:41:12 +0100

    Updated author

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.236.47.245', '100.26.156.253']
 
 
 def do_deploy(archive_path):
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['54.236.47.245', '100.26.156.253']
 
 
 def do_pack():
