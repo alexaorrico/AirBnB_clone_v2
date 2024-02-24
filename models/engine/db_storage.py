bf6e83e12e3a907b7fec2839810861f1ff427e73
sundaymba <sundinoh@gmail.com>
2024-02-24 16:14:32 +0000

    removed print test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -46,7 +46,6 @@ class DBStorage:
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
                 objs = self.__session.query(classes[clss]).all()
-                print(objs)
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
