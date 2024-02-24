a487a2d607d75471235eceac4d9f0eeb655758da
DouibaOmar <douibaomar40@gmail.com>
2024-02-24 15:30:44 +0100

    Done

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -14,7 +14,7 @@ from models.amenity import Amenity
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
-def get_city_places(city_id):
+def get_city_places(city_id=None):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
     if city is None:
@@ -25,7 +25,7 @@ def get_city_places(city_id):
 
 @app_views.route('/places/<place_id>', methods=['GET'],
                  strict_slashes=False)
-def get_place(place_id):
+def get_place(place_id=None):
     """Retrieves a Place object"""
     place = storage.get(Place, place_id)
     if place is None:
@@ -35,7 +35,7 @@ def get_place(place_id):
 
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
-def delete_place(place_id):
+def delete_place(place_id=None):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
     if place is None:
