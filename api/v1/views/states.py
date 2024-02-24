17ebb9e5fb0ecbbbf4db47a1c4a6dec1a2e7fdfd
DouibaOmar <douibaomar40@gmail.com>
2024-02-24 15:27:28 +0100

    Done

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -19,7 +19,7 @@ def get_states():
 @app_views.route('/states/<state_id>',
                  methods=['GET'],
                  strict_slashes=False)
-def get_state(state_id):
+def get_state(state_id=None):
     """Retrieves a State object"""
     state = storage.get(State, state_id)
     if state is None:
