e6485536f89b670ca75b240aff269fd86fd7b7d2
Georgen21 <chukwudinwabia42@gmail.com>
2024-02-24 15:20:53 -0800

    State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""
-route for handling State objects and operations
-"""
+""" route for handling State objects and operations """
 from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
 from models.state import State
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
-def state_get_all():
-    """
-    retrieves all State objects
-    :return: json of all states
-    """
+def get_state():
+    """ retrieves all State objects
+    :return: json of all states """
     state_list = []
     state_obj = storage.all("State")
     for obj in state_obj.values():
@@ -22,11 +18,9 @@ def state_get_all():
 
 
 @app_views.route("/states", methods=["POST"], strict_slashes=False)
-def state_create():
-    """
-    create state route
-    :return: newly created state obj
-    """
+def create_state():
+    """ create state route
+    :return: newly created state obj """
     state_json = request.get_json(silent=True)
     if state_json is None:
         abort(400, 'Not a JSON')
@@ -35,19 +29,17 @@ def state_create():
 
     new_state = State(**state_json)
     new_state.save()
-    resp = jsonify(new_state.to_json())
-    resp.status_code = 201
+    response = jsonify(new_state.to_json())
+    response.status_code = 201
 
-    return resp
+    return response
 
 
 @app_views.route("/states/<state_id>",  methods=["GET"], strict_slashes=False)
 def state_by_id(state_id):
-    """
-    gets a specific State object by ID
+    """ gets a specific State object by ID
     :param state_id: state object id
-    :return: state obj with the specified id or error
-    """
+    :return: state obj with the specified id or error """
 
     fetched_obj = storage.get("State", str(state_id))
 
@@ -59,11 +51,9 @@ def state_by_id(state_id):
 
 @app_views.route("/states/<state_id>",  methods=["PUT"], strict_slashes=False)
 def state_put(state_id):
-    """
-    updates specific State object by ID
+    """ updates specific State object by ID
     :param state_id: state object ID
-    :return: state object and 200 on success, or 400 or 404 on failure
-    """
+    :return: state object and 200 on success, or 400 or 404 on failure """
     state_json = request.get_json(silent=True)
     if state_json is None:
         abort(400, 'Not a JSON')
@@ -80,11 +70,9 @@ def state_put(state_id):
 @app_views.route("/states/<state_id>", methods=["DELETE"],
                  strict_slashes=False)
 def state_delete_by_id(state_id):
-    """
-    deletes State by id
+    """ deletes State by id
     :param state_id: state object id
-    :return: empty dict with 200 or 404 if not found
-    """
+    :return: empty dict with 200 or 404 if not found """
 
     fetched_obj = storage.get("State", str(state_id))
 
