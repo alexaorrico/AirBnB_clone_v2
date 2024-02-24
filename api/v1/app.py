6fa98e3448d3adea625070e9333ca1b370ad6aae
dylanmlilo <dylanmlilo12@gmail.com>
2024-02-24 06:37:04 -0800

    updated app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,20 +7,24 @@ from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from flasgger import Swagger
 
+
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """ 404 Error """
     return make_response(jsonify({'error': "Not found"}), 404)
+
 
 # Configure Swagger
 app.config['SWAGGER'] = {
