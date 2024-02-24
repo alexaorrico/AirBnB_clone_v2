4718a3621e3d43d191e40d43f674ac777b48927a
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-24 17:53:28 +0200

    trying another json method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,10 @@
 """ app_view Blueprint """
 
 from api.v1.views import app_views
-from flask import jsonify
+import json
 
 
 @app_views.route('/status', strict_slashes=False)
 def show_status():
     """ Shows the api response status """
-    return jsonify(status="OK")
+    return json.dumps({"status": 'OK'})
