2b64f702e55fc3d049d13565bbae7c26919f8468
mohanned-sameh <mohanned2678@gmail.com>
2024-02-24 14:14:54 +0200

    Add jsonify import and update error handler in app.py, jsonify response in index.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Module for the API """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -8,26 +8,21 @@ from os import getenv
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(self):
+def close(self):
     """Method to close the session"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
+def page_not_found(e):
     """Method to handle 404 errors"""
-    return {"error": "Not found"}, 404
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST")
-    port = getenv("HBNB_API_PORT")
-    if not host:
-        host = "0.0.0.0"
-    if not port:
-        port = 5000
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,8 @@
 from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
     """Returns a JSON"""
-    return {"status": "OK"}
+    return jsonify(status="OK")
