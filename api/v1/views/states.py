9f14e5ffc124a7a61706e03e44cb6fc93cb19fcc
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-24 10:47:21 +0200

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,12 +7,14 @@ from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_state.yml', methods=['GET'])
 def get_states():
     """Retrieve the list of all State objects"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_id_state.yml', methods=['GET'])
 def get_state(state_id):
     """Retrieve a State object by ID"""
     state = storage.get(State, state_id)
@@ -21,6 +23,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
 def delete_state(state_id):
     """Delete a State object by ID"""
     state = storage.get(State, state_id)
@@ -31,6 +34,7 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/state/post_state.yml', methods=['POST'])
 def create_state():
     """Create a new State"""
     data = request.get_json()
@@ -44,6 +48,7 @@ def create_state():
     return jsonify(new_state.to_dict()), 201
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/state/put_state.yml', methods=['PUT'])
 def update_state(state_id):
     """Update a State object by ID"""
     state = storage.get(State, state_id)
