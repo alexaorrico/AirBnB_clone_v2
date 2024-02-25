a3d60d5594632151b7b5ed21f96c04d46d0fa167
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-26 00:40:53 +0100

    api status

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exc):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
-
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
