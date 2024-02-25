3ad1af5b1d8d60b8a3ca6935bfae8fa62b36f41b
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-25 23:26:53 +0200

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,13 @@
 """index"""
 
 from flask import jsonify
-
 from api.v1.views import app_views
+from models import storage
+
+
+classes = {"users": "User", "places": "Place", "states": "State",
+               "cities": "City", "amenities": "Amenity",
+               "reviews": "Review"}
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -14,3 +19,12 @@ def status():
     }
 
     return jsonify(response)
+
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def stats():
+    """stats route"""
+    counts = {}
+    for key, value in classes.items():
+        counts[key] = storage.count(value)
+    return jsonify(counts)
