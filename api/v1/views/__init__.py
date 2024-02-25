23fc7c2fd3f749c8a01b325014eea15cfcca5e8c
kavetech2023 <kavetech@gmail.com>
2024-02-25 11:33:13 -0800

    update init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,23 +1,9 @@
-"""Initializes main blueprint for flask application"""
+#!/usr/bin/python3
+"""makes views folder a module"""
 from flask import Blueprint
-
-app_views = Blueprint("site", __name__)
-<<<<<<< HEAD
-from api.v1.views.index import *  # noqa
-from api.v1.views.states import *  # noqa
-from api.v1.views.cities import *  # noqa
-from api.v1.views.amenities import *  # noqa
-from api.v1.views.users import *  # noqa
-from api.v1.views.places import *  # noqa
-from api.v1.views.places_reviews import *  # noqa
-from api.v1.views.places_amenities import *  # noqa
-=======
-from api.v1.views.index import *  # 
-from api.v1.views.states import *  # n
-from api.v1.views.cities import *  # 
-from api.v1.views.amenities import *  n
-from api.v1.views.users import *  
-from api.v1.views.places import *  # 
-from api.v1.views.places_reviews import *  # 
-from api.v1.views.places_amenities import *  #
->>>>>>> master
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
