6463fe2ea2e6019b4fb1891f3ecec13c281e2a8f
Andrew <andrewsolitei@gmail.com>
2024-02-25 20:22:33 +0300

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,17 +11,3 @@ from models import storage
 def api_status():
     """ returns json string when request api status"""
     return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', methods=['GET'])
-def returns_no():
-    """Retrieves the number of each object type"""
-    stats_dict = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
-    }
-    return jsonify(stats_dict)
