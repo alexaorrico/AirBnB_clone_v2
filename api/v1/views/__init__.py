84e5da3da64eb6df90783565c99f31d5a28f2889
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-02-25 17:25:59 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 """Initializes main blueprint for flask application"""
 from flask import Blueprint
 
-app_views = Blueprint("site", __name__)
+
 from api.v1.views.index import *  # noqa
 from api.v1.views.states import *  # noqa
 from api.v1.views.cities import *  # noqa
@@ -10,3 +10,4 @@ from api.v1.views.users import *  # noqa
 from api.v1.views.places import *  # noqa
 from api.v1.views.places_reviews import *  # noqa
 from api.v1.views.places_amenities import *  # noqa
+app_views = Blueprint("site", __name__)
