248ca5a2f5524f463baae68aeb84d07b49e8e05e
aminoxlhd <aminoxlhd@gmail.com>
2024-02-26 00:42:20 +0100

    edit app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ app.py file"""
-from flask import Flask, Blueprint
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views())
@@ -14,5 +15,6 @@ def teardown_appcontext():
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')))
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, threaded=True)
