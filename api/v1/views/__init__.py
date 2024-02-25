5c8d84b1da4c2be3fc0d942493ebb55239a59c42
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-25 11:31:57 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Init file
+Views
 """
 from flask import Blueprint
 
-app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
 
-"""wildcard import from index.py"""
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
