d0d9153124dfa693d6eab272fb37edbc99b88d04
Just-andreew <andrewsolitei@gmail.com>
2024-02-25 20:05:24 +0300

    api

---
+++ api/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module for api configuration
+"""
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ tear doen appcontext """
+    storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    """handling error when non-existing url is querired"""
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host, port, threaded=True)
