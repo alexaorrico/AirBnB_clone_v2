f976223035da78c5ac0e9e54ed537021326cf69d
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-02-25 14:17:50 +0300

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,20 +3,39 @@
 A variable app
 """
 
-from flask import Flask
-from models import storage
+from flask import Flask, jsoify
+from flask_cors import CORS
+from os import getenv
+
 from api.v1.views import app_views
+from models import storage
+
 
 app = Flask(__name__)
 
-app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown(exception):
+    """
+    Teardown functions
+    """
     storage.close()
 
-if __name__ == "__main__":
-    import os
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    Handles 404 errors
+    :return: Returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
+
+    if __name__ == "__main__":
+        app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
