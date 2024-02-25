fc2e5d0d86ec63480f786cc6fc11c6c3b0a5d6d9
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-02-25 20:59:46 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,12 @@
-"""Initializes main blueprint for flask application"""
+#!/usr/bin/python3
+"""makes views folder a module"""
 from flask import Blueprint
-
-app_views = Blueprint("site", __name__)
-from api.v1.views.index import *  # 
-from api.v1.views.states import *  # n
-from api.v1.views.cities import *  # 
-from api.v1.views.amenities import *  n
-from api.v1.views.users import *  
-from api.v1.views.places import *  # 
-from api.v1.views.places_reviews import *  # 
-from api.v1.views.places_amenities import *  #
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
