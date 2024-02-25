62c0c9c7eeb05a3d85742bd08b1a86728e957c3d
wallace <exnesswallace@gmail.com>
2024-02-25 22:35:36 +0200

    A method to retrieve one object, A method to count the number of objects in storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,13 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """A method to retrieve one object:"""
+        for obj in self.__objects:
+            if isinstance(obj, cls) and obj.id == id:
+                return (obj)
+    
+    def count(self, cls=None):
+        """returns the number of objects in storage matching the given class(optional)"""
+        return (len(self.all(cls)))
