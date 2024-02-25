e5e0466bc1aeeb90c6587336a5184c84692cef69
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-25 22:34:37 +0200

    API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,7 +8,7 @@ from api.v1.views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    "status route"
+    """status route"""
     response = {
         "status": "OK"
     }
