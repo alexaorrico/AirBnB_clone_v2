ba026190bd9726a56790069abdf3275a89756cad
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-25 12:24:25 +0200

    Adding Task 4 endpoint to retrieve objects count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,22 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def show_status():
     """ Shows the api response status """
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def show_stats():
+    """ Shows the number of each object """
+    classes = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+    class_count = {}
+    for clas in classes:
+        count = storage.count(clas)
+        if count > 0:
+            class_count[clas] = count
+    return jsonify(class_count)
