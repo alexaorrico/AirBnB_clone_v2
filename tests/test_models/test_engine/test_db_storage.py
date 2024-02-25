c9ede40fe81c15e2676cb0dce916f33083988d6e
mohanned-sameh <mohanned2678@gmail.com>
2024-02-25 18:51:23 +0200

    Refactor test cases in test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,51 +87,46 @@ class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
 
     @unittest.skipIf(models.storage_t != "db", "not testing db storage")
-    def test_all_returns_dict(self):
+    def allReturnDict(self):
         """Test that all returns a dictionaty"""
         self.assertIs(type(models.storage.all()), dict)
 
     @unittest.skipIf(models.storage_t != "db", "not testing db storage")
-    def test_all_no_class(self):
+    def allNoClass(self):
         """Test that all returns all rows when no class is passed"""
 
     @unittest.skipIf(models.storage_t != "db", "not testing db storage")
-    def test_new(self):
+    def new(self):
         """test that new adds an object to the database"""
 
     @unittest.skipIf(models.storage_t != "db", "not testing db storage")
-    def test_save(self):
+    def save(self):
         """Test that save properly saves objects to file.json"""
 
-    @unittest.skipIf(models.storage_t != "db", "not testing db storage")
+
+class TestDBStorage(unittest.TestCase):
+    """Test the DBStorage class"""
+
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != "db", "not testing db storage")
     def test_get(self):
-        """test that get returns an object based on class and id"""
+        """Test that get returns specific object, or none"""
+        newState = State(name="New York")
+        newState.save()
+        newUser = User(email="bob@foobar.com", password="password")
+        newUser.save()
+        self.assertIs(newState, models.storage.get("State", newState.id))
+        self.assertIs(None, models.storage.get("State", "blah"))
+        self.assertIs(None, models.storage.get("blah", "blah"))
+        self.assertIs(newUser, models.storage.get("User", newUser.id))
 
-    @unittest.skipIf(models.storage_t != "db", "not testing db storage")
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") != "db", "not testing db storage")
     def test_count(self):
-        """test that count returns the number of objects in storage"""
-        new_state = State(name="California")
-        models.storage.new(new_state)
-        models.storage.save()
-        self.assertEqual(models.storage.count(), 1)
-
-    @unittest.skipIf(models.storage_t != "db", "not testing db storage")
-    def test_get_none(self):
-        """test that get returns None when no class or id matches"""
-
-    @unittest.skipIf(models.storage_t != "db", "not testing db storage")
-    def test_count_none(self):
-        """test that count returns 0 when no class is passed"""
-
-    @unittest.skipIf(models.storage_t != "db", "not testing db storage")
-    def test_count_class(self):
-        """test that count returns the number of objects in storage matching the class"""
-        new_state = State(name="California")
-        models.storage.new(new_state)
-        models.storage.save()
-        self.assertEqual(models.storage.count("State"), 1)
-
-    @unittest.skipIf(models.storage_t != "db", "not testing db storage")
-    def test_count_class_none(self):
-        """test that count returns 0 when no objects match the class"""
-        self.assertEqual(models.storage.count("State"), 0)
+        """add new object to db"""
+        startCount = models.storage.count()
+        self.assertEqual(models.storage.count("Blah"), 0)
+        newState = State(name="Montevideo")
+        newState.save()
+        newUser = User(email="ralexrivero@gmail.com.com", password="dummypass")
+        newUser.save()
+        self.assertEqual(models.storage.count("State"), startCount + 1)
+        self.assertEqual(models.storage.count(), startCount + 2)
