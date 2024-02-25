b32c6fa99197c910dbf72f2deeadbe7e52715226
mohanned-sameh <mohanned2678@gmail.com>
2024-02-25 17:49:41 +0200

    Refactor delete_state route in states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -40,7 +40,11 @@ def get_state(state_id):
 
 
 # Route for deleting a specific State object by ID
-@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
+@app_views.route(
+    "/states/<state_id>",
+    methods=["DELETE"],
+    strict_slashes=False,
+)
 def delete_state(state_id):
     """
     Deletes a State object.
