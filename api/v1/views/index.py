a4295d4fd08d64144a4e48d21870d7e401b3c768
ben <benzoic09@gmail.com>
2024-02-25 06:16:03 -0800

    update task 2

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """ index"""
 
-from flask import Flask
+from flask import Flask, jsonify, Blueprint
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+status_bp = Blueprint('status', __name__)
 
-@app_views.route('/status', methods=['GET'])
+@status_bp.route('/status', methods=['GET'])
 def status():
-    return jsonify ({'ststus: OK'})
+    return jsonify ({'status': 'OK'})
+
+app.register_blueprint(status_bp)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
