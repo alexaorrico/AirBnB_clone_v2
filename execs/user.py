ab64574768663fa40fe367f7c5454dbe4be6fb90
BnKeTia <bernardnketia51@gmail.com>
2024-02-25 15:26:56 +0000

    running all executables in the root of the repo

---
+++ execs/user.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import hashlib
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, key, value):
+        """Method that encodes user password"""
+        if key == "password":
+            value = hashlib.md5(value.encode()).hexdigest()
+        super().__setattr__(key, value)
