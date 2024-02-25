960200bfa2adf0da437e6d7e9ee36e86d7fb20e6
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-02-25 21:50:52 +0000

    feat(index.py): add function to retrieve object count
    Can't be checked yet because the function it depends on hasnt been merged yet

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,22 @@
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 @app_views.route('/status')
 def status():
     """ returns a JSON message"""
     return jsonify(status="OK")
+
+
+@app_views.route('/api/v1/stats')
+def get_count():
+    """Retrieves the number of each object by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
