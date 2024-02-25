95e13738b10fb966376b274c1a8393c22f9e5b26
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-25 23:16:39 +0100

    some changes

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+
+
+app = Flask(__name__)
+
+host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+port = os.getenv('HBNB_API_PORT', 5000)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """close db session after each request"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=host, port=port)
---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from api.v1.views import api_views
+from flask import jsonify, request
+
+
+@app_views.route('/status', METHOD=['GET'])
+def status():
+    """returns the status"""
+    if request.method == 'GET':
+        response = {"status": "OK"}
+        return jsonify(response)
