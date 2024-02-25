1822b19cc3c694626033d72c2e1ff4d2ae7ead93
mohanned-sameh <mohanned2678@gmail.com>
2024-02-26 00:34:37 +0200

    edited amenities and index

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,118 +1,65 @@
 #!/usr/bin/python3
-"""
-Creates a view for Amenity objects - handles all default RESTful API actions.
-"""
+"""State objects that handles all default RESTFul API actions"""
 
-# Import necessary modules
-from flask import abort, jsonify, request
-from models.amenity import Amenity
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from flask import abort, request, jsonify
 
 
-# Route for retrieving all Amenity objects
-@app_views.route("/amenities", methods=["GET"], strict_slashes=False)
-def get_all_amenities():
-    """Retrieves the list of all Amenity objects"""
-    # Get all Amenity objects from the storage
-    amenities = storage.all(Amenity).values()
-    # Convert objects to dictionaries and jsonify the list
-    return jsonify([amenity.to_dict() for amenity in amenities])
-
-
-# Route for retrieving a specific Amenity object by ID
-@app_views.route(
-    "/amenities/<amenity_id>",
-    methods=["GET"],
-    strict_slashes=False,
-)
-def get_amenity(amenity_id):
-    """Retrieves an Amenity object"""
-    # Get the Amenity object with the given ID from the storage
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity:
-        # Return the Amenity object in JSON format
-        return jsonify(amenity.to_dict())
+@app_views.route("/amenities", strict_slashes=False, methods=["GET"])
+@app_views.route("/amenities/<amenity_id>", strict_slashes=False,
+                 methods=["GET"])
+def amenity(amenity_id=None):
+    """show amenity and amenity with id"""
+    amenity_list = []
+    if amenity_id is None:
+        all_objs = storage.all(Amenity).values()
+        for v in all_objs:
+            amenity_list.append(v.to_dict())
+        return jsonify(amenity_list)
     else:
-        # Return 404 error if the Amenity object is not found
+        result = storage.get(Amenity, amenity_id)
+        if result is None:
+            abort(404)
+        return jsonify(result.to_dict())
+
+
+@app_views.route("/amenities/<amenity_id>", strict_slashes=False,
+                 methods=["DELETE"])
+def amenity_delete(amenity_id):
+    """delete method"""
+    obj = storage.get(Amenity, amenity_id)
+    if obj is None:
         abort(404)
+    storage.delete(obj)
+    storage.save()
+    return jsonify({}), 200
 
 
-# Route for deleting a specific Amenity object by ID
-@app_views.route(
-    "/amenities/<amenity_id>",
-    methods=["DELETE"],
-    strict_slashes=False,
-)
-def delete_amenity(amenity_id):
-    """Deletes an Amenity object"""
-    # Get the Amenity object with the given ID from the storage
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity:
-        # Delete the Amenity object from the storage and save changes
-        storage.delete(amenity)
-        storage.save()
-        # Return an empty JSON with 200 status code
-        return jsonify({}), 200
-    else:
-        # Return 404 error if the Amenity object is not found
-        abort(404)
-
-
-# Route for creating a new Amenity object
-@app_views.route(
-    "/amenities",
-    methods=["POST"],
-    strict_slashes=False,
-)
+@app_views.route("/amenities", strict_slashes=False, methods=["POST"])
 def create_amenity():
-    """Creates an Amenity object"""
-    if not request.get_json():
-        # Return 400 error if the request data is not in JSON format
+    """create a new post req"""
+    data = request.get_json(force=True, silent=True)
+    if not data:
         abort(400, "Not a JSON")
-
-    # Get the JSON data from the request
-    data = request.get_json()
     if "name" not in data:
-        # Return 400 error if 'name' key is missing in the JSON data
         abort(400, "Missing name")
-
-    # Create a new Amenity object with the JSON data
-    amenity = Amenity(**data)
-    # Save the Amenity object to the storage
-    amenity.save()
-    # Return the newly created Amenity
-    #   object in JSON format with 201 status code
-    return jsonify(amenity.to_dict()), 201
+    new_amenity = Amenity(**data)
+    new_amenity.save()
+    return jsonify(new_amenity.to_dict()), 201
 
 
-# Route for updating an existing Amenity object by ID
-@app_views.route(
-    "/amenities/<amenity_id>",
-    methods=["PUT"],
-    strict_slashes=False,
-)
+@app_views.route("/amenities/<amenity_id>", strict_slashes=False,
+                 methods=["PUT"])
 def update_amenity(amenity_id):
-    """Updates an Amenity object"""
-    # Get the Amenity object with the given ID from the storage
-    amenity = storage.get(Amenity, amenity_id)
-    if amenity:
-        # Return 400 error if the request data is not in JSON format
-        if not request.get_json():
-            abort(400, "Not a JSON")
-
-        # Get the JSON data from the request
-        data = request.get_json()
-        ignore_keys = ["id", "created_at", "updated_at"]
-        # Update the attributes of the Amenity object with the JSON data
-        for key, value in data.items():
-            if key not in ignore_keys:
-                setattr(amenity, key, value)
-
-        # Save the updated Amenity object to the storage
-        amenity.save()
-        # Return the updated Amenity object in JSON format with 200 status code
-        return jsonify(amenity.to_dict()), 200
-    else:
-        # Return 404 error if the Amenity object is not found
+    """update amenity"""
+    obj = storage.get(Amenity, amenity_id)
+    if obj is None:
         abort(404)
+    data = request.get_json(force=True, silent=True)
+    if not data:
+        abort(400, "Not a JSON")
+    obj.name = data.get("name", obj.name)
+    obj.save()
+    return jsonify(obj.to_dict()), 200
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
-"""
-Create a route `/status` on the object app_views.
+"""define routes of blueprint
 """
 
-from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
-@app_views.route("/status", methods=["GET"])
-def api_status():
-    """
-    Returns a JSON response for RESTful API health.
-    """
-    return jsonify({"status": "OK"})
-
-
-@app_views.route("/stats", methods=["GET"])
-def get_stats():
-    """
-    Retrieves the number of each objects by type.
-    """
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
+@app_views.route("/status", strict_slashes=False, methods=["GET"])
+def status():
+    return {
+        "status": "OK",
     }
-    return jsonify(stats)
+
+
+@app_views.route("/stats", strict_slashes=False, methods=["GET"])
+def stats():
+    amenities = storage.count(Amenity)
+    cities = storage.count(City)
+    places = storage.count(Place)
+    reviews = storage.count(Review)
+    states = storage.count(State)
+    users = storage.count(User)
+    return {
+        "amenities": amenities,
+        "cities": cities,
+        "places": places,
+        "reviews": reviews,
+        "states": states,
+        "users": users,
+    }
