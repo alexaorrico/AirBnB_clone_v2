a0d30dbf63c363dc5ed2ee398b42f99a9780daee
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-25 05:49:05 -0800

    task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_engine(exception):
-    '''Removes the current SQLAlchemy Session object after each 
+    '''Removes the current SQLAlchemy Session object after each
         request.
     '''
     storage.close()
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,26 @@
 '''index.py file'''
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     '''returns a JSON: "status": "OK"'''
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def states():
+    '''Create an endpoint that retrieves the number of each 
+        objects by type
+    '''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
