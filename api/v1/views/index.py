d42259efc4ab0c29a9030c9fc2d27ed9c4a6f4a8
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-25 12:54:18 +0200

    Adding Task 4 endpoint to retrieve objects count with dict

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,8 +15,9 @@ def show_status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def show_stats():
     """ Shows the number of each class objects """
-    classes = ['Amenity', 'City', 'Place', 'Review', 'State', 'User']
+    classes = {'amenities': 'Amenity', 'cities': 'City', 'places': 'Place',
+               'reviews': 'Review', 'states': 'State', 'users': 'User'}
     class_count = {}
-    for clas in classes:
-        class_count[clas] = storage.count(clas)
+    for k, v in classes:
+        class_count[k] = storage.count(v)
     return jsonify(class_count)
