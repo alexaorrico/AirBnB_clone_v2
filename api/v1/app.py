09817610e69ae308b5664bfa66282969d7215b8d
MDIMACat <romanmdima@gmail.com>
2024-02-25 22:51:51 +0200

    Fixed pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*" :{"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
