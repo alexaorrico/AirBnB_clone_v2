89d59f18dd71fa0e5630b80bc6751afc9c44051e
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-25 12:05:14 +0200

    Adding jsonify method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,12 +2,10 @@
 """ app_view Blueprint """
 
 from api.v1.views import app_views
-import json
+from flask import jsonify
 
 
 @app_views.route('/status', strict_slashes=False)
 def show_status():
     """ Shows the api response status """
-    return json.dumps({
-        "status": "OK"
-        })
+    return jsonify(status="OK")
