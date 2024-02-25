a09a00cb21b34be6089009c00ee0dcaf51075470
mohanned-sameh <mohanned2678@gmail.com>
2024-02-25 17:52:08 +0200

    Add strict_slashes=False to state routes

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,7 +11,7 @@ from models import storage
 
 
 # Route for retrieving all State objects
-@app_views.route("/states", methods=["GET"])
+@app_views.route("/states", methods=["GET"], strict_slashes=False)
 def get_all_states():
     """
     Retrieves the list of all State objects.
@@ -24,7 +24,7 @@ def get_all_states():
 
 
 # Route for retrieving a specific State object by ID
-@app_views.route("/states/<state_id>", methods=["GET"])
+@app_views.route("/states/<state_id>", methods=["GET"], strict_slashes=False)
 def get_state(state_id):
     """
     Retrieves a State object.
@@ -43,6 +43,7 @@ def get_state(state_id):
 @app_views.route(
     "/states/<state_id>",
     methods=["DELETE"],
+    strict_slashes=False,
 )
 def delete_state(state_id):
     """
@@ -62,7 +63,7 @@ def delete_state(state_id):
 
 
 # Route for creating a new State object
-@app_views.route("/states", methods=["POST"])
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
 def create_state():
     """
     Creates a State object.
@@ -86,7 +87,7 @@ def create_state():
 
 
 # Route for updating an existing State object by ID
-@app_views.route("/states/<state_id>", methods=["PUT"])
+@app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
 def update_state(state_id):
     """
     Updates a State object.
