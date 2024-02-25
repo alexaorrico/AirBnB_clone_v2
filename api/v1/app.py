433dbfbd9b551ac703081fc284c4d1683db98442
Mussa Milanzi <viewholder74@gmail.com>
2024-02-25 08:37:07 -0800

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,9 +19,7 @@ def tear_down(self):
     storage.close()
 
 if __name__ == "__main__":
-
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
-
-    app.run(host, port, threaded=True, debug=True)
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+    app.run(host, int(port), threaded=True)
 
