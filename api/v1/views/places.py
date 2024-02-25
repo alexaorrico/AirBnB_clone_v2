2ed388af09d127bf68cdeceed25f2e25946561b1
Cha1mae <chaimastaifa@gmail.com>
2024-02-25 07:48:47 -0800

    places pycodestyle

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,7 +4,6 @@ Place model
 """
 
 from flask import jsonify, abort, request
-
 from api.v1.views import app_views
 from models import storage
 from models.city import City
@@ -13,13 +12,13 @@ from models.user import User
 from models.state import State
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
+                 strict_slashes=False)
 def get_places(city_id):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-
     places = [place.to_dict() for place in city.places]
     return jsonify(places)
 
@@ -33,7 +32,8 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_place(place_id):
     """Deletes Place"""
     place = storage.get(Place, place_id)
@@ -44,7 +44,8 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
 def create_place(city_id):
     """Creates Place"""
     city = storage.get(City, city_id)
