b1176c2e26ed0b430353144fb18a07d03961a414
CletsyMedia <Cletussam12@yahoo.com>
2024-02-25 21:17:38 +0100

    Updating the storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,12 +4,12 @@ Contains the class DBStorage
 """
 
 import models
+from models.review import Review
+from models.state import State
 from models.amenity import Amenity
 from models.base_model import BaseModel, Base
 from models.city import City
 from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
 from os import getenv
 import sqlalchemy
@@ -75,15 +75,6 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-    def count(self, cls=None):
-        '''count:
-        count the number of objects in storage matching the given class.
-        '''
-        data = self.all(cls)
-        if cls in classes.values():
-            data = self.all(cls)
-        return len(data)
-
     def get(self, cls, id):
         '''get:
         retrieve an object from the file storage by class and id.
@@ -94,3 +85,12 @@ class DBStorage:
                 if key.split(".")[1] == id:
                     return value
         return None
+
+    def count(self, cls=None):
+        '''count:
+        this count the number of objects in storage matching the given class.
+        '''
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
