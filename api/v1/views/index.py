dad2032a2f97564ddba43a8dddc0004faec68013
Kevin Kavete <137928651+kavetech2023@users.noreply.github.com>
2024-02-25 20:55:25 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,9 @@ from api.v1.views import app_views
 
 @app_views.route("/status")
 def status():
+    response = jsonify(data)
+    response.status_code = 200
+    return response
     """Defines a status route returning a json object of the api status.
     """
     return jsonify({"status": "OK"})
