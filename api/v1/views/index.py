7b996976d12c83e6bde22775aea07d9faa6834de
mouhdjouab <mouh.bw@gmail.com>
2024-02-25 02:33:40 +0100

    d

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,38 @@ contains status and  endpoints
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
 
 
 @app_views.route("/status")
 def status_json():
     ''' route returning status page '''
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats_obj():
+    '''retrieves the number of each objects by type'''
+    classes = {
+    "amenities": Amenity,
+    "cities": City,
+    "places": Place,
+    "reviews": Review,
+    "states": State,
+    "users": User,
+    }
+    stats={}
+    for key, value in classes.items():
+        stats[key] = storage.count(value)
+
+    return jsonify(stats)
+
 
 
