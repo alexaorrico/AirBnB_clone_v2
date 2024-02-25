02fbcaeb23b2cfa0c5dfe3de71bbde4434432b53
MDIMACat <romanmdima@gmail.com>
2024-02-25 15:21:05 +0200

    Second apit amenity try

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,82 +1,79 @@
 #!/usr/bin/python3
 """
-Amenity view
+Amenities view
 """
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
-from flask import jsonify, abort, request
+
+
+@app_views.route("/amenities", methods=["GET"],
+                 strict_slashes=False)
+def get_amenities():
+    """Retrieves the list of all Amenity objects"""
+    amenity_list = [amenity.to_dict() for amenity in storage.all("Amenity").values()]
+    return jsonify(amenity_list)
 
 
 @app_views.route("/amenities/<amenity_id>", methods=["GET"],
                  strict_slashes=False)
-def get_amenity_id(amenity_id):
-    """Retrieves all amenity objects from a specific id"""
-    amenity_obj = storage.get("Amenity", str(amenity_id))
-
+def get_amenity(amenity_id):
+    """Retrieves a specific Amenity object by ID"""
+    amenity_obj = storage.get("Amenity", amenity_id)
     if amenity_obj is None:
         abort(404)
-
-    return jsonify(amenity_obj.to_json())
-
-
-@app_views.route("/amenities", methods=["POST"], strict_slashes=False)
-def city_create():
-    """Creates a Amenity """
-    amenity_json = request.get_json(silent=True)
-    if amenity_json is None:
-        abort(400, 'Not a JSON')
-
-    if "name" not in amenity_json:
-        abort(400, 'Missing name')
-
-    new_amenity = Amenity(**amenity_json)
-    new_amenity.save()
-    resp = jsonify(new_amenity.to_json())
-    resp.status_code = 201
-
-    return resp
-
-
-@app_views.route("/amenities", methods=["GET"], strict_slashes=False)
-def get_amentiy():
-    """Gets all amenity objects"""
-    amenity_list = []
-    fetched_obj = storage.all("Amenity")
-
-    for obj in fetched_obj.values():
-        amenity_list.append(obj.to_json())
-
-    return jsonify(amenity_list)
-
-
-@app_views.route("/amenities/<amenity_id>", methods=["PUT"],
-                 strict_slashes=False)
-def city_put(amenity_id):
-    """Updates a specific amenity object by ID"""
-    amenity_json = request.get_json(silent=True)
-    if amenity_json is None:
-        abort(400, 'Not a JSON')
-    fetched_obj = storage.get("Amenity", str(amenity_id))
-    if fetched_obj is None:
-        abort(404)
-    for key, val in amenity_json.items():
-        if key not in ["id", "created_at", "updated_at"]:
-            setattr(fetched_obj, key, val)
-    fetched_obj.save()
-    return jsonify(fetched_obj.to_json())
+    return jsonify(amenity_obj.to_dict())
 
 
 @app_views.route("/amenities/<amenity_id>", methods=["DELETE"],
                  strict_slashes=False)
-def city_delete_by_id(amenity_id):
-    """Deletes Amenity by ID"""
-    fetched_obj = storage.get("Amenity", str(amenity_id))
+def delete_amenity(amenity_id):
+    """Deletes a specific Amenity object by ID"""
+    amenity_obj = storage.get("Amenity", amenity_id)
+    if amenity_obj is None:
+        abort(404)
+    storage.delete(amenity_obj)
+    storage.save()
+    return jsonify({})
 
-    if fetched_obj is None:
+
+@app_views.route("/amenities", methods=["POST"], strict_slashes=False)
+def create_amenity():
+    """Creates a new Amenity object"""
+    amenity_data = request.get_json()
+    if amenity_data is None:
+        abort(400, "Not a JSON")
+
+    if "name" not in amenity_data:
+        abort(400, "Missing name")
+
+    new_amenity = Amenity(**amenity_data)
+    new_amenity.save()
+
+    resp = jsonify(new_amenity.to_dict())
+    resp.status_code = 201
+    return resp
+
+
+@app_views.route("/amenities/<amenity_id>", methods=["PUT"],
+                 strict_slashes=False)
+def update_amenity(amenity_id):
+    """Updates a specific Amenity object by ID"""
+    amenity_data = request.get_json()
+    if amenity_data is None:
+        abort(400, "Not a JSON")
+
+    amenity_obj = storage.get("Amenity", amenity_id)
+    if amenity_obj is None:
         abort(404)
 
-    storage.delete(fetched_obj)
-    storage.save()
+    for key, value in amenity_data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(amenity_obj, key, value)
 
-    return jsonify({})
+    amenity_obj.save()
+
+    resp = jsonify(amenity_obj.to_dict())
+    resp.status_code = 200
+    return resp
