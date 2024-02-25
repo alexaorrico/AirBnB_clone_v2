8314c31d5e7c47dcfd93a93b90b4ceca2582a63c
Joe Egboka <joeegboka@gmail.com>
2024-02-25 23:02:19 +0100

    add '/stats' route in index.py file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
 """index module"""
+from flask import jsonify
+
 from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route("/status")
+@app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status():
     """returns the response status, OK"""
-    return {"status": "OK"}
+    return jsonify({"status": "OK"}), 200
 
 
-@app_views.route("/api/v1/stats")
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
 def stats():
     """endpoint retrieves the number of each objects by type"""
-    num = storage.count()
+    num_objs = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+    return jsonify(num_objs), 200
