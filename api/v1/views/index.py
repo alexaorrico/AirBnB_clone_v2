a48b99022e6be37e8cf8a14be2268fc9c2192470
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-26 17:52:20 +0200

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,6 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def ret_status():
     return jsonify({"status": "OK"})
-
-
-if __name__ == "__main__":
-    app_views.url_map.strict_slashes = False
