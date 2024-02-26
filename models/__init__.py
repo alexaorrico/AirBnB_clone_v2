00eaf6a6e5770d091f32ee2c9a6ac9484799123b
aminoxlhd <aminoxlhd@gmail.com>
2024-02-26 23:08:18 +0100

    edit

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,6 @@ from os import getenv
 
 
 storage_t = getenv("HBNB_TYPE_STORAGE")
-print(storage_t)
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
