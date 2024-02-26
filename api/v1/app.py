dd549271fdf0d5b675f0b2b47cd49a8059afe8cc
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-26 11:50:16 +0100

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
-"""register blueprint"""
-
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response, render_template, url_for
 from models import storage
 import os
+from werkzeug.exceptions import HTTPException
 
-
+# Global Flask Application Variable: app
 app = Flask(__name__)
 
+# global strict slashes
+app.url_map.strict_slashes = False
+
+# flask server environmental setup
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 
+# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
 
+# begin flask page rendering
 @app.teardown_appcontext
 def teardown_db(exception):
-    """close db session after each request"""
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
     storage.close()
 
 
