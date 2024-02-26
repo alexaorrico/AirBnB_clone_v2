2142df8cd482273e84102f9d8dfac14ea71f332b
kipngeno kevin <kipngenokevin254@gmail.com>
2024-02-26 13:15:00 +0000

    pycodestyle checks

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,6 @@
 '''makes directory a package'''
 
 from flask import Blueprint
+from api.v1.views.index import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ for starting api """
 from flask import jsonify
-
 from api.v1.views import app_views
+
 
 @app_views.route("/status", strict_slashes=False)
 def status():
+    '''returns a JSON: "status": "OK"'''
     return jsonify({'status': 'OK'})
