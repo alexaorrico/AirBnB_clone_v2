5b11cbf7f3db0063e57900ba5da3737c3fcd624d
kiplimoboor <kiplimoboor@hotmail.com>
2024-02-26 15:50:58 +0300

    count by class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -27,11 +27,13 @@ def stats():
     """
         Simple stats for the web app
     """
-    return jsonify({
-        'amenities': storage.count(Amenity),
-        'cities': storage.count(City),
-        'places': storage.count(Place),
-        'reviews': storage.count(Review),
-        'states': storage.count(State),
-        'users': storage.count(User),
-        })
+    objects = {'users': User,
+               'cities': City,
+               'places': Place,
+               'reviews': Review,
+               'states': State,
+               'users': User
+               }
+    for obj in objects:
+        objects[obj] = storage.count(objects[obj])
+    return jsonify(objects)
