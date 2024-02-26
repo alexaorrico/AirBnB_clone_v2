a6752171cd082c29a97c67cbd4ecd6284f4e2805
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-26 12:04:57 +0100

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-route returning json response
+flask route return json response
 """
 from api.v1.views import app_views
 from flask import jsonify, request
@@ -10,8 +10,28 @@ from models import storage
 @app_views.route('/status', methods=['GET'])
 def status():
     """
-    returns status
+    returns json status
     """
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
