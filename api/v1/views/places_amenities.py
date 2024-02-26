90932c8db4e04d655b673d3fa7d8e7495aba467c
Colightech <saintchris4@gmail.com>
2024-02-26 13:00:46 -0800

    AirBnB_clone_v3

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,97 +1,105 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for Place - Amenity """
+"""
+route for handling place and amenities linking
+"""
+from flask import jsonify, abort
+from os import getenv
+
+from api.v1.views import app_views, storage
 
 
-from models.place import Place
-from models.amenity import Amenity
-from models import storage
-from api.v1.views import app_views
-from os import environ
-from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
-
-
-@app_views.route('places/<place_id>/amenities', methods=['GET'],
+@app_views.route("/places/<place_id>/amenities",
+                 methods=["GET"],
                  strict_slashes=False)
-@swag_from('documentation/place_amenity/get_places_amenities.yml',
-           methods=['GET'])
-def get_place_amenities(place_id):
+def amenity_by_place(place_id):
     """
-    Retrieves the list of all Amenity objects of a Place
+    get all amenities of a place
+    :param place_id: amenity id
+    :return: all amenities
     """
-    place = storage.get(Place, place_id)
+    fetched_obj = storage.get("Place", str(place_id))
 
-    if not place:
+    all_amenities = []
+
+    if fetched_obj is None:
         abort(404)
 
-    if environ.get('HBNB_TYPE_STORAGE') == "db":
-        amenities = [amenity.to_dict() for amenity in place.amenities]
-    else:
-        amenities = [storage.get(Amenity, amenity_id).to_dict()
-                     for amenity_id in place.amenity_ids]
+    for obj in fetched_obj.amenities:
+        all_amenities.append(obj.to_json())
 
-    return jsonify(amenities)
+    return jsonify(all_amenities)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'], strict_slashes=False)
-@swag_from('documentation/place_amenity/delete_place_amenities.yml',
-           methods=['DELETE'])
-def delete_place_amenity(place_id, amenity_id):
-    """
-    Deletes a Amenity object of a Place
-    """
-    place = storage.get(Place, place_id)
-
-    if not place:
-        abort(404)
-
-    amenity = storage.get(Amenity, amenity_id)
-
-    if not amenity:
-        abort(404)
-
-    if environ.get('HBNB_TYPE_STORAGE') == "db":
-        if amenity not in place.amenities:
-            abort(404)
-        place.amenities.remove(amenity)
-    else:
-        if amenity_id not in place.amenity_ids:
-            abort(404)
-        place.amenity_ids.remove(amenity_id)
-
-    storage.save()
-    return make_response(jsonify({}), 200)
-
-
-@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 methods=["DELETE"],
                  strict_slashes=False)
-@swag_from('documentation/place_amenity/post_place_amenities.yml',
-           methods=['POST'])
-def post_place_amenity(place_id, amenity_id):
+def unlink_amenity_from_place(place_id, amenity_id):
     """
-    Link a Amenity object to a Place
+    unlinks an amenity in a place
+    :param place_id: place id
+    :param amenity_id: amenity id
+    :return: empty dict or error
     """
-    place = storage.get(Place, place_id)
-
-    if not place:
+    if not storage.get("Place", str(place_id)):
+        abort(404)
+    if not storage.get("Amenity", str(amenity_id)):
         abort(404)
 
-    amenity = storage.get(Amenity, amenity_id)
+    fetched_obj = storage.get("Place", place_id)
+    found = 0
 
-    if not amenity:
+    for obj in fetched_obj.amenities:
+        if str(obj.id) == amenity_id:
+            if getenv("HBNB_TYPE_STORAGE") == "db":
+                fetched_obj.amenities.remove(obj)
+            else:
+                fetched_obj.amenity_ids.remove(obj.id)
+            fetched_obj.save()
+            found = 1
+            break
+
+    if found == 0:
         abort(404)
-
-    if environ.get('HBNB_TYPE_STORAGE') == "db":
-        if amenity in place.amenities:
-            return make_response(jsonify(amenity.to_dict()), 200)
-        else:
-            place.amenities.append(amenity)
     else:
-        if amenity_id in place.amenity_ids:
-            return make_response(jsonify(amenity.to_dict()), 200)
-        else:
-            place.amenity_ids.append(amenity_id)
+        resp = jsonify({})
+        resp.status_code = 201
+        return resp
 
-    storage.save()
-    return make_response(jsonify(amenity.to_dict()), 201)
+
+@app_views.route("/places/<place_id>/amenities/<amenity_id>",
+                 methods=["POST"],
+                 strict_slashes=False)
+def link_amenity_to_place(place_id, amenity_id):
+    """
+    links a amenity with a place
+    :param place_id: place id
+    :param amenity_id: amenity id
+    :return: return Amenity obj added or error
+    """
+
+    fetched_obj = storage.get("Place", str(place_id))
+    amenity_obj = storage.get("Amenity", str(amenity_id))
+    found_amenity = None
+
+    if not fetched_obj or not amenity_obj:
+        abort(404)
+
+    for obj in fetched_obj.amenities:
+        if str(obj.id) == amenity_id:
+            found_amenity = obj
+            break
+
+    if found_amenity is not None:
+        return jsonify(found_amenity.to_json())
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        fetched_obj.amenities.append(amenity_obj)
+    else:
+        fetched_obj.amenities = amenity_obj
+
+    fetched_obj.save()
+
+    resp = jsonify(amenity_obj.to_json())
+    resp.status_code = 201
+
+    return resp
--- models/user.py
+++ models/user.py
@@ -1,40 +1,51 @@
 #!/usr/bin/python3
-""" holds class User"""
-import models
+"""
+User Class from Models Module
+"""
+import os
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import hashlib
+from sqlalchemy import Column, Integer, String, Float
+from hashlib import md5
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
-    if models.storage_t == 'db':
+    """User class handles all application users"""
+    if storage_type == "db":
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+        password = Column("password", String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+
+        places = relationship('Place', backref='user', cascade='delete')
+        reviews = relationship('Review', backref='user', cascade='delete')
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
 
     def __init__(self, *args, **kwargs):
         """
-            instantiates user object
+        initialize User Model, inherits from BaseModel
         """
-        if kwargs:
-            pwd = kwargs.pop('password', None)
-            if pwd:
-                # Hash the password using MD5
-                secure = hashlib.md5()
-                secure.update(pwd.encode("utf-8"))
-                secure_password = secure.hexdigest()
-                kwargs['password'] = secure_password
         super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """
+        getter for password
+        :return: password (hashed)
+        """
+        return self.__dict__.get("password")
+
+    @password.setter
+    def password(self, password):
+        """
+        Password setter, with md5 hasing
+        :param password: password
+        :return: nothing
+        """
+        self.__dict__["password"] = md5(password.encode('utf-8')).hexdigest()
