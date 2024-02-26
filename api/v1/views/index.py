88cca8a2268f178865ce6d132385526a18f3397f
kiplimoboor <kiplimoboor@hotmail.com>
2024-02-26 16:01:05 +0300

    count by class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,12 +6,6 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status')
@@ -24,14 +18,11 @@ def status():
 
 @app_views.route('/stats', strict_slashes=False)
 def stuff():
-    '''JSON Responses'''
-    todos = {'states': State,
-             'users': User,
-             'amenities': Amenity,
-             'cities': City,
-             'places': Place,
-             'reviews': Review
-             }
-    for key in todos:
-        todos[key] = storage.count(todos[key])
-    return jsonify(todos)
+    '''Count By Class and Return JSON'''
+    return jsonify({'states': storage.count('State'),
+                    'users': storage.count('User'),
+                    'amenities': storage.count('Amenity'),
+                    'cities': storage.count('City'),
+                    'places': storage.count('Place'),
+                    'reviews': storage.count('Review'),
+                    })
