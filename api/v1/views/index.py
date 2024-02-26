5368d7e004e9c68e9ece38c3620783f1616eb42c
danielntow <danielntow@gmail.com>
2024-02-26 12:53:18 +0000

    endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,15 @@ routes
 from api.v1.views import app_views
 # api/v1/views/index.py
 from flask import jsonify, Blueprint
+from models import storage
+import models
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.review import Review
+from models.city import City
 
 app_views = Blueprint('app_views', __name__)
 
@@ -13,3 +22,11 @@ app_views = Blueprint('app_views', __name__)
 def status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each object by type"""
+    classes = ["Amenity", "City", "Place", "Review", "State", "User"]
+    stats = {cls.lower(): storage.count(eval(cls)) for cls in classes}
+    return jsonify(stats)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,17 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve one object"""
+        obj = self.__session.query(cls).get(id)
+        return obj if obj else None
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        if cls:
+            count = self.__session.query(cls).count()
+        else:
+            count = sum(self.__session.query(cls).count()
+                        for cls in classes.values())
+        return count
