978b2658b90df85192cc3b30f105e3cae515862e
hepona <hepona10@gmail.com>
2024-02-26 13:48:58 +0100

    404 + count cobj

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """app.py"""
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 HBNB_API_HOST = "0.0.0.0"
@@ -18,9 +18,16 @@ def close_storage(exception=None):
     storage.close()
 
 
+@app.errorhandler(404)
+def error_handler(e):
+    """handle 404 error"""
+    return jsonify({"error": "Not found"})
+
+
 if __name__ == "__main__":
-    host = (
-        "0.0.0.0" if "HBNB_API_HOST" not in os.environ else os.getenv("HBNB_API_HOST")
-    )
-    port = 5000 if "HBNB_API_PORT" not in os.environ else os.getenv("HBNB_API_PORT")
+    host = "0.0.0.0" if "HBNB_API_HOST" not in \
+            os.environ else os.getenv("HBNB_API_HOST")
+
+    port = 5000 if "HBNB_API_PORT" not in \
+        os.environ else os.getenv("HBNB_API_PORT")
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,25 @@
 """index """
 from api.v1.views import app_views
 from flask import Flask, jsonify
+from models import storage
 
 
 @app_views.route("/status")
-def app_views():
+def statue():
     """return the status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def state_count():
+    """return the count"""
+    return jsonify(
+        {
+            "amenities": storage.count("amenity"),
+            "cities": storage.count("city"),
+            "places": storage.count("place"),
+            "reviews": storage.count("review"),
+            "states": storage.count("state"),
+            "users": storage.count("user")
+        }
+    )
