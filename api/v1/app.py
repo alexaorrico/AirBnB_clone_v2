1fa05f4a604ec085c0d0be4e4f744d7d86ea456e
aminoxlhd <aminoxlhd@gmail.com>
2024-02-26 03:23:22 +0100

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ app.py file"""
-from flask import Flask, make_response ,jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,9 +13,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext():
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
