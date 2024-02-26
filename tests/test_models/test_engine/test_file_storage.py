d5ac4cd242c2de0f99ca6afa34650cf338335396
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-26 06:58:05 +0200

    tests updated

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,7 +10,6 @@ from models.engine import file_storage
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
-from models.engine.db_storage import DBStorage
 from models.place import Place
 from models.review import Review
 from models.state import State
