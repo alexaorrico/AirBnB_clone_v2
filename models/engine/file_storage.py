00dcb6cc9edd59c1c15a46579ef77d493c9c1fbd
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-26 22:45:24 +0100

    fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -78,6 +78,7 @@ class FileStorage:
             k = key.split('.')
             if k[1] == id:
                 return {key: value}
+        return None
 
     def count(self, cls=None):
         """ Retrieves the count of object(s) stored """
