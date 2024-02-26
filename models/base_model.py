6f7cff0eb5419791a1e77406a261e9450ec565cc
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-26 22:53:47 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -58,25 +58,20 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
-    """returns a dictionary containing all keys/values of the instance"""
-    new_dict = self.__dict__.copy()
-    if "created_at" in new_dict:
-        new_dict["created_at"] = new_dict["created_at"].strftime(time)
-    if "updated_at" in new_dict:
-        new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
-    new_dict["__class__"] = self.__class__.__name__
-    if "_sa_instance_state" in new_dict:
-        del new_dict["_sa_instance_state"]
-    frame = inspect.currentframe().f_back
-    func_name = frame.f_code.co_name
-    class_name = ''
-    if 'self' in frame.f_locals:
-        class_name = frame.f_locals["self"].__class__.__name__
-    is_fs_writing = func_name == 'save' and class_name == 'FileStorage'
-    if 'password' in new_dict and not is_fs_writing:
-        del new_dict['password']
-    return new_dict
+    def to_dict(self, save_fs=None):
+        """returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        if save_fs is None:
+            if "password" in new_dict:
+                del new_dict["password"]
+        return new_dict
 
     def delete(self):
         """delete the current instance from the storage"""
