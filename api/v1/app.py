e685d57a65dd40e4a9e1b8a5e1ca0d112dfaa349
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-26 11:51:07 -0800

    task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 '''app.py file'''
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+CORS(app, resources={r'/api/v1/*': {'origins': '0.0.0.0'}})
 
 
 @app.teardown_appcontext
