45a89c4564a77dd3b67d6dbaaf30be583a817a6e
Naftali-B <nbudamba@gmail.com>
2024-02-26 13:07:39 +0300

    incomplete 3

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+returns api
+"""
+
+from flask import Flask, jsonify, make_response
+# from flask_cors import CORS
+import os
+# from os import getenv
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})  # Allowing CORS from all origins
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """removes the current sqlalchemy session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def errorhandler(error):
+    """handles 404 errors"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get('HBNB_API_HOST'),
+            port=os.environ.get('HBNB_API_PORT'))
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''makes directory a package'''
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.i import *
