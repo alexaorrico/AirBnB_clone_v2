0fc9cee140a397924e71df3012b71db18520db5d
DouibaOmar <douibaomar40@gmail.com>
2024-02-26 11:01:39 +0100

    Done

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,38 +11,44 @@ from flask import jsonify, abort, request
 
 
 @app_views.route('/states/<string:state_id>/cities',
-                 methods=['GET'], strict_slashes=False)
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
 def states(state_id=None):
     states = storage.get(State, state_id)
     if states is None:
         abort(404)
     else:
-        cities = []
-        for city in states.cities:
-            cities.append(city.to_dict())
-        return jsonify(cities)
+        if request.methods == 'GET':
+            cities = []
+            for city in states.cities:
+                cities.append(city.to_dict())
+            return jsonify(cities)
+        elif request.methods == 'POST':
+            if request.get_json:
+                data = request.get_json
+                name = data.get('name')
+                if name:
+                    new_city = City(**data)
+                    setattr(new_city, 'state_id', state_id)
+                    storage.save(new_city)
+                    return jsonify(new_city.to_dict()), 201
+                else:
+                    abort(400, 'Missing name')
+            else:
+                abort(400, 'Not a JSON')
 
 
 @app_views.route('/cities/<string:city_id>',
-                 methods=['GET'], strict_slashes=False)
+                 methods=['GET', 'DELETE'],
+                 strict_slashes=False)
 def cities(city_id=None):
     cities = storage.get(City, city_id)
     if cities is None:
         abort(404)
     else:
-        return jsonify(cities.to_dict())
-
-
-@app_views.route('/cities/<city_id>',
-                 methods=['DELETE'], strict_slashes=False)
-def del_cities(city_id=None):
-    """
-    remove city that has specific id
-    """
-    cities = storage.get(City, city_id)
-    if cities is None:
-        abort(404)
-    else:
-        storage.delete(cities)
-        storage.save()
-        return {}, 200
+        if request.methods == 'GET':
+            return jsonify(cities.to_dict())
+        elif request.methods == 'DELETE':
+            storage.delete(cities)
+            storage.save()
+            return {}, 200
