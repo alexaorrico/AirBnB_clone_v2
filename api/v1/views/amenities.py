7125b01a5cd7d0d402fcf7f368bceb47e7bdef89
fae713 <favourmichael47@gmail.com>
2024-02-26 06:50:06 -0800

    task 6 debugging

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python3
-"""this is the state view for the API"""
+"""this is the amenities view for the API"""
 from flask import jsonify, request
 from werkzeug.exceptions import NotFound, MethodNotAllowed, BadRequest
 
 from api.v1.views import app_views
 from models import storage
-from models.state import State
+from models.amenity import Amenity
 
 ALLOWED_METHODS = ['GET', 'DELETE', 'POST', 'PUT']
-"""HTTP methods allowed for states"""
+"""HTTP methods allowed for amenities"""
 
 
-@app_views.route('/states', methods=ALLOWED_METHODS)
-@app_views.route('/states/<state_id>', methods=ALLOWED_METHODS)
+@app_views.route('/amenities', methods=ALLOWED_METHODS)
+@app_views.route('/amenities/<amenity_id>', methods=ALLOWED_METHODS)
 def handle_states(state_id=None):
-    """handles all allowed HTTP methods to state(id)."""
+    """handles all allowed HTTP methods to amenity(id)."""
     handlers = {
-        'GET': get_states,
-        'DELETE': del_state,
-        'POST': add_state,
-        'PUT': update_state,
+        'GET': get_amenity,
+        'DELETE': del_amenity,
+        'POST': add_amenity,
+        'PUT': update_amenity,
     }
     if request.method in handlers:
-        return handlers[request.method](state_id)
+        return handlers[request.method](amenity_id)
     else:
         raise MethodNotAllowed(list(handlers.keys()))
 
 
-def get_states(state_id=None):
-    """uses the GET method to retrieve a state(id)."""
-    all_states = storage.all(State).values()
-    if state_id:
-        unique_state = [state for state in all_states if state.id == state_id]
-        if unique_state:
-            return jsonify(unique_state[0].to_dict())
+def get_amenity(amenity_id=None):
+    """uses the GET method to retrieve an amenity(id) or all amenities."""
+    all_amenities = storage.all(Amenity).values()
+    if amenity_id:
+        unique_amenity = [amenity for amenity in all_amenities if amenity.id == amenity_id]
+        if unique_amenity:
+            return jsonify(unique_amenity[0].to_dict())
         else:
             raise NotFound()
     else:
-        all_states_dicts = [state.to_dict() for state in all_states]
-        return jsonify(all_states_dicts)
+        all_amenities_dicts = [amenity.to_dict() for amenity in all_amenities]
+        return jsonify(all_amenities_dicts)
 
 
-def del_state(state_id=None):
-    """uses the DELETE method to delete a state(id)."""
-    all_states = storage.all(State).values()
-    unique_state = [state for state in all_states if state.id == state_id]
+def del_amenity(amenity_id=None):
+    """uses the DELETE method to delete an amenity(id)."""
+    all_amenities = storage.all(Amenity).values()
+    unique_amenity = [amenity for amenity in all_amenities if amenity.id == amenity_id]
     if unique_state:
-        state_to_delete = unique_state[0]
-        storage.delete(state_to_delete)
+        amenity_to_delete = unique_unique[0]
+        storage.delete(amenity_to_delete)
         storage.save()
 
         return jsonify({}), 200
