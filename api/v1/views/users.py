5bbde26dbc8399283ea674e3ee26e75ee443c63d
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-26 22:30:40 +0200

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,18 +7,36 @@ from models.user import User
 
 @app_views.route("/users", methods=["GET"], strict_slashes=False)
 def get_all_users():
-    """
-    Retrieve all User objects
-    """
+    """ Retrieve all User objects """
     users = storage.all(User).values()
     return jsonify([user.to_dict() for user in users])
 
 
+@app_views.route("/users/<user_id>", methods=["GET"], strict_slashes=False)
+def get_user_by_id(user_id):
+    """ Get a specific User by ID """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route("/users/<user_id>", methods=["DELETE"], strict_slashes=False)
+def delete_user_by_id(user_id):
+    """ Delete a User by ID """
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+
+    storage.delete(user)
+    storage.save()
+
+    return jsonify({}), 200
+
+
 @app_views.route("/users", methods=["POST"], strict_slashes=False)
 def create_user():
-    """
-    Create a new User
-    """
+    """ Create a new User """
     user_json = request.get_json()
     if not user_json:
         abort(400, 'Not a JSON')
@@ -33,22 +51,9 @@ def create_user():
     return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route("/users/<user_id>",  methods=["GET"], strict_slashes=False)
-def get_user_by_id(user_id):
-    """
-    Get a specific User by ID
-    """
-    user = storage.get(User, user_id)
-    if not user:
-        abort(404)
-    return jsonify(user.to_dict())
-
-
-@app_views.route("/users/<user_id>",  methods=["PUT"], strict_slashes=False)
+@app_views.route("/users/<user_id>", methods=["PUT"], strict_slashes=False)
 def update_user(user_id):
-    """
-    Update a specific User by ID
-    """
+    """ Update a specific User by ID """
     user = storage.get(User, user_id)
     if not user:
         abort(404)
@@ -64,18 +69,3 @@ def update_user(user_id):
     user.save()
 
     return jsonify(user.to_dict()), 200
-
-
-@app_views.route("/users/<user_id>",  methods=["DELETE"], strict_slashes=False)
-def delete_user_by_id(user_id):
-    """
-    Delete a User by ID
-    """
-    user = storage.get(User, user_id)
-    if not user:
-        abort(404)
-
-    storage.delete(user)
-    storage.save()
-
-    return jsonify({}), 200
