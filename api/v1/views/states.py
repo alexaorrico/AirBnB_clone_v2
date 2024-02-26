fb8ace0780e72c543b05fb06d57d3207a110f78f
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-26 15:52:48 +0100

    api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """State objects that handles all default RESTFul API actions:"""
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
 from models.state import State
+from flasgger.utils import swag_from
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_state.yml', methods=['GET'])
 def get_states():
     """Retrieves the list of all State objects"""
     states = [state.to_dict() for state in storage.all(State).values()]
@@ -14,6 +16,7 @@ def get_states():
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@swag_from('documentation/state/get_id_state.yml', methods=['get'])
 def get_state(state_id):
     """Retrieves a State object"""
     state = storage.get(State, state_id)
@@ -23,6 +26,7 @@ def get_state(state_id):
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@swag_from('documentation/state/delete_state.yml', methods=['DELETE'])
 def delete_state(state_id):
     """Deletes a State object"""
     state = storage.get(State, state_id)
@@ -30,10 +34,11 @@ def delete_state(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return jsonify({})
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
+@swag_from('documentation/state/post_state.yml', methods=['POST'])
 def create_state():
     """Creates a State"""
     data = request.get_json()
@@ -47,6 +52,7 @@ def create_state():
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@swag_from('documentation/state/put_state.yml', methods=['PUT'])
 def update_state(state_id):
     """Updates a State object"""
     state = storage.get(State, state_id)
