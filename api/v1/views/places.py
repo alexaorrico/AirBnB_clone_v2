79fab2cdd4329fcedf6fd7dc925b94ff840281f7
danielntow <danielntow@gmail.com>
2024-02-26 19:28:08 +0000

    implement search abilitites

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -10,6 +10,7 @@ from models.place import Place
 from models.city import City
 from models.user import User
 from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
 
