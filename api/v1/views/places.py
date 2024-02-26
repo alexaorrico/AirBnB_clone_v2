2e98ad1a13002426b83539becbf5c17e34d09058
othmane <othmane.alaouimekkaoui@gmail.com>
2024-02-26 11:15:56 -0800

    task 10 update

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Create a new view for Place objects that handles all 
+'''Create a new view for Place objects that handles all
 default RESTFul API actions'''
 from flask import abort, jsonify, request
 from models.city import City
@@ -84,75 +84,3 @@ def update_place(place_id):
         return jsonify(place.to_dict()), 200
     else:
         abort(404)
-
-
-# New endpoint: POST /api/v1/places_search
-@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
-def places_search():
-    """
-    Retrieves Place objects based on the provided JSON search criteria
-    """
-
-    # Check if the request contains valid JSON
-    if request.get_json() is None:
-        abort(400, description="Not a JSON")
-
-    # Extract data from the JSON request body
-    data = request.get_json()
-
-    if data and len(data):
-        states = data.get('states', None)
-        cities = data.get('cities', None)
-        amenities = data.get('amenities', None)
-
-    # If no criteria provided, retrieve all places
-    if not data or not len(data) or (
-            not states and
-            not cities and
-            not amenities):
-        places = storage.all(Place).values()
-        list_places = []
-        for place in places:
-            list_places.append(place.to_dict())
-        return jsonify(list_places)
-
-    list_places = []
-
-    # Filter and retrieve places based on states criteria
-    if states:
-        states_obj = [storage.get(State, s_id) for s_id in states]
-        for state in states_obj:
-            if state:
-                for city in state.cities:
-                    if city:
-                        for place in city.places:
-                            list_places.append(place)
-
-    # Filter and retrieve places based on cities criteria
-    if cities:
-        city_obj = [storage.get(City, c_id) for c_id in cities]
-        for city in city_obj:
-            if city:
-                for place in city.places:
-                    if place not in list_places:
-                        list_places.append(place)
-
-    # Filter places based on amenities criteria
-    if amenities:
-        if not list_places:
-            list_places = storage.all(Place).values()
-        amenities_obj = [storage.get(Amenity, a_id) for a_id in amenities]
-
-        list_places = [place for place in list_places
-                       if all([am in place.amenities
-                               for am in amenities_obj])]
-
-    # Prepare the final list of places for response
-    places = []
-    for p in list_places:
-        d = p.to_dict()
-        d.pop('amenities', None)
-        places.append(d)
-
-    # Return the list of places in JSON format
-    return jsonify(places)
