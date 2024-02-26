ea0e4053ad2e84ceebdce2ffece17989e82c4cf7
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-26 11:29:02 +0100

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from flask import Flask
-import os
+from os import environ
 from models import storage
 from api.v1.views import app_views
 
@@ -15,6 +15,11 @@ def close_db(error):
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
