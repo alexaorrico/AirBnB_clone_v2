b5083174d9eb4b8472ae67c065e9a71d12381934
Alhadji Oumate <alhadjioum0@gmail.com>
2024-02-27 00:41:23 +0100

    state request

---
+++ api/v1/views/states.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""flask return json response"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage, classes
+
+
+@app_views.route('/states', methods=['GET', 'POST'])
+def states_no_id():
+    """
+        handle http method with no id provided for state request
+    """
+    if request.method == 'GET':
+        states = storage.all('State')
+        states = list(obj.to_json() for obj in states.values())
+        return jsonify(states)
+
+    if request.method == 'POST':
+        json_request = request.get_json()
+        if json_request is None:
+            abort(400, 'Not a JSON')
+        if json_request.get("name") is None:
+            abort(400, 'Missing name')
+        State = classes.get("State")
+        new_object = State(**json_request)
+        new_object.save()
+        return jsonify(new_object.to_json()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+def states_with_id(state_id=None):
+    """
+        handle http method with id provided for state request
+    """
+    state_obj = storage.get('State', state_id)
+    if state_obj is None:
+        abort(404, 'Not found')
+
+    if request.method == 'GET':
+        return jsonify(state_obj.to_json())
+
+    if request.method == 'DELETE':
+        state_obj.delete()
+        del state_obj
+        return jsonify({})
+
+    if request.method == 'PUT':
+        json_request = request.get_json()
+        if json_request is None:
+            abort(400, 'Not a JSON')
+        state_obj.bm_update(json_request)
+        return jsonify(state_obj.to_json())
--- models/__init__.py
+++ models/__init__.py
@@ -9,9 +9,11 @@ from os import getenv
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
+    from models.engine import db_storage
+    storage = db_storage.DBStorage()
+    classes = db_storage.classes
 else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+    from models.engine import file_storage
+    storage = file_storage.FileStorage()
+    classes = file_storage.classes
 storage.reload()
