812276bca2e5601fb28ea0baa98adfc4d39cd6c9
Lamber22 <Lamberjoe5@gmail.com>
2024-02-26 23:40:59 +0000

    update2

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -83,7 +83,9 @@ class FileStorage:
         try:
             with open(fname, mode='r', encoding='utf-8') as f_io:
                 new_objs = json.load(f_io)
-        except:
+        except:   # noqa E722
+            pass  # file doesn't exist yet
+        else:
             return
         for o_id, d in new_objs.items():
             k_cls = d['__class__']
