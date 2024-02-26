87763ece173ae163d0ca1064a1b942ac4979dec4
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-02-26 23:07:49 +0200

    Update places_reviews.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ Route for handling place reviews """
-from flask import jsonify, abort, request
-from api.v1.views import app_views, storage
+from flask import jsonify, abort, request, make_response
+from api.v1.views import app_views
 from models.review import Review
 from models.place import Place
 from models.user import User
+from models import storage
+
 
 @app_views.route("/places/<place_id>/reviews", methods=["GET"],
                  strict_slashes=False)
-def get_reviews_by_place(place_id):
+def get_reviews(place_id):
     """
     Retrieves all Review objects by place.
     :param place_id: ID of the place
@@ -16,20 +18,20 @@ def get_reviews_by_place(place_id):
     """
 
     list_of_reviews = []
-    obj_place = storage.get(Place, str(place_id))
+    obj_place = storage.get(Place, place_id)
 
     if obj_place is None:
         abort(404)
 
     for obj in obj_place.reviews:
-        list_of_reviews.append(obj.to_json())
+        list_of_reviews.append(obj.to_dict())
 
     return jsonify(list_of_reviews)
 
 
 @app_views.route("/places/<place_id>/reviews", methods=["POST"],
                  strict_slashes=False)
-def create_review(place_id):
+def post_review(place_id):
     """
     Creates a new Review object.
     :param place_id: ID of the place
@@ -52,7 +54,7 @@ def create_review(place_id):
 
     new_review = Review(**review_json)
     new_review.save()
-    rsponse = jsonify(new_review.to_json())
+    rsponse = jsonify(new_review.to_dict())
     rsponse.status_code = 201
 
     return rsponse
@@ -60,14 +62,14 @@ def create_review(place_id):
 
 @app_views.route("/reviews/<review_id>", methods=["GET"],
                  strict_slashes=False)
-def get_review_by_id(review_id):
+def get_review(review_id):
     """
     Gets a specific Review object by ID.
     :param review_id: ID of the review
     :return: Review object with the specified ID or error
     """
 
-    obj_fetched = storage.get(Review, str(review_id))
+    obj_fetched = storage.get(Review, review_id)
 
     if obj_fetched is None:
         abort(404)
@@ -77,24 +79,24 @@ def get_review_by_id(review_id):
 
 @app_views.route("/reviews/<review_id>", methods=["PUT"],
                  strict_slashes=False)
-def update_review_by_id(review_id):
+def put_review(review_id):
     """
     Updates a specific Review object by ID.
     :param review_id: ID of the review
     :return: Review object and 200 on success, or 400 or 404 on failure
     """
 
-    review_json = request.get_json(silent=True)
+    place_json = request.get_json(silent=True)
 
-    if review_json is None:
+    if place_json is None:
         abort(400, 'Not a JSON')
 
-    obj_fetched = storage.get(Review, str(review_id))
+    obj_fetched = storage.get(Review, review_id)
 
     if obj_fetched is None:
         abort(404)
 
-    for key, val in review_json.items():
+    for key, val in place_json.items():
         if key not in ["id", "created_at", "updated_at", "user_id",
                        "place_id"]:
             setattr(obj_fetched, key, val)
@@ -106,14 +108,14 @@ def update_review_by_id(review_id):
 
 @app_views.route("/reviews/<review_id>", methods=["DELETE"],
                  strict_slashes=False)
-def delete_review_by_id(review_id):
+def delete_review(review_id):
     """
     Deletes a Review by ID.
     :param review_id: ID of the review
     :return: Empty dictionary with 200, or 404 if not found
     """
 
-    obj_fetched = storage.get(Review, str(review_id))
+    obj_fetched = storage.get(Review, review_id)
 
     if obj_fetched is None:
         abort(404)
