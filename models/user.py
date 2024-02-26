a10b6ad668cedbca93f552f2aedde67f3f364daa
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-02-26 17:07:43 +0200

    Updated user.py and base_model.py by adding to both of them a password hash function for security

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,8 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        secure = hashlib.md5()
+        secure.update(pwd.encode("utf-8"))
+        secure_password = secure.hexdigest()
+        kwargs['password'] = secure_password
         super().__init__(*args, **kwargs)
