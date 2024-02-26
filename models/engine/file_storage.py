f0e05fc041dbe4969319b6bd10f16966f1bb4282
stanleywanekeya <stanleywanekeya1123@gmail.com>
2024-02-26 15:33:54 +0300

    Addition of tests

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,22 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Method to return object from storage"""
+        if cls is not None and type(cls) is str and id is not None and\
+            type(id) is str and cls in classes:
+            key = cls + '.' + id
+            obj = self.__objects.get(key, None)
+            return obj
+        else:
+            return None
+
+    def count(self, cls=None):
+        """Returns the number of object in storage"""
+        total = 0
+        if type(cls) == str and cls in classes:
+            total = len(self.all(cls))
+        elif cls is None:
+            total = len(self.__objects)
+        return total
