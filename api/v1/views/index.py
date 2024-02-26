09964e0f17f7d90cf395b84229adea942e056c0a
El Houssain Souhail <sou.ductive@gmail.com>
2024-02-26 18:07:10 +0100

    Added /api/v1/stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Status route for the AirBnB API"""
 from flask import jsonify
+from models import storage
 from api.v1.views import app_views
 
 
@@ -8,3 +9,16 @@ from api.v1.views import app_views
 def status():
     """Returns the status of the server"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    """Returns the number of each object type"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
