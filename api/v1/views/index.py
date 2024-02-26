9de01875f1f01245192ee19fb087c19fe5963a2a
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-26 11:35:17 +0100

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """ Status of API """
     return jsonify({"status": "OK"})
