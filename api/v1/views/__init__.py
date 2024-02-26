ab4466988ded64c9016d8f0e55bf4cc584f9c756
danielntow <danielntow@gmail.com>
2024-02-26 12:22:53 +0000

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,9 +2,11 @@
 """the init file for this package
 """
 
-#
 from flask import Blueprint
 
-# app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-# Create an empty Blueprint
-app_views = Blueprint('app_views', __name__)
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+#from api.v1.views.states import *
+from api.v1.views.index import *
