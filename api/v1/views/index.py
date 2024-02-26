ee9108a044737e4e2bd4320f3c7319d4259a0474
DouibaOmar <douibaomar40@gmail.com>
2024-02-26 11:49:38 +0100

    Done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,35 +1,35 @@
-#!/usr/bin/python3
-""" Index script """
-from api.v1.views import app_views
+"""Index script"""
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models import storage
 
-
-@app_views.route('/status', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Returns a JSON with status "OK"."""
+    """
+    Returns a JSON response indicating the status of the API.
+    """
     return jsonify(status="OK")
 
-
-@app_views.route('/stats', methods=['GET'],
-                 strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """
-    Retrieves the number of objects by type.
+    Retrieves the counts of various object types in the database.
     """
-    stats = {
-        'amenities': storage.count(Amenity),
-        'cities': storage.count(City),
-        'places': storage.count(Place),
-        'reviews': storage.count(Review),
-        'states': storage.count(State),
-        'users': storage.count(User)
-    }
-    return jsonify(stats)
+    try:
+        stats = {
+            'amenities': storage.count(Amenity),
+            'cities': storage.count(City),
+            'places': storage.count(Place),
+            'reviews': storage.count(Review),
+            'states': storage.count(State),
+            'users': storage.count(User)
+        }
+        return jsonify(stats)
+    except Exception as e:
+        return jsonify(error=str(e)), 500
