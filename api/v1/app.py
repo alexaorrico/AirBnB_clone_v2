51aa4891e3585de42432ff7288d067b01a716d56
DouibaOmar <douibaomar40@gmail.com>
2024-02-26 15:08:31 +0100

    Done

--- api/v1/app.py
+++ api/v1/app.py
@@ -33,3 +33,4 @@ if __name__ == '__main__':
                 threaded=True)
     else:
         app.run(host='0.0.0.0', port='5000', threaded=True)
+        
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ User script """
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
 from models import storage
@@ -12,7 +12,17 @@ def get_or_add_user():
         return jsonify(users.to_dict())
     elif request.methods == 'POST':
         if request.get_json:
-            data = request.get_json 
+            data = request.get_json
+            email = data.get("email")
+            password = data.get('password')
 
+            if email:
+                abort(400, 'Missing email')
+            if password:
+                abort(400, 'Missing password')
+
+            new_user = User(**data)
+            storage.save(new_user)
+            return jsonify(new_user.to_dict()), 201
         else:
             abort(400, 'Not a JSON')
