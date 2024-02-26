d754cbe96e306cebe747b5ed071a48dac96ed7e6
Ajoe62 <ijeborjoe@gmail.com>
2024-02-26 18:48:07 +0000

    refactor file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,19 +74,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def get(self, cls, id):
-        """ retrieves """
-        if cls in classes.values() and id and type(id) == str:
-            d_obj = self.all(cls)
-            for key, value in d_obj.items():
-                if key.split(".")[1] == id:
-                    return value
-        return None
-
-    def count(self, cls=None):
-        """ counts """
-        data = self.all(cls)
-        if cls in classes.values():
-            data = self.all(cls)
-        return len(data)
