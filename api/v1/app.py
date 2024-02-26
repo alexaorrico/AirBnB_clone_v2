ab319ae81eacf9fa0526fac54fd38d418a8e1110
kipngeno kevin <kipngenokevin254@gmail.com>
2024-02-26 13:23:09 +0000

    new folder changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-returns api
+Your first endpoint (route) will be to return the status of your API:
 """
 
 from flask import Flask, jsonify, make_response
-# from flask_cors import CORS
+from flask_cors import CORS
 import os
 # from os import getenv
 from models import storage
@@ -12,7 +12,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})  # Allowing CORS from all origins
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -24,9 +24,10 @@ def teardown(exc):
 @app.errorhandler(404)
 def errorhandler(error):
     """handles 404 errors"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 
 if __name__ == "__main__":
+    """runs the api using  environment variables"""
     app.run(host=os.environ.get('HBNB_API_HOST'),
             port=os.environ.get('HBNB_API_PORT'))
