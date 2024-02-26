f63604bff1bbe55030526baf7974e596b7149648
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-02-26 13:47:01 +0100

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""index for blueprint"""
-from api.v1.views import app_views
-from flask import jsonify
-import models
+""" Index """
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -18,13 +18,13 @@ def status():
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """An endpoint that retrieves the number of each objects by type:"""
+def number_objects():
+    """ Retrieves the number of each objects by type """
     classes = [Amenity, City, Place, Review, State, User]
     names = ["amenities", "cities", "places", "reviews", "states", "users"]
 
     num_objs = {}
     for i in range(len(classes)):
-        num_objs[names[i]] = models.storage.count(classes[i])
+        num_objs[names[i]] = storage.count(classes[i])
 
     return jsonify(num_objs)
