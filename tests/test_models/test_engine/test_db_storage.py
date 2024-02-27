3bbc282f4aaf546421b1c1de3531ba3e32b427d7
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-27 22:25:13 +0100

    add tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -95,7 +95,7 @@ class TestFileStorage(unittest.TestCase):
     def test_new(self):
         """test that new adds an object to the database"""
         db = models.storage
-        in
+        ini = len(db.all(State))
         state = State({'name': "california", 'age': 20})
         db.new(state)
         st = 
