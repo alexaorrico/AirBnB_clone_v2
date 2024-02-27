7ca36d955b2486ed19571f6f8fe809ce73a5e876
Kwizera Daniel <kwizeradan.kd27@gmail.com>
2024-02-26 16:09:53 -0800

    Done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,46 +1,30 @@
 #!/usr/bin/python3
 """
-index
+Create app view
 """
 
 from flask import jsonify
 from api.v1.views import app_views
-
 from models import storage
+from models import amenity, city, place, review, state, user
 
 
-@app_views.route("/status", methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
-    """
-    status route
-    :return: response with json
-    """
-    data = {
-        "status": "OK"
-    }
+    ''' return status '''
 
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    return (jsonify({"status": "OK"}))
 
 
-@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats():
-    """
-    stats of all objs route
-    :return: json of all objs
-    """
-    data = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
+    """Returns statistics."""
 
-    resp = jsonify(data)
-    resp.status_code = 200
-
-    return resp
+    # Get the counts from your data storage
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")
+                    })
