0d3b8e3bbfd9796322d00208e587458447b4d6cb
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-27 23:29:07 +0100

    add tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -159,4 +159,5 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
         """ Tests the count() """
-        db = mode
+        db = models.storage
+        self.assertFalse
