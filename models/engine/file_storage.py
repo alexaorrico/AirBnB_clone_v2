1b7cf9c9be1d18e3a12344c6ae7786d09fe958be
Kevin7744 <arapbiisubmissions@gmail.com>
2024-02-27 10:09:45 +0300

    Update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        '''object to get'''
+        if cls and id:
+            takeObj = '{}.{}'.format(cls, id)
+            everyObj = self.all(cls)
+            return everyObj.get(takeObj)
+        else:
+            return None
+
+    def count(self, cls=None):
+        '''class that is (optional)'''
+        return (len(self.all(cls)))
