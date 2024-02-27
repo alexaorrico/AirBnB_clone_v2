b45c5f6aff504339210b71f007536f82591f611e
mostafashraf18 <ma2963629@gmail.com>
2024-02-27 07:24:29 +0200

    Gg

--- models/user.py
+++ models/user.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """ holds class User"""
+import models
 from models.base_model import BaseModel, Base
 from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """Representation of a user """
-    import models
-    if models.storage_t == 'db':
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user", cascade='all, delete-orphan')
-        reviews = relationship("Review", backref="user", cascade='all, delete-orphan')
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
