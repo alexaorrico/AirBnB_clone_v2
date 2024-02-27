9ed6f4d0191339d2067c9c821a9c26d497d97f51
root <deantoshdaiddoh@gmail.com>
2024-02-27 01:15:58 -0800

    update file to resolve circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,11 @@
 #!/bin/python3
-"""initialize views pacage"""
+"""initialize views package"""
 
 # imports
 from flask import Blueprint
-from api.v1.views.index import *
 
 # define a blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
+# avoid circular import
+from api.v1.views.index import *
