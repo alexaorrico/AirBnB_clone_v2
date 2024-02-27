da392730e1a029ee28b669bf057ccb56c176dfc6
root <deantoshdaiddoh@gmail.com>
2024-02-27 01:14:05 -0800

    app updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,28 +5,28 @@ Variable app, instance of Flask.
 from os import environ
 from flask import Flask
 from models import storage
-from  api.vi.views import app_views
+from  api.v1.views import app_views
 
 
 app = Flask(__name__)
 
 # register blueprint
-app.regiser_blueprint(app_views)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown_storage():
+def teardown_storage(exec=None):
     """closes storage connection"""
     storage.close()
 
 if __name__ == "__main__":
     # get environment variables and set default values if not provided
     host_name = environ.get("HBNB_API_HOST", default=None)
-    if !host_name:
+    if host_name is None:
         host_name = "0.0.0.0"
     conn_port = environ.get("HBNB_API_PORT", default=None)
-    if !conn_port:
+    if conn_port is None:
         conn_port = "5000"
 
     # run app
-    app.run(host=host_name, port=conn_port, threaded=True)
+    app.run(host=host_name, port=conn_port, threaded=True, debug=True)
