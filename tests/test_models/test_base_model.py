25a857905ce983af0f47c0223d5b2f73bc4a9ed6
aissap <jesus.jesus.core@gmail.com>
2024-02-27 15:22:51 -0800

    .\test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -86,7 +86,7 @@ class TestBaseModel(unittest.TestCase):
         inst1 = BaseModel()
         toc = datetime.now()
         self.assertTrue(tic <= inst1.created_at <= toc)
-        time.sleep(1e-4)
+        time.sleep(0.001)
         tic = datetime.now()
         inst2 = BaseModel()
         toc = datetime.now()
