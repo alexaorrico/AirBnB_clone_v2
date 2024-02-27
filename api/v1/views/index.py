f75bab887765c1c177788da5e20d1f754785e00c
khalildahbane <kelldani100@gmail.com>
2024-02-27 06:20:09 -0800

    RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""define routes of blueprint
-"""
+"""define routes of blueprint"""
 
 from api.v1.views import app_views
 from models import storage
@@ -14,6 +13,7 @@ from flask import jsonify
 
 """Defines routes using app_views"""
 @app_views.route("/status", strict_slashes=False, methods=["GET"])
+
 def status():
     return {
         "status": "OK",
