d66d1765e4a5c2eb247c13c1f18a6e7ac1affe74
achiri <achiri@achiritangu.com>
2024-02-26 22:16:35 -0800

    status and not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,6 +2,10 @@
 '''
 Createw Flask app; and register the blueprint app_views to Flask instance app.
 '''
+
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
