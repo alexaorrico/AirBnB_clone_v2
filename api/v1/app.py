5742812d80bc6f56998e97dcae5085b69c5de905
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 18:52:38 +0100

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,14 @@ from flask import Flask
 from models import storage
 from os import getenv
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def app_teardown():
+    """ calls storage.close() """
+    storage.close()
 
 if __name__ == "__main__":
     HBNB_API_HOST = getenv("HBNB_API_HOST")
