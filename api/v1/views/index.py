3f64ea5472cdecce1d0f362c39eb9015f16a6c84
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 21:18:59 +0100

    building endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,8 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 from models.amenity import Amenity
+from models.city import City
+from 
 
 
 @app_views.route('/status')
