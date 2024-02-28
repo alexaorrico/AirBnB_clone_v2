3f956081278796a8451582d73067b6c6e9bbf7f9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 22:04:03 +0100

    states objects to API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,9 +3,10 @@
 
 from api.v1.views import app_views
 from models.state import State
-from mo
+from models import storage
 
 
 @app_views.route('/states', methods=['GET'])
 def all(states):
     """ Returns a JSON of State objects """
+    
