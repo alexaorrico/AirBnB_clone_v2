6019aef24db4fbc6ced45ea8ed7274d16d6e0cc9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 18:25:41 +0100

    flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     HBNB_API_HOST = getenv("HBNB_API_HOST")
+    HBNB_API_PORT = getenv("HBNB_API_PORT")
     if HBNB_API_HOST is None:
-        HBNB_API_HOST = ""
+        HBNB_API_HOST = "0.0.0.0"
+    if 
     app.run(host=)
