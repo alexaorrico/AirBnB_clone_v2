67d3b78bd1a2f00dee1615eaead8af45b52ad7af
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 22:35:36 +0100

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -28,7 +28,7 @@ def one_state(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete_state(state_id):
     """ Deletes an obj whose id was passed """
-    
-    if storage.get(State, state_id) is None:
+    obj = storage.get(State, state_id)
+    if obj is None:
         return make_response(jsonify({"error": "Not found"}), 404)
     storage.delete()
