7584e7f76db0070e65ce491d890d6b5a402ddbf5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 21:20:24 +0100

    building endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,6 +9,7 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
