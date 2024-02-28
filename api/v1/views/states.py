197204466f1fddba812f493b7150efdfb650797f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 22:09:28 +0100

    states objects to API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,6 +2,7 @@
 """ Handles all State requests for the API """
 
 from api.v1.views import app_views
+from flask import jsonify
 from models.state import State
 from models import storage
 
@@ -12,4 +13,4 @@ def all(state):
     states = []
     for obj in storage.all(State).values():
         states.append(obj.to_dict())
-    
+    return jsonify(states)
