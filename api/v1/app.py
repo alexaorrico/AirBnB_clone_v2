36d7495d610638c5a6aa9384f82fbc68e9ac7a05
Tommytinny <tommyteenboi@gmail.com>
2024-02-28 13:53:40 -0500

    route

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pytho3
+"""
+endpoint (route) will be to return the status of your API
+"""
+from models import storage
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close_storage(exception=None):
+    """
+    Function to be called when the application context is torn down.
+    Closes the SQLAlchemy session.
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+index file
+"""
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route("/status", strict_slashes=False)
+def status():
+    """
+    Return a JSON response with status OK
+    """
+    return jsonify({"status": "OK"})
