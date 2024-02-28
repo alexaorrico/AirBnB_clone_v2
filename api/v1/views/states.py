a3af22972626c27d9d3ebf03f714b3f6737caae0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 23:23:45 +0100

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -52,7 +52,7 @@ def add_state(state):
 @app_views.route('/states/<state_id>', methods=['PUT'])
 def update_state(state_id):
     """ Updates a state obj with the dict from HTTP request """
-    
+    obj = 
     try:
         new = request.get_json()
     except Exception:
