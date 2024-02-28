f91ec8c2f781815d52f8c1ec9185f8a92f21434c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-29 00:08:41 +0100

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -25,7 +25,8 @@ def one_state(state_id):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-@app_views.route('/states/<string:state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """ Deletes an obj whose id was passed """
     obj = storage.get(State, state_id)
@@ -49,7 +50,8 @@ def add_state(state):
     return jsonify(obj.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_state(state_id):
     """ Updates a state obj with the dict from HTTP request """
     obj = storage.get(State, state_id)
