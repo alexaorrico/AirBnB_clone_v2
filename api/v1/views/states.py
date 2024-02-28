c1771d197035ec73e27570b7bd8dc21f45858b27
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-28 22:36:19 +0100

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -31,4 +31,5 @@ def delete_state(state_id):
     obj = storage.get(State, state_id)
     if obj is None:
         return make_response(jsonify({"error": "Not found"}), 404)
-    storage.delete()
+    storage.delete(obj)
+    
