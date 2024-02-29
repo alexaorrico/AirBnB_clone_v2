a986e3646cdcc61a9b0f5ba1837fa07f7b9debdb
Tommytinny <tommyteenboi@gmail.com>
2024-02-29 00:04:44 -0500

    added new files

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,13 @@
 """
 index file
 """
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -12,3 +19,17 @@ def status():
     Return a JSON response with status OK
     """
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """
+    endpoint that retrieves the number of each objects by type
+    """
+    classes = {"Amenity": Amenity, "City": City,
+               "Place": Place, "Review": Review, "State": State, "User": User}
+    objs = {}
+    for cls in classes:
+        count = storage.count(classes[cls])
+        objs[cls] = int(count)
+    
+    return jsonify(objs)
