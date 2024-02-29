1ba1876974d64eb83138b897681ca4547107956b
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-02-29 20:20:45 +0100

    fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,8 +3,8 @@
 
 from api.v1.views import app_views
 from flask import jsonify, make_response, request, abort
-from models.state import State
 from models import storage
+from models.state import State
 
 
 @app_views.route('/states', strict_slashes=False)
