4e53cfce82df5f7498c073b4cb39dd8337f6f0a6
root <deantoshdaiddoh@gmail.com>
2024-02-29 20:05:43 -0800

    package initializer updated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,12 @@
+#!/usr/bin/python3
+"""
+Package initializer for app_views blueprint
+"""
 from flask import Blueprint
 
 # define a blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# avoid circular import
+# imports all the routes of app_views blueprint
 from api.v1.views.index import *
+from api.v1.views.states import *
