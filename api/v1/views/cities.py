b3098ecdc76b33dbc7d672b4fd69ea15de130449
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-01 23:01:03 +0100

    creating a new view for cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -13,5 +13,7 @@ from models.state import State
 def all_cities(state_id):
     """ Returns all cities linked to a particular city """
     city_objs = storage.all(City)
+    cities = []
     for obj in city_objs.values():
-        if obj.state_id == state_id
+        if obj.state_id == state_id:
+            cities
