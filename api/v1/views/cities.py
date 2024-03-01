55be5de4db119df749c259f69ba37c9339344dd9
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-01 22:50:32 +0100

    creating a new view for cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,10 +2,12 @@
 """ New City view """
 
 from api.v1.views import app_views
+from models import storage
 from models.city import City
 from models.state import State
 
 
 @app_views.route('/api/v1/states/<state_id>/cities', methods=['GET'],
                  strict_slashes=False)
-def all_cities(state_id)
+def all_cities(state_id):
+    """ """
