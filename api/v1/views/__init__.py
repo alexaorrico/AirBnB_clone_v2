2ec2f7a0c3f6d02163585bb6f6bc1c2825b6ecea
root <deantoshdaiddoh@gmail.com>
2024-03-04 09:44:57 -0800

    initializer package: updated

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,4 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 # imports modules of app_views blueprint
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
