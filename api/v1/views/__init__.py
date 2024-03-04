3fef5d7b901529f0e061c320551f78cfab9b2745
root <deantoshdaiddoh@gmail.com>
2024-03-04 05:17:12 -0800

    state objects view: added

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,6 +7,6 @@ from flask import Blueprint
 # define a blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# imports all the routes of app_views blueprint
+# imports modules of app_views blueprint
 from api.v1.views.index import *
 from api.v1.views.states import *
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,45 +9,44 @@ from werkzeug.exceptions import BadRequest
 
 
 @app_views.route("/states", strict_slashes=False)
-def retrieve_all_objs():
-    """returns a list of all state objects"""
-    state_dict = storage.all(State)
+def get_states_objects():
+    """returns: a list of all state objects"""
+    states = storage.all(State)
     states_list = []
-    for obj in state_dict.values():
-        obj_dict = obj.to_dict()
-        states_list.append(obj_dict)
+    for state in states.values():
+        states_list.append(state.to_dict())
     return states_list
 
 
 @app_views.route("states/<state_id>", strict_slashes=False)
-def retrieve_obj(state_id):
-    """returns an object of a specified id"""
-    state_dict = storage.all(State)
-    for obj in state_dict.values():
-        if obj.id == state_id:
-            return obj.to_dict()
+def get_state_object(state_id):
+    """returns: an object of a specified id"""
+    states = storage.all(State)
+    for state in states.values():
+        if state.id == state_id:
+            return state.to_dict()
     # obj not found
     abort(404)
 
 
 @app_views.route("/states/<state_id>",
                  methods=['DELETE'], strict_slashes=False)
-def delete_obj(state_id):
-    """removes an object from storage"""
-    objs = storage.all(State)
-    data = {}
-    for obj in objs.values():
-        if obj.id == state_id:
+def delete_state_object(state_id):
+    """delete: a state object"""
+    states = storage.all(State)
+    for state in states.values():
+        if state.id == state_id:
             # delete obj and save changes to storage
-            storage.delete(obj)
+            storage.delete(state)
             storage.save()
-            return jsonify(data), 200
+            return jsonify({}), 200
     # obj not found
     abort(404)
 
 
 @app_views.route("/states", methods=['POST'], strict_slashes=False)
-def create_state():
+def create_state_object():
+    """create: a state object"""
     try:
         data = request.get_json()
     except BadRequest:
@@ -55,30 +54,28 @@ def create_state():
     if 'name' not in data:
         abort(400, 'Missing name')
     # create state object
-    for v in data.values():
-        state_obj = State(name=v)
-    storage.new(state_obj)
+    new_state = State(**data)
+    storage.new(new_state)
     storage.save()
-    obj_dict = state_obj.to_dict()
-    return jsonify(obj_dict), 201
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route("/states/<state_id>", methods=['PUT'], strict_slashes=False)
 def update_obj(state_id):
-    """update a state object"""
-    objs = storage.all(State)
-    for obj in objs.values():
-        if obj.id == state_id:
+    """update: a state object"""
+    states = storage.all(State)
+    for state in states.values():
+        if state.id == state_id:
             try:
                 data = request.get_json()
             except BadRequest:
                 abort(400, 'Not a JSON')
             for k, v in data.items():
-                # update value
-                setattr(obj, k, v)
+                if k == 'id' or k == 'created_at' or k == 'updated_at':
+                    continue
+                setattr(state, k, v)
             # save changes to storage
             storage.save()
-            state_obj = obj.to_dict()
-            return jsonify(state_obj), 201
+            return jsonify(state.to_dict()), 201
     # object not found
     abort(404)
