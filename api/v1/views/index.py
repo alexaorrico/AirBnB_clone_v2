90dab2a5853866254096462cf678bb476c4081e5
root <deantoshdaiddoh@gmail.com>
2024-03-03 19:12:09 -0800

    delete: route /api/v1/stats -- test purposes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,18 +9,3 @@ from models import storage, Amenity, City, Place, Review, State, User
 def status():
     """return: status ok"""
     return jsonify({"status": "OK"})
-
-
-@app_views.route("/stats", strict_slashes=False)
-def cls_obj_count():
-    """retrieves the number of each objects by type"""
-    data = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-    }
-
-    return jsonify(data)
