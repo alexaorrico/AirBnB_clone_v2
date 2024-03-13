3566b46f0d6132467dc2f54fdab0e3d537408f56
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-13 22:38:55 +0100

    a new view for place_review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -5,9 +5,10 @@ from api.v1.views import app_views
 from flask import abort, Flask, jsonify, make_response
 from models.amenity import Amenity
 from models.place import place_amenity, Place
+from models import storage
 
 
 @app_views.route('/places/<place_id>/reviews', strict_slashes=False)
 def get(place_id):
     """ Returns a list of Reviews objects """
-    
+    place_obj = storage.get()
