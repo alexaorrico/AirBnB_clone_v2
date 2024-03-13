5c69bb83dacdd57b5f85e2b0daf98824f85fa3ed
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-13 22:36:00 +0100

    a new view for place_review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -6,4 +6,5 @@ from flask import abort, Flask, jsonify, make_response
 from models.place import place_amenity
 
 
-@app_views.route()
+@app_views.route('/places/<place_id>/reviews', strict_slashes=False)
+def get()
