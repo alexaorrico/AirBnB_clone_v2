0651d4e501cb84e4c7630439fbabf163e1934156
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-03-13 23:17:07 +0100

    a new view for place_review

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -57,7 +57,8 @@ def createReview(place_id):
             if "user_id" in data:
                 user_obj = storage.get(User, data["user_id"])
                 if user_obj:
-                    data["place_id"] = pl
+                    data["place_id"] = place_id
+                    new_review = Review(**data)
             abort(400, "Missing user_id")
         abort(400, "Not a JSON")
     abort(404)
