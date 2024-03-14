ecb7d22372bc2c60133aefe5656bc7ae53e0ab65
ally2211 <amendieta.va@gmail.com>
2024-03-14 17:32:16 -0500

    add def count to dbstorage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -39,6 +39,7 @@ class DBStorage:
                                              HBNB_MYSQL_DB))
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
+        self.reload()
 
     def all(self, cls=None):
         """query on the current database session"""
@@ -74,3 +75,25 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def close(self):
+            """call remove() method on the private session attribute"""
+            self.__session.remove()
+            
+    def get(self, cls, id):
+        """ retrieve a specific object from db by its class and ID """
+        obj = self.__session.query(cls).filter(cls.id == id).first()
+        if obj is not None:
+            return obj
+        else:
+            None
+        
+    def count(self, cls=None):
+        """ A method to count the number of objects in storage """
+        if cls is None:
+            count_obj = 0
+            for cls in classes.values():  # Accessing the global `classes` dictionary
+                count_obj += self.__session.query(cls).count()
+            return count_obj
+        else:
+            return self.__session.query(cls).count()
