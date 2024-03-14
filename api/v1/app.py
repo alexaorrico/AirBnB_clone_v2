7d9281ea5bbf075b62c6ef1b752cbaa36a10d839
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-03-14 18:16:46 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-""" This is the Flask Application module """
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -7,24 +5,23 @@ from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from flasgger import Swagger
 
-
+# Create Flask app
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
-
+# Teardown app context
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
     storage.close()
 
-
+# 404 Error handler
 @app.errorhandler(404)
 def not_found(error):
     """ 404 Error """
     return make_response(jsonify({'error': "Not found"}), 404)
-
 
 # Configure Swagger
 app.config['SWAGGER'] = {
