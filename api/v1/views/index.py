e7d6453c5c2090eb633fffd33535ba4d4610c702
meddizzle316 <meddizzle316@gmail.com>
2024-03-15 14:55:03 -0500

    pyfix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,11 @@
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
+
 @app_views.route("/status")
 def status():
     status = {
         "status": "OK"
     }
-    
+
     return jsonify(status)
