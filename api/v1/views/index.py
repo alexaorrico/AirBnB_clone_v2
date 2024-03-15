e1af2f027fa11049a2b66978f409260b12687e5c
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-03-15 21:43:55 +0000

    make stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,31 @@
 """ Temp """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
+    """ Retrieve API Status """
+
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Retrieve stats of objects, by class """
+
+    classes = {
+        'amenities': 'Amenity',
+        'cities': 'City',
+        'places': 'Place',
+        'reviews': 'Review',
+        'states': 'State',
+        'users': 'User'
+    }
+
+    stats = {}
+    for cls_name, cls in classes.items():
+        count = storage.count(cls)
+        stats[cls_name] = count
+    return jsonify(stats)
