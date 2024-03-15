c3f2e25d66610a6537c1146ebbd7811c1adc246c
Cody Walenciak <codywalenciak@gmail.com>
2024-03-15 19:12:33 +0000

    Added documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module containing API definitions"""
+"""Module containing API engine"""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,7 @@
 from api.v1.views import app_views
 
+"""Module containing API routes"""
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     res = {"status": "OK"}
