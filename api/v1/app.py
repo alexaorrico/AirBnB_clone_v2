c63d320ffa513c394cf2bca4cd00198d1c37ce73
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-16 15:58:23 -0500

    pulling from master

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,9 +4,11 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -19,6 +21,7 @@ def close_db(exception):
 def not_found(error):
     response = jsonify({'error': 'Not Found'})
     response.status_code = 404
+    response.headers['Content-Type'] = 'application/json'
     return response
 
 
