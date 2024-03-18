fe5c06001cd52741d52b18b23f2adb1190a0350d
Luke Schula <schulaluke@gmail.com>
2024-03-18 13:30:57 -0500

    created script to start web app and tested

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,5 +4,26 @@ from flask import Flask, jsonify, make_response
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
+app = Flask(__name__)
+
+
+app.register_blueprint(app_views)
+
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def close(self):
+    """closes session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(
+            host=os.getenv("HBNB_API_HOST", '0.0.0.0'),
+            port=os.getenv("HBNB_API_PORT",5000),
+            threaded=True,
+            debug=True
+            )
