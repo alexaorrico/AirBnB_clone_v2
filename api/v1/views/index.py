1b64c3596cabb46918b3195f83213f243d107e15
Luke Schula <schulaluke@gmail.com>
2024-03-18 14:45:26 -0500

    added route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,3 +16,17 @@ def status():
     """return status: OK"""
     return jsonify({"status": "OK"})
 
+
+@app_views.route('/stats')
+def stats():
+    """returns count of models"""
+    return jsonify(
+        {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review")
+            "states": storage.count("State"),
+            "users": storage.count("User")
+        }
+    )
