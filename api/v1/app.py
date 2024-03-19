23de8d906701445366d0cbca9e6cc68290b68912
Luke Schula <schulaluke@gmail.com>
2024-03-19 11:38:52 -0500

    rework app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-import os
+from os import getenv
 
 app = Flask(__name__)
 
 
 app.register_blueprint(app_views)
 
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close(self):
+def close(exception):
     """closes session"""
     storage.close()
 
@@ -23,13 +23,10 @@ def close(self):
 @app.errorhandler(404)
 def error_message(error):
     """error message"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == '__main__':
-    app.run(
-            host=os.getenv("HBNB_API_HOST", '0.0.0.0'),
-            port=os.getenv("HBNB_API_PORT",5000),
-            threaded=True,
-            debug=True
-            )
+    API_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    API_PORT = getenv("HBNB_API_PORT", 5000)
+    app.run(host=API_HOST, port=API_PORT, threaded=True,debug=True)
