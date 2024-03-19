b04ea4fdc3f3a78bf330d97e183b39a45423750e
AdrianWolfP <wolfy52032@gmail.com>
2024-03-19 03:32:11 -0500

    fixing an error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+
 """ Flask Application """
 from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, render_template, make_respone, jsonify
 from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
@@ -29,21 +28,3 @@ def not_found(error):
         description: a resourse was not found
     """
     return make_respone(jsonify({'error': "Not found"}), 404)
-
-app.config['SWAGGER'] = {
-    'title': 'Airbnb clone Restful API'
-    'uiversion': 3
-}
-
-Swagger(app)
-
-
-if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('Hbnb_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
