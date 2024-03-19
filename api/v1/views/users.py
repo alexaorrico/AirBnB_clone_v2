e61460d386eccc9e91b8c1203732c41f4fc47055
NolbsAtHolbs <hikikomode@gmail.com>
2024-03-19 18:31:49 -0500

    boilerplate

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+"""Users object module that handles all default RESTFul API actions"""
+from api.v1.views import app_views
+from flask import jsonify, request, current_app, abort
+from models import storage
+from models.user import User
