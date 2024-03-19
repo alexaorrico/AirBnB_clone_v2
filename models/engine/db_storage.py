f827b27eead92508b4b3fd57fd20610ac72bda20
Kyle Headley <143755756+Y-T-O-1@users.noreply.github.com>
2024-03-19 17:19:01 -0500

    updated files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,5 +72,17 @@ class DBStorage:
         self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """Call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Method to retrieve one specific object"""
+        key = cls.__name__ + "." + id
+        try:
+            return self.all()[key]
+        except KeyError:
+            return None
+
+    def count(self, cls=None):
+        """Count current number of class instances"""
+        return len(self.all(cls))
