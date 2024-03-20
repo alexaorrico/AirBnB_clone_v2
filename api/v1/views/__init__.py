ccdd2062c11ad3c8610fbbc42bf54d755b7d58af
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-19 20:24:14 -0500

    moved imports to top of file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,11 +2,11 @@
 """blue print veiws"""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
 from api.v1.views.index import *
 from api.v1.views.users import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places import *
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
