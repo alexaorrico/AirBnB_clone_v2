1e5ef50be86c8304744993c809bfd084614ad6d4
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-19 21:45:40 -0500

    changed wording in app.py, deleted pycache

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+import os
 from os import getenv
 from flask_cors import CORS
 
@@ -26,6 +27,6 @@ def page_not_found(error):
 
 
 if __name__ == "__main__":
-    host1 = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port1 = getenv('HBNB_API_PORT', default=5000)
-    app.run(host=host1, port=port1, threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
