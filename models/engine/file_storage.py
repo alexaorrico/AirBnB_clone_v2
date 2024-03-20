f449a2929c0dedcf943ba6fa86ea36da4651798a
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-03-19 21:04:47 -0500

    added get and count methods to db and file storage, added unit tests for both.
    added git ignore

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,8 +55,12 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
-            pass
+        except FileNotFoundError:
+            print("File not found")
+        except json.JSONDecodeError:
+            print("Error decoding JSON")
+        except TypeError:
+            print("TypeError occurred")
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
@@ -68,3 +72,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Retrieve one object based on class and ID."""
+        key = cls.__name__ + "." + id
+        return self.__objects.get(key, None)
+
+    def count(self, cls=None):
+        """
+        Count the number of objects in storage,
+        optionally filtered by class."""
+        if cls is None:
+            return len(self.__objects)
+        else:
+            return sum(1 for key, value in self.__objects.items()
+                       if value.__class__ == cls)
